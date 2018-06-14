0d9470616203469407960096a61d52a3d77e46dd
Even Falch-Larsen <even.falch-larsen@nordicsemi.no>
2018-06-14 05:56:44 -0700

    Prototyping NRF connect structure

---
+++ samples/bluetooth/gatt/nus.c
@@ -0,0 +1,79 @@
+#include <bluetooth/conn.h>                                                                                                            
+#include <bluetooth/uuid.h>                                                                                                            
+#include <bluetooth/gatt.h> 
+
+#include <gatt/nus.h>
+
+static struct bt_gatt_ccc_cfg nuslc_ccc_cfg[BT_GATT_CCC_MAX] = {};                                                                     
+static u8_t                   notify_enabled = 0;
+static void                   (*nus_receive_cb)(u8_t);
+static void                   (*nus_send_cb)(u8_t);
+                                                                                                                                       
+#define BT_UUID_NUS_SERVICE   BT_UUID_DECLARE_128(NUS_UUID_SERVICE)                                                                    
+#define BT_UUID_NUS_RX        BT_UUID_DECLARE_128(NUS_UUID_NUS_RX_CHAR)                                                                
+#define BT_UUID_NUS_TX        BT_UUID_DECLARE_128(NUS_UUID_NUS_TX_CHAR)
+
+static void nuslc_ccc_cfg_changed(const struct bt_gatt_attr *attr,
+                                 u16_t value)
+{                                                                                                                                      
+        notify_enabled = (value == BT_GATT_CCC_NOTIFY) ? 1 : 0;
+}
+                                                                                                                                       
+static ssize_t on_receive(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+                          const void *buf, u16_t len, u16_t offset,
+                          u8_t flags)                                                                                                    
+{
+        nus_receive_cb(*(u8_t*)buf);
+        return len;
+}
+
+static ssize_t on_send(struct bt_conn *conn,
+                       const struct bt_gatt_attr *attr,
+                       void *buf, u16_t len,
+                       u16_t offset)
+{
+        printk("Data sendt (Char:%c)\n",*(char*)buf);
+        nus_send_cb(*(u8_t*)buf);
+
+        return len;
+}
+
+
+/* Heart Rate Service Declaration */
+static struct bt_gatt_attr attrs[] = {
+        BT_GATT_PRIMARY_SERVICE(BT_UUID_NUS_SERVICE),
+        BT_GATT_CHARACTERISTIC(BT_UUID_NUS_TX,
+                               BT_GATT_CHRC_NOTIFY,
+                               BT_GATT_PERM_READ,
+                               on_send, NULL, NULL),
+        BT_GATT_CCC(nuslc_ccc_cfg, nuslc_ccc_cfg_changed),                                       
+        BT_GATT_CHARACTERISTIC(BT_UUID_NUS_RX,
+                               BT_GATT_CHRC_WRITE | BT_GATT_CHRC_WRITE_WITHOUT_RESP,
+                               BT_GATT_PERM_READ | BT_GATT_PERM_WRITE, NULL, on_receive, NULL),
+};
+
+static struct bt_gatt_service nus_svc = BT_GATT_SERVICE(attrs);
+
+void nus_init(void *receive_cb, void *send_cb)
+{
+        nus_receive_cb = receive_cb;
+        nus_send_cb    = send_cb;
+        bt_gatt_service_register(&nus_svc);
+}
+
+void nus_data_send(u8_t data)
+{
+        int err;
+
+        if (!notify_enabled) {
+                return;
+        }
+
+        err = bt_gatt_notify(NULL, &attrs[2], &data, sizeof(data));
+
+        if(err) {
+                printk("Sending data (%d) failed (err:%d)\n", data, err);
+        }
+}
+
+
---
+++ samples/bluetooth/peripheral_uart/src/main.c
@@ -0,0 +1,173 @@
+#include <zephyr/types.h>
+#include <zephyr.h>
+#include <uart.h>
+
+#include <board.h>
+#include <gpio.h>
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/uuid.h>
+#include <bluetooth/gatt.h>
+#include <bluetooth/hci.h>
+
+#include <gatt/nus.h>
+
+#include <stdio.h>
+
+#define TEST_MACRO(string) string
+
+#define DEVICE_NAME		CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN		(sizeof(DEVICE_NAME) - 1)
+
+/* Change this if you have an LED connected to a custom port */
+#ifndef LED0_GPIO_CONTROLLER
+#define LED0_GPIO_CONTROLLER    LED0_GPIO_PORT
+#endif
+
+#define LED_PORT                LED0_GPIO_CONTROLLER
+
+#define RUN_STATUS_LED          TEST_MACRO(CONFIG_RUN_STATUS_LED)
+#define CON_STATUS_LED          CONFIG_CON_STATUS_LED
+
+#define LED_BLINK_INTERVAL      1000
+
+#define LED_ON                  0
+#define LED_OFF                 1
+
+static struct bt_conn *default_conn;
+static struct device  *led_port;
+static struct device  *uart;
+static u8_t           uart_data = 0;
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, NUS_UUID_SERVICE),
+};
+
+static void connected(struct bt_conn *conn, u8_t err)                                                                                  
+{                                                                                                                                      
+        if (err) {                                                                                                                     
+                printk("Connection failed (err %u)\n", err);                                                                           
+        } else {                                                                                                                       
+                default_conn = bt_conn_ref(conn);                                                                                      
+                printk("Connected\n");                                                                                                 
+        }                                               
+
+        default_conn = bt_conn_ref(conn);
+        gpio_pin_write(led_port, CON_STATUS_LED, LED_ON);
+}          
+
+static void disconnected(struct bt_conn *conn, u8_t reason)                                                                            
+{                                                                                                                                      
+        printk("Disconnected (reason %u)\n", reason);                                                                                  
+                                                                                                                                       
+        if (default_conn) {                                                                                                            
+                bt_conn_unref(default_conn);                                                                                           
+                default_conn = NULL;                                                                                                   
+        }
+
+        gpio_pin_write(led_port, CON_STATUS_LED, LED_OFF);
+}          
+
+static struct bt_conn_cb conn_callbacks = {                                                                                            
+        .connected = connected,
+        .disconnected = disconnected,
+};
+
+void bt_usart_write_cb(u8_t data)
+{
+        printk("Got data over bluetooth (%c)\n", data);
+
+        //uart_fifo_fill(uart,data,1);
+}
+
+void bt_usart_sendt_cb(u8_t data)
+{
+        printk("Data was sendt over bluetooth (%c)\n", data);
+}
+
+void bt_ready(int err)
+{
+	if(err) {
+		printk("BT initialization failed (err %d)\n", err);
+		return;
+	}
+
+	printk("BT initialization OK\n");
+
+	nus_init(bt_usart_write_cb, bt_usart_sendt_cb);
+
+	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad),
+			      sd, ARRAY_SIZE(sd));
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Advertising successfully started\n");
+}
+
+void init_leds() 
+{
+        led_port = device_get_binding(LED_PORT);
+        if (!led_port) {
+                printk("Could not bind to LED port\n");
+                return;
+        }
+
+        gpio_pin_configure(led_port, RUN_STATUS_LED,
+                           GPIO_DIR_OUT);
+        gpio_pin_write(led_port, RUN_STATUS_LED, LED_OFF);
+
+        gpio_pin_configure(led_port, CON_STATUS_LED,
+                           GPIO_DIR_OUT);
+        gpio_pin_write(led_port, CON_STATUS_LED, LED_OFF);
+}
+
+/*void uart_cb(struct device *uart)
+{
+        int len=0;
+        u8_t buf[UART_MAX_RX_SIZE];
+
+        if(uart_irq_rx_ready(uart)) {
+                len = uart_fifo_read(uart, buf, UART_MAX_RX_SIZE);
+        }
+}
+
+void init_usart()
+{
+        uart = device_get_binding(USART0);
+        if(!led_port) {
+                printk("Could not bind to USART module\n");
+                return;
+        }
+
+        uart_irq_callback_set(uart,uart_cb);
+        uart_irq_rx_enable(uart);
+}*/
+
+int main()
+{
+        int blink_status = 0;
+
+        printf("Starting Nordic UART service example\n");
+
+        init_leds();
+
+	bt_enable(bt_ready);
+        bt_conn_cb_register(&conn_callbacks);
+
+        for(;;) {
+                gpio_pin_write(led_port, RUN_STATUS_LED, (++blink_status) % 2);
+                k_sleep(LED_BLINK_INTERVAL);
+
+                nus_data_send(uart_data++);
+        }
+
+	return 0;
+}
+
