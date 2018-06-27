1bc62aa2a1ba82e076ef8f69d147fdf583fa1728
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2018-06-27 13:20:40 +0200

    boards: arm: Add board PCA20041
    This commit adds nRF52 Desktop project specific board. Note that the
    board definition is entirely temporary and will be modified later to
    match the HW.
    
    Co-authored-by: Kubicz, Filip <filip.kubicz@nordicsemi.no>
    Co-authored-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

---
+++ boards/arm/nrf52_pca20041/board.cmake
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2018 Nordic Semiconductor
+#
+# SPDX-License-Identifier: BSD-5-Clause-Nordic
+#
+
+board_runner_args(nrfjprog "--nrf-family=NRF52")
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
---
+++ boards/arm/nrf52_pca20041/board.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: BSD-5-Clause-Nordic
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+#endif /* __INC_BOARD_H */
