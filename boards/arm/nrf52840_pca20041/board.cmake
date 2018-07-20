c522454fe867cad2d0f3285ec86a56da5c62fccb
jhn-nordic <jon.helge.nistad@nordicsemi.no>
2018-07-20 01:48:28 -0700

    boards: arm: nrf52840_pca20041: Added soft reset.
    The nrf52840_pca20041 has no pin reset support in HW.
    This change will prevent nrfjprog to enable pinreset during
    programming to target, and also reset the target properly after
    programming.
    
    Signed-off-by: Jon Helge Nistad <jon.helge.nistad@nordicsemi.no>

--- boards/arm/nrf52840_pca20041/board.cmake
+++ boards/arm/nrf52840_pca20041/board.cmake
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: BSD-5-Clause-Nordic
 #
 
-board_runner_args(nrfjprog "--nrf-family=NRF52")
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 board_runner_args(jlink "--device=nrf52" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
