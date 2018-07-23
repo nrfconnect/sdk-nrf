8ff806bd029279e9d94fe3e8d00ca3c0f1d4fd93
Filip Kubicz <filip.kubicz@nordicsemi.no>
2018-07-23 14:30:56 +0200

    samples: nrf_desktop: HID key map configuration
    Allow application for each board to choose HID key mapping
    independently from the hardware.
    
    Jira: DESK-189
    
    Signed-off-by: Filip Kubicz <filip.kubicz@nordicsemi.no>

---
+++ samples/nrf_desktop/configuration/hid_keymap_pca20041.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: BSD-5-Clause-Nordic
+ */
+
+#include <misc/util.h>
+
+#include "hid_keymap.h"
+
+/*
+ * HID keymap. The Consumer Control keys are defined in section 15 of
+ * the HID Usage Tables document under the following URL:
+ * http://www.usb.org/developers/hidpage/Hut1_12v2.pdf
+ */
+const struct hid_keymap hid_keymap[] = {
+	/*
+	 * Intentionally empty.
+	 * Should be filled to add mouse buttons support to PCA20041.
+	 */
+};
+
+const size_t hid_keymap_size = ARRAY_SIZE(hid_keymap);
+
