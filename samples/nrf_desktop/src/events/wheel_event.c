5607ffadbb498074d3e53304f4e4ef6409d2b208
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2018-08-09 15:59:34 +0200

    samples: nrf_desktop: Add wheel event
    Change adds a wheel event and code to handle it in hid state
    and service.
    
    Jira:DESK-202
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

---
+++ samples/nrf_desktop/src/events/wheel_event.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: BSD-5-Clause-Nordic
+ */
+
+#include "wheel_event.h"
+
+static void print_event(const struct event_header *eh)
+{
+	struct wheel_event *event = cast_wheel_event(eh);
+
+	printk("wheel=%d", event->wheel);
+}
+
+EVENT_TYPE_DEFINE(wheel_event, print_event);
---
+++ samples/nrf_desktop/src/events/wheel_event.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: BSD-5-Clause-Nordic
+ */
+
+#ifndef _WHEEL_EVENT_H_
+#define _WHEEL_EVENT_H_
+
+/**
+ * @brief Wheel Event
+ * @defgroup wheel_event Wheel Event
+ * @{
+ */
+
+#include "event_manager.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct wheel_event {
+	struct event_header header;
+
+	s16_t wheel;
+};
+
+EVENT_TYPE_DECLARE(wheel_event);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _WHEEL_EVENT_H_ */
