bf0fc351f9cffcd299873ba081c46d27534385a4
Marek Pieta <Marek.Pieta@nordicsemi.no>
2018-08-28 11:16:09 +0200

    samples: nrf_desktop: Added event tracing functions
    Change adds functions to trace event submits for button_event,
    motion_event, keep_active_event, hid_mouse_xy_event,
    hid_mouse_wp_event.
    
    Jira: DESK-203
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- samples/nrf_desktop/src/events/button_event.c
+++ samples/nrf_desktop/src/events/button_event.c
@@ -15,4 +15,16 @@ static void print_event(const struct event_header *eh)
 			(event->pressed)?("pressed"):("released"));
 }
 
-EVENT_TYPE_DEFINE(button_event, print_event, NULL);
+static void log_args(struct log_event_buf *buf, const struct event_header *eh)
+{
+	struct button_event *event = cast_button_event(eh);
+
+	ARG_UNUSED(event);
+	profiler_log_encode_u32(buf, event->key_id);
+	profiler_log_encode_u32(buf, (event->pressed)?(1):(0));
+}
+
+EVENT_INFO_DEFINE(button_event, ENCODE(PROFILER_ARG_U32, PROFILER_ARG_U32),
+			ENCODE("button_id", "status"), log_args);
+
+EVENT_TYPE_DEFINE(button_event, print_event, &button_event_info);
--- samples/nrf_desktop/src/events/hid_event.c
+++ samples/nrf_desktop/src/events/hid_event.c
@@ -10,8 +10,36 @@ static void print_event(const struct event_header *eh)
 {
 }
 
+static void log_args_mouse_xy(struct log_event_buf *buf,
+			     const struct event_header *eh)
+{
+	struct hid_mouse_xy_event *event = cast_hid_mouse_xy_event(eh);
+
+	ARG_UNUSED(event);
+	profiler_log_encode_u32(buf, event->dx);
+	profiler_log_encode_u32(buf, event->dy);
+}
+
+static void log_args_mouse_wp(struct log_event_buf *buf,
+			     const struct event_header *eh)
+{
+	struct hid_mouse_wp_event *event = cast_hid_mouse_wp_event(eh);
+
+	ARG_UNUSED(event);
+	profiler_log_encode_u32(buf, event->wheel);
+	profiler_log_encode_u32(buf, event->pan);
+}
 
 EVENT_TYPE_DEFINE(hid_keyboard_event, print_event, NULL);
-EVENT_TYPE_DEFINE(hid_mouse_xy_event, print_event, NULL);
-EVENT_TYPE_DEFINE(hid_mouse_wp_event, print_event, NULL);
+
+EVENT_INFO_DEFINE(hid_mouse_xy_event,
+		  ENCODE(PROFILER_ARG_S32, PROFILER_ARG_S32),
+		  ENCODE("dx", "dy"), log_args_mouse_xy);
+EVENT_TYPE_DEFINE(hid_mouse_xy_event, print_event, &hid_mouse_xy_event_info);
+
+EVENT_INFO_DEFINE(hid_mouse_wp_event,
+		  ENCODE(PROFILER_ARG_S32, PROFILER_ARG_S32),
+		  ENCODE("wheel", "pan"), log_args_mouse_wp);
+EVENT_TYPE_DEFINE(hid_mouse_wp_event, print_event, &hid_mouse_wp_event_info);
+
 EVENT_TYPE_DEFINE(hid_mouse_button_event, print_event, NULL);
