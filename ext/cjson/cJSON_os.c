b68f3ac6456a1516fbf9d5ea01aca7bdbfbede26
Joakim Andre Tønnesen <joakim.tonnesen@nordicsemi.no>
2018-09-19 10:53:18 +0200

    ext: Add cJSON lib
    Adding the cJSON library as a dependency needed by nRF Cloud library.
    
    Signed-off-by: Joakim Andre Tønnesen <joakim.tonnesen@nordicsemi.no>
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

---
+++ ext/cjson/cJSON_os.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include "cJSON_os.h"
+#include "cJSON.h"
+#include <stdint.h>
+#include <zephyr.h>
+
+static cJSON_Hooks _cjson_hooks;
+
+/**@brief malloc() function definition. */
+static void *malloc_fn_hook(size_t sz) { return k_malloc(sz); }
+
+/**@brief free() function definition. */
+static void free_fn_hook(void *p_ptr) { k_free(p_ptr); }
+
+/**@brief Initialize cJSON by assigning function hooks. */
+void cJSON_Init(void)
+{
+	_cjson_hooks.malloc_fn = malloc_fn_hook;
+	_cjson_hooks.free_fn = free_fn_hook;
+
+	cJSON_InitHooks(&_cjson_hooks);
+}
---
+++ ext/cjson/cJSON_os.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#ifndef cJSON_OS_H__
+#define cJSON_OS_H__
+
+#include <stdint.h>
+
+/**
+ * @brief Initialize cJSON with OS hooks.
+ */
+void cJSON_Init(void);
+
+#endif /* cJSON_OS_H__ */
