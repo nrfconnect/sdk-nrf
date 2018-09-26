f3c7ac68a135b072ca4b42280ec0d53ab2a6839b
Joakim Andersson <joakim.andersson@nordicsemi.no>
2018-09-26 14:08:37 +0200

    cmake: Support BOARD from environment variable in samples
    Environment variable BOARD does not work in the nrf_desktop sample.
    The zephyr handling of BOARD cannot be reused because we need to set
    BOARD_ROOT for out of tree boards.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

---
+++ cmake/boilerplate.cmake
@@ -0,0 +1,27 @@
+#
+# Copyright (c) 2018 Nordic Semiconductor
+#
+# SPDX-License-Identifier: BSD-5-Clause-Nordic
+#
+
+if(NOT BOARD)
+        set(BOARD $ENV{BOARD})
+endif()
+
+# Check if selected board is supported.
+if(DEFINED NRF_SUPPORTED_BOARDS)
+        if(NOT BOARD IN_LIST NRF_SUPPORTED_BOARDS)
+                message(FATAL_ERROR "board ${BOARD} is not supported")
+        endif()
+endif()
+
+# Point to NCS root directory.
+set(NRF_DIR ${CMAKE_CURRENT_LIST_DIR}/..)
+
+# Set BOARD_ROOT if board definition exists out of tree.
+find_path(_BOARD_DIR NAMES "${BOARD}_defconfig" PATHS ${NRF_DIR}/boards/*/*
+          NO_DEFAULT_PATH)
+
+if (_BOARD_DIR)
+        set(BOARD_ROOT ${NRF_DIR})
+endif()
