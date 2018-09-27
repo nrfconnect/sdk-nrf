2f859a5313be8e9d98d79cfb0c5e44c77b1e26bb
Marek Pieta <Marek.Pieta@nordicsemi.no>
2018-09-27 11:49:58 +0200

    samples: nrf_desktop: Add led event
    Change adds led event for hardware interface.
    
    Jira: DESK-211
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ samples/nrf_desktop/src/events/led_event.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: BSD-5-Clause-Nordic
+ */
+
+#include <misc/printk.h>
+
+#include "led_event.h"
+
+static const char * const mode_name[] = {
+#define X(name) STRINGIFY(name),
+	LED_MODE_LIST
+#undef X
+};
+
+static void print_event(const struct event_header *eh)
+{
+	struct led_event *event = cast_led_event(eh);
+
+	printk("led_id:%u mode:%s period:%u color: <", event->led_id,
+		mode_name[event->mode], event->period);
+	for (size_t i = 0; i < sizeof(event->color.c); i++) {
+		printk(" %u", event->color.c[i]);
+	}
+	printk(" >");
+}
+
+EVENT_TYPE_DEFINE(led_event, print_event, NULL);
