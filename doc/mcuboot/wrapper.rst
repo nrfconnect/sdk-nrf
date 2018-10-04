3c7c9c62fab671879aa0fd711e3d529d78323444
Ruth Fuchss <ruth.fuchss@nordicsemi.no>
2018-10-04 13:40:37 +0200

    documentation: Include mcuboot in documentation
    Add Sphinx configuration and wrapper file for generating documentation
    from mcuboot markdown files.
    Update CMakeLists and themes to add a target for mcuboot.
    Update documentation to include mcuboot.
    
    Signed-off-by: Ruth Fuchss <ruth.fuchss@nordicsemi.no>

---
+++ doc/mcuboot/wrapper.rst
@@ -0,0 +1,21 @@
+.. _mcuboot:
+
+MCUboot documentation
+#####################
+
+.. toctree::
+   :maxdepth: 1
+
+   index.md
+   release-notes.md
+   design.md
+   signed_images.md
+   imgtool.md
+   readme-zephyr.md
+   readme-mynewt.md
+   readme-riot.md
+   SubmittingPatches.md
+   testplan-zephyr.md
+   testplan-mynewt.md
+   release.md
+   PORTING.md
---
+++ doc/scripts/fix_markdown.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2018 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+#
+#
+# Script to go through all *.md files in a folder and replace occurrences
+# of ".md)" with ".html)" and delete comments.
+# Sphinx transforms .md files to .html files, but it does not fix the links
+# so they break.
+# Sphinx has difficulties parsing multiple comments in a file.
+
+import argparse
+import glob
+import fileinput
+import sys
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('dir', nargs=1)
+    args = parser.parse_args()
+
+    files = glob.glob(args.dir[0] + "/*.md")
+
+    comment = 0
+
+    for line in fileinput.input(files, inplace=1):
+      if "<!--" in line:
+         comment = 1
+
+      if "-->" in line:
+         comment = 0
+         line = ""
+
+      line = line.replace(".md)",".html)")
+
+      if comment:
+         line = ""
+
+      sys.stdout.write(line)
+
+
+if __name__ == "__main__":
+    main()
