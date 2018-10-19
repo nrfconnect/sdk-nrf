a8ebb7981762574395fb5567c86248162167d56c
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2018-10-19 17:11:21 +0200

    samples: nrf_desktop: Handle charging state
    Jira:DESK-198
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

--- samples/nrf_desktop/src/modules/led_state.c
+++ samples/nrf_desktop/src/modules/led_state.c
@@ -11,6 +11,7 @@
 
 #include "led_event.h"
 #include "ble_event.h"
+#include "battery_event.h"
 
 #define SYS_LOG_DOMAIN	MODULE_NAME
 #define SYS_LOG_LEVEL	CONFIG_DESKTOP_SYS_LOG_STATE_STATE_LEVEL
@@ -18,8 +19,27 @@
 
 #include "led_state.h"
 
-static void set_leds(enum system_state state)
+
+static bool error;
+static bool connected;
+static bool charging;
+
+static void set_leds(void)
 {
+	enum system_state state = SYSTEM_STATE_ERROR;
+
+	if (!error) {
+		if (!connected && !charging) {
+			state = SYSTEM_STATE_DISCONNECTED;
+		} else if (connected && !charging) {
+			state = SYSTEM_STATE_CONNECTED;
+		} else if (!connected && charging) {
+			state = SYSTEM_STATE_DISCONNECTED_CHARGING;
+		} else if (connected && charging) {
+			state = SYSTEM_STATE_CONNECTED_CHARGING;
+		}
+	}
+
 	const struct led_config *cfg = led_config[state];
 
 	for (size_t i = 0; i < CONFIG_DESKTOP_LED_COUNT; i++) {
@@ -40,11 +60,39 @@ static bool event_handler(const struct event_header *eh)
 	if (is_ble_peer_event(eh)) {
 		struct ble_peer_event *event = cast_ble_peer_event(eh);
 
-		if (event->state == PEER_STATE_CONNECTED) {
-			set_leds(SYSTEM_STATE_CONNECTED);
-		} else if (event->state == PEER_STATE_DISCONNECTED) {
-			set_leds(SYSTEM_STATE_DISCONNECTED);
+		switch (event->state)  {
+		case PEER_STATE_SECURED:
+		case PEER_STATE_CONNECTED:
+			connected = true;
+			break;
+		case PEER_STATE_DISCONNECTED:
+			connected = false;
+			break;
+		default:
+			error = true;
+			break;
 		}
+		set_leds();
+
+		return false;
+	}
+
+	if (is_battery_state_event(eh)) {
+		struct battery_state_event *event = cast_battery_state_event(eh);
+
+		switch (event->state) {
+		case BATTERY_STATE_CHARGING:
+			charging = true;
+			break;
+		case BATTERY_STATE_IDLE:
+			charging = false;
+			break;
+		default:
+			error = true;
+			break;
+		}
+		set_leds();
+
 		return false;
 	}
 
@@ -52,7 +100,10 @@ static bool event_handler(const struct event_header *eh)
 		struct module_state_event *event = cast_module_state_event(eh);
 
 		if (check_state(event, MODULE_ID(main), MODULE_STATE_READY)) {
-			set_leds(SYSTEM_STATE_DISCONNECTED);
+			set_leds();
+		} else if (event->state == MODULE_STATE_ERROR) {
+			error = true;
+			set_leds();
 		}
 		return false;
 	}
@@ -66,3 +117,4 @@ static bool event_handler(const struct event_header *eh)
 EVENT_LISTENER(MODULE, event_handler);
 EVENT_SUBSCRIBE(MODULE, module_state_event);
 EVENT_SUBSCRIBE(MODULE, ble_peer_event);
+EVENT_SUBSCRIBE(MODULE, battery_state_event);
