8827255a07c7a225875328e83c8ec466854d8248
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2018-10-19 16:53:28 +0200

    samples: nrf_desktop: Add battery state event
    Jira:DESK-198
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

--- samples/nrf_desktop/src/events/battery_event.h
+++ samples/nrf_desktop/src/events/battery_event.h
@@ -19,13 +19,40 @@
 extern "C" {
 #endif
 
-struct battery_event {
+/** @brief Battery state list. */
+#define BATTERY_STATE_LIST	\
+	X(IDLE)			\
+	X(CHARGING)		\
+	X(ERROR)
+
+/** @brief Battery states. */
+enum battery_state {
+#define X(name) _CONCAT(BATTERY_STATE_, name),
+	BATTERY_STATE_LIST
+#undef X
+
+	BATTERY_STATE_COUNT
+};
+
+
+/** @brief Battery state event. */
+struct battery_state_event {
+	struct event_header header;
+
+	enum battery_state state;
+};
+
+EVENT_TYPE_DECLARE(battery_state_event);
+
+
+/** @brief Battery voltage level event. */
+struct battery_level_event {
 	struct event_header header;
 
 	u8_t level;
 };
 
-EVENT_TYPE_DECLARE(battery_event);
+EVENT_TYPE_DECLARE(battery_level_event);
 
 #ifdef __cplusplus
 }
