2934b21d78fedc9af1d455c26d63e8ce27859a50
Radoslaw Koppel <radoslaw.koppel@nordicsemi.no>
2018-10-30 12:21:39 +0100

    subsys: bluetooth: gatt_db_discovery: Functional update
    This commit implement changes in the GATT DB discovery logic
    to work properly if there are "holes" in the handlers numeration.
    Now only the number of attributes find is important for the
    placement in the array.
    
    Jira: NCSDK-574
    
    Signed-off-by: Radoslaw Koppel <radoslaw.koppel@nordicsemi.no>

--- subsys/bluetooth/common/gatt_db_discovery.c
+++ subsys/bluetooth/common/gatt_db_discovery.c
@@ -16,12 +16,9 @@ LOG_MODULE_REGISTER();
 /* Available sizes: 128, 512, 2048... */
 #define CHUNK_SIZE (128 - sizeof(struct k_mem_block_id))
 
-#define ATTRS_LEN_GET(svc_attr) \
-	(((struct bt_gatt_service_val *) svc_attr.user_data)->end_handle \
-	 - svc_attr.handle + 1)
-
 static struct bt_gatt_discover_params discover_params;
 static struct bt_gatt_attr attrs[CONFIG_NRF_BT_GATT_DB_DISCOVERY_MAX_ATTRS];
+static size_t cur_attr_id;
 
 static u8_t *user_data_chunks[CONFIG_NRF_BT_GATT_DB_DISCOVERY_MAX_MEM_CHUNKS];
 static size_t cur_chunk_id;
@@ -72,41 +69,54 @@ static void *user_data_store(const void *user_data, size_t len)
 
 static void svc_attr_memory_release(void)
 {
-	for (size_t i = 0; i < ARRAY_SIZE(user_data_chunks); i++) {
-		if (user_data_chunks[i]) {
-			k_free(user_data_chunks[i]);
-			user_data_chunks[i] = NULL;
-		}
-	}
-
-	cur_chunk_data_len = 0;
-	cur_chunk_id = 0;
-
+	LOG_DBG("Attr memory release");
+	/* Clear attributes */
 	memset(attrs, 0, sizeof(attrs));
+	cur_attr_id = 0;
+	/* Release dynamic memory data chunks */
+	for (size_t i = 0; i < cur_chunk_id; i++) {
+		k_free(user_data_chunks[i]);
+		user_data_chunks[i] = NULL;
+	}
+	cur_chunk_id = 0;
+	cur_chunk_data_len = 0;
 }
 
 static struct bt_gatt_attr *attr_store(const struct bt_gatt_attr *attr)
 {
 	struct bt_gatt_attr *cur_attr;
-	u16_t i;
 
-	if (!attrs[0].uuid) {
-		cur_attr = &attrs[0];
-	} else {
-		i = attr->handle - attrs[0].handle;
-		if (i >= ARRAY_SIZE(attrs)) {
-			LOG_ERR("New attribute index is out of the "
-				    "declared range.");
-
-			return NULL;
-		}
-
-		cur_attr = &attrs[i];
+	LOG_DBG("Attr store, pos: %u, handle: %u", cur_attr_id, attr->handle);
+	if (cur_attr_id >= ARRAY_SIZE(attrs)) {
+		LOG_ERR("No space for new attribute.");
+		return NULL;
 	}
-
+	cur_attr = &attrs[cur_attr_id++];
 	memcpy(cur_attr, attr, sizeof(*attr));
 
 	return cur_attr;
+}
+
+static struct bt_gatt_attr *attr_find_by_handle(u16_t handle)
+{
+	size_t lower = 0;
+	size_t upper = cur_attr_id;
+
+	while (upper >= lower) {
+		size_t m = (lower + upper) / 2;
+		struct bt_gatt_attr *cur_attr = &attrs[m];
+
+		if (cur_attr->handle < handle) {
+			lower = m + 1;
+		} else if (cur_attr->handle > handle) {
+			upper = m - 1;
+		} else {
+			return cur_attr;
+		}
+	}
+
+	/* handle not found */
+	return NULL;
 }
 
 static struct bt_uuid *uuid_16_store(const struct bt_uuid *uuid)
@@ -161,171 +171,181 @@ static struct bt_uuid *uuid_store(const struct bt_uuid *uuid)
 	}
 }
 
-static bool is_desc_dicovery_reqd(u16_t *start_handle, u16_t *end_handle)
+static void discovery_complete(struct bt_conn *conn)
 {
-	bool in_chrc_range = false;
-	u16_t start_handle_int = *start_handle;
-	struct bt_gatt_service_val *gatt_service = attrs[0].user_data;
-
-	for (size_t i = *start_handle - attrs[0].handle; i < ARRAY_SIZE(attrs);
-	     i++) {
-		if (!attrs[i].uuid && !in_chrc_range) {
-			in_chrc_range = true;
-			*start_handle = attrs[i - 1].handle + 1;
-		}
-
-		if (attrs[i].uuid && in_chrc_range) {
-			in_chrc_range = false;
-			*end_handle = attrs[i].handle - 1;
-			break;
-		}
+	LOG_DBG("Discovery complete.");
+	atomic_set_bit(state_flags, STATE_ATTRS_RELEASE_PENDING);
+	if (callback->completed) {
+		callback->completed(conn, attrs, cur_attr_id);
 	}
-
-	/* The last group of characteristic descriptors. */
-	if (in_chrc_range) {
-		*end_handle = gatt_service->end_handle;
-	}
-
-	/* No place for more descriptors. */
-	if (start_handle_int == *start_handle) {
-		return false;
-	}
-
-	return true;
 }
 
-static int descriptor_discovery_start(struct bt_conn *conn)
+static void discovery_complete_not_found(struct bt_conn *conn)
 {
-	int err = 0;
-	bool is_disc_reqd;
+	LOG_DBG("Discover complete. No service found.");
 
-	discover_params.type = BT_GATT_DISCOVER_DESCRIPTOR;
+	svc_attr_memory_release();
+	atomic_clear_bit(state_flags, STATE_ATTRS_LOCKED);
 
-	is_disc_reqd = is_desc_dicovery_reqd(&discover_params.start_handle,
-					     &discover_params.end_handle);
-	if (is_disc_reqd) {
-		err = bt_gatt_discover(conn, &discover_params);
-		if (err) {
-			LOG_ERR("Discover failed, error: %d.", err);
+	if (callback->service_not_found) {
+		callback->service_not_found(conn);
+	}
+}
 
-			return err;
-		}
-	} else {
-		LOG_DBG("Discovery complete.");
-		atomic_set_bit(state_flags, STATE_ATTRS_RELEASE_PENDING);
-		if (callback->completed) {
-			callback->completed(conn, attrs,
-					    ATTRS_LEN_GET(attrs[0]));
-		}
+static void discovery_complete_error(struct bt_conn *conn, int err)
+{
+	svc_attr_memory_release();
+	atomic_clear_bit(state_flags, STATE_ATTRS_LOCKED);
+	if (callback->error_found) {
+		callback->error_found(conn, err);
+	}
+}
+
+static u8_t discovery_process_service(struct bt_conn *conn,
+				      const struct bt_gatt_attr *attr,
+				      struct bt_gatt_discover_params *params)
+{
+	int err;
+
+	if (!attr) {
+		discovery_complete_not_found(conn);
+		return BT_GATT_ITER_STOP;
 	}
 
-	return err;
+	struct bt_gatt_service_val *service_val = attr->user_data;
+	struct bt_gatt_attr *cur_attr = attr_store(attr);
+
+	if (!cur_attr) {
+		LOG_ERR("Not enough memory for service attribute.");
+		discovery_complete_error(conn, -ENOMEM);
+		return BT_GATT_ITER_STOP;
+	}
+	LOG_DBG("Service detected, handles range: <%u, %u>",
+		attr->handle + 1,
+		service_val->end_handle);
+
+	cur_attr->uuid = uuid_store(attr->uuid);
+	service_val->uuid = uuid_store(service_val->uuid);
+	cur_attr->user_data = user_data_store(service_val,
+			sizeof(*service_val));
+	if (!cur_attr->uuid || !cur_attr->user_data ||
+	    !service_val->uuid) {
+		LOG_ERR("Not enough memory for service attribute data.");
+		discovery_complete_error(conn, -ENOMEM);
+		return BT_GATT_ITER_STOP;
+	}
+
+	discover_params.uuid = NULL;
+	discover_params.type = BT_GATT_DISCOVER_DESCRIPTOR;
+	discover_params.start_handle = attr->handle + 1;
+	discover_params.end_handle = service_val->end_handle;
+	LOG_DBG("Starting descriptors discovery");
+	err = bt_gatt_discover(conn, &discover_params);
+
+	if (err) {
+		LOG_ERR("Descriptor discover failed, error: %d.", err);
+		discovery_complete_error(conn, -ENOMEM);
+		return BT_GATT_ITER_STOP;
+	}
+
+	return BT_GATT_ITER_STOP;
+}
+
+static u8_t discovery_process_descriptor(struct bt_conn *conn,
+					 const struct bt_gatt_attr *attr,
+					 struct bt_gatt_discover_params *params)
+{
+	if (!attr) {
+		if (cur_attr_id > 1) {
+			LOG_DBG("Starting characteristic discovery");
+			discover_params.start_handle = attrs[0].handle + 1;
+			discover_params.type = BT_GATT_DISCOVER_CHARACTERISTIC;
+			int err = bt_gatt_discover(conn, &discover_params);
+
+			if (err) {
+				LOG_ERR("Characteristic discover failed,"
+					" error: %d.",
+					err);
+				discovery_complete_error(conn, err);
+			}
+		} else {
+			discovery_complete(conn);
+		}
+		return BT_GATT_ITER_STOP;
+	}
+
+	struct bt_gatt_attr *cur_attr = attr_store(attr);
+
+	if (!cur_attr) {
+		LOG_ERR("Not enough memory for next attribute descriptor"
+			" at handle %u.",
+			attr->handle);
+		discovery_complete_error(conn, -ENOMEM);
+		return BT_GATT_ITER_STOP;
+	}
+	cur_attr->uuid = uuid_store(attr->uuid);
+	if (!cur_attr->uuid) {
+		LOG_ERR("Not enough memory for UUID at handle %u.",
+			attr->handle);
+		discovery_complete_error(conn, -ENOMEM);
+		return BT_GATT_ITER_STOP;
+	}
+
+	return BT_GATT_ITER_CONTINUE;
+}
+
+static u8_t discovery_process_characteristic(struct bt_conn *conn,
+					     const struct bt_gatt_attr *attr,
+					     struct bt_gatt_discover_params *params)
+{
+	if (!attr) {
+		discovery_complete(conn);
+		return BT_GATT_ITER_STOP;
+	}
+
+	struct bt_gatt_attr *cur_attr = attr_find_by_handle(attr->handle);
+	struct bt_gatt_chrc *gatt_chrc;
+
+	if (!cur_attr) {
+		/* Note: We should never be here is the server is working properly */
+		discovery_complete_error(conn, -ESRCH);
+		return BT_GATT_ITER_STOP;
+	}
+
+	gatt_chrc = attr->user_data;
+	gatt_chrc->uuid = uuid_store(gatt_chrc->uuid);
+	cur_attr->user_data = user_data_store(gatt_chrc,
+			sizeof(*gatt_chrc));
+	if (!cur_attr->user_data || !gatt_chrc->uuid) {
+		discovery_complete_error(conn, -ENOMEM);
+		return BT_GATT_ITER_STOP;
+	}
+
+	return BT_GATT_ITER_CONTINUE;
 }
 
 static u8_t discovery_callback(struct bt_conn *conn,
 			       const struct bt_gatt_attr *attr,
 			       struct bt_gatt_discover_params *params)
 {
-	int err = 0;
-	struct bt_gatt_attr *cur_attr;
-	struct bt_gatt_service_val *gatt_service;
-	struct bt_gatt_chrc *gatt_chrc;
-
 	if (!attr) {
-		if (params->type == BT_GATT_DISCOVER_CHARACTERISTIC) {
-			discover_params.start_handle = attrs[0].handle;
-
-			err = descriptor_discovery_start(conn);
-			if (err) {
-				goto error;
-			}
-		} else if (params->type == BT_GATT_DISCOVER_DESCRIPTOR) {
-			err = descriptor_discovery_start(conn);
-			if (err) {
-				goto error;
-			}
-		} else {
-			LOG_DBG("Discover complete. No service found.");
-
-			svc_attr_memory_release();
-			atomic_clear_bit(state_flags, STATE_ATTRS_LOCKED);
-
-			if (callback->service_not_found) {
-				callback->service_not_found(conn);
-			}
-
-			return BT_GATT_ITER_STOP;
-		}
-
-		return BT_GATT_ITER_STOP;
-	}
-
-	cur_attr = attr_store(attr);
-	if (!cur_attr) {
-		LOG_ERR("Not enough memory for next attribute descriptor.");
-		err = -ENOMEM;
-		goto error;
+		LOG_DBG("NULL attribute");
+	} else {
+		LOG_DBG("Attr: handle %u", attr->handle);
 	}
 
 	switch (params->type) {
 	case BT_GATT_DISCOVER_PRIMARY:
 	case BT_GATT_DISCOVER_SECONDARY:
-		gatt_service = attr->user_data;
-
-		cur_attr->uuid = uuid_store(attr->uuid);
-		gatt_service->uuid = uuid_store(gatt_service->uuid);
-		cur_attr->user_data = user_data_store(gatt_service,
-				sizeof(*gatt_service));
-		if (!cur_attr->uuid || !cur_attr->user_data ||
-		    !gatt_service->uuid) {
-			err = -ENOMEM;
-			goto error;
-		}
-
-		discover_params.uuid = NULL;
-		discover_params.start_handle = attr->handle;
-		discover_params.end_handle = gatt_service->end_handle;
-		discover_params.type = BT_GATT_DISCOVER_CHARACTERISTIC;
-
-		err = bt_gatt_discover(conn, &discover_params);
-		if (err) {
-			LOG_ERR("Discover failed, error: %d.", err);
-			goto error;
-		}
-
-		return BT_GATT_ITER_STOP;
-	case BT_GATT_DISCOVER_CHARACTERISTIC:
-		gatt_chrc = attr->user_data;
-
-		cur_attr->uuid = uuid_store(attr->uuid);
-		gatt_chrc->uuid = uuid_store(gatt_chrc->uuid);
-		cur_attr->user_data = user_data_store(gatt_chrc,
-				sizeof(*gatt_chrc));
-		if (!cur_attr->uuid || !cur_attr->user_data ||
-		    !gatt_chrc->uuid) {
-			err = -ENOMEM;
-			goto error;
-		}
-
-		return BT_GATT_ITER_CONTINUE;
+		return discovery_process_service(conn, attr, params);
 	case BT_GATT_DISCOVER_DESCRIPTOR:
-		cur_attr->uuid = uuid_store(attr->uuid);
-		if (!cur_attr->uuid) {
-			err = -ENOMEM;
-			goto error;
-		}
-
-		return BT_GATT_ITER_CONTINUE;
+		return discovery_process_descriptor(conn, attr, params);
+	case BT_GATT_DISCOVER_CHARACTERISTIC:
+		return discovery_process_characteristic(conn, attr, params);
 	default:
-		LOG_ERR("Unknown param type.");
+		/* Error here means some error in this code - should not be possible */
+		__ASSERT(false, "Unknown param type.");
 		break;
-	}
-
-error:
-	svc_attr_memory_release();
-	atomic_clear_bit(state_flags, STATE_ATTRS_LOCKED);
-	if (callback->error_found) {
-		callback->error_found(conn, err);
 	}
 
 	return BT_GATT_ITER_STOP;
@@ -351,6 +371,9 @@ int gatt_db_discovery_start(struct bt_conn *conn,
 	}
 
 	callback = cb;
+	cur_attr_id = 0;
+	cur_chunk_data_len = 0;
+	cur_chunk_id = 0;
 
 	discover_params.uuid = uuid_store(svc_uuid);
 	discover_params.func = discovery_callback;
