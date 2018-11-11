ff1ba3c94a792fc23b66b6737fe1848b1eea04d9
P.K.Chan <pak.kee.chan@nordicsemi.no>
2018-11-11 17:06:32 +0800

    [at_client] Changed configuration stucture of at_host.

--- samples/at_client/src/main.c
+++ samples/at_client/src/main.c
@@ -13,10 +13,15 @@
 void main(void)
 {
 	int err;
+    at_host_config_t l_config;
+
+    memset(&l_config, 0, sizeof(at_host_config_t));
+    l_config.uart = CONFIG_AT_HOST_UART;
+    l_config.mode = CONFIG_AT_HOST_TERMINATION;
 
 	printf("The AT host sample started\n");
 
-	err = at_host_init(CONFIG_AT_HOST_UART, CONFIG_AT_HOST_TERMINATION);
+	err = at_host_init(&l_config);
 
 	if (err != 0) {
 		printf("ERROR: AT Host not initialized \r\n");
