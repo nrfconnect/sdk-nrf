9627fa201ba35c2cc04f3459c8cdd8c35a91dd8a
Marek Pieta <Marek.Pieta@nordicsemi.no>
2018-12-04 10:10:27 +0100

    scripts: profiler: Plot translation
    Added initial plot translation.
    
    JIRA:DESK-352
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- scripts/profiler/rtt_nordic_config.py
+++ scripts/profiler/rtt_nordic_config.py
@@ -9,7 +9,7 @@ RttNordicConfig = {
     'rtt_command_channel': 1,
     'ms_per_timestamp_tick': 0.03125,
     'byteorder': 'little',
-    'reset_on_start': True,
+    'reset_on_start': False,
     'connection_timeout': -1,
     'timestamp_raw_max': 2**32 #timestamp on uC is stored as 32-bit value
 }
