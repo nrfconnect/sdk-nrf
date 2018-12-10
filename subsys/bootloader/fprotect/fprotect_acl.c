ee3ef2818d561126ab328efb3eecda4b73cf2f0e
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2018-12-10 11:22:58 +0000

    bootloader: Protect provision flash area.
    This commit adds write protection to the provision page.
    
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- subsys/bootloader/fprotect/fprotect_acl.c
+++ subsys/bootloader/fprotect/fprotect_acl.c
@@ -5,9 +5,25 @@
  */
 
 #include <nrf_acl.h>
+#include <errno.h>
 
-void fprotect_area(u32_t start, size_t length)
+int fprotect_area(u32_t start, size_t length)
 {
-	nrf_acl_region_set(NRF_ACL, 0, start, length,
+	static u32_t region_idx;
+
+	if (region_idx > ACL_REGIONS_COUNT) {
+		return -ENOSPC;
+	}
+
+	if (start  % NRF_FICR->CODEPAGESIZE != 0 ||
+	    length % NRF_FICR->CODEPAGESIZE != 0 ||
+	    length > NRF_ACL_REGION_SIZE_MAX ||
+	    length == 0) {
+		return -EINVAL;
+	}
+
+	nrf_acl_region_set(NRF_ACL, region_idx++, start, length,
 			   NRF_ACL_PERM_READ_NO_WRITE);
+
+	return 0;
 }
