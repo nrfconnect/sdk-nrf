6f15ab6e42000139a0d70023faa4d6a7e8173d67
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2018-12-10 11:22:58 +0000

    bootloader: Protect provision flash area.
    This commit adds write protection to the provision page.
    
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- subsys/bootloader/include/fprotect.h
+++ subsys/bootloader/include/fprotect.h
@@ -10,7 +10,18 @@
 #include <stdint.h>
 #include <string.h>
 
-/* Flash protect specified area */
-void fprotect_area(u32_t start, size_t length);
+/**
+ * @brief Protect flash area against writes.
+ *
+ * @param[in]  start   Start of range to protect.
+ * @param[in]  length  Length in bytes of range to protect.
+ *
+ * @retval 0        On success.
+ * @retval -EINVAL  If any of the argument are incorrect.
+ * @retval -ENOSPC  If function is called too many times. Applies to
+ *                  devices where there is a limited number of configuration
+ *                  registers which are used for all address ranges.
+ */
+int fprotect_area(u32_t start, size_t length);
 
 #endif /* FPROTECT_H_ */
