fa4eb16f3bcddb4173af7ced3117165e7b693a49
Filip Kubicz <filip.kubicz@nordicsemi.no>
2019-01-03 12:51:57 +0100

    scripts: config channel: Control mouse power saving modes from Python
    Add downshift times of nRF52 Desktop optical sensor to the configuration
    channel Python application.
    
    Signed-off-by: Filip Kubicz <filip.kubicz@nordicsemi.no>

--- scripts/hid_configurator/configurator.py
+++ scripts/hid_configurator/configurator.py
@@ -12,6 +12,21 @@ MOUSE_PID = 0x52DE
 
 class ConfigEventID(Enum):
     MOUSE_CPI = 1
+    DOWNSHIFT_RUN = 2
+    DOWNSHIFT_REST1 = 3
+    DOWNSHIFT_REST2 = 4
+
+
+def check_range(value, lower, upper, value_name):
+    if value > upper or value < lower:
+        raise ValueError('{} {} out of range'.format(value_name, value))
+
+
+def create_report(event_id, value):
+    """ Function creating a HID feature report with 32-bit unsigned value """
+
+    data = struct.pack('<BBI', REPORT_ID, event_id, value)
+    return data
 
 
 def configurator():
@@ -21,6 +36,15 @@ def configurator():
     parser = argparse.ArgumentParser()
     parser.add_argument('--cpi', type=int,
                         help='CPI resolution of a mouse sensor')
+    parser.add_argument('--downshift_run', type=int,
+                        help='time in milliseconds of switching from Run mode'
+                             'to Rest 1 mode')
+    parser.add_argument('--downshift_rest1', type=int,
+                        help='time in milliseconds of switching from Rest 1 '
+                             'mode to Rest 2 mode')
+    parser.add_argument('--downshift_rest2', type=int,
+                        help='time in milliseconds of switching from Rest 2 '
+                             'mode to Rest 3 mode')
     args = parser.parse_args()
 
     # Open HID device
@@ -28,22 +52,52 @@ def configurator():
     logging.debug('Opened device')
 
     if (args.cpi):
-        data = create_report_mouse_cpi(args.cpi)
+        name = 'CPI'
+        value = args.cpi
+        check_range(value, 100, 12000, name)
+
+        event_id = ConfigEventID.MOUSE_CPI.value
+        data = create_report(event_id, value)
 
         # Send configuration report
         dev.send_feature_report(data)
+        logging.debug('Sent request to update {}: {}'.format(name, value))
 
+    if (args.downshift_run):
+        name = 'Downshift run time'
+        value = args.downshift_run
+        check_range(value, 10, 2550, name)
 
-def create_report_mouse_cpi(cpi):
-    """ Function creating a HID feature report with 16-bit unsigned CPI resolution """
-    if cpi > 12000 or cpi < 100:
-        raise ValueError('CPI out of range')
+        event_id = ConfigEventID.DOWNSHIFT_RUN.value
+        data = create_report(event_id, value)
 
-    event_id = ConfigEventID.MOUSE_CPI.value
+        # Send configuration report
+        dev.send_feature_report(data)
+        logging.debug('Sent request to update {}: {}'.format(name, value))
 
-    data = struct.pack('<BBH', REPORT_ID, event_id, cpi)
-    logging.debug('Sent request to update CPI: %d' % cpi)
-    return data
+    if (args.downshift_rest1):
+        name = 'Downshift rest1 time'
+        value = args.downshift_rest1
+        check_range(value, 320, 81600, name)
+
+        event_id = ConfigEventID.DOWNSHIFT_REST1.value
+        data = create_report(event_id, value)
+
+        # Send configuration report
+        dev.send_feature_report(data)
+        logging.debug('Sent request to update {}: {}'.format(name, value))
+
+    if (args.downshift_rest2):
+        name = 'Downshift rest2 time'
+        value = args.downshift_rest2
+        check_range(value, 3200, 816000, name)
+
+        event_id = ConfigEventID.DOWNSHIFT_REST2.value
+        data = create_report(event_id, value)
+
+        # Send configuration report
+        dev.send_feature_report(data)
+        logging.debug('Sent request to update {}: {}'.format(name, value))
 
 
 if __name__ == '__main__':
