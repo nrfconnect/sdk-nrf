9029d4435335b5d303a9723e8dcf2c5231298f49
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2019-01-09 14:12:36 +0100

    samples: nrf9160: asset_tracker: Add orientation detector filtering
    This patch adds basic filtering of noisy measurements to the
    orientation detector. When based on real sensors, data readings
    can be noisy and have static offsets that need to be filtered.
    This commit implements a simple calibration procedure by
    averaging the offset and subtracting from each subsequent measurement.
    Each measurement can also be averaged over a configurable number
    of samples to smoothen out noise.
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

--- samples/nrf9160/asset_tracker/src/orientation_detector/orientation_detector.c
+++ samples/nrf9160/asset_tracker/src/orientation_detector/orientation_detector.c
@@ -4,40 +4,59 @@
  * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
  */
 
-#include "orientation_detector.h"
 #include <sensor.h>
+#include "orientation_detector.h"
 
-#define FLIP_ACCELERATION_THRESHOLD		7.0
+#define FLIP_ACCELERATION_THRESHOLD	5.0
+#define CALIBRATION_ITERATIONS		CONFIG_ACCEL_CALIBRATION_ITERATIONS
+#define MEASUREMENT_ITERATIONS		CONFIG_ACCEL_ITERATIONS
+#define ACCEL_INVERTED			CONFIG_ACCEL_INVERTED
 
 static struct device *dev;
+static double accel_offset[3];
 
 int orientation_detector_poll(
 	struct orientation_detector_sensor_data *sensor_data)
 {
 	int err;
+	u8_t i;
+	double aggregated_data[3] = {0};
 	struct sensor_value accel_data[3];
 	enum orientation_state current_orientation;
 
-	err = sensor_sample_fetch_chan(dev, SENSOR_CHAN_ACCEL_XYZ);
+	for (i = 0; i < MEASUREMENT_ITERATIONS; i++) {
+		err = sensor_sample_fetch_chan(dev, SENSOR_CHAN_ACCEL_XYZ);
+		if (err) {
+			printk("sensor_sample_fetch failed\n");
+			return err;
+		}
 
-	if (err) {
-		printk("sensor_sample_fetch failed\n");
-		return err;
+		err = sensor_channel_get(dev,
+				SENSOR_CHAN_ACCEL_XYZ, &accel_data[0]);
+
+		if (err) {
+			printk("sensor_channel_get failed\n");
+			return err;
+		}
+
+		aggregated_data[2] += sensor_value_to_double(&accel_data[2]);
 	}
 
-	err = sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, &accel_data[0]);
-
-	if (err) {
-		printk("sensor_channel_get failed\n");
-		return err;
-	}
-
-	sensor_data->z = sensor_value_to_double(&accel_data[2]);
+	sensor_data->z = (aggregated_data[2] / (double)MEASUREMENT_ITERATIONS) -
+				accel_offset[2];
 
 	if (sensor_data->z >= FLIP_ACCELERATION_THRESHOLD) {
-		current_orientation = ORIENTATION_NORMAL;
+		if (IS_ENABLED(ACCEL_INVERTED)) {
+			current_orientation = ORIENTATION_UPSIDE_DOWN;
+		} else {
+			current_orientation = ORIENTATION_NORMAL;
+		}
 	} else if (sensor_data->z <= -FLIP_ACCELERATION_THRESHOLD) {
-		current_orientation = ORIENTATION_UPSIDE_DOWN;
+		if (IS_ENABLED(ACCEL_INVERTED)) {
+			current_orientation = ORIENTATION_NORMAL;
+		} else {
+			current_orientation = ORIENTATION_UPSIDE_DOWN;
+		}
 	} else {
 		current_orientation = ORIENTATION_ON_SIDE;
 	}
@@ -47,6 +66,41 @@ int orientation_detector_poll(
 	return 0;
 }
 
+int orientation_detector_calibrate(void)
+{
+	u8_t i;
+	int err;
+	struct sensor_value accel_data[3];
+	double aggregated_data[3] = {0};
+
+	for (i = 0; i < CALIBRATION_ITERATIONS; i++) {
+		err = sensor_sample_fetch_chan(dev, SENSOR_CHAN_ACCEL_XYZ);
+		if (err) {
+			printk("sensor_sample_fetch failed\n");
+			return err;
+		}
+
+		err = sensor_channel_get(dev,
+				SENSOR_CHAN_ACCEL_XYZ, &accel_data[0]);
+
+		if (err) {
+			printk("sensor_channel_get failed\n");
+			return err;
+		}
+
+		aggregated_data[0] += sensor_value_to_double(&accel_data[0]);
+		aggregated_data[1] += sensor_value_to_double(&accel_data[1]);
+		aggregated_data[2] +=
+			(sensor_value_to_double(&accel_data[2])
+			+ ((double)SENSOR_G) / 1000000.0);
+	}
+
+	accel_offset[0] = aggregated_data[0] / (double)CALIBRATION_ITERATIONS;
+	accel_offset[1] = aggregated_data[1] / (double)CALIBRATION_ITERATIONS;
+	accel_offset[2] = aggregated_data[2] / (double)CALIBRATION_ITERATIONS;
+
+	return 0;
+}
 
 void orientation_detector_init(struct device *accel_device)
 {
