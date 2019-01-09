84d991fe98084b56d60cf09eb058b63f920d57dc
Radoslaw Koppel <radoslaw.koppel@nordicsemi.no>
2019-01-09 09:36:02 +0100

    samples: bluetooth: central_hids: Add protocol switching and tests
    This commit adds:
    - A functionality to switch between report and boot protocol.
    - Read and write tests using capslock LED in boot mode.
      Read without response and read with response is tested.
    
    JIRA: NCSDK-1068
    
    Signed-off-by: Radoslaw Koppel <radoslaw.koppel@nordicsemi.no>

--- samples/bluetooth/central_hids/src/main.c
+++ samples/bluetooth/central_hids/src/main.c
@@ -21,9 +21,33 @@
 #include <misc/byteorder.h>
 #include <bluetooth/scan.h>
 #include <bluetooth/services/hids_c.h>
+#include <dk_buttons_and_leds.h>
+
+/**
+ * Switch between boot protocol and report protocol mode.
+ */
+#define KEY_BOOTMODE_MASK DK_BTN2_MSK
+/**
+ * Switch CAPSLOCK state.
+ *
+ * @note
+ * For simplicity of the code it works only in boot mode.
+ */
+#define KEY_CAPSLOCK_MASK DK_BTN1_MSK
+/**
+ * Switch CAPSLOCK state with response
+ *
+ * Write CAPSLOCK with response.
+ * Just for testing purposes.
+ * The result should be the same like usine @ref KEY_CAPSLOCK_MASK
+ */
+#define KEY_CAPSLOCK_RSP_MASK DK_BTN3_MSK
+
 
 static struct bt_conn *default_conn;
 static struct bt_gatt_hids_c hids_c;
+static u8_t capslock_state;
+
 
 static void scan_filter_match(struct bt_scan_device_info *device_info,
 			      struct bt_scan_filter_match *filter_match,
@@ -209,9 +233,48 @@ static u8_t hids_c_notify_cb(struct bt_gatt_hids_c *hids_c,
 	return BT_GATT_ITER_CONTINUE;
 }
 
+static u8_t hids_c_boot_mouse_report(struct bt_gatt_hids_c *hids_c,
+				     struct bt_gatt_hids_c_rep_info *rep,
+				     u8_t err,
+				     const u8_t *data)
+{
+	u8_t size = rep->size;
+	u8_t i;
+
+	if (!data) {
+		return BT_GATT_ITER_STOP;
+	}
+	printk("Notification, mouse boot, size: %u, data:", size);
+	for (i = 0; i < size; ++i) {
+		printk(" 0x%x", data[i]);
+	}
+	printk("\n");
+	return BT_GATT_ITER_CONTINUE;
+}
+
+static u8_t hids_c_boot_kbd_report(struct bt_gatt_hids_c *hids_c,
+				   struct bt_gatt_hids_c_rep_info *rep,
+				   u8_t err,
+				   const u8_t *data)
+{
+	u8_t size = rep->size;
+	u8_t i;
+
+	if (!data) {
+		return BT_GATT_ITER_STOP;
+	}
+	printk("Notification, keyboard boot, size: %u, data:", size);
+	for (i = 0; i < size; ++i) {
+		printk(" 0x%x", data[i]);
+	}
+	printk("\n");
+	return BT_GATT_ITER_CONTINUE;
+}
+
 static void hids_c_ready_cb(struct bt_gatt_hids_c *hids_c)
 {
 	u8_t i;
+	int err;
 
 	printk("HIDS is ready to work\n");
 	for (i = 0; i < hids_c->rep_cnt; ++i) {
@@ -222,8 +285,29 @@ static void hids_c_ready_cb(struct bt_gatt_hids_c *hids_c)
 		    BT_GATT_HIDS_C_REPORT_TYPE_INPUT) {
 			printk("Subscribe in report id: %u\n",
 			       rep->ref.id);
-			bt_gatt_hids_c_rep_subscribe(hids_c, rep,
-						     hids_c_notify_cb);
+			err = bt_gatt_hids_c_rep_subscribe(hids_c, rep,
+							   hids_c_notify_cb);
+			if (err) {
+				printk("Subscribe error (%d)\n", err);
+			}
+		}
+	}
+	if (hids_c->rep_boot.kbd_inp) {
+		printk("Subscribe in boot keyboard report\n");
+		err = bt_gatt_hids_c_rep_subscribe(hids_c,
+						   hids_c->rep_boot.kbd_inp,
+						   hids_c_boot_kbd_report);
+		if (err) {
+			printk("Subscribe error (%d)\n", err);
+		}
+	}
+	if (hids_c->rep_boot.mouse_inp) {
+		printk("Subscribe in boot mouse report\n");
+		err = bt_gatt_hids_c_rep_subscribe(hids_c,
+						   hids_c->rep_boot.mouse_inp,
+						   hids_c_boot_mouse_report);
+		if (err) {
+			printk("Subscribe error (%d)\n", err);
 		}
 	}
 }
@@ -247,6 +331,136 @@ static const struct bt_gatt_hids_c_init_params hids_c_init_params = {
 	.pm_update_cb  = hids_c_pm_update_cb
 };
 
+
+static void button_bootmode(void)
+{
+	if (!bt_gatt_hids_ready_check(&hids_c)) {
+		printk("HID device not ready\n");
+		return;
+	}
+	int err;
+	enum bt_gatt_hids_c_pm pm = bt_gatt_hids_c_pm_get(&hids_c);
+
+	printk("Setting protocol mode: %s\n",
+	       (pm == BT_GATT_HIDS_C_PM_BOOT) ?
+	       "BOOT" : "REPORT");
+	err = bt_gatt_hids_c_pm_write(&hids_c,
+				      (pm == BT_GATT_HIDS_C_PM_BOOT) ?
+				      BT_GATT_HIDS_C_PM_REPORT :
+				      BT_GATT_HIDS_C_PM_BOOT);
+	if (err) {
+		printk("Cannot change protocol mode (err %d)\n", err);
+	}
+}
+
+
+static void button_capslock(void)
+{
+	int err;
+	u8_t data;
+
+	if (!bt_gatt_hids_ready_check(&hids_c)) {
+		printk("HID device not ready\n");
+		return;
+	}
+	if (!hids_c.rep_boot.kbd_out) {
+		printk("HID device does not have Keyboard OUT report\n");
+		return;
+	}
+	if (bt_gatt_hids_c_pm_get(&hids_c) != BT_GATT_HIDS_C_PM_BOOT) {
+		printk("This function works only in BOOT Report mode\n");
+		return;
+	}
+	capslock_state = capslock_state ? 0 : 1;
+	data = capslock_state ? 0x02 : 0;
+	err = bt_gatt_hids_c_rep_write_wo_rsp(&hids_c,
+					      hids_c.rep_boot.kbd_out,
+					      &data, sizeof(data));
+	if (err) {
+		printk("Keyboard data write error (err: %d)\n", err);
+		return;
+	}
+	printk("Caps lock send (val: 0x%x)\n", data);
+}
+
+
+static u8_t capslock_read_cb(struct bt_gatt_hids_c *hids_c,
+			     struct bt_gatt_hids_c_rep_info *rep,
+			     u8_t err,
+			     const u8_t *data)
+{
+	if (err) {
+		printk("Capslock read error (err: %u)\n", err);
+		return BT_GATT_ITER_STOP;
+	}
+	if (!data) {
+		printk("Capslock read - no data\n");
+		return BT_GATT_ITER_STOP;
+	}
+	printk("Received data (size: %u, data[0]: 0x%x)\n", rep->size, data[0]);
+
+	return BT_GATT_ITER_STOP;
+}
+
+
+static void capslock_write_cb(struct bt_gatt_hids_c *hids_c,
+			      struct bt_gatt_hids_c_rep_info *rep,
+			      u8_t err)
+{
+	int ret;
+
+	printk("Capslock write result: %u\n", err);
+
+	ret = bt_gatt_hids_c_rep_read(hids_c, rep, capslock_read_cb);
+	if (ret) {
+		printk("Cannot read capslock value (err: %d)\n", ret);
+	}
+}
+
+
+static void button_capslock_rsp(void)
+{
+	if (!bt_gatt_hids_ready_check(&hids_c)) {
+		printk("HID device not ready\n");
+		return;
+	}
+	if (!hids_c.rep_boot.kbd_out) {
+		printk("HID device does not have Keyboard OUT report\n");
+		return;
+	}
+	int err;
+	u8_t data;
+
+	capslock_state = capslock_state ? 0 : 1;
+	data = capslock_state ? 0x02 : 0;
+	err = bt_gatt_hids_c_rep_write(&hids_c,
+				       hids_c.rep_boot.kbd_out,
+				       capslock_write_cb,
+				       &data, sizeof(data));
+	if (err) {
+		printk("Keyboard data write error (err: %d)\n", err);
+		return;
+	}
+	printk("Caps lock send using write with response (val: 0x%x)\n", data);
+}
+
+
+static void button_handler(u32_t button_state, u32_t has_changed)
+{
+	u32_t button = button_state & has_changed;
+
+	if (button & KEY_BOOTMODE_MASK) {
+		button_bootmode();
+	}
+	if (button & KEY_CAPSLOCK_MASK) {
+		button_capslock();
+	}
+	if (button & KEY_CAPSLOCK_RSP_MASK) {
+		button_capslock_rsp();
+	}
+}
+
+
 void main(void)
 {
 	int err;
@@ -263,6 +477,12 @@ void main(void)
 
 	scan_init();
 	bt_conn_cb_register(&conn_callbacks);
+
+	err = dk_buttons_init(button_handler);
+	if (err) {
+		printk("Failed to initialize buttons (err %d)\n", err);
+		return;
+	}
 
 	err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
 	if (err) {
