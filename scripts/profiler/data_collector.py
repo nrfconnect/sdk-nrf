7a1efd332243ce3d9d7f88e9cd0d6ec217286786
Marek Pieta <Marek.Pieta@nordicsemi.no>
2019-01-15 16:03:47 +0100

    scripts: profiler: Add SigInt to data_collector
    Change adds proper handling of SigInt signal by application.
    
    JIRA:DESK-392
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- scripts/profiler/data_collector.py
+++ scripts/profiler/data_collector.py
@@ -5,12 +5,14 @@ from rtt_nordic_profiler_host import RttNordicProfilerHost
 import sys
 import argparse
 import logging
+import signal
+import threading
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Collecting data from Nordic profiler for given time and saving to files.')
-    parser.add_argument('time', type=int, help='Time of collecting data')
+    parser.add_argument('time', type=int, help='Time of collecting data [s]')
     parser.add_argument('event_csv', help='.csv file to save collected events')
     parser.add_argument('event_descr', help='.json file to save events descriptions')
     parser.add_argument('--log', help='Log level')
@@ -21,12 +23,18 @@ def main():
     else:
 	    log_lvl_number = logging.DEBUG
 
+    def sigint_handler(sig, frame):
+        end_ev.set()
+
+    signal.signal(signal.SIGINT, sigint_handler)
+    end_ev = threading.Event()
+
     profiler = RttNordicProfilerHost(event_filename=args.event_csv,
+                                     finish_event=end_ev,
                                      event_types_filename=args.event_descr,
                                      log_lvl=log_lvl_number)
     profiler.get_events_descriptions()
     profiler.read_events_rtt(args.time)
-    profiler.disconnect()
 
 if __name__ == "__main__":
     main()
