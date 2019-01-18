083a304ab3d822e121c0d50b89897c7b8f158212
Marek Pieta <Marek.Pieta@nordicsemi.no>
2019-01-18 15:27:40 +0100

    scripts: profiler: Speedup stats calculating
    Change speeds up stats calculating in Python.
    
    JIRA:DESK-394
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- scripts/profiler/calc_stats.py
+++ scripts/profiler/calc_stats.py
@@ -7,6 +7,7 @@ import sys
 import argparse
 import logging
 
+
 def main():
     parser = argparse.ArgumentParser(description='Calculating stats for events.')
     parser.add_argument('event_csv', help='.csv file to read raw events data')
