df3ab3bce8fd5c89b0c3ad3fc8986760493ff297
Emanuele Di Santo <emdi@nordicsemi.no>
2019-01-23 13:59:35 +0100

    net: lib: mqtt_socket: fix log level macro
    Use the correct macro to set the log level.
    This fixes compilation of the MQTT library with logging enabled.
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>

--- subsys/net/lib/mqtt_socket/mqtt_rx.c
+++ subsys/net/lib/mqtt_socket/mqtt_rx.c
@@ -5,7 +5,7 @@
  */
 
 #include <logging/log.h>
-LOG_MODULE_REGISTER(net_mqtt_rx, CONFIG_MQTT_LOG_LEVEL);
+LOG_MODULE_REGISTER(net_mqtt_rx, CONFIG_MQTT_SOCKET_LOG_LEVEL);
 
 #include "mqtt_internal.h"
 #include "mqtt_os.h"
--- subsys/net/lib/mqtt_socket/mqtt_transport_socket_tcp.c
+++ subsys/net/lib/mqtt_socket/mqtt_transport_socket_tcp.c
@@ -10,7 +10,7 @@
  */
 
 #include <logging/log.h>
-LOG_MODULE_REGISTER(net_mqtt_sock_tcp, CONFIG_MQTT_LOG_LEVEL);
+LOG_MODULE_REGISTER(net_mqtt_sock_tcp, CONFIG_MQTT_SOCKET_LOG_LEVEL);
 
 #include <errno.h>
 #include <net/socket.h>
