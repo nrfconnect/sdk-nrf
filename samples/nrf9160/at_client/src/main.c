a1ae87a6a7af687ce8c4876e4b50fd70e573c704
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2019-01-24 15:34:34 +0100

    samples: nrf9160: at_client: Adapt to AT host running in thread
    The AT host library now processes AT commands in a dedicated
    thread, and it's therefore not needed to call library functions
    from main.
    This patch removes inclusion of now deleted at_host.h and
    the function calls to the AT host library. The main stack
    is also increased to avoid stack overflow.
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

--- samples/nrf9160/at_client/src/main.c
+++ samples/nrf9160/at_client/src/main.c
@@ -8,22 +8,8 @@
 #include <stdio.h>
 #include <uart.h>
 #include <string.h>
-#include <at_host.h>
 
 void main(void)
 {
-	int err;
-
-	printf("The AT host sample started\n");
-
-	err = at_host_init(CONFIG_AT_HOST_UART, CONFIG_AT_HOST_TERMINATION);
-
-	if (err != 0) {
-		printf("ERROR: AT Host not initialized \r\n");
-		return;
-	}
-
-	while (1) {
-		at_host_process();
-	}
+	printk("The AT host sample started\n");
 }
