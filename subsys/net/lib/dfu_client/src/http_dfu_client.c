f1cc903326c14c6589ec7addc1cae213bc3b72da
Krishna Shingala <krishna.shingala@nordicsemi.no>
2019-02-27 19:21:54 +0100

    subsys:net:dfu_client: Status updated, dynamic resource, cleanup.
    Download status updated in the module. Applciation can now know when the
    dfu client is connected, downloading, done with the download or halted.
    
    Application can now set the resource and the host, no hard coded values.
    
    Misc cleanup: removed unused variables and data types.
    
    Signed-off-by: Krishna Shingala <krishna.shingala@nordicsemi.no>

--- subsys/net/lib/dfu_client/src/http_dfu_client.c
+++ subsys/net/lib/dfu_client/src/http_dfu_client.c
@@ -14,18 +14,43 @@
 
 LOG_MODULE_REGISTER(http_dfu);
 
-#define REQUEST_TEMPLATE "GET /nordic-firmware-files/749fc958-b75b-4ca1-8161-bcf564530aac HTTP/1.1\r\n"\
-	"Host: s3.amazonaws.com\r\n"\
+#define REQUEST_TEMPLATE "GET %s HTTP/1.1\r\n"\
+	"Host: %s\r\n"\
 	"Range: bytes=%d-%d\r\n\r\n"
+
+
+
+static int fragment_request(struct dfu_client_object * const dfu) {
+	if (dfu == NULL || dfu->host == NULL || dfu->callback == NULL || dfu->resource == NULL ) {
+		LOG_ERR("fragment_request(): Invalid dfu object");
+		return -1;
+	}
+
+	int request_len = snprintf (dfu->req_buf, CONFIG_NRF_DFU_HTTP_MAX_REQUEST_SIZE, REQUEST_TEMPLATE, dfu->resource,
+							dfu->host, dfu->download_size,
+							(dfu->download_size + CONFIG_NRF_DFU_HTTP_MAX_FRAGMENT_SIZE - 1));
+
+	if (request_len > 0) {
+		dfu->status = DFU_CLIENT_STATUS_DOWNLOAD_INPROGRESS;
+		LOG_INF("Request: %s", dfu->req_buf);
+		return httpc_request(dfu->fd, dfu->req_buf, request_len);
+	} else {
+		LOG_ERR("httpc_request() failed, err %d", errno);
+		return -1;
+	}
+}
 
 
 int dfu_client_init(struct dfu_client_object *const dfu)
 {
 	LOG_INF("dfu_client_init()\n");
 
-	if (dfu == NULL || dfu->host == NULL || dfu->callback == NULL) {
+	if (dfu == NULL || dfu->host == NULL || dfu->callback == NULL || dfu->resource == NULL ) {
 		return -1;
 	}
+
+	dfu->fd = -1;
+	dfu->status = DFU_CLIENT_STATUS_IDLE;
 
 	return 0;
 }
@@ -38,14 +63,20 @@ int dfu_client_connect(struct dfu_client_object *const dfu)
 		return -1;
 	}
 
+	if ((dfu->fd != -1) && (dfu->status == DFU_CLIENT_STATUS_CONNECTED)) {
+		LOG_INF("dfu_client_connect(): already connected, fd %d", dfu->fd);
+		return 0;
+	}
+
 	/* TODO: Parse the post for name, port and protocol. */
 	fd = httpc_connect(dfu->host, NULL, AF_INET, IPPROTO_TCP);
 	if (fd < 0) {
-		LOG_ERR("httpc_connect() failed, err %d", errno);
+		LOG_ERR("dfu_client_connect(): httpc_connect() failed, err %d", errno);
 		return -1;
 	}
 
 	dfu->fd = fd;
+	dfu->status = DFU_CLIENT_STATUS_CONNECTED;
 
 	return 0;
 }
@@ -58,6 +89,7 @@ void dfu_client_disconnect(struct dfu_client_object *const dfu)
 
 	close(dfu->fd);
 	dfu->fd = -1;
+	dfu->status = DFU_CLIENT_STATUS_IDLE;
 }
 
 int dfu_client_download(struct dfu_client_object *const dfu)
@@ -67,20 +99,12 @@ int dfu_client_download(struct dfu_client_object *const dfu)
 	}
 
 	dfu->firmware_size = -1;
-
-	int request_len = snprintf (dfu->req_buf, CONFIG_NRF_DFU_HTTP_MAX_REQUEST_SIZE, REQUEST_TEMPLATE, dfu->offset, dfu->offset+1023);
-
-	if (request_len > 0) {
-		return httpc_request(dfu->fd, dfu->req_buf, request_len);
-	} else {
-		return -1;
-	}
+	return fragment_request(dfu);
 }
 
 void dfu_client_process(struct dfu_client_object *const dfu)
 {
 	int len;
-	int sent;
 
 	if (dfu == NULL || dfu->fd < 0) {
 		return;
@@ -88,10 +112,12 @@ void dfu_client_process(struct dfu_client_object *const dfu)
 
 	len = httpc_recv(dfu->fd, dfu->resp_buf, sizeof(dfu->resp_buf),MSG_PEEK);
 	if (len ==  -1) {
-		dfu->callback(dfu, DFU_CLIENT_EVT_ERROR, ENOTCONN);
+		if (errno != EAGAIN) {
+			dfu->callback(dfu, DFU_CLIENT_EVT_ERROR, ENOTCONN);
+		}
 		return;
 	}
-    if (len ==  0) {
+	if (len ==  0) {
 		dfu->callback(dfu, DFU_CLIENT_EVT_ERROR, ECONNRESET);
 		return;
 	}
@@ -104,23 +130,23 @@ void dfu_client_process(struct dfu_client_object *const dfu)
 	char * content_range_header = strstr(dfu->resp_buf, "Content-Range: bytes");
 	if (content_range_header != NULL) {
 		content_range_header += strlen("Content-Range: bytes");
-	    char * p_start_range = strstr(content_range_header, " ");
+		char * p_start_range = strstr(content_range_header, " ");
 
-	    if (p_start_range != NULL) {
-	    	int start_offset = atoi(p_start_range+1);
-	    	LOG_DBG("Start offset %d, expected %d", start_offset, dfu->offset);
-	    }
-	    char * p_totalsize = strstr(content_range_header, "/");
-	    if (p_totalsize != NULL) {
-	    	total_size = atoi(p_totalsize+1);
-	    	LOG_DBG("Total size %d", total_size);
-	    }
+		if (p_start_range != NULL) {
+			int start_download_size = atoi(p_start_range+1);
+			LOG_DBG("Start download_size %d, expected %d", start_download_size, dfu->download_size);
+		}
+		char * p_totalsize = strstr(content_range_header, "/");
+		if (p_totalsize != NULL) {
+			total_size = atoi(p_totalsize+1);
+			LOG_DBG("Total size %d", total_size);
+		}
 	}
 
 	char * content_length_header = strstr(dfu->resp_buf, "Content-Length: ");
 	if (content_length_header != NULL) {
 		content_length_header += strlen("Content-Length: ");
-    	payload_size = atoi(content_length_header);
+		payload_size = atoi(content_length_header);
 	}
 
 	if (total_size)	{
@@ -135,7 +161,7 @@ void dfu_client_process(struct dfu_client_object *const dfu)
 
 	/* Allow a full sized fragment, except the last one which can be smaller. */
 	if ((payload_size == CONFIG_NRF_DFU_HTTP_MAX_FRAGMENT_SIZE) ||
-		(dfu->offset + payload_size == dfu->firmware_size))	{
+		(dfu->download_size + payload_size == dfu->firmware_size))	{
 		len = httpc_recv(dfu->fd, dfu->resp_buf, sizeof(dfu->resp_buf),0);
 		if (len ==  -1) {
 			dfu->callback(dfu, DFU_CLIENT_EVT_ERROR, 0);
@@ -144,7 +170,7 @@ void dfu_client_process(struct dfu_client_object *const dfu)
 	}
 	else {
 		len = httpc_recv(dfu->fd, dfu->resp_buf, sizeof(dfu->resp_buf),0);
-		dfu_client_download(dfu);
+		fragment_request(dfu);
 		return;
 	}
 
@@ -158,19 +184,24 @@ void dfu_client_process(struct dfu_client_object *const dfu)
 		 */
 		dfu->fragment = p_payload;
 		dfu->fragment_size = payload_size;
-		if (dfu->offset == dfu->firmware_size) {
-			if (dfu->callback(dfu, DFU_CLIENT_EVT_DOWNLOAD_DONE, 0) == 0) {
-				// Continue download
-				dfu_client_download(dfu);
+
+		/* Continue download if application returns success, else. Halt. */
+		if (dfu->callback(dfu, DFU_CLIENT_EVT_DOWNLOAD_FRAG, 0) == 0) {
+			dfu->download_size += payload_size;
+
+			if (dfu->download_size == dfu->firmware_size) {
+				dfu->status = DFU_CLIENT_STATUS_DOWNLOAD_COMPLETE;
+				if (dfu->callback(dfu, DFU_CLIENT_EVT_DOWNLOAD_DONE, 0) != 0) {
+					dfu->download_size -= payload_size;
+					fragment_request(dfu);
+				}
+			}
+			else {
+				fragment_request(dfu);
 			}
 		}
 		else {
-			if (dfu->callback(dfu, DFU_CLIENT_EVT_DOWNLOAD_FRAG, 0) == 0) {
-				// Continue download, else. Halt.
-				dfu->offset += payload_size;
-				dfu_client_download(dfu);
-			}
-
+			dfu->status = DFU_CLIENT_STATUS_HALTED;
 		}
 	}
 }
