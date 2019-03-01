5ae109815638782ccfb125fb887bf88f6f77cecc
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2019-03-01 15:19:52 +0100

    drivers: Add ADP536x driver
    Adds basic ADP536x driver, supporting basic functionality:
    - Set buck output voltage
    - Set buck-boost output voltage
    - Set USB current limit
    - Set battery charging current
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

---
+++ include/drivers/adp536x.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <zephyr.h>
+
+int adp536x_init(const char *dev_name);
+int adp536x_vbus_current_set(u8_t value);
+int adp536x_charger_current_set(u8_t value);
+int adp536x_charger_termination_voltage_set(u8_t value);
+int adp536x_charging_enable(bool enable);
+int adp536x_charger_status_1_read(u8_t *buf);
+int adp536x_charger_status_2_read(u8_t *buf);
+int adp536x_oc_chg_hiccup_set(bool enable);
+int adp536x_oc_dis_hiccup_set(bool enable);
+int adp536x_buckbst_enable(bool enable);
+int adp536x_buck_1v8_set(void);
+int adp536x_buckbst_3v3_set(void);
+int adp536x_factory_reset(void);
