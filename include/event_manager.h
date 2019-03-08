7f6ac8ccf34e8895e467a7360cd093716df16037
Marek Pieta <Marek.Pieta@nordicsemi.no>
2019-03-08 09:33:02 +0100

    subsys: event_manger: Add docs
    Change adds conceptual documentation for Event Manager.
    
    JIRA:DESK-430
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>
    Signed-off-by: Ruth Fuchss <ruth.fuchss@nordicsemi.no>

--- include/event_manager.h
+++ include/event_manager.h
@@ -13,55 +13,8 @@
 
 
 /**
- * @brief Event Manager
  * @defgroup event_manager Event Manager
- *
- * The event manager module provides an infrastructure required for the
- * implementation using an event-driven architecture approach.
- *
- * When defining a new event type, user must provide definition of this event
- * type structure, that contains at least one member 'header' of type
- * @ref struct event_header. If more members are provided 'header' must be first
- * in the event type structure.
- *
- * New event type is defined by using @ref EVENT_TYPE_DEFINE. The only
- * argument passed to this macro is the name of the structure declared in
- * previous paragraph.
- * This macro will expand into definition of new element in array of event
- * types, and define various functions required for its usage.
- *
- * When above is done user can create and submit events of this new type.
- * The new event object is created by using function defined by macro
- * new_'event type name' (e.g. new_motion_event). If there is no memory
- * available for event allocation reset is triggered.
- * After new event object is created, its field can be filled in by the user.
- * Event is submitted to the event manager using a @ref EVENT_SUBMIT macro.
- *
- * Note: an event allocated with new__'event type name' MUST be submitted with
- * @ref EVENT_SUBMIT. Otherwise it will not be handled and the memory will
- * not be freed.
- *
- * After event is submitted the event manager adds it into its processing
- * queue. When event is handled the event manager will notify all modules
- * that subscribed for its reception.
- * A module willing to subscribe for a reception of events must first define
- * itself as event listener using a @ref EVENT_LISTENER macro. First argument
- * should be module name, while second is a callback function called for
- * each event being processed.
- * Event listener can subscribe to events of specific type using either:
- * @ref EVENT_SUBSCRIBE_EARLY, @ref EVENT_SUBSCRIBE, @ref EVENT_SUBSCRIBE_FINAL.
- * Early subscribers are notified first, before any other. There is no
- * defined order in which subscribers of the same time are notified.
- * Only one listener can be final subscriber for any event type, and if so
- * it will be notified last, after all other modules subscribed for that
- * event.
- *
- * Single listener can be subscribed to events of multiple types. The same
- * callback function is called when any of subscribed events is being processed.
- * To check type of incoming event user should use macro defined function
- * is_'event type name' (e.g. is_motion_event). Function accepts pointer event
- * header which is an argument to event handling function and returns true
- * is event type matches.
+ * @brief Event Manager
  *
  * @{
  */
@@ -101,10 +54,10 @@ extern "C" {
 #define SUBS_PRIO_COUNT (SUBS_PRIO_MAX - SUBS_PRIO_MIN + 1)
 
 
-/** @brief Event header structure.
+/** @brief Event header.
  *
- * @warning When event structure is defined event header must be placed
- *          as the first field.
+ * When defining an event structure, the event header
+ * must be placed as the first field.
  */
 struct event_header {
 	/** Linked list node used to chain events. */
@@ -115,20 +68,21 @@ struct event_header {
 };
 
 
-/** @brief Event listener structure.
+/** @brief Event listener.
  *
- * @note All event listeners must be defined using @ref EVENT_LISTENER.
+ * All event listeners must be defined using @ref EVENT_LISTENER.
  */
 struct event_listener {
 	/** Name of this listener. */
 	const char *name;
 
-	/** Pointer to function that is called when event is handled. */
+	/** Pointer to the function that is called when an event
+	 *  is handled. */
 	bool (*notification)(const struct event_header *eh);
 };
 
 
-/** @brief Event subscriber structure.
+/** @brief Event subscriber.
  */
 struct event_subscriber {
 	/** Pointer to the listener. */
@@ -139,22 +93,22 @@ struct event_subscriber {
 /** @brief Event description for profiling or logging.
  */
 struct event_info {
-	/** Function to profile this event. */
+	/** Function for profiling this event. */
 	void (*profile_fn)(struct log_event_buf *buf,
 			   const struct event_header *eh);
 
-	/** Number of logged datafields. */
+	/** Number of logged data fields. */
 	const u8_t log_arg_cnt;
 
-	/** Labels of logged datafields. */
+	/** Labels of logged data fields. */
 	const char **log_arg_labels;
 
-	/** Types of logged datafields. */
+	/** Types of logged data fields. */
 	const enum profiler_arg *log_arg_types;
 };
 
 
-/** @brief Event type structure.
+/** @brief Event type.
  */
 struct event_type {
 	/** Event name. */
@@ -163,11 +117,11 @@ struct event_type {
 	/** Array of pointers to the array of subscribers. */
 	const struct event_subscriber	*subs_start[SUBS_PRIO_COUNT];
 
-	/** Array of pointers to element directly after the array of
+	/** Array of pointers to the element directly after the array of
 	 * subscribers. */
 	const struct event_subscriber	*subs_stop[SUBS_PRIO_COUNT];
 
-	/** Bool indicating if event is logged by default. */
+	/** Bool indicating if the event is logged by default. */
 	bool init_log_enable;
 
 	/** Function to log data from this event. */
@@ -186,19 +140,16 @@ extern const struct event_type __start_event_types[];
 extern const struct event_type __stop_event_types[];
 
 
-/** @def EVENT_LISTENER
- *
- * @brief Create event listener object.
+/** Create an event listener object.
  *
  * @param lname   Module name.
- * @param cb_fn  Pointer to the event notification function.
+ * @param cb_fn  Pointer to the event handler function.
  */
 #define EVENT_LISTENER(lname, cb_fn) _EVENT_LISTENER(lname, cb_fn)
 
 
-/** @def EVENT_SUBSCRIBE_EARLY
- *
- * @brief Subscribe listener to the event type early notification list.
+/** Subscribe a listener to the early notification list for an
+ *  event type.
  *
  * @param lname  Name of the listener.
  * @param ename  Name of the event.
@@ -207,9 +158,8 @@ extern const struct event_type __stop_event_types[];
 	_EVENT_SUBSCRIBE(lname, ename, _SUBS_PRIO_ID(_SUBS_PRIO_FIRST))
 
 
-/** @def EVENT_SUBSCRIBE
- *
- * @brief Subscribe listener to the event type to normal notification list.
+/** Subscribe a listener to the normal notification list for an event
+ *  type.
  *
  * @param lname  Name of the listener.
  * @param ename  Name of the event.
@@ -218,9 +168,8 @@ extern const struct event_type __stop_event_types[];
 	_EVENT_SUBSCRIBE(lname, ename, _SUBS_PRIO_ID(_SUBS_PRIO_NORMAL))
 
 
-/** @def EVENT_SUBSCRIBE_FINAL
- *
- * @brief Subscribe listener to the event type as final module being notified.
+/** Subscribe a listener to an event type as final module that is
+ *  being notified.
  *
  * @param lname  Name of the listener.
  * @param ename  Name of the event.
@@ -230,94 +179,94 @@ extern const struct event_type __stop_event_types[];
 	const struct {} _CONCAT(_CONCAT(__event_subscriber_, ename), final_sub_redefined) = {}
 
 
-/** @def ENCODE
- *
- * @brief Encode event data types or labels.
+/** Encode event data types or labels.
  *
  * @param ... Data types or labels to be encoded.
  */
 #define ENCODE(...) __VA_ARGS__
 
 
-/** @def EVENT_INFO_DEFINE
+/** Define event profiling information.
  *
- * @brief Declare event logging information.
+ * This macro provides definitions required for an event to be profiled.
  *
- * Macro provides declarations required for event to be logged by profiler.
+ * @note Types and labels of the profiled values should be wrapped
+ *       with the @ref ENCODE macro.
  *
  * @param ename Name of the event.
- * @param types Types of values to log.
- * @param labels Labels of values to log (represented as enum).
- * @param log_arg_func Function used to log event data.
+ * @param types Types of values to profile (represented as @ref profiler_arg).
+ * @param labels Labels of values to profile.
+ * @param log_arg_func Function used to profile event data.
  */
 #define EVENT_INFO_DEFINE(ename, types, labels, profile_func) \
 	_EVENT_INFO_DEFINE(ename, ENCODE(types), ENCODE(labels), profile_func)
 
 
-/** @def EVENT_TYPE_DECLARE
+/** Declare an event type.
  *
- * @brief Declare event type.
- *
- * Macro provides declarations required for event to be used by other modules.
+ * This macro provides declarations required for an event to be used
+ * by other modules.
  *
  * @param ename  Name of the event.
  */
 #define EVENT_TYPE_DECLARE(ename) _EVENT_TYPE_DECLARE(ename)
 
 
-/** @def EVENT_TYPE_DEFINE
+/** Define an event type.
  *
- * @brief Define event type.
+ * This macro defines an event type. In addition, it defines functions
+ * specific to the event type and the event type structure.
  *
- * Macro defines the event type. By doing that it defines the event type
- * specific functions as well event type structure.
+ * For every defined event, the following functions are created, where
+ * <i>%event_type</i> is replaced with the given event type name @p ename
+ * (for example, button_event):
+ * - new_<i>%event_type</i>  - Allocates an event of a given type.
+ * - is_<i>%event_type</i>   - Checks if the event header that is provided
+ *                            as argument represents the given event type.
+ * - cast_<i>%event_type</i> - Casts the event header that is provided
+ *                            as argument to an event of the given type.
  *
- * @param ename     		Name of the event.
- * @param init_log_en		Bool indicating if event is logged by default.
- * @param log_fn  		Function to stringify event of this type.
- * @param ev_info_struct	Data structure describing event type.
+ * @param ename     	   Name of the event.
+ * @param init_log_en	   Bool indicating if the event is logged
+ *                         by default.
+ * @param log_fn  	   Function to stringify an event of this type.
+ * @param ev_info_struct   Data structure describing the event type.
  */
 #define EVENT_TYPE_DEFINE(ename, init_log_en, log_fn, ev_info_struct) \
 	_EVENT_TYPE_DEFINE(ename, init_log_en, log_fn, ev_info_struct)
 
 
-/** @def ASSERT_EVENT_ID
+/** Verify if an event ID is valid.
  *
- * @brief Verify if event id is valid.
+ * The pointer to an event type structure is used as its ID. This macro
+ * validates that the provided pointer is within the range where event
+ * type structures are defined.
  *
- * Pointer to event type structure is used as its id. Macro validates that
- * provided pointer is withing range where event type structures are defined.
- *
- * @param id
+ * @param id  ID.
  */
 #define ASSERT_EVENT_ID(id) \
 	__ASSERT_NO_MSG((id >= __start_event_types) && (id < __stop_event_types))
 
 
-/**
- * @brief Submit an event.
- *
- * Function submits an event to the event manager.
+/** Submit an event to the Event Manager.
  *
  * @param eh  Pointer to the event header element in the event object.
  */
 void _event_submit(struct event_header *eh);
 
 
-/** @def EVENT_SUBMIT
+/** Submit an event.
  *
- * @brief Submit an event.
- *
- * This is a helper macro that simplifies the event submission.
+ * This helper macro simplifies the event submission.
  *
  * @param event  Pointer to the event object.
  */
 #define EVENT_SUBMIT(event) _event_submit(&event->header)
 
 
-/** Initialize the event manager.
+/** Initialize the Event Manager.
  *
- * @return Zero if successful.
+ * @retval 0 If the operation was successful.
  */
 int event_manager_init(void);
 
