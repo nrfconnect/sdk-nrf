0c58e85d598fcddf991b9470229e700bfe14a96c
Kamil Gawor <Kamil.Gawor@nordicsemi.no>
2019-03-08 15:13:42 +0100

    subsys: bluetooth: services: lbs: Add documentation
    Add service documentation.
    
    Signed-off-by: Kamil Gawor <Kamil.Gawor@nordicsemi.no>
    Signed-off-by: Ruth Fuchss <ruth.fuchss@nordicsemi.no>

--- include/bluetooth/services/lbs.h
+++ include/bluetooth/services/lbs.h
@@ -10,7 +10,7 @@
 /**@file
  * @defgroup bt_gatt_lbs LED Button Service API
  * @{
- * @brief API for the LED Button Service (LBS)
+ * @brief API for the LED Button Service (LBS).
  */
 
 #ifdef __cplusplus
@@ -19,54 +19,59 @@ extern "C" {
 
 #include <zephyr/types.h>
 
+/** @brief LBS Service UUID. */
 #define LBS_UUID_SERVICE 0x23, 0xD1, 0xBC, 0xEA, 0x5F, 0x78, 0x23, 0x15, \
 			 0xDE, 0xEF, 0x12, 0x12, 0x23, 0x15, 0x00, 0x00
 
+/** @brief Button Characteristic UUID. */
 #define LBS_UUID_BUTTON_CHAR 0x23, 0xD1, 0xBC, 0xEA, 0x5F, 0x78, 0x23, 0x15, \
 			     0xDE, 0xEF, 0x12, 0x12, 0x24, 0x15, 0x00, 0x00
 
+/** @brief LED Characteristic UUID. */
 #define LBS_UUID_LED_CHAR    0x23, 0xD1, 0xBC, 0xEA, 0x5F, 0x78, 0x23, 0x15, \
 			     0xDE, 0xEF, 0x12, 0x12, 0x25, 0x15, 0x00, 0x00
 
-/** @brief Callback type for when a LED state change is received */
+/** @brief Callback type for when an LED state change is received. */
 typedef void (*led_cb_t)(const bool led_state);
 
-/** @brief Callback type for when the button state is pulled */
+/** @brief Callback type for when the button state is pulled. */
 typedef bool (*button_cb_t)(void);
 
-/** @brief Callback struct used by the LBS service */
+/** @brief Callback struct used by the LBS Service. */
 struct bt_gatt_lbs_cb {
+	/** LED state change callback. */
 	led_cb_t    led_cb;
+	/** Button read callback. */
 	button_cb_t button_cb;
 };
 
-/** @brief Initialization
+/** @brief Initialize the LBS Service.
  *
- * @details This function will register a BLE with two characteristics, BUTTON
- *          and LED. Notification can be enabled for the BUTTON characteristics
- *          to let a connected BLE unit know when the button state changes.
- *          The LED characteristics can be written to change the state of the
- *          LED on the board.
+ * This function registers a BLE service with two characteristics: Button
+ * and LED. Enable notifications for the Button Characteristic to let a
+ * connected BLE unit know when the button state changes.
+ * Write to the LED Characteristic to change the state of the LED on the
+ * board.
  *
  * @param[in] callbacks Struct containing pointers to callback functions
  *			used by the service. This pointer can be NULL
  *			if no callback functions are defined.
  *
  *
- * @return Returns 0 if initialization was successful,
- *         otherwise negative value
+ * @retval 0 If the operation was successful.
+ *           Otherwise, a (negative) error code is returned.
  */
 int bt_gatt_lbs_init(struct bt_gatt_lbs_cb *callbacks);
 
-/** @brief Send button state
+/** @brief Send the button state.
  *
- * @details This function will send a binary state, typically the state of a
- *          button, to the connected BLE unit.
+ * This function sends a binary state, typically the state of a
+ * button, to the connected BLE unit.
  *
- * @param[in] button_state The state of the button
+ * @param[in] button_state The state of the button.
  *
- * @return Returns 0 if the operation was successful,
- *         otherwise negative value.
+ * @retval 0 If the operation was successful.
+ *           Otherwise, a (negative) error code is returned.
  */
 int bt_gatt_lbs_send_button_state(bool button_state);
 
---
+++ include/bluetooth/services/lbs.rst
@@ -0,0 +1,40 @@
+.. _lbs_readme:
+
+LED Button Service (LBS)
+########################
+
+The GATT LED Button Service (LBS) is a custom service that receives information about the state of an LED and sends notifications when a button changes its state.
+
+The LBS Service is used in the :ref:`peripheral_lbs` sample.
+
+Service UUID
+************
+
+The 128-bit vendor-specific service UUID is 00001523-1212-EFDE-1523-785FEABCD123.
+
+Characteristics
+***************
+
+This service has two characteristics.
+
+Button Characteristic (00001524-1212-EFDE-1523-785FEABCD123)
+============================================================
+
+Notify:
+    Enable notifications for the Button Characteristic to receive button data from the application.
+
+Read:
+    Read button data from the application.
+
+LED Characteristic (00001525-1212-EFDE-1523-785FEABCD123)
+=========================================================
+
+Write:
+    Write data to the LED Characteristic to change the LED state.
+
+API documentation
+*****************
+
+.. doxygengroup:: bt_gatt_lbs
+   :project: nrf
+   :members:
