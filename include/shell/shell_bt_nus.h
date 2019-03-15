8855c5dab1611ed560ddc07442dfb3ec110516ba
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2019-03-15 14:12:42 +0100

    shell: Add bluetooth NUS transport
    Adding Bluetooth NUS shell transport.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

---
+++ include/shell/shell_bt_nus.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#ifndef SHELL_BT_NUS_H_
+#define SHELL_BT_NUS_H_
+
+#include <shell/shell.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/services/nus.h>
+#include <ring_buffer.h>
+
+/**
+ * @file
+ * @defgroup shell_bt_nus Nordic UART (NUS) shell transport
+ * @{
+ * @brief Nordic UART (NUS) shell transport API.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const struct shell_transport_api shell_bt_nus_transport_api;
+
+/** @brief Shell BT NUS transport instance control block (RW data). */
+struct shell_bt_nus_ctrl_blk {
+	struct bt_conn *conn;
+	atomic_t tx_busy;
+	shell_transport_handler_t handler;
+	void *context;
+};
+
+/** @brief Shell BT NUS transport instance structure. */
+struct shell_bt_nus {
+	struct shell_bt_nus_ctrl_blk *ctrl_blk;
+	struct ring_buf *tx_ringbuf;
+	struct ring_buf *rx_ringbuf;
+};
+
+/** @brief Macro for creating shell BT NUS transport instance. */
+#define SHELL_BT_NUS_DEFINE(_name, _tx_ringbuf_size, _rx_ringbuf_size)	\
+	static struct shell_bt_nus_ctrl_blk _name##_ctrl_blk;		\
+	RING_BUF_DECLARE(_name##_tx_ringbuf, _tx_ringbuf_size);		\
+	RING_BUF_DECLARE(_name##_rx_ringbuf, _rx_ringbuf_size);		\
+	static const struct shell_bt_nus _name##_shell_bt_nus = {	\
+		.ctrl_blk = &_name##_ctrl_blk,				\
+		.tx_ringbuf = &_name##_tx_ringbuf,			\
+		.rx_ringbuf = &_name##_rx_ringbuf,			\
+	};								\
+	struct shell_transport _name = {				\
+		.api = &shell_bt_nus_transport_api,			\
+		.ctx = (struct shell_bt_nus *)&_name##_shell_bt_nus	\
+	}
+
+/**
+ * @brief Initialize shell BT NUS transport instance.
+ *
+ * Function initializes shell thread and BT NUS service.
+ *
+ * @return 0 on success or error.
+ */
+int shell_bt_nus_init(void);
+
+/**
+ * @brief Enable BT NUS shell instance.
+ *
+ * Should be called once connection is established.
+ *
+ * @param conn Connection handle.
+ */
+void shell_bt_nus_enable(struct bt_conn *conn);
+
+/**
+ * @brief Disable BT NUS shell instance.
+ */
+void shell_bt_nus_disable(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ *@}
+ */
+
+#endif /* SHELL_BT_NUS_H_ */
