5dafcb44ba0e460a967a694e6c735830c8e20250
Henrik Malvik Halvorsen <henrik.halvorsen@nordicsemi.no>
2019-03-20 16:37:40 +0100

    drivers: adp536x: Add OC_CHG config
    The API is expanded to set the OC_CHG register.
    
    Signed-off-by: Henrik Malvik Halvorsen <henrik.halvorsen@nordicsemi.no>

--- include/drivers/adp536x.h
+++ include/drivers/adp536x.h
@@ -19,3 +19,4 @@ int adp536x_buckbst_enable(bool enable);
 int adp536x_buck_1v8_set(void);
 int adp536x_buckbst_3v3_set(void);
 int adp536x_factory_reset(void);
+int adp536x_oc_chg_current_set(u8_t value);
