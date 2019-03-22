d6340d735dedf8b82d44b3be673e0fc0a4bd5505
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2019-03-22 15:29:33 +0100

    samples: nrf_desktop: Rework HID keymap configuration
    Jira:DESK-488
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

--- samples/nrf_desktop/configuration/nrf52840_pca10056/hid_keymap_def.h
+++ samples/nrf_desktop/configuration/nrf52840_pca10056/hid_keymap_def.h
@@ -4,20 +4,19 @@
  * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
  */
 
-#include <misc/util.h>
-
 #include "hid_keymap.h"
+
+/* This file must be included only once */
+const struct {} hid_keymap_def_include_once;
 
 /*
  * HID keymap. The Consumer Control keys are defined in section 15 of
  * the HID Usage Tables document under the following URL:
  * http://www.usb.org/developers/hidpage/Hut1_12v2.pdf
  */
-const struct hid_keymap hid_keymap[] = {
+static const struct hid_keymap hid_keymap[] = {
 	/*
 	 * Intentionally empty.
 	 * Should be filled to add keyboard/mouse buttons support to PCA10056.
 	 */
 };
-
-const size_t hid_keymap_size = ARRAY_SIZE(hid_keymap);
