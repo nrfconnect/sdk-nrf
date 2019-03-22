e71815fd51322991827eacd1cb5bed4162ae186e
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2019-03-22 15:12:09 +0100

    samples: nrf_desktop: Rework LED config files
    Jira:DESK-488
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

--- samples/nrf_desktop/configuration/nrf52840_pca10056/led_state_def.h
+++ samples/nrf_desktop/configuration/nrf52840_pca10056/led_state_def.h
@@ -1,25 +1,21 @@
 /*
- * Copyright (c) 2019 Nordic Semiconductor ASA
+ * Copyright (c) 2018 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
  */
 
 #include "led_state.h"
 
-/* Mapping the PWM channels to pin numbers */
-const size_t led_pins[CONFIG_DESKTOP_LED_COUNT]
-		     [CONFIG_DESKTOP_LED_COLOR_COUNT] = {
-	{
-		DT_NORDIC_NRF_PWM_PWM_0_CH0_PIN,
-	},
-	{
-		DT_NORDIC_NRF_PWM_PWM_1_CH0_PIN,
-	}
+/* This file must be included only once */
+const struct {} led_state_def_include_once;
+
+/* Map function to LED ID */
+static const u8_t led_map[LED_ID_COUNT] = {
+	[LED_ID_SYSTEM_STATE] = 0,
+	[LED_ID_PEER_STATE] = 1
 };
 
-const u8_t led_map[LED_ID_COUNT] = {0, 1};
-
-const struct led_effect led_system_state_effect[LED_SYSTEM_STATE_COUNT] = {
+static const struct led_effect led_system_state_effect[LED_SYSTEM_STATE_COUNT] = {
 	[LED_SYSTEM_STATE_IDLE] = {
 		.mode = LED_MODE_ON,
 	},
@@ -32,7 +28,7 @@ const struct led_effect led_system_state_effect[LED_SYSTEM_STATE_COUNT] = {
 	}
 };
 
-const struct led_color led_system_state_color[LED_SYSTEM_STATE_COUNT] = {
+static const struct led_color led_system_state_color[LED_SYSTEM_STATE_COUNT] = {
 	[LED_SYSTEM_STATE_IDLE] = {
 		.c = {200},
 	},
@@ -44,7 +40,7 @@ const struct led_color led_system_state_color[LED_SYSTEM_STATE_COUNT] = {
 	}
 };
 
-const struct led_effect led_peer_state_effect[LED_PEER_STATE_COUNT] = {
+static const struct led_effect led_peer_state_effect[LED_PEER_STATE_COUNT] = {
 	[LED_PEER_STATE_DISCONNECTED] = {
 		.mode = LED_MODE_BLINKING,
 		.period = 1000,
@@ -58,11 +54,11 @@ const struct led_effect led_peer_state_effect[LED_PEER_STATE_COUNT] = {
 	},
 	[LED_PEER_STATE_CONFIRM_ERASE] = {
 		.mode = LED_MODE_BLINKING,
-		.period = 25
+		.period = 25,
 	}
 };
 
-const struct led_color led_peer_state_color[CONFIG_BT_MAX_PAIRED] = {
+static const struct led_color led_peer_state_color[CONFIG_BT_MAX_PAIRED] = {
 	[0] = {
 		.c = {100},
 	},
---
+++ samples/nrf_desktop/configuration/nrf52840_pca10056/leds_def.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/* This file must be included only once */
+const struct {} leds_def_include_once;
+
+/* Mapping the PWM channels to pin numbers */
+static const size_t led_pins[CONFIG_DESKTOP_LED_COUNT]
+			    [CONFIG_DESKTOP_LED_COLOR_COUNT] = {
+	{
+		DT_NORDIC_NRF_PWM_PWM_0_CH0_PIN,
+	},
+	{
+		DT_NORDIC_NRF_PWM_PWM_1_CH0_PIN,
+	}
+};
