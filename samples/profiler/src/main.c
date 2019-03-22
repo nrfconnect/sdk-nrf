8530ea635bc96b465d7490fadbf83cb21bc0d5c0
Marek Pieta <Marek.Pieta@nordicsemi.no>
2019-03-22 14:57:56 +0100

    samples: profiler: Add sample
    Change adds Profiler sample.
    
    JIRA:DESK-489
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>
    Signed-off-by: Ruth Fuchss <ruth.fuchss@nordicsemi.no>

---
+++ samples/profiler/src/main.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <zephyr.h>
+#include <profiler.h>
+
+#define MAX_VAL1 1000
+
+static u16_t data_event_id;
+static u16_t no_data_event_id;
+
+static void profile_no_data_event(void)
+{
+	struct log_event_buf buf;
+
+	profiler_log_start(&buf);
+	profiler_log_send(&buf, no_data_event_id);
+}
+
+static void profile_data_event(u32_t val1, s32_t val2)
+{
+	struct log_event_buf buf;
+
+	profiler_log_start(&buf);
+	/* Use this function for every data type */
+	profiler_log_encode_u32(&buf, val1);
+	profiler_log_encode_u32(&buf, val2);
+	profiler_log_send(&buf, data_event_id);
+}
+
+static void register_profiler_events(void)
+{
+	static const char *data_names[] = {"value1", "value2"};
+	static const enum profiler_arg data_types[] = {PROFILER_ARG_U32,
+						       PROFILER_ARG_S32};
+
+	no_data_event_id = profiler_register_event_type("no data event", NULL,
+							NULL, 0);
+	data_event_id = profiler_register_event_type("data event", data_names,
+						     data_types, 2);
+}
+
+void main(void)
+{
+	if (profiler_init()) {
+		printk("Profiler failed to initialize\n");
+	};
+	printk("Profiler initialized\n");
+	register_profiler_events();
+	printk("Events registered\n");
+	u32_t val1 = 50;
+	s32_t val2 = -50;
+
+	/* Periodically changing values and profiling events */
+	while (1) {
+		val2 = -val2;
+		val1++;
+		if (val1 > MAX_VAL1) {
+			val1 = 0;
+		}
+
+		profile_no_data_event();
+		k_sleep(10);
+		profile_data_event(val1, val2);
+		printk("Events sent\n");
+		k_sleep(500);
+	}
+
+}
