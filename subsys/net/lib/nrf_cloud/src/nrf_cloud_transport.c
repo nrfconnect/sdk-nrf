eee98670ece579d89a3aeb2e95ba1c9cd2b78b0d
Robert Lubos <robert.lubos@nordicsemi.no>
2019-04-02 14:29:27 +0200

    net: lib: nrf_cloud: Clear hints structure before use
    Unused fileds of the `hints` structure for `getaddrinfo` funcion should
    be either 0 or NULL according to the man pages.
    
    This became especially important, as bsdlib now verifies one of the
    non-standard fields (ai_next).
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/net/lib/nrf_cloud/src/nrf_cloud_transport.c
+++ subsys/net/lib/nrf_cloud/src/nrf_cloud_transport.c
@@ -640,12 +640,10 @@ int nct_connect(void)
 	int err;
 	struct addrinfo *result;
 	struct addrinfo *addr;
-	struct addrinfo hints;
-
-	hints.ai_flags = 0;
-	hints.ai_family = NRF_CLOUD_AF_FAMILY;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = 0;
+	struct addrinfo hints = {
+		.ai_family = NRF_CLOUD_AF_FAMILY,
+		.ai_socktype = SOCK_STREAM
+	};
 
 	err = getaddrinfo(NRF_CLOUD_HOSTNAME, NULL, &hints, &result);
 	if (err) {
