c32ec3c04e6fb3e616891bb6eb771b1b06f7030a
Kamil Gawor <Kamil.Gawor@nordicsemi.no>
2019-04-08 14:39:15 +0200

    samples: peripheral_lbs: Add dk_buttons_and_leds
    Deprecated API for buttons and leds replaced by
    dk_buttons_and_leds library.
    
    Signed-off-by: Kamil Gawor <Kamil.Gawor@nordicsemi.no>

--- samples/bluetooth/peripheral_lbs/src/main.c
+++ samples/bluetooth/peripheral_lbs/src/main.c
@@ -21,70 +21,26 @@
 #include <bluetooth/gatt.h>
 
 #include <bluetooth/services/lbs.h>
+#include <dk_buttons_and_leds.h>
 
 #define DEVICE_NAME             CONFIG_BT_DEVICE_NAME
 #define DEVICE_NAME_LEN         (sizeof(DEVICE_NAME) - 1)
 
-/* Change this if you have a LED connected to a custom port */
-#ifndef LED0_GPIO_CONTROLLER
-#define LED0_GPIO_CONTROLLER	LED0_GPIO_PORT
-#endif
 
-#define LED_PORT                LED0_GPIO_CONTROLLER
-
-#define RUN_STATUS_LED          LED0_GPIO_PIN
-#define CON_STATUS_LED          LED1_GPIO_PIN
+#define RUN_STATUS_LED          DK_LED1
+#define CON_STATUS_LED          DK_LED2
 #define RUN_LED_BLINK_INTERVAL  1000
 
-#define LED_ON                  0
-#define LED_OFF                 1
+#define LED_ON                  1
+#define LED_OFF                 0
 
-#define USER_LED                LED2_GPIO_PIN
+#define USER_LED                DK_LED3
 
-/* change this to use another GPIO port */
-#ifndef SW0_GPIO_CONTROLLER
-#ifdef SW0_GPIO_NAME
-#define SW0_GPIO_CONTROLLER SW0_GPIO_NAME
-#else
-#error SW0_GPIO_NAME or SW0_GPIO_CONTROLLER needs to be set in soc.h
-#endif
-#endif
-#define BUTTON_PORT SW0_GPIO_CONTROLLER
-
-/* change this to use another GPIO pin */
-#ifdef SW0_GPIO_PIN
-#define USER_BUTTON SW0_GPIO_PIN
-#else
-#error SW0_GPIO_PIN needs to be set in soc.h
-#endif
-
-/* change to use another GPIO pin interrupt config */
-#ifdef SW0_GPIO_INT_CONF
-#define EDGE_SENSE_CONF SW0_GPIO_INT_CONF
-#else
-/*
- * If SW0_GPIO_INT_CONF not defined used default EDGE value.
- * Change this to use a different interrupt trigger
- */
-#define EDGE_SENSE_CONF (GPIO_INT_EDGE | \
-			 GPIO_INT_DOUBLE_EDGE | \
-			 GPIO_INT_ACTIVE_LOW)
-#endif
-
-/* change this to enable pull-up/pull-down */
-#ifdef SW0_GPIO_PIN_PUD
-#define PULL_UP SW0_GPIO_PIN_PUD
-#else
-#define PULL_UP	0
-#endif
+#define USER_BUTTON             DK_BTN1_MSK
 
 static K_SEM_DEFINE(ble_init_ok, 0, 1);
 
-static struct device        *button_port;
-static struct device        *led_port;
-
-static struct gpio_callback button_cb;
-static bool                 button_state;
+static bool app_button_state;
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -95,13 +51,6 @@ static const struct bt_data sd[] = {
 	BT_DATA_BYTES(BT_DATA_UUID128_ALL, LBS_UUID_SERVICE),
 };
 
-static void set_led_state(int led, bool state)
-{
-	if (led_port) {
-		gpio_pin_write(led_port, led, state);
-	}
-}
-
 static void connected(struct bt_conn *conn, u8_t err)
 {
 	if (err) {
@@ -111,14 +60,14 @@ static void connected(struct bt_conn *conn, u8_t err)
 
 	printk("Connected\n");
 
-	set_led_state(CON_STATUS_LED, LED_ON);
+	dk_set_led(CON_STATUS_LED, LED_ON);
 }
 
 static void disconnected(struct bt_conn *conn, u8_t reason)
 {
 	printk("Disconnected (reason %u)\n", reason);
 
-	set_led_state(CON_STATUS_LED, LED_OFF);
+	dk_set_led(CON_STATUS_LED, LED_OFF);
 }
 
 #ifdef CONFIG_BT_GATT_LBS_SECURITY_ENABLED
@@ -166,12 +115,12 @@ static struct bt_conn_auth_cb conn_auth_callbacks;
 
 static void app_led_cb(bool led_state)
 {
-	set_led_state(USER_LED, !led_state);
+	dk_set_led(USER_LED, led_state);
 }
 
 static bool app_button_cb(void)
 {
-	return button_state;
+	return app_button_state;
 }
 
 static struct bt_gatt_lbs_cb lbs_callbacs = {
@@ -205,85 +154,29 @@ static void bt_ready(int err)
 	k_sem_give(&ble_init_ok);
 }
 
-static void button_pressed(struct device *button_port, struct gpio_callback *cb,
-		    u32_t pins)
+static void button_changed(u32_t button_state, u32_t has_changed)
 {
-	button_state = button_state ^ 0x01;
-	bt_gatt_lbs_send_button_state(button_state);
+	if (has_changed & USER_BUTTON) {
+		bt_gatt_lbs_send_button_state(button_state);
+		app_button_state = button_state ? true : false;
+	}
 }
 
 static int init_button(void)
 {
 	int err;
 
-	button_port = device_get_binding(BUTTON_PORT);
-	if (!button_port) {
-		printk("Could not bind to BUTTON port\n");
-		return -ENXIO;
-	}
-
-	err = gpio_pin_configure(button_port, USER_BUTTON,
-			   GPIO_DIR_IN | GPIO_INT | PULL_UP |
-			   EDGE_SENSE_CONF);
-
-	if (!err) {
-		gpio_init_callback(&button_cb,
-				    button_pressed,
-				    BIT(USER_BUTTON));
-		err = gpio_add_callback(button_port, &button_cb);
-	}
-
-	if (!err) {
-		err = gpio_pin_enable_callback(button_port, USER_BUTTON);
-	}
-
+	err = dk_buttons_init(button_changed);
 	if (err) {
-		printk("Unable to initialize button pin (err:%d)\n", err);
+		printk("Cannot init buttons (err: %d)\n", err);
 	}
 
 	return err;
 }
 
-static void init_leds(void)
-{
-	int err;
-
-	led_port = device_get_binding(LED_PORT);
-	if (!led_port) {
-		printk("Could not bind to LED port\n");
-	}
-
-	err = gpio_pin_configure(led_port, RUN_STATUS_LED, GPIO_DIR_OUT);
-	err += gpio_pin_configure(led_port, USER_LED, GPIO_DIR_OUT);
-	err += gpio_pin_configure(led_port, CON_STATUS_LED, GPIO_DIR_OUT);
-	if (!err) {
-		err = gpio_port_write(led_port, LED_OFF << RUN_STATUS_LED |
-						LED_OFF << USER_LED |
-						LED_OFF << CON_STATUS_LED);
-	}
-
-	if (err) {
-		printk("Unable to initialize LED pins (err:%d)\n", err);
-		led_port = NULL;
-	}
-}
-
 static void error(void)
 {
-	int err = -1;
-
-	led_port = device_get_binding(LED_PORT);
-
-	if (led_port) {
-		err = gpio_port_configure(led_port, GPIO_DIR_OUT);
-	}
-
-	if (!err) {
-		gpio_port_write(led_port, LED_ON << LED0_GPIO_PIN |
-					  LED_ON << LED1_GPIO_PIN |
-					  LED_ON << LED2_GPIO_PIN |
-					  LED_ON << LED3_GPIO_PIN);
-	}
+	dk_set_leds_state(DK_ALL_LEDS_MSK, DK_NO_LEDS_MSK);
 
 	while (true) {
 		/* Spin for ever */
@@ -298,7 +191,11 @@ void main(void)
 
 	printk("Starting Nordic LED-Button service example\n");
 
-	err = init_button();
+	err = dk_leds_init();
+	if (!err) {
+		err = init_button();
+	}
+
 	if (!err) {
 		err = bt_enable(bt_ready);
 	}
@@ -324,10 +221,9 @@ void main(void)
 		error();
 	}
 
-	init_leds();
-
 	for (;;) {
-		set_led_state(RUN_STATUS_LED, (++blink_status) % 2);
+		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
 		k_sleep(RUN_LED_BLINK_INTERVAL);
 	}
 }
+
