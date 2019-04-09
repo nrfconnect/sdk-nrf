8e12a8353b19fc56591e24be1129e35d5b59d30b
sigvartmh <sigvart.m@gmail.com>
2019-04-09 15:58:46 +0200

    subsys: bootloader: cmake public cert sign CI error
    CI test
    
    Signed-off-by: sigvartmh <sigvart.m@gmail.com>

--- subsys/bootloader/cmake/sign.cmake
+++ subsys/bootloader/cmake/sign.cmake
@@ -7,100 +7,99 @@
 set(sign_depends kernel_elf ${KEY_FILE_DEPENDS})
 
 set(hash_file firmware.sha256)
-set(signature_file firmware.signature)
-set(SIGNATURE_PUBLIC_KEY_FILE public.pem)
+    set(signature_file firmware.signature)
+    set(SIGNATURE_PUBLIC_KEY_FILE ${PROJECT_BINARY_DIR}/public.pem)
 
-include(${CMAKE_CURRENT_LIST_DIR}/../cmake/fw_info_magic.cmake)
+    include(${CMAKE_CURRENT_LIST_DIR}/../cmake/fw_info_magic.cmake)
 
-set(hashcmd
-  ${PYTHON_EXECUTABLE}
-  ${NRF_BOOTLOADER_SCRIPTS}/hash.py
-  --in ${PROJECT_BINARY_DIR}/${KERNEL_BIN_NAME}
-  > ${hash_file}
-  )
+    set(hashcmd
+      ${PYTHON_EXECUTABLE}
+      ${NRF_BOOTLOADER_SCRIPTS}/hash.py
+      --in ${PROJECT_BINARY_DIR}/${KERNEL_BIN_NAME}
+      > ${hash_file}
+      )
 
-if (CONFIG_SB_SIGNING_PYTHON)
-  set(signcmd
-    ${PYTHON_EXECUTABLE}
-    ${NRF_BOOTLOADER_SCRIPTS}/do_sign.py
-    --private-key ${SIGNATURE_PRIVATE_KEY_FILE}
-    --in ${hash_file}
-    > ${signature_file}
-    )
-  set(pubgencmd
-    ${PYTHON_EXECUTABLE}
-    ${NRF_BOOTLOADER_SCRIPTS}/keygen.py
-    --public
-    --in ${SIGNATURE_PRIVATE_KEY_FILE}
-    --out ${SIGNATURE_PUBLIC_KEY_FILE}
-    )
-elseif (CONFIG_SB_SIGNING_OPENSSL)
-  set(signcmd
-    openssl dgst
-    -sha256
-    -sign ${SIGNATURE_PRIVATE_KEY_FILE} ${hash_file} |
-    ${PYTHON_EXECUTABLE}
-    ${NRF_BOOTLOADER_SCRIPTS}/asn1parse.py
-    --alg ecdsa
-    --contents signature
-    > ${signature_file}
-    )
-  set(pubgencmd
-    openssl ec
-    -pubout
-    -in ${SIGNATURE_PRIVATE_KEY_FILE}
-    -out ${SIGNATURE_PUBLIC_KEY_FILE}
-    )
-elseif (CONFIG_SB_SIGNING_CUSTOM)
-  set(signcmd
-    ${CONFIG_SB_SIGNING_COMMAND}
-    ${hash_file}
-    > ${signature_file}
-    )
-  set(SIGNATURE_PUBLIC_KEY_FILE ${CONFIG_SB_SIGNING_PUBLIC_KEY})
-  if (signcmd STREQUAL "" OR NOT EXISTS ${SIGNATURE_PUBLIC_KEY_FILE})
-    message(WARNING "You must specify a signing command and valid public key file.")
-  endif()
-else ()
-  message(WARNING "Unable to parse signing config.")
-endif()
+    if (CONFIG_SB_SIGNING_PYTHON)
+      set(signcmd
+        ${PYTHON_EXECUTABLE}
+        ${NRF_BOOTLOADER_SCRIPTS}/do_sign.py
+        --private-key ${SIGNATURE_PRIVATE_KEY_FILE}
+        --in ${hash_file}
+        > ${signature_file}
+        )
+      set(pubgencmd
+        ${PYTHON_EXECUTABLE}
+        ${NRF_BOOTLOADER_SCRIPTS}/keygen.py
+        --public
+        --in ${SIGNATURE_PRIVATE_KEY_FILE}
+        --out ${SIGNATURE_PUBLIC_KEY_FILE}
+        )
+    elseif (CONFIG_SB_SIGNING_OPENSSL)
+      set(signcmd
+        openssl dgst
+        -sha256
+        -sign ${SIGNATURE_PRIVATE_KEY_FILE} ${hash_file} |
+        ${PYTHON_EXECUTABLE}
+        ${NRF_BOOTLOADER_SCRIPTS}/asn1parse.py
+        --alg ecdsa
+        --contents signature
+        > ${signature_file}
+        )
+      set(pubgencmd
+        openssl ec
+        -pubout
+        -in ${SIGNATURE_PRIVATE_KEY_FILE}
+        -out ${SIGNATURE_PUBLIC_KEY_FILE}
+        )
+    elseif (CONFIG_SB_SIGNING_CUSTOM)
+      set(signcmd
+        ${CONFIG_SB_SIGNING_COMMAND}
+        ${hash_file}
+        > ${signature_file}
+        )
+      set(SIGNATURE_PUBLIC_KEY_FILE ${CONFIG_SB_SIGNING_PUBLIC_KEY})
+      if (signcmd STREQUAL "" OR NOT EXISTS ${SIGNATURE_PUBLIC_KEY_FILE})
+        message(WARNING "You must specify a signing command and valid public key file.")
+      endif()
+    else ()
+      message(WARNING "Unable to parse signing config.")
+    endif()
 
-add_custom_command(
-  OUTPUT
-  ${signature_file}
-  COMMAND
-  ${hashcmd}
-  COMMAND
-  ${signcmd}
-  DEPENDS
-  ${sign_depends}
-  WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-  COMMENT
-  "Creating signature of application"
-  USES_TERMINAL
-  )
+    add_custom_command(
+      OUTPUT
+      ${signature_file}
+      COMMAND
+      ${hashcmd}
+      COMMAND
+      ${signcmd}
+      DEPENDS
+      ${sign_depends}
+      WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+      COMMENT
+      "Creating signature of application"
+      USES_TERMINAL
+      )
 
-if (CONFIG_SB_PRIVATE_KEY_PROVIDED)
-  add_custom_command(
-    OUTPUT
-    ${SIGNATURE_PUBLIC_KEY_FILE}
-    COMMAND
-    ${pubgencmd}
-    DEPENDS
-    ${sign_depends}
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-    COMMENT
-    "Creating public key from private key used for signing"
-    USES_TERMINAL
-    )
-endif()
+    if (CONFIG_SB_PRIVATE_KEY_PROVIDED)
+      add_custom_command(
+        OUTPUT
+        ${SIGNATURE_PUBLIC_KEY_FILE}
+        COMMAND
+        ${pubgencmd}
+        DEPENDS
+        ${sign_depends}
+        COMMENT
+        "Creating public key from private key used for signing"
+        USES_TERMINAL
+        )
+    endif()
 
-add_custom_command(
-  OUTPUT
-  ${SIGNED_KERNEL_HEX}
-  COMMAND
-  ${PYTHON_EXECUTABLE}
-  ${NRF_BOOTLOADER_SCRIPTS}/validation_data.py
+    add_custom_command(
+      OUTPUT
+      ${SIGNED_KERNEL_HEX}
+      COMMAND
+      ${PYTHON_EXECUTABLE}
+      ${NRF_BOOTLOADER_SCRIPTS}/validation_data.py
   --input ${PROJECT_BINARY_DIR}/${KERNEL_HEX_NAME}
   --output ${SIGNED_KERNEL_HEX}
   --offset ${CONFIG_FW_VALIDATION_METADATA_OFFSET}
@@ -117,6 +116,12 @@ add_custom_command(
   "Creating validation for ${KERNEL_HEX_NAME}, storing to ${SIGNED_KERNEL_HEX_NAME}"
   USES_TERMINAL
   )
+
+add_custom_target(
+    signature_public_file_target
+    DEPENDS
+    ${signature_file}
+)
 
 add_custom_target(
   signed_kernel_hex_target
