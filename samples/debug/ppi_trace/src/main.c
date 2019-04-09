70fd3edd65d526dcc856ee9556719b160781cf64
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2019-04-09 14:53:32 +0200

    samples: debug: Add ppi_trace sample
    Added sample demonstarating ppi_trace.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

---
+++ samples/debug/ppi_trace/src/main.c
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <zephyr.h>
+#include <debug/ppi_trace.h>
+#include <counter.h>
+#include <nrf_rtc.h>
+#include <nrf_clock.h>
+#include <device.h>
+#include <logging/log.h>
+
+LOG_MODULE_REGISTER(app);
+
+#define ALARM_PERIOD_US 50000
+
+#define RTC COND_CODE_1(CONFIG_USE_RTC2, (NRF_RTC2), (NRF_RTC0))
+
+#define RTC_LABEL \
+	COND_CODE_1( \
+		CONFIG_USE_RTC2, \
+		(DT_NORDIC_NRF_RTC_RTC_2_LABEL), \
+		(DT_NORDIC_NRF_RTC_RTC_0_LABEL) \
+	)
+
+static void alarm_callback(struct device *dev, u8_t chan_id, u32_t ticks,
+			   void *user_data);
+
+static struct counter_alarm_cfg alarm_cfg = {
+	.callback = alarm_callback,
+	.absolute = true
+};
+
+extern void bluetooth_enable(void);
+
+static void ppi_trace_pin_setup(u32_t pin, u32_t evt)
+{
+	void *handle;
+
+	handle = ppi_trace_config(pin, evt);
+	__ASSERT(handle != NULL,
+		"Failed to initialize trace pin, no PPI or GPIOTE resources?");
+
+	ppi_trace_enable(handle);
+}
+
+static void ppi_trace_setup(void)
+{
+	/* chan_id=0 maps to COMPARE_1, COMPARE_0 is internally used by the
+	 * driver.
+	 */
+	ppi_trace_pin_setup(CONFIG_PPI_TRACE_PIN_RTC_COMPARE_EVT,
+		nrf_rtc_event_address_get(RTC, NRF_RTC_EVENT_COMPARE_1));
+
+	/* Due to low power domain events must be explicitly enabled in RTC. */
+	nrf_rtc_event_enable(RTC, NRF_RTC_INT_TICK_MASK);
+	ppi_trace_pin_setup(CONFIG_PPI_TRACE_PIN_RTC_TICK_EVT,
+		nrf_rtc_event_address_get(RTC, NRF_RTC_EVENT_TICK));
+
+	ppi_trace_pin_setup(CONFIG_PPI_TRACE_PIN_LFCLOCK_STARTED_EVT,
+		nrf_clock_event_address_get(NRF_CLOCK_EVENT_LFCLKSTARTED));
+
+	LOG_INF("PPI trace setup done.");
+}
+
+static void alarm_callback(struct device *dev, u8_t chan_id, u32_t ticks,
+			   void *user_data)
+{
+	int err;
+	u32_t alarm_cnt = (u32_t)user_data + 1;
+
+	alarm_cfg.ticks = ticks + counter_us_to_ticks(dev, ALARM_PERIOD_US);
+	alarm_cfg.user_data = (void *)alarm_cnt;
+
+	err = counter_set_channel_alarm(dev, 0, &alarm_cfg);
+	__ASSERT_NO_MSG(err == 0);
+	(void)err;
+}
+
+static void counter_setup(void)
+{
+	int err;
+	struct device *dev = device_get_binding(RTC_LABEL);
+
+	__ASSERT(dev, "Sample cannot run on this board.");
+
+	alarm_cfg.ticks = counter_us_to_ticks(dev, ALARM_PERIOD_US);
+	err = counter_set_channel_alarm(dev, 0, &alarm_cfg);
+	__ASSERT_NO_MSG(err == 0);
+
+	err = counter_start(dev);
+	__ASSERT_NO_MSG(err == 0);
+}
+
+void main(void)
+{
+	ppi_trace_setup();
+	counter_setup();
+
+	if (IS_ENABLED(CONFIG_BT)) {
+		bluetooth_enable();
+	}
+}
