a007774777cd90d812dc5a5cc0ae6c37c2a46e8e
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2019-04-23 16:38:43 +0200

    boards: Add PCA20037
    Jira:DESK-483
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

---
+++ boards/arm/nrf52_pca20037/board.cmake
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 Nordic Semiconductor
+#
+# SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+#
+
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
