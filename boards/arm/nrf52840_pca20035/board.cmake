56073ff606acf84ffa079fd3bc54869e8f6053a4
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2019-04-26 16:20:17 +0200

    boards: nrf52840_pca20035: Support only newest HW version
    This patch removes support for early prototype hardware and
    moves to only support latest revision. This simplifies the
    device tree file sctructure. In addition, UART with DMA
    is enabled by default for UART instance 0.
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

--- boards/arm/nrf52840_pca20035/board.cmake
+++ boards/arm/nrf52840_pca20035/board.cmake
@@ -1,3 +1,6 @@
+# Copyright (c) 2019 Nordic Semiconductor ASA.
+# SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+
 board_runner_args(nrfjprog "--nrf-family=NRF52")
 board_runner_args(jlink "--device=nrf52" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
