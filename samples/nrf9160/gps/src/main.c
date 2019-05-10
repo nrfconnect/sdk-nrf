f76722bc521676b4d558999b496d1b5d8b91b8f7
Carles Cufi <carles.cufi@nordicsemi.no>
2019-05-10 10:54:14 +0200

    license: Add missing license in several files
    Add the Nordic license in files that had none.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- samples/nrf9160/gps/src/main.c
+++ samples/nrf9160/gps/src/main.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
 #include <zephyr.h>
 #include <bsd.h>
 #include <nrf_socket.h>
