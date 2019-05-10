402eb65c27b583fd483f04c381d4c883f1e81f4a
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2019-05-10 13:33:53 +0200

    subsys: spm: Extract SPU helper defines into separate file
    Add local include file spm_spu.h
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- subsys/spm/spm.c
+++ subsys/spm/spm.c
@@ -8,6 +8,7 @@
 #include <misc/printk.h>
 #include <device.h>
 #include <gpio.h>
+#include "spm_internal.h"
 
 #if !defined(CONFIG_ARM_SECURE_FIRMWARE)
 #error "Module requires compiling for Secure ARM Firmware"
@@ -26,8 +27,6 @@
 #define NON_SECURE_APP_ADDRESS DT_FLASH_AREA_IMAGE_0_NONSECURE_OFFSET_0
 #endif /* USE_PARTITION_MANAGER */
 
-/* Size of secure attribution configurable flash region */
-#define FLASH_SECURE_ATTRIBUTION_REGION_SIZE (32*1024)
 #define LAST_SECURE_ADDRESS (NON_SECURE_APP_ADDRESS - 1)
 #define LAST_SECURE_REGION \
 	(LAST_SECURE_ADDRESS / FLASH_SECURE_ATTRIBUTION_REGION_SIZE)
@@ -103,80 +102,6 @@ extern int irq_target_state_is_secure(unsigned int irq);
 	}
 #endif
 
-
-#define FLASH_EXEC                                                             \
-	((SPU_FLASHREGION_PERM_EXECUTE_Enable                                  \
-	  << SPU_FLASHREGION_PERM_EXECUTE_Pos) &                               \
-	 SPU_FLASHREGION_PERM_EXECUTE_Msk)
-
-#define FLASH_WRITE                                                            \
-	((SPU_FLASHREGION_PERM_WRITE_Enable                                    \
-	  << SPU_FLASHREGION_PERM_WRITE_Pos) &                                 \
-	 SPU_FLASHREGION_PERM_WRITE_Msk)
-
-#define FLASH_READ                                                             \
-	((SPU_FLASHREGION_PERM_READ_Enable << SPU_FLASHREGION_PERM_READ_Pos) & \
-	 SPU_FLASHREGION_PERM_READ_Msk)
-
-#define FLASH_LOCK                                                             \
-	((SPU_FLASHREGION_PERM_LOCK_Locked << SPU_FLASHREGION_PERM_LOCK_Pos) & \
-	 SPU_FLASHREGION_PERM_LOCK_Msk)
-
-#define FLASH_SECURE                                                           \
-	((SPU_FLASHREGION_PERM_SECATTR_Secure                                  \
-	  << SPU_FLASHREGION_PERM_SECATTR_Pos) &                               \
-	 SPU_FLASHREGION_PERM_SECATTR_Msk)
-
-#define FLASH_NONSEC                                                           \
-	((SPU_FLASHREGION_PERM_SECATTR_Non_Secure                              \
-	  << SPU_FLASHREGION_PERM_SECATTR_Pos) &                               \
-	 SPU_FLASHREGION_PERM_SECATTR_Msk)
-
-#define SRAM_EXEC                                                              \
-	((SPU_RAMREGION_PERM_EXECUTE_Enable                                    \
-	  << SPU_RAMREGION_PERM_EXECUTE_Pos) &                                 \
-	 SPU_RAMREGION_PERM_EXECUTE_Msk)
-
-#define SRAM_WRITE                                                             \
-	((SPU_RAMREGION_PERM_WRITE_Enable << SPU_RAMREGION_PERM_WRITE_Pos) &   \
-	 SPU_RAMREGION_PERM_WRITE_Msk)
-
-#define SRAM_READ                                                              \
-	((SPU_RAMREGION_PERM_READ_Enable << SPU_RAMREGION_PERM_READ_Pos) &     \
-	 SPU_RAMREGION_PERM_READ_Msk)
-
-#define SRAM_LOCK                                                              \
-	((SPU_RAMREGION_PERM_LOCK_Locked << SPU_RAMREGION_PERM_LOCK_Pos) &     \
-	 SPU_RAMREGION_PERM_LOCK_Msk)
-
-#define SRAM_SECURE                                                            \
-	((SPU_RAMREGION_PERM_SECATTR_Secure                                    \
-	  << SPU_RAMREGION_PERM_SECATTR_Pos) &                                 \
-	 SPU_RAMREGION_PERM_SECATTR_Msk)
-
-#define SRAM_NONSEC                                                            \
-	((SPU_RAMREGION_PERM_SECATTR_Non_Secure                                \
-	  << SPU_RAMREGION_PERM_SECATTR_Pos) &                                 \
-	 SPU_RAMREGION_PERM_SECATTR_Msk)
-
-#define PERIPH_PRESENT                                                         \
-	((SPU_PERIPHID_PERM_PRESENT_IsPresent                                  \
-	  << SPU_PERIPHID_PERM_PRESENT_Pos) &                                  \
-	 SPU_PERIPHID_PERM_PRESENT_Msk)
-
-#define PERIPH_NONSEC                                                          \
-	((SPU_PERIPHID_PERM_SECATTR_NonSecure                                  \
-	  << SPU_PERIPHID_PERM_SECATTR_Pos) &                                  \
-	 SPU_PERIPHID_PERM_SECATTR_Msk)
-
-#define PERIPH_DMA_NOSEP                                                       \
-	((SPU_PERIPHID_PERM_DMA_NoSeparateAttribute                            \
-	  << SPU_PERIPHID_PERM_DMA_Pos) &                                      \
-	 SPU_PERIPHID_PERM_DMA_Msk)
-
-#define PERIPH_LOCK                                                            \
-	((SPU_PERIPHID_PERM_LOCK_Locked << SPU_PERIPHID_PERM_LOCK_Pos) &       \
-	 SPU_PERIPHID_PERM_LOCK_Msk)
 
 static void spm_config_flash(void)
 {
