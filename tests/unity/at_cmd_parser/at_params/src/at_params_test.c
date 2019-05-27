19e96083bc1705d0c9740a04215c1d0949545b74
Christopher Métrailler <christopher.metrailler@nordicsemi.no>
2019-05-27 18:03:44 +0200

    Mocking kernel.h makes the native app crashing.
    Signed-off-by: Christopher Métrailler <christopher.metrailler@nordicsemi.no>

--- tests/unity/at_cmd_parser/at_params/src/at_params_test.c
+++ tests/unity/at_cmd_parser/at_params/src/at_params_test.c
@@ -8,6 +8,8 @@
 #include <at_params.h>
 #include <errno.h>
 
+#include "kernel/mock_kernel.h"
+
 void setUp(void)
 {
 	/* Empty. */
@@ -17,4 +19,16 @@ void test_at_params_list_init_null_param(void)
 {
 	int err = at_params_list_init(NULL, 0);
 	TEST_ASSERT_EQUAL(-EINVAL, err);
+}
+
+void test_at_params_list_init_no_mem(void)
+{
+	const size_t MAX_PARAMS = 4;
+
+	/* Want a null pointer when allocating memory in the at_params module. */
+	__wrap_k_calloc_ExpectAndReturn(MAX_PARAMS, sizeof(struct at_param), NULL);
+
+	struct at_param_list list;
+	int err = at_params_list_init(&list, MAX_PARAMS);
+	TEST_ASSERT_EQUAL(-ENOMEM, err);
 }
