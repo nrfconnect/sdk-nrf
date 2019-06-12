8c4b38798b6c15e3d3fcdcd85dfd55112a58080a
Christian Wilgaard <christian.wilgaard@nordicsemi.no>
2019-06-12 12:26:07 +0200

    cmake: DT: Allow dts bindings in nrf/dts
    Allow dts binding to be placed in nrf/dts by using
    the new DTS_ROOT feature.
    
    Signed-off-by: Christian Wilgaard <christian.wilgaard@nordicsemi.no>

--- cmake/boilerplate.cmake
+++ cmake/boilerplate.cmake
@@ -28,5 +28,5 @@ endif()
 # Add NRF_DIR as a BOARD_ROOT in case the board is in NRF_DIR
 list(APPEND BOARD_ROOT ${NRF_DIR})
 
-# Add NRF_DIR as a DTS_ROOT in case the dts binding is in NRF_DIR
+# Add NRF_DIR as a DTS_ROOT to include nrf/dts
 list(APPEND DTS_ROOT ${NRF_DIR})
