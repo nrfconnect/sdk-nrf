b24a6fabe4f1bb752737f1c08ba6bceaa76dc477
Christian Wilgaard <christian.wilgaard@nordicsemi.no>
2019-06-12 14:50:57 +0200

    samples: sensor: Adds bh1479 sample code
    Adds a sample showing how to enable the color sensor to trigger on
    data ready, or at a specified trigger level for a chosen
    color channel.
    
    Signed-off-by: Christian Wilgaard <christian.wilgaard@nordicsemi.no>
    Signed-off-by: Martin Koen Hongset <martin.hongset@nordicsemi.no>

---
+++ samples/sensor/bh1749/README.rst
@@ -0,0 +1,58 @@
+.. _bh1749:
+
+BH1749: Ambient Light Sensor IC
+#####################################################
+
+Description
+***********
+
+This sample application sets up the BH1749 color sensor to provide 8-bit
+measurement data every time a set threshold value (>50) is reached for the
+RED color channel.
+It also shows how to enable interrupt every time data is ready, instead
+of threshold trigging.
+
+References
+**********
+
+ - BH1749NUC-E: https://www.rohm.com/datasheet/BH1749NUC/bh1749nuc-e
+
+Requirements
+************
+
+* The following development board:
+
+  * PCA20035
+
+Building and Running
+********************
+
+This project outputs sensor data to the console. It requires a BH1749
+sensor. It should work with any platform featuring a I2C peripheral interface.
+It does not work on QEMU.
+In this example below the PCA20035 board is used.
+
+
+.. |sample path| replace:: :file:`samples/sensor/bh1749`
+
+.. include:: /includes/build_and_run.txt
+
+Sample Output
+=============
+
+.. J-Link RTT Viewer
+
+    device is 0x20022384, name is BH1749
+
+    Threshold trigger
+
+    BH1749 RED: 387
+
+    BH1749 GREEN: 753
+
+    BH1749 BLUE: 397
+
+    BH1749 IR: 81
+
+    (continues when trigger level reached)
+
---
+++ samples/sensor/bh1749/src/main.c
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+#include <zephyr.h>
+#include <device.h>
+#include <sensor.h>
+#include <stdio.h>
+#include <misc/__assert.h>
+
+#define THRESHOLD_UPPER                 50
+#define THRESHOLD_LOWER                 0
+#define TRIGGER_ON_DATA_READY           0
+
+static K_SEM_DEFINE(sem, 0, 1);
+
+static void trigger_handler(struct device *dev, struct sensor_trigger *trigger)
+{
+	ARG_UNUSED(dev);
+	switch (trigger->type) {
+	case SENSOR_TRIG_THRESHOLD:
+		printk("Threshold trigger\r\n");
+		break;
+	case SENSOR_TRIG_DATA_READY:
+		printk("Data ready trigger\r\n");
+		break;
+	default:
+		printk("Unknown trigger event %d\r\n", trigger->type);
+		break;
+	}
+	k_sem_give(&sem);
+}
+
+static int bh1479_set_attribute(struct device *dev, enum sensor_channel chan,
+				enum sensor_attribute attr, int value)
+{
+	int ret;
+	struct sensor_value sensor_val;
+
+	sensor_val.val1 = (value);
+
+	ret = sensor_attr_set(dev, chan, attr, &sensor_val);
+	if (ret) {
+		printk("sensor_attr_set failed ret %d\n", ret);
+	}
+
+	return ret;
+}
+
+/* This sets up  the sensor to signal valid data when a threshold
+ * value is reached.
+ */
+static void process(struct device *dev)
+{
+	int ret;
+	struct sensor_value temp_val;
+	struct sensor_trigger sensor_trig_conf = {
+		#if (TRIGGER_ON_DATA_READY)
+		.type = SENSOR_TRIG_DATA_READY,
+		#else
+		.type = SENSOR_TRIG_THRESHOLD,
+		#endif
+		.chan = SENSOR_CHAN_RED,
+	};
+
+	if (IS_ENABLED(CONFIG_BH1749_TRIGGER)) {
+		bh1479_set_attribute(dev, SENSOR_CHAN_ALL,
+				     SENSOR_ATTR_LOWER_THRESH,
+				     THRESHOLD_LOWER);
+		bh1479_set_attribute(dev, SENSOR_CHAN_ALL,
+				     SENSOR_ATTR_UPPER_THRESH,
+				     THRESHOLD_UPPER);
+
+		if (sensor_trigger_set(dev, &sensor_trig_conf,
+				trigger_handler)) {
+			printk("Could not set trigger\n");
+			return;
+		}
+	}
+
+	while (1) {
+		if (IS_ENABLED(CONFIG_BH1749_TRIGGER)) {
+			/* Waiting for a trigger event */
+			k_sem_take(&sem, K_FOREVER);
+		}
+		ret = sensor_sample_fetch_chan(dev, SENSOR_CHAN_ALL);
+		/* The sensor does only support fetching SENSOR_CHAN_ALL */
+		if (ret) {
+			printk("sensor_sample_fetch failed ret %d\n", ret);
+			return;
+		}
+
+		ret = sensor_channel_get(dev, SENSOR_CHAN_RED, &temp_val);
+		if (ret) {
+			printk("sensor_channel_get failed ret %d\n", ret);
+			return;
+		}
+		printk("BH1749 RED: %d\n", temp_val.val1);
+
+		ret = sensor_channel_get(dev, SENSOR_CHAN_GREEN, &temp_val);
+		if (ret) {
+			printk("sensor_channel_get failed ret %d\n", ret);
+			return;
+		}
+		printk("BH1749 GREEN: %d\n", temp_val.val1);
+
+		ret = sensor_channel_get(dev, SENSOR_CHAN_BLUE, &temp_val);
+		if (ret) {
+			printk("sensor_channel_get failed ret %d\n", ret);
+			return;
+		}
+		printk("BH1749 BLUE: %d\n", temp_val.val1);
+
+		ret = sensor_channel_get(dev, SENSOR_CHAN_IR, &temp_val);
+		if (ret) {
+			printk("sensor_channel_get failed ret %d\n", ret);
+			return;
+		}
+		printk("BH1749 IR: %d\n", temp_val.val1);
+		k_sleep(2000);
+	}
+}
+
+void main(void)
+{
+	struct device *dev;
+
+	if (IS_ENABLED(CONFIG_LOG_BACKEND_RTT)) {
+		/* Give RTT log time to be flushed before executing tests */
+		k_sleep(500);
+	}
+	dev = device_get_binding("BH1749");
+	if (dev == NULL) {
+		printk("Failed to get device binding\n");
+		return;
+	}
+	printk("device is %p, name is %s\n", dev, dev->config->name);
+	process(dev);
+}
