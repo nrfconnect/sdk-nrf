c41a41bf457036977982898e62bc1b0812d90625
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2019-07-29 10:26:48 +0200

    drivers: paw3212: Support CPI and sleep modes update
    Jira:DESK-586
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

---
+++ include/sensor/paw3212.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+#ifndef ZEPHYR_INCLUDE_PAW3212_H_
+#define ZEPHYR_INCLUDE_PAW3212_H_
+
+#include <sensor.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum paw3212_attribute {
+	PAW3212_ATTR_CPI = SENSOR_ATTR_PRIV_START,
+	PAW3212_ATTR_SLEEP1_TIMEOUT,
+	PAW3212_ATTR_SLEEP2_TIMEOUT,
+	PAW3212_ATTR_SLEEP3_TIMEOUT,
+};
+
+#define PAW3212_SVALUE_TO_CPI(svalue) ((u32_t)(svalue).val1)
+#define PAW3212_CPI_TO_SVALUE(cpi) ((struct sensor_value) {.val1 = cpi})
+
+#define PAW3212_SVALUE_TO_TIME(svalue) ((u32_t)(svalue).val1)
+#define PAW3212_TIME_TO_SVALUE(time) ((struct sensor_value) {.val1 = time})
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_PAW3212_H_ */
