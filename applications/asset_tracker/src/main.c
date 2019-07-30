nrfconnect/sdk-nrf

commit 81e002f920c85742bf1c16bf22b5c51103231927

applications: asset_tracker: Add cloud command decoding
The cloud codec has been expanded to parse an
incoming JSON string and perform a search after
known commands.

Signed-off-by: Henrik Malvik Halvorsen <henrik.halvorsen@nordicsemi.no>

applications/asset_tracker/src/main.c
@@ -60,7 +60,7 @@ defined(CONFIG_NRF_CLOUD_PROVISION_CERTIFICATES)
 #define CLOUD_LED_MSK UI_LED_1
 
 struct env_sensor {
-	enum cloud_sensor type;
+	enum cloud_channel type;
 	enum sensor_channel channel;
 	u8_t *dev_name;
 	struct device *dev;
@@ -82,19 +82,19 @@ static struct rsrp_data rsrp = {
 static struct cloud_backend *cloud_backend;
 
 static struct env_sensor temp_sensor = {
-	.type = CLOUD_SENSOR_TEMP,
+	.type = CLOUD_CHANNEL_TEMP,
 	.channel = SENSOR_CHAN_AMBIENT_TEMP,
 	.dev_name = CONFIG_TEMP_DEV_NAME
 };
 
 static struct env_sensor humid_sensor = {
-	.type = CLOUD_SENSOR_HUMID,
+	.type = CLOUD_CHANNEL_HUMID,
 	.channel = SENSOR_CHAN_HUMIDITY,
 	.dev_name = CONFIG_TEMP_DEV_NAME
 };
 
 static struct env_sensor pressure_sensor = {
-	.type = CLOUD_SENSOR_AIR_PRESS,
+	.type = CLOUD_CHANNEL_AIR_PRESS,
 	.channel = SENSOR_CHAN_PRESS,
 	.dev_name = CONFIG_TEMP_DEV_NAME
 };
@@ -108,14 +108,14 @@ static struct env_sensor *env_sensors[] = {
 
 /* Sensor data */
 static struct gps_data nmea_data;
-static struct cloud_sensor_data flip_cloud_data;
-static struct cloud_sensor_data gps_cloud_data;
-static struct cloud_sensor_data button_cloud_data;
-static struct cloud_sensor_data env_cloud_data[ARRAY_SIZE(env_sensors)];
+static struct cloud_channel_data flip_cloud_data;
+static struct cloud_channel_data gps_cloud_data;
+static struct cloud_channel_data button_cloud_data;
+static struct cloud_channel_data env_cloud_data[ARRAY_SIZE(env_sensors)];
 #if CONFIG_MODEM_INFO
-static struct cloud_sensor_data signal_strength_cloud_data;
-static struct cloud_sensor_data device_cloud_data;
 static struct modem_param_info modem_param;
+static struct cloud_channel_data signal_strength_cloud_data;
+static struct cloud_channel_data device_cloud_data;
 #endif /* CONFIG_MODEM_INFO */
 static atomic_val_t send_data_enable;
 
@@ -148,7 +148,7 @@ static void flip_send(struct k_work *work);
 static void env_data_send(void);
 static void sensors_init(void);
 static void work_init(void);
-static void sensor_data_send(struct cloud_sensor_data *data);
+static void sensor_data_send(struct cloud_channel_data *data);
 
 /**@brief nRF Cloud error handler. */
 void error_handler(enum error_type err_type, int err_code)
@@ -343,6 +343,11 @@ exit:
 	}
 }
 
+static void cloud_cmd_handler(struct cloud_command *cmd)
+{
+	/* Command handling goes here. */
+}
+
 #if CONFIG_MODEM_INFO
 /**@brief Callback handler for LTE RSRP data. */
 static void modem_rsrp_handler(char rsrp_value)
@@ -471,7 +476,7 @@ static void env_data_send(void)
 }
 
 /**@brief Send sensor data to nRF Cloud. **/
-static void sensor_data_send(struct cloud_sensor_data *data)
+static void sensor_data_send(struct cloud_channel_data *data)
 {
 	int err = 0;
 	struct cloud_data output;
@@ -480,8 +485,8 @@ static void sensor_data_send(struct cloud_sensor_data *data)
 		return;
 	}
 
-	if (data->type != CLOUD_DEVICE_INFO) {
-		err = cloud_encode_sensor_data(data, &output);
+	if (data->type != CLOUD_CHANNEL_DEVICE_INFO) {
+		err = cloud_encode_data(data, &output);
 	} else {
 		err = cloud_encode_digital_twin_data(data, &output);
 	}
@@ -489,6 +494,7 @@ static void sensor_data_send(struct cloud_sensor_data *data)
 	if (err) {
 		printk("Unable to encode cloud data: %d\n", err);
 	}
+	err = cloud_encode_data(data, &output);
 
 	struct cloud_msg msg = {
 		.buf = output.buf,
@@ -497,7 +503,7 @@ static void sensor_data_send(struct cloud_sensor_data *data)
 		.endpoint.type = CLOUD_EP_TOPIC_MSG
 	};
 
-	if (data->type == CLOUD_DEVICE_INFO) {
+	if (data->type == CLOUD_CHANNEL_DEVICE_INFO) {
 		msg.endpoint.type = CLOUD_EP_TOPIC_STATE;
 	}
 
@@ -550,6 +556,7 @@ void cloud_event_handler(const struct cloud_backend *const backend,
 		break;
 	case CLOUD_EVT_DATA_RECEIVED:
 		printk("CLOUD_EVT_DATA_RECEIVED\n");
+		cloud_decode_command(evt->data.msg.buf);
 		break;
 	default:
 		printk("**** Unknown cloud event type ****\n");
@@ -742,7 +749,7 @@ static void env_sensor_init(void)
 
 static void button_sensor_init(void)
 {
-	button_cloud_data.type = CLOUD_SENSOR_BUTTON;
+	button_cloud_data.type = CLOUD_CHANNEL_BUTTON;
 	button_cloud_data.tag = 0x1;
 }
 
@@ -759,10 +766,10 @@ static void modem_data_init(void)
 
 	modem_info_params_init(&modem_param);
 
-	signal_strength_cloud_data.type = CLOUD_LTE_LINK_RSRP;
+	signal_strength_cloud_data.type = CLOUD_CHANNEL_LTE_LINK_RSRP;
 	signal_strength_cloud_data.tag = 0x1;
 
-	device_cloud_data.type = CLOUD_DEVICE_INFO;
+	device_cloud_data.type = CLOUD_CHANNEL_DEVICE_INFO;
 	device_cloud_data.tag = 0x1;
 
 	k_work_submit(&device_status_work);
@@ -785,12 +792,12 @@ static void sensors_init(void)
 		button_sensor_init();
 	}
 
-	gps_cloud_data.type = CLOUD_SENSOR_GPS;
+	gps_cloud_data.type = CLOUD_CHANNEL_GPS;
 	gps_cloud_data.tag = 0x1;
 	gps_cloud_data.data.buf = nmea_data.str;
 	gps_cloud_data.data.len = nmea_data.len;
 
-	flip_cloud_data.type = CLOUD_SENSOR_FLIP;
+	flip_cloud_data.type = CLOUD_CHANNEL_FLIP;
 
 	/* Send sensor data after initialization, as it may be a long time until
 	 * next time if the application is in power optimized mode.
@@ -856,6 +863,13 @@ void main(void)
 	}
 
 	ui_init(ui_evt_handler);
+
+	ret = cloud_decode_init(cloud_cmd_handler);
+	if (ret) {
+		printk("Cloud command decoder could not be initialized, error: %d\n", ret);
+		cloud_error_handler(ret);
+	}
+
 	work_init();
 	modem_configure();
 

applications/asset_tracker/src/cloud_codec/cloud_codec.c
@@ -14,20 +14,187 @@
 #include "cJSON_os.h"
 #include "cloud_codec.h"
 
+#include <logging/log.h>
 
-static const char * const sensor_type_str[] = {
-	[CLOUD_SENSOR_GPS] = "GPS",
-	[CLOUD_SENSOR_FLIP] = "FLIP",
-	[CLOUD_SENSOR_BUTTON] = "BUTTON",
-	[CLOUD_SENSOR_TEMP] = "TEMP",
-	[CLOUD_SENSOR_HUMID] = "HUMID",
-	[CLOUD_SENSOR_AIR_PRESS] = "AIR_PRESS",
-	[CLOUD_SENSOR_AIR_QUAL] = "AIR_QUAL",
-	[CLOUD_LTE_LINK_RSRP] = "RSRP",
-	[CLOUD_DEVICE_INFO] = "DEVICE",
+struct cmd {
+	char *name;
+	union {
+		enum cloud_cmd_group group;
+		enum cloud_cmd_recipient recipient;
+		enum cloud_channel channel;
+		enum cloud_cmd_type type;
+	};
+
+	struct cmd *children;
+	size_t num_children;
 };
 
-/* --- A few wrappers for cJSON APIs --- */
+#define CMD_ARRAY(...) ((struct cmd[]) {__VA_ARGS__})
+
+#define CMD_NEW_TYPE(_name, _type)			\
+	{ 						\
+		.name = STRINGIFY(_name),		\
+		.type = _type, 				\
+	}
+
+#define CMD_NEW_CHAN(_name, _chan, _children)		\
+	{						\
+		.name = STRINGIFY(_name),		\
+		.channel = _chan,			\
+		.children = _children,			\
+		.num_children = ARRAY_SIZE(_children),	\
+	}
+
+#define CMD_NEW_RECIPIENT(_name, _recipient, _children)	\
+	{						\
+		.name = STRINGIFY(_name),		\
+		.recipient = _recipient,		\
+		.children = _children,			\
+		.num_children = ARRAY_SIZE(_children),	\
+	}
+
+#define CMD_NEW_GROUP(_name, _group, _children)		\
+	struct cmd _name = {				\
+		.name = STRINGIFY(_name),		\
+		.group = _group,			\
+		.children = _children,			\
+		.num_children = ARRAY_SIZE(_children),	\
+	};
+
+
+static CMD_NEW_GROUP(group_set, CLOUD_CMD_GROUP_SET, CMD_ARRAY(
+	CMD_NEW_RECIPIENT(environment, CLOUD_RCPT_ENVIRONMENT, CMD_ARRAY(
+		CMD_NEW_CHAN(humidity, CLOUD_CHANNEL_HUMID, CMD_ARRAY(
+			CMD_NEW_TYPE(enable, CLOUD_CMD_ENABLE),
+			CMD_NEW_TYPE(disable, CLOUD_CMD_DISABLE),
+			CMD_NEW_TYPE(threshold_high, CLOUD_CMD_THRESHOLD_HIGH),
+			CMD_NEW_TYPE(threshold_low, CLOUD_CMD_THRESHOLD_LOW))
+		),
+		CMD_NEW_CHAN(pressure, CLOUD_CHANNEL_AIR_PRESS, CMD_ARRAY(
+			CMD_NEW_TYPE(enable, CLOUD_CMD_ENABLE),
+			CMD_NEW_TYPE(disable, CLOUD_CMD_DISABLE),
+			CMD_NEW_TYPE(threshold_high, CLOUD_CMD_THRESHOLD_HIGH),
+			CMD_NEW_TYPE(threshold_low, CLOUD_CMD_THRESHOLD_LOW))
+		),
+		CMD_NEW_CHAN(temperature, CLOUD_CHANNEL_TEMP, CMD_ARRAY(
+			CMD_NEW_TYPE(enable, CLOUD_CMD_ENABLE),
+			CMD_NEW_TYPE(disable, CLOUD_CMD_DISABLE),
+			CMD_NEW_TYPE(threshold_high, CLOUD_CMD_THRESHOLD_HIGH),
+			CMD_NEW_TYPE(threshold_low, CLOUD_CMD_THRESHOLD_LOW))
+		),
+		CMD_NEW_CHAN(air_quality, CLOUD_CHANNEL_AIR_QUAL, CMD_ARRAY(
+			CMD_NEW_TYPE(enable, CLOUD_CMD_ENABLE),
+			CMD_NEW_TYPE(disable, CLOUD_CMD_DISABLE),
+			CMD_NEW_TYPE(threshold_high, CLOUD_CMD_THRESHOLD_HIGH),
+			CMD_NEW_TYPE(threshold_low, CLOUD_CMD_THRESHOLD_LOW))
+		))
+	),
+	CMD_NEW_RECIPIENT(motion, CLOUD_RCPT_MOTION, CMD_ARRAY(
+		CMD_NEW_CHAN(flip, CLOUD_CHANNEL_FLIP, CMD_ARRAY(
+			CMD_NEW_TYPE(enable, CLOUD_CMD_ENABLE),
+			CMD_NEW_TYPE(disable, CLOUD_CMD_DISABLE),
+			CMD_NEW_TYPE(threshold_high, CLOUD_CMD_THRESHOLD_HIGH),
+			CMD_NEW_TYPE(threshold_low, CLOUD_CMD_THRESHOLD_LOW))
+		),
+		CMD_NEW_CHAN(impact, CLOUD_CHANNEL_IMPACT, CMD_ARRAY(
+			CMD_NEW_TYPE(enable, CLOUD_CMD_ENABLE),
+			CMD_NEW_TYPE(disable, CLOUD_CMD_DISABLE),
+			CMD_NEW_TYPE(threshold_high, CLOUD_CMD_THRESHOLD_HIGH),
+			CMD_NEW_TYPE(threshold_low, CLOUD_CMD_THRESHOLD_LOW))
+		))
+	),
+	CMD_NEW_RECIPIENT(ui, CLOUD_RCPT_UI, CMD_ARRAY(
+		CMD_NEW_CHAN(pin, CLOUD_CHANNEL_PIN, CMD_ARRAY(
+			CMD_NEW_TYPE(enable, CLOUD_CMD_ENABLE),
+			CMD_NEW_TYPE(disable, CLOUD_CMD_DISABLE),
+			CMD_NEW_TYPE(pwm, CLOUD_CMD_PWM))
+		),
+		CMD_NEW_CHAN(led, CLOUD_CHANNEL_RGB_LED, CMD_ARRAY(
+			CMD_NEW_TYPE(red, CLOUD_CMD_LED_RED),
+			CMD_NEW_TYPE(green, CLOUD_CMD_LED_GREEN),
+			CMD_NEW_TYPE(blue, CLOUD_CMD_LED_BLUE),
+			CMD_NEW_TYPE(pulse_length, CLOUD_CMD_LED_PULSE_LENGTH),
+			CMD_NEW_TYPE(pause, CLOUD_CMD_LED_PAUSE_LENGTH))
+		),
+		CMD_NEW_CHAN(buzzer, CLOUD_CHANNEL_BUZZER, CMD_ARRAY(
+			CMD_NEW_TYPE(enable, CLOUD_CMD_ENABLE),
+			CMD_NEW_TYPE(disable, CLOUD_CMD_DISABLE),
+			CMD_NEW_TYPE(play_melody, CLOUD_CMD_PLAY_MELODY),
+			CMD_NEW_TYPE(play_note, CLOUD_CMD_PLAY_NOTE))
+		))
+	))
+);
+
+static CMD_NEW_GROUP(group_get, CLOUD_CMD_GROUP_GET, CMD_ARRAY(
+	CMD_NEW_RECIPIENT(environment, CLOUD_RCPT_ENVIRONMENT, CMD_ARRAY(
+		CMD_NEW_CHAN(humidity, CLOUD_CHANNEL_HUMID, CMD_ARRAY(
+			CMD_NEW_TYPE(read, CLOUD_CMD_READ),
+			CMD_NEW_TYPE(read_all, CLOUD_CMD_READ))
+		),
+		CMD_NEW_CHAN(pressure, CLOUD_CHANNEL_AIR_PRESS, CMD_ARRAY(
+			CMD_NEW_TYPE(read, CLOUD_CMD_READ),
+			CMD_NEW_TYPE(read_all, CLOUD_CMD_READ))
+		),
+		CMD_NEW_CHAN(temperature, CLOUD_CHANNEL_TEMP, CMD_ARRAY(
+			CMD_NEW_TYPE(read, CLOUD_CMD_READ),
+			CMD_NEW_TYPE(read_all, CLOUD_CMD_READ))
+		),
+		CMD_NEW_CHAN(air_quality, CLOUD_CHANNEL_AIR_QUAL, CMD_ARRAY(
+			CMD_NEW_TYPE(read, CLOUD_CMD_READ),
+			CMD_NEW_TYPE(read_all, CLOUD_CMD_READ))
+		))
+	),
+	CMD_NEW_RECIPIENT(modem_info, CLOUD_RCPT_MODEM_INFO, CMD_ARRAY(
+		CMD_NEW_CHAN(device, CLOUD_CHANNEL_DEVICE_INFO, CMD_ARRAY(
+			CMD_NEW_TYPE(read, CLOUD_CMD_READ),
+			CMD_NEW_TYPE(read_all, CLOUD_CMD_READ),
+			CMD_NEW_TYPE(read_new, CLOUD_CMD_READ_NEW))
+		),
+		CMD_NEW_CHAN(rsrp, CLOUD_CHANNEL_LTE_LINK_RSRP, CMD_ARRAY(
+			CMD_NEW_TYPE(read, CLOUD_CMD_READ),
+			CMD_NEW_TYPE(read_all, CLOUD_CMD_READ))
+		))
+	),
+	CMD_NEW_RECIPIENT(motion, CLOUD_RCPT_MOTION, CMD_ARRAY(
+		CMD_NEW_CHAN(flip, CLOUD_CHANNEL_FLIP, CMD_ARRAY(
+			CMD_NEW_TYPE(read, CLOUD_CMD_READ),
+			CMD_NEW_TYPE(read_all, CLOUD_CMD_READ))
+		),
+		CMD_NEW_CHAN(impact, CLOUD_CHANNEL_IMPACT, CMD_ARRAY(
+			CMD_NEW_TYPE(read, CLOUD_CMD_READ),
+			CMD_NEW_TYPE(read_all, CLOUD_CMD_READ))
+		))
+	),
+	CMD_NEW_RECIPIENT(ui, CLOUD_RCPT_UI, CMD_ARRAY(
+		CMD_NEW_CHAN(pin, CLOUD_CHANNEL_PIN, CMD_ARRAY(
+			CMD_NEW_TYPE(read, CLOUD_CMD_READ),
+			CMD_NEW_TYPE(read_all, CLOUD_CMD_READ))
+		),
+		CMD_NEW_CHAN(led, CLOUD_CHANNEL_RGB_LED, CMD_ARRAY(
+			CMD_NEW_TYPE(read, CLOUD_CMD_READ),
+			CMD_NEW_TYPE(read_all, CLOUD_CMD_READ))
+		),
+		CMD_NEW_CHAN(buzzer, CLOUD_CHANNEL_BUZZER, CMD_ARRAY(
+			CMD_NEW_TYPE(read, CLOUD_CMD_READ),
+			CMD_NEW_TYPE(read_all, CLOUD_CMD_READ))
+		))
+	))
+);
+
+static const char *const channel_type_str[] = {
+	[CLOUD_CHANNEL_GPS] = "GPS",
+	[CLOUD_CHANNEL_FLIP] = "FLIP",
+	[CLOUD_CHANNEL_BUTTON] = "BUTTON",
+	[CLOUD_CHANNEL_TEMP] = "TEMP",
+	[CLOUD_CHANNEL_HUMID] = "HUMID",
+	[CLOUD_CHANNEL_AIR_PRESS] = "AIR_PRESS",
+	[CLOUD_CHANNEL_AIR_QUAL] = "AIR_QUAL",
+	[CLOUD_CHANNEL_LTE_LINK_RSRP] = "RSRP",
+	[CLOUD_CHANNEL_DEVICE_INFO] = "DEVICE",
+};
+
+static cloud_cmd_cb_t cloud_command_cb;
+struct cloud_command cmd_parsed;
 
 static int json_add_obj(cJSON *parent, const char *str, cJSON *item)
 {
@@ -48,27 +215,30 @@ static int json_add_str(cJSON *parent, const char *str, const char *item)
 	return json_add_obj(parent, str, json_str);
 }
 
-int cloud_encode_sensor_data(const struct cloud_sensor_data *const sensor,
-				 struct cloud_data *const output)
+static cJSON *json_object_decode(cJSON *obj, const char *str)
+{
+	return obj ? cJSON_GetObjectItem(obj, str) : NULL;
+}
+
+int cloud_encode_data(const struct cloud_channel_data *channel,
+		      struct cloud_data *output)
 {
 	int ret;
 
-	__ASSERT_NO_MSG(sensor != NULL);
-	__ASSERT_NO_MSG(sensor->data.buf != NULL);
-	__ASSERT_NO_MSG(sensor->data.len != 0);
-	__ASSERT_NO_MSG(output != NULL);
+	if (channel == NULL || channel->data.buf == NULL ||
+	    channel->data.len == 0 || output == NULL) {
+		return -EINVAL;
+	}
 
 	cJSON *root_obj = cJSON_CreateObject();
-
 	if (root_obj == NULL) {
 		cJSON_Delete(root_obj);
 		return -ENOMEM;
 	}
 
-	ret = json_add_str(root_obj, "appId", sensor_type_str[sensor->type]);
-	ret += json_add_str(root_obj, "data", sensor->data.buf);
+	ret = json_add_str(root_obj, "appId", channel_type_str[channel->type]);
+	ret += json_add_str(root_obj, "data", channel->data.buf);
 	ret += json_add_str(root_obj, "messageType", "DATA");
-
 	if (ret != 0) {
 		cJSON_Delete(root_obj);
 		return -ENOMEM;
@@ -85,15 +255,15 @@ int cloud_encode_sensor_data(const struct cloud_sensor_data *const sensor,
 	return 0;
 }
 
-int cloud_encode_digital_twin_data(const struct cloud_sensor_data *sensor,
+int cloud_encode_digital_twin_data(const struct cloud_channel_data *channel,
 				 struct cloud_data *output)
 {
 	int ret;
 	char *buffer;
 
-	__ASSERT_NO_MSG(sensor != NULL);
-	__ASSERT_NO_MSG(sensor->data.buf != NULL);
-	__ASSERT_NO_MSG(sensor->data.len != 0);
+	__ASSERT_NO_MSG(channel != NULL);
+	__ASSERT_NO_MSG(channel->data.buf != NULL);
+	__ASSERT_NO_MSG(channel->data.len != 0);
 	__ASSERT_NO_MSG(output != NULL);
 
 	cJSON *root_obj = cJSON_CreateObject();
@@ -107,8 +277,8 @@ int cloud_encode_digital_twin_data(const struct cloud_sensor_data *sensor,
 		return -ENOMEM;
 	}
 
-	ret = json_add_obj(reported_obj, sensor_type_str[sensor->type],
-			   (cJSON *)sensor->data.buf);
+	ret = json_add_obj(reported_obj, channel_type_str[channel->type],
+			   (cJSON *)channel->data.buf);
 	ret += json_add_obj(state_obj, "reported", reported_obj);
 	ret += json_add_obj(root_obj, "state", state_obj);
 
@@ -124,6 +294,106 @@ int cloud_encode_digital_twin_data(const struct cloud_sensor_data *sensor,
 
 	output->buf = buffer;
 	output->len = strlen(buffer);
+
+	return 0;
+}
+
+static int cloud_search_cmd(cJSON *group_obj, enum cloud_cmd_group group)
+{
+	struct cmd *cmd_group;
+	cJSON *recipient_obj	= NULL;
+	cJSON *channel_obj	= NULL;
+	cJSON *type_obj		= NULL;
+
+	if (group_obj == NULL) {
+		return -EINVAL;
+	}
+
+	if (group == CLOUD_CMD_GROUP_SET) {
+		cmd_group = &group_set;
+	} else if (group == CLOUD_CMD_GROUP_GET) {
+		cmd_group = &group_get;
+	} else {
+		return -EINVAL;
+	}
+
+	for (size_t i = 0; i < cmd_group->num_children; i++) {
+		struct cmd rcpt = cmd_group->children[i];
+		recipient_obj = json_object_decode(group_obj,
+						   rcpt.name);
+		if (recipient_obj == NULL) {
+			continue;
+		}
+
+		cmd_parsed.recipient = rcpt.recipient;
+
+		for (size_t j = 0; j < rcpt.num_children; j++) {
+			struct cmd chan = rcpt.children[j];
+			channel_obj = json_object_decode(recipient_obj,
+							 chan.name);
+			if (channel_obj == NULL) {
+				continue;
+			}
+
+			cmd_parsed.channel = chan.channel;
+
+			for (size_t k = 0; k < chan.num_children; k++) {
+				struct cmd typ = chan.children[k];
+				type_obj = json_object_decode(channel_obj,
+							      typ.name);
+				if (type_obj == NULL) {
+					continue;
+				}
+
+				cmd_parsed.type = typ.type;
+
+				cloud_command_cb(&cmd_parsed);
+			}
+		}
+	}
+
+	return 0;
+}
+
+int cloud_decode_command(char const *input)
+{
+	int ret;
+	cJSON *root_obj		= NULL;
+	cJSON *group_obj	= NULL;
+
+	if (input == NULL) {
+		return -EINVAL;
+	}
+
+	root_obj = cJSON_Parse(input);
+	if (root_obj == NULL) {
+		return -ENOENT;
+	}
+
+	group_obj = json_object_decode(root_obj, "get");
+	if (group_obj != NULL) {
+		cmd_parsed.group = group_get.group;
+		ret = cloud_search_cmd(group_obj, cmd_parsed.group);
+		if (ret) {
+			return ret;
+		}
+	}
+
+	group_obj = json_object_decode(root_obj, "set");
+	if (group_obj != NULL) {
+		cmd_parsed.group = group_set.group;
+		ret = cloud_search_cmd(group_obj, cmd_parsed.group);
+		if (ret) {
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
+int cloud_decode_init(cloud_cmd_cb_t cb)
+{
+	cloud_command_cb = cb;
 
 	return 0;
 }

applications/asset_tracker/src/cloud_codec/cloud_codec.h
@@ -18,25 +18,33 @@ extern "C" {
 #endif
 
 /** @brief Cloud sensor types. */
-enum cloud_sensor {
+enum cloud_channel {
 	/** The GPS sensor on the device. */
-	CLOUD_SENSOR_GPS,
+	CLOUD_CHANNEL_GPS,
 	/** The FLIP movement sensor on the device. */
-	CLOUD_SENSOR_FLIP,
+	CLOUD_CHANNEL_FLIP,
+	/** The IMPACT movement sensor on the device. */
+	CLOUD_CHANNEL_IMPACT,
 	/** The Button press sensor on the device. */
-	CLOUD_SENSOR_BUTTON,
+	CLOUD_CHANNEL_BUTTON,
+	/** The PIN unit on the device. */
+	CLOUD_CHANNEL_PIN,
+	/** The RGB LED on the device. */
+	CLOUD_CHANNEL_RGB_LED,
+	/** The BUZZER on the device. */
+	CLOUD_CHANNEL_BUZZER,
 	/** The TEMP sensor on the device. */
-	CLOUD_SENSOR_TEMP,
+	CLOUD_CHANNEL_TEMP,
 	/** The Humidity sensor on the device. */
-	CLOUD_SENSOR_HUMID,
+	CLOUD_CHANNEL_HUMID,
 	/** The Air Pressure sensor on the device. */
-	CLOUD_SENSOR_AIR_PRESS,
+	CLOUD_CHANNEL_AIR_PRESS,
 	/** The Air Quality sensor on the device. */
-	CLOUD_SENSOR_AIR_QUAL,
+	CLOUD_CHANNEL_AIR_QUAL,
 	/** The RSPR data obtained from the modem. */
-	CLOUD_LTE_LINK_RSRP,
+	CLOUD_CHANNEL_LTE_LINK_RSRP,
 	/** The descriptive DEVICE data indicating its status. */
-	CLOUD_DEVICE_INFO,
+	CLOUD_CHANNEL_DEVICE_INFO,
 };
 
 struct cloud_data {
@@ -45,9 +53,9 @@ struct cloud_data {
 };
 
 /**@brief Sensor data parameters. */
-struct cloud_sensor_data {
+struct cloud_channel_data {
 	/** The sensor that is the source of the data. */
-	enum cloud_sensor type;
+	enum cloud_channel type;
 	/** Sensor data to be transmitted. */
 	struct cloud_data data;
 	/** Unique tag to identify the sent data.
@@ -56,17 +64,74 @@ struct cloud_sensor_data {
 	u32_t tag;
 };
 
+enum cloud_cmd_group {
+	CLOUD_CMD_GROUP_SET,
+	CLOUD_CMD_GROUP_GET,
+};
+
+enum cloud_cmd_recipient {
+	CLOUD_RCPT_ENVIRONMENT,
+	CLOUD_RCPT_MOTION,
+	CLOUD_RCPT_UI,
+	CLOUD_RCPT_MODEM_INFO,
+};
+
+enum cloud_cmd_type {
+	CLOUD_CMD_ENABLE,
+	CLOUD_CMD_DISABLE,
+	CLOUD_CMD_THRESHOLD_HIGH,
+	CLOUD_CMD_THRESHOLD_LOW,
+	CLOUD_CMD_READ,
+	CLOUD_CMD_READ_NEW,
+	CLOUD_CMD_PWM,
+	CLOUD_CMD_LED_RED,
+	CLOUD_CMD_LED_GREEN,
+	CLOUD_CMD_LED_BLUE,
+	CLOUD_CMD_LED_PULSE_LENGTH,
+	CLOUD_CMD_LED_PAUSE_LENGTH,
+	CLOUD_CMD_PLAY_MELODY,
+	CLOUD_CMD_PLAY_NOTE,
+};
+
+struct cloud_command {
+	enum cloud_cmd_group group; /* The group the decoded command belongs to. */
+	enum cloud_cmd_recipient recipient; /* The command's recipient module. */
+	enum cloud_channel channel; /* The command's desired channel. */
+	enum cloud_cmd_type type; /* The command type, the desired action. */
+	double value; /* The value to be written to the recipient/channel. */
+	bool state; /* The truth value to be written to the recipient/channel. */
+};
+
+typedef void (*cloud_cmd_cb_t)(struct cloud_command *cmd);
+
 /**
- * @brief Encode data from sensor data structure to a cloud data structure
- *	  containing a JSON string.
+ * @brief Encode cloud data.
  *
- * @param sensor Pointer to sensor data.
- * @param output Pointer to encoded data structure.
+ * @param channel The cloud channel type.
+ * @param output Pointer to the cloud data output.
  *
  * @return 0 if the operation was successful, otherwise a (negative) error code.
  */
-int cloud_encode_sensor_data(const struct cloud_sensor_data *const sensor,
-				 struct cloud_data *const output);
+int cloud_encode_data(const struct cloud_channel_data *channel,
+		      struct cloud_data *output);
+
+/**
+ * @brief Decode cloud data.
+ *
+ * @param output Pointer to the cloud data input.
+ *
+ * @return 0 if the operation was successful, otherwise a (negative) error code.
+ */
+int cloud_decode_command(char const *input);
+
+/**
+ * @brief Init the cloud decoder.
+ *
+ * @param cb The callback handler to be used by the cloud decoder.
+ *
+ * @return 0 if the operation was successful, otherwise a (negative) error code.
+ */
+int cloud_decode_init(cloud_cmd_cb_t cb);
 
 /**
  * @brief Encode data to be transmitted to the digital twin,
@@ -78,7 +143,7 @@ int cloud_encode_sensor_data(const struct cloud_sensor_data *const sensor,
  *
  * @return 0 if the operation was successful, otherwise a (negative) error code.
  */
-int cloud_encode_digital_twin_data(const struct cloud_sensor_data *sensor,
+int cloud_encode_digital_twin_data(const struct cloud_channel_data *channel,
 				 struct cloud_data *output);
 
 /**

commit 7cd1fb0316b9c85c0960266a91216651311578e0

applications: asset_tracker: fix memory leak
A duplicate call to cloud_encode_data introduced in 81e002f9 has been
removed, solving a memory leak.

Signed-off-by: Bernt Johan Damslora <bernt.johan.damslora@nordicsemi.no>

applications/asset_tracker/src/main.c
@@ -587,7 +587,6 @@ static void sensor_data_send(struct cloud_channel_data *data)
 	if (err) {
 		printk("Unable to encode cloud data: %d\n", err);
 	}
-	err = cloud_encode_data(data, &output);
 
 	struct cloud_msg msg = {
 		.buf = output.buf,