e0e0339e467dcc19353c94fc1606317b8ed33788
Robert Lubos <robert.lubos@nordicsemi.no>
2019-08-02 10:36:50 +0200

    samples: lwm2m_carrier: Add sample for library
    Add a sample to demonstrate how to use the LwM2M carrier library.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>
    Signed-off-by: Bartosz Gentkowski <bartosz.gentkowski@nordicsemi.no>

---
+++ samples/nrf9160/lwm2m_carrier/src/main.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+#include <lwm2m_carrier.h>
+#include <zephyr.h>
+
+void bsd_recoverable_error_handler(uint32_t err)
+{
+	printk("bsdlib recoverable error: %u\n", (unsigned int)err);
+}
+
+void bsd_irrecoverable_error_handler(uint32_t err)
+{
+	printk("bsdlib irrecoverable error: %u\n", (unsigned int)err);
+	__ASSERT_NO_MSG(false);
+}
+
+void lwm2m_carrier_event_handler(const lwm2m_carrier_event_t *event)
+{
+	switch (event->type) {
+	case LWM2M_CARRIER_EVENT_BSDLIB_INIT:
+		printk("LWM2M_CARRIER_EVENT_BSDLIB_INIT\n");
+		break;
+	case LWM2M_CARRIER_EVENT_CONNECT:
+		printk("LWM2M_CARRIER_EVENT_CONNECT\n");
+		break;
+	case LWM2M_CARRIER_EVENT_DISCONNECT:
+		printk("LWM2M_CARRIER_EVENT_DISCONNECT\n");
+		break;
+	case LWM2M_CARRIER_EVENT_READY:
+		printk("LWM2M_CARRIER_EVENT_READY\n");
+		break;
+	case LWM2M_CARRIER_EVENT_FOTA_START:
+		printk("LWM2M_CARRIER_EVENT_FOTA_START\n");
+		break;
+	case LWM2M_CARRIER_EVENT_REBOOT:
+		printk("LWM2M_CARRIER_EVENT_REBOOT\n");
+		break;
+	}
+}
+
+void main(void)
+{
+	printk("LWM2M Carrier library sample.\n");
+}
