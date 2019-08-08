2d01f78a4b3e8f54bed99554299a34e78f600180
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2019-08-08 08:46:25 +0000

    scripts: provision: remove verbose print
    The data printed can easily be debugged using an IDE, and the
    functionality takes up a large portion of the file.
    
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- scripts/bootloader/provision.py
+++ scripts/bootloader/provision.py
@@ -9,16 +9,8 @@ from intelhex import IntelHex
 
 import argparse
 import struct
-from ecdsa import SigningKey
 from ecdsa import VerifyingKey
 from hashlib import sha256
-
-VERBOSE = False
-
-
-def verbose_print(printstr):
-    if VERBOSE:
-        print(printstr)
 
 
 def generate_provision_hex_file(s0_address, s1_address, hashes, provision_address, output):
@@ -42,18 +34,14 @@ def parse_args():
                         help="Semicolon-separated list of public key .pem files.")
     parser.add_argument("-o", "--output", required=False, default="provision.hex",
                         help="Output file name.")
-    parser.add_argument("-v", "--verbose", required=False, action="count",
-                        help="Verbose mode.")
     return parser.parse_args()
 
 
 def get_hashes(public_key_files):
     hashes = list()
     for fn in public_key_files:
-        verbose_print("Getting hash of %s" % fn)
         with open(fn, 'rb') as f:
             hashes.append(sha256(VerifyingKey.from_pem(f.read()).to_string()).digest()[:16])
-        verbose_print("hash: " + hashes[-1].hex())
 
     if len(hashes) != len(set(hashes)):
         raise RuntimeError("Duplicate public key found. Note that the public key corresponding to the private"
@@ -64,9 +52,6 @@ def get_hashes(public_key_files):
 
 def main():
     args = parse_args()
-
-    global VERBOSE
-    VERBOSE = args.verbose
 
     s0_address = args.s0_addr
     s1_address = args.s1_addr
