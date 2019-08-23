54891c2dd36a64f005acaac70cbf20597ad94dbe
Michal Patyk <michal.patyk@nordicsemi.no>
2019-08-23 09:54:06 +0200

    applications: nrf_desktop: Add led_ready_event
    Add led_ready_event to notify led_stream that leds are ready for next
    led effect step.
    
    Jira:DESK-431
    
    Signed-off-by: Michal Patyk <michal.patyk@nordicsemi.no>

--- applications/nrf_desktop/src/events/led_event.c
+++ applications/nrf_desktop/src/events/led_event.c
@@ -22,3 +22,17 @@ EVENT_TYPE_DEFINE(led_event,
 		  IS_ENABLED(CONFIG_DESKTOP_INIT_LOG_LED_EVENT),
 		  log_led_event,
 		  NULL);
+
+static int log_led_ready_event(const struct event_header *eh, char *buf,
+			 size_t buf_len)
+{
+	const struct led_ready_event *event = cast_led_ready_event(eh);
+
+	return snprintf(buf, buf_len, "led_id:%u effect:%p",
+			event->led_id, event->led_effect);
+}
+
+EVENT_TYPE_DEFINE(led_ready_event,
+		  IS_ENABLED(CONFIG_DESKTOP_INIT_LOG_LED_READY_EVENT),
+		  log_led_ready_event,
+		  NULL);
