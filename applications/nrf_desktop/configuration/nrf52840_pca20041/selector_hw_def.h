d2f17aaf43041a24c3bb55fd6f356c587fc45dd7
Marek Pieta <Marek.Pieta@nordicsemi.no>
2019-08-29 16:12:41 +0200

    applications: nrf_desktop: Add selector HW interface
    Change adds selector HW interface.
    
    Jira:DESK-628
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/nrf_desktop/configuration/nrf52840_pca20041/selector_hw_def.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include "selector_hw.h"
+
+/* This configuration file is included only once from selector_hw module
+ * and holds information about pins used by module.
+ */
+
+/* This structure enforces the header file is included only once in the build.
+ * Violating this requirement triggers a multiple definition error at link time.
+ */
+const struct {} selector_hw_def_include_once;
+
+static const struct gpio_pin pins0[] = {
+	{ .port = 1, .pin = 11 },
+	{ .port = 1, .pin = 12 },
+};
+static const struct selector_config config0 = {
+	.id = 0,
+	.pins = pins0,
+	.pins_size = ARRAY_SIZE(pins0)
+};
+
+static const struct selector_config selector_config[] = {
+	config0,
+};
