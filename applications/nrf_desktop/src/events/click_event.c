1774c4af897bef72153c092cecc30325e42ec10f
Marek Pieta <Marek.Pieta@nordicsemi.no>
2019-09-02 12:54:45 +0200

    applications: nrf_desktop: Click event
    Change adds click event.
    
    Jira:DESK-628
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/nrf_desktop/src/events/click_event.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <stdio.h>
+
+#include "click_event.h"
+
+static const char * const click_name[] = {
+#define X(name) STRINGIFY(name),
+	CLICK_LIST
+#undef X
+};
+
+static int log_click_event(const struct event_header *eh, char *buf,
+			   size_t buf_len)
+{
+	const struct click_event *event = cast_click_event(eh);
+
+	__ASSERT_NO_MSG(event->click < CLICK_COUNT);
+	return snprintf(buf, buf_len, "key_id: %" PRIu16 " click: %s",
+			event->key_id,
+			click_name[event->click]);
+}
+
+static void profile_click_event(struct log_event_buf *buf,
+				const struct event_header *eh)
+{
+	const struct click_event *event = cast_click_event(eh);
+
+	ARG_UNUSED(event);
+	profiler_log_encode_u32(buf, event->key_id);
+	profiler_log_encode_u32(buf, event->click);
+}
+
+EVENT_INFO_DEFINE(click_event,
+		  ENCODE(PROFILER_ARG_U16, PROFILER_ARG_U8),
+		  ENCODE("key_id", "click"),
+		  profile_click_event);
+
+EVENT_TYPE_DEFINE(click_event,
+		  IS_ENABLED(CONFIG_DESKTOP_INIT_LOG_CLICK_EVENT),
+		  log_click_event,
+		  &click_event_info);
---
+++ applications/nrf_desktop/src/events/click_event.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#ifndef _CLICK_EVENT_H_
+#define _CLICK_EVENT_H_
+
+/**
+ * @brief Click Event
+ * @defgroup click_event Click Event
+ * @{
+ */
+
+#include "event_manager.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @brief Click types list. */
+#define CLICK_LIST		\
+	X(NONE)			\
+	X(SHORT)		\
+	X(LONG)			\
+	X(DOUBLE)		\
+
+/** @brief Click types. */
+enum click {
+#define X(name) _CONCAT(CLICK_, name),
+	CLICK_LIST
+#undef X
+	CLICK_COUNT
+};
+
+struct click_event {
+	struct event_header header;
+
+	u16_t key_id;
+	enum click click;
+};
+
+EVENT_TYPE_DECLARE(click_event);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _CLICK_EVENT_H_ */
