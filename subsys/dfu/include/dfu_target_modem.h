7dae0bf2b7bb39f6cf8fbab843d46f91fa31446e
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2019-10-10 10:06:42 +0200

    subsys: dfu: dfu_target: Add generic target API for DFU operations
    Add new subsystem called `dfu_target` which contains an
    abstraction layer for doing multiple types of DFU operations. In it's
    current form it will be able to handle modem updates, application
    updates and updates of MCUboot when the immutable bootloader is used.
    
    NCSDK-3102
    NCSDK-2892
    NCSDK-2891
    
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

---
+++ subsys/dfu/include/dfu_target_modem.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/** @file dfu_target_modem.h
+ *
+ * @defgroup dfu_target_modem Modem DFU Target
+ * @{
+ * @brief DFU Target for upgrades performed by Modem
+ */
+
+#ifndef DFU_TARGET_MODEM_H__
+#define DFU_TARGET_MODEM_H__
+
+#include <zephyr/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief See if data in buf indicates modem upgrade.
+ *
+ * @retval true if data matches, false otherwise.
+ */
+bool dfu_target_modem_identify(const void *const buf);
+
+/**
+ * @brief Initialize dfu target, perform steps necessary to receive firmware.
+ *
+ * @param[in] file_size Size of the current file being downloaded.
+ *
+ * @retval 0 If successful, negative errno otherwise.
+ */
+int dfu_target_modem_init(size_t file_size);
+
+/**
+ * @brief Get offset of firmware
+ *
+ * @param[out] offset Returns the offset of the firmware upgrade.
+ *
+ * @return 0 if success, otherwise negative value if unable to get the offset
+ */
+int dfu_target_modem_offset_get(size_t *offset);
+
+/**
+ * @brief Write firmware data.
+ *
+ * @param[in] buf Pointer to data that should be written.
+ * @param[in] len Length of data to write.
+ *
+ * @return 0 on success, negative errno otherwise.
+ */
+int dfu_target_modem_write(const void *const buf, size_t len);
+
+/**
+ * @brief Deinitialize resources and finalize firmware upgrade if successful.
+ *
+ * @param[in] successful Indicate whether the firmware was successfully recived.
+ *
+ * @return 0 on success, negative errno otherwise.
+ */
+int dfu_target_modem_done(bool successful);
+
+#endif /* DFU_TARGET_MODEM_H__ */
+
+/**@} */
---
+++ subsys/dfu/src/dfu_target.c
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+#include <zephyr.h>
+#include <logging/log.h>
+#include <dfu/mcuboot.h>
+#include <dfu/dfu_target.h>
+#include "dfu_target_mcuboot.h"
+#include "dfu_target_modem.h"
+
+const struct dfu_target dfu_target_modem = {
+	.init = dfu_target_modem_init,
+	.offset_get = dfu_target_modem_offset_get,
+	.write = dfu_target_modem_write,
+	.done = dfu_target_modem_done,
+};
+
+const struct dfu_target dfu_target_mcuboot = {
+	.init  = dfu_target_mcuboot_init,
+	.offset_get = dfu_target_mcuboot_offset_get,
+	.write = dfu_target_mcuboot_write,
+	.done  = dfu_target_mcuboot_done,
+};
+
+#define MIN_SIZE_IDENTIFY_BUF 32
+
+LOG_MODULE_REGISTER(dfu_target, CONFIG_DFU_TARGET_LOG_LEVEL);
+
+static const struct dfu_target *current_target;
+
+int dfu_target_img_type(const void *const buf, size_t len)
+{
+	if (IS_ENABLED(CONFIG_DFU_TARGET_MCUBOOT) &&
+	    dfu_target_mcuboot_identify(buf)) {
+		return DFU_TARGET_IMAGE_TYPE_MCUBOOT;
+	}
+
+	if (IS_ENABLED(CONFIG_DFU_TARGET_MODEM) &&
+	    dfu_target_modem_identify(buf)) {
+		return DFU_TARGET_IMAGE_TYPE_MODEM_DELTA;
+	}
+
+	if (len < MIN_SIZE_IDENTIFY_BUF) {
+		return -EAGAIN;
+	}
+
+	LOG_ERR("No supported image type found");
+	return -ENOTSUP;
+}
+
+int dfu_target_init(int img_type, size_t file_size)
+{
+	const struct dfu_target *new_target = NULL;
+
+	if (IS_ENABLED(CONFIG_BOOTLOADER_MCUBOOT) &&
+	    img_type == DFU_TARGET_IMAGE_TYPE_MCUBOOT) {
+		new_target = &dfu_target_mcuboot;
+	} else if (IS_ENABLED(CONFIG_DFU_TARGET_MODEM) &&
+		   img_type == DFU_TARGET_IMAGE_TYPE_MODEM_DELTA) {
+		new_target = &dfu_target_modem;
+	}
+
+	if (new_target == NULL) {
+		LOG_ERR("Unknown image type");
+		return -ENOTSUP;
+	}
+
+	/* The user is re-initializing with an previously aborted target.
+	 * Avoid re-initializing to ensure that the download can continue where
+	 * it left off.
+	 */
+	if (new_target == current_target) {
+		return 0;
+	}
+
+	current_target = new_target;
+
+	return current_target->init(file_size);
+}
+
+int dfu_target_offset_get(size_t *offset)
+{
+	if (current_target == NULL) {
+		return -EACCES;
+	}
+
+	return current_target->offset_get(offset);
+}
+
+int dfu_target_write(const void *const buf, size_t len)
+{
+	if (current_target == NULL || buf == NULL) {
+		return -EACCES;
+	}
+
+	return current_target->write(buf, len);
+}
+
+int dfu_target_done(bool successful)
+{
+	int err;
+
+	if (current_target == NULL) {
+		return -EACCES;
+	}
+
+	err = current_target->done(successful);
+
+	if (successful) {
+		current_target = NULL;
+	}
+
+	if (err != 0) {
+		LOG_ERR("Unable to clean up dfu_target");
+		return err;
+	}
+
+	return 0;
+}
+
