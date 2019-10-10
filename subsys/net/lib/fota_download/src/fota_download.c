7f9ad90cb8a6430111edbe674d7b6d47ad4bc68a
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2019-10-10 10:12:08 +0200

    subsys: net: lib: fota_download: Change to use dfu_target API
    Change the implementaion to use the functionality of `dfu_target` so
    that it can support both application, bootloader and modem updates.
    `fota_download` will be able to resume on unexpected disconnects for
    application/bootloader update and resume on reboot and disconnects for
    modem updates.
    
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- subsys/net/lib/fota_download/src/fota_download.c
+++ subsys/net/lib/fota_download/src/fota_download.c
@@ -4,24 +4,23 @@
  * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
  */
 
-#include <stdint.h>
 #include <zephyr.h>
-#include <flash.h>
-#include <net/download_client.h>
-#include <dfu/mcuboot.h>
-#include <dfu/flash_img.h>
-#include <pm_config.h>
 #include <logging/log.h>
 #include <net/fota_download.h>
+#include <net/download_client.h>
+#include <dfu/dfu_target.h>
 
 LOG_MODULE_REGISTER(fota_download, CONFIG_FOTA_DOWNLOAD_LOG_LEVEL);
 
-static		fota_download_callback_t callback;
-static struct	flash_img_context flash_img;
-static struct	download_client dfu;
+static fota_download_callback_t callback;
+static struct download_client   dlc;
+static struct k_delayed_work    dlc_with_offset_work;
 
 static int download_client_callback(const struct download_client_evt *event)
 {
+	static bool first_fragment = true;
+	size_t file_size;
+	size_t offset;
 	int err;
 
 	if (event == NULL) {
@@ -30,60 +29,75 @@ static int download_client_callback(const struct download_client_evt *event)
 
 	switch (event->id) {
 	case DOWNLOAD_CLIENT_EVT_FRAGMENT: {
-		size_t size;
+		if (first_fragment) {
+			err = download_client_file_size_get(&dlc, &file_size);
+			if (err != 0) {
+				LOG_DBG("download_client_file_size_get err: %d",
+					err);
+				callback(FOTA_DOWNLOAD_EVT_ERROR);
+				return err;
+			}
+			first_fragment = false;
+			int img_type = dfu_target_img_type(event->fragment.buf,
+							event->fragment.len);
+			err = dfu_target_init(img_type, file_size);
+			if ((err < 0) && (err != -EBUSY)) {
+				LOG_ERR("dfu_target_init error %d", err);
+				return err;
+			}
 
-		err = download_client_file_size_get(&dfu, &size);
+			err = dfu_target_offset_get(&offset);
+			LOG_INF("Offset: 0x%x", offset);
+
+			if (offset != 0) {
+				/* Abort current download procedure, and
+				 * schedule new download from offset.
+				 */
+				k_delayed_work_submit(&dlc_with_offset_work,
+						K_SECONDS(1));
+				LOG_INF("Refuse fragment, restart with offset");
+
+				return -1;
+			}
+		}
+
+		err = dfu_target_write(event->fragment.buf,
+				       event->fragment.len);
 		if (err != 0) {
-			LOG_ERR("download_client_file_size_get error %d", err);
+			LOG_ERR("dfu_target_write error %d", err);
+			err = download_client_disconnect(&dlc);
 			callback(FOTA_DOWNLOAD_EVT_ERROR);
 			return err;
 		}
-		if (size > PM_MCUBOOT_SECONDARY_SIZE) {
-			LOG_ERR("Requested file too big to fit in flash\n");
-			callback(FOTA_DOWNLOAD_EVT_ERROR);
-			return -EFBIG;
-		}
-
-		err = flash_img_buffered_write(&flash_img,
-				(u8_t *)event->fragment.buf,
-				event->fragment.len, false);
-		if (err != 0) {
-			LOG_ERR("flash_img_buffered_write error %d", err);
-			callback(FOTA_DOWNLOAD_EVT_ERROR);
-			return err;
-		}
-		break;
+	break;
 	}
 
 	case DOWNLOAD_CLIENT_EVT_DONE:
-		/* Write with 0 length to flush the write operation to flash. */
-		err = flash_img_buffered_write(&flash_img,
-				(u8_t *)event->fragment.buf,
-				0, true);
+		err = dfu_target_done(true);
 		if (err != 0) {
-			LOG_ERR("flash_img_buffered_write error %d", err);
+			LOG_ERR("dfu_target_done error: %d", err);
 			callback(FOTA_DOWNLOAD_EVT_ERROR);
 			return err;
 		}
 
-		err = boot_request_upgrade(BOOT_UPGRADE_TEST);
+		err = download_client_disconnect(&dlc);
 		if (err != 0) {
-			LOG_ERR("boot_request_upgrade error %d", err);
-			callback(FOTA_DOWNLOAD_EVT_ERROR);
-			return err;
-		}
-		err = download_client_disconnect(&dfu);
-		if (err != 0) {
-			LOG_ERR("download_client_disconncet error %d", err);
 			callback(FOTA_DOWNLOAD_EVT_ERROR);
 			return err;
 		}
 		callback(FOTA_DOWNLOAD_EVT_FINISHED);
+		first_fragment = true;
 		break;
 
 	case DOWNLOAD_CLIENT_EVT_ERROR: {
-		download_client_disconnect(&dfu);
+		download_client_disconnect(&dlc);
 		LOG_ERR("Download client error");
+		err = dfu_target_done(false);
+		if (err != 0) {
+			LOG_ERR("Unable to deinitialze resources used "
+				"by dfu_target.");
+		}
+		first_fragment = true;
 		callback(FOTA_DOWNLOAD_EVT_ERROR);
 		return event->error;
 	}
@@ -94,8 +108,23 @@ static int download_client_callback(const struct download_client_evt *event)
 	return 0;
 }
 
+static void download_with_offset(struct k_work *unused)
+{
+	int offset;
+	int err = dfu_target_offset_get(&offset);
+
+	err = download_client_start(&dlc, dlc.file, offset);
+
+	LOG_INF("Downloading from offset: 0x%x", offset);
+	if (err != 0) {
+		LOG_ERR("%s failed with error %d", __func__, err);
+	}
+}
+
 int fota_download_start(char *host, char *file)
 {
+	int err = -1;
+
 	struct download_client_cfg config = {
 		.sec_tag = -1, /* HTTP */
 	};
@@ -104,29 +133,15 @@ int fota_download_start(char *host, char *file)
 		return -EINVAL;
 	}
 
-	/* Verify that a download is not already ongoing */
-	if (dfu.fd != -1) {
-		return -EALREADY;
-	}
-
-	int err = flash_img_init(&flash_img);
-
+	err = download_client_connect(&dlc, host, &config);
 	if (err != 0) {
-		LOG_ERR("flash_img_init error %d", err);
 		return err;
 	}
 
-	err = download_client_connect(&dfu, host, &config);
 
+	err = download_client_start(&dlc, file, 0);
 	if (err != 0) {
-		LOG_ERR("download_client_connect error %d", err);
-		return err;
-	}
-
-	err = download_client_start(&dfu, file, 0);
-	if (err != 0) {
-		LOG_ERR("download_client_start error %d", err);
-		download_client_disconnect(&dfu);
+		download_client_disconnect(&dlc);
 		return err;
 	}
 	return 0;
@@ -140,10 +155,11 @@ int fota_download_init(fota_download_callback_t client_callback)
 
 	callback = client_callback;
 
-	int err = download_client_init(&dfu, download_client_callback);
+	k_delayed_work_init(&dlc_with_offset_work, download_with_offset);
+
+	int err = download_client_init(&dlc, download_client_callback);
 
 	if (err != 0) {
-		LOG_ERR("download_client_init error %d", err);
 		return err;
 	}
 
