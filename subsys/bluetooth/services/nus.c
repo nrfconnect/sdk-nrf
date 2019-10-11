2ce76e23e8d1cc61df916b11884dfc4d8e5039b3
Joakim Andersson <joakim.andersson@nordicsemi.no>
2019-10-11 12:05:56 +0200

    Bluetooth: Services: NUS: Fix bt_gatt_nus_send with given conn
    Fix is_notification_enabled when checking if notifications is enabled
    for a given connection. This function used the wrong user_data
    definition and compared addresses using the wrong pointer.
    The function was also given the wrong attributed.
    Also checking if notifications is enabled would fail if indications was
    also enabled.
    Also deleted the cfg_changed as it only contains information about the
    the attribute and the value, not which peer subscribed.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- subsys/bluetooth/services/nus.c
+++ subsys/bluetooth/services/nus.c
@@ -15,39 +15,6 @@ LOG_MODULE_REGISTER(bt_gatt_nus, CONFIG_BT_GATT_NUS_LOG_LEVEL);
 
 static struct bt_gatt_nus_cb nus_cb;
 
-static bool is_notification_enabled(struct bt_conn *conn,
-				    const struct bt_gatt_ccc_cfg *ccd)
-{
-	const bt_addr_le_t *conn_addr = bt_conn_get_dst(conn);
-
-	for (size_t i = 0; i < BT_GATT_CCC_MAX; i++) {
-
-		const bt_addr_le_t *ccd_addr = &ccd[i].peer;
-
-		if ((!memcmp(conn_addr, ccd_addr, sizeof(bt_addr_le_t))) &&
-		    (ccd[i].value == BT_GATT_CCC_NOTIFY)) {
-			return true;
-		}
-	}
-
-	LOG_DBG("Notification disabled for conn: %p", conn);
-
-	return false;
-}
-
-static void nuslc_ccc_cfg_changed(const struct bt_gatt_attr *attr,
-				  u16_t value)
-{
-	char addr[BT_ADDR_LE_STR_LEN] = {0};
-	struct bt_gatt_ccc_cfg *cfg =
-		(struct bt_gatt_ccc_cfg *)attr->user_data;
-
-	bt_addr_le_to_str(&cfg->peer, addr, ARRAY_SIZE(addr));
-
-	LOG_DBG("%s CCCD has changed, state: %s", log_strdup(addr),
-		value ? "notification enabled" : "notification disabled");
-}
-
 static ssize_t on_receive(struct bt_conn *conn,
 			  const struct bt_gatt_attr *attr,
 			  const void *buf,
@@ -82,8 +49,7 @@ BT_GATT_PRIMARY_SERVICE(BT_UUID_NUS_SERVICE),
 			       BT_GATT_CHRC_NOTIFY,
 			       BT_GATT_PERM_READ,
 			       NULL, NULL, NULL),
-	BT_GATT_CCC(nuslc_ccc_cfg_changed,
-		    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+	BT_GATT_CCC(NULL, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 	BT_GATT_CHARACTERISTIC(BT_UUID_NUS_RX,
 			       BT_GATT_CHRC_WRITE |
 			       BT_GATT_CHRC_WRITE_WITHOUT_RESP,
@@ -104,18 +70,17 @@ int bt_gatt_nus_init(struct bt_gatt_nus_cb *callbacks)
 int bt_gatt_nus_send(struct bt_conn *conn, const u8_t *data, uint16_t len)
 {
 	struct bt_gatt_notify_params params = {0};
-	const struct bt_gatt_attr *ccc_attr = &nus_svc.attrs[2];
-	const struct bt_gatt_ccc_cfg *ccc_cfg = ccc_attr->user_data;
+	const struct bt_gatt_attr *attr = &nus_svc.attrs[2];
 
-	params.attr = ccc_attr;
+	params.attr = attr;
 	params.data = data;
 	params.len = len;
 	params.func = on_sent;
 
 	if (!conn) {
 		LOG_DBG("Notification send to all connected peers");
-		return  bt_gatt_notify_cb(NULL, &params);
-	} else if (is_notification_enabled(conn, ccc_cfg)) {
+		return bt_gatt_notify_cb(NULL, &params);
+	} else if (bt_gatt_is_subscribed(conn, attr, BT_GATT_CCC_NOTIFY)) {
 		return bt_gatt_notify_cb(conn, &params);
 	} else {
 		return -EINVAL;
