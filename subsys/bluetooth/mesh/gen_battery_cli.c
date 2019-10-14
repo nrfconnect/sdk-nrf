de9aa88340bc2ba97814a834f38dec1d2ba19110
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2019-10-14 16:15:31 +0200

    Bluetooth: Mesh: Generic Battery models
    Adds the Generic Battery Client and Generic Battery Server models.
    
    Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

---
+++ subsys/bluetooth/mesh/gen_battery_cli.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <string.h>
+#include <bluetooth/mesh/gen_battery_cli.h>
+#include "model_utils.h"
+
+static void decode_status(struct net_buf_simple *buf,
+			  struct bt_mesh_battery_status *status)
+{
+	status->battery_lvl = net_buf_simple_pull_le16(buf);
+
+	u8_t *discharge_minutes = net_buf_simple_pull_mem(buf, 3);
+
+	status->discharge_minutes =
+		((discharge_minutes[0] >> 16) | (discharge_minutes[1] >> 8) |
+		 (discharge_minutes[2]));
+
+	u8_t *charge_minutes = net_buf_simple_pull_mem(buf, 3);
+
+	status->charge_minutes =
+		((charge_minutes[0] >> 16) | (charge_minutes[1] >> 8) |
+		 (charge_minutes[2]));
+
+	u8_t flags = net_buf_simple_pull_u8(buf);
+
+	status->presence = flags & BIT_MASK(2);
+	status->indicator = (flags >> 2) & BIT_MASK(2);
+	status->charging = (flags >> 4) & BIT_MASK(2);
+	status->service = (flags >> 6) & BIT_MASK(2);
+}
+
+static void handle_status(struct bt_mesh_model *model,
+			  struct bt_mesh_msg_ctx *ctx,
+			  struct net_buf_simple *buf)
+{
+	struct bt_mesh_battery_cli *cli = model->user_data;
+	struct bt_mesh_battery_status status;
+
+	decode_status(buf, &status);
+
+	if (model_ack_match(&cli->ack_ctx, BT_MESH_BATTERY_OP_STATUS, ctx)) {
+		struct bt_mesh_battery_status *rsp =
+			(struct bt_mesh_battery_status *)cli->ack_ctx.user_data;
+
+		*rsp = status;
+		model_ack_rx(&cli->ack_ctx);
+	}
+
+	if (cli->status_handler) {
+		cli->status_handler(cli, ctx, &status);
+	}
+}
+
+const struct bt_mesh_model_op _bt_mesh_battery_cli_op[] = {
+	{ BT_MESH_BATTERY_OP_STATUS, BT_MESH_BATTERY_MSG_LEN_STATUS,
+	  handle_status },
+	BT_MESH_MODEL_OP_END,
+};
+
+static int bt_mesh_battery_cli_init(struct bt_mesh_model *model)
+{
+	struct bt_mesh_battery_cli *cli = model->user_data;
+
+	cli->model = model;
+	return 0;
+}
+
+const struct bt_mesh_model_cb _bt_mesh_battery_cli_cb = {
+	.init = bt_mesh_battery_cli_init,
+};
+
+int bt_mesh_battery_cli_status_get(struct bt_mesh_battery_cli *cli,
+				   struct bt_mesh_msg_ctx *ctx,
+				   struct bt_mesh_battery_status *rsp)
+{
+	BT_MESH_MODEL_BUF_DEFINE(msg, BT_MESH_BATTERY_OP_GET,
+				 BT_MESH_BATTERY_MSG_LEN_GET);
+	bt_mesh_model_msg_init(&msg, BT_MESH_BATTERY_OP_GET);
+
+	return model_ackd_send(cli->model, ctx, &msg,
+			       rsp ? &cli->ack_ctx : NULL,
+			       BT_MESH_BATTERY_OP_STATUS, rsp);
+}
---
+++ subsys/bluetooth/mesh/gen_battery_srv.c
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <string.h>
+#include <bluetooth/mesh/gen_battery_srv.h>
+#include <bluetooth/mesh/gen_dtt_srv.h>
+#include "model_utils.h"
+
+#define BATTERY_STATUS_DEFAULT                                                 \
+	{                                                                      \
+		.battery_lvl = BT_MESH_BATTERY_LVL_UNKNOWN,                    \
+		.discharge_minutes = BT_MESH_BATTERY_TIME_UNKNOWN,             \
+		.charge_minutes = BT_MESH_BATTERY_TIME_UNKNOWN,                \
+		.presence = BT_MESH_BATTERY_PRESENCE_UNKNOWN,                  \
+		.indicator = BT_MESH_BATTERY_INDICATOR_UNKNOWN,                \
+		.charging = BT_MESH_BATTERY_CHARGING_UNKNOWN,                  \
+		.service = BT_MESH_BATTERY_SERVICE_UNKNOWN,                    \
+	}
+
+static void encode_status(struct net_buf_simple *buf,
+			  const struct bt_mesh_battery_status *status)
+{
+	bt_mesh_model_msg_init(buf, BT_MESH_BATTERY_OP_STATUS);
+	net_buf_simple_add_u8(buf, status->battery_lvl);
+
+	u8_t *discharge = net_buf_simple_add(buf, 3);
+
+	discharge[0] = status->discharge_minutes;
+	discharge[1] = status->discharge_minutes >> 8;
+	discharge[2] = status->discharge_minutes >> 16;
+
+	u8_t *charge = net_buf_simple_add(buf, 3);
+
+	charge[0] = status->charge_minutes;
+	charge[1] = status->charge_minutes >> 8;
+	charge[2] = status->charge_minutes >> 16;
+
+	u8_t flags = ((status->presence & BIT_MASK(2)) |
+		      ((status->indicator & BIT_MASK(2)) << 2) |
+		      ((status->charging & BIT_MASK(2)) << 4) |
+		      ((status->service & BIT_MASK(2)) << 6));
+
+	net_buf_simple_add_u8(buf, flags);
+}
+
+static void rsp_status(struct bt_mesh_model *model,
+		       struct bt_mesh_msg_ctx *rx_ctx,
+		       const struct bt_mesh_battery_status *status)
+{
+	BT_MESH_MODEL_BUF_DEFINE(msg, BT_MESH_BATTERY_OP_STATUS,
+				 BT_MESH_BATTERY_MSG_LEN_STATUS);
+	encode_status(&msg, status);
+
+	(void)bt_mesh_model_send(model, rx_ctx, &msg, NULL, NULL);
+}
+
+static void handle_get(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx,
+		       struct net_buf_simple *buf)
+{
+	struct bt_mesh_battery_srv *srv = model->user_data;
+	struct bt_mesh_battery_status status = BATTERY_STATUS_DEFAULT;
+
+	srv->get(srv, ctx, &status);
+
+	rsp_status(model, ctx, &status);
+}
+
+const struct bt_mesh_model_op _bt_mesh_battery_srv_op[] = {
+	{ BT_MESH_BATTERY_OP_GET, BT_MESH_BATTERY_MSG_LEN_GET, handle_get },
+	BT_MESH_MODEL_OP_END,
+};
+
+static int bt_mesh_battery_srv_init(struct bt_mesh_model *model)
+{
+	struct bt_mesh_battery_srv *srv = model->user_data;
+
+	srv->model = model;
+	return 0;
+}
+
+const struct bt_mesh_model_cb _bt_mesh_battery_srv_cb = {
+	.init = bt_mesh_battery_srv_init
+};
+
+int _bt_mesh_battery_srv_update_handler(struct bt_mesh_model *model)
+{
+	struct bt_mesh_battery_srv *srv = model->user_data;
+	struct bt_mesh_battery_status status = BATTERY_STATUS_DEFAULT;
+
+	srv->get(srv, NULL, &status);
+
+	encode_status(model->pub->msg, &status);
+
+	return 0;
+}
+
+s32_t bt_mesh_battery_srv_pub(struct bt_mesh_battery_srv *srv,
+			      struct bt_mesh_msg_ctx *ctx,
+			      const struct bt_mesh_battery_status *status)
+{
+	BT_MESH_MODEL_BUF_DEFINE(msg, BT_MESH_BATTERY_OP_STATUS,
+				 BT_MESH_BATTERY_MSG_LEN_STATUS);
+	encode_status(&msg, status);
+	return model_send(srv->model, ctx, &msg);
+}
