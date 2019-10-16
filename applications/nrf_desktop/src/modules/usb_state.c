bf0cbb723f107d16af6f7c890a7dd209802448bf
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2019-10-16 11:24:27 +0200

    applications: nrf_desktop: Workaround for USB callback race
    Due to how USB stack is constructed in Zephyr, driver may issue
    callbacks indicating the USB state before application had a chance
    to register its callback function.
    
    So far the problem is visible only with Nordic LL, only when settings
    are erased and only if device boots with USB connected. In such case
    application misses callback about the device being powered.
    
    To fix the problem take the assumption that the device was powered
    if we receive suspend event from the driver.
    
    Jira:DESK-633
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

--- applications/nrf_desktop/src/modules/usb_state.c
+++ applications/nrf_desktop/src/modules/usb_state.c
@@ -328,7 +328,15 @@ static void device_status(enum usb_dc_status_code cb_status, const u8_t *param)
 		break;
 
 	case USB_DC_SUSPEND:
-		__ASSERT_NO_MSG(state != USB_STATE_DISCONNECTED);
+		if (state == USB_STATE_DISCONNECTED) {
+			/* Due to the way USB driver and stack are written
+			 * some events may be issued before application
+			 * connect its callback.
+			 * We assume that device was powered.
+			 */
+			state = USB_STATE_POWERED;
+			LOG_WRN("USB suspended while disconnected");
+		}
 		before_suspend = state;
 		new_state = USB_STATE_SUSPENDED;
 		LOG_WRN("USB suspend");
