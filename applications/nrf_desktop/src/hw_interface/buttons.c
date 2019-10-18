26e977f1436b3786aa19eca4d7255f2e9f9a8f51
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2019-10-18 12:47:29 +0200

    applications: nrf_desktop: Add buttons debouncing
    Jira:DESK-741
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

--- applications/nrf_desktop/src/hw_interface/buttons.c
+++ applications/nrf_desktop/src/hw_interface/buttons.c
@@ -238,6 +238,17 @@ static void scan_fn(struct k_work *work)
 		goto error;
 	}
 
+	static u32_t settled_state[COLUMNS];
+
+	/* Prevent bouncing */
+	static u32_t prev_state[COLUMNS];
+	for (size_t i = 0; i < COLUMNS; i++) {
+		u32_t bounce_mask = prev_state[i] ^ raw_state[i];
+		prev_state[i] = raw_state[i];
+		raw_state[i] &= ~bounce_mask;
+		raw_state[i] |= settled_state[i] & bounce_mask;
+	}
+
 	/* Prevent ghosting */
 	u32_t cur_state[COLUMNS];
 	for (size_t i = 0; i < COLUMNS; i++) {
@@ -257,7 +268,6 @@ static void scan_fn(struct k_work *work)
 	}
 
 	/* Emit event for any key state change */
-	static u32_t old_state[COLUMNS];
 	bool any_pressed = false;
 	size_t evt_limit = 0;
 
@@ -265,7 +275,7 @@ static void scan_fn(struct k_work *work)
 		for (size_t j = 0; j < ARRAY_SIZE(row); j++) {
 			bool is_raw_pressed = raw_state[i] & BIT(j);
 			bool is_pressed = cur_state[i] & BIT(j);
-			bool was_pressed = old_state[i] & BIT(j);
+			bool was_pressed = settled_state[i] & BIT(j);
 
 			if ((is_pressed != was_pressed) &&
 			    (is_pressed == is_raw_pressed) &&
@@ -278,12 +288,12 @@ static void scan_fn(struct k_work *work)
 
 				evt_limit++;
 
-				WRITE_BIT(old_state[i], j, is_pressed);
+				WRITE_BIT(settled_state[i], j, is_pressed);
 			}
 		}
 
 		any_pressed = any_pressed ||
-			      (old_state[i] != 0) ||
+			      (settled_state[i] != 0) ||
 			      (cur_state[i] != 0);
 	}
 
