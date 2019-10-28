785a031e358e3530c0220d680cad09490ff9c254
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2019-10-28 13:18:35 +0000

    tests: add test for dfu_target_mcuboot
    Add unit test for this specific target to test the non-general
    function for parsing b1 upgrades.
    
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

---
+++ tests/subsys/dfu/dfu_target_mcuboot/pm_config.h
@@ -0,0 +1,7 @@
+/* generated file copied to simplify building the test */
+#ifndef PM_CONFIG_H__
+#define PM_CONFIG_H__
+#define PM_S0_ADDRESS 0x8000
+#define PM_S1_ADDRESS 0x15000
+#define PM_MCUBOOT_SECONDARY_SIZE 0x5e000
+#endif /* PM_CONFIG_H__ */
