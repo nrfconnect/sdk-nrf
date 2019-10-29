3bcd75cb14e56e434ba7272dbbda9bbfb76c27a3
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2019-10-29 09:33:45 +0100

    Bluetooth: Mesh: Basic light example
    Adds a simple mesh light example using the Generic OnOff Server for
    controlling the LEDs.
    
    Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

---
+++ samples/bluetooth/mesh/light/src/model_handler.c
@@ -0,0 +1,200 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/mesh/models.h>
+#include <dk_buttons_and_leds.h>
+#include "model_handler.h"
+
+static void led_set(struct bt_mesh_onoff_srv *srv, struct bt_mesh_msg_ctx *ctx,
+		    const struct bt_mesh_onoff_set *set,
+		    struct bt_mesh_onoff_status *rsp);
+
+static void led_get(struct bt_mesh_onoff_srv *srv, struct bt_mesh_msg_ctx *ctx,
+		    struct bt_mesh_onoff_status *rsp);
+
+static const struct bt_mesh_onoff_srv_handlers onoff_handlers = {
+	.set = led_set,
+	.get = led_get,
+};
+
+struct led_ctx {
+	struct bt_mesh_onoff_srv srv;
+	struct k_delayed_work work;
+	u32_t remaining;
+	bool value;
+};
+
+static struct led_ctx led_ctx[4] = {
+	[0 ... 3] = {
+		.srv = BT_MESH_ONOFF_SRV_INIT(&onoff_handlers),
+	}
+};
+
+static void led_transition_start(struct led_ctx *led)
+{
+	int led_idx = led - &led_ctx[0];
+
+	/* As long as the transition is in progress, the onoff
+	 * state is "on":
+	 */
+	dk_set_led(led_idx, true);
+	k_delayed_work_submit(&led->work, led->remaining);
+	led->remaining = 0;
+}
+
+static void led_status(struct led_ctx *led, struct bt_mesh_onoff_status *status)
+{
+	status->remaining_time =
+		k_delayed_work_remaining_get(&led->work) + led->remaining;
+	status->target_on_off = led->value;
+	/* As long as the transition is in progress, the onoff state is "on": */
+	status->present_on_off = led->value || status->remaining_time;
+}
+
+static void led_set(struct bt_mesh_onoff_srv *srv, struct bt_mesh_msg_ctx *ctx,
+		    const struct bt_mesh_onoff_set *set,
+		    struct bt_mesh_onoff_status *rsp)
+{
+	struct led_ctx *led = CONTAINER_OF(srv, struct led_ctx, srv);
+	int led_idx = led - &led_ctx[0];
+
+	if (set->on_off == led->value) {
+		goto respond;
+	}
+
+	led->value = set->on_off;
+	led->remaining = set->transition->time;
+
+	if (set->transition->delay > 0) {
+		k_delayed_work_submit(&led->work, set->transition->delay);
+	} else if (set->transition->time > 0) {
+		led_transition_start(led);
+	} else {
+		dk_set_led(led_idx, set->on_off);
+	}
+
+respond:
+	if (rsp) {
+		led_status(led, rsp);
+	}
+}
+
+static void led_get(struct bt_mesh_onoff_srv *srv, struct bt_mesh_msg_ctx *ctx,
+		    struct bt_mesh_onoff_status *rsp)
+{
+	struct led_ctx *led = CONTAINER_OF(srv, struct led_ctx, srv);
+
+	led_status(led, rsp);
+}
+
+static void led_work(struct k_work *work)
+{
+	struct led_ctx *led = CONTAINER_OF(work, struct led_ctx, work.work);
+	int led_idx = led - &led_ctx[0];
+
+	if (led->remaining) {
+		led_transition_start(led);
+	} else {
+		dk_set_led(led_idx, led->value);
+
+		/* Publish the new value at the end of the transition */
+		struct bt_mesh_onoff_status status;
+
+		led_status(led, &status);
+		bt_mesh_onoff_srv_pub(&led->srv, NULL, &status);
+	}
+}
+
+/** Configuration server definition */
+static struct bt_mesh_cfg_srv cfg_srv = {
+	.relay = IS_ENABLED(CONFIG_BT_MESH_RELAY),
+	.beacon = BT_MESH_BEACON_ENABLED,
+	.frnd = IS_ENABLED(CONFIG_BT_MESH_FRIEND),
+	.gatt_proxy = IS_ENABLED(CONFIG_BT_MESH_GATT_PROXY),
+	.default_ttl = 7,
+
+	/* 3 transmissions with 20ms interval */
+	.net_transmit = BT_MESH_TRANSMIT(2, 20),
+	.relay_retransmit = BT_MESH_TRANSMIT(2, 20),
+};
+
+/* Set up a repeating delayed work to blink the DK's LEDs when attention is
+ * requested.
+ */
+static struct k_delayed_work attention_blink_work;
+
+static void attention_blink(struct k_work *work)
+{
+	static int idx;
+	const u8_t pattern[] = {
+		BIT(0) | BIT(1),
+		BIT(1) | BIT(2),
+		BIT(2) | BIT(3),
+		BIT(3) | BIT(0),
+	};
+	dk_set_leds(pattern[idx++ % ARRAY_SIZE(pattern)]);
+	k_delayed_work_submit(&attention_blink_work, 30);
+}
+
+static void attention_on(struct bt_mesh_model *mod)
+{
+	k_delayed_work_submit(&attention_blink_work, 0);
+}
+
+static void attention_off(struct bt_mesh_model *mod)
+{
+	k_delayed_work_cancel(&attention_blink_work);
+	dk_set_leds(DK_NO_LEDS_MSK);
+}
+
+static const struct bt_mesh_health_srv_cb health_srv_cb = {
+	.attn_on = attention_on,
+	.attn_off = attention_off,
+};
+
+static struct bt_mesh_health_srv health_srv = {
+	.cb = &health_srv_cb,
+};
+
+BT_MESH_HEALTH_PUB_DEFINE(health_pub, 0);
+
+#define BT_MESH_MODEL_LIST(...) ((struct bt_mesh_model[]){ __VA_ARGS__ })
+
+static struct bt_mesh_elem elements[] = {
+	BT_MESH_ELEM(
+		1, BT_MESH_MODEL_LIST(
+			BT_MESH_MODEL_CFG_SRV(&cfg_srv),
+			BT_MESH_MODEL_HEALTH_SRV(&health_srv, &health_pub),
+			BT_MESH_MODEL_ONOFF_SRV(&led_ctx[0].srv)),
+		BT_MESH_MODEL_NONE),
+	BT_MESH_ELEM(
+		2, BT_MESH_MODEL_LIST(BT_MESH_MODEL_ONOFF_SRV(&led_ctx[1].srv)),
+		BT_MESH_MODEL_NONE),
+	BT_MESH_ELEM(
+		3, BT_MESH_MODEL_LIST(BT_MESH_MODEL_ONOFF_SRV(&led_ctx[2].srv)),
+		BT_MESH_MODEL_NONE),
+	BT_MESH_ELEM(
+		4, BT_MESH_MODEL_LIST(BT_MESH_MODEL_ONOFF_SRV(&led_ctx[3].srv)),
+		BT_MESH_MODEL_NONE),
+};
+
+static const struct bt_mesh_comp comp = {
+	.cid = CONFIG_BT_CTLR_COMPANY_ID,
+	.elem = elements,
+	.elem_count = ARRAY_SIZE(elements),
+};
+
+const struct bt_mesh_comp *model_handler_init(void)
+{
+	k_delayed_work_init(&attention_blink_work, attention_blink);
+
+	for (int i = 0; i < ARRAY_SIZE(led_ctx); ++i) {
+		k_delayed_work_init(&led_ctx[i].work, led_work);
+	}
+
+	return &comp;
+}
