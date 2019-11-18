1f66c62dc6054c673432cef1cf1551dc102ce1b5
Joakim Andersson <joakim.andersson@nordicsemi.no>
2019-11-18 15:20:32 +0100

    Bluetooth: hci_driver: Fix hci_driver debug for command status.
    Add own debug print for receiving command status, also only print
    command complete using the command complete structure. We reported all
    events using the command complete byte order.
    
    Also did general cleanup in the hci_driver.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- subsys/bluetooth/controller/hci_driver.c
+++ subsys/bluetooth/controller/hci_driver.c
@@ -106,6 +106,8 @@ static int cmd_handle(struct net_buf *cmd)
 #if defined(CONFIG_BT_CONN)
 static int acl_handle(struct net_buf *acl)
 {
+	BT_DBG("");
+
 	int errcode = MULTITHREADING_LOCK_ACQUIRE();
 
 	if (!errcode) {
@@ -127,7 +129,7 @@ static int hci_driver_send(struct net_buf *buf)
 	int err;
 	u8_t type;
 
-	BT_DBG("Enter");
+	BT_DBG("");
 
 	if (!buf->len) {
 		BT_DBG("Empty HCI packet");
@@ -138,12 +140,10 @@ static int hci_driver_send(struct net_buf *buf)
 	switch (type) {
 #if defined(CONFIG_BT_CONN)
 	case BT_BUF_ACL_OUT:
-		BT_DBG("ACL_OUT");
 		err = acl_handle(buf);
 		break;
 #endif          /* CONFIG_BT_CONN */
 	case BT_BUF_CMD:
-		BT_DBG("CMD");
 		err = cmd_handle(buf);
 		break;
 	default:
@@ -155,29 +155,38 @@ static int hci_driver_send(struct net_buf *buf)
 		net_buf_unref(buf);
 	}
 
-	BT_DBG("Exit");
+	BT_DBG("Exit: %d", err);
 	return err;
 }
+
+#ifndef bt_acl_flag_pb
+/* Temporary defines, missing from hci.h */
+#define bt_acl_flags_pb(h)		((h) >> 12 & BIT_MASK(2))
+#define bt_acl_flags_bc(h)		((h) >> 14 & BIT_MASK(2))
+#endif /* bt_acl_flag_pb */
 
 static void data_packet_process(u8_t *hci_buf)
 {
 	struct net_buf *data_buf = bt_buf_get_rx(BT_BUF_ACL_IN, K_FOREVER);
+	struct bt_hci_acl_hdr *hdr = (void *)hci_buf;
+	u16_t hf, handle, len;
+	u8_t pb, bc;
 
 	if (!data_buf) {
 		BT_ERR("No data buffer available");
 		return;
 	}
 
-	u16_t handle = hci_buf[0] | (hci_buf[1] & 0xF) << 8;
-	u16_t data_length = hci_buf[2] | hci_buf[3] << 8;
-	u8_t pb_flag = (hci_buf[1] >> 4) & 0x3;
-	u8_t bc_flag = (hci_buf[1] >> 6) & 0x3;
+	len = sys_le16_to_cpu(hdr->len);
+	hf = sys_le16_to_cpu(hdr->handle);
+	handle = bt_acl_handle(hf);
+	pb = bt_acl_flags_pb(hf);
+	bc = bt_acl_flags_bc(hf);
 
-	BT_DBG("Data: Handle(%02x), PB(%01d), "
-	       "BC(%01d), Length(%02x)",
-	       handle, pb_flag, bc_flag, data_length);
+	BT_DBG("Data: handle (0x%02x), PB(%01d), BC(%01d), len(%u)", handle,
+	       pb, bc, len);
 
-	net_buf_add_mem(data_buf, &hci_buf[0], data_length + 4);
+	net_buf_add_mem(data_buf, &hci_buf[0], len + sizeof(*hdr));
 	bt_recv(data_buf);
 }
 
@@ -198,22 +207,30 @@ static void event_packet_process(u8_t *hci_buf)
 		return;
 	}
 
-	if (hdr->evt == 0x3E) {
-		BT_DBG("LE Meta Event: subevent code "
-		       "(%02x), length (%d)",
-		       hci_buf[2], hci_buf[1]);
-	} else {
-		u16_t opcode = sys_get_be16(&hci_buf[2]);
+	if (hdr->evt == BT_HCI_EVT_LE_META_EVENT) {
+		struct bt_hci_evt_le_meta_event *me = (void *)&hci_buf[2];
 
-		BT_DBG("Event: event code (0x%02x), "
-		       "length (%d), "
-		       "num_complete (%d), "
-		       "opcode (%d)"
-		       "status (%d)\n",
-		       hci_buf[0], hci_buf[1], hci_buf[2], opcode, hci_buf[5]);
+		BT_DBG("LE Meta Event (0x%02x), len (%u)",
+		       me->subevent, hdr->len);
+	} else if (hdr->evt == BT_HCI_EVT_CMD_COMPLETE) {
+		struct bt_hci_evt_cmd_complete *cc = (void *)&hci_buf[2];
+		struct bt_hci_evt_cc_status *ccs = (void *)&hci_buf[5];
+		u16_t opcode = sys_le16_to_cpu(cc->opcode);
+
+		BT_DBG("Command Complete (0x%04x) status: 0x%02x,"
+		       " ncmd: %u, len %u",
+		       opcode, ccs->status, cc->ncmd, hdr->len);
+	} else if (hdr->evt == BT_HCI_EVT_CMD_STATUS) {
+		struct bt_hci_evt_cmd_status *cs = (void *)&hci_buf[2];
+		u16_t opcode = sys_le16_to_cpu(cs->opcode);
+
+		BT_DBG("Command Status (0x%04x) status: 0x%02x",
+		       opcode, cs->status);
+	} else {
+		BT_DBG("Event (0x%02x) len %u", hdr->evt, hdr->len);
 	}
 
-	net_buf_add_mem(evt_buf, &hci_buf[0], hdr->len + 2);
+	net_buf_add_mem(evt_buf, &hci_buf[0], hdr->len + sizeof(*hdr));
 	if (bt_hci_evt_is_prio(hdr->evt)) {
 		bt_recv_prio(evt_buf);
 	} else {
@@ -237,7 +254,6 @@ static bool fetch_and_process_hci_evt(uint8_t *p_hci_buffer)
 
 	event_packet_process(p_hci_buffer);
 	return true;
-
 }
 
 static bool fetch_and_process_acl_data(uint8_t *p_hci_buffer)
