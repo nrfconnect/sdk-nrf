a25e14597d19753cf7ca0debca0035d29015809e
Solveig Fure <Solveig.Fure@nordicsemi.no>
2019-11-21 13:49:48 +0100

    mpsl: subsys: improve ISRs and IRQ connects for MPSL
    - Call mpsl_low_prio_process() from thread to offload the MPSL low
    prio ISR. This also ensures that mpsl_low_prio_process() does not
    preempt ongoing MPSL processing if called from cooperative threads.
    
    - Initialize ISRs properly by using the Zephyr ISR declare macros.
    
    Signed-off-by: Solveig Fure <Solveig.Fure@nordicsemi.no>

--- subsys/mpsl/mpsl_init.c
+++ subsys/mpsl/mpsl_init.c
@@ -9,13 +9,67 @@
 #include <kernel.h>
 #include <mpsl.h>
 
-#define MPSL_LOW_PRIO_IRQ SWI4_IRQn
+#define MPSL_LOW_PRIO_IRQn SWI5_IRQn
 #define MPSL_LOW_PRIO (4)
+
+static K_SEM_DEFINE(sem_signal, 0, UINT_MAX);
+static struct k_thread signal_thread_data;
+static K_THREAD_STACK_DEFINE(signal_thread_stack,
+			     CONFIG_MPSL_SIGNAL_STACK_SIZE);
 
 
 static void mpsl_low_prio_irq_handler(void)
 {
-	mpsl_low_priority_process();
+	k_sem_give(&sem_signal);
+}
+
+static void signal_thread(void *p1, void *p2, void *p3)
+{
+	ARG_UNUSED(p1);
+	ARG_UNUSED(p2);
+	ARG_UNUSED(p3);
+
+	while (true) {
+		k_sem_take(&sem_signal, K_FOREVER);
+		mpsl_low_priority_process();
+	}
+}
+
+ISR_DIRECT_DECLARE(mpsl_timer0_isr_wrapper)
+{
+	MPSL_IRQ_TIMER0_Handler();
+
+	ISR_DIRECT_PM();
+
+
+	/* We may need to reschedule in case a radio timeslot callback
+	 * accesses zephyr primitives.
+	 */
+	return 1;
+}
+
+ISR_DIRECT_DECLARE(mpsl_rtc0_isr_wrapper)
+{
+	MPSL_IRQ_RTC0_Handler();
+
+	ISR_DIRECT_PM();
+
+	/* No need for rescheduling, because the interrupt handler
+	 * does not access zephyr primitives.
+	 */
+	return 0;
+}
+
+ISR_DIRECT_DECLARE(mpsl_radio_isr_wrapper)
+{
+	MPSL_IRQ_RADIO_Handler();
+
+	ISR_DIRECT_PM();
+
+	/* We may need to reschedule in case a radio timeslot callback
+	 * accesses zephyr primitives.
+	 */
+	return 1;
 }
 
 static void m_assert_handler(const char *const file, const u32_t line)
@@ -24,7 +78,7 @@ static void m_assert_handler(const char *const file, const u32_t line)
 }
 
 
-uint8_t m_config_clock_source_get(void)
+static uint8_t m_config_clock_source_get(void)
 {
 #ifdef CONFIG_CLOCK_CONTROL_NRF_K32SRC_RC
 	return MPSL_CLOCK_LF_SRC_RC;
@@ -50,22 +104,38 @@ static int mpsl_lib_init(struct device *dev)
 	clock_cfg.rc_ctiv = MPSL_RECOMMENDED_RC_CTIV;
 	clock_cfg.rc_temp_ctiv = MPSL_RECOMMENDED_RC_TEMP_CTIV;
 
-	err = mpsl_init(&clock_cfg, MPSL_LOW_PRIO_IRQ, m_assert_handler);
+	err = mpsl_init(&clock_cfg, MPSL_LOW_PRIO_IRQn, m_assert_handler);
 	if (err) {
 		return err;
 	}
 
 	IRQ_DIRECT_CONNECT(TIMER0_IRQn, MPSL_HIGH_IRQ_PRIORITY,
-			   MPSL_IRQ_TIMER0_Handler, IRQ_ZERO_LATENCY);
+			   mpsl_timer0_isr_wrapper, IRQ_ZERO_LATENCY);
 	IRQ_DIRECT_CONNECT(RTC0_IRQn, MPSL_HIGH_IRQ_PRIORITY,
-			   MPSL_IRQ_RTC0_Handler, IRQ_ZERO_LATENCY);
+			   mpsl_rtc0_isr_wrapper, IRQ_ZERO_LATENCY);
 	IRQ_DIRECT_CONNECT(RADIO_IRQn, MPSL_HIGH_IRQ_PRIORITY,
-			   MPSL_IRQ_RADIO_Handler, IRQ_ZERO_LATENCY);
-	IRQ_CONNECT(MPSL_LOW_PRIO_IRQ, MPSL_LOW_PRIO,
+			   mpsl_radio_isr_wrapper, IRQ_ZERO_LATENCY);
+
+	return 0;
+}
+
+static int mpsl_signal_thread_init(struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	k_thread_create(&signal_thread_data, signal_thread_stack,
+			K_THREAD_STACK_SIZEOF(signal_thread_stack),
+			signal_thread, NULL, NULL, NULL,
+			K_PRIO_COOP(CONFIG_MPSL_SIGNAL_THREAD_PRIO),
+			0, K_NO_WAIT);
+
+	IRQ_CONNECT(MPSL_LOW_PRIO_IRQn, MPSL_LOW_PRIO,
 		    mpsl_low_prio_irq_handler, NULL, 0);
-	irq_enable(MPSL_LOW_PRIO_IRQ);
+	irq_enable(MPSL_LOW_PRIO_IRQn);
 
 	return 0;
 }
 
 SYS_INIT(mpsl_lib_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+SYS_INIT(mpsl_signal_thread_init, POST_KERNEL,
+	 CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
