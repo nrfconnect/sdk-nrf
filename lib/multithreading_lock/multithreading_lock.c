69255e7e64ea7cea21c9449886529ca13a2a18f6
Solveig Fure <Solveig.Fure@nordicsemi.no>
2019-11-28 13:05:37 +0100

    ble_controller: mpsl: subsys: mpsl and ble_controller linked in NCS
    Updates manifest file to point to nrfxlib with new ble_controller.
    Updates to enable using a ble_controller library that does not
    contain MPSL, and to make MPSL link with ble_controller in NCS.
    This makes it possible for the clock driver to only interface with
    MPSL.
    MPSL must be initialized before ble_controller.
    Adds NCS_SAMPLES_DEFAULTS to mpsl timeslot sample to ease debugging.
    Update copyright headers.
    
    Signed-off-by: Solveig Fure <Solveig.Fure@nordicsemi.no>

--- lib/multithreading_lock/multithreading_lock.c
+++ lib/multithreading_lock/multithreading_lock.c
@@ -1,21 +1,21 @@
 /*
- * Copyright (c) 2019 Nordic Semiconductor ASA
+ * Copyright (c) 2019 - 2020 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
  */
 
 #include "multithreading_lock.h"
 
-static K_SEM_DEFINE(ble_controller_lock, 1, 1);
+static K_SEM_DEFINE(mpsl_lock, 1, 1);
 
 int multithreading_lock_acquire(int32_t timeout)
 {
-	return k_sem_take(&ble_controller_lock,
+	return k_sem_take(&mpsl_lock,
 			  ((timeout == K_FOREVER || timeout == K_NO_WAIT) ?
 			   timeout : K_MSEC(timeout)));
 }
 
 void multithreading_lock_release(void)
 {
-	k_sem_give(&ble_controller_lock);
+	k_sem_give(&mpsl_lock);
 }
