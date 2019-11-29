4672e1323b47b6904cc435e469f45873d893ada1
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2019-11-29 12:43:54 +0100

    tests: unity: Conform to sanitycheck output format
    Adds result output and returns 1 if the test failed. Any other result
    breaks sanitycheck.
    
    Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

--- tests/unity/src/native_posix.c
+++ tests/unity/src/native_posix.c
@@ -8,15 +8,21 @@
  * @file
  * @brief Additional Unity support code for the native_posix board.
  */
-
+#include <zephyr.h>
 #include "posix_board_if.h"
 
 int suiteTearDown(int num_failures)
 {
+	/* Sanitycheck bases the result of native_posix based unit tests on the
+	 * output:
+	 */
+	printk("PROJECT EXECUTION %s\n",
+	       num_failures == 0 ? "SUCCESSFUL" : "FAILED");
+
 	/* The native posix board will loop forever after leaving the runner's
 	 * main, so we have to explicitly call exit() to terminate the test.
 	 */
-	posix_exit(num_failures > 0 ? -1 : 0);
+	posix_exit(num_failures > 0 ? 1 : 0);
 
 	/* Should be unreachable: */
 	return 1;
