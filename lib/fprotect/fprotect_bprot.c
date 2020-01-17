hakonfam/fw-nrfconnect-nrf-1

fprotect: partition_manager: introduce option for fprotect block size
Introduce Kconfig option which describe the block size of
the fprotect module.

This is used to set alignment of partitions which need to be protected
with fprotect.

Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

drivers/fprotect/fprotect_bprot.c
@@ -40,6 +40,8 @@ int fprotect_area(u32_t start, size_t length)
 	u32_t block_end   = (start + length) / BPROT_REGIONS_SIZE;
 	u32_t block_mask[BPROT_CONFIGS_NUM] = {0};
 
+	assert(BPROT_REGIONS_SIZE == CONFIG_FPROTECT_BLOCK_SIZE);
+
 	if ((start % BPROT_REGIONS_SIZE) ||
 	    (length % BPROT_REGIONS_SIZE) ||
 	    (block_end > BPROT_REGIONS_NUM) ||

nrfconnect/sdk-nrf

lib/fprotect/fprotect_bprot.c
@@ -40,6 +40,8 @@ int fprotect_area(u32_t start, size_t length)
 	u32_t block_end   = (start + length) / BPROT_REGIONS_SIZE;
 	u32_t block_mask[BPROT_CONFIGS_NUM] = {0};
 
+	BUILD_ASSERT(BPROT_REGIONS_SIZE == CONFIG_FPROTECT_BLOCK_SIZE);
+
 	if ((start % BPROT_REGIONS_SIZE) ||
 	    (length % BPROT_REGIONS_SIZE) ||
 	    (block_end > BPROT_REGIONS_NUM) ||