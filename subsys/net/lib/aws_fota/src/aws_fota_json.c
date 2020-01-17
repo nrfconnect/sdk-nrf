0516f79f9b9143222db262798961c7ac56c1f2c5
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2020-01-17 13:24:33 +0100

    net: lib: aws_fota: Switch from Zephyr's JSON parser to cJSON
    Switch from Zephyr's JSON parser to cJSON since Zephyr's JSON parser did
    not handle nested JSON structures in a controllable manner. Where it
    would either bus fault or copy random data into structure fields when
    decoding structures with missing data.
    
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>

--- subsys/net/lib/aws_fota/src/aws_fota_json.c
+++ subsys/net/lib/aws_fota/src/aws_fota_json.c
@@ -6,119 +6,11 @@
 
 #include <zephyr.h>
 #include <string.h>
-#include <data/json.h>
+#include <cJSON.h>
 #include <sys/util.h>
 #include <net/aws_jobs.h>
 
 #include "aws_fota_json.h"
-
-struct location_obj {
-	const char *protocol;
-	const char *host;
-	const char *path;
-};
-
-struct job_document_obj {
-	const char *operation;
-	const char *fw_version;
-	int size;
-	struct location_obj location;
-};
-
-struct status_details_obj {
-	const char *next_state;
-};
-
-struct execution_state_obj {
-	const char *status;
-	struct status_details_obj status_details;
-	int version_number;
-};
-
-struct execution_obj {
-	const char *job_id;
-	const char *status;
-	struct status_details_obj status_details;
-	int queued_at;
-	int last_update_at;
-	int version_number;
-	int execution_number;
-	struct job_document_obj job_document;
-};
-
-struct desc_job_execution_obj {
-	const char *client_token;
-	int timestamp;
-	struct execution_obj execution;
-};
-
-static const struct json_obj_descr location_obj_descr[] = {
-	JSON_OBJ_DESCR_PRIM(struct location_obj, protocol, JSON_TOK_STRING),
-	JSON_OBJ_DESCR_PRIM(struct location_obj, host, JSON_TOK_STRING),
-	JSON_OBJ_DESCR_PRIM(struct location_obj, path, JSON_TOK_STRING),
-};
-
-static const struct json_obj_descr job_document_obj_descr[] = {
-	JSON_OBJ_DESCR_PRIM(struct job_document_obj, operation,
-			    JSON_TOK_STRING),
-	JSON_OBJ_DESCR_PRIM_NAMED(struct job_document_obj, "fwversion",
-				  fw_version, JSON_TOK_STRING),
-	JSON_OBJ_DESCR_PRIM(struct job_document_obj, size, JSON_TOK_NUMBER),
-	JSON_OBJ_DESCR_OBJECT(struct job_document_obj, location,
-			      location_obj_descr),
-};
-
-static const struct json_obj_descr status_details_obj_descr[] = {
-	JSON_OBJ_DESCR_PRIM_NAMED(struct status_details_obj, "nextState",
-				  next_state, JSON_TOK_STRING),
-};
-
-static const struct json_obj_descr execution_obj_descr[] = {
-	JSON_OBJ_DESCR_PRIM_NAMED(struct execution_obj, "jobId", job_id,
-				  JSON_TOK_STRING),
-	JSON_OBJ_DESCR_PRIM(struct execution_obj, status, JSON_TOK_STRING),
-	JSON_OBJ_DESCR_OBJECT_NAMED(struct execution_obj, "statusDetails",
-				    status_details, status_details_obj_descr),
-	JSON_OBJ_DESCR_PRIM_NAMED(struct execution_obj, "queuedAt", queued_at,
-				  JSON_TOK_NUMBER),
-	JSON_OBJ_DESCR_PRIM_NAMED(struct execution_obj, "lastUpdatedAt",
-				  last_update_at, JSON_TOK_NUMBER),
-	JSON_OBJ_DESCR_PRIM_NAMED(struct execution_obj, "versionNumber",
-				  version_number, JSON_TOK_NUMBER),
-	JSON_OBJ_DESCR_PRIM_NAMED(struct execution_obj, "executionNumber",
-				  execution_number, JSON_TOK_NUMBER),
-	JSON_OBJ_DESCR_OBJECT_NAMED(struct execution_obj, "jobDocument",
-				    job_document, job_document_obj_descr),
-};
-
-static const struct json_obj_descr desc_job_execution_obj_descr[] = {
-	JSON_OBJ_DESCR_PRIM_NAMED(struct desc_job_execution_obj, "clientToken",
-				  client_token, JSON_TOK_STRING),
-	JSON_OBJ_DESCR_PRIM(struct desc_job_execution_obj, timestamp,
-			    JSON_TOK_NUMBER),
-	JSON_OBJ_DESCR_OBJECT(struct desc_job_execution_obj, execution,
-			      execution_obj_descr),
-};
-
-struct update_rsp_obj {
-	const char *status;
-	struct status_details_obj status_details;
-	const char *expected_version;
-	int timestamp;
-	const char *client_token;
-};
-
-static const struct json_obj_descr update_job_exec_stat_rsp_descr[] = {
-	JSON_OBJ_DESCR_PRIM_NAMED(struct update_rsp_obj, "status",
-				  status, JSON_TOK_STRING),
-	JSON_OBJ_DESCR_OBJECT_NAMED(struct update_rsp_obj, "statusDetails",
-				    status_details, status_details_obj_descr),
-	JSON_OBJ_DESCR_PRIM_NAMED(struct update_rsp_obj, "expectedVersion",
-				  expected_version, JSON_TOK_STRING),
-	JSON_OBJ_DESCR_PRIM(struct update_rsp_obj, timestamp, JSON_TOK_NUMBER),
-	JSON_OBJ_DESCR_PRIM_NAMED(struct update_rsp_obj, "clientToken",
-				  client_token, JSON_TOK_STRING),
-};
 
 /**@brief Copy max maxlen bytes from src to dst. Insert null-terminator.
  */
@@ -132,92 +24,121 @@ static void strncpy_nullterm(char *dst, const char *src, size_t maxlen)
 	}
 }
 
-int aws_fota_parse_UpdateJobExecution_rsp(char *update_rsp_document,
-					  size_t payload_len, char *status)
+int aws_fota_parse_UpdateJobExecution_rsp(const char *update_rsp_document,
+					  size_t payload_len, char *status_buf)
 {
-	if (update_rsp_document == NULL || status == NULL) {
+	if (update_rsp_document == NULL || status_buf == NULL) {
 		return -EINVAL;
 	}
 
-	struct update_rsp_obj rsp;
+	int ret;
 
-	int ret = json_obj_parse(update_rsp_document, payload_len,
-			update_job_exec_stat_rsp_descr,
-			ARRAY_SIZE(update_job_exec_stat_rsp_descr), &rsp);
+	cJSON *update_response = cJSON_Parse(update_rsp_document);
 
-	if (ret < 0) {
-		return ret;
+	if (update_response == NULL) {
+		ret = -ENODATA;
+		goto cleanup;
 	}
 
-	/* Check if the status field(1st field) of the object has been parsed */
-	if (ret & BIT(UPDATE_JOB_EXECUTION_STATUS_DECODED_BIT)) {
-		strncpy_nullterm(status, rsp.status, STATUS_MAX_LEN);
+	cJSON *status = cJSON_GetObjectItemCaseSensitive(update_response,
+							  "status");
+	if (cJSON_IsString(status) && status->valuestring != NULL) {
+		strncpy_nullterm(status_buf, status->valuestring,
+				 STATUS_MAX_LEN);
 	} else {
-		return -ENODATA;
+		ret = -ENODATA;
+		goto cleanup;
 	}
 
-	return 0;
+	ret = 0;
+cleanup:
+	cJSON_Delete(update_response);
+	return ret;
 }
 
-int aws_fota_parse_DescribeJobExecution_rsp(char *job_document,
+int aws_fota_parse_DescribeJobExecution_rsp(const char *job_document,
 					   u32_t payload_len,
 					   char *job_id_buf,
 					   char *hostname_buf,
 					   char *file_path_buf,
-					   int *version_number)
+					   int *execution_version_number)
 {
 	if (job_document == NULL
 	    || job_id_buf == NULL
 	    || hostname_buf == NULL
 	    || file_path_buf == NULL
-	    || version_number == NULL) {
+	    || execution_version_number == NULL) {
 		return -EINVAL;
 	}
 
-	struct desc_job_execution_obj root_obj;
-	struct job_document_obj *job_doc_obj;
-	struct execution_obj *exec_obj;
+	int ret;
 
-	int ret = json_obj_parse(job_document, payload_len,
-				 desc_job_execution_obj_descr,
-				 ARRAY_SIZE(desc_job_execution_obj_descr),
-				 &root_obj);
+	cJSON *json_data = cJSON_Parse(job_document);
 
-	if (ret < 0) {
-		return ret;
+	if (json_data == NULL) {
+		ret = -ENODATA;
+		goto cleanup;
 	}
 
-	if (!(ret & BIT(EXECUTION_OBJ_DECODED_BIT))) {
-		/* No execution object in JSON */
-		return 0;
+	cJSON *execution = cJSON_GetObjectItemCaseSensitive(json_data,
+							    "execution");
+	if (execution == NULL) {
+		ret = 0;
+		goto cleanup;
 	}
 
-	/* Check if the execution field of the object has been parsed. This
-	 * test is currently non-functional as Zephyr's JSON parser does not
-	 * populate fields as NULL in nested structure if the field in the
-	 * nested structure is not found.
-	 */
-	exec_obj = &root_obj.execution;
-	job_doc_obj = &exec_obj->job_document;
-	if (exec_obj->job_id != 0
-	    && exec_obj->version_number != 0
-	    && job_doc_obj->location.host != 0
-	    && job_doc_obj->location.path != 0) {
-		strncpy_nullterm(job_id_buf, exec_obj->job_id,
+	cJSON *job_id = cJSON_GetObjectItemCaseSensitive(execution, "jobId");
+
+	if (cJSON_GetStringValue(job_id) != NULL) {
+		strncpy_nullterm(job_id_buf, job_id->valuestring,
 				AWS_JOBS_JOB_ID_MAX_LEN);
-		*version_number = exec_obj->version_number;
-		strncpy_nullterm(hostname_buf,
-				job_doc_obj->location.host,
+	} else {
+		ret = -ENODATA;
+		goto cleanup;
+	}
+
+	cJSON *job_data = cJSON_GetObjectItemCaseSensitive(execution,
+							   "jobDocument");
+
+	if (!cJSON_IsObject(job_data)) {
+		ret = -ENODATA;
+		goto cleanup;
+	}
+
+	cJSON *location = cJSON_GetObjectItemCaseSensitive(job_data,
+							    "location");
+
+	if (!cJSON_IsObject(location)) {
+		ret = -ENODATA;
+		goto cleanup;
+	}
+
+	cJSON *hostname = cJSON_GetObjectItemCaseSensitive(location, "host");
+	cJSON *path = cJSON_GetObjectItemCaseSensitive(location, "path");
+
+	if ((cJSON_GetStringValue(hostname) != NULL)
+	   && (cJSON_GetStringValue(path) != NULL)) {
+		strncpy_nullterm(hostname_buf, hostname->valuestring,
 				CONFIG_AWS_FOTA_HOSTNAME_MAX_LEN);
-		strncpy_nullterm(file_path_buf,
-				job_doc_obj->location.path,
+		strncpy_nullterm(file_path_buf, path->valuestring,
 				CONFIG_AWS_FOTA_FILE_PATH_MAX_LEN);
 	} else {
-		/* A field was not decoded correctly which means we are missing
-		 * data.
-		 */
-		return -ENODATA;
+		ret = -ENODATA;
+		goto cleanup;
 	}
 
-	return 1;
+	cJSON *version_number = cJSON_GetObjectItemCaseSensitive(
+					execution, "versionNumber");
+
+	if (cJSON_IsNumber(version_number)) {
+		*execution_version_number = version_number->valueint;
+	} else {
+		ret = -ENODATA;
+		goto cleanup;
+	}
+
+	ret = 1;
+cleanup:
+	cJSON_Delete(json_data);
+	return ret;
 }
--- tests/subsys/net/lib/aws_fota/aws_fota_json/src/main.c
+++ tests/subsys/net/lib/aws_fota/aws_fota_json/src/main.c
@@ -11,29 +11,22 @@
 
 static void test_parse_job_execution(void)
 {
+	int ret;
+	int version_number;
 	int expected_version_number = 1;
 	char expected_job_id[] = "9b5caac6-3e8a-45dd-9273-c1b995762f4a";
 	char expected_hostname[] = "fota-update-bucket.s3.eu-central-1.amazonaws.com";
 	char expected_file_path[] = "/update.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWXEL53DXIU7W72AE%2F20190606%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20190606T081505Z&X-Amz-Expires=604800&X-Amz-Signature=913e00b97efe5565a901df4ff0b87e4878a406941d711f59d45915035989adcc&X-Amz-SignedHeaders=host";
-
 	char encoded[] = "{\"timestamp\":1559808907,\"execution\":{\"jobId\":\"9b5caac6-3e8a-45dd-9273-c1b995762f4a\",\"status\":\"QUEUED\",\"queuedAt\":1559808906,\"lastUpdatedAt\":1559808906,\"versionNumber\":1,\"executionNumber\":1,\"jobDocument\":{\"operation\":\"app_fw_update\",\"fwversion\":\"2\",\"size\":181124,\"location\":{\"protocol\":\"https:\",\"host\":\"fota-update-bucket.s3.eu-central-1.amazonaws.com\",\"path\":\"/update.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWXEL53DXIU7W72AE%2F20190606%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20190606T081505Z&X-Amz-Expires=604800&X-Amz-Signature=913e00b97efe5565a901df4ff0b87e4878a406941d711f59d45915035989adcc&X-Amz-SignedHeaders=host\"}}}}";
-	int ret;
 	char job_id[100];
 	char hostname[100];
 	char file_path[1000];
-	int version_number;
-
-	/* Memset to ensure correct null-termination */
-	memset(job_id, 0xff, sizeof(job_id));
-	memset(hostname, 0xff, sizeof(hostname));
-	memset(file_path, 0xff, sizeof(file_path));
 
 	ret = aws_fota_parse_DescribeJobExecution_rsp(encoded,
 						      sizeof(encoded) - 1,
 						      job_id, hostname,
 						      file_path,
 						      &version_number);
-
 	zassert_equal(ret, 1, NULL);
 	zassert_true(!strcmp(job_id, expected_job_id), NULL);
 	zassert_equal(version_number, expected_version_number, NULL);
@@ -41,48 +34,102 @@ static void test_parse_job_execution(void)
 	zassert_true(!strcmp(file_path, expected_file_path), NULL);
 }
 
-static void test_parse_job_execution_missing_field(void)
+static void test_parse_malformed_job_execution(void)
 {
-	char expected_job_id[] = "9b5caac6-3e8a-45dd-9273-c1b995762f4a";
-	char expected_hostname[] = "fota-update-bucket.s3.eu-central-1.amazonaws.com";
-	char expected_file_path[] = "/update.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWXEL53DXIU7W72AE%2F20190606%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20190606T081505Z&X-Amz-Expires=604800&X-Amz-Signature=913e00b97efe5565a901df4ff0b87e4878a406941d711f59d45915035989adcc&X-Amz-SignedHeaders=host";
-
 	int ret;
 	char job_id[100];
 	char hostname[100];
 	char file_path[1000];
 	int version_number;
+	char malformed[] = "{\"timestamp\":1559808907,\"execution\":{jobId\":\"9b5caac6-3e8a-45dd-9273-c1b995762f4a\",\"status\":\"QUEUED\"\"queuedAt\":1559808906,\"lastUpdatedAt\":1559808906,\"versionNumber\":1,\"executionNumber\":1,\"jobDocument\":{\"operation\":\"app_fw_update\",\"fwversion\":\"2\",\"size\":181124,\"location\":{\"protocol\":\"https:\",\"host\":\"fota-update-bucket.s3.eu-central-1.amazonaws.com\",\"path\":\"/update.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWXEL53DXIU7W72AE%2F20190606%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20190606T081505Z&X-Amz-Expires=604800&X-Amz-Signature=913e00b97efe5565a901df4ff0b87e4878a406941d711f59d45915035989adcc&X-Amz-SignedHeaders=host\"}}}}";
 
-	char encoded[] = "{\"timestamp\":1559808907,\"execution\":{\"status\":\"QUEUED\",\"queuedAt\":1559808906,\"lastUpdatedAt\":1559808906,\"versionNumber\":1,\"executionNumber\":1,\"jobDocument\":{\"operation\":\"app_fw_update\",\"fwversion\":\"2\",\"size\":181124,\"location\":{\"protocol\":\"https:\",\"host\":\"fota-update-bucket.s3.eu-central-1.amazonaws.com\",\"path\":\"/update.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWXEL53DXIU7W72AE%2F20190606%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20190606T081505Z&X-Amz-Expires=604800&X-Amz-Signature=913e00b97efe5565a901df4ff0b87e4878a406941d711f59d45915035989adcc&X-Amz-SignedHeaders=host\"}}}}";
-/* Memset to ensure correct null-termination */
-	memset(job_id, 0xff, sizeof(job_id));
-	memset(hostname, 0xff, sizeof(hostname));
-	memset(file_path, 0xff, sizeof(file_path));
+	ret = aws_fota_parse_DescribeJobExecution_rsp(malformed,
+						      sizeof(malformed) - 1,
+						      job_id, hostname,
+						      file_path,
+						      &version_number);
+	zassert_equal(ret, -ENODATA, NULL);
+}
+
+static void test_parse_job_execution_missing_host_field(void)
+{
+	int ret;
+	int version_number;
+	char job_id[100];
+	char hostname[100];
+	char file_path[1000];
+
+	char encoded[] = "{\"timestamp\":1559808907,\"execution\":{\"jobId\":\"9b5caac6-3e8a-45dd-9273-c1b995762f4a\",\"status\":\"QUEUED\",\"queuedAt\":1559808906,\"lastUpdatedAt\":1559808906,\"versionNumber\":1,\"executionNumber\":1,\"jobDocument\":{\"operation\":\"app_fw_update\",\"fwversion\":\"2\",\"size\":181124,\"location\":{\"protocol\":\"https:\",\"path\":\"/update.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWXEL53DXIU7W72AE%2F20190606%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20190606T081505Z&X-Amz-Expires=604800&X-Amz-Signature=913e00b97efe5565a901df4ff0b87e4878a406941d711f59d45915035989adcc&X-Amz-SignedHeaders=host\"}}}}";
 
 	ret = aws_fota_parse_DescribeJobExecution_rsp(encoded,
 						      sizeof(encoded) - 1,
 						      job_id, hostname,
 						      file_path,
 						      &version_number);
-
 	zassert_equal(ret, -ENODATA, NULL);
-	zassert_true(!strcmp(job_id, expected_job_id), NULL);
-	zassert_equal(version_number, 1, NULL);
-	zassert_true(!strcmp(hostname, expected_hostname), NULL);
-	zassert_true(!strcmp(file_path, expected_file_path), NULL);
+}
+
+static void test_parse_job_execution_missing_path_field(void)
+{
+	int ret;
+	int version_number;
+	char job_id[100];
+	char hostname[100];
+	char file_path[1000];
+	char encoded[] = "{\"timestamp\":1559808907,\"execution\":{\"jobId\":\"9b5caac6-3e8a-45dd-9273-c1b995762f4a\",\"status\":\"QUEUED\",\"queuedAt\":1559808906,\"lastUpdatedAt\":1559808906,\"versionNumber\":1,\"executionNumber\":1,\"jobDocument\":{\"operation\":\"app_fw_update\",\"fwversion\":\"2\",\"size\":181124,\"location\":{\"protocol\":\"https:\",\"host\":\"fota-update-bucket.s3.eu-central-1.amazonaws.com\"}}}}";
+
+	ret = aws_fota_parse_DescribeJobExecution_rsp(encoded,
+						      sizeof(encoded) - 1,
+						      job_id, hostname,
+						      file_path,
+						      &version_number);
+	zassert_equal(ret, -ENODATA, NULL);
+}
+
+static void test_parse_job_execution_missing_job_id_field(void)
+{
+	int ret;
+	int version_number;
+	char job_id[100];
+	char hostname[100];
+	char file_path[1000];
+	char encoded[] = "{\"timestamp\":1559808907,\"execution\":{\"status\":\"QUEUED\",\"queuedAt\":1559808906,\"lastUpdatedAt\":1559808906,\"versionNumber\":1,\"executionNumber\":1,\"jobDocument\":{\"operation\":\"app_fw_update\",\"fwversion\":\"2\",\"size\":181124,\"location\":{\"protocol\":\"https:\",\"host\":\"fota-update-bucket.s3.eu-central-1.amazonaws.com\",\"path\":\"/update.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWXEL53DXIU7W72AE%2F20190606%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20190606T081505Z&X-Amz-Expires=604800&X-Amz-Signature=913e00b97efe5565a901df4ff0b87e4878a406941d711f59d45915035989adcc&X-Amz-SignedHeaders=host\"}}}}";
+
+	ret = aws_fota_parse_DescribeJobExecution_rsp(encoded,
+						      sizeof(encoded) - 1,
+						      job_id, hostname,
+						      file_path,
+						      &version_number);
+	zassert_equal(ret, -ENODATA, NULL);
+}
+
+static void test_parse_job_execution_missing_location_obj(void)
+{
+	int ret;
+	int version_number;
+	char job_id[100];
+	char hostname[100];
+	char file_path[1000];
+	char encoded[] = "{\"timestamp\":1559808907,\"execution\":{\"jobId\":\"9b5caac6-3e8a-45dd-9273-c1b995762f4a\",\"status\":\"QUEUED\",\"queuedAt\":1559808906,\"lastUpdatedAt\":1559808906,\"versionNumber\":1,\"executionNumber\":1,\"jobDocument\":{\"operation\":\"app_fw_update\",\"fwversion\":\"2\",\"size\":181124}}}}";
+
+	ret = aws_fota_parse_DescribeJobExecution_rsp(encoded,
+						      sizeof(encoded) - 1,
+						      job_id, hostname,
+						      file_path,
+						      &version_number);
+	zassert_equal(ret, -ENODATA, NULL);
 }
 
 static void test_update_job_longer_than_max(void)
 {
+	int ret;
+	char status[100];
 	char expected_status[STATUS_MAX_LEN] = "12345678901";
 	char encoded[] = "{\"status\":\"12345678901234567890\","
 			  "\"statusDetails\":{\"nextState\":"
 			  "\"download_firmware\"},\"expectedVersion\":"
 			  "\"1\",\"clientToken\": \"\"}";
-	char status[100];
-	int ret;
 
-	memset(status, 0xff, sizeof(status));
 
 	ret = aws_fota_parse_UpdateJobExecution_rsp(encoded,
 			sizeof(encoded) - 1, status);
@@ -105,7 +152,6 @@ static void test_timestamp_only(void)
 						      hostname,
 						      file_path,
 						      &version_number);
-
 	zassert_equal(ret, 0, "Timestamp decoded correctly");
 }
 
@@ -118,19 +164,16 @@ static void test_update_job_exec_rsp_minimal(void)
 	ret = aws_fota_parse_UpdateJobExecution_rsp(encoded,
 						    sizeof(encoded) - 1,
 						    status);
-
 	/* Only two last fields are set */
 	zassert_equal(ret, -ENODATA, "All fields decoded correctly");
 }
 
 static void test_update_job_exec_rsp(void)
 {
+	int ret;
+	char status[100];
 	char expected_status[] = "IN_PROGRESS";
 	char encoded[] = "{\"status\":\"IN_PROGRESS\",\"statusDetails\":{\"nextState\":\"download_firmware\"},\"expectedVersion\":\"1\",\"clientToken\": \"\"}";
-	char status[100];
-	int ret;
-
-	memset(status, 0xff, sizeof(status));
 
 	ret = aws_fota_parse_UpdateJobExecution_rsp(encoded,
 						    sizeof(encoded) - 1,
@@ -142,11 +185,16 @@ static void test_update_job_exec_rsp(void)
 void test_main(void)
 {
 	ztest_test_suite(lib_json_test,
-			 ztest_unit_test(test_update_job_exec_rsp_minimal),
-			 ztest_unit_test(test_update_job_exec_rsp),
+			 ztest_unit_test(test_parse_job_execution),
+			 ztest_unit_test(test_parse_job_execution_missing_job_id_field),
+			 ztest_unit_test(test_parse_job_execution_missing_location_obj),
+			 ztest_unit_test(test_parse_job_execution_missing_path_field),
+			 ztest_unit_test(test_parse_job_execution_missing_host_field),
 			 ztest_unit_test(test_update_job_longer_than_max),
 			 ztest_unit_test(test_timestamp_only),
-			 ztest_unit_test(test_parse_job_execution)
+			 ztest_unit_test(test_parse_malformed_job_execution),
+			 ztest_unit_test(test_update_job_exec_rsp_minimal),
+			 ztest_unit_test(test_update_job_exec_rsp)
 			 );
 
 	ztest_run_test_suite(lib_json_test);
