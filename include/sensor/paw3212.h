cd1a7d60f662509bed20bee3f5a458eb8162da4c
Marek Pieta <Marek.Pieta@nordicsemi.no>
2020-01-17 02:51:16 -0800

    drivers: paw3212: Extend configuration
    Change adds sleep mode disabling and setting sleep mode sample
    times.
    
    Jira:DESK-824
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- include/sensor/paw3212.h
+++ include/sensor/paw3212.h
@@ -14,16 +14,18 @@ extern "C" {
 
 enum paw3212_attribute {
 	PAW3212_ATTR_CPI = SENSOR_ATTR_PRIV_START,
+	PAW3212_ATTR_SLEEP_ENABLE,
 	PAW3212_ATTR_SLEEP1_TIMEOUT,
 	PAW3212_ATTR_SLEEP2_TIMEOUT,
 	PAW3212_ATTR_SLEEP3_TIMEOUT,
+	PAW3212_ATTR_SLEEP1_SAMPLE_TIME,
+	PAW3212_ATTR_SLEEP2_SAMPLE_TIME,
+	PAW3212_ATTR_SLEEP3_SAMPLE_TIME,
 };
 
 #define PAW3212_SVALUE_TO_CPI(svalue) ((u32_t)(svalue).val1)
-#define PAW3212_CPI_TO_SVALUE(cpi) ((struct sensor_value) {.val1 = cpi})
-
 #define PAW3212_SVALUE_TO_TIME(svalue) ((u32_t)(svalue).val1)
-#define PAW3212_TIME_TO_SVALUE(time) ((struct sensor_value) {.val1 = time})
+#define PAW3212_SVALUE_TO_BOOL(svalue) ((svalue).val1 != 0)
 
 #ifdef __cplusplus
 }
