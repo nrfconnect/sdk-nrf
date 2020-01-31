219e7aecf7aed8cf8b187bfbc3590e143645845f
Erik Johnson <erik.johnson@nimbelink.com>
2020-01-31 08:20:05 -0600

    samples: nrf9160: serial_lte_modem: Updated string write to const char
    This doesn't need to be able to change the buffer contents, and it
    prevents having to do a dangerous cast-away of const in a few
    locations.
    
    Signed-off-by: Erik Johnson <erik.johnson@nimbelink.com>

--- samples/nrf9160/serial_lte_modem/src/slm_at_host.c
+++ samples/nrf9160/serial_lte_modem/src/slm_at_host.c
@@ -70,7 +70,7 @@ extern void enter_sleep(u16_t mode);
 /* forward declaration */
 void slm_at_host_uninit(void);
 
-static inline void write_uart_string(char *str, size_t len)
+static inline void write_uart_string(const char *str, size_t len)
 {
 	LOG_HEXDUMP_DBG(str, len, "TX");
 
