6e9b802bfcd411d91c9697b7f5711da8319a4c3c
Marek Pieta <Marek.Pieta@nordicsemi.no>
2020-02-04 02:07:28 -0800

    applications: nrf_desktop: Add nrf52833_pca10100
    Change adds nrf52833_pca10100 board configurations.
    The board is used as nRF Destkop dongle.
    
    Jira:DESK-804
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/nrf_desktop/configuration/nrf52833_pca10100/port_state_def.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <soc.h>
+
+#include "port_state.h"
+
+/* This configuration file is included only once from board module and holds
+ * information about default pin states set while board is on and off.
+ */
+
+/* This structure enforces the header file is included only once in the build.
+ * Violating this requirement triggers a multiple definition error at link time.
+ */
+const struct {} port_state_def_include_once;
+
+
+static const struct pin_state port0_on[] = {
+	{DT_GPIO_LEDS_LED2_GPIOS_PIN, 1},
+	{DT_GPIO_LEDS_LED3_GPIOS_PIN, 1},
+};
+
+static const struct pin_state port1_on[] = {
+};
+
+static const struct pin_state port0_off[] = {
+};
+
+static const struct pin_state port1_off[] = {
+};
+
+
+static const struct port_state port_state_on[] = {
+	{
+		.name     = DT_GPIO_P0_DEV_NAME,
+		.ps       = port0_on,
+		.ps_count = ARRAY_SIZE(port0_on),
+	},
+	{
+		.name     = DT_GPIO_P1_DEV_NAME,
+		.ps       = port1_on,
+		.ps_count = ARRAY_SIZE(port1_on),
+	}
+};
+
+static const struct port_state port_state_off[] = {
+	{
+		.name     = DT_GPIO_P0_DEV_NAME,
+		.ps       = port0_off,
+		.ps_count = ARRAY_SIZE(port0_off),
+	},
+	{
+		.name     = DT_GPIO_P1_DEV_NAME,
+		.ps       = port1_off,
+		.ps_count = ARRAY_SIZE(port1_off),
+	}
+};
