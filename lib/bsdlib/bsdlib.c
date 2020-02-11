aee3b31479fe9129692c7e6d762355f36d84bfe5
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2020-02-11 18:20:54 +0100

    lib: bsdlib: Add Kconfig option to allow running BSDLib in secure
    This allows a user for BSDLib to utilize the full chip and all it's
    peripherals. The whole RAM is available except the block which is
    occupied by statically allocating the size of memory needed and putting
    it at the start of the location BSDLib expects it to be through the
    `__attribute__` hint and linker script sections.
    
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>

--- lib/bsdlib/bsdlib.c
+++ lib/bsdlib/bsdlib.c
@@ -11,9 +11,84 @@
 #include <bsd.h>
 #include <bsd_platform.h>
 
-#ifndef CONFIG_TRUSTED_EXECUTION_NONSECURE
-#error  bsdlib must be run as non-secure firmware.\
-	Are you building for the correct board ?
+#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) &&\
+	!defined(CONFIG_BSD_LIBRARY_ALLOW_SECURE)
+#error  bsdlib must be run as non-secure firmware. Are you building for the correct board ?
+#endif
+
+#ifdef CONFIG_BSD_LIBRARY_ALLOW_SECURE
+#define SRAM_WRITE                                                             \
+	((SPU_RAMREGION_PERM_WRITE_Enable << SPU_RAMREGION_PERM_WRITE_Pos) &   \
+	 SPU_RAMREGION_PERM_WRITE_Msk)
+
+#define SRAM_READ                                                              \
+	((SPU_RAMREGION_PERM_READ_Enable << SPU_RAMREGION_PERM_READ_Pos) &     \
+	 SPU_RAMREGION_PERM_READ_Msk)
+
+#define SRAM_LOCK                                                              \
+	((SPU_RAMREGION_PERM_LOCK_Locked << SPU_RAMREGION_PERM_LOCK_Pos) &     \
+	 SPU_RAMREGION_PERM_LOCK_Msk)
+
+#define SRAM_NONSEC                                                            \
+	((SPU_RAMREGION_PERM_SECATTR_Non_Secure                                \
+	  << SPU_RAMREGION_PERM_SECATTR_Pos) &                                 \
+	 SPU_RAMREGION_PERM_SECATTR_Msk)
+
+#define PERIPH_NONSEC                                                          \
+	((SPU_PERIPHID_PERM_SECATTR_NonSecure                                  \
+	  << SPU_PERIPHID_PERM_SECATTR_Pos) &                                  \
+	 SPU_PERIPHID_PERM_SECATTR_Msk)
+
+#define PERIPH_LOCK                                                            \
+	((SPU_PERIPHID_PERM_LOCK_Locked << SPU_PERIPHID_PERM_LOCK_Pos) &       \
+	 SPU_PERIPHID_PERM_LOCK_Msk)
+#if defined(CONFIG_SOC_NRF9160)
+#define SPU_RAMREGION_SIZE (256/32)
+#define RAM_START_ADDR 0x20000000
+#endif
+static inline void setup_ns_ram(void)
+{
+	u8_t start_block = (BSD_RESERVED_MEMORY_ADDRESS - RAM_START_ADDR) /
+			   SPU_RAMREGION_SIZE;
+	u8_t size = BSD_RESERVED_MEMORY_SIZE / SPU_RAMREGION_SIZE;
+
+	for (u8_t i = start_block; i < (start_block + size); i++) {
+		NRF_SPU_S->RAMREGION[i].PERM = SRAM_READ | SRAM_WRITE |
+					       SRAM_LOCK  | SRAM_NONSEC;
+	}
+}
+
+static inline void setup_clock(void)
+{
+	NRF_CLOCK_S->LFCLKSRC = CLOCK_LFCLKSRCCOPY_SRC_LFRC
+				<< CLOCK_LFCLKSRC_SRC_Pos;
+	NRF_CLOCK_S->EVENTS_LFCLKSTARTED  = 0;
+	NRF_CLOCK_S->TASKS_LFCLKSTART = 1;
+	while (NRF_CLOCK_S->EVENTS_LFCLKSTARTED == 0) {
+	};
+	NRF_CLOCK_S->EVENTS_LFCLKSTARTED = 0;
+}
+
+static inline void setup_ns_power(void)
+{
+	NRF_SPU_S->PERIPHID[NRFX_PERIPHERAL_ID_GET(NRF_POWER_NS)].PERM =
+	       PERIPH_LOCK | PERIPH_NONSEC;
+	NRF_SPU_S->PERIPHID[NRFX_PERIPHERAL_ID_GET(NRF_POWER_S)].PERM = 0;
+}
+
+static inline void setup_s_ipc(void)
+{
+	NRF_SPU_S->PERIPHID[NRFX_PERIPHERAL_ID_GET(NRF_IPC_S)].PERM =
+		PERIPH_LOCK | PERIPH_NONSEC;
+}
+
+static inline void setup_for_secure(void)
+{
+	setup_clock();
+	setup_ns_power();
+	setup_s_ipc();
+	setup_ns_ram();
+}
 #endif
 
 extern void ipc_proxy_irq_handler(void);
@@ -44,6 +119,9 @@ static int _bsdlib_init(struct device *unused)
 
 int bsdlib_init(void)
 {
+#ifdef CONFIG_BSD_LIBRARY_ALLOW_SECURE
+	setup_for_secure();
+#endif
 	return _bsdlib_init(NULL);
 }
 
