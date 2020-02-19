0a68cc3f988452356f62d0f94cceb6e59ee5bbfb
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2020-02-19 10:07:50 +0100

    applications: nrf_desktop: Add configuration for PCA10111
    Jira:DESK-760
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

---
+++ applications/nrf_desktop/configuration/nrf52833_pca10111/buttons_def.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include "gpio_pins.h"
+
+/* This configuration file is included only once from button module and holds
+ * information about pins forming keyboard matric.
+ */
+
+/* This structure enforces the header file is included only once in the build.
+ * Violating this requirement triggers a multiple definition error at link time.
+ */
+const struct {} buttons_def_include_once;
+
+static const struct gpio_pin col[] = {};
+
+static const struct gpio_pin row[] = {
+	{ .port = 0, .pin = DT_ALIAS_SW0_GPIOS_PIN },
+};
