ad95cfef454c94f4d45631c02df6adb4245dfa93
Ryan Chu <Ryan.Chu@nordicsemi.no>
2020-02-20 13:20:54 +0100

    drivers: mpsl: clock_control: Support clock async_on feature
    Request clock to start with callback notification when clock has been
    started. Because LFCLK is started by default by mpsl_init, the callback
    will be immediately called from the context of the function call. When
    requesting HFCLK, it will be called from either clock interrupt context
    or function call depending on the status of clock.
    
    If same request already scheduled and not yet completed, the status
    -EBUSY will be returned.
    
    If clock is started and not running, the reference of callback will be
    stored in the driver. Therefore, the input data must be valid until the
    callback is called or clock is turned off.
    
    When the clock has been started, the buffered callback will be called
    on-by-one and removed from the driver buffer
    
    Signed-off-by: Ryan Chu <Ryan.Chu@nordicsemi.no>

--- drivers/mpsl/clock_control/clock_control_mpsl.c
+++ drivers/mpsl/clock_control/clock_control_mpsl.c
@@ -18,6 +18,15 @@
 #include <hal/nrf_power.h>
 #endif
 
+/* MPSL clock control structure */
+struct mpsl_clock_control_data {
+	sys_slist_t async_on_list;
+	u8_t hfclk_count;
+};
+
+/* MPSL clock control callbacks */
+static void hf_clock_started_callback(void);
+
 static bool is_context_atomic(void)
 {
 	int key = irq_lock();
@@ -26,7 +35,32 @@ static bool is_context_atomic(void)
 	return arch_irq_unlocked(key) || k_is_in_isr();
 }
 
-static int hf_clock_start(void)
+static bool clock_list_find(sys_slist_t *list, sys_snode_t *node)
+{
+	bool found = false;
+	int key = irq_lock();
+	struct clock_control_async_data *clock_async_data = NULL;
+
+	SYS_SLIST_FOR_EACH_CONTAINER(list, clock_async_data, node) {
+		if (&(clock_async_data->node) == node) {
+			found = true;
+			break;
+		}
+	}
+
+	irq_unlock(key);
+	return found;
+}
+
+static void clock_list_append(sys_slist_t *list, sys_snode_t *node)
+{
+	int key = irq_lock();
+
+	sys_slist_append(list, node);
+	irq_unlock(key);
+}
+
+static int hf_clock_start(mpsl_clock_hfclk_callback_t hfclk_callback)
 {
 	int errcode;
 
@@ -38,7 +72,7 @@ static int hf_clock_start(void)
 	if (errcode) {
 		return -EFAULT;
 	}
-	errcode = mpsl_clock_hfclk_request(NULL);
+	errcode = mpsl_clock_hfclk_request(hfclk_callback);
 	MULTITHREADING_LOCK_RELEASE();
 	if (errcode) {
 		return -EFAULT;
@@ -132,13 +166,28 @@ void nrf_power_clock_isr(void)
 
 static int clock_start(struct device *dev, clock_control_subsys_t subsys)
 {
-	ARG_UNUSED(dev);
-
+	struct mpsl_clock_control_data *mpsl_control_data = dev->driver_data;
 	enum clock_control_nrf_type type = (enum clock_control_nrf_type)subsys;
+	int errcode = 0;
+	int key;
 
 	switch (type) {
 	case CLOCK_CONTROL_NRF_TYPE_HFCLK:
-		return hf_clock_start();
+		key = irq_lock();
+		if ((u8_t)(mpsl_control_data->hfclk_count + 1) == 0) {
+			/* overflow the start count that it can support */
+			irq_unlock(key);
+			return -ENOTSUP;
+		} else if (mpsl_control_data->hfclk_count == 0) {
+			errcode = hf_clock_start(hf_clock_started_callback);
+		}
+
+		if (errcode == 0) {
+			++mpsl_control_data->hfclk_count;
+		}
+
+		irq_unlock(key);
+		return errcode;
 	case CLOCK_CONTROL_NRF_TYPE_LFCLK:
 		/* No-op. LFCLK is started by default by mpsl_init(). */
 		return 0;
@@ -149,13 +198,32 @@ static int clock_start(struct device *dev, clock_control_subsys_t subsys)
 
 static int clock_stop(struct device *dev, clock_control_subsys_t subsys)
 {
-	ARG_UNUSED(dev);
-
+	struct mpsl_clock_control_data *mpsl_control_data = dev->driver_data;
 	enum clock_control_nrf_type type = (enum clock_control_nrf_type)subsys;
+	int errcode = 0;
+	int key;
 
 	switch (type) {
 	case CLOCK_CONTROL_NRF_TYPE_HFCLK:
-		return hf_clock_stop();
+		key = irq_lock();
+		if (mpsl_control_data->hfclk_count == 0) {
+			irq_unlock(key);
+			return -EALREADY;
+		} else if (mpsl_control_data->hfclk_count == 1) {
+			sys_slist_t *list = &(mpsl_control_data->async_on_list);
+
+			errcode = hf_clock_stop();
+			if (errcode == 0) {
+				sys_slist_init(list);
+			}
+		}
+
+		if (errcode == 0) {
+			--mpsl_control_data->hfclk_count;
+		}
+
+		irq_unlock(key);
+		return errcode;
 	case CLOCK_CONTROL_NRF_TYPE_LFCLK:
 		/* Stopping of LFCLK is not supported. */
 		return -ENOTSUP;
@@ -207,9 +275,64 @@ static enum clock_control_status clock_get_status(struct device *dev,
 	}
 }
 
+static int clock_async_start(struct device *dev, clock_control_subsys_t subsys,
+			     struct clock_control_async_data *data)
+{
+	struct mpsl_clock_control_data *mpsl_control_data = dev->driver_data;
+	int errcode = 0;
+	int key = irq_lock();
+	enum clock_control_status clock_status = clock_get_status(dev, subsys);
+
+	switch (clock_status) {
+	case CLOCK_CONTROL_STATUS_ON:
+		if (subsys == CLOCK_CONTROL_NRF_SUBSYS_HF) {
+			if ((u8_t)(mpsl_control_data->hfclk_count + 1) == 0) {
+				/*  the counter of HFCLK gets overflow */
+				irq_unlock(key);
+				return -ENOTSUP;
+			}
+			++mpsl_control_data->hfclk_count;
+		}
+
+		/* LFCLK is started by default and not able to stop it.
+		 * Therefore, LFCLK is always handled in case STATUS_ON.
+		 */
+		if ((data != NULL) && (data->cb != NULL)) {
+			data->cb(dev, data->user_data);
+		}
+
+		break;
+	case CLOCK_CONTROL_STATUS_STARTING:
+	case CLOCK_CONTROL_STATUS_OFF:
+		if ((data != NULL) && (data->cb != NULL)) {
+			sys_slist_t *list = &(mpsl_control_data->async_on_list);
+
+			/* if the same request is already scheduled and not yet
+			 * completed.
+			 */
+			if (clock_list_find(list, &data->node)) {
+				irq_unlock(key);
+				return -EBUSY;
+			}
+
+			clock_list_append(list, &data->node);
+		}
+
+		errcode =  clock_start(dev, subsys);
+		break;
+	default:
+		errcode = -ENOTSUP;
+		break;
+	}
+
+	irq_unlock(key);
+	return errcode;
+}
+
 static int clock_control_init(struct device *dev)
 {
-	ARG_UNUSED(dev);
+	struct mpsl_clock_control_data *mpsl_control_data = dev->driver_data;
+
 	/* No-op. LFCLK is initialized by mpsl_init() at PRE_KERNEL_1,
 	 * see subsys/mpsl/mpsl_init.c.
 	 */
@@ -218,19 +341,24 @@ static int clock_control_init(struct device *dev)
 		    DT_INST_0_NORDIC_NRF_CLOCK_IRQ_0_PRIORITY,
 		    nrf_power_clock_isr, 0, 0);
 
+	sys_slist_init(&(mpsl_control_data->async_on_list));
+	mpsl_control_data->hfclk_count = 0;
 	return 0;
 }
 
 static const struct clock_control_driver_api clock_control_api = {
 	.on = clock_start,
 	.off = clock_stop,
+	.async_on = clock_async_start,
 	.get_rate = clock_get_rate,
 	.get_status = clock_get_status,
 };
 
+static struct mpsl_clock_control_data clock_control_data;
+
 DEVICE_AND_API_INIT(clock_nrf,
 		    DT_INST_0_NORDIC_NRF_CLOCK_LABEL,
-		    clock_control_init, NULL, NULL, PRE_KERNEL_1,
+		    clock_control_init, &clock_control_data, NULL, PRE_KERNEL_1,
 		    CONFIG_KERNEL_INIT_PRIORITY_DEVICE, &clock_control_api);
 
 #if IS_ENABLED(CONFIG_USB_NRFX)
@@ -253,3 +381,37 @@ void nrf5_power_usb_power_int_enable(bool enable)
 }
 
 #endif
+
+static void hf_clock_started_callback(void)
+{
+	/* NOTE: Don't acquire the lock of mpsl API twice when calling
+	 * mpsl_clock_hfclk_is_running() here.
+	 * When this callback is running in ISR, it won't be preempted by other
+	 * threads.
+	 * If it is called in a thread context, the lock of mpsl is acquired in
+	 * hf_clock_start().
+	 */
+	int key = irq_lock();
+	u32_t is_running = 0;
+	int errcode = mpsl_clock_hfclk_is_running(&is_running);
+
+	if (errcode) {
+		__ASSERT_NO_MSG(errcode == 0); /* it should never happen */
+		irq_unlock(key);
+		return;
+	}
+
+	sys_slist_t *list = &(clock_control_data.async_on_list);
+	struct device *dev = DEVICE_GET(clock_nrf);
+
+	if (is_running) {
+		struct clock_control_async_data *clock_async_data = NULL;
+
+		SYS_SLIST_FOR_EACH_CONTAINER(list, clock_async_data, node) {
+			clock_async_data->cb(dev, clock_async_data->user_data);
+		}
+	}
+
+	sys_slist_init(list);
+	irq_unlock(key);
+}
