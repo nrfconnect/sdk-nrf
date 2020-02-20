fae85b25c43b85084fe9866ad9403c00fd6b1bef
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2020-02-20 14:54:07 +0100

    fprotect: Add workaround for nRF53 erratum 19
    SPU region size is 32 kb instead of 16. This requires run-time check.
    
    Ref: NCSDK-4805
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- lib/fprotect/fprotect_spu.c
+++ lib/fprotect/fprotect_spu.c
@@ -8,17 +8,26 @@
 #include <string.h>
 #include <hal/nrf_spu.h>
 #include <errno.h>
+#include <soc.h>
+
+
+#if defined(CONFIG_SOC_NRF5340_CPUAPP) \
+	&& defined(CONFIG_NRF5340_CPUAPP_ERRATUM19)
+#define SPU_BLOCK_SIZE (nrf53_has_erratum19() ? 32*1024 : 16*1024)
+#else
+#define SPU_BLOCK_SIZE CONFIG_FPROTECT_BLOCK_SIZE
+#endif
 
 int fprotect_area(u32_t start, size_t length)
 {
-	if (start % CONFIG_FPROTECT_BLOCK_SIZE != 0 ||
-		length % CONFIG_FPROTECT_BLOCK_SIZE != 0) {
+	if (start % SPU_BLOCK_SIZE != 0 ||
+		length % SPU_BLOCK_SIZE != 0) {
 		return -EINVAL;
 	}
 
-	for (u32_t i = 0; i < length / CONFIG_FPROTECT_BLOCK_SIZE; i++) {
+	for (u32_t i = 0; i < length / SPU_BLOCK_SIZE; i++) {
 		nrf_spu_flashregion_set(NRF_SPU_S,
-				(start / CONFIG_FPROTECT_BLOCK_SIZE) + i,
+				(start / SPU_BLOCK_SIZE) + i,
 				true,
 				NRF_SPU_MEM_PERM_EXECUTE |
 				NRF_SPU_MEM_PERM_READ,
