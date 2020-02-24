122bcbae80a1c089c9121a65b743f19d141daaa9
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2020-02-24 14:31:40 +0100

    applications: asset_tracker: Use updated GPS API
    Makes adjustmens to the application to support the updated GPS API:
    - Remove GPS controller module. Its functionality should now be
    handled by the GPS drivers and configurable through the GPS API.
    - Handle all events coming from the GPS instead of only the one
    trigger source that was previously used.
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

--- applications/asset_tracker/src/gps_controller/gps_controller.h
+++ applications/asset_tracker/src/gps_controller/gps_controller.h
@@ -18,7 +18,7 @@
 extern "C" {
 #endif
 
-int gps_control_init(struct k_work_q *work_q, gps_trigger_handler_t handler);
+int gps_control_init(struct k_work_q *work_q, gps_event_handler_t handler);
 
 void gps_control_on_trigger(void);
 
@@ -26,15 +26,11 @@ void gps_control_stop(u32_t delay_ms);
 
 void gps_control_start(u32_t delay_ms);
 
-bool gps_control_is_active(void);
-
 bool gps_control_is_enabled(void);
 
-void gps_control_enable(void);
+bool gps_control_is_active(void);
 
-void gps_control_disable(void);
-
-s64_t gps_control_get_last_active_time(void);
+bool gps_control_set_active(bool active);
 
 #ifdef __cplusplus
 }
