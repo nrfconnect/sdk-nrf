11e936537fdcfc9f378b7bcd1ced5e6e9ef68866
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2020-02-26 16:38:21 +0100

    samples: nrf9160: lwm2m_client: Adjustments to use new GPS API
    Makes the required changes to use the updated GPS API.
    Also adds a couple of missing log_strdup(...) along the way.
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

--- samples/nrf9160/lwm2m_client/src/lwm2m/lwm2m_location.c
+++ samples/nrf9160/lwm2m_client/src/lwm2m/lwm2m_location.c
@@ -14,26 +14,27 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(app_lwm2m_loc, CONFIG_APP_LOG_LEVEL);
 
-static struct gps_data nmea_data;
+static struct gps_nmea nmea_data;
 static struct device *gps_dev;
-static struct gps_trigger gps_trig = {
-	.type = GPS_TRIG_DATA_READY,
-};
 
-static void update_location_data(struct gps_data *nmea)
+static void update_location_data(struct gps_nmea *nmea)
 {
+	ARG_UNUSED(nmea);
 	LOG_DBG("nmea_data");
 	/* TODO: change API to send actual values (not string)
 	 * so we can update Location object resources
 	 */
 }
 
-/**@brief Callback for GPS trigger events */
-static void gps_trigger_handler(struct device *dev, struct gps_trigger *trigger)
+/**@brief Callback for GPS events */
+static void gps_event_handler(struct device *dev, struct gps_event *evt)
 {
-	ARG_UNUSED(trigger);
-	int err;
+	ARG_UNUSED(dev);
 	static u32_t timestamp_prev;
+
+	if (evt->type != GPS_EVT_NMEA_FIX) {
+		return;
+	}
 
 	if (k_uptime_get_32() - timestamp_prev <
 	    K_SECONDS(CONFIG_APP_HOLD_TIME_GPS)) {
@@ -41,21 +42,12 @@ static void gps_trigger_handler(struct device *dev, struct gps_trigger *trigger)
 	}
 
 	if (gps_dev == NULL) {
-		LOG_ERR("Could not get %s device", CONFIG_GPS_DEV_NAME);
+		LOG_ERR("Could not get %s device",
+			log_strdup(CONFIG_GPS_DEV_NAME));
 		return;
 	}
 
-	err = gps_sample_fetch(gps_dev);
-	if (err < 0) {
-		LOG_ERR("GPS sample fetch error: %d", err);
-		return;
-	}
-
-	err = gps_channel_get(gps_dev, GPS_CHAN_NMEA, &nmea_data);
-	if (err < 0) {
-		LOG_ERR("GPS channel get error: %d", err);
-		return;
-	}
+	memcpy(&nmea_data, &evt->nmea, sizeof(struct gps_nmea));
 
 	update_location_data(&nmea_data);
 	timestamp_prev = k_uptime_get_32();
@@ -67,26 +59,18 @@ int lwm2m_init_location(void)
 
 	gps_dev = device_get_binding(CONFIG_GPS_DEV_NAME);
 	if (gps_dev == NULL) {
-		LOG_ERR("Could not get %s device", CONFIG_GPS_DEV_NAME);
+		LOG_ERR("Could not get %s device",
+			log_strdup(CONFIG_GPS_DEV_NAME));
 		return -EINVAL;
 	}
 
-	LOG_DBG("GPS device found: %s", CONFIG_GPS_DEV_NAME);
+	LOG_DBG("GPS device found: %s", log_strdup(CONFIG_GPS_DEV_NAME));
 
-	if (IS_ENABLED(CONFIG_GPS_TRIGGER)) {
-		err = gps_trigger_set(gps_dev, &gps_trig, gps_trigger_handler);
-		if (err) {
-			LOG_ERR("Could not set trigger, error code: %d", err);
-			return err;
-		}
+	err = gps_init(gps_dev, gps_event_handler);
+	if (err) {
+		LOG_ERR("Could not set trigger, error code: %d", err);
+		return err;
 	}
 
-	err = gps_sample_fetch(gps_dev);
-	__ASSERT(err == 0, "GPS sample could not be fetched.");
-
-	err = gps_channel_get(gps_dev, GPS_CHAN_NMEA, &nmea_data);
-	__ASSERT(err == 0, "GPS sample could not be retrieved.");
-
-	update_location_data(&nmea_data);
 	return 0;
 }
