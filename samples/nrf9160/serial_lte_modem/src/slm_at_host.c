c896fe2a24d54b6fb8ddb0231b6360d55b489b38
Jun Qing Zou <jun.qing.zou@nordicsemi.no>
2020-02-29 16:04:50 +0900

    sample: nrf9160: serial_lte_modem low power idle mode
    Add active idle handling so as not to affect LTE connection
    .Use Serial Sync mode instead of Interrupt driven
    .Use device power management to power on/off UART interface
    .Use interface GPIO to exit idle as well as wake up
    .Redefine AT#XSLEEP[=<shutdown_mode>] for both Idle and Sleep
    .Move console and logging to Segger RTT
    .Disable serial/console/logging in SPM app
    
    Signed-off-by: Jun Qing Zou <jun.qing.zou@nordicsemi.no>

--- samples/nrf9160/serial_lte_modem/src/slm_at_host.c
+++ samples/nrf9160/serial_lte_modem/src/slm_at_host.c
@@ -34,7 +34,8 @@ LOG_MODULE_REGISTER(at_host, CONFIG_SLM_LOG_LEVEL);
 #define AT_CMD_SLEEP	"AT#XSLEEP"
 #define AT_CMD_CLAC	"AT#XCLAC"
 
-#define AT_MAX_CMD_LEN		CONFIG_AT_CMD_RESPONSE_MAX_LEN
+#define AT_MAX_CMD_LEN	CONFIG_AT_CMD_RESPONSE_MAX_LEN
+#define UART_RX_LEN	8  /* UART FIFO depth is 6 (?) */
 
 /** @brief Termination Modes. */
 enum term_modes {
@@ -45,11 +46,11 @@ enum term_modes {
 	MODE_COUNT      /* Counter of term_modes */
 };
 
-
-/** @brief UARTs. */
-enum select_uart {
-	UART_0,
-	UART_2
+/**@brief Shutdown modes. */
+enum shutdown_modes {
+	SHUTDOWN_MODE_IDLE,
+	SHUTDOWN_MODE_SLEEP,
+	SHUTDOWN_MODE_INVALID
 };
 
 static enum term_modes term_mode;
@@ -59,19 +60,42 @@ static size_t at_buf_len;
 static struct k_work cmd_send_work;
 static const char termination[3] = { '\0', '\r', '\n' };
 
+static u8_t uart_rx_buf[UART_RX_LEN];
+static u8_t *uart_tx_buf;
+static u8_t buf_num;
+
+static K_SEM_DEFINE(tx_done, 0, 1);
+
+/* global functions defined in different files */
+void enter_idle(void);
+void enter_sleep(void);
+
 /* global variable defined in different files */
 extern struct at_param_list m_param_list;
-extern void enter_sleep(void);
 
 /* forward declaration */
 void slm_at_host_uninit(void);
 
-static inline void write_uart_string(const char *str, size_t len)
+static void write_uart_string(const char *str, size_t len)
 {
+	int ret;
+
+	k_sem_take(&tx_done, K_FOREVER);
+
+	uart_tx_buf = k_malloc(len);
+	if (uart_tx_buf == NULL) {
+		LOG_WRN("No ram buffer");
+		return;
+	}
+
 	LOG_HEXDUMP_DBG(str, len, "TX");
 
-	for (size_t i = 0; i < len; i++) {
-		uart_poll_out(uart_dev, str[i]);
+	memcpy(uart_tx_buf, str, len);
+	ret = uart_tx(uart_dev, uart_tx_buf, len, K_FOREVER);
+	if (ret) {
+		LOG_WRN("uart_tx failed: %d", ret);
+		k_free(uart_tx_buf);
+		k_sem_give(&tx_done);
 	}
 }
 
@@ -100,10 +124,11 @@ static void handle_at_clac(void)
 	slm_at_gps_clac();
 }
 
-static int handle_at_sleep(const char *at_cmd)
+static int handle_at_sleep(const char *at_cmd, enum shutdown_modes *mode)
 {
 	int ret = -EINVAL;
 	enum at_cmd_type type;
+	u16_t shutdown_mode;
 
 	ret = at_parser_params_from_str(at_cmd, NULL, &m_param_list);
 	if (ret < 0) {
@@ -113,9 +138,37 @@ static int handle_at_sleep(const char *at_cmd)
 
 	type = at_parser_cmd_type_get(at_cmd);
 	if (type == AT_CMD_TYPE_SET_COMMAND) {
-		slm_at_host_uninit();
-		enter_sleep();
-		return 0; /* Cannot reach here */
+		shutdown_mode = SHUTDOWN_MODE_IDLE;
+		if (at_params_valid_count_get(&m_param_list) > 1) {
+			ret = at_params_short_get(&m_param_list, 1,
+					&shutdown_mode);
+			if (ret < 0) {
+				LOG_ERR("AT parameter error");
+				return -EINVAL;
+			}
+		}
+		if (shutdown_mode == SHUTDOWN_MODE_IDLE) {
+			slm_at_host_uninit();
+			enter_idle();
+			*mode = SHUTDOWN_MODE_IDLE;
+			ret = 0; /*Will send no "OK"*/
+		} else if (shutdown_mode == SHUTDOWN_MODE_SLEEP) {
+			slm_at_host_uninit();
+			enter_sleep();
+			ret = 0; /* Cannot reach here */
+		} else {
+			LOG_ERR("AT parameter error");
+			ret = -EINVAL;
+		}
+	}
+
+	if (type == AT_CMD_TYPE_TEST_COMMAND) {
+		char buf[64];
+
+		sprintf(buf, "#XSLEEP: (%d, %d)\r\n", SHUTDOWN_MODE_IDLE,
+			SHUTDOWN_MODE_SLEEP);
+		write_uart_string(buf, strlen(buf));
+		ret = 0;
 	}
 
 	return ret;
@@ -152,12 +205,21 @@ static void cmd_send(struct k_work *work)
 
 	size_cmd = sizeof(AT_CMD_SLEEP) - 1;
 	if (slm_at_cmd_cmp(at_buf, AT_CMD_SLEEP, size_cmd)) {
-		err = handle_at_sleep(at_buf);
-		if (err != 0) {
+		enum shutdown_modes mode = SHUTDOWN_MODE_INVALID;
+
+		err = handle_at_sleep(at_buf, &mode);
+		if (err) {
 			write_uart_string(ERROR_STR, sizeof(ERROR_STR) - 1);
 			goto done;
 		} else {
-			return;
+			if (mode == SHUTDOWN_MODE_INVALID) {
+				/*Test command*/
+				write_uart_string(OK_STR, sizeof(OK_STR) - 1);
+				goto done;
+			} else {
+				/*Entered IDLE*/
+				return;
+			}
 		}
 	}
 
@@ -189,7 +251,7 @@ static void cmd_send(struct k_work *work)
 
 	err = at_cmd_write(at_buf, buf, AT_MAX_CMD_LEN, &state);
 	if (err < 0) {
-		LOG_ERR("Could not send AT command to modem: %d", err);
+		LOG_ERR("AT command error: %d", err);
 		state = AT_CMD_ERROR;
 	}
 
@@ -214,7 +276,11 @@ static void cmd_send(struct k_work *work)
 	}
 
 done:
-	uart_irq_rx_enable(uart_dev);
+	err = uart_rx_enable(uart_dev, &uart_rx_buf[0], 1, K_FOREVER);
+	if (err) {
+		LOG_WRN("UART RX failed: %d", err);
+	}
+	buf_num = 1U;
 }
 
 static void uart_rx_handler(u8_t character)
@@ -285,53 +351,50 @@ static void uart_rx_handler(u8_t character)
 
 	return;
 send:
-	uart_irq_rx_disable(uart_dev);
+	uart_rx_disable(uart_dev);
 	k_work_submit(&cmd_send_work);
 	at_buf_len = cmd_len;
 	cmd_len = 0;
 }
 
-static void isr(struct device *dev)
-{
-	u8_t character;
-
-	uart_irq_update(dev);
-
-	if (!uart_irq_rx_ready(dev)) {
-		return;
-	}
-
-	while (uart_fifo_read(dev, &character, 1)) {
-		uart_rx_handler(character);
-	}
-}
-
-static int at_uart_init(char *uart_dev_name)
+static void uart_callback(struct uart_event *evt, void *user_data)
 {
 	int err;
-	u8_t dummy;
 
-	uart_dev = device_get_binding(uart_dev_name);
-	if (uart_dev == NULL) {
-		LOG_ERR("Cannot bind %s\n", uart_dev_name);
-		return -EINVAL;
-	}
-	/* Wait for the UART line to become valid */
-	do {
-		err = uart_err_check(uart_dev);
+	ARG_UNUSED(user_data);
+
+	switch (evt->type) {
+	case UART_TX_DONE:
+		k_free(uart_tx_buf);
+		k_sem_give(&tx_done);
+		break;
+	case UART_TX_ABORTED:
+		k_free(uart_tx_buf);
+		k_sem_give(&tx_done);
+		LOG_INF("TX_ABORTED");
+		break;
+	case UART_RX_RDY:
+		uart_rx_handler(evt->data.rx.buf[0]);
+		break;
+	case UART_RX_BUF_REQUEST:
+		err = uart_rx_buf_rsp(uart_dev, &uart_rx_buf[buf_num], 1);
 		if (err) {
-			LOG_ERR("UART check failed: %d. "
-				"Dropping buffer and retrying.", err);
-
-			while (uart_fifo_read(uart_dev, &dummy, 1)) {
-				/* Do nothing with the data */
-			}
-			k_sleep(10);
+			LOG_WRN("UART RX buf rsp: %d", err);
 		}
-	} while (err);
-
-	uart_irq_callback_set(uart_dev, isr);
-	return err;
+		buf_num++;
+		buf_num %= UART_RX_LEN;
+		break;
+	case UART_RX_BUF_RELEASED:
+		break;
+	case UART_RX_STOPPED:
+		LOG_WRN("RX_STOPPED");
+		break;
+	case UART_RX_DISABLED:
+		LOG_DBG("RX_DISABLED");
+		break;
+	default:
+		break;
+	}
 }
 
 static void slm_at_callback(const char *str)
@@ -343,8 +406,8 @@ int slm_at_host_init(void)
 {
 	char *uart_dev_name;
 	int err;
-	enum select_uart uart_id = CONFIG_SLM_CONNECT_UART;
 	enum term_modes mode = CONFIG_SLM_AT_HOST_TERMINATION;
+	u32_t start_time;
 
 	/* Choosing the termination mode */
 	if (mode < MODE_COUNT) {
@@ -354,35 +417,53 @@ int slm_at_host_init(void)
 	}
 
 	/* Choose which UART to use */
-	switch (uart_id) {
-	case UART_0:
+#if defined(CONFIG_SLM_CONNECT_UART_0)
 		uart_dev_name = SLM_UART_0_NAME;
-		break;
-	case UART_2:
+#elif defined(CONFIG_SLM_CONNECT_UART_2)
 		uart_dev_name = SLM_UART_2_NAME;
-		break;
-	default:
-		LOG_ERR("Unknown UART instance %d", uart_id);
+#else
+	LOG_ERR("Unsupported UART instance");
+	return -EINVAL;
+#endif
+	/* Initialize the UART module */
+	uart_dev = device_get_binding(uart_dev_name);
+	if (uart_dev == NULL) {
+		LOG_ERR("Cannot bind %s\n", uart_dev_name);
 		return -EINVAL;
 	}
+	/* Wait for the UART line to become valid */
+	start_time = k_uptime_get_32();
+	do {
+		err = uart_err_check(uart_dev);
+		if (err) {
+			if (k_uptime_get_32() - start_time > K_MSEC(500)) {
+				LOG_ERR("UART check failed: %d. "
+					"UART initialization timed out.", err);
+				return -EIO;
+			}
+		}
+	} while (err);
+	/* Register async handling callback */
+	err = uart_callback_set(uart_dev, uart_callback, NULL);
+	if (err) {
+		LOG_ERR("Cannot set callback: %d", err);
+		return -EFAULT;
+	}
+	/* Power on UART module */
+	device_set_power_state(uart_dev, DEVICE_PM_ACTIVE_STATE,
+				NULL, NULL);
+	err = uart_rx_enable(uart_dev, &uart_rx_buf[0], 1, K_FOREVER);
+	if (err) {
+		LOG_ERR("Cannot enable rx: %d", err);
+		return -EFAULT;
+	}
+	buf_num = 1U;
 
 	err = at_notif_register_handler(NULL, response_handler);
-	if (err != 0) {
+	if (err) {
 		LOG_ERR("Can't register handler err=%d", err);
 		return err;
 	}
-
-	/* Initialize the UART module */
-	err = at_uart_init(uart_dev_name);
-	if (err) {
-		LOG_ERR("UART can not be initialized: %d", err);
-		return -EFAULT;
-	}
-
-	k_work_init(&cmd_send_work, cmd_send);
-	uart_irq_rx_enable(uart_dev);
-
-	write_uart_string(SLM_SYNC_STR, sizeof(SLM_SYNC_STR)-1);
 
 	err = slm_at_tcpip_init(slm_at_callback);
 	if (err) {
@@ -399,6 +480,10 @@ int slm_at_host_init(void)
 		LOG_ERR("GPS could not be initialized: %d", err);
 		return -EFAULT;
 	}
+
+	k_work_init(&cmd_send_work, cmd_send);
+	k_sem_give(&tx_done);
+	write_uart_string(SLM_SYNC_STR, sizeof(SLM_SYNC_STR)-1);
 
 	LOG_DBG("at_host init done");
 	return err;
@@ -421,15 +506,18 @@ void slm_at_host_uninit(void)
 		LOG_WRN("GPS could not be uninitialized: %d", err);
 	}
 	err = at_notif_deregister_handler(NULL, response_handler);
-	if (err != 0) {
-		LOG_WRN("Can't deregister handler err=%d", err);
+	if (err) {
+		LOG_WRN("Can't deregister handler: %d", err);
 	}
-#if defined(CONFIG_DEVICE_POWER_MANAGEMENT)
+
+	/* Power off UART module */
+	uart_rx_disable(uart_dev);
+	k_sleep(100);
 	err = device_set_power_state(uart_dev, DEVICE_PM_OFF_STATE,
 				NULL, NULL);
-	if (err != 0) {
-		LOG_WRN("Can't power off uart err=%d", err);
+	if (err) {
+		LOG_WRN("Can't power off uart: %d", err);
 	}
-#endif
+
 	LOG_DBG("at_host uninit done");
 }
