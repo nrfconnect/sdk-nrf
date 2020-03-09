c97869bd34cd6427196d172a9b9897eb838b6ac7
Marek Pieta <Marek.Pieta@nordicsemi.no>
2020-03-09 01:55:03 -0700

    scripts: hid_configurator: Update GUI to use dynamic IDs
    Change updates GUI to use dynamic module and option IDs.
    
    Jira:DESK-593
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- scripts/hid_configurator/gui/gui_backend.py
+++ scripts/hid_configurator/gui/gui_backend.py
@@ -8,11 +8,11 @@ import time
 
 import hid
 
-# configurator_core.py is in upper directory so we add it to path
+# NrfHidDevice.py is in upper directory so we add it to path
 sys.path.append('..')
 from devices import DEVICE
 from devices import get_device_pid, get_device_vid, get_device_type
-from configurator_core import open_device
+from NrfHidDevice import NrfHidDevice
 from modules.dfu import get_dfu_image_version, fwinfo, fwreboot, dfu_transfer
 from modules.config import fetch_config, change_config
 
@@ -20,8 +20,10 @@ from modules.config import fetch_config, change_config
 class Device:
     def __init__(self, device_type):
         self.type = device_type
-        self.pid = get_device_pid(device_type)
-        self.dev = open_device(device_type)
+        self.dev = NrfHidDevice(device_type,
+                                get_device_vid(device_type),
+                                get_device_pid(device_type),
+                                get_device_pid('dongle'))
 
     @staticmethod
     def list_devices():
@@ -37,54 +39,49 @@ class Device:
         return device_list
 
     def perform_fwinfo(self):
-        return fwinfo(self.dev, self.pid)
+        return fwinfo(self.dev)
 
     def perform_dfu_fwreboot(self, update_animation):
-        fwreboot(self.dev, self.pid)
-        self.dev.close()
+        fwreboot(self.dev)
 
         WAIT_TIME_MAX = 60
         CHECK_PERIOD = 0.5
         start_time = time.time()
 
         # Wait until device turns off
-        while self.dev is not None:
-            self.dev.close()
-            self.dev = open_device(self.type)
+        while self.dev.initialized():
+            self.dev.close_device()
+            self.dev = NrfHidDevice(self.type,
+                                    get_device_vid(self.type),
+                                    get_device_pid(self.type),
+                                    get_device_pid('dongle'))
             time.sleep(CHECK_PERIOD)
 
-        while self.dev is None:
+        while not self.dev.initialized():
             update_animation()
-            self.dev = open_device(self.type)
+            self.dev = NrfHidDevice(self.type,
+                                    get_device_vid(self.type),
+                                    get_device_pid(self.type),
+                                    get_device_pid('dongle'))
             if time.time() - start_time > WAIT_TIME_MAX:
                 break
             time.sleep(CHECK_PERIOD)
 
-        if self.dev is not None:
+        if self.dev.initialized():
             print('DFU completed')
         else:
             print('Cannot connect to device after reboot')
-        return self.dev is not None
+
+        return self.dev.initialized()
 
     def setcpi(self, value):
-        config_name = 'cpi'
-        module_config = DEVICE[self.type]['config']['sensor']
-        options = module_config['options']
-        module_id = module_config['id']
-        recipient = self.pid
-        dev = self.dev
-        config_value = int(value)
-        success = change_config(dev, recipient, config_name, config_value, options, module_id)
+        opt_config = DEVICE[self.type]['config']['sensor']['options']['cpi']
+        success = change_config(self.dev, 'sensor', 'cpi', value, opt_config)
         return success
 
     def fetchcpi(self):
-        config_name = 'cpi'
-        module_config = DEVICE[self.type]['config']['sensor']
-        options = module_config['options']
-        module_id = module_config['id']
-        recipient = self.pid
-        dev = self.dev
-        success, val = fetch_config(dev, recipient, config_name, options, module_id)
+        opt_config = DEVICE[self.type]['config']['sensor']['options']['cpi']
+        success, val = fetch_config(self.dev, 'sensor', 'cpi', opt_config)
         if success:
             return val
         else:
@@ -93,7 +90,7 @@ class Device:
 
     def perform_dfu(self, file, update_progressbar):
         dfu_image = file
-        return dfu_transfer(self.dev, self.pid, dfu_image, update_progressbar)
+        return dfu_transfer(self.dev, dfu_image, update_progressbar)
 
     @staticmethod
     def dfu_image_version(filepath):
