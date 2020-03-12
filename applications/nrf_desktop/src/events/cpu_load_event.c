96c0463623f9966631ae671586e9579676b17e92
Marek Pieta <Marek.Pieta@nordicsemi.no>
2020-03-12 02:42:35 -0700

    applications: nrf_desktop: Add CPU load event
    Change adds event informing about the current CPU load.
    
    Jira:DESK-726
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/nrf_desktop/src/events/cpu_load_event.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+
+#include <stdio.h>
+
+#include "cpu_load_event.h"
+
+
+static int log_cpu_load_event(const struct event_header *eh, char *buf,
+			      size_t buf_len)
+{
+	const struct cpu_load_event *event = cast_cpu_load_event(eh);
+
+	return snprintf(buf, buf_len, "CPU load: %03u,%03u%%",
+			event->load / 1000, event->load % 1000);
+}
+
+static void profile_cpu_load_event(struct log_event_buf *buf,
+				   const struct event_header *eh)
+{
+	const struct cpu_load_event *event = cast_cpu_load_event(eh);
+
+	profiler_log_encode_u32(buf, event->load);
+}
+
+EVENT_INFO_DEFINE(cpu_load_event,
+		  ENCODE(PROFILER_ARG_U32),
+		  ENCODE("load"),
+		  profile_cpu_load_event);
+
+EVENT_TYPE_DEFINE(cpu_load_event,
+		  IS_ENABLED(CONFIG_DESKTOP_INIT_LOG_CPU_LOAD_EVENT),
+		  log_cpu_load_event,
+		  &cpu_load_event_info);
---
+++ applications/nrf_desktop/src/events/cpu_load_event.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#ifndef _CPU_LOAD_EVENT_H_
+#define _CPU_LOAD_EVENT_H_
+
+/**
+ * @brief CPU Load Event
+ * @defgroup cpu_load_event CPU Load Event
+ * @{
+ */
+
+#include "event_manager.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @brief CPU load event. */
+struct cpu_load_event {
+	struct event_header header; /**< Event header. */
+
+	u32_t load; /**< CPU load [in 0,001% units]. */
+};
+
+EVENT_TYPE_DECLARE(cpu_load_event);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _CPU_LOAD_EVENT_H_ */
