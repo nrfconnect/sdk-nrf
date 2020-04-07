3d7eed90911adcac84fcf34f09d63235963a33ab
Thomas Stilwell <Thomas.Stilwell@nordicsemi.no>
2020-04-07 16:27:58 +0200

    ci: adding requirements-ci.sh/txt
    static pip for ci/test
    
    Signed-off-by: Thomas Stilwell <Thomas.Stilwell@nordicsemi.no>

---
+++ scripts/requirements-ci.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# This script merges the py-reqs from mcuboot, zephyr, and nrf.
+# Then generates a frozen requirments-ci.txt for tests to run with.
+
+#OPTIONAL
+OUT_FILE=$1
+if [ -z "$OUT_FILE" ]; then
+    OUT_FILE=nrf/scripts/requirements-ci.txt
+fi
+echo "Writing frozen requirements to: $OUT_FILE"
+
+
+TOPDIR=$(west topdir)
+cd $TOPDIR
+
+
+source ~/.local/bin/virtualenvwrapper.sh
+
+rmvirtualenv pip-freeze-ci-venv
+mkvirtualenv pip-freeze-ci-venv
+workon pip-freeze-ci-venv
+
+pip3 install --isolated \
+    -r bootloader/mcuboot/scripts/requirements.txt \
+    -r zephyr/scripts/requirements.txt  \
+    -r nrf/scripts/requirements.txt
+pip3 freeze > $OUT_FILE
+
+deactivate
+rmvirtualenv pip-freeze-ci-venv
