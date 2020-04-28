6a99293744bd0f4233b4bfe2a2a47c9d652cb1a3
Martí Bolívar <marti.bolivar@nordicsemi.no>
2020-04-27 21:20:27 -0700

    tree-wide: adjust CONFIG_ENTROPY_NAME usages
    This is gone now; the chosen zephyr,entropy device is the replacement.
    Get this label from the common test code.
    
    In the case of entropy_ll_nrfxlib.c, I'm assuming this device
    instantiation was because the driver is intended to be the entropy
    device exactly when it's compiled in, so just name the device
    according to the chosen node. Hopefully that is OK.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- tests/crypto/src/common_test.c
+++ tests/crypto/src/common_test.c
@@ -61,7 +61,8 @@ int init_drbg(const unsigned char *p_optional_seed, size_t len)
 		p_seed = p_optional_seed;
 	}
 
-	struct device *p_device = device_get_binding(CONFIG_ENTROPY_NAME);
+	struct device *p_device = device_get_binding(
+		DT_LABEL(DT_CHOSEN(zephyr_entropy)));
 	if (p_device == NULL) {
 		return -ENODEV;
 	}
