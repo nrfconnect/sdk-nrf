900b2b25903682f077b1318411038d49bfb0cfd4
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2020-04-28 11:38:28 +0200

    bluetooth/controller: align bt_read_static_addr() to upsteram
    API of bt_read_static_addr() was extended upsteram.
    This patch align nordic implementation.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- subsys/bluetooth/controller/hci_driver.c
+++ subsys/bluetooth/controller/hci_driver.c
@@ -444,19 +444,20 @@ static const struct bt_hci_driver drv = {
 	.send = hci_driver_send,
 };
 
-uint8_t bt_read_static_addr(struct bt_hci_vs_static_addr *addr)
+uint8_t bt_read_static_addr(struct bt_hci_vs_static_addr addrs[], uint8_t size)
 {
-	if (((NRF_FICR->DEVICEADDR[0] != UINT32_MAX) ||
-	    ((NRF_FICR->DEVICEADDR[1] & UINT16_MAX) != UINT16_MAX)) &&
-	     (NRF_FICR->DEVICEADDRTYPE & 0x01)) {
-		sys_put_le32(NRF_FICR->DEVICEADDR[0], &addr->bdaddr.val[0]);
-		sys_put_le16(NRF_FICR->DEVICEADDR[1], &addr->bdaddr.val[4]);
+	if ((size > 0) &&
+	    (((NRF_FICR->DEVICEADDR[0] != UINT32_MAX) ||
+	     ((NRF_FICR->DEVICEADDR[1] & UINT16_MAX) != UINT16_MAX)) &&
+	      (NRF_FICR->DEVICEADDRTYPE & 0x01))) {
+		sys_put_le32(NRF_FICR->DEVICEADDR[0], &addrs->bdaddr.val[0]);
+		sys_put_le16(NRF_FICR->DEVICEADDR[1], &addrs->bdaddr.val[4]);
 
 		/* The FICR value is a just a random number, with no knowledge
 		 * of the Bluetooth Specification requirements for random
 		 * static addresses.
 		 */
-		BT_ADDR_SET_STATIC(&addr->bdaddr);
+		BT_ADDR_SET_STATIC(&addrs->bdaddr);
 
 		/* If no public address is provided and a static address is
 		 * available, then it is recommended to return an identity root
@@ -466,13 +467,13 @@ uint8_t bt_read_static_addr(struct bt_hci_vs_static_addr *addr)
 		    (NRF_FICR->IR[1] != UINT32_MAX) &&
 		    (NRF_FICR->IR[2] != UINT32_MAX) &&
 		    (NRF_FICR->IR[3] != UINT32_MAX)) {
-			sys_put_le32(NRF_FICR->IR[0], &addr->ir[0]);
-			sys_put_le32(NRF_FICR->IR[1], &addr->ir[4]);
-			sys_put_le32(NRF_FICR->IR[2], &addr->ir[8]);
-			sys_put_le32(NRF_FICR->IR[3], &addr->ir[12]);
+			sys_put_le32(NRF_FICR->IR[0], &addrs->ir[0]);
+			sys_put_le32(NRF_FICR->IR[1], &addrs->ir[4]);
+			sys_put_le32(NRF_FICR->IR[2], &addrs->ir[8]);
+			sys_put_le32(NRF_FICR->IR[3], &addrs->ir[12]);
 		} else {
 			/* Mark IR as invalid */
-			(void)memset(addr->ir, 0x00, sizeof(addr->ir));
+			(void)memset(addrs->ir, 0x00, sizeof(addrs->ir));
 		}
 
 		return 1;
