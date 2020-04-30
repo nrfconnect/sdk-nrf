23502f8a84a56ca6bc033044c2f7b02eb6f689c8
Marek Porwisz <marek.porwisz@nordicsemi.no>
2020-04-30 12:16:52 +0200

    samples: openthread: Create the very basic thread CLI sample
    Thread CLI sample in zephyr does not have much code and quits
    the main function right after start but still it is the important one.
    Thread Command Line Interface is already integrated into the
    Zephyr shell and it only requires proper configuration in the
    prj.conf file.
    This sample is also needed for thread certification for cmd line
    access without additional unwanted trafic.
    
    Signed-off-by: Marek Porwisz <marek.porwisz@nordicsemi.no>

---
+++ samples/openthread/ot_cli/src/main.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+#include <zephyr.h>
+#include <logging/log.h>
+
+LOG_MODULE_REGISTER(cli_sample, CONFIG_OT_COMMAND_LINE_INTERFACE_LOG_LEVEL);
+
+#define WELLCOME_TEXT \
+	"\n\r"\
+	"\n\r"\
+	"OpenThread Command Line Interface is now running.\n\r" \
+	"Use the 'ot' keyword to invoke OpenThread commands e.g. " \
+	"'ot thread start.'\n\r" \
+	"For the full commands list refer to the OpenThread CLI " \
+	"documentation at:\n\r" \
+	"https://github.com/openthread/openthread/blob/master/src/cli/README.md\n\r"
+
+
+void main(void)
+{
+	LOG_INF(WELLCOME_TEXT);
+	/* nothing to do for the app, so let's return */
+}
