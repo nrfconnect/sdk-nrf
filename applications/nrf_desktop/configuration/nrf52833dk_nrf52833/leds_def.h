3695416585aa00a2e673034f1182566bea157a52
Joakim Andersson <joakim.andersson@nordicsemi.no>
2020-05-15 11:09:30 +0200

    applications: nrf_desktop: Convert buttons and LEDs to new DT macros
    Convert button and LED definitions to the new device tree macros.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- applications/nrf_desktop/configuration/nrf52833dk_nrf52833/leds_def.h
+++ applications/nrf_desktop/configuration/nrf52833dk_nrf52833/leds_def.h
@@ -17,9 +17,9 @@ const struct {} leds_def_include_once;
 static const size_t led_pins[CONFIG_DESKTOP_LED_COUNT]
 			    [CONFIG_DESKTOP_LED_COLOR_COUNT] = {
 	{
-		DT_NORDIC_NRF_PWM_PWM_0_CH0_PIN,
+		DT_PROP(DT_ALIAS(pwm_0), ch0_pin),
 	},
 	{
-		DT_NORDIC_NRF_PWM_PWM_1_CH0_PIN,
+		DT_PROP(DT_ALIAS(pwm_1), ch0_pin),
 	}
 };
