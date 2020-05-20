206970cf55c4c7acf324000a8ff4b3c5249ef997
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2020-05-20 12:08:23 +0200

    drivers: Adapt optical sensor drivers to new timer API
    Jira:DESK-945
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

--- drivers/sensor/paw3212/paw3212.c
+++ drivers/sensor/paw3212/paw3212.c
@@ -664,7 +664,7 @@ static void paw3212_async_init(struct k_work *work)
 			LOG_INF("PAW3212 initialized");
 		} else {
 			k_delayed_work_submit(&dev_data->init_work,
-					      async_init_delay[dev_data->async_init_step]);
+					      K_MSEC(async_init_delay[dev_data->async_init_step]));
 		}
 	}
 }
@@ -764,7 +764,7 @@ static int paw3212_init(struct device *dev)
 	k_delayed_work_init(&dev_data->init_work, paw3212_async_init);
 
 	k_delayed_work_submit(&dev_data->init_work,
-			      async_init_delay[dev_data->async_init_step]);
+			      K_MSEC(async_init_delay[dev_data->async_init_step]));
 
 	return err;
 }
