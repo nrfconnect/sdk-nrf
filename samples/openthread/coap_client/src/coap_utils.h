2e06e86281c39599dde65769fc36c9b5fe30eaef
Lukasz Maciejonczyk <lukasz.maciejonczyk@nordicsemi.no>
2020-05-20 09:36:48 +0200

    samples: openthread: coap_utils refactoring
    Simplify the code. Add API comments.
    
    Signed-off-by: Lukasz Maciejonczyk <lukasz.maciejonczyk@nordicsemi.no>

--- samples/openthread/coap_client/src/coap_utils.h
+++ samples/openthread/coap_client/src/coap_utils.h
@@ -1,12 +1,41 @@
+/**
+ * @file
+ * @defgroup coap_utils CoAP Zephyr socket based API
+ * @{
+ */
+
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
 #ifndef __COAP_UTILS_H__
 #define __COAP_UTILS_H__
 
 #include <net/coap.h>
 
+/** @brief Initialize CoAP utilities.
+ */
 void coap_init(void);
 
+/** @brief Send CoAP non-confirmable request.
+ *
+ * @param[in] method           CoAP method type.
+ * @param[in] addr6            pointer to socket address struct for IPv6.
+ * @param[in] uri_path_options pointer to CoAP URI schemes option.
+ * @param[in] payload          pointer to the CoAP message payload.
+ * @param[in] payload_size     size of the CoAP message payload.
+ * @param[in] reply_cb         function to call when the response comes.
+ *
+ * @retval 0    On success.
+ * @retval != 0 On failure.
+ */
 int coap_send_request(enum coap_method method, const struct sockaddr_in6 *addr6,
 		      const char *const *uri_path_options, u8_t *payload,
 		      u16_t payload_size, coap_reply_t reply_cb);
 
 #endif
+
+/**
+ * @}
+ */
