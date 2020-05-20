e3bab2b4d816304743831585e883c8935ac5f163
Lukasz Maciejonczyk <lukasz.maciejonczyk@nordicsemi.no>
2020-05-20 11:33:20 +0200

    lib: Separate CoAP utils library
    Create a library from CoAP utils file. It provides sending and receiving
    non-confirmable CoAP requests. It utilizes Zephyr CoAP and BSD Sockets
    libraries.
    
    Signed-off-by: Lukasz Maciejonczyk <lukasz.maciejonczyk@nordicsemi.no>

--- include/net/coap_utils.h
+++ include/net/coap_utils.h
@@ -1,6 +1,6 @@
 /**
  * @file
- * @defgroup coap_utils CoAP Zephyr socket based API
+ * @defgroup coap_utils CoAP communication BSD socket API based
  * @{
  */
 
@@ -13,15 +13,18 @@
 #define __COAP_UTILS_H__
 
 #include <net/coap.h>
+#include <net/net_ip.h>
 
-/** @brief Initialize CoAP utilities.
+/** @brief Open socket and start the receiving thread.
+ *
+ * @param[in] ip_family network ip protocol family (AF_INET or AF_INET6)
  */
-void coap_init(void);
+void coap_init(int ip_family);
 
 /** @brief Send CoAP non-confirmable request.
  *
  * @param[in] method           CoAP method type.
- * @param[in] addr6            pointer to socket address struct for IPv6.
+ * @param[in] addr             pointer to socket address struct for IPv6.
  * @param[in] uri_path_options pointer to CoAP URI schemes option.
  * @param[in] payload          pointer to the CoAP message payload.
  * @param[in] payload_size     size of the CoAP message payload.
@@ -30,7 +33,7 @@ void coap_init(void);
  * @retval 0    On success.
  * @retval != 0 On failure.
  */
-int coap_send_request(enum coap_method method, const struct sockaddr_in6 *addr6,
+int coap_send_request(enum coap_method method, const struct sockaddr *addr,
 		      const char *const *uri_path_options, u8_t *payload,
 		      u16_t payload_size, coap_reply_t reply_cb);
 
