4e9e85955d029c48e9a8a2e7e22820c02491ead6
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2020-05-20 12:03:17 +0200

    applications: nrf_desktop: Adapt to DTS changes
    Jira:DESK-945
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

--- applications/nrf_desktop/configuration/nrf52833dongle_nrf52833/port_state_def.h
+++ applications/nrf_desktop/configuration/nrf52833dongle_nrf52833/port_state_def.h
@@ -19,8 +19,8 @@ const struct {} port_state_def_include_once;
 
 
 static const struct pin_state port0_on[] = {
-	{DT_GPIO_LEDS_LED1_RED_GPIOS_PIN, 1},
-	{DT_GPIO_LEDS_LED1_GREEN_GPIOS_PIN, 1},
+	{DT_GPIO_PIN(DT_ALIAS(led1), gpios), 1},
+	{DT_GPIO_PIN(DT_ALIAS(led2), gpios), 1}
 };
 
 static const struct pin_state port1_on[] = {
