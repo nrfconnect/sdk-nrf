4d509ada7fec813a78bd75e47beadb5c755f275a
Kamil Gawor <Kamil.Gawor@nordicsemi.no>
2020-05-21 14:49:40 +0200

    tnep: Change the order of The Initial NDEF Message
    The TNEP service select record and standard NDEF record
    according to TNEP specification can be placed in default
    order. In case of the Adroid phones which does not supported
    TNEP, when TNEP Service Select Record is first the standard
    record are not recognised.
    
    Signed-off-by: Kamil Gawor <Kamil.Gawor@nordicsemi.no>

--- subsys/nfc/tnep/tag.c
+++ subsys/nfc/tnep/tag.c
@@ -184,19 +184,17 @@ static int tnep_tx_initial_msg_set(void)
 
 	tnep_tx_msg_clear();
 
-	for (size_t i = 0; i < tnep.svc_cnt; i++) {
-		err = tnep_tx_msg_add_rec(tnep.svc[i].ndef_record);
-		if (err) {
-			return err;
+	if (tnep.records && tnep.records_cnt) {
+		for (size_t i = 0; i < tnep.records_cnt; i++) {
+			err = tnep_tx_msg_add_rec(&tnep.records[i]);
+			if (err) {
+				return err;
+			}
 		}
 	}
 
-	if (!tnep.records) {
-		return tnep_tx_msg_encode();
-	}
-
-	for (size_t i = 0; i < tnep.records_cnt; i++) {
-		err = tnep_tx_msg_add_rec(&tnep.records[i]);
+	for (size_t i = 0; i < tnep.svc_cnt; i++) {
+		err = tnep_tx_msg_add_rec(tnep.svc[i].ndef_record);
 		if (err) {
 			return err;
 		}
