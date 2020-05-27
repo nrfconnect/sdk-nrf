0392659e0039b0ca13a0f0689d92db44ebe9e3b2
Håvard Vermeer <havard.vermeer@nordicsemi.no>
2020-05-27 08:35:58 +0200

    lib: bin: lwm2m_carrier: update to version 0.9.0
    Update library to version 0.9.0.
    
    Signed-off-by: Håvard Vermeer <havard.vermeer@nordicsemi.no>
    Signed-off-by: Uma Praseeda <uma.praseeda@nordicsemi.no>
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>

--- samples/nrf9160/lwm2m_carrier/src/main.c
+++ samples/nrf9160/lwm2m_carrier/src/main.c
@@ -25,12 +25,53 @@ void print_err(const lwm2m_carrier_event_t *evt)
 			"Failed to connect to the LTE network",
 		[LWM2M_CARRIER_ERROR_DISCONNECT_FAIL] =
 			"Failed to disconnect from the LTE network",
+		[LWM2M_CARRIER_ERROR_FOTA_PKG] =
+			"Package refused from modem",
+		[LWM2M_CARRIER_ERROR_FOTA_PROTO] =
+			"Protocol error",
+		[LWM2M_CARRIER_ERROR_FOTA_CONN] =
+			"Connection to remote server failed",
+		[LWM2M_CARRIER_ERROR_FOTA_CONN_LOST] =
+			"Connection to remote server lost",
+		[LWM2M_CARRIER_ERROR_FOTA_FAIL] =
+			"Modem firmware update failed",
 	};
 
 	__ASSERT(PART_OF_ARRAY(strerr[err->code]),
 		 "Unhandled liblwm2m_carrier error");
 
 	printk("%s, reason %d\n", strerr[err->code], err->value);
+}
+
+void print_deferred(const lwm2m_carrier_event_t *evt)
+{
+	const lwm2m_carrier_event_deferred_t *def =
+		(lwm2m_carrier_event_deferred_t *)evt->data;
+
+	static const char *strdef[] = {
+		[LWM2M_CARRIER_DEFERRED_NO_REASON] =
+			"No reason given",
+		[LWM2M_CARRIER_DEFERRED_PDN_ACTIVATE] =
+			"Failed to activate PDN",
+		[LWM2M_CARRIER_DEFERRED_BOOTSTRAP_NO_ROUTE] =
+			"No route to bootstrap server",
+		[LWM2M_CARRIER_DEFERRED_BOOTSTRAP_CONNECT] =
+			"Failed to connect to bootstrap server",
+		[LWM2M_CARRIER_DEFERRED_BOOTSTRAP_SEQUENCE] =
+			"Bootstrap sequence not completed",
+		[LWM2M_CARRIER_DEFERRED_SERVER_NO_ROUTE] =
+			"No route to server",
+		[LWM2M_CARRIER_DEFERRED_SERVER_CONNECT] =
+			"Failed to connect to server",
+		[LWM2M_CARRIER_DEFERRED_SERVER_REGISTRATION] =
+			"Server registration sequence not completed",
+	};
+
+	__ASSERT(PART_OF_ARRAY(strdef[def->reason]),
+		 "Unhandled deferred reason");
+
+	printk("Reason: %s, timeout: %d seconds\n", strdef[def->reason],
+		def->timeout);
 }
 
 int lwm2m_carrier_event_handler(const lwm2m_carrier_event_t *event)
@@ -59,6 +100,7 @@ int lwm2m_carrier_event_handler(const lwm2m_carrier_event_t *event)
 		break;
 	case LWM2M_CARRIER_EVENT_DEFERRED:
 		printk("LWM2M_CARRIER_EVENT_DEFERRED\n");
+		print_deferred(event);
 		break;
 	case LWM2M_CARRIER_EVENT_FOTA_START:
 		printk("LWM2M_CARRIER_EVENT_FOTA_START\n");
