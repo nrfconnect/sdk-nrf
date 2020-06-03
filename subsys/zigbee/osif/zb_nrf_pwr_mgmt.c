09c50dc622e6d80d77dcba0f3efbea0f267605d7
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2020-06-03 16:21:02 +0200

    zigbee: Allow to build Zigbee apps without TRACEs
    ZBOSS traces are disabled if ZB_TRACE_LEVEL is not defined.
    In such case, the API for setting TRACE mask and level is not
    available.
    Additionally, the API to enable/disable interrupts is defined
    in TRACE subsystem, so a small reordering of functions  is required.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- subsys/zigbee/osif/zb_nrf_pwr_mgmt.c
+++ subsys/zigbee/osif/zb_nrf_pwr_mgmt.c
@@ -45,7 +45,10 @@ __weak zb_uint32_t zb_osif_sleep(zb_uint32_t sleep_tmo)
 	ZVUNUSED(time_slept_ms);
 	return ZB_SLEEP_INVALID_VALUE;
 #else
+
+#if ZB_TRACE_LEVEL
 	ZB_SET_TRACE_OFF();
+#endif /* ZB_TRACE_LEVEL */
 
 	/* Lock timer value from updating during sleep period. */
 	ZVUNUSED(atomic_set((atomic_t *)&is_sleeping, 1));
@@ -72,7 +75,9 @@ __weak zb_uint32_t zb_osif_sleep(zb_uint32_t sleep_tmo)
  */
 __weak zb_void_t zb_osif_wake_up(void)
 {
+#if ZB_TRACE_LEVEL
 	ZB_SET_TRACE_ON();
+#endif /* ZB_TRACE_LEVEL */
 	/* Restore trace interrupts. TODO: Restore something else if needed */
 }
 
