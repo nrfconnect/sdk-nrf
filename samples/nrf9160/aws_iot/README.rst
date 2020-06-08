ac553027fc0d7fa97aec9ed9edb8b6cb99ad1ac3
Simen S. Røstad <simen.rostad@nordicsemi.no>
2020-06-08 12:20:12 +0200

    samples: nrf9160: Added AWS IoT sample
    This patch adds a sample that showcases the functionality
    of the AWS IoT library and Date time library. The sample
    publishes customizable and timestamped data sequentially
    to the AWS IoT message broker and supports application
    and modem FOTA DFU.
    TG91-270
    
    Signed-off-by: Simen S. Røstad <simen.rostad@nordicsemi.no>

---
+++ samples/nrf9160/aws_iot/README.rst
@@ -0,0 +1,153 @@
+.. _aws_iot:
+
+nRF9160: AWS IoT
+################
+
+The AWS IoT sample shows the communication of an nRF9160-based device with the AWS IoT message broker over MQTT.
+This sample uses the :ref:`lib_aws_iot` library.
+
+Overview
+********
+
+The sample publishes two different types of messages in `JSON`_ object string format to the AWS IoT message broker.
+These messages are sent to the AWS IoT shadow topic ``$aws/things/<thingname>/shadow/update``.
+
+Below are the two types of messages that are published:
+
+* Type 1: The message comprises of a battery voltage value sampled from the nRF9160 SiP and a corresponding timestamp in milliseconds (UNIX time), that is retrieved from the :ref:`lib_date_time` library.
+
+* Type 2: The message adds a configurable firmware version number to type 1 messages. This firmware version number is used in correlation with FOTA DFU, which is supported by the sample and the :ref:`lib_aws_iot` library.
+
+A type 2 message is only published upon an initial connection to the sample, while a type 1 message is published sequentially with a configurable time in between each publishing of the data.
+In addition to publishing data, the sample also subscribes to the AWS IoT shadow delta topic, and two customizable application specific topics.
+The customizable topics are not part of the AWS IoT shadow and must therefore be passed to the :ref:`lib_aws_iot` library using the :cpp:func:`aws_iot_subscription_topics_add` function.
+
+Requirements
+************
+
+The sample supports the following development kits:
+
+.. include:: /includes/boardname_tables/sample_boardnames.txt
+   :start-after: set6_start
+   :end-before: set6_end
+
+.. include:: /includes/spm.txt
+
+.. _setup_awsiot:
+
+Setup
+*****
+
+Before starting this sample, you should complete the following steps that are described in the :ref:`lib_aws_iot` documentation:
+
+1. `Setting up an AWS account`_
+#. :ref:`set_up_conn_to_iot`
+#. :ref:`Flashing device certificates <flash_certi_device>`
+#. :ref:`Configuring the sample options <configure_options>`
+
+For FOTA DFU related documentation, see :ref:`aws_fota_sample`.
+
+.. _configure_options:
+
+Configuration
+*************
+
+The configurations used in the sample are listed below. They are located in ``aws_iot/src/prj.conf``.
+
+.. option:: CONFIG_APP_VERSION
+
+Publishes the application version number to the AWS IoT message broker.
+
+.. option:: CONFIG_PUBLICATION_INTERVAL_SECONDS
+
+Configures the time interval between each publishing of the message.
+
+Building and running
+********************
+
+.. |sample path| replace:: :file:`samples/nrf9160/aws_iot`
+.. include:: /includes/build_and_run.txt
+.. include:: /includes/spm.txt
+
+Testing
+=======
+
+1. Make sure that you have completed the steps in :ref:`setup_awsiot`.
+   This retrieves the AWS IoT broker hostname, security tag and client-id.
+
+#. Set the :option:`CONFIG_AWS_IOT_BROKER_HOST_NAME`, :option:`CONFIG_AWS_IOT_SEC_TAG`, and :option:`CONFIG_AWS_IOT_CLIENT_ID_STATIC` options to reflect the values retrieved during step 1.
+#. Flash the sample.
+
+.. note::
+
+   The sample might require increasing the values of :option:`CONFIG_AWS_IOT_MQTT_RX_TX_BUFFER_LEN` and :option:`CONFIG_AWS_IOT_MQTT_PAYLOAD_BUFFER_LEN` options.
+
+Sample Output
+=============
+
+When the sample runs, the device boots, and the sample displays the following output in the terminal over UART:
+
+.. code-block:: console
+
+        *** Booting Zephyr OS build v2.3.0-rc1-ncs1-snapshot1-6-gad0444b058ef  ***
+        AWS IoT sample started, version: v1.0.0
+        LTE cell changed: Cell ID: 33703711, Tracking area: 2305
+        PSM parameter update: TAU: -1, Active time: -1
+        RRC mode: Connected
+        Network registration status: Connected - roaming
+        PSM parameter update: TAU: 3600, Active time: 60
+        RRC mode: Idle
+        AWS_IOT_EVT_CONNECTING
+        RRC mode: Connected
+        RRC mode: Idle
+        RRC mode: Connected
+        AWS_IOT_EVT_CONNECTED
+        Publishing: {
+        "state":    {
+                "reported":    {
+                "app_version":    "v1.0.0",
+                "batv":    4304,
+                "ts":    1592305354579
+                }
+        }
+        } to AWS IoT broker
+        AWS_IOT_EVT_READY
+        RRC mode: Idle
+        LTE cell changed: Cell ID: 34237195, Tracking area: 2305
+        Publishing: {
+        "state":    {
+                "reported":    {
+                "batv":    4308,
+                "ts":    1592305414579
+                }
+        }
+        } to AWS IoT broker
+        Next data publication in 60 seconds
+        RRC mode: Connected
+        RRC mode: Idle
+        LTE cell changed: Cell ID: 33703711, Tracking area: 2305
+        RRC mode: Connected
+        Publishing: {
+        "state":    {
+                "reported":    {
+                "batv":    4308,
+                "ts":    1592305474596
+                }
+        }
+        } to AWS IoT broker
+        Next data publication in 60 seconds
+
+Dependencies
+************
+
+This sample uses the following |NCS| libraries and drivers:
+
+* :ref:`lib_aws_iot`
+* :ref:`lib_date_time`
+* ``lib/bsd_lib``
+* ``lib/lte_link_control``
+* ``lib/modem_info``
+
+In addition, it uses the Secure Partition Manager sample:
+
+* :ref:`secure_partition_manager`
---
+++ samples/nrf9160/aws_iot/src/main.c
@@ -0,0 +1,430 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#include <zephyr.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <modem/lte_lc.h>
+#include <modem/bsdlib.h>
+#include <modem/at_cmd.h>
+#include <modem/at_notif.h>
+#include <net/aws_iot.h>
+#include <power/reboot.h>
+#include <date_time.h>
+#include <dfu/mcuboot.h>
+#include <bsd.h>
+#include <cJSON.h>
+#include <cJSON_os.h>
+#include <modem/modem_info.h>
+
+BUILD_ASSERT(!IS_ENABLED(CONFIG_LTE_AUTO_INIT_AND_CONNECT),
+		"This sample does not support LTE auto-init and connect");
+
+#define APP_TOPICS_COUNT CONFIG_AWS_IOT_APP_SUBSCRIPTION_LIST_COUNT
+
+static struct k_delayed_work shadow_update_work;
+static struct k_delayed_work shadow_update_version_work;
+
+K_SEM_DEFINE(lte_connected, 0, 1);
+
+static int json_add_obj(cJSON *parent, const char *str, cJSON *item)
+{
+	cJSON_AddItemToObject(parent, str, item);
+
+	return 0;
+}
+
+static int json_add_str(cJSON *parent, const char *str, const char *item)
+{
+	cJSON *json_str;
+
+	json_str = cJSON_CreateString(item);
+	if (json_str == NULL) {
+		return -ENOMEM;
+	}
+
+	return json_add_obj(parent, str, json_str);
+}
+
+static int json_add_number(cJSON *parent, const char *str, double item)
+{
+	cJSON *json_num;
+
+	json_num = cJSON_CreateNumber(item);
+	if (json_num == NULL) {
+		return -ENOMEM;
+	}
+
+	return json_add_obj(parent, str, json_num);
+}
+
+static int shadow_update(bool version_number_include)
+{
+	int err;
+	char *message;
+	s64_t message_ts;
+	s16_t bat_voltage;
+
+	err = date_time_now(&message_ts);
+	if (err) {
+		printk("date_time_now, error: %d\n", err);
+		return err;
+	}
+
+	/* Request battery voltage data from the modem. */
+	err = modem_info_short_get(MODEM_INFO_BATTERY, &bat_voltage);
+	if (err != sizeof(bat_voltage)) {
+		printk("modem_info_short_get, error: %d\n", err);
+		return err;
+	}
+
+	cJSON *root_obj = cJSON_CreateObject();
+	cJSON *state_obj = cJSON_CreateObject();
+	cJSON *reported_obj = cJSON_CreateObject();
+
+	if (root_obj == NULL || state_obj == NULL || reported_obj == NULL) {
+		cJSON_Delete(root_obj);
+		cJSON_Delete(state_obj);
+		cJSON_Delete(reported_obj);
+		err = -ENOMEM;
+		return err;
+	}
+
+	if (version_number_include) {
+		err = json_add_str(reported_obj, "app_version",
+				    CONFIG_APP_VERSION);
+	} else {
+		err = 0;
+	}
+
+	err += json_add_number(reported_obj, "batv", bat_voltage);
+	err += json_add_number(reported_obj, "ts", message_ts);
+	err += json_add_obj(state_obj, "reported", reported_obj);
+	err += json_add_obj(root_obj, "state", state_obj);
+
+	if (err) {
+		printk("json_add, error: %d\n", err);
+		goto cleanup;
+	}
+
+	message = cJSON_Print(root_obj);
+	if (message == NULL) {
+		printk("cJSON_Print, error: returned NULL\n");
+		err = -ENOMEM;
+		goto cleanup;
+	}
+
+	struct aws_iot_data tx_data = {
+		.qos = MQTT_QOS_0_AT_MOST_ONCE,
+		.topic.type = AWS_IOT_SHADOW_TOPIC_UPDATE,
+		.ptr = message,
+		.len = strlen(message)
+	};
+
+	printk("Publishing: %s to AWS IoT broker\n", message);
+
+	err = aws_iot_send(&tx_data);
+	if (err) {
+		printk("aws_iot_send, error: %d\n", err);
+	}
+
+	k_free(message);
+
+cleanup:
+
+	cJSON_Delete(root_obj);
+
+	return err;
+}
+
+static void shadow_update_work_fn(struct k_work *work)
+{
+	int err;
+
+	err = shadow_update(false);
+	if (err) {
+		printk("shadow_update, error: %d\n", err);
+	}
+
+	printk("Next data publication in %d seconds\n",
+	       CONFIG_PUBLICATION_INTERVAL_SECONDS);
+
+	k_delayed_work_submit(&shadow_update_work,
+			      K_SECONDS(CONFIG_PUBLICATION_INTERVAL_SECONDS));
+}
+
+static void shadow_update_version_work_fn(struct k_work *work)
+{
+	int err;
+
+	err = shadow_update(true);
+	if (err) {
+		printk("shadow_update, error: %d\n", err);
+	}
+}
+
+void aws_iot_event_handler(const struct aws_iot_evt *const evt)
+{
+	int err;
+
+	switch (evt->type) {
+	case AWS_IOT_EVT_CONNECTING:
+		printk("AWS_IOT_EVT_CONNECTING\n");
+		break;
+	case AWS_IOT_EVT_CONNECTED:
+		printk("AWS_IOT_EVT_CONNECTED\n");
+
+		if (evt->data.persistent_session) {
+			printk("Persistent session enabled\n");
+		}
+
+		/** Successfully connected to AWS IoT broker, mark image as
+		 *  working to avoid reverting to the former image upon reboot.
+		 */
+		boot_write_img_confirmed();
+
+		/** Send version number to AWS IoT broker to verify that the
+		 *  FOTA update worked.
+		 */
+		k_delayed_work_submit(&shadow_update_version_work, K_NO_WAIT);
+
+		/** Start sequential shadow data updates.
+		 */
+		k_delayed_work_submit(&shadow_update_work,
+				K_SECONDS(CONFIG_PUBLICATION_INTERVAL_SECONDS));
+
+		err = lte_lc_psm_req(true);
+		if (err) {
+			printk("Requesting PSM failed, error: %d\n", err);
+		}
+		break;
+	case AWS_IOT_EVT_READY:
+		printk("AWS_IOT_EVT_READY\n");
+		break;
+	case AWS_IOT_EVT_DISCONNECTED:
+		printk("AWS_IOT_EVT_DISCONNECTED\n");
+		k_delayed_work_cancel(&shadow_update_work);
+		break;
+	case AWS_IOT_EVT_DATA_RECEIVED:
+		printk("AWS_IOT_EVT_DATA_RECEIVED\n");
+		break;
+	case AWS_IOT_EVT_FOTA_START:
+		printk("AWS_IOT_EVT_FOTA_START\n");
+		break;
+	case AWS_IOT_EVT_FOTA_ERASE_PENDING:
+		printk("AWS_IOT_EVT_FOTA_ERASE_PENDING\n");
+		printk("Disconnect LTE link or reboot\n");
+		err = lte_lc_offline();
+		if (err) {
+			printk("Error disconnecting from LTE\n");
+		}
+		break;
+	case AWS_IOT_EVT_FOTA_ERASE_DONE:
+		printk("AWS_FOTA_EVT_ERASE_DONE\n");
+		printk("Reconnecting the LTE link");
+		err = lte_lc_connect();
+		if (err) {
+			printk("Error connecting to LTE\n");
+		}
+		break;
+	case AWS_IOT_EVT_FOTA_DONE:
+		printk("AWS_IOT_EVT_FOTA_DONE\n");
+		printk("FOTA done, rebooting device\n");
+		aws_iot_disconnect();
+		sys_reboot(0);
+		break;
+	case AWS_IOT_EVT_ERROR:
+		printk("AWS_IOT_EVT_ERROR, %d\n", evt->data.err);
+		break;
+	default:
+		printk("Unknown AWS IoT event type: %d\n", evt->type);
+		break;
+	}
+}
+
+static void work_init(void)
+{
+	k_delayed_work_init(&shadow_update_work, shadow_update_work_fn);
+	k_delayed_work_init(&shadow_update_version_work,
+			    shadow_update_version_work_fn);
+}
+
+static void lte_handler(const struct lte_lc_evt *const evt)
+{
+	int err;
+
+	switch (evt->type) {
+	case LTE_LC_EVT_NW_REG_STATUS:
+		if ((evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
+		     (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)) {
+			break;
+		}
+
+		printk("Network registration status: %s\n",
+			evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ?
+			"Connected - home network" : "Connected - roaming");
+
+		err = lte_lc_psm_req(true);
+		if (err) {
+			printk("PSM request failed, error: %d", err);
+		}
+		k_sem_give(&lte_connected);
+		break;
+	case LTE_LC_EVT_PSM_UPDATE:
+		printk("PSM parameter update: TAU: %d, Active time: %d\n",
+			evt->psm_cfg.tau, evt->psm_cfg.active_time);
+		break;
+	case LTE_LC_EVT_EDRX_UPDATE: {
+		char log_buf[60];
+		ssize_t len;
+
+		len = snprintf(log_buf, sizeof(log_buf),
+			       "eDRX parameter update: eDRX: %f, PTW: %f",
+			       evt->edrx_cfg.edrx, evt->edrx_cfg.ptw);
+		if (len > 0) {
+			printk("%s\n", log_buf);
+		}
+		break;
+	}
+	case LTE_LC_EVT_RRC_UPDATE:
+		printk("RRC mode: %s\n",
+			evt->rrc_mode == LTE_LC_RRC_MODE_CONNECTED ?
+			"Connected" : "Idle");
+		break;
+	case LTE_LC_EVT_CELL_UPDATE:
+		printk("LTE cell changed: Cell ID: %d, Tracking area: %d\n",
+			evt->cell.id, evt->cell.tac);
+		break;
+	default:
+		break;
+	}
+}
+
+static void modem_configure(void)
+{
+	int err;
+
+	if (IS_ENABLED(CONFIG_LTE_AUTO_INIT_AND_CONNECT)) {
+		/* Do nothing, modem is already configured and LTE connected. */
+	} else {
+		err = lte_lc_init_and_connect_async(lte_handler);
+		if (err) {
+			printk("Modem could not be configured, error: %d\n",
+				err);
+			return;
+		}
+	}
+}
+
+static void at_configure(void)
+{
+	int err;
+
+	err = at_notif_init();
+	__ASSERT(err == 0, "AT Notify could not be initialized.");
+	err = at_cmd_init();
+	__ASSERT(err == 0, "AT CMD could not be established.");
+}
+
+static void bsd_lib_modem_dfu_handler(void)
+{
+	int err;
+
+	err = bsdlib_init();
+
+	switch (err) {
+	case MODEM_DFU_RESULT_OK:
+		printk("Modem update suceeded, reboot\n");
+		sys_reboot(SYS_REBOOT_COLD);
+		break;
+	case MODEM_DFU_RESULT_UUID_ERROR:
+	case MODEM_DFU_RESULT_AUTH_ERROR:
+		printk("Modem update failed, error: %d\n", err);
+		printk("Modem will use old firmware\n");
+		sys_reboot(SYS_REBOOT_COLD);
+		break;
+	case MODEM_DFU_RESULT_HARDWARE_ERROR:
+	case MODEM_DFU_RESULT_INTERNAL_ERROR:
+		printk("Modem update malfunction, error: %d, reboot\n", err);
+		sys_reboot(SYS_REBOOT_COLD);
+		break;
+	default:
+		break;
+	}
+
+	at_configure();
+}
+
+static int app_topics_subscribe(void)
+{
+	int err;
+	char custom_topic[75] = "my-custom-topic/example";
+	char custom_topic_2[75] = "my-custom-topic/example_2";
+
+	const struct aws_iot_topic_data topics_list[APP_TOPICS_COUNT] = {
+		[0].str = custom_topic,
+		[0].len = strlen(custom_topic),
+		[1].str = custom_topic_2,
+		[1].len = strlen(custom_topic)
+	};
+
+	err = aws_iot_subscription_topics_add(topics_list,
+					      ARRAY_SIZE(topics_list));
+	if (err) {
+		printk("aws_iot_subscription_topics_add, error: %d\n", err);
+	}
+
+	return err;
+}
+
+void main(void)
+{
+	int err;
+
+	printk("The AWS IoT sample started, version: %s\n", CONFIG_APP_VERSION);
+
+	cJSON_Init();
+
+	bsd_lib_modem_dfu_handler();
+
+	err = aws_iot_init(NULL, aws_iot_event_handler);
+	if (err) {
+		printk("AWS IoT library could not be initialized, error: %d\n",
+		       err);
+	}
+
+	/** Subscribe to customizable non-shadow specific topics
+	 *  to AWS IoT backend.
+	 */
+	err = app_topics_subscribe();
+	if (err) {
+		printk("Adding application specific topics failed, error: %d\n",
+			err);
+	}
+
+	work_init();
+	modem_configure();
+
+	err = modem_info_init();
+	if (err) {
+		printk("Failed initializing modem info module, error: %d\n",
+			err);
+	}
+
+	k_sem_take(&lte_connected, K_FOREVER);
+
+	date_time_update();
+
+	/** Sleep to ensure that time has been obtained before
+	 *  communication with AWS IoT.
+	 */
+	k_sleep(K_SECONDS(15));
+
+	err = aws_iot_connect(NULL);
+	if (err) {
+		printk("aws_iot_connect failed: %d\n", err);
+	}
+}
