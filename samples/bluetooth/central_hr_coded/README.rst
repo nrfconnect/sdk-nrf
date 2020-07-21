00674ec7a0395e2571118689245e7a31879de83b
Christian Hirsch <christian.hirsch@tuwien.ac.at>
2020-07-21 15:57:54 +0200

    samples: add Coded PHY peripheral and central
    The samples are based on the heart-monitor samples
    except that they are using Coded PHY for communication
    
    Signed-off-by: Christian Hirsch <christian.hirsch@tuwien.ac.at>
    Signed-off-by: Bartosz Gentkowski <bartosz.gentkowski@nordicsemi.no>
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

---
+++ samples/bluetooth/central_hr_coded/README.rst
@@ -0,0 +1,83 @@
+.. _bluetooth_central_hr_coded:
+
+Bluetooth: Central Heart Rate Monitor with Coded PHY
+####################################################
+
+The Central Heart Rate Monitor with Coded PHY offers similar functionality to the :ref:`zephyr:bluetooth_central_hr` sample from Zephyr.
+However, this sample specifically looks for heart rate monitors using LE Coded PHY.
+
+Overview
+********
+
+The sample demonstrates a |BLE| Central role functionality by scanning for other |BLE| devices that run a Heart Rate Server with LE Coded PHY support.
+It then establishes a connection to the first Peripheral device in range.
+It can be used together with the :ref:`peripheral_hr_coded` sample.
+
+Requirements
+************
+
+The sample supports the following development kits:
+
+.. table-from-rows:: /includes/sample_board_rows.txt
+   :header: heading
+   :rows: nrf5340pdk_nrf5340_cpuapp_and_cpuappns, nrf52840dongle_nrf52840, nrf52840dk_nrf52840
+
+The sample also requires:
+
+* A device running a Heart Rate Server with LE Coded PHY support to connect to.
+  For example, another development kit running the :ref:`peripheral_hr_coded` sample.
+
+Building and Running
+********************
+.. |sample path| replace:: :file:`samples/bluetooth/central_hr_coded`
+
+.. include:: /includes/build_and_run.txt
+
+Testing
+=======
+
+After programming the sample to your board, you can test it by connecting to another board that runs the :ref:`peripheral_hr_coded`.
+
+1. |connect_terminal_specific|
+#. Reset the board.
+#. Program the other board with the :ref:`peripheral_hr_coded`.
+#. Wait until the Coded advertiser is detected by the Central.
+   In the terminal window, check for information similar to the following::
+
+      Filters matched. Address: xx.xx.xx.xx.xx.xx (random) connectable: yes
+      Connection pending
+      Connected: xx.xx.xx.xx.xx.xx (random), tx_phy 4, rx_phy 4
+      The discovery procedure succeeded
+
+#. Observe that the received notifications are output in the terminal window::
+
+      [SUBSCRIBED]
+      [NOTIFICATION] Heart Rate 113 bpm
+      [NOTIFICATION] Heart Rate 114 bpm
+      [NOTIFICATION] Heart Rate 115 bpm
+
+Dependencies
+************
+
+This sample uses the following |NCS| libraries:
+
+* :ref:`gatt_dm_readme`
+* :ref:`nrf_bt_scan_readme`
+
+In addition, it uses the following Zephyr libraries:
+
+* ``include/zephyr/types.h``
+* ``include/errno.h``
+* ``include/zephyr.h``
+* ``include/sys/printk.h``
+* ``include/sys/byteorder.h``
+* :ref:`zephyr:kernel_api`:
+
+  * ``include/kernel.h``
+
+* :ref:`zephyr:bluetooth_api`:
+
+* ``include/bluetooth/bluetooth.h``
+* ``include/bluetooth/conn.h``
+* ``include/bluetooth/gatt.h``
+* ``include/bluetooth/uuid.h``
---
+++ samples/bluetooth/peripheral_hr_coded/src/main.c
@@ -0,0 +1,190 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/** @file
+ *  @brief Peripheral Heart Rate over LE Coded PHY sample
+ */
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/printk.h>
+#include <zephyr.h>
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/uuid.h>
+#include <bluetooth/gatt.h>
+#include <bluetooth/services/bas.h>
+#include <bluetooth/services/hrs.h>
+
+static struct k_work start_advertising_worker;
+
+static struct bt_le_ext_adv *adv;
+static struct bt_conn *default_conn;
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0x0d, 0x18, 0x0f, 0x18, 0x0a, 0x18),
+};
+
+static void connected(struct bt_conn *conn, uint8_t conn_err)
+{
+	int err;
+	struct bt_conn_info info;
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (conn_err) {
+		printk("Connection failed (err %d)\n", conn_err);
+		return;
+	}
+
+	default_conn = bt_conn_ref(conn);
+
+	err = bt_conn_get_info(conn, &info);
+
+	if (err) {
+		printk("Failed to get connection info\n");
+	} else {
+		const struct bt_conn_le_phy_info *phy_info;
+		phy_info = info.le.phy;
+
+		printk("Connected: %s, tx_phy %u, rx_phy %u\n",
+		       addr, phy_info->tx_phy, phy_info->rx_phy);
+	}
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	printk("Disconnected (reason 0x%02x)\n", reason);
+
+	if (default_conn) {
+		bt_conn_unref(default_conn);
+		default_conn = NULL;
+	}
+
+	k_work_submit(&start_advertising_worker);
+}
+
+static struct bt_conn_cb conn_callbacks = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+static int create_advertising_coded(void)
+{
+	int err;
+	struct bt_le_adv_param param =
+		BT_LE_ADV_PARAM_INIT(BT_LE_ADV_OPT_CONNECTABLE |
+				     BT_LE_ADV_OPT_EXT_ADV |
+				     BT_LE_ADV_OPT_CODED,
+				     BT_GAP_ADV_FAST_INT_MIN_2,
+				     BT_GAP_ADV_FAST_INT_MAX_2,
+				     NULL);
+
+	err = bt_le_ext_adv_create(&param, NULL, &adv);
+	if (err) {
+		printk("Failed to create advertiser set (%d)\n", err);
+		return err;
+	}
+
+	printk("Created adv: %p\n", adv);
+
+	err = bt_le_ext_adv_set_data(adv, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		printk("Failed to set advertising data (%d)\n", err);
+		return err;
+	}
+
+	return 0;
+}
+
+static void start_advertising_coded(struct k_work *item)
+{
+	int err;
+
+	err = bt_le_ext_adv_start(adv, NULL);
+	if (err) {
+		printk("Failed to start advertising set (%d)\n", err);
+		return;
+	}
+
+	printk("Advertiser %p set started\n", adv);
+}
+
+static void bt_ready(void)
+{
+	int err = 0;
+
+	printk("Bluetooth initialized\n");
+
+	k_work_init(&start_advertising_worker, start_advertising_coded);
+
+	err = create_advertising_coded();
+	if (err) {
+		printk("Advertising failed to create (err %d)\n", err);
+		return;
+	}
+
+	k_work_submit(&start_advertising_worker);
+}
+
+static void bas_notify(void)
+{
+	uint8_t battery_level = bt_gatt_bas_get_battery_level();
+
+	battery_level--;
+
+	if (!battery_level) {
+		battery_level = 100U;
+	}
+
+	bt_gatt_bas_set_battery_level(battery_level);
+}
+
+static void hrs_notify(void)
+{
+	static uint8_t heartrate = 90U;
+
+	/* Heartrate measurements simulation */
+	heartrate++;
+	if (heartrate == 160U) {
+		heartrate = 90U;
+	}
+
+	bt_gatt_hrs_notify(heartrate);
+}
+
+void main(void)
+{
+	int err;
+
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	bt_ready();
+
+	bt_conn_cb_register(&conn_callbacks);
+
+	/* Implement notification. At the moment there is no suitable way
+	 * of starting delayed work so we do it here
+	 */
+	while (1) {
+		k_sleep(K_SECONDS(1));
+
+		/* Heartrate measurements simulation */
+		hrs_notify();
+
+		/* Battery level simulation */
+		bas_notify();
+	}
+}
