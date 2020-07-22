852f03293a2ed8010e80e0b88baa85115fd35c2e
Marek Pieta <Marek.Pieta@nordicsemi.no>
2020-07-22 04:31:06 -0700

    applications: nrf_desktop: config_event cleanup
    Change switches to using only one event related to configuration data.
    
    Jira:DESK-1020
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- applications/nrf_desktop/src/events/config_event.c
+++ applications/nrf_desktop/src/events/config_event.c
@@ -8,98 +8,27 @@
 
 #include "config_event.h"
 
+static const char * const status_name[] = {
+#define X(name) STRINGIFY(name),
+	CONFIG_STATUS_LIST
+#undef X
+};
+
 static int log_config_event(const struct event_header *eh, char *buf,
-			   size_t buf_len)
+			    size_t buf_len)
 {
 	const struct config_event *event = cast_config_event(eh);
 
-	return snprintf(buf, buf_len, "id=%u", event->id);
+	__ASSERT_NO_MSG(event->status < ARRAY_SIZE(status_name));
+
+	return snprintf(buf, buf_len, "%s %s rcpt: %04x id: %02x",
+			status_name[event->status],
+			event->is_request ? "req" : "rsp",
+			event->recipient,
+			event->event_id);
 }
-
-static void profile_config_event(struct log_event_buf *buf,
-				 const struct event_header *eh)
-{
-	const struct config_event *event = cast_config_event(eh);
-
-	profiler_log_encode_u32(buf, event->id);
-}
-
-static int log_config_fetch_event(const struct event_header *eh, char *buf,
-				  size_t buf_len)
-{
-	struct config_fetch_event *event = cast_config_fetch_event(eh);
-
-	return snprintf(buf, buf_len, "id=%u", event->id);
-}
-
-static void profile_config_fetch_event(struct log_event_buf *buf,
-				       const struct event_header *eh)
-{
-	struct config_fetch_event *event = cast_config_fetch_event(eh);
-
-	profiler_log_encode_u32(buf, event->id);
-}
-
-static int log_config_fetch_request_event(const struct event_header *eh,
-					  char *buf,
-					  size_t buf_len)
-{
-	struct config_fetch_request_event *event =
-		cast_config_fetch_request_event(eh);
-
-	return snprintf(buf, buf_len, "id=%u", event->id);
-}
-
-static void profile_config_fetch_request_event(struct log_event_buf *buf,
-					       const struct event_header *eh)
-{
-	struct config_fetch_request_event *event =
-		cast_config_fetch_request_event(eh);
-
-	profiler_log_encode_u32(buf, event->id);
-}
-
-EVENT_INFO_DEFINE(config_event,
-		  ENCODE(PROFILER_ARG_U8),
-		  ENCODE("id"),
-		  profile_config_event);
 
 EVENT_TYPE_DEFINE(config_event,
 		  IS_ENABLED(CONFIG_DESKTOP_INIT_LOG_CONFIG_EVENT),
 		  log_config_event,
-		  &config_event_info);
-
-EVENT_INFO_DEFINE(config_fetch_event,
-		  ENCODE(PROFILER_ARG_U8),
-		  ENCODE("id"),
-		  profile_config_fetch_event);
-
-EVENT_TYPE_DEFINE(config_fetch_event,
-		  IS_ENABLED(CONFIG_DESKTOP_INIT_LOG_CONFIG_EVENT),
-		  log_config_fetch_event,
-		  &config_fetch_event_info);
-
-EVENT_INFO_DEFINE(config_fetch_request_event,
-		  ENCODE(PROFILER_ARG_U8),
-		  ENCODE("id"),
-		  profile_config_fetch_request_event);
-
-EVENT_TYPE_DEFINE(config_fetch_request_event,
-		  IS_ENABLED(CONFIG_DESKTOP_INIT_LOG_CONFIG_EVENT),
-		  log_config_fetch_request_event,
-		  &config_fetch_request_event_info);
-
-EVENT_TYPE_DEFINE(config_forward_event,
-		  IS_ENABLED(CONFIG_DESKTOP_INIT_LOG_CONFIG_EVENT),
-		  NULL,
-		  NULL);
-
-EVENT_TYPE_DEFINE(config_forward_get_event,
-		  IS_ENABLED(CONFIG_DESKTOP_INIT_LOG_CONFIG_EVENT),
-		  NULL,
-		  NULL);
-
-EVENT_TYPE_DEFINE(config_forwarded_event,
-		  IS_ENABLED(CONFIG_DESKTOP_INIT_LOG_CONFIG_EVENT),
-		  NULL,
 		  NULL);
