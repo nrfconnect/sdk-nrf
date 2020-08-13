522f3b37d77f45e591e12dbe80eb34228a65f792
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2020-08-13 08:35:41 +0200

    applications: asset_tracker: Enable GPS radio priority
    Enable prioritized access to shared radio resource for GPS.
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

--- applications/asset_tracker/src/gps_controller/gps_controller.c
+++ applications/asset_tracker/src/gps_controller/gps_controller.c
@@ -33,7 +33,8 @@ static void start(struct k_work *work)
 		.power_mode = GPS_POWER_MODE_DISABLED,
 		.timeout = CONFIG_GPS_CONTROL_FIX_TRY_TIME,
 		.interval = CONFIG_GPS_CONTROL_FIX_TRY_TIME +
-			gps_reporting_interval_seconds
+			gps_reporting_interval_seconds,
+		.priority = true,
 	};
 
 	if (gps_dev == NULL) {
