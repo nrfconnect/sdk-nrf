6f203170d857d82904c9b3dde243fa22faabc582
Ruth Fuchss <ruth.fuchss@nordicsemi.no>
2020-09-18 09:38:57 +0200

    doc: fix stylesheet order for Zephyr
    Update the order of stylesheets that are used for the Zephyr
    documentation to first include the common stylesheet and then
    the one specific to Zephyr.
    Also clean up some content in zephyr.css that is duplicated.
    
    Signed-off-by: Ruth Fuchss <ruth.fuchss@nordicsemi.no>

--- doc/zephyr/conf.py
+++ doc/zephyr/conf.py
@@ -342,6 +342,6 @@ linkcheck_anchors = False
 
 def setup(app):
     app.add_stylesheet("zephyr-custom.css")
-    app.add_stylesheet("css/zephyr.css")
     app.add_stylesheet("css/common.css")
+    app.add_stylesheet("css/zephyr.css")
     app.add_js_file("js/ncs.js")
