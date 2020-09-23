ada245b414a60e9ba3c41ec3724ffc3f0009cf78
Dominik Kilian <Dominik.Kilian@nordicsemi.no>
2020-09-23 10:03:47 +0200

    nrf_rpc: Adjust ipm_callback to normalized IPM callback signature
    IPM has a new callback signature as described in Zephyr issue #26923.
    
    Signed-off-by: Dominik Kilian <Dominik.Kilian@nordicsemi.no>

--- subsys/nrf_rpc/rp_ll.c
+++ subsys/nrf_rpc/rp_ll.c
@@ -125,7 +125,8 @@ const struct virtio_dispatch dispatch = {
 };
 
 /* Callback launch right after some data has arrived. */
-static void ipm_callback(void *context, uint32_t id, volatile void *data)
+static void ipm_callback(struct device *ipmdev, void *user_data, uint32_t id,
+			 volatile void *data)
 {
 	k_work_submit_to_queue(&my_work_q, &work_item);
 }
