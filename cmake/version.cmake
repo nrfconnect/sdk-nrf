05f6d0dc1a36d3413b12795a59b7ed5b951a7255
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2020-10-08 15:52:32 +0200

    version: creating ncs_version.h with input from VERSION.
    This commit generates a ncs_version.h which can be included in source
    code.
    version.h is generated from version.h.in with data from VERSION.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

---
+++ cmake/version.cmake
@@ -0,0 +1,6 @@
+math(EXPR NCS_VERSION_CODE "(${NCS_VERSION_MAJOR} << 16) + (${NCS_VERSION_MINOR} << 8)  + (${NCS_VERSION_PATCH})")
+
+# to_hex is made available by ${ZEPHYR_BASE}/cmake/hex.cmake
+to_hex(${NCS_VERSION_CODE} NCS_VERSION_NUMBER)
+
+configure_file(${NRF_DIR}/ncs_version.h.in ${ZEPHYR_BINARY_DIR}/include/generated/ncs_version.h)
