1b29eb4254a1b5aea11e3bfab63bf3a07479d2d9
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2020-10-19 15:08:17 +0200

    fprotect: Use MDK function nrf53_errata_19()
    Instead of Zephyr variant which causes problems on nRF53 PDK 0.8.0
    
    Ref: NCSDK-6769
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- lib/fprotect/fprotect_spu.c
+++ lib/fprotect/fprotect_spu.c
@@ -9,11 +9,12 @@
 #include <hal/nrf_spu.h>
 #include <errno.h>
 #include <soc.h>
+#include <nrf_erratas.h>
 
 
 #if defined(CONFIG_SOC_NRF5340_CPUAPP) \
 	&& defined(CONFIG_NRF5340_CPUAPP_ERRATUM19)
-#define SPU_BLOCK_SIZE (nrf53_has_erratum19() ? 32*1024 : 16*1024)
+#define SPU_BLOCK_SIZE (nrf53_errata_19() ? 32*1024 : 16*1024)
 #else
 #define SPU_BLOCK_SIZE CONFIG_FPROTECT_BLOCK_SIZE
 #endif
