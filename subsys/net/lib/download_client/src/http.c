bf32569b8356d03acefeef1d62308d1df1d845c4
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2020-10-26 12:47:34 +0100

    net: lib: download_client: Check dynamically set fragment size
    Adds a check for dynamically set fragment size in addition to
    the compile time default value when determining if a full file
    fragment has been downloaded.
    
    Fixes NCSdk-6856.
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

--- subsys/net/lib/download_client/src/http.c
+++ subsys/net/lib/download_client/src/http.c
@@ -247,8 +247,10 @@ int http_parse(struct download_client *client, size_t len)
 	client->progress += MIN(client->offset, len);
 
 	/* Have we received a whole fragment or the whole file? */
-	if ((client->offset < CONFIG_DOWNLOAD_CLIENT_HTTP_FRAG_SIZE) &&
-	    (client->progress != client->file_size)) {
+	if (client->progress != client->file_size &&
+	    client->offset < (client->config.frag_size_override != 0 ?
+			      client->config.frag_size_override :
+			      CONFIG_DOWNLOAD_CLIENT_HTTP_FRAG_SIZE)) {
 		return 1;
 	}
 
