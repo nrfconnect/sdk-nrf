3a816689633c250a10bacca6d3d975ca937e61bd
Joakim Andersson <joakim.andersson@nordicsemi.no>
2020-10-27 14:10:59 +0100

    multithreading_lock: Change lock to mutex.
    Change the multithreading lock to a mutex.
    This allows the thread to acquire the lock multiple times.
    A mutex handles priority inversion as opposed to a semaphore.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- lib/multithreading_lock/multithreading_lock.c
+++ lib/multithreading_lock/multithreading_lock.c
@@ -6,14 +6,14 @@
 
 #include "multithreading_lock.h"
 
-static K_SEM_DEFINE(mpsl_lock, 1, 1);
+static K_MUTEX_DEFINE(mpsl_lock);
 
 int multithreading_lock_acquire(k_timeout_t timeout)
 {
-	return k_sem_take(&mpsl_lock, timeout);
+	return k_mutex_lock(&mpsl_lock, timeout);
 }
 
 void multithreading_lock_release(void)
 {
-	k_sem_give(&mpsl_lock);
+	k_mutex_unlock(&mpsl_lock);
 }
