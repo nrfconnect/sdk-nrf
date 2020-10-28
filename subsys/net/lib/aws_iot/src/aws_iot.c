e1142966f23fca9290bd549969923a4c3e10122c
Simen S. Røstad <simen.rostad@nordicsemi.no>
2020-10-28 10:00:08 +0100

    net: lib: aws_iot: Add missing licence header
    Add header.
    
    Signed-off-by: Simen S. Røstad <simen.rostad@nordicsemi.no>

--- subsys/net/lib/aws_iot/src/aws_iot.c
+++ subsys/net/lib/aws_iot/src/aws_iot.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
 #include <net/aws_iot.h>
 #include <net/mqtt.h>
 #include <net/socket.h>
