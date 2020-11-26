91ee70eb49d83d39e281fa7c9ecb11c6161ab2c6
Stephen Stauts <stephen.stauts@nordicsemi.no>
2020-11-26 09:19:01 +0100

    partition_manager: report: Update string format calls and consistency
    Replaces legacy string formatting with format calls and f-strings.
    Update consistency across file.
    
    Ref: NCSDK-7210
    
    Signed-off-by: Stephen Stauts <stephen.stauts@nordicsemi.no>

--- scripts/partition_manager_report.py
+++ scripts/partition_manager_report.py
@@ -14,23 +14,23 @@ from os import path
 def print_region(region, size, pm_config):
     # Prepare colors (color code taken from size_report)
     bcolors_ansi = {
-        "HEADER"    : '\033[95m',
-        "OKBLUE"    : '\033[94m',
-        "OKGREEN"   : '\033[92m',
-        "WARNING"   : '\033[93m',
-        "FAIL"      : '\033[91m',
-        "ENDC"      : '\033[0m',
-        "BOLD"      : '\033[1m',
-        "UNDERLINE" : '\033[4m'
+        'HEADER'    : '\033[95m',
+        'OKBLUE'    : '\033[94m',
+        'OKGREEN'   : '\033[92m',
+        'WARNING'   : '\033[93m',
+        'FAIL'      : '\033[91m',
+        'ENDC'      : '\033[0m',
+        'BOLD'      : '\033[1m',
+        'UNDERLINE' : '\033[4m'
     }
-    if platform.system() == "Windows":
+    if platform.system() == 'Windows':
         # Set all color codes to empty string on Windows
         bcolors = dict.fromkeys(bcolors_ansi, '')
     else:
         bcolors = bcolors_ansi
 
     # Print header
-    print(bcolors["OKBLUE"] + "%s (0x%x - %dkB):" % (region, size, size/1024) + bcolors["ENDC"])
+    print(bcolors['OKBLUE'] + f'{region} ({hex(size)} - {size/1024}kB):' + bcolors['ENDC'])
 
     # Sort partitions three times:
     #  1. On whether they are a container (has a 'span'), containers first.
@@ -41,21 +41,21 @@ def print_region(region, size, pm_config):
     sorted_pm_config = sorted(sorted_pm_config, key=lambda x: pm_config[x]['address'])
 
     # Create text lines
-    lines = ["%s0x%x: %s (0x%x)%s" %
-                ("| "+bcolors["WARNING"] if 'span' not in pm_config[name] else "+---"+bcolors["OKBLUE"],
-                pm_config[name]['address'],
+    lines = ['{}{}: {} ({}){}'.format(
+                '| '+bcolors['WARNING'] if 'span' not in pm_config[name] else '+---'+bcolors['OKBLUE'],
+                hex(pm_config[name]['address']),
                 name,
-                pm_config[name]['size'],
-                bcolors["ENDC"])
+                hex(pm_config[name]['size']),
+                bcolors['ENDC'])
             for name in sorted_pm_config]
     maxlen = max(map(len, lines)) + 1
 
     # Add top and bottom of frame. Add dummy color so alignment always works.
-    top_bottom = "+" + bcolors["OKBLUE"] + bcolors["ENDC"]
+    top_bottom = '+' + bcolors['OKBLUE'] + bcolors['ENDC']
     lines = [top_bottom, *lines, top_bottom]
 
     # Print left-justified, framed lines
-    list(map(lambda s: print('%s' % s.ljust(maxlen, " ") + '|' if s[0] != '+' else s.ljust(maxlen, "-") + '+'), lines))
+    list(map(lambda s: print(s.ljust(maxlen, ' ') + '|' if s[0] != '+' else s.ljust(maxlen, '-') + '+'), lines))
 
 
 def parse_args():
@@ -65,9 +65,9 @@ def parse_args():
     # This argument has nargs set to '*' even though it is required. This is because of the logic associated with the
     # '--quiet' argument, which makes the script quit immediately. Hence we verify that this is set after 'quiet' is
     # checked.
-    parser.add_argument("-i", "--input", required=True, type=str, nargs="*",
-                        help="Path to the domain specific YAML files from Partition Manager")
-    parser.add_argument("-q", "--quiet", required=False, action='store_true',
+    parser.add_argument('-i', '--input', required=True, type=str, nargs='*',
+                        help='Path to the domain specific YAML files from Partition Manager')
+    parser.add_argument('-q', '--quiet', required=False, action='store_true',
                         help="Don't print anything")
 
     args = parser.parse_args()
@@ -82,12 +82,12 @@ def main():
         sys.exit(0)
 
     if not args.input:
-        raise RuntimeError("No input files provided")
+        raise RuntimeError('No input files provided')
 
     for i in args.input:
         fn = path.basename(i)
         if '_' in fn:
-            domain_name = fn[fn.index("partitions_") + len("partitions_"):fn.index(".yml")]
+            domain_name = fn[fn.index('partitions_') + len('partitions_'):fn.index('.yml')]
         else:
             domain_name = ''
         with open(i, 'r') as f:
@@ -97,5 +97,5 @@ def main():
         print_region(domain_name, max_address - min_address, pm_config_primary)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
