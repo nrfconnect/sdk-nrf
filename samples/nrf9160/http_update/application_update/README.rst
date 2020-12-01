abec1ee7f43626d32feb7632b53bf8638b406cd5
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2020-12-01 10:34:24 +0100

    samples: application_update: extract common functionality
    Extract code which could be shared between different
    update samples.
    
    Ref: NCSDK-7263
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- samples/nrf9160/http_update/application_update/README.rst
+++ samples/nrf9160/http_update/application_update/README.rst
@@ -7,9 +7,8 @@ nRF9160: HTTP application update
    :local:
    :depth: 2
 
-The HTTP application update sample demonstrates how to do a basic FOTA (firmware over-the-air) update.
+The HTTP application update sample demonstrates how to do a basic firmware over-the-air (FOTA) update.
 It uses the :ref:`lib_fota_download` library to download a file from a remote server and write it to flash.
-
 
 Overview
 ********
@@ -18,9 +17,8 @@ The sample connects to an HTTP server to download a signed firmware image.
 The image is generated when building the sample, but you must upload it to a server and configure where it can be downloaded.
 See `Specifying the image file`_ for more information.
 
-By default, the image is saved to `MCUboot`_ bootloader secondary slot.
-The downloaded image must be signed for use by MCUboot with imgtool.
-
+By default, the image is saved to the `MCUboot`_ bootloader secondary slot.
+To be used by MCUboot, the downloaded image must be signed using imgtool.
 
 Requirements
 ************
@@ -36,7 +34,6 @@ This image is automatically generated when building the application.
 
 .. include:: /includes/spm.txt
 
-
 Building and running
 ********************
 
@@ -48,7 +45,6 @@ The sample is built as a non-secure firmware image for the nrf9160dk_nrf9160ns b
 Because of this, it automatically includes the :ref:`secure_partition_manager`.
 The sample also uses MCUboot, which is automatically built and merged into the final HEX file when building the sample.
 
-
 Specifying the image file
 =========================
 
@@ -59,7 +55,7 @@ See `Setting up an AWS S3 bucket`_ for instructions.
 To specify the location in |SES|:
 
 1. Select :guilabel:`Project` -> :guilabel:`Configure nRF Connect SDK Project`.
-#. Navigate to :guilabel:`HTTP application update sample` and specify the download host name (``CONFIG_DOWNLOAD_HOST``) and the file to download (``CONFIG_DOWNLOAD_FILE``).
+#. Navigate to :guilabel:`HTTP application update sample` and specify the download hostname (``CONFIG_DOWNLOAD_HOST``) and the file to download (``CONFIG_DOWNLOAD_FILE``).
 #. Click :guilabel:`Configure` to save the configuration.
 
 .. include:: /includes/aws_s3_bucket.txt
@@ -69,13 +65,13 @@ Hosting your image on an AWS S3 Server
 
 1. Go to `AWS S3 console`_ and sign in.
 #. Go to the bucket you have created.
-#. Click :guilabel:`Upload` and select the file :file:`app_update.bin` (located in the :file:`zephyr` subfolder of your build directory).
+#. Click :guilabel:`Upload` and select the file :file:`app_update.bin`.
+   It is located in the :file:`zephyr` subfolder of your build directory.
 #. Click the file you uploaded in the bucket and check the :guilabel:`Object URL` field to find the download URL for the file.
 
 When specifying the image file, use the ``<bucket-name>.s3.<region>.amazonaws.com`` part of the URL for the download hostname.
-Make sure to not include the ``https``.
-Specify the rest of the URL as file name.
-
+Make sure to not include ``https``.
+Specify the file name as the remaining part of the URL.
 
 Testing
 =======
@@ -89,10 +85,12 @@ After programming the sample to the board, test it by performing the following s
    To upload the file on nRF Cloud, click :guilabel:`Upload` for the firmware URL that you generated earlier.
    Then select the file :file:`update.bin` and upload it.
 #. Reset your nRF9160 DK to start the application.
-#. Open a terminal emulator and observe that an output similar to this is printed::
+#. Open a terminal emulator and observe that an output similar to this is printed:
 
-    SPM: prepare to jump to Non-Secure image
-    ***** Booting Zephyr OS v1.13.99 *****
+.. code-block::
+
+   SPM: prepare to jump to Non-Secure image
+   ***** Booting Zephyr OS v1.13.99 *****
 
 #. Observe that **LED 1** is lit.
    This indicates that version 1 of the application is running.
@@ -121,6 +119,6 @@ It uses the following Zephyr libraries:
 * :ref:`zephyr:logging_api`
 * :ref:`zephyr:gpio_api`
 
-In addition, it uses the following MCUboot library:
+Also, it uses the following MCUboot library:
 
 * `MCUboot`_
--- samples/nrf9160/http_update/application_update/src/main.c
+++ samples/nrf9160/http_update/application_update/src/main.c
@@ -4,180 +4,29 @@
  * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
  */
 #include <zephyr.h>
-#include <drivers/gpio.h>
-#include <drivers/flash.h>
-#include <nrf_modem.h>
-#include <modem/lte_lc.h>
-#include <modem/at_cmd.h>
-#include <modem/at_notif.h>
-#include <modem/nrf_modem_lib.h>
-#include <modem/modem_key_mgmt.h>
-#include <net/fota_download.h>
 #include <dfu/mcuboot.h>
-
-#define LED_PORT	DT_GPIO_LABEL(DT_ALIAS(led0), gpios)
-#define TLS_SEC_TAG 42
-
-static const struct	device *gpiob;
-static struct		gpio_callback gpio_cb;
-static struct k_work	fota_work;
-
-
-/**@brief Recoverable modem library error. */
-void nrf_modem_recoverable_error_handler(uint32_t err)
-{
-	printk("Modem library recoverable error: %u\n", err);
-}
-
-int cert_provision(void)
-{
-	static const char cert[] = {
-		#include "../cert/BaltimoreCyberTrustRoot"
-	};
-	BUILD_ASSERT(sizeof(cert) < KB(4), "Certificate too large");
-	int err;
-	bool exists;
-	uint8_t unused;
-
-	err = modem_key_mgmt_exists(TLS_SEC_TAG,
-				    MODEM_KEY_MGMT_CRED_TYPE_CA_CHAIN,
-				    &exists, &unused);
-	if (err) {
-		printk("Failed to check for certificates err %d\n", err);
-		return err;
-	}
-
-	if (exists) {
-		/* For the sake of simplicity we delete what is provisioned
-		 * with our security tag and reprovision our certificate.
-		 */
-		err = modem_key_mgmt_delete(TLS_SEC_TAG,
-					    MODEM_KEY_MGMT_CRED_TYPE_CA_CHAIN);
-		if (err) {
-			printk("Failed to delete existing certificate, err %d\n",
-			       err);
-		}
-	}
-
-	printk("Provisioning certificate\n");
-
-	/*  Provision certificate to the modem */
-	err = modem_key_mgmt_write(TLS_SEC_TAG,
-				   MODEM_KEY_MGMT_CRED_TYPE_CA_CHAIN,
-				   cert, sizeof(cert) - 1);
-	if (err) {
-		printk("Failed to provision certificate, err %d\n", err);
-		return err;
-	}
-
-	return 0;
-}
-
-/**@brief Start transfer of the file. */
-static void app_dfu_transfer_start(struct k_work *unused)
-{
-	int retval;
-	int sec_tag;
-	char *apn = NULL;
-
-#ifndef CONFIG_USE_HTTPS
-	sec_tag = -1;
-#else
-	sec_tag = TLS_SEC_TAG;
-#endif
-
-	retval = fota_download_start(CONFIG_DOWNLOAD_HOST,
-				     CONFIG_DOWNLOAD_FILE,
-				     sec_tag,
-				     apn,
-				     0);
-	if (retval != 0) {
-		/* Re-enable button callback */
-		gpio_pin_interrupt_configure(gpiob,
-					     DT_GPIO_PIN(DT_ALIAS(sw0), gpios),
-					     GPIO_INT_EDGE_TO_ACTIVE);
-
-		printk("fota_download_start() failed, err %d\n",
-			retval);
-	}
-
-}
-
-/**@brief Turn on LED0 and LED1 if CONFIG_APPLICATION_VERSION
- * is 2 and LED0 otherwise.
- */
-static int led_app_version(void)
-{
-	const struct device *dev;
-
-	dev = device_get_binding(LED_PORT);
-	if (dev == 0) {
-		printk("Nordic nRF GPIO driver was not found!\n");
-		return 1;
-	}
-
-	gpio_pin_configure(dev, DT_GPIO_PIN(DT_ALIAS(led0), gpios),
-			   GPIO_OUTPUT_ACTIVE |
-			   DT_GPIO_FLAGS(DT_ALIAS(led0), gpios));
+#include <dfu/dfu_target_mcuboot.h>
+#include <modem/nrf_modem_lib.h>
+#include <net/fota_download.h>
+#include <stdio.h>
+#include "update.h"
 
 #if CONFIG_APPLICATION_VERSION == 2
-	gpio_pin_configure(dev, DT_GPIO_PIN(DT_ALIAS(led1), gpios),
-			   GPIO_OUTPUT_ACTIVE |
-			   DT_GPIO_FLAGS(DT_ALIAS(led1), gpios));
+#define NUM_LEDS 2
+#else
+#define NUM_LEDS 1
 #endif
-	return 0;
-}
 
-void dfu_button_pressed(const struct device *gpiob, struct gpio_callback *cb,
-			uint32_t pins)
-{
-	k_work_submit(&fota_work);
-	gpio_pin_interrupt_configure(gpiob, DT_GPIO_PIN(DT_ALIAS(sw0), gpios),
-				     GPIO_INT_DISABLE);
-}
+static uint8_t fota_buf[512];
 
-static int dfu_button_init(void)
-{
-	int err;
-
-	gpiob = device_get_binding(DT_GPIO_LABEL(DT_ALIAS(sw0), gpios));
-	if (gpiob == 0) {
-		printk("Nordic nRF GPIO driver was not found!\n");
-		return 1;
-	}
-	err = gpio_pin_configure(gpiob, DT_GPIO_PIN(DT_ALIAS(sw0), gpios),
-				 GPIO_INPUT |
-				 DT_GPIO_FLAGS(DT_ALIAS(sw0), gpios));
-	if (err == 0) {
-		gpio_init_callback(&gpio_cb, dfu_button_pressed,
-			BIT(DT_GPIO_PIN(DT_ALIAS(sw0), gpios)));
-		err = gpio_add_callback(gpiob, &gpio_cb);
-	}
-	if (err == 0) {
-		err = gpio_pin_interrupt_configure(gpiob,
-						   DT_GPIO_PIN(DT_ALIAS(sw0),
-							       gpios),
-						   GPIO_INT_EDGE_TO_ACTIVE);
-	}
-	if (err != 0) {
-		printk("Unable to configure SW0 GPIO pin!\n");
-		return 1;
-	}
-	return 0;
-}
-
-
-void fota_dl_handler(const struct fota_download_evt *evt)
+static void fota_dl_handler(const struct fota_download_evt *evt)
 {
 	switch (evt->id) {
 	case FOTA_DOWNLOAD_EVT_ERROR:
 		printk("Received error from fota_download\n");
 		/* Fallthrough */
 	case FOTA_DOWNLOAD_EVT_FINISHED:
-		/* Re-enable button callback */
-		gpio_pin_interrupt_configure(gpiob,
-					     DT_GPIO_PIN(DT_ALIAS(sw0), gpios),
-					     GPIO_INT_EDGE_TO_ACTIVE);
+		update_sample_done();
 		break;
 
 	default:
@@ -185,58 +34,17 @@ void fota_dl_handler(const struct fota_download_evt *evt)
 	}
 }
 
-/**@brief Configures modem to provide LTE link.
- *
- * Blocks until link is successfully established.
- */
-static void modem_configure(void)
-{
-#if defined(CONFIG_LTE_LINK_CONTROL)
-	BUILD_ASSERT(!IS_ENABLED(CONFIG_LTE_AUTO_INIT_AND_CONNECT),
-			"This sample does not support auto init and connect");
-	int err;
-#if !defined(CONFIG_NRF_MODEM_LIB_SYS_INIT)
-	/* Initialize AT only if nrf_modem_lib_init() is manually
-	 * called by the main application
-	 */
-	err = at_notif_init();
-	__ASSERT(err == 0, "AT Notify could not be initialized.");
-	err = at_cmd_init();
-	__ASSERT(err == 0, "AT CMD could not be established.");
-#if defined(CONFIG_USE_HTTPS)
-	err = cert_provision();
-	__ASSERT(err == 0, "Could not provision root CA to %d", TLS_SEC_TAG);
-#endif
-#endif
-	printk("LTE Link Connecting ...\n");
-	err = lte_lc_init_and_connect();
-	__ASSERT(err == 0, "LTE link could not be established.");
-	printk("LTE Link Connected!\n");
-#endif
-}
-
-static int application_init(void)
+static void update_start(void)
 {
 	int err;
+	char *apn = NULL;
 
-	k_work_init(&fota_work, app_dfu_transfer_start);
-
-	err = dfu_button_init();
+	err = fota_download_start(CONFIG_DOWNLOAD_HOST, CONFIG_DOWNLOAD_FILE,
+				  SEC_TAG, apn, 0);
 	if (err != 0) {
-		return err;
+		update_sample_done();
+		printk("fota_download_start() failed, err %d\n", err);
 	}
-
-	err = led_app_version();
-	if (err != 0) {
-		return err;
-	}
-
-	err = fota_download_init(fota_dl_handler);
-	if (err != 0) {
-		return err;
-	}
-
-	return 0;
 }
 
 void main(void)
@@ -244,48 +52,35 @@ void main(void)
 	int err;
 
 	printk("HTTP application update sample started\n");
-	printk("Initializing modem library\n");
-#if !defined(CONFIG_NRF_MODEM_LIB_SYS_INIT)
+
 	err = nrf_modem_lib_init();
-#else
-	/* If nrf_modem_lib is initialized on post-kernel we should
-	 * fetch the returned error code instead of nrf_modem_lib_init
-	 */
-	err = nrf_modem_lib_get_init_ret();
-#endif
-	switch (err) {
-	case MODEM_DFU_RESULT_OK:
-		printk("Modem firmware update successful!\n");
-		printk("Modem will run the new firmware after reboot\n");
-		k_thread_suspend(k_current_get());
-		break;
-	case MODEM_DFU_RESULT_UUID_ERROR:
-	case MODEM_DFU_RESULT_AUTH_ERROR:
-		printk("Modem firmware update failed\n");
-		printk("Modem will run non-updated firmware on reboot.\n");
-		break;
-	case MODEM_DFU_RESULT_HARDWARE_ERROR:
-	case MODEM_DFU_RESULT_INTERNAL_ERROR:
-		printk("Modem firmware update failed\n");
-		printk("Fatal error.\n");
-		break;
-	case -1:
-		printk("Could not initialize momdem library.\n");
-		printk("Fatal error.\n");
+	if (err) {
+		printk("Failed to initialize modem library!");
 		return;
-	default:
-		break;
 	}
-	printk("Initialized modem library\n");
-
-	modem_configure();
-
+	/* This is needed so that MCUBoot won't revert the update */
 	boot_write_img_confirmed();
 
-	err = application_init();
+	err = dfu_target_mcuboot_set_buf(fota_buf, sizeof(fota_buf));
 	if (err != 0) {
+		printk("dfu_target_mcuboot_set_buf() failed, err %d\n", err);
 		return;
 	}
 
-	printk("Press Button 1 to start the FOTA download\n");
+	err = fota_download_init(fota_dl_handler);
+	if (err != 0) {
+		printk("fota_download_init() failed, err %d\n", err);
+		return;
+	}
+
+	err = update_sample_init(&(struct update_sample_init_params){
+					.update_start = update_start,
+					.num_leds = NUM_LEDS
+				});
+	if (err != 0) {
+		printk("update_sample_init() failed, err %d\n", err);
+		return;
+	}
+
+	printk("Press Button 1 to perform application firmware update\n");
 }
