a2aec705d18efbe027a2ab5d76b273574618efdd
Jonathan Rico <jonathan.rico@nordicsemi.no>
2020-12-22 16:26:58 +0100

    Bluetooth: Add read chip temp HCI command support in SDC
    Add support for the Read_Chip_Temperature zephyr HCI VS command.
    A matching Kconfig option should be added later to zephyr too.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- subsys/bluetooth/controller/hci_internal.c
+++ subsys/bluetooth/controller/hci_internal.c
@@ -760,6 +760,12 @@ static uint8_t vs_cmd_put(uint8_t const * const cmd,
 		return sdc_hci_cmd_vs_zephyr_write_tx_power((void *)cmd_params,
 							    (void *)event_out_params);
 #endif /* CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL */
+
+	case SDC_HCI_OPCODE_CMD_VS_ZEPHYR_READ_CHIP_TEMP:
+		*param_length_out +=
+			sizeof(sdc_hci_cmd_vs_zephyr_read_chip_temp_return_t);
+		return sdc_hci_cmd_vs_zephyr_read_chip_temp(
+			(void *)event_out_params);
 #endif /* CONFIG_BT_HCI_VS_EXT */
 
 	case SDC_HCI_OPCODE_CMD_VS_LLPM_MODE_SET:
