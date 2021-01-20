eacd681c410a7b5081a866e92d6d0762b532b10a
Filip Zajdel <filip.zajdel@nordicsemi.no>
2021-01-20 14:10:18 +0100

    zigbee: Change the platform API
    Change the return type of zb_get_reset_source function due to MISRA
    violations.
    
    Signed-off-by: Filip Zajdel <filip.zajdel@nordicsemi.no>

--- subsys/zigbee/osif/zb_nrf_platform.c
+++ subsys/zigbee/osif/zb_nrf_platform.c
@@ -575,9 +575,9 @@ void zigbee_enable(void)
 /**
  * @brief Get the reason that triggered the last reset
  *
- * @return zb_reset_source_t
+ * @return zb_uint8_t
  * */
-zb_reset_source_t zb_get_reset_source(void)
+zb_uint8_t zb_get_reset_source(void)
 {
 	uint32_t reas;
 
