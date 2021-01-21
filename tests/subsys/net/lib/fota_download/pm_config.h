d1bd52dbc5919d85946ffa957fe501892d78aa8a
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2021-01-21 15:14:47 +0100

    tests: fota_download: adapt to using 'spm_s0_active'
    Also support secure execution by performing flash
    operations to set if s0 is active or not.
    
    Ref: NCSDK-7737
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- tests/subsys/net/lib/fota_download/pm_config.h
+++ tests/subsys/net/lib/fota_download/pm_config.h
@@ -7,6 +7,6 @@
 /* generated file copied to simplify building the test */
 #ifndef PM_CONFIG_H__
 #define PM_CONFIG_H__
-#define PM_S0_ADDRESS 0x8000
-#define PM_S1_ADDRESS 0x15000
+#define PM_S0_ADDRESS 0x10000
+#define PM_S1_ADDRESS 0x20000
 #endif /* PM_CONFIG_H__ */
