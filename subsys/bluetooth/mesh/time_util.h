7c0a9ae39dda9a9a4c4e04d1c38cc0ce5fdc241d
Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
2021-01-26 18:32:21 +0100

    Bluetooth: Mesh: Scheduler model implementation
    Implements the following models:
    
    Scheduler client
    Scheduler server
    Scheduler setup server
    
    Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>

--- subsys/bluetooth/mesh/time_util.h
+++ subsys/bluetooth/mesh/time_util.h
@@ -9,10 +9,31 @@
 
 #include <zephyr/types.h>
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define TAI_START_YEAR 2000
+#define TM_START_YEAR 1900
+#define TAI_START_DAY 6
+
+#define DAYS_YEAR 365ULL
+#define DAYS_LEAP_YEAR 366ULL
+#define SEC_PER_MIN (60ULL)
+#define SEC_PER_HOUR (60ULL * SEC_PER_MIN)
+#define SEC_PER_DAY (24ULL * SEC_PER_HOUR)
+
+#define SEC_PER_YEAR (DAYS_YEAR * SEC_PER_DAY)
+#define SEC_PER_LEAP_YEAR (DAYS_LEAP_YEAR * SEC_PER_DAY)
+#define FEB_DAYS 28
+#define FEB_LEAP_DAYS 29
+#define WEEKDAY_CNT 7
+
+static inline bool is_leap_year(uint32_t year)
+{
+	return ((year % 4) == 0) &&
+	       (((year % 100) != 0) || ((year % 400) == 0));
+}
 
 void tai_to_ts(const struct bt_mesh_time_tai *tai, struct tm *timeptr);
 int ts_to_tai(struct bt_mesh_time_tai *tai, struct tm *timeptr);
