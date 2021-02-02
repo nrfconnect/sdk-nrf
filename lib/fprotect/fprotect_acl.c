e5053c112f2e97e10192305bde6241b84183b5f9
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2021-02-02 15:15:32 +0100

    fprotect: Add support for no access protection for nrf52840
    -Add Kconfig option to enable no access protection
    -Changed fprotect_acl.c to include an additional function for no access
    
    Ref: NCSDK-8098
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

--- lib/fprotect/fprotect_acl.c
+++ lib/fprotect/fprotect_acl.c
@@ -9,10 +9,11 @@
 #include <errno.h>
 #include <sys/__assert.h>
 
-int fprotect_area(uint32_t start, size_t length)
-{
-	static uint32_t region_idx;
+static uint32_t region_idx;
 
+static int fprotect_set_permission(uint32_t start, size_t length,
+				   size_t permission)
+{
 	__ASSERT_NO_MSG(nrf_ficr_codepagesize_get(NRF_FICR) ==
 			CONFIG_FPROTECT_BLOCK_SIZE);
 
@@ -20,15 +21,29 @@ int fprotect_area(uint32_t start, size_t length)
 		return -ENOSPC;
 	}
 
-	if (start  % nrf_ficr_codepagesize_get(NRF_FICR) != 0 ||
+	if (start % nrf_ficr_codepagesize_get(NRF_FICR) != 0 ||
 	    length % nrf_ficr_codepagesize_get(NRF_FICR) != 0 ||
-	    length > NRF_ACL_REGION_SIZE_MAX ||
-	    length == 0) {
+	    length > NRF_ACL_REGION_SIZE_MAX || length == 0) {
 		return -EINVAL;
 	}
 
-	nrf_acl_region_set(NRF_ACL, region_idx++, start, length,
-			   NRF_ACL_PERM_READ_NO_WRITE);
+	nrf_acl_region_set(NRF_ACL, region_idx++, start, length, permission);
 
 	return 0;
 }
+
+int fprotect_area(uint32_t start, size_t length)
+{
+	return fprotect_set_permission(start, length,
+				       NRF_ACL_PERM_READ_NO_WRITE);
+}
+
+#if defined(CONFIG_FPROTECT_ENABLE_NO_ACCESS)
+
+int fprotect_area_no_access(uint32_t start, size_t length)
+{
+	return fprotect_set_permission(start, length,
+				       NRF_ACL_PERM_NO_READ_NO_WRITE);
+}
+
+#endif
