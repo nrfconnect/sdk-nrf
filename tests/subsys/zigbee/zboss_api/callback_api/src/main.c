a06c35dfb695059a35dd20a0940a2331d96f4db4
Filip Zajdel <filip.zajdel@nordicsemi.no>
2021-02-04 14:36:15 +0100

    zigbee: Fix static analysis issues
    Initialize a variable in "ota_client_attr_init".
    Make "add_to_queue_from_callback" static.
    
    Signed-off-by: Filip Zajdel <filip.zajdel@nordicsemi.no>

--- tests/subsys/zigbee/zboss_api/callback_api/src/main.c
+++ tests/subsys/zigbee/zboss_api/callback_api/src/main.c
@@ -80,7 +80,7 @@ void test_zboss_startup_signals(void)
 
 K_MSGQ_DEFINE(zb_callback_queue, sizeof(uint32_t), N_THREADS, 4);
 
-void add_to_queue_from_callback(uint8_t int_to_put)
+static void add_to_queue_from_callback(uint8_t int_to_put)
 {
 	const uint32_t ref_int[N_THREADS] = {0, 1, 2, 3};
 	int ret_val;
