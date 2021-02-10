fc6fc63f552dd6b8c9abcf66b93fa02efd2be40d
Emil Obalski <emil.obalski@nordicsemi.no>
2021-02-10 15:25:50 +0100

    caf: applications: Add common header files to CAF directory.
    Add common header files to CAF include directory.
    Files are copies from nrf_desktop application.
    
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>

---
+++ subsys/common_application_framework/include/gpio_pins.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2019-2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _GPIO_PINS_H_
+#define _GPIO_PINS_H_
+
+static const char * const port_map[] = {
+#ifdef CONFIG_GPIO_NRF_P0
+		DT_LABEL(DT_NODELABEL(gpio0)),
+#else
+		NULL,
+#endif /* CONFIG_GPIO_NRF_P0 */
+#ifdef CONFIG_GPIO_NRF_P1
+		DT_LABEL(DT_NODELABEL(gpio1)),
+#else
+		NULL,
+#endif /* CONFIG_GPIO_NRF_P1 */
+};
+
+struct gpio_pin {
+	uint8_t port;
+	uint8_t pin;
+};
+
+#endif /* _GPIO_PINS_H_ */
