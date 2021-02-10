9c7d0d7b4ab6255b7306fe86acea30cbf927a8eb
Emil Obalski <emil.obalski@nordicsemi.no>
2021-02-10 16:21:06 +0100

    caf: applications: Add PM events files to CAF directory.
    Add PM events files to CAF directory.
    
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>

---
+++ subsys/common_application_framework/events/power_event.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018-2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _POWER_EVENT_H_
+#define _POWER_EVENT_H_
+
+/**
+ * @brief Power Event
+ * @defgroup power_event Power Event
+ * @{
+ */
+
+#include "event_manager.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct power_down_event {
+	struct event_header header;
+	bool error;
+};
+
+EVENT_TYPE_DECLARE(power_down_event);
+
+
+struct wake_up_event {
+	struct event_header header;
+};
+
+EVENT_TYPE_DECLARE(wake_up_event);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _POWER_EVENT_H_ */
