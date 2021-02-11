aa383824abed458527e7033a2aa3351e7b49dfd7
Emil Obalski <emil.obalski@nordicsemi.no>
2021-02-11 12:05:24 +0100

    caf: applications: Add buttons module.
    Add buttons module to CAF. Module is based on nrf_desktop
    buttons hw_interface. Subsequent patches will make use of
    CAF buttons module in nrf_desktop application.
    
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>

---
+++ subsys/common_application_framework/events/button_event.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _BUTTON_EVENT_H_
+#define _BUTTON_EVENT_H_
+
+/**
+ * @brief Button Event
+ * @defgroup button_event Button Event
+ * @{
+ */
+
+#include "event_manager.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct button_event {
+	struct event_header header;
+
+	uint16_t key_id;
+	bool pressed;
+};
+
+EVENT_TYPE_DECLARE(button_event);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _BUTTON_EVENT_H_ */
