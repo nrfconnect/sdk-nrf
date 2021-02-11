4a5d1da72441857de566fab9002f5b23fabb1861
Ievgenii Meshcheriakov <ievgenii.meshcheriakov@nordicsemi.no>
2021-02-11 15:21:56 +0100

    Revert "crypto: Use the cc3xx CTR_DRBG apis in entropy driver"
    This reverts commit f46d6f81e2f4d998d4dbcc11e23dc7b319db06a0.
    
    Signed-off-by: Ievgenii Meshcheriakov <ievgenii.meshcheriakov@nordicsemi.no>

--- include/secure_services.h
+++ include/secure_services.h
@@ -58,13 +58,18 @@ void spm_request_system_reboot(void);
 
 /** Request a random number from the Secure Firmware.
  *
- * This provides a CTR_DRBG number from the CC3XX platform libraries.
+ * This provides a True Random Number from the on-board random number generator.
  *
- * @param[out] output  The CTR_DRBG number. Must be at least @c len long.
- * @param[in]  len     The length of the output array.
+ * @note Currently, the RNG hardware is run each time this is called. This
+ *       spends significant time and power.
+ *
+ * @param[out] output  The random number. Must be at least @c len long.
+ * @param[in]  len     The length of the output array. Currently, @c len must be
+ *                     144.
  * @param[out] olen    The length of the random number provided.
  *
- * @return non-negative on success, negative errno code on fail
+ * @retval 0        If successful.
+ * @retval -EINVAL  If @c len is invalid. Currently, @c len must be 144.
  */
 int spm_request_random_number(uint8_t *output, size_t len, size_t *olen);
 
