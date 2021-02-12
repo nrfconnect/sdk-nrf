4960eb4ee7563719059956168445a664eeda9577
Marek Pieta <Marek.Pieta@nordicsemi.no>
2021-02-12 14:10:24 +0100

    applications: machine_learning: Add senor_sim_ctrl
    Change adds module to control generated sensor signal.
    
    Jira: NCSDK-8457
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/machine_learning/configuration/common/sensor_sim_ctrl.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _SENSOR_SIM_CTRL_H_
+#define _SENSOR_SIM_CTRL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <zephyr/types.h>
+#include <drivers/sensor.h>
+#include <wave_gen.h>
+
+struct sim_signal_params {
+	enum sensor_channel chan;
+	const struct wave_gen_param *waves;
+	uint8_t waves_cnt;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SENSOR_SIM_CTRL_H_ */
