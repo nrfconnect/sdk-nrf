6f79a4591186b2cb04bf0a3824783bbf80f90815
Emil Obalski <emil.obalski@nordicsemi.no>
2021-02-15 11:31:43 +0100

    caf: applications: Add LEDs module.
    Add LEDs module to CAF. Module is based on nrf_desktop
    LEDs hw_interface. Subsequent patches will make use of
    CAF LEDs module in nrf_desktop application.
    
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>

---
+++ subsys/caf/events/led_event.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018-2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <stdio.h>
+
+#include <caf/events/led_event.h>
+
+static int log_led_event(const struct event_header *eh, char *buf,
+			  size_t buf_len)
+{
+	const struct led_event *event = cast_led_event(eh);
+
+	return snprintf(buf, buf_len, "led_id:%u effect:%p",
+			event->led_id, event->led_effect);
+}
+
+EVENT_TYPE_DEFINE(led_event,
+		  IS_ENABLED(CONFIG_CAF_INIT_LOG_LED_EVENTS),
+		  log_led_event,
+		  NULL);
+
+static int log_led_ready_event(const struct event_header *eh, char *buf,
+			 size_t buf_len)
+{
+	const struct led_ready_event *event = cast_led_ready_event(eh);
+
+	return snprintf(buf, buf_len, "led_id:%u effect:%p",
+			event->led_id, event->led_effect);
+}
+
+EVENT_TYPE_DEFINE(led_ready_event,
+		  IS_ENABLED(CONFIG_CAF_INIT_LOG_LED_READY_EVENTS),
+		  log_led_ready_event,
+		  NULL);
