edc5525ce941488e88f658dbb93e1e2ba18a1482
Marek Pieta <Marek.Pieta@nordicsemi.no>
2021-03-02 16:27:47 +0100

    applications: machine_learning: Add Thingy52 configuration
    Change adds debug configuration for Thingy52 board.
    The configuration uses LIS2DH accelerometer.
    
    Jira: NCSDK-8505
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/machine_learning/configuration/thingy52_nrf52832/sensor_sampler_def.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <caf/sensor_sampler.h>
+
+/* This configuration file is included only once from sensor_sampler module and holds
+ * information about the sampled sensors.
+ */
+
+/* This structure enforces the header file is included only once in the build.
+ * Violating this requirement triggers a multiple definition error at link time.
+ */
+const struct {} sensor_sampler_def_include_once;
+
+
+static const struct sampled_channel accel_chan[] = {
+	{
+		.chan = SENSOR_CHAN_ACCEL_XYZ,
+		.data_cnt = 3,
+	},
+};
+
+static const struct sensor_config sensor_configs[] = {
+	{
+		.dev_name = "LIS2DH12-ACCEL",
+		.event_descr = "accel_xyz",
+		.chans = accel_chan,
+		.chan_cnt = ARRAY_SIZE(accel_chan),
+		.sampling_period_ms = 50,
+	},
+};
