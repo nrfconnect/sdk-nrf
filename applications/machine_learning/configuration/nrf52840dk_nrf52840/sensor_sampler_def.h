963b026ded702c75213872449f8a559c7ec3f5a3
Marek Pieta <Marek.Pieta@nordicsemi.no>
2021-03-04 16:32:02 +0100

    applications: machine_learning: Add sensor sampler
    Change adds sensor sampler to the nrf52840DK configuration.
    
    Jira: NCSDK-8037
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/machine_learning/configuration/nrf52840dk_nrf52840/sensor_sampler_def.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <caf/sensor_sampler.h>
+
+/* This configuration file is included only once from sensor_sampler module and holds
+ * information about the sampled sensors.
+ */
+
+/* This structure enforces the header file is included only once in the build.
+ * Violating this requirement triggers a multiple definition error at link time.
+ */
+const struct {} sensor_sampler_def_include_once;
+
+
+static const struct sampled_channel accel_chan[] = {
+	{
+		.chan = SENSOR_CHAN_ACCEL_X,
+		.data_cnt = 1,
+	},
+	{
+		.chan = SENSOR_CHAN_ACCEL_Y,
+		.data_cnt = 1,
+	},
+	{
+		.chan = SENSOR_CHAN_ACCEL_Z,
+		.data_cnt = 1,
+	},
+};
+
+static const struct sensor_config sensor_configs[] = {
+	{
+		.dev_name = CONFIG_SENSOR_SIM_DEV_NAME,
+		.event_descr = "accel_sim_xyz",
+		.chans = accel_chan,
+		.chan_cnt = ARRAY_SIZE(accel_chan),
+		.sampling_period_ms = 50,
+	},
+};
