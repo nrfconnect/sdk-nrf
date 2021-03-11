91b8f777631551157692b8ee48ec1fecfba41f07
Kamil Gawor <Kamil.Gawor@nordicsemi.no>
2021-03-11 14:24:36 +0100

    samples: bluetooth: Add rpc_host sample
    Add the RPC Host sample for the nRF5340 network core.
    This sample is used when the full BLE stack is running on
    the Network Core.
    
    NCSDK-8436
    
    Signed-off-by: Kamil Gawor <Kamil.Gawor@nordicsemi.no>
    Signed-off-by: Dominik Kilian <Dominik.Kilian@nordicsemi.no>

---
+++ samples/bluetooth/rpc_host/src/main.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <sys/printk.h>
+
+void main(void)
+{
+	printk("Starting nRF RPC bluetooth host\n");
+}
