ea7f385f96e6e8f760a6d8a081be761ed42c2ff8
Robert Lubos <robert.lubos@nordicsemi.no>
2021-03-15 11:34:15 +0100

    samples: nrf9160: mqtt_simple: Randomize client ID by default
    Currently, the Client ID the MQTT client connected with was statically
    defined with the Kconfig. Because of this, every user of the sample
    which did not customize the Client ID was connecting with the same,
    default value. This resulted in some unexpected behaviour, when the MQTT
    server disconnected the existing connection when a new client with the
    same Client ID connected to the server.
    
    Fix this, by using a unique Client ID in the sample. For nRF91 based
    devices, the Client ID is generated using its IMEI number, other devices
    will use the board name and a 10-digit random number. The option to set
    the Client ID with Kconfig is still available - if set, it will override
    the default value.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- samples/nrf9160/mqtt_simple/src/main.c
+++ samples/nrf9160/mqtt_simple/src/main.c
@@ -11,6 +11,7 @@
 #include <random/rand32.h>
 #include <net/mqtt.h>
 #include <net/socket.h>
+#include <modem/at_cmd.h>
 #include <modem/lte_lc.h>
 #include <logging/log.h>
 #if defined(CONFIG_MODEM_KEY_MGMT)
@@ -348,6 +349,59 @@ static int broker_init(void)
 	return err;
 }
 
+#if defined(CONFIG_NRF_MODEM_LIB)
+#define IMEI_LEN 15
+#define CGSN_RESPONSE_LENGTH 19
+#define CLIENT_ID_LEN sizeof("nrf-") + IMEI_LEN
+#else
+#define RANDOM_LEN 10
+#define CLIENT_ID_LEN sizeof(CONFIG_BOARD) + 1 + RANDOM_LEN
+#endif /* defined(CONFIG_NRF_MODEM_LIB) */
+
+/* Function to get the client id */
+static const uint8_t* client_id_get(void)
+{
+	static uint8_t client_id[MAX(sizeof(CONFIG_MQTT_CLIENT_ID),
+				     CLIENT_ID_LEN)];
+
+	if (strlen(CONFIG_MQTT_CLIENT_ID) > 0) {
+		snprintf(client_id, sizeof(client_id), "%s",
+			 CONFIG_MQTT_CLIENT_ID);
+		goto exit;
+	}
+
+#if defined(CONFIG_NRF_MODEM_LIB)
+	char imei_buf[CGSN_RESPONSE_LENGTH + 1];
+	int err;
+
+	if (!IS_ENABLED(CONFIG_AT_CMD_SYS_INIT)) {
+		err = at_cmd_init();
+		if (err) {
+			LOG_ERR("at_cmd failed to initialize, error: %d", err);
+			goto exit;
+		}
+	}
+
+	err = at_cmd_write("AT+CGSN", imei_buf, sizeof(imei_buf), NULL);
+	if (err) {
+		LOG_ERR("Failed to obtain IMEI, error: %d", err);
+		goto exit;
+	}
+
+	imei_buf[IMEI_LEN] = '\0';
+
+	snprintf(client_id, sizeof(client_id), "nrf-%.*s", IMEI_LEN, imei_buf);
+#else
+	uint32_t id = sys_rand32_get();
+	snprintf(client_id, sizeof(client_id), "%s-%010u", CONFIG_BOARD, id);
+#endif /* !defined(NRF_CLOUD_CLIENT_ID) */
+
+exit:
+	LOG_DBG("client_id = %s", log_strdup(client_id));
+
+	return client_id;
+}
+
 /**@brief Initialize the MQTT client structure
  */
 static int client_init(struct mqtt_client *client)
@@ -365,8 +419,8 @@ static int client_init(struct mqtt_client *client)
 	/* MQTT client configuration */
 	client->broker = &broker;
 	client->evt_cb = mqtt_evt_handler;
-	client->client_id.utf8 = (uint8_t *)CONFIG_MQTT_CLIENT_ID;
-	client->client_id.size = strlen(CONFIG_MQTT_CLIENT_ID);
+	client->client_id.utf8 = client_id_get();
+	client->client_id.size = strlen(client->client_id.utf8);
 	client->password = NULL;
 	client->user_name = NULL;
 	client->protocol_version = MQTT_VERSION_3_1_1;
