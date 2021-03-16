f6d7fa87e5fcf33d7bcf07b9d38089cff93fe5a7
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2021-03-16 22:31:18 +0100

    doc: kconfig: simplify configuration
    Simplify Sphinx conf.py by removing defaults, using utils module and
    improve code formatting.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- doc/kconfig/conf.py
+++ doc/kconfig/conf.py
@@ -1,71 +1,35 @@
 # Kconfig documentation build configuration file
 
-import os
+from pathlib import Path
+import sys
 
-NRF_BASE = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 
-# -- General configuration ------------------------------------------------
+# Paths ------------------------------------------------------------------------
 
-# Sphinx 2.0 changes the default from 'index' to 'contents'
-master_doc = 'index'
+NRF_BASE = Path(__file__).absolute().parent / ".." / ".."
 
-# General information about the project.
-project = 'Kconfig reference'
-copyright = '2019-2021, Nordic Semiconductor'
-author = 'Nordic Semiconductor'
+sys.path.insert(0, str(NRF_BASE / "doc" / "utils"))
+import utils
 
-# Get rid of version number while keeping the spacing the same as for other
-# docsets
-version = '&nbsp;'
+# General configuration --------------------------------------------------------
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+project = "Kconfig reference"
+copyright = "2019-2021, Nordic Semiconductor"
+author = "Nordic Semiconductor"
+# NOTE: use blank space as version to preserve space
+version = "&nbsp;"
 
-# -- Options for HTML output ----------------------------------------------
+# Options for HTML output ------------------------------------------------------
 
 html_theme = "sphinx_ncs_theme"
-html_favicon = '{}/doc/static/images/favicon.ico'.format(NRF_BASE)
-
-# The name for this set of Sphinx documents.  If None, it defaults to
-# "<project> v<release> documentation".
-html_title = "Kconfig reference"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['{}/doc/static'.format(NRF_BASE)]
-
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-html_last_updated_fmt = '%b %d, %Y'
-
-# If false, no module index is generated.
-html_domain_indices = False
-
-# If false, no index is generated.
-html_use_index = True
-
-# If true, the index is split into individual pages for each letter.
-html_split_index = True
-
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_static_path = [str(NRF_BASE / "doc" / "static")]
+html_title = project
+html_last_updated_fmt = "%b %d, %Y"
+html_show_sourcelink = True
 html_show_sphinx = False
 
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-html_show_copyright = True
+html_theme_options = {"docsets": utils.get_docsets("kconfig")}
 
-# If true, license is shown in the HTML footer. Default is True.
-html_show_license = True
-
-html_theme_options = {
-    "versions": {
-        "nrf": "nRF Connect SDK",
-        "nrfxlib": "nrfxlib",
-        "zephyr": "Zephyr",
-        "mcuboot": "MCUboot"
-    }
-}
 
 def setup(app):
     app.add_css_file("css/common.css")
