51da6f1c2cd9355aa02f4272c1ae1e7d42dcf582
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2021-03-18 13:59:16 +0100

    Bluetooth: Mesh: Move internal defines in model headers to the bottom
    Moves all sections marked with INTERNAL_HIDDEN to the bottom of their
    files, as they're not really the first information you should find when
    you look at these headers.
    
    Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

--- include/bluetooth/mesh/gen_onoff.h
+++ include/bluetooth/mesh/gen_onoff.h
@@ -20,19 +20,6 @@
 extern "C" {
 #endif
 
-/** @cond INTERNAL_HIDDEN */
-#define BT_MESH_ONOFF_OP_GET BT_MESH_MODEL_OP_2(0x82, 0x01)
-#define BT_MESH_ONOFF_OP_SET BT_MESH_MODEL_OP_2(0x82, 0x02)
-#define BT_MESH_ONOFF_OP_SET_UNACK BT_MESH_MODEL_OP_2(0x82, 0x03)
-#define BT_MESH_ONOFF_OP_STATUS BT_MESH_MODEL_OP_2(0x82, 0x04)
-
-#define BT_MESH_ONOFF_MSG_LEN_GET 0
-#define BT_MESH_ONOFF_MSG_MINLEN_SET 2
-#define BT_MESH_ONOFF_MSG_MAXLEN_SET 4
-#define BT_MESH_ONOFF_MSG_MINLEN_STATUS 1
-#define BT_MESH_ONOFF_MSG_MAXLEN_STATUS 3
-/** @endcond */
-
 /** Mandatory parameters for the Generic OnOff Set message. */
 struct bt_mesh_onoff_set {
 	/** State to set. */
@@ -50,6 +37,19 @@ struct bt_mesh_onoff_status {
 	/** Remaining time value in milliseconds. */
 	int32_t remaining_time;
 };
+
+/** @cond INTERNAL_HIDDEN */
+#define BT_MESH_ONOFF_OP_GET BT_MESH_MODEL_OP_2(0x82, 0x01)
+#define BT_MESH_ONOFF_OP_SET BT_MESH_MODEL_OP_2(0x82, 0x02)
+#define BT_MESH_ONOFF_OP_SET_UNACK BT_MESH_MODEL_OP_2(0x82, 0x03)
+#define BT_MESH_ONOFF_OP_STATUS BT_MESH_MODEL_OP_2(0x82, 0x04)
+
+#define BT_MESH_ONOFF_MSG_LEN_GET 0
+#define BT_MESH_ONOFF_MSG_MINLEN_SET 2
+#define BT_MESH_ONOFF_MSG_MAXLEN_SET 4
+#define BT_MESH_ONOFF_MSG_MINLEN_STATUS 1
+#define BT_MESH_ONOFF_MSG_MAXLEN_STATUS 3
+/** @endcond */
 
 #ifdef __cplusplus
 }
