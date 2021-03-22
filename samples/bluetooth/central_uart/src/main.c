df18f77ae021a735182cf3bd98eaf920ea82b295
Marek Pieta <Marek.Pieta@nordicsemi.no>
2021-03-22 11:01:25 +0100

    samples: central_uart: Add MTU exchange
    The MTU exchange is required to receive bigger NUS data packets.
    
    Jira: NCSDK-8807
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- samples/bluetooth/central_uart/src/main.c
+++ samples/bluetooth/central_uart/src/main.c
@@ -328,6 +328,15 @@ static void gatt_discover(struct bt_conn *conn)
 	}
 }
 
+static void exchange_func(struct bt_conn *conn, uint8_t err, struct bt_gatt_exchange_params *params)
+{
+	if (!err) {
+		LOG_INF("MTU exchange done");
+	} else {
+		LOG_WRN("MTU exchange failed (err %" PRIu8 ")", err);
+	}
+}
+
 static void connected(struct bt_conn *conn, uint8_t conn_err)
 {
 	char addr[BT_ADDR_LE_STR_LEN];
@@ -354,6 +363,14 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 	}
 
 	LOG_INF("Connected: %s", log_strdup(addr));
+
+	static struct bt_gatt_exchange_params exchange_params;
+
+	exchange_params.func = exchange_func;
+	err = bt_gatt_exchange_mtu(conn, &exchange_params);
+	if (err) {
+		LOG_WRN("MTU exchange failed (err %d)", err);
+	}
 
 	err = bt_conn_set_security(conn, BT_SECURITY_L2);
 	if (err) {
