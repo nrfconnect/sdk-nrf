b9c00c0a8430e98f8d0360248bb8ebf644ec3684
Marek Pieta <Marek.Pieta@nordicsemi.no>
2021-04-07 17:04:51 +0200

    applications: machine_learning: Display simulated signal and result
    Change adds displaying simulated signal and machine learning result
    using LEDs.
    
    Jira: NCSDK-8939
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/machine_learning/configuration/common/led_state.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _LED_STATE_H_
+#define _LED_STATE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <caf/led_effect.h>
+
+struct ml_result_led_effect {
+	const char *label;
+	struct led_effect effect;
+};
+
+enum led_id {
+	LED_ID_ML_STATE,
+	LED_ID_SENSOR_SIM,
+
+	LED_ID_COUNT
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LED_STATE_H_ */
