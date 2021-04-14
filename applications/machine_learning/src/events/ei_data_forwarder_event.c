1573ea6518dc2ab619e8a8490607ccc279c269f0
Marek Pieta <Marek.Pieta@nordicsemi.no>
2021-04-14 10:23:25 +0200

    applications: machine_learning: Add EI data forwader event
    Change adds EI data forwader event that is used to broadcast
    information about state of the EI data forwarder.
    
    Jira: NCSDK-8938
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/machine_learning/src/events/ei_data_forwarder_event.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <stdio.h>
+
+#include "ei_data_forwarder_event.h"
+
+static const char * const ei_data_forwarder_state_name[] = {
+	[EI_DATA_FORWARDER_STATE_DISCONNECTED] = "DISCONNECTED",
+	[EI_DATA_FORWARDER_STATE_CONNECTED] = "CONNECTED",
+	[EI_DATA_FORWARDER_STATE_TRANSMITTING] = "TRANSMITTING",
+};
+
+static int log_ei_data_forwarder_event(const struct event_header *eh, char *buf, size_t buf_len)
+{
+	const struct ei_data_forwarder_event *event = cast_ei_data_forwarder_event(eh);
+
+	BUILD_ASSERT(ARRAY_SIZE(ei_data_forwarder_state_name) == EI_DATA_FORWARDER_STATE_COUNT);
+	__ASSERT_NO_MSG(event->state != EI_DATA_FORWARDER_STATE_DISABLED);
+	__ASSERT_NO_MSG(event->state < EI_DATA_FORWARDER_STATE_COUNT);
+	__ASSERT_NO_MSG(ei_data_forwarder_state_name[event->state] != NULL);
+
+	return snprintf(buf, buf_len, "state: %s", ei_data_forwarder_state_name[event->state]);
+}
+
+EVENT_TYPE_DEFINE(ei_data_forwarder_event,
+		  IS_ENABLED(CONFIG_ML_APP_INIT_LOG_EI_DATA_FORWARDER_EVENTS),
+		  log_ei_data_forwarder_event,
+		  NULL);
---
+++ applications/machine_learning/src/events/ei_data_forwarder_event.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _EI_DATA_FORWARDER_EVENT_H_
+#define _EI_DATA_FORWARDER_EVENT_H_
+
+/**
+ * @brief Edge Impulse Data Forwarder Event
+ * @defgroup ei_data_forwarder_event Edge Impulse Data Forwarder Event
+ * @{
+ */
+
+#include "event_manager.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @brief Edge Impulse data forwarder states. */
+enum ei_data_forwarder_state {
+	EI_DATA_FORWARDER_STATE_DISABLED,
+	EI_DATA_FORWARDER_STATE_DISCONNECTED,
+	EI_DATA_FORWARDER_STATE_CONNECTED,
+	EI_DATA_FORWARDER_STATE_TRANSMITTING,
+
+	EI_DATA_FORWARDER_STATE_COUNT
+};
+
+/** @brief Edge Impulse data forwarder event. */
+struct ei_data_forwarder_event {
+	struct event_header header; /**< Event header. */
+
+	enum ei_data_forwarder_state state; /**< Edge Impulse data forwarder state. */
+};
+
+EVENT_TYPE_DECLARE(ei_data_forwarder_event);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _EI_DATA_FORWARDER_EVENT_H_ */
