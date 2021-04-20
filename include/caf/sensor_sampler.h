3b6252c9d0fb115558900b78f550735d6338d112
Emil Obalski <emil.obalski@nordicsemi.no>
2021-04-20 12:28:43 +0200

    caf: sensor_sampler: Add sensor sleep functionality.
    This change adds possibility to stop sampling sensors if
    the the measured values are not changing for time specified
    by the user. This may help to save power.
    
    Jira: NCSDK-8613
    
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>

--- include/caf/sensor_sampler.h
+++ include/caf/sensor_sampler.h
@@ -13,6 +13,22 @@ extern "C" {
 
 #include <drivers/sensor.h>
 
+enum act_type {
+	ACT_TYPE_PERC,
+	ACT_TYPE_ABS,
+};
+
+struct trigger_activation {
+	enum act_type type;
+	float thresh;
+	unsigned int timeout_ms;
+};
+
+struct trigger {
+	struct sensor_trigger cfg;
+	struct trigger_activation activation;
+};
+
 struct sampled_channel {
 	enum sensor_channel chan;
 	uint8_t data_cnt;
@@ -24,6 +40,7 @@ struct sensor_config {
 	const struct sampled_channel *chans;
 	uint8_t chan_cnt;
 	unsigned int sampling_period_ms;
+	struct trigger *trigger;
 };
 
 #ifdef __cplusplus
