30329c7f420d94d012cf1beec2cc70d9c7a19a48
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2021-04-21 12:15:51 +0200

    crypto: Add persistent key example using PSA apis
    -Shows usage of persistent keys with the PSA apis
    
    Ref:NCSDK-9125
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

---
+++ samples/crypto/persistent_key_usage/src/main.c
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <logging/log.h>
+#include <stdio.h>
+#include <psa/crypto.h>
+#include <psa/crypto_extra.h>
+#include <crypto_common.h>
+
+LOG_MODULE_REGISTER(persistent_key_usage, LOG_LEVEL_DBG);
+
+/* ====================================================================== */
+/*			Global variables/defines for the persistent key  example	  */
+
+/* The key id for the persistent key. The macros PSA_KEY_ID_USER_MIN and
+ * PSA_KEY_ID_USER_MAX define the range of freely available key ids.
+ */
+#define SAMPLE_PERS_KEY_ID PSA_KEY_ID_USER_MIN
+
+
+static psa_key_handle_t key_handle;
+/* ====================================================================== */
+
+int crypto_finish(void)
+{
+	psa_status_t status;
+
+	/* Destroy the key handle */
+	status = psa_destroy_key(key_handle);
+	if (status != PSA_SUCCESS) {
+		PRINT_ERROR("psa_destroy_key", status);
+		return APP_ERROR;
+	}
+
+	return APP_SUCCESS;
+}
+
+int generate_prersistent_key(void)
+{
+	psa_status_t status;
+
+	PRINT_MESSAGE("Generating random persistent AES key...");
+
+	/* Configure the key attributes */
+	psa_key_attributes_t key_attributes = PSA_KEY_ATTRIBUTES_INIT;
+
+	psa_set_key_usage_flags(&key_attributes, PSA_KEY_USAGE_ENCRYPT | PSA_KEY_USAGE_DECRYPT);
+	psa_set_key_algorithm(&key_attributes, PSA_ALG_CTR);
+	psa_set_key_type(&key_attributes, PSA_KEY_TYPE_AES);
+	psa_set_key_bits(&key_attributes, 128);
+
+	/* Persistent key specific settings */
+	psa_set_key_lifetime(&key_attributes, PSA_KEY_LIFETIME_PERSISTENT);
+	psa_set_key_id(&key_attributes, SAMPLE_PERS_KEY_ID);
+
+	/* Generate a random AES key with persistent lifetime. The key can be used for
+	 * encryption/decryption using the key_handle.
+	 */
+	status = psa_generate_key(&key_attributes, &key_handle);
+	if (status != PSA_SUCCESS) {
+		PRINT_ERROR("psa_generate_key", status);
+		return APP_ERROR;
+	}
+
+	/* After the key handle is acquired the attributes are not needed */
+	psa_reset_key_attributes(&key_attributes);
+
+	PRINT_MESSAGE("Persistent key generated successfully!");
+
+	return APP_SUCCESS;
+}
+
+int main(void)
+{
+	int status;
+
+	PRINT_MESSAGE("Starting persistent key example...");
+
+	status = crypto_init();
+	if (status != APP_SUCCESS) {
+		PRINT_MESSAGE(APP_ERROR_MESSAGE);
+		return APP_ERROR;
+	}
+
+	status = generate_prersistent_key();
+	if (status != APP_SUCCESS) {
+		PRINT_MESSAGE(APP_ERROR_MESSAGE);
+		return APP_ERROR;
+	}
+
+	status = crypto_finish();
+	if (status != APP_SUCCESS) {
+		PRINT_MESSAGE(APP_ERROR_MESSAGE);
+		return APP_ERROR;
+	}
+
+	PRINT_MESSAGE(APP_SUCCESS_MESSAGE);
+
+	return APP_SUCCESS;
+}
