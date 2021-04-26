5480791c510d372c13b2c26b197358353f2cfc45
Peter Bigot <peter.bigot@nordicsemi.no>
2021-04-26 08:00:08 -0500

    nfc: rote conversion of k_work API
    Replace all existing deprecated API with the recommended alternative.
    
    Be aware that this does not address architectural errors in the use
    of the work API.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- subsys/nrf_rpc/rp_ll.c
+++ subsys/nrf_rpc/rp_ll.c
@@ -277,9 +277,9 @@ int rp_ll_init(void)
 	/* Get RPMsg device from RPMsg VirtIO device. */
 	rdev = rpmsg_virtio_get_rpmsg_device(&rvdev);
 
-	k_work_q_start(&my_work_q, rx_thread_stack,
+	k_work_queue_start(&my_work_q, rx_thread_stack,
 		K_THREAD_STACK_SIZEOF(rx_thread_stack),
-		CONFIG_NRF_RPC_TR_PRMSG_RX_PRIORITY);
+		CONFIG_NRF_RPC_TR_PRMSG_RX_PRIORITY, NULL);
 	k_work_init(&work_item, work_callback);
 
 	LOG_DBG("initializing %s: SUCCESS", __func__);
