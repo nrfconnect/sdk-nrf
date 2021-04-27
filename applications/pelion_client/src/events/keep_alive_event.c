pdunaj/fw-nrfconnect-nrf

applications: pelion_client: Add pelion reference application
Jira:NCSDK-8705

Co-authored-by: Emil Obalski <emil.obalski@nordicsemi.no>
Co-authored-by: Radoslaw Koppel <radoslaw.koppel@nordicsemi.no>
Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>
Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>
Signed-off-by: Radoslaw Koppel <radoslaw.koppel@nordicsemi.no>

applications/pelion_client/src/events/keep_alive_event.c
@@ -1,0 +1,27 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include "keep_alive_event.h"
+
+#include <zephyr.h>
+
+
+static void profile_simple_result_event(struct log_event_buf *buf,
+					const struct event_header *eh)
+{
+	(void)buf;
+	(void)eh;
+}
+
+EVENT_INFO_DEFINE(keep_alive_event,
+		  ENCODE(),
+		  ENCODE(),
+		  profile_simple_result_event);
+
+EVENT_TYPE_DEFINE(keep_alive_event,
+		  IS_ENABLED(CONFIG_PELION_CLIENT_INIT_LOG_KEEP_ALIVE_EVENT),
+		  NULL,
+		  &keep_alive_event_info);

nrfconnect/sdk-nrf

applications/pelion_client/src/events/keep_alive_event.c
@@ -1,0 +1,27 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include "keep_alive_event.h"
+
+#include <zephyr.h>
+
+
+static void profile_simple_result_event(struct log_event_buf *buf,
+					const struct event_header *eh)
+{
+	(void)buf;
+	(void)eh;
+}
+
+EVENT_INFO_DEFINE(keep_alive_event,
+		  ENCODE(),
+		  ENCODE(),
+		  profile_simple_result_event);
+
+EVENT_TYPE_DEFINE(keep_alive_event,
+		  IS_ENABLED(CONFIG_PELION_CLIENT_INIT_LOG_KEEP_ALIVE_EVENTS),
+		  NULL,
+		  &keep_alive_event_info);