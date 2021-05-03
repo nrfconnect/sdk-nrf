94e93b76c0125989335b66b8bdd907b80cdb0c05
Tommi Kangas <tommi.kangas@nordicsemi.no>
2021-05-03 15:31:07 +0300

    samples: nrf9160: gps: update GPS sample to use the new GNSS API
    Update sample to use the new function based GNSS API.
    
    Signed-off-by: Tommi Kangas <tommi.kangas@nordicsemi.no>

--- include/supl_os_client.h
+++ include/supl_os_client.h
@@ -7,7 +7,7 @@
 #ifndef SUPL_OS_CLIENT_H_
 #define SUPL_OS_CLIENT_H_
 
-#include <nrf_socket.h>
+#include <nrf_modem_gnss.h>
 #include <supl_session.h>
 
 /**
@@ -23,12 +23,12 @@
  * @brief Start a SUPL session
  *
  * @param[in] agps_request This contain the information about the AGPS data
- *                         that the GPS module is requesting from the server.
+ *                         that the GNSS module is requesting from the server.
  *
  * @return 0  SUPL session was successful.
  *         <0 SUPL session failed.
  */
-int supl_session(const nrf_gnss_agps_data_frame_t *const agps_request);
+int supl_session(const struct nrf_modem_gnss_agps_data_frame *const agps_request);
 
 /**
  * @brief Setup the API and the buffers required by
