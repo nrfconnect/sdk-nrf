1a4bc20a11e7e1f121d5383c245b79e61c39da32
Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
2021-05-11 16:20:56 +0200

    Bluetooth: Mesh: sensor properties tests
    Unit test to check encoding, decoding and boundary values
    for the motion sensor properties.
    
    Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>

---
+++ tests/subsys/bluetooth/mesh/sensor_subsys/src/main.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <math.h>
+#include <stdint.h>
+
+#include <ztest.h>
+#include <bluetooth/mesh/properties.h>
+#include <bluetooth/mesh/sensor.h>
+#include <bluetooth/mesh/sensor_types.h>
+#include <sensor.h> // private header from the source folder
+
+/****************** mock section **********************************/
+/****************** mock section **********************************/
+
+/****************** callback section ******************************/
+/****************** callback section ******************************/
+static int raw_scalar_value_get(int r, int m, int d, int b)
+{
+	return r / (m * pow(10.0, d) * pow(2.0, b));
+}
+
+static void test_motion_sensor(void)
+{
+	const struct bt_mesh_sensor_type *sensor_type;
+	struct sensor_value in_value;
+	struct sensor_value out_value;
+	int err;
+	int test_vector[] = {0, 25, 50, 75, 100, 0xFF};
+
+	sensor_type = bt_mesh_sensor_type_get(BT_MESH_PROP_ID_MOTION_SENSED);
+
+	for (int i = 0; i < ARRAY_SIZE(test_vector); i++) {
+		printk("step: %i, test value: %i\n", i, test_vector[i]);
+		BT_MESH_MODEL_BUF_DEFINE(msg, BT_MESH_SENSOR_OP_SETTING_SET,
+					BT_MESH_SENSOR_MSG_MAXLEN_SETTING_SET);
+		bt_mesh_model_msg_init(&msg, BT_MESH_SENSOR_OP_SETTING_SET);
+
+		in_value.val1 = test_vector[i];
+
+		int expected = test_vector[i] > 100 ?
+				0xFF : raw_scalar_value_get(in_value.val1, 1, 0, -1);
+
+		err = sensor_value_encode(&msg, sensor_type, &in_value);
+		zassert_ok(err, "Encoding failed with error code: %i", err);
+
+		zassert_equal(msg.data[1], expected,
+			"Encoded value: %i is not equal expected: %i",
+			msg.data[1], expected);
+
+		(void)net_buf_simple_pull_u8(&msg);
+
+		err = sensor_value_decode(&msg, sensor_type, &out_value);
+		zassert_ok(err, "Decoding failed with error code: %i", err);
+
+		zassert_equal(in_value.val1, out_value.val1,
+				"Encoded value: %i is not equal decoded: %i",
+				in_value.val1, out_value.val1);
+	}
+}
+
+void test_main(void)
+{
+	ztest_test_suite(sensor_types_test,
+			ztest_unit_test(test_motion_sensor)
+			 );
+
+	ztest_run_test_suite(sensor_types_test);
+}
