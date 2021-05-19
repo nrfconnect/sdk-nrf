d7680e78a2766f513e380e3cf6f97cd68b86ffaa
Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
2021-05-19 14:57:32 +0200

    Bluetooth: Mesh: Fix encoding, decoding undefined state
    Undefined state for some sensor properties is valid state and should be
    ecoded and decoded without errors.
    
    Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>

--- tests/subsys/bluetooth/mesh/sensor_subsys/src/main.c
+++ tests/subsys/bluetooth/mesh/sensor_subsys/src/main.c
@@ -9,7 +9,6 @@
 
 #include <ztest.h>
 #include <bluetooth/mesh/properties.h>
-#include <bluetooth/mesh/sensor.h>
 #include <bluetooth/mesh/sensor_types.h>
 #include <sensor.h> // private header from the source folder
 
@@ -23,26 +22,37 @@ static int raw_scalar_value_get(int r, int m, int d, int b)
 	return r / (m * pow(10.0, d) * pow(2.0, b));
 }
 
+static void sensor_type_sanitize(const struct bt_mesh_sensor_type *sensor_type)
+{
+	zassert_not_null(sensor_type, "NULL sensor type pointer");
+	zassert_not_null(sensor_type->channels, "NULL sensor channels pointer");
+	zassert_true(sensor_type->channel_count > 0, "Wrong channel number");
+
+	for (int i = 0; i < sensor_type->channel_count; i++) {
+		printk("Sensor channel name: %s\n", sensor_type->channels[i].name);
+	}
+}
+
 static void test_motion_sensor(void)
 {
 	const struct bt_mesh_sensor_type *sensor_type;
-	struct sensor_value in_value;
-	struct sensor_value out_value;
+	struct sensor_value in_value = {0};
+	struct sensor_value out_value = {0};
 	int err;
 	int test_vector[] = {0, 25, 50, 75, 100, 0xFF};
 
 	sensor_type = bt_mesh_sensor_type_get(BT_MESH_PROP_ID_MOTION_SENSED);
+	sensor_type_sanitize(sensor_type);
 
 	for (int i = 0; i < ARRAY_SIZE(test_vector); i++) {
-		printk("step: %i, test value: %i\n", i, test_vector[i]);
 		BT_MESH_MODEL_BUF_DEFINE(msg, BT_MESH_SENSOR_OP_SETTING_SET,
 					BT_MESH_SENSOR_MSG_MAXLEN_SETTING_SET);
 		bt_mesh_model_msg_init(&msg, BT_MESH_SENSOR_OP_SETTING_SET);
 
 		in_value.val1 = test_vector[i];
 
-		int expected = test_vector[i] > 100 ?
-				0xFF : raw_scalar_value_get(in_value.val1, 1, 0, -1);
+		int expected = test_vector[i] == 0xFF ?
+			0xFF : raw_scalar_value_get(test_vector[i], 1, 0, -1);
 
 		err = sensor_value_encode(&msg, sensor_type, &in_value);
 		zassert_ok(err, "Encoding failed with error code: %i", err);
