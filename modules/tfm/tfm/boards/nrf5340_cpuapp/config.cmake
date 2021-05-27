25f0b701ded1e43bcdacce88cc83ab239aa6b646
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2021-05-27 12:20:10 +0200

    tfm: Set default AEAD algorithm for nRF9160, nRF5340
    -Enables AES-CCM as the default AEAD algorithm
     for protected storage on nRF9160
    -Enables AES-GCM as the default AEAD algorithm
     for protected storage on nRF5340
    
    Ref: NCSDK-9006
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

--- modules/tfm/tfm/boards/nrf5340_cpuapp/config.cmake
+++ modules/tfm/tfm/boards/nrf5340_cpuapp/config.cmake
@@ -6,3 +6,6 @@
 #-------------------------------------------------------------------------------
 
 include(platform/ext/target/nordic_nrf/common/nrf5340/config.cmake)
+
+# Override the AEAD algorithm configuration
+set(PS_CRYPTO_AEAD_ALG                  PSA_ALG_GCM CACHE STRING    "The AEAD algorithm to use for authenticated encryption in Protected Storage")
