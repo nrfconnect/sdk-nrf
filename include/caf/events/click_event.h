48a75868a3e85de9d38e63fc21c80b83a8a9a038
Marek Pieta <Marek.Pieta@nordicsemi.no>
2021-05-27 13:16:58 +0200

    caf: events: Add documentation for hardware-related events
    Change adds doxygen documentation for button_event, click_event
    and led_event.
    
    Jira: NCSDK-9551
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>
    Signed-off-by: Grzegorz Ferenc <Grzegorz.Ferenc@nordicsemi.no>

--- include/caf/events/click_event.h
+++ include/caf/events/click_event.h
@@ -8,9 +8,10 @@
 #define _CLICK_EVENT_H_
 
 /**
- * @brief CAF Click Event
+ * @file
  * @defgroup caf_click_event CAF Click Event
  * @{
+ * @brief CAF Click Event.
  */
 
 #include "event_manager.h"
@@ -19,20 +20,41 @@
 extern "C" {
 #endif
 
-/** @brief Click types. */
+/** @brief Click types.
+ *
+ * The click type refers to the way a button can be pressed.
+ */
 enum click {
+	/** Button pressed and held for a period of time that is too long for CLICK_SHORT,
+	 * but too short for CLICK_LONG.
+	 */
 	CLICK_NONE,
+
+	/** Button pressed and released after a short time. */
 	CLICK_SHORT,
+
+	/** Button pressed and held for a long period of time. */
 	CLICK_LONG,
+
+	/** Two sequences of the button press and release in a short time interval. */
 	CLICK_DOUBLE,
 
+	/** Number of click types. */
 	CLICK_COUNT
 };
 
+/** @brief Click event.
+ *
+ * The click event is submitted when a click type is recorded for one of the monitored buttons.
+ */
 struct click_event {
+	/** Event header. */
 	struct event_header header;
 
+	/** ID of the button - matching the key_id used by the @ref button_event. */
 	uint16_t key_id;
+
+	/** Detected click type. */
 	enum click click;
 };
 
