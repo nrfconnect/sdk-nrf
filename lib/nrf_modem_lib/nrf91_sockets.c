d6354ed5316ed7e72c910a5d8d7f5161851fb3e1
Mirko Covizzi <mirko.covizzi@nordicsemi.no>
2021-05-28 01:35:12 -0700

    lib: nrf_modem: add support for TCP server timeout
    This commit adds support in the offloading layer for the new
    TCP server timeout socket option.
    
    Signed-off-by: Mirko Covizzi <mirko.covizzi@nordicsemi.no>

--- lib/nrf_modem_lib/nrf91_sockets.c
+++ lib/nrf_modem_lib/nrf91_sockets.c
@@ -218,6 +218,9 @@ static int z_to_nrf_optname(int z_in_level, int z_in_optname,
 		case SO_IPV6_ECHO_REPLY:
 			*nrf_out_optname = NRF_SO_SILENCE_IPV6_ECHO_REPLY;
 			break;
+		case SO_TCP_SRV_SESSTIMEO:
+			*nrf_out_optname = NRF_SO_TCP_SRV_SESSTIMEO;
+			break;
 		case SO_RAI_LAST:
 			*nrf_out_optname = NRF_SO_RAI_LAST;
 			break;
