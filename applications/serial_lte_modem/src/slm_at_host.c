nvlsianpu/fw-nrfconnect-nrf

tree-wide: Align with changes in device power management API
The device power management (PM) APIs and data structures have
been renamed from device_pm_* to pm_device_*.
The same applies to enumerations and definitions,
they now follow the PM_DEVICE_* convention. Some other API calls
such as device_set_power_state and device_get_power_state
have been renamed to pm_device_state_set and pm_device_state_get.


Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

applications/serial_lte_modem/src/slm_at_host.c
@@ -165,7 +165,7 @@ int poweroff_uart(void)
 
 	uart_rx_disable(uart_dev);
 	k_sleep(K_MSEC(100));
-	err = device_set_power_state(uart_dev, DEVICE_PM_OFF_STATE, NULL, NULL);
+	err = pm_device_state_set(uart_dev, DEVICE_PM_OFF_STATE, NULL, NULL);
 	if (err) {
 		LOG_ERR("Can't power off uart: %d", err);
 	}
@@ -178,14 +178,14 @@ int poweron_uart(void)
 	int err;
 	uint32_t current_state = 0;
 
-	err = device_get_power_state(uart_dev, &current_state);
+	err = pm_device_state_get(uart_dev, &current_state);
 	if (err) {
 		LOG_ERR("Device get power_state: %d", err);
 		return err;
 	}
 
-	if (current_state != DEVICE_PM_ACTIVE_STATE) {
-		device_set_power_state(uart_dev, DEVICE_PM_ACTIVE_STATE, NULL, NULL);
+	if (current_state != PM_DEVICE_STATE_ACTIVE) {
+		pm_device_state_set(uart_dev, PM_DEVICE_STATE_ACTIVE, NULL, NULL);
 		k_sleep(K_MSEC(100));
 		err = uart_receive();
 		if (err == 0) {
@@ -720,7 +720,7 @@ int slm_at_host_init(void)
 		return -EFAULT;
 	}
 	/* Power on UART module */
-	device_set_power_state(uart_dev, DEVICE_PM_ACTIVE_STATE, NULL, NULL);
+	pm_device_state_set(uart_dev, PM_DEVICE_STATE_ACTIVE, NULL, NULL);
 	err = uart_receive();
 	if (err) {
 		return -EFAULT;
@@ -781,7 +781,7 @@ void slm_at_host_uninit(void)
 	/* Power off UART module */
 	uart_rx_disable(uart_dev);
 	k_sleep(K_MSEC(100));
-	err = device_set_power_state(uart_dev, DEVICE_PM_OFF_STATE, NULL, NULL);
+	err = pm_device_state_set(uart_dev, DEVICE_PM_OFF_STATE, NULL, NULL);
 	if (err) {
 		LOG_WRN("Can't power off uart: %d", err);
 	}

samples/bluetooth/direct_test_mode/src/fem/nrf21540.c
@@ -706,8 +706,8 @@ int nrf21540_tx_gain_set(uint8_t gain)
 	}
 
 	if (uart_ready) {
-		err = device_set_power_state(uart, DEVICE_PM_OFF_STATE,
-					     NULL, NULL);
+		err = pm_device_state_set(uart, DEVICE_PM_OFF_STATE,
+					  NULL, NULL);
 		if (err) {
 			goto error;
 		}
@@ -728,8 +728,8 @@ int nrf21540_tx_gain_set(uint8_t gain)
 	uarte_configuration_restore(uarte_inst, &uarte_cfg);
 
 	if (uart_ready) {
-		err = device_set_power_state(uart, DEVICE_PM_ACTIVE_STATE,
-					     NULL, NULL);
+		err = pm_device_state_set(uart, PM_DEVICE_STATE_ACTIVE,
+					  NULL, NULL);
 	}
 
 	if (uart_irq) {

nrfconnect/sdk-nrf

Signed-off-by: Lukasz Maciejonczyk <lukasz.maciejonczyk@nordicsemi.no>

applications/serial_lte_modem/src/slm_at_host.c
@@ -174,7 +174,7 @@ int poweroff_uart(void)
 
 	uart_rx_disable(uart_dev);
 	k_sleep(K_MSEC(100));
-	err = device_set_power_state(uart_dev, DEVICE_PM_OFF_STATE, NULL, NULL);
+	err = pm_device_state_set(uart_dev, PM_DEVICE_STATE_OFF, NULL, NULL);
 	if (err) {
 		LOG_ERR("Can't power off uart: %d", err);
 	}
@@ -187,14 +187,14 @@ int poweron_uart(void)
 	int err;
 	uint32_t current_state = 0;
 
-	err = device_get_power_state(uart_dev, &current_state);
+	err = pm_device_state_get(uart_dev, &current_state);
 	if (err) {
 		LOG_ERR("Device get power_state: %d", err);
 		return err;
 	}
 
-	if (current_state != DEVICE_PM_ACTIVE_STATE) {
-		device_set_power_state(uart_dev, DEVICE_PM_ACTIVE_STATE, NULL, NULL);
+	if (current_state != PM_DEVICE_STATE_ACTIVE) {
+		pm_device_state_set(uart_dev, PM_DEVICE_STATE_ACTIVE, NULL, NULL);
 		k_sleep(K_MSEC(100));
 		err = uart_receive();
 		if (err == 0) {
@@ -738,7 +738,7 @@ int slm_at_host_init(void)
 		return -EFAULT;
 	}
 	/* Power on UART module */
-	device_set_power_state(uart_dev, DEVICE_PM_ACTIVE_STATE, NULL, NULL);
+	pm_device_state_set(uart_dev, PM_DEVICE_STATE_ACTIVE, NULL, NULL);
 	err = uart_receive();
 	if (err) {
 		return -EFAULT;
@@ -797,7 +797,7 @@ void slm_at_host_uninit(void)
 	/* Power off UART module */
 	uart_rx_disable(uart_dev);
 	k_sleep(K_MSEC(100));
-	err = device_set_power_state(uart_dev, DEVICE_PM_OFF_STATE, NULL, NULL);
+	err = pm_device_state_set(uart_dev, PM_DEVICE_STATE_OFF, NULL, NULL);
 	if (err) {
 		LOG_WRN("Can't power off uart: %d", err);
 	}

samples/bluetooth/direct_test_mode/src/fem/nrf21540.c
@@ -706,8 +706,8 @@ int nrf21540_tx_gain_set(uint8_t gain)
 	}
 
 	if (uart_ready) {
-		err = device_set_power_state(uart, DEVICE_PM_OFF_STATE,
-					     NULL, NULL);
+		err = pm_device_state_set(uart, PM_DEVICE_STATE_OFF,
+					  NULL, NULL);
 		if (err) {
 			goto error;
 		}
@@ -728,8 +728,8 @@ int nrf21540_tx_gain_set(uint8_t gain)
 	uarte_configuration_restore(uarte_inst, &uarte_cfg);
 
 	if (uart_ready) {
-		err = device_set_power_state(uart, DEVICE_PM_ACTIVE_STATE,
-					     NULL, NULL);
+		err = pm_device_state_set(uart, PM_DEVICE_STATE_ACTIVE,
+					  NULL, NULL);
 	}
 
 	if (uart_irq) {