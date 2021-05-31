90362bf337d2cfe30322c8ed10313b489002f862
Marek Pieta <Marek.Pieta@nordicsemi.no>
2021-05-31 11:44:13 +0200

    caf: events: Add documentation for power events
    Change adds doxygen documentation for power events.
    
    Jira: NCSDK-9551
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- include/caf/events/power_event.h
+++ include/caf/events/power_event.h
@@ -8,9 +8,10 @@
 #define _POWER_EVENT_H_
 
 /**
- * @brief CAF Power Event
+ * @file
  * @defgroup caf_power_event CAF Power Event
  * @{
+ * @brief CAF Power Event.
  */
 
 #include "event_manager.h"
@@ -19,13 +20,63 @@
 extern "C" {
 #endif
 
+/** @brief Power down event.
+ *
+ * The power down event is submitted to inform the application modules that they should be suspended
+ * to reduce the power consumption. The power down event can also be submitted to suspend
+ * the modules after an application module reports fatal error submitting @ref module_state_event
+ * with state set to MODULE_STATE_ERROR.
+ *
+ * An application module that handles the power down event should submit @ref module_state_event
+ * with state set to MODULE_STATE_OFF or MODULE_STATE_STANDBY. The MODULE_STATE_STANDBY should
+ * be submitted by a module that is capable of submitting @ref wake_up_event to wake up the system
+ * on external event. Otherwise MODULE_STATE_OFF should be reported. If the module is unable to
+ * suspend at the moment, it should consume the received power down events until it's ready to
+ * suspend itself. The module should also delay submitting the @ref module_state_event until it's
+ * ready to suspend itself.
+ *
+ * The module may handle the power down event differently if it's related to fatal error.
+ * In general, an application module should suspend itself, but some of the modules may still need
+ * to be active after the fatal error. For example, module that controls hardware LEDs may be used
+ * to display LED effect related to the fatal error.
+ *
+ * Only the module that controls the power management in the application can submit the power down
+ * event. It is also the final subscriber for the power down event. When it receives the power down
+ * event, it's ensured that all other application modules that handle power down event are already
+ * suspended. Then it can continue the power down procedure.
+ *
+ * @warning An application module that handles power down event must also handle @ref wake_up_event.
+ *          Otherwise the module will never be woken up after suspending.
+ */
 struct power_down_event {
+	/** Event header. */
 	struct event_header header;
+
+	/** Information if the power down was triggered by a fatal error. */
 	bool error;
 };
 
 
+/** @brief Wake up event.
+ *
+ * The wake up event is submitted to trigger exiting the suspended state.
+ *
+ * The wake up event can be submitted by any application module after the application is suspended.
+ * The event can be submitted for example on button press or sensor trigger.
+ *
+ * Every application module that handles wake up event should exit suspended state, turn on
+ * functionalities disabled on @ref power_down_event and submit @ref module_state_event with state
+ * set to MODULE_STATE_READY. If the module is already woken up, it should ignore the event.
+ * The module must not consume the wake up event to ensure that other listeners will be informed.
+ *
+ * The module that controls power management of the application should be an only early subscriber
+ * for the wake up event. This module can consume the wake up events to prevent waking up the
+ * application modules after fatal error.
+ *
+ * @note For more information about suspending modules see @ref power_down_event.
+ */
 struct wake_up_event {
+	/** Event header. */
 	struct event_header header;
 };
 
