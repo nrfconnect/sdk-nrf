bbe45f8afde2868dae41b5fd77dcf76bc53addad
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2021-06-08 11:01:15 +0200

    samples: openthread: Added DFU support overlay to CoAP client.
    * Added an overlay that enables performing Device Firmware Upgrade
    over Bluetooth LE using SMP protocol.
    * Extended documentation to describe how to perform DFU.
    
    Signed-off-by: Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>

--- samples/openthread/coap_client/src/coap_client.c
+++ samples/openthread/coap_client/src/coap_client.c
@@ -16,6 +16,10 @@
 #include "ble_utils.h"
 #endif
 
+#ifdef CONFIG_BOOTLOADER_MCUBOOT
+#include <dfu/mcuboot.h>
+#endif
+
 LOG_MODULE_REGISTER(coap_client, CONFIG_COAP_CLIENT_LOG_LEVEL);
 
 #define CONSOLE_LABEL DT_LABEL(DT_CHOSEN(zephyr_console))
@@ -132,6 +136,20 @@ void main(void)
 	if (IS_ENABLED(CONFIG_RAM_POWER_DOWN_LIBRARY)) {
 		power_down_unused_ram();
 	}
+
+#ifdef CONFIG_BOOTLOADER_MCUBOOT
+	/* Check if the image is run in the REVERT mode and eventually
+	 * confirm it to prevent reverting on the next boot.
+	 */
+	if (mcuboot_swap_type() == BOOT_SWAP_TYPE_REVERT) {
+		if (boot_write_img_confirmed()) {
+			LOG_ERR("Confirming firmware image failed, it will be reverted on the next "
+				"boot.");
+		} else {
+			LOG_INF("New device firmware image confirmed.");
+		}
+	}
+#endif
 
 	ret = dk_buttons_init(on_button_changed);
 	if (ret) {
