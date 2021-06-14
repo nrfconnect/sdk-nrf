961ddb2d5f31071e4c5d356f565ffdbb2e077cd2
Andreas Vibeto <andreas.vibeto@nordicsemi.no>
2021-06-14 16:50:48 +0200

    modules: tfm: disable unsupported crypto regression tests
    Disable tests for unsupported crypto regression tests
    in board configurations
    
    Signed-off-by: Andreas Vibeto <andreas.vibeto@nordicsemi.no>

--- modules/tfm/tfm/boards/nrf9160/config.cmake
+++ modules/tfm/tfm/boards/nrf9160/config.cmake
@@ -10,3 +10,7 @@ include(platform/ext/target/nordic_nrf/common/nrf9160/config.cmake)
 # Override the AEAD algorithm configuration since nRF9160 supports only AES_CCM
 set(PS_CRYPTO_AEAD_ALG                  PSA_ALG_CCM CACHE STRING    "The AEAD algorithm to use for authenticated encryption in Protected Storage")
 set(PLATFORM_DUMMY_CRYPTO_KEYS          FALSE      CACHE BOOL      "Use dummy crypto keys. Should not be used in production.")
+
+# Disable crypto regression tests that are not supported
+set(TFM_CRYPTO_TEST_ALG_CFB             OFF         CACHE BOOL      "Test CFB cryptography mode")
+set(TFM_CRYPTO_TEST_ALG_GCM             OFF         CACHE BOOL      "Test GCM cryptography mode")
