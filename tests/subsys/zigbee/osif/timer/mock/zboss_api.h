0afc3e50f63bc6b68e02e194af32a4e1c022911c
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2021-06-17 17:24:45 +0200

    zigbee: Align and fix unit tests
    After ZBOSS updates, the Zigbee unit tests implementation has to be
    aligned (especially: ZBOSS mocks and stubs).
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- tests/subsys/zigbee/osif/timer/mock/zboss_api.h
+++ tests/subsys/zigbee/osif/timer/mock/zboss_api.h
@@ -11,4 +11,25 @@
 #define ZB_TIMER_GET()
 #define ZB_TIME_BEACON_INTERVAL_TO_USEC(t) 0
 
+/**
+ *   Callback function typedef.
+ *   Callback is function planned to execute by another function.
+ *
+ *   @param  param - callback parameter - usually, but not always, ref to packet buf
+ *
+ *   See any sample
+ */
+typedef void (*zb_callback_t)(zb_uint8_t param);
+
+/**
+ *   Callback function with 2 parameters typedef.
+ *   Callback is function planned to execute by another function.
+ *
+ *   @param  param - callback parameter - usually, but not always, ref to packet buf
+ *   @param  cb_param - additional 2-byte callback parameter, user data.
+ *
+ *   See any sample
+ */
+typedef void (*zb_callback2_t)(zb_uint8_t param, zb_uint16_t cb_param);
+
 void zb_osif_busy_loop_delay(zb_uint32_t count);
--- tests/subsys/zigbee/osif/timer/stubs/zb_types.h
+++ tests/subsys/zigbee/osif/timer/stubs/zb_types.h
@@ -30,6 +30,7 @@ typedef signed short       zb_int16_t;
 /** @brief General purpose boolean type. */
 typedef enum zb_bool_e zb_bool_t;
 typedef unsigned int       zb_uint32_t;
+typedef int                zb_int32_t;
 
 typedef zb_uint32_t zb_time_t;
 
@@ -43,5 +44,8 @@ typedef zb_uint8_t zb_64bit_addr_t[8];
  * Long (64-bit) device address
  */
 typedef zb_64bit_addr_t zb_ieee_addr_t;
+
+/** @brief Return type for ZB functions returning execution status. @see ::RET_OK. */
+typedef zb_int32_t zb_ret_t;
 
 #endif /* ! defined ZB_TYPES_H */
