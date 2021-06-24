42c6c2a0ed8addb209ceb1344ef13025f9468eda
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2021-06-24 23:13:20 +0200

    partition_manager: add offset from region start
    A region has a base address and currently partition manager
    only outputs the absolute address to the partitions, not
    any offsets from the base of the region it is located in.
    
    This is problematic for several APIs that operate on offsets.
    Working with offsets also mimics the devicetree model
    more closely.
    
    To work better with these APIs, and to be more aligned with
    devicetree we add the _OFFSET define in the output of
    partition manager.
    
    In addition we change flash_map_pm.h to provide this value
    to the 'FLASH_AREA_OFFSET' macro, which is what users
    would expect in the first place.
    
    Ref: NCSDK-9797
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- include/flash_map_pm.h
+++ include/flash_map_pm.h
@@ -37,7 +37,7 @@
 #define FLASH_AREA_ID(label) PM_ID(label)
 
 #define FLASH_AREA_OFFSET(label) \
-	UTIL_CAT(PM_, UTIL_CAT(UTIL_CAT(PM_, UTIL_CAT(PM_ID(label), _LABEL)), _ADDRESS))
+	UTIL_CAT(PM_, UTIL_CAT(UTIL_CAT(PM_, UTIL_CAT(PM_ID(label), _LABEL)), _OFFSET))
 
 #define FLASH_AREA_SIZE(label) \
 	UTIL_CAT(PM_, UTIL_CAT(UTIL_CAT(PM_, UTIL_CAT(PM_ID(label), _LABEL)), _SIZE))
