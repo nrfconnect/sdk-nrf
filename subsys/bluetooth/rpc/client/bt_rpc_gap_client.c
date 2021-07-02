KAGA164/fw-nrfconnect-nrf

bluetooth: rpc: GATT server API serialization
This commit adds the serialization for the Bluetooth Low Energy
GATT Server layer. The samples which have a service server can
be build serialized.

NCSDK-9981

Signed-off-by: Kamil Gawor <Kamil.Gawor@nordicsemi.no>

subsys/bluetooth/rpc/client/bt_rpc_gap_client.c
@@ -13,6 +13,7 @@
 
 #include <nrf_rpc_cbor.h>
 
+#include "bt_rpc_gatt_svc_client.h"
 #include "bt_rpc_common.h"
 #include "serialize.h"
 #include "cbkproxy.h"
@@ -113,6 +114,14 @@ int bt_enable(bt_ready_cb_t cb)
 
 	nrf_rpc_cbor_cmd_no_err(&bt_rpc_grp, BT_ENABLE_RPC_CMD,
 				&_ctx, ser_rsp_decode_i32, &_result);
+	
+	if (_result) {
+		return _result;
+	}
+
+	if (IS_ENABLED(CONFIG_BT_CONN)) {
+		_result = bt_rpc_gatt_init();
+	}
 
 	if (_result) {
 		return _result;

nrfconnect/sdk-nrf

subsys/bluetooth/rpc/client/bt_rpc_gap_client.c
@@ -13,6 +13,7 @@
 
 #include <nrf_rpc_cbor.h>
 
+#include "bt_rpc_gatt_svc_client.h"
 #include "bt_rpc_common.h"
 #include "serialize.h"
 #include "cbkproxy.h"
@@ -113,6 +114,14 @@ int bt_enable(bt_ready_cb_t cb)
 
 	nrf_rpc_cbor_cmd_no_err(&bt_rpc_grp, BT_ENABLE_RPC_CMD,
 				&ctx, ser_rsp_decode_i32, &result);
+
+	if (result) {
+		return result;
+	}
+
+	if (IS_ENABLED(CONFIG_BT_CONN)) {
+		result = bt_rpc_gatt_init();
+	}
 
 	if (result) {
 		return result;