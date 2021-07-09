9b590c100303fe8f0a579bebc42a47bc483c6649
Marcin Jeliński <marcin.jelinski@nordicsemi.no>
2021-07-09 12:30:47 +0200

    samples: bluetooth: add peripheral_rscs sample
    Added the Peripheral RSCS sample to demonstrate how to use
    the Running Speed and Cadence Service
    
    Ref: NCSDK-10498
    
    Signed-off-by: Marcin Jeliński <marcin.jelinski@nordicsemi.no>

---
+++ samples/bluetooth/peripheral_rscs/README.rst
@@ -0,0 +1,94 @@
+.. _peripheral_rscs:
+
+Bluetooth: Peripheral Running Speed and Cadence Service (RSCS)
+###############################################################
+
+.. contents::
+   :local:
+   :depth: 2
+
+The peripheral RSCS sample demonstrates how to use the :ref:`rscs_readme`.
+
+Overview
+********
+
+This sample demonstrates the use of Running Speed and Cadence Service.
+It simulates a sensor and sends measurements to the connected device, such as a phone or a tablet.
+
+The mobile application on the device can configure sensor parameters using the SC Control Point characteristic.
+
+Requirements
+************
+
+The sample supports the following development kits:
+
+.. table-from-rows:: /includes/sample_board_rows.txt
+   :header: heading
+   :rows: nrf5340dk_nrf5340_cpuapp_and_cpuapp_ns, nrf52840dk_nrf52840, nrf52dk_nrf52832, nrf52833dk_nrf52833
+
+The sample also requires a phone or tablet running a compatible application, for example `nRF Connect for Mobile`_ or `nRF Toolbox`_.
+
+User interface
+**************
+
+LED 1:
+   Blinks every two seconds with the duty cycle set to 50% when the main loop is running and the device is advertising.
+
+LED 2:
+   On when connected.
+
+Building and running
+********************
+.. |sample path| replace:: :file:`samples/bluetooth/peripheral_rscs`
+
+.. include:: /includes/build_and_run.txt
+
+.. _peripheral_rscs_testing:
+
+Testing
+=======
+
+This testing procedure assumes that you are using `nRF Connect for Mobile`_.
+After programming the sample to your development kit, test it by performing the following steps:
+
+1. Power on your development kit.
+#. Connect to the device through nRF Connect for Mobile (the device is advertising as "Nordic_RSCS").
+#. Observe that the services of the connected device are shown.
+#. In :guilabel:`Running Speed and Cadence Service`, tap the :guilabel:`Notify` button for the "RSC Measurement" characteristic.
+#. Observe that notifications with the measurement values are received.
+#. In :guilabel:`RSC Feature`, tap the :guilabel:`Read` button to get the supported features.
+#. In :guilabel:`Sensor Location`, tap the :guilabel:`Read` button to read the location of the sensor.
+#. In :guilabel:`SC Control Point`, tap the :guilabel:`Indicate` button to control the sensor.
+#. The following Op Codes (with data if required) can be written into :guilabel:`SC Control Point`:
+
+   * ``01 xx xx xx`` to set the Total Distance Value to the entered value (if the server supports the Total Distance Measurement feature).
+   * ``02`` to start the sensor calibration process (if the server supports the Sensor Calibration feature).
+   * ``03 xx`` to update the sensor location (if the server supports the Multiple Sensor Locations feature).
+   * ``04`` to get a list of supported localizations (if the server supports the Multiple Sensor Locations feature).
+#. The answer consists of the following fields:
+
+   * ``10`` Response Code.
+   * ``xx`` Required Op Code.
+   * ``xx`` Status: ``01`` Success, ``02`` Op Code not supported, ``03`` Invalid Operand, ``04`` Operation Failed.
+   * ``data`` Optional, response data.
+
+Dependencies
+************
+
+This sample uses the following |NCS| libraries:
+
+* :ref:`rscs_readme`
+* :ref:`dk_buttons_and_leds_readme`
+
+In addition, it uses the following Zephyr libraries:
+
+* ``include/zephyr/types.h``
+* ``lib/libc/minimal/include/errno.h``
+* ``include/sys/printk.h``
+* ``include/random/rand32.h``
+* :ref:`zephyr:bluetooth_api`:
+
+  * ``include/bluetooth/bluetooth.h``
+  * ``include/bluetooth/conn.h``
+  * ``include/bluetooth/uuid.h``
+  * ``include/bluetooth/gatt.h``
---
+++ samples/bluetooth/peripheral_rscs/src/main.c
@@ -0,0 +1,280 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/printk.h>
+#include <random/rand32.h>
+#include <zephyr.h>
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/uuid.h>
+#include <bluetooth/gatt.h>
+
+#include <bluetooth/services/rscs.h>
+
+#include <settings/settings.h>
+
+#include <dk_buttons_and_leds.h>
+
+#define RUN_STATUS_LED          DK_LED1
+#define CON_STATUS_LED          DK_LED2
+#define RUN_LED_BLINK_INTERVAL  1000
+
+static struct bt_rscs_measurement measurement;
+static struct bt_conn *current_conn;
+
+static void rsc_simulation(struct bt_rscs_measurement *measurement)
+{
+	static uint8_t i;
+	uint32_t rand = sys_rand32_get();
+
+	measurement->inst_speed = 256*1 + rand % (256*11);	/* 1-10 m/s */
+	measurement->inst_cadence = 150 + rand % 31;		/* 150-180 1/min */
+	measurement->is_inst_stride_len = false;
+	measurement->is_total_distance = false;
+	measurement->is_running = false;
+
+	if (measurement->inst_speed > 3*256) {
+		measurement->is_running = true;
+	}
+
+	if (!(i%2)) {
+		measurement->is_inst_stride_len = true;
+		measurement->inst_stride_length = 50 + rand % 101;	/* 50-150 cm */
+	}
+	if (!(i%3)) {
+		measurement->is_total_distance = true;
+		measurement->total_distance += 10 + rand % 50;
+	}
+
+	i++;
+}
+
+int set_cumulative(uint32_t total_distance)
+{
+	printk("Set total distance: %d\n", total_distance);
+	measurement.total_distance = total_distance;
+	return 0;
+}
+
+int calibrarion(void)
+{
+	printk("Start the calibration procedure\n");
+	return 0;
+}
+
+void update_loc(uint8_t location)
+{
+	printk("Update sensor location: %d\n", location);
+}
+
+void evt_handler(enum bt_rscs_evt evt)
+{
+	switch (evt) {
+	case RSCS_EVT_MEAS_NOTIFY_ENABLE:
+		printk("Measurement notify enable\n");
+		break;
+	case RSCS_EVT_MEAS_NOTIFY_DISABLE:
+		printk("Measurement notify disable\n");
+	default:
+		break;
+	}
+}
+
+static const struct bt_rscs_cp_cb control_point_cb = {
+	.set_cumulative = set_cumulative,
+	.calibrarion = calibrarion,
+	.update_loc = update_loc,
+};
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err) {
+		printk("Connection failed (err 0x%02x)\n", err);
+	} else {
+		printk("Connected\n");
+	}
+
+	current_conn = bt_conn_ref(conn);
+
+	dk_set_led_on(CON_STATUS_LED);
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	printk("Disconnected (reason 0x%02x)\n", reason);
+
+	if (current_conn) {
+		bt_conn_unref(current_conn);
+		current_conn = NULL;
+	}
+
+	dk_set_led_off(CON_STATUS_LED);
+}
+
+#ifdef CONFIG_BT_LBS_SECURITY_ENABLED
+static void security_changed(struct bt_conn *conn, bt_security_t level, enum bt_security_err err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (!err) {
+		printk("Security changed: %s level %u\n", addr, level);
+	} else {
+		printk("Security failed: %s level %u err %d\n", addr, level,
+			err);
+	}
+}
+#endif
+
+static struct bt_conn_cb conn_callbacks = {
+	.connected    = connected,
+	.disconnected = disconnected,
+#ifdef CONFIG_BT_LBS_SECURITY_ENABLED
+	.security_changed = security_changed,
+#endif
+};
+
+#if defined(CONFIG_BT_LBS_SECURITY_ENABLED)
+static void auth_passkey_display(struct bt_conn *conn, unsigned int passkey)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Passkey for %s: %06u\n", addr, passkey);
+}
+
+static void auth_cancel(struct bt_conn *conn)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Pairing cancelled: %s\n", addr);
+}
+
+static void pairing_confirm(struct bt_conn *conn)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	bt_conn_auth_pairing_confirm(conn);
+
+	printk("Pairing confirmed: %s\n", addr);
+}
+
+static void pairing_complete(struct bt_conn *conn, bool bonded)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Pairing completed: %s, bonded: %d\n", addr, bonded);
+}
+
+static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Pairing failed conn: %s, reason %d\n", addr, reason);
+}
+
+static struct bt_conn_auth_cb conn_auth_callbacks = {
+	.passkey_display = auth_passkey_display,
+	.cancel = auth_cancel,
+	.pairing_confirm = pairing_confirm,
+	.pairing_complete = pairing_complete,
+	.pairing_failed = pairing_failed
+};
+#else
+static struct bt_conn_auth_cb conn_auth_callbacks;
+#endif
+
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL, BT_UUID_16_ENCODE(BT_UUID_RSCS_VAL))
+};
+
+void main(void)
+{
+	int err;
+	int blink_status = 0;
+
+	printk("Starting Running Speed and Cadence peripheral example\n");
+
+	err = dk_leds_init();
+	if (err) {
+		printk("LEDs init failed (err %d)\n", err);
+		return;
+	}
+
+	bt_conn_cb_register(&conn_callbacks);
+	if (IS_ENABLED(CONFIG_BT_RSCS_SECURITY_ENABLED)) {
+		bt_conn_auth_cb_register(&conn_auth_callbacks);
+	}
+
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	if (IS_ENABLED(CONFIG_SETTINGS)) {
+		settings_load();
+	}
+
+	struct bt_rscs_init_params rscs_init = {0};
+
+	rscs_init.features.inst_stride_len = 1;
+	rscs_init.features.multi_sensor_loc = 1;
+	rscs_init.features.sensor_calib_proc = 1;
+	rscs_init.features.total_distance = 1;
+	rscs_init.features.walking_or_running = 1;
+
+	rscs_init.supported_locations.other = 1;
+	rscs_init.supported_locations.top_of_shoe = 1;
+	rscs_init.supported_locations.in_shoe = 1;
+	rscs_init.supported_locations.hip = 1;
+
+	rscs_init.location = RSC_LOC_HIP;
+
+	rscs_init.cp_cb = &control_point_cb;
+	rscs_init.evt_handler = evt_handler;
+
+	err = bt_rscs_init(&rscs_init);
+	if (err) {
+		printk("Failed to init RSCS (err %d)\n", err);
+		return;
+	}
+
+	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Advertising successfully started\n");
+
+	for (;;) {
+		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
+		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+
+		rsc_simulation(&measurement);
+		bt_rscs_measurement_send(current_conn, &measurement);
+	}
+}
