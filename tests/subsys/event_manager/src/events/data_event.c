ffed62ca4d387ec14a7956d2c5fbb92a3d34b405
Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>
2021-07-20 05:41:16 -0700

    tests: subsys: event_manager: Optimize numeric data encoding
    Those changes result from changing profiler API.
    
    Jira: NCSDK-10033
    
    Signed-off-by: Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>

--- tests/subsys/event_manager/src/events/data_event.c
+++ tests/subsys/event_manager/src/events/data_event.c
@@ -13,19 +13,22 @@ static void profile_data_event(struct log_event_buf *buf,
 	struct data_event *event = cast_data_event(eh);
 
 	ARG_UNUSED(event);
-	profiler_log_encode_u32(buf, event->val1);
-	profiler_log_encode_u32(buf, event->val2);
-	profiler_log_encode_u32(buf, event->val3);
-	profiler_log_encode_u32(buf, event->val1u);
-	profiler_log_encode_u32(buf, event->val2u);
-	profiler_log_encode_u32(buf, event->val3u);
+	profiler_log_encode_int8(buf, event->val1);
+	profiler_log_encode_int16(buf, event->val2);
+	profiler_log_encode_int32(buf, event->val3);
+	profiler_log_encode_uint8(buf, event->val1u);
+	profiler_log_encode_uint16(buf, event->val2u);
+	profiler_log_encode_uint32(buf, event->val3u);
+	profiler_log_encode_string(buf, event->descr);
 }
 
 EVENT_INFO_DEFINE(data_event,
 		  ENCODE(PROFILER_ARG_S8, PROFILER_ARG_S16, PROFILER_ARG_S32,
-			 PROFILER_ARG_U8, PROFILER_ARG_U16, PROFILER_ARG_U32),
+			 PROFILER_ARG_U8, PROFILER_ARG_U16, PROFILER_ARG_U32,
+			 PROFILER_ARG_STRING),
 		  ENCODE("val1", "val2", "val3",
-			 "val1u", "val2u", "val3u"),
+			 "val1u", "val2u", "val3u",
+			 "descr"),
 		  profile_data_event);
 
 EVENT_TYPE_DEFINE(data_event,
