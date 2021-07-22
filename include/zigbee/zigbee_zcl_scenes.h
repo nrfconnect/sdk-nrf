03278599481f0eff7b4d332b5260909baab97faa
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2021-07-22 19:03:20 +0200

    zigbee: Add API docs for ZCL scenes helper library
    Define a new doxygen group as well as provide description for public
    APIs of the Zigbee ZCL scenes helper library.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- include/zigbee/zigbee_zcl_scenes.h
+++ include/zigbee/zigbee_zcl_scenes.h
@@ -9,10 +9,33 @@
 
 #include <zboss_api.h>
 
-/** Initialize ZCL scene table. */
+/** @file zigbee_zcl_scenes.h
+ *
+ * @defgroup zigbee_scenes Zigbee ZCL scenes helper library.
+ * @{
+ * @brief  Library for handling ZCL scenes events for common clusters.
+ *
+ * @details Provides ZCL callback event processing function that handles ZCL scenes commands.
+ */
+
+/**
+ * @brief Initialize the Zigbee ZCL scenes helper library.
+ *
+ * @details This function initializes the ZCL scene table as well as registers the
+ * settings observer for restoring scene table entries.
+ */
 void zcl_scenes_init(void);
 
-/** Handle scene-related ZCL callbacks. */
+/**
+ * @brief Function for passing ZCL callback events to the scenes helper library logic.
+ *
+ * @param[in] bufid  Reference to the Zigbee stack buffer used to pass event.
+ *
+ * @retval ZB_TRUE   if the event was handled by the library
+ * @retval ZB_FALSE  if the event was not handled by the library
+ */
 zb_bool_t zcl_scenes_cb(zb_bufid_t bufid);
 
 #endif /* ZIGBEE_ZCL_SCENES_H__ */
+
+/**@} */
