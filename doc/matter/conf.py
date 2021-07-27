8f196b59affeb5352f49ae5685302d4c788ab63c
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2021-07-27 13:33:37 +0200

    doc: initial support for matter docset
    This patch adds initial support for the Matter docset.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

---
+++ doc/matter/conf.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+import sys
+
+
+# Paths ------------------------------------------------------------------------
+
+NRF_BASE = Path(__file__).absolute().parents[2]
+
+sys.path.insert(0, str(NRF_BASE / "doc" / "_utils"))
+import utils
+
+MATTER_BASE = utils.get_projdir("matter")
+
+# General configuration --------------------------------------------------------
+
+project = "Matter"
+
+sys.path.insert(0, str(NRF_BASE / "doc" / "_extensions"))
+
+extensions = [
+    "recommonmark",
+    "sphinx_markdown_tables",
+    "ncs_cache",
+    "external_content"
+]
+source_suffix = [".rst", ".md"]
+
+# Options for HTML output ------------------------------------------------------
+
+html_theme = "sphinx_ncs_theme"
+html_static_path = [str(NRF_BASE / "doc" / "_static")]
+html_last_updated_fmt = "%b %d, %Y"
+html_show_sourcelink = True
+html_show_sphinx = False
+
+html_theme_options = {"docsets": utils.get_docsets("matter")}
+
+# Options for external_content -------------------------------------------------
+
+external_content_contents = [
+    (NRF_BASE / "doc" / "matter", "*.rst"),
+    (MATTER_BASE / "docs" / "guides", "images"),
+    (MATTER_BASE / "docs" / "guides", "nrfconnect_*.md"),
+    (MATTER_BASE / "docs" / "guides", "openthread_*.md"),
+    (MATTER_BASE / "docs" / "guides", "python_*.md"),
+    (MATTER_BASE / "docs" / "guides", "android_*.md"),
+    (MATTER_BASE / "docs" / "guides", "BUILDING.md"),
+]
+
+# Options for ncs_cache --------------------------------------------------------
+
+ncs_cache_docset = "matter"
+ncs_cache_build_dir = utils.get_builddir()
+ncs_cache_config = NRF_BASE / "doc" / "cache.yml"
+ncs_cache_manifest = NRF_BASE / "west.yml"
+
+
+def setup(app):
+    app.add_css_file("css/common.css")
+    app.add_css_file("css/matter.css")
