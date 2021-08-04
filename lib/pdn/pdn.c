e212f6869b072f76bcad24d184fb1bbdf22bbcac
Emanuele Di Santo <emdi@nordicsemi.no>
2021-08-04 13:28:06 +0200

    lib: pdn: port to nrf_modem_at and at_monitor
    - Update to use new nrf_modem_at APIs and at_monitor library.
    - Update modem_shell sample to enable at_monitor, since now
      the PDN library depends on it.
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>

--- lib/pdn/pdn.c
+++ lib/pdn/pdn.c
@@ -11,22 +11,12 @@
 #include <stdbool.h>
 #include <zephyr.h>
 #include <init.h>
+#include <nrf_modem_at.h>
 #include <modem/pdn.h>
-#include <modem/at_cmd.h>
-#include <modem/at_notif.h>
+#include <modem/at_monitor.h>
 #include <logging/log.h>
 
 LOG_MODULE_REGISTER(pdn, CONFIG_PDN_LOG_LEVEL);
-
-/* Buffer size for AT command and responses.
- * Longest AT command is:
- *   AT+CGDCONT=20,"IPV4V6","<APN>",0,0,0,0,0,0,0,0,0
- * where the APN can be up to 100 chars.
- * That's 43 chars, plus 100, which we round to 160.
- */
-#define AT_BUF_SIZE (160)
-/* Global buffer for AT commands and responses. */
-static char at_buf[AT_BUF_SIZE];
 
 #define CID_UNASSIGNED (-1)
 
@@ -39,6 +29,12 @@ static int esm_from_notif;
 static struct k_sem notif_sem;
 static struct pdn pdn_contexts[CONFIG_PDN_CONTEXTS_MAX];
 static pdn_event_handler_t default_callback;
+
+/* Use one monitor for all CGEV events and distinguish
+ * between the different type of events later (ME, IPV6, etc).
+ */
+AT_MONITOR(pdn_cgev, "+CGEV", on_cgev);
+AT_MONITOR(pdn_cnec_esm, "+CNEC ESM", on_cnec_esm);
 
 static struct pdn *pdn_find(int cid)
 {
@@ -58,60 +54,47 @@ static struct pdn *pdn_avail_get(void)
 	return pdn_find(CID_UNASSIGNED);
 }
 
-static void at_notif_handler(void *context, const char *notif)
+static void on_cnec_esm(const char *notif)
+{
+	int match;
+	uint32_t cid;
+	uint32_t esm_err;
+	struct pdn *pdn;
+
+	/* +CNEC_ESM: <cause>,<cid> */
+	match = sscanf(notif, "+CNEC_ESM: %u,%u", &esm_err, &cid);
+	if (match != 2) {
+		return;
+	}
+
+	pdn = pdn_find(cid);
+	if (!pdn) {
+		return;
+	}
+
+	if (pdn->callback) {
+		pdn->callback((intptr_t)cid, PDN_EVENT_CNEC_ESM, esm_err);
+	}
+
+	esm_from_notif = esm_err;
+	k_sem_give(&notif_sem);
+}
+
+static void on_cgev(const char *notif)
 {
 	char *p;
 	uint8_t cid;
-	uint8_t esm_err;
 	struct pdn *pdn;
-
-	/* These notifications are at least 10 characters long,
-	 * skip shorter strings.
-	 */
-	if (strlen(notif) < 10) {
-		return;
-	}
-
-	/* AT notif: +CNEC_ESM: <cause>,<cid>
-	 * Handle this by itself, we need to extract both CID and ESM
-	 * TODO: check before all other notifs?
-	 */
-	p = strstr(notif, "CNEC_ESM: ");
-	if (p) {
-		esm_err = strtoul(p + strlen("CNEC_ESM: "), NULL, 10);
-
-		p = strchr(p, ',');
-		if (!p) {
-			return;
-		}
-
-		cid = strtoul(p + 1, NULL, 10);
-
-		pdn = pdn_find(cid);
-		if (!pdn) {
-			return;
-		}
-
-		if (pdn->callback) {
-			pdn->callback((intptr_t)cid, PDN_EVENT_CNEC_ESM,
-				      esm_err);
-		}
-
-		esm_from_notif = esm_err;
-		k_sem_give(&notif_sem);
-
-		return;
-	}
 
 	const struct {
 		const char *notif;
 		const enum pdn_event event;
 	} map[] = {
-		{"CGEV: ME PDN ACT",	PDN_EVENT_ACTIVATED},	/* +CGEV: ME PDN ACT <cid> */
-		{"CGEV: ME PDN DEACT",	PDN_EVENT_DEACTIVATED},	/* +CGEV: ME PDN DEACT <cid> */
+		{"ME PDN ACT",		PDN_EVENT_ACTIVATED},	/* +CGEV: ME PDN ACT <cid> */
+		{"ME PDN DEACT",	PDN_EVENT_DEACTIVATED},	/* +CGEV: ME PDN DEACT <cid> */
 		/* Order is important */
-		{"CGEV: IPV6 FAIL",	PDN_EVENT_IPV6_DOWN},	/* +CGEV: IPV6 FAIL <cid> */
-		{"CGEV: IPV6",		PDN_EVENT_IPV6_UP},	/* +CGEV: IPV6 <cid> */
+		{"IPV6 FAIL",		PDN_EVENT_IPV6_DOWN},	/* +CGEV: IPV6 FAIL <cid> */
+		{"IPV6",		PDN_EVENT_IPV6_UP},	/* +CGEV: IPV6 <cid> */
 	};
 
 	for (size_t i = 0; i < ARRAY_SIZE(map); i++) {
@@ -142,6 +125,8 @@ static void at_notif_handler(void *context, const char *notif)
 int pdn_init(void)
 {
 	int err;
+	(void)err;
+
 	static bool has_init;
 
 	if (has_init) {
@@ -151,7 +136,7 @@ int pdn_init(void)
 	k_sem_init(&notif_sem, 0, 1);
 
 #if defined(CONFIG_PDN_LEGACY_PCO)
-	err = at_cmd_write("AT%XEPCO=0", NULL, 0, NULL);
+	err = nrf_modem_at_printf("AT%XEPCO=0");
 	if (err) {
 		LOG_WRN("Failed to set legacy PCO mode, err %d", err);
 	}
@@ -159,11 +144,6 @@ int pdn_init(void)
 
 	for (size_t i = 0; i < ARRAY_SIZE(pdn_contexts); i++) {
 		pdn_contexts[i].context_id = CID_UNASSIGNED;
-	}
-
-	err = at_notif_register_handler(NULL, at_notif_handler);
-	if (err) {
-		return err;
 	}
 
 	has_init = true;
@@ -184,7 +164,6 @@ int pdn_default_callback_set(pdn_event_handler_t cb)
 int pdn_ctx_create(uint8_t *cid, pdn_event_handler_t cb)
 {
 	int err;
-	char *p;
 	struct pdn *pdn;
 
 	if (!cid) {
@@ -196,17 +175,10 @@ int pdn_ctx_create(uint8_t *cid, pdn_event_handler_t cb)
 		return -ENOMEM;
 	}
 
-	err = at_cmd_write("AT%XNEWCID?", at_buf, sizeof(at_buf), NULL);
-	if (err) {
+	err = nrf_modem_at_scanf("AT%XNEWCID?", "%%XNEWCID: %d", &pdn->context_id);
+	if (err < 0) {
 		return err;
 	}
-
-	p = strchr(at_buf, ':');
-	if (!p) {
-		return -EBADMSG;
-	}
-
-	pdn->context_id = strtoul(p + 1, NULL, 10);
 
 	*cid = pdn->context_id;
 
@@ -247,22 +219,15 @@ int pdn_ctx_configure(uint8_t cid, const char *apn, enum pdn_fam fam,
 		 * where passing default values does not work as intended.
 		 * Only format these params when `opt` is passed explicitly.
 		 */
-		err = snprintf(at_buf, sizeof(at_buf),
-			"AT+CGDCONT=%u,%s,%s,,,,%u,,,,%u,%u",
+		err = nrf_modem_at_printf("AT+CGDCONT=%u,%s,%s,,,,%u,,,,%u,%u",
 			cid, family, apn,
 			opt->ip4_addr_alloc,
 			opt->nslpi,
 			opt->secure_pco);
 	} else {
-		err = snprintf(at_buf, sizeof(at_buf),
-			"AT+CGDCONT=%u,%s,%s", cid, family, apn);
+		err = nrf_modem_at_printf("AT+CGDCONT=%u,%s,%s", cid, family, apn);
 	}
 
-	if (err < 0 || err >= sizeof(at_buf)) {
-		return -ENOBUFS;
-	}
-
-	err = at_cmd_write(at_buf, NULL, 0, NULL);
 	if (err) {
 		LOG_ERR("Failed to configure CID %d, err, %d", cid, err);
 		return err;
@@ -280,14 +245,7 @@ int pdn_ctx_auth_set(uint8_t cid, enum pdn_auth method,
 		return -EINVAL;
 	}
 
-	err = snprintf(at_buf, sizeof(at_buf),
-		       "AT+CGAUTH=%u,%d,%s,%s", cid, method, user, password);
-
-	if (err < 0 || err >= sizeof(at_buf)) {
-		return -ENOBUFS;
-	}
-
-	err = at_cmd_write(at_buf, NULL, 0, NULL);
+	err = nrf_modem_at_printf("AT+CGAUTH=%u,%d,%s,%s", cid, method, user, password);
 	if (err) {
 		LOG_ERR("Failed to configure auth for CID %d, err, %d", cid, err);
 		return err;
@@ -306,12 +264,7 @@ int pdn_ctx_destroy(uint8_t cid)
 		return -EINVAL;
 	}
 
-	err = snprintf(at_buf, sizeof(at_buf), "AT+CGDCONT=%u", cid);
-	if (err < 0 || err >= sizeof(at_buf)) {
-		return -ENOBUFS;
-	}
-
-	err = at_cmd_write(at_buf, NULL, 0, NULL);
+	err = nrf_modem_at_printf("AT+CGDCONT=%u", cid);
 	if (err) {
 		return err;
 	}
@@ -326,13 +279,7 @@ static int cgact(uint8_t cid, bool activate)
 {
 	int err;
 
-	err = snprintf(at_buf, sizeof(at_buf), "AT+CGACT=%u,%u",
-		       activate ? 1 : 0, cid);
-	if (err < 0 || err >= sizeof(at_buf)) {
-		return -ENOBUFS;
-	}
-
-	err = at_cmd_write(at_buf, NULL, 0, NULL);
+	err = nrf_modem_at_printf("AT+CGACT=%u,%u", activate ? 1 : 0, cid);
 	if (err) {
 		LOG_WRN("Failed to activate PDN for CID %d, err %d", cid, err);
 		return err;
@@ -378,12 +325,7 @@ int pdn_id_get(uint8_t cid)
 	char *p;
 	char resp[32];
 
-	err = snprintf(at_buf, sizeof(at_buf), "AT%%XGETPDNID=%u", cid);
-	if (err < 0 || err >= sizeof(at_buf)) {
-		return -ENOBUFS;
-	}
-
-	err = at_cmd_write(at_buf, resp, sizeof(resp), NULL);
+	err = nrf_modem_at_cmd(resp, sizeof(resp), "AT%%XGETPDNID=%u", cid);
 	if (err) {
 		LOG_ERR("Failed to read PDN ID for CID %d, err %d", cid, err);
 		return err;
@@ -400,42 +342,21 @@ int pdn_id_get(uint8_t cid)
 int pdn_default_apn_get(char *buf, size_t len)
 {
 	int err;
-	char *p, *end;
+	char apn[64] = {0};
 
-	err = at_cmd_write("AT+CGDCONT?", at_buf, sizeof(at_buf), NULL);
-	if (err) {
+	/* +CGDCONT: 0,"family","apn.name" */
+	err = nrf_modem_at_scanf("AT+CGDCONT?",
+		"+CGDCONT: 0,\"%*[^\"]\",\"%64[^\"]\"", apn);
+	if (err < 0) {
 		LOG_ERR("Failed to read PDP contexts, err %d", err);
 		return err;
 	}
 
-	/* CGDCONT: #,"Family","APN" */
-	p = strstr(at_buf, "CGDCONT: 0");
-	if (!p) {
-		return -EBADMSG;
+	if (strlen(apn) + 1 > len) {
+		return -E2BIG;
 	}
 
-	/* Skip to the third double quote `"` */
-	for (size_t i = 0; i < 3; i++) {
-		p = strchr(p, '\"');
-		if (!p) {
-			return -EBADMSG;
-		}
-		p++;
-	}
-
-	/* Find closing quote and replace with \0 */
-	end = strchr(p, '\"');
-	if (!end) {
-		return -EBADMSG;
-	}
-	*end = '\0';
-
-	if (end - p + sizeof('\0') > len) {
-		return -ENOBUFS;
-	}
-
-	memcpy(buf, p, end - p + sizeof('\0'));
-
+	strcpy(buf, apn);
 	return 0;
 }
 
