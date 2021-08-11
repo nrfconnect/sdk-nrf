4d5b175402ea38e7ac9a8e6e3b165dbe9dfbb3e6
Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>
2021-08-11 05:43:21 -0700

    scripts: profiler: plot: Generic approach to data transfer
    This module plots data.
    Data can be either read from files or
    received from Model Creator via socket.
    
    Jira: NCSDK-10669
    
    Signed-off-by: Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>

--- scripts/profiler/plot_from_files.py
+++ scripts/profiler/plot_from_files.py
@@ -4,7 +4,6 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
 from plot_nordic import PlotNordic
-import sys
 import argparse
 import logging
 
@@ -16,15 +15,14 @@ def main():
     args = parser.parse_args()
 
     if args.log is not None:
-	    log_lvl_number = int(getattr(logging, args.log.upper(), None))
+        log_lvl_number = int(getattr(logging, args.log.upper(), None))
     else:
-	    log_lvl_number = logging.WARNING
+        log_lvl_number = logging.WARNING
 
     pn = PlotNordic(log_lvl=log_lvl_number)
     pn.read_data_from_files(args.dataset_name + ".csv",
                             args.dataset_name + ".json")
     pn.plot_events_from_file()
-    pn.log_stats('log')
 
 if __name__ == "__main__":
     main()
