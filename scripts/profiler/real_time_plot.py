c6e3c8c624b74e65242e68fb0f64964f73ae55c7
Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>
2021-08-11 05:54:37 -0700

    scripts: profiler: real_time_plot: Generic approach to data transfer
    As earlier, this is responsible for collecting, plotting
    and saving data to files.
    
    Jira: NCSDK-10669
    
    Signed-off-by: Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>

--- scripts/profiler/real_time_plot.py
+++ scripts/profiler/real_time_plot.py
@@ -3,26 +3,66 @@
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-from plot_nordic import PlotNordic
-from rtt_nordic_profiler_host import RttNordicProfilerHost
-
+from multiprocessing import Process, Event
 import argparse
-import threading
-import queue
-import sys
 import logging
+from rtt2socket import Rtt2Socket
+from model_creator import ModelCreator
+from plot_nordic import PlotNordic
 
-def rtt_thread(queue, finish_event, event_filename, event_types_filename, log_lvl_number):
-    profiler = RttNordicProfilerHost(finish_event=finish_event, queue=queue,
-                                     event_filename=event_filename,
-                                     event_types_filename=event_types_filename,
-                                     log_lvl=log_lvl_number)
-    profiler.get_events_descriptions()
-    profiler.read_events_rtt(-1)
+RTT2SOCKET_OUT_ADDR_DICT = {
+    'descriptions': '\0' + 'rtt2socket_desc',
+    'events': '\0' + 'rtt2socket_ev'
+}
+
+MODEL_CREATOR_IN_ADDR_DICT = {
+    'descriptions': '\0' + 'model_creator_desc',
+    'events': '\0' + 'model_creator_ev'
+}
+
+MODEL_CREATOR_OUT_ADDR_DICT = {
+    'descriptions': '\0' + 'model_creator_proc_desc',
+    'events': '\0' + 'model_creator_proc_ev'
+}
+
+PLOT_IN_ADDR_DICT = {
+    'descriptions': '\0' + 'plot_desc',
+    'events': '\0' + 'plot_ev'
+}
+
+def rtt2socket(event_plot, event_model_creator, log_lvl_number):
+    try:
+        rtt2s = Rtt2Socket(RTT2SOCKET_OUT_ADDR_DICT, MODEL_CREATOR_IN_ADDR_DICT, log_lvl=log_lvl_number)
+        event_plot.wait()
+        event_model_creator.wait()
+        rtt2s.read_and_transmit_data()
+    except KeyboardInterrupt:
+        rtt2s.close()
+
+def model_creator(event, dataset_name, log_lvl_number):
+    try:
+        mc = ModelCreator(MODEL_CREATOR_IN_ADDR_DICT,
+                          own_send_socket_dict=MODEL_CREATOR_OUT_ADDR_DICT,
+                          remote_socket_dict=PLOT_IN_ADDR_DICT,
+                          event_filename=dataset_name + ".csv",
+                          event_types_filename=dataset_name + ".json",
+                          log_lvl=log_lvl_number)
+        event.set()
+        mc.start()
+    except KeyboardInterrupt:
+        mc.close()
+
+def dynamic_plot(event, log_lvl_number):
+    try:
+        pn = PlotNordic(own_recv_socket_dict=PLOT_IN_ADDR_DICT, log_lvl=log_lvl_number)
+        event.set()
+        pn.plot_events_real_time()
+    except KeyboardInterrupt:
+        pn.close_event(None)
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Collecting data from Nordic profiler for given time and saving to files.')
+        description='Collecting data from Nordic profiler for given time, plotting and saving to files.')
     parser.add_argument('dataset_name', help='Name of dataset')
     parser.add_argument('--log', help='Log level')
     args = parser.parse_args()
@@ -30,18 +70,30 @@ def main():
     if args.log is not None:
         log_lvl_number = int(getattr(logging, args.log.upper(), None))
     else:
-        log_lvl_number=logging.WARNING
+        log_lvl_number = logging.INFO
 
-    ev = threading.Event()
-    que = queue.Queue()
-    t_rtt = threading.Thread(
-        target=rtt_thread,
-        args=[que, ev, args.dataset_name + ".csv",
-              args.dataset_name + ".json", log_lvl_number])
-    t_rtt.start()
+    # events are made to ensure that PlotNordic and ModelCreator classes are initialized before Rtt2Socket starts sending data
+    event_plot = Event()
+    event_model_creator = Event()
 
-    pn = PlotNordic(log_lvl=log_lvl_number)
-    pn.plot_events_real_time(que, ev, None, one_line=False)
+    try:
+        processes = []
+        processes.append(Process(target=dynamic_plot,
+                                 args=(event_plot, log_lvl_number),
+                                 daemon=True))
+        processes.append(Process(target=model_creator,
+                                 args=(event_model_creator, args.dataset_name, log_lvl_number),
+                                 daemon=True))
+        processes.append(Process(target=rtt2socket,
+                                 args=(event_plot, event_model_creator, log_lvl_number),
+                                 daemon=True))
+        for p in processes:
+            p.start()
+        for p in processes:
+            p.join()
+    except KeyboardInterrupt:
+        for p in processes:
+            p.join()
 
 if __name__ == "__main__":
     main()
