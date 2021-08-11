14204fa821e7f6c96ecc618fda4bb3e6744ffc90
Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>
2021-08-11 05:48:56 -0700

    scripts: profiler: data_collector: Generic approach to data transfer
    As earlier, this is responsible for collecting and saving data to files.
    
    Jira: NCSDK-10669
    
    Signed-off-by: Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>

--- scripts/profiler/data_collector.py
+++ scripts/profiler/data_collector.py
@@ -3,12 +3,43 @@
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-from rtt_nordic_profiler_host import RttNordicProfilerHost
-import sys
+import os
+from multiprocessing import Process, Event
 import argparse
-import logging
 import signal
-import threading
+import time
+import logging
+from rtt2socket import Rtt2Socket
+from model_creator import ModelCreator
+
+RTT2SOCKET_OUT_ADDR_DICT = {
+    'descriptions': '\0' + 'rtt2socket_desc',
+    'events': '\0' + 'rtt2socket_ev'
+}
+
+MODEL_CREATOR_IN_ADDR_DICT = {
+    'descriptions': '\0' + 'model_creator_desc',
+    'events': '\0' + 'model_creator_ev'
+}
+
+def rtt2socket(event, log_lvl_number):
+    try:
+        rtt2s = Rtt2Socket(RTT2SOCKET_OUT_ADDR_DICT, MODEL_CREATOR_IN_ADDR_DICT, log_lvl=log_lvl_number)
+        event.wait()
+        rtt2s.read_and_transmit_data()
+    except KeyboardInterrupt:
+        rtt2s.close()
+
+def model_creator(event, dataset_name, log_lvl_number):
+    try:
+        mc = ModelCreator(MODEL_CREATOR_IN_ADDR_DICT,
+                          event_filename=dataset_name + ".csv",
+                          event_types_filename=dataset_name + ".json",
+                          log_lvl=log_lvl_number)
+        event.set()
+        mc.start()
+    except KeyboardInterrupt:
+        mc.close()
 
 
 def main():
@@ -20,23 +51,31 @@ def main():
     args = parser.parse_args()
 
     if args.log is not None:
-	    log_lvl_number = int(getattr(logging, args.log.upper(), None))
+        log_lvl_number = int(getattr(logging, args.log.upper(), None))
     else:
-	    log_lvl_number = logging.DEBUG
+        log_lvl_number = logging.INFO
 
-    def sigint_handler(sig, frame):
-        end_ev.set()
+    # event is made to ensure that ModelCreator class is initialized before Rtt2Socket starts sending data
+    event = Event()
 
-    signal.signal(signal.SIGINT, sigint_handler)
-    end_ev = threading.Event()
-
-    profiler = RttNordicProfilerHost(
-                event_filename=args.dataset_name + ".csv",
-                finish_event=end_ev,
-                event_types_filename=args.dataset_name + ".json",
-                log_lvl=log_lvl_number)
-    profiler.get_events_descriptions()
-    profiler.read_events_rtt(args.time)
+    try:
+        processes = []
+        processes.append(Process(target=model_creator,
+                                 args=(event, args.dataset_name, log_lvl_number),
+                                 daemon=True))
+        processes.append(Process(target=rtt2socket,
+                                 args=(event, log_lvl_number),
+                                 daemon=True))
+        for p in processes:
+            p.start()
+        time.sleep(args.time)
+        for p in processes:
+            os.kill(p.pid, signal.SIGINT)
+        for p in processes:
+            p.join()
+    except KeyboardInterrupt:
+        for p in processes:
+            p.join()
 
 if __name__ == "__main__":
     main()
