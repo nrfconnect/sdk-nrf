dcc6abb15dfc61d5bf5eda273ace59a2bf9dcb31
Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>
2021-08-19 16:34:20 +0300

    lib: location: cellular: more corner case tolerance
    Signed-off-by: Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>

--- lib/location/method_cellular.c
+++ lib/location/method_cellular.c
@@ -49,18 +49,28 @@ void method_cellular_lte_ind_handler(const struct lte_lc_evt *const evt)
 			evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ?
 			"Connected - home network" : "Connected - roaming");
 		lte_connected = true;
-		break;		
-	case LTE_LC_EVT_NEIGHBOR_CELL_MEAS:
-		LOG_INF("Neighbor cell measurements received");
-
-		/* Copy current and neighbor cell information */
-		memcpy(&cell_data, &evt->cells_info, sizeof(struct lte_lc_cells_info));
-		memcpy(neighbor_cells, evt->cells_info.neighbor_cells,
-			sizeof(struct lte_lc_ncell) * cell_data.ncells_count);
-		cell_data.neighbor_cells = neighbor_cells;
-
-		k_sem_give(&cellmeas_data_ready);
 		break;
+	case LTE_LC_EVT_NEIGHBOR_CELL_MEAS: {
+		struct lte_lc_cells_info cells = evt->cells_info;
+		struct lte_lc_cell cur_cell = cells.current_cell;
+		
+		LOG_INF("Cell measurements results received");
+		memset(&cell_data, 0, sizeof(struct lte_lc_cells_info));
+
+		if (cur_cell.id) {
+			/* Copy current and neighbor cell information */
+			memcpy(&cell_data, &evt->cells_info, sizeof(struct lte_lc_cells_info));
+			memcpy(neighbor_cells, evt->cells_info.neighbor_cells,
+				sizeof(struct lte_lc_ncell) * cell_data.ncells_count);
+			cell_data.neighbor_cells = neighbor_cells;
+		} else {
+			LOG_INF("No current cell information from modem.");
+		}
+		if (!cells.ncells_count) {
+			LOG_INF("No neighbor cell information from modem.");
+		}
+		k_sem_give(&cellmeas_data_ready);
+	} break;
 	default:
 		break;
 	}		
@@ -68,14 +78,16 @@ void method_cellular_lte_ind_handler(const struct lte_lc_evt *const evt)
 
 static int method_cellular_ncellmeas_start(void)
 {
-	int err = lte_lc_neighbor_cell_measurement();
+	int err;
+
+	LOG_INF("Triggering start of cell measurements");
+	err = lte_lc_neighbor_cell_measurement();
 	if (err) {
 		LOG_ERR("Failed to initiate neighbor cell measurements: %d", err);
 		loc_core_event_cb_error();
 		running = false;
 		return err;
 	}
-	k_work_submit(&method_cellular_positioning_work);
 	return 0;
 }
 
@@ -86,9 +98,29 @@ static void method_cellular_positioning_work_fn(struct k_work *work)
 	int ret;
 
 	ARG_UNUSED(work);
-	k_sem_take(&cellmeas_data_ready, K_FOREVER); /* TODO: cannot be forever? interval only */
 
-	if (running) {
+	ret = method_cellular_ncellmeas_start();
+	if (ret) {
+		LOG_WRN("Cannot start cell measurements - cannot fetch location");
+		loc_core_event_cb_error();
+		running = false;		
+		return;
+	}
+
+	ret = k_sem_take(&cellmeas_data_ready, K_SECONDS(60)); /* TODO: timeout to params? */
+
+	if (ret) {
+		LOG_WRN("Timeout for waiting cell measurements - cannot fetch location");
+		loc_core_event_cb_error();
+		running = false;
+	} else if (running) {
+		if (cell_data.current_cell.id == 0) {
+			LOG_WRN("No cells were found - cannot fetch location");
+			loc_core_event_cb_error();
+			running = false;			
+			return;
+		}
+		
 		ret = multicell_location_get(&cell_data, &location);
 		if (ret) {
 			LOG_ERR("Failed to acquire location from multicell_location lib, error: %d", ret);		
@@ -98,15 +130,6 @@ static void method_cellular_positioning_work_fn(struct k_work *work)
 			location_result.latitude = location.latitude;
 			location_result.longitude = location.longitude;
 			location_result.accuracy = location.accuracy;
-			//TODO:
-			/*location_result.datetime.valid = true;
-			location_result.datetime.year = pvt_data.datetime.year;
-			location_result.datetime.month = pvt_data.datetime.month;
-			location_result.datetime.day = pvt_data.datetime.day;
-			location_result.datetime.hour = pvt_data.datetime.hour;
-			location_result.datetime.minute = pvt_data.datetime.minute;
-			location_result.datetime.second = pvt_data.datetime.seconds;
-			location_result.datetime.ms = pvt_data.datetime.ms;*/
 			(void)lte_lc_neighbor_cell_measurement_cancel();
 			if (running) {
 				loc_core_event_cb(&location_result);
@@ -118,22 +141,19 @@ static void method_cellular_positioning_work_fn(struct k_work *work)
 
 int method_cellular_location_get(const struct loc_method_config *config)
 {
-	int ret = 0;
 	const struct loc_cellular_config *cellular_config = &config->config.cellular;
 
 	ARG_UNUSED(cellular_config);
+
+	LOG_INF("Starting to get a location by using cellular method");
 
 	if (running) {
 		LOG_ERR("Previous operation on going.");
 		return -EBUSY;
 	}
 
-	ret = method_cellular_ncellmeas_start();
-	if (ret) {
-		return ret;
-	}
+	k_work_submit(&method_cellular_positioning_work);
 
-	LOG_INF("Triggered start of cell measurements");
 	running = true;
 
 	return 0;
@@ -157,6 +177,9 @@ int method_cellular_cancel(void)
 int method_cellular_init(void)
 {
 	int ret;
+	struct k_work_queue_config cfg = {
+		.name = "location_api_cellular_workq",
+	};	
 
 	lte_connected = false;
 	running = false;
@@ -164,18 +187,18 @@ int method_cellular_init(void)
 	k_work_queue_start(
 		&method_cellular_work_q, method_cellular_stack,
 		K_THREAD_STACK_SIZEOF(method_cellular_stack),
-		METHOD_CELLULAR_PRIORITY, NULL);
+		METHOD_CELLULAR_PRIORITY, &cfg);
 
 	k_work_init(&method_cellular_positioning_work, method_cellular_positioning_work_fn);
 	lte_lc_register_handler(method_cellular_lte_ind_handler);
 
-	/* TODO: fetch LTE connection status from modem?
-	 * ...because we do not know when lib is initialized
-	 */
-
 	ret = multicell_location_provision_certificate(false);
 	if (ret) {
-		LOG_ERR("Certificate provisioning failed");
+		LOG_ERR("Certificate provisioning failed, ret %d", ret);
+		if (ret == -EACCES) {
+			LOG_INF("err: -EACCESS, that might indicate that modem is in"
+				"state where cert cannot be written, i.e. not in pwroff");
+		}		
 		return ret;
 	}
 
