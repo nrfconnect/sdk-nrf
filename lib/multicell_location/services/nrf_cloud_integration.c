4368492a449decc92ad079bb80f861a19650df29
Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>
2021-08-27 15:26:44 +0300

    lib: multicell_location: compile time JWT string support
    Possibility to have JWT either run time generated (default) or as compile time pre generated string.
    
    Signed-off-by: Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>

--- lib/multicell_location/services/nrf_cloud_integration.c
+++ lib/multicell_location/services/nrf_cloud_integration.c
@@ -69,9 +69,10 @@ int location_service_get_cell_location(const struct lte_lc_cells_info *cell_data
 {
 	int err;
 	struct nrf_cloud_cell_pos_result result;
+#if defined(CONFIG_MULTICELL_LOCATION_SERVICE_NRF_CLOUD_JWT_GENERATED)
 	struct jwt_data jwt = {
 		.subject = device_id,
-		.audience = NULL,
+		.audience = NULL, /* TODO: does not work if having multiple teams in nrf cloud */
 		.exp_delta_s = (5 * 60),
 		.sec_tag = CONFIG_MULTICELL_LOCATION_NRF_CLOUD_JWT_SEC_TAG,
 		.key = JWT_KEY_TYPE_CLIENT_PRIV,
@@ -80,6 +81,7 @@ int location_service_get_cell_location(const struct lte_lc_cells_info *cell_data
 		.jwt_buf = NULL,
 		.jwt_sz = 0
 	};
+#endif
 	struct nrf_cloud_rest_context rest_ctx = {
 		.connect_socket = -1,
 		.keep_alive = false,
@@ -92,7 +94,7 @@ int location_service_get_cell_location(const struct lte_lc_cells_info *cell_data
 	const struct nrf_cloud_rest_cell_pos_request loc_req = {
 		.net_info = (struct lte_lc_cells_info *)cell_data
 	};
-
+#if defined(CONFIG_MULTICELL_LOCATION_SERVICE_NRF_CLOUD_JWT_GENERATED)
 	err = modem_jwt_generate(&jwt);
 	if (err) {
 		LOG_ERR("Failed to generate JWT, error: %d", err);
@@ -105,7 +107,12 @@ int location_service_get_cell_location(const struct lte_lc_cells_info *cell_data
 
 	modem_jwt_free(jwt.jwt_buf);
 	jwt.jwt_buf = NULL;
+#else
+	BUILD_ASSERT(IS_ENABLED(CONFIG_MULTICELL_LOCATION_SERVICE_NRF_CLOUD_JWT_COMPILE_TIME));
 
+	rest_ctx.auth = CONFIG_MULTICELL_LOCATION_SERVICE_NRF_CLOUD_JWT_STRING;
+	err = nrf_cloud_rest_cell_pos_get(&rest_ctx, &loc_req, &result);
+#endif
 	if (!err) {
 		location->accuracy = (double)result.unc;
 		location->latitude = result.lat;
