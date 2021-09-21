c1657fc0b1b35618a8ad276ccd880bec2c36d26f
Simen S. Røstad <simen.rostad@nordicsemi.no>
2021-09-21 15:25:23 +0200

    applications: asset_tracker_v2: Enable debug module when using memfault
    The memfault integration in Asset Tracker v2 depends on the
    debug module to work properly. Add `CONFIG_DEBUG_MODULE=y` to
    `overlay-memfault.conf` to ensure this.
    
    NCSDK-11445
    
    Signed-off-by: Simen S. Røstad <simen.rostad@nordicsemi.no>

--- applications/asset_tracker_v2/configuration/memfault/memfault_platform_config.h
+++ applications/asset_tracker_v2/configuration/memfault/memfault_platform_config.h
@@ -6,6 +6,8 @@
  * "<NCS folder>/modules/lib/memfault-firmware-sdk/components/include/memfault/default_config.h"
  */
 
+#if defined(CONFIG_DEBUG_MODULE)
  /* Prepare captured metric data for upload to Memfault cloud every configured interval. */
 #define MEMFAULT_METRICS_HEARTBEAT_INTERVAL_SECS CONFIG_DEBUG_MODULE_MEMFAULT_HEARTBEAT_INTERVAL_SEC
 #define MEMFAULT_DATA_EXPORT_CHUNK_MAX_LEN CONFIG_DEBUG_MODULE_MEMFAULT_CHUNK_SIZE_MAX
+#endif /* defined(CONFIG_DEBUG_MODULE) */
