80d5924e04eea0b8662e9101bbd3e5f08e0aac98
Emanuele Di Santo <emdi@nordicsemi.no>
2021-09-29 10:44:20 +0200

    lib: modem_jwt: update to use new AT APIs
    Update library to use new APIs from libmodem.
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>
    Signed-off-by: Seppo Takalo <seppo.takalo@nordicsemi.no>

--- tests/lib/modem_jwt/src/jwt_test.c
+++ tests/lib/modem_jwt/src/jwt_test.c
@@ -9,7 +9,7 @@
 #include <string.h>
 #include <kernel.h>
 #include <modem/modem_jwt.h>
-#include <mock_at_cmd.h>
+#include <mock_nrf_modem_at.h>
 
 
 void setUp(void)
@@ -37,19 +37,15 @@ static const char jwt_token[] =
 	"eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6ImU2YzM1Y2VhMWY1NDFhNGJlNDg1ZmY4ZGIyMWY1MWRmZTcyNDJlN2NhN2I5NDU2YzkwZjY5ODE1ZTc3ZWE2MWYifSAg.eyJpc3MiOiJuUkY5MTYwLjUwMzYzMTU0LTM5MzAtNGQ5Ni04MGY5LTEzMTIxY2YzNWFkYyIsImp0aSI6Im5SRjkxNjAuZjcyZDk3ODktMmU3My00NzgwLTkyNDgtYjM5NTJmODliODcyLjk0MmQ5OWI5MDNhY2EyYzNkZDY0OWI1ZDYxMjMyMmRjIiwic3ViIjoiNTAzNjMxNTQtMzkzMC00ZDk2LTgwZjktMTMxMjFjZjM1YWRjIn0g.tW2wG-sSE01jtrcT9JmFvPVIvEB99jUGGHIUCCH65fG5HtBrisOD1cl0L49cuyo2OE6XXvI9wdKSASxDQPJ8lw"
 	"\"";
 
-int cmd_cb(const char * const cmd, char *buf, size_t buf_len, enum at_cmd_state *state,
-	   int cmock_num_calls)
+int cmd_cb(void *buf, size_t len, const char *fmt, int cmock_num_calls)
 {
 	if (cmock_num_calls == 0) {
 		/* Fail first call */
-		return AT_CMD_ERROR;
-	} else if (cmock_num_calls == 1) {
-		TEST_ASSERT_EQUAL_STRING("AT%JWT=0,0,\"\",\"\"", cmd);
-	} else if (cmock_num_calls == 2) {
-		TEST_ASSERT_EQUAL_STRING("AT%JWT=0,100,\"sub\",\"aud\",10,2", cmd);
+		return -1;
 	}
-	strncpy(buf, jwt_token, buf_len - 1);
-	buf[buf_len-1] = 0;
+
+	strncpy(buf, jwt_token, len - 1);
+	((char *)buf)[len-1] = 0;
 	return 0;
 }
 
@@ -70,17 +66,13 @@ void test_modem_jwt_generate(void)
 
 	jwt.jwt_buf = NULL;
 
-	__wrap_at_cmd_init_ExpectAndReturn(-1);
-	rc = modem_jwt_generate(&jwt);
-	TEST_ASSERT_EQUAL_INT(-EPIPE, rc);
+	__wrap_nrf_modem_at_cmd_Stub(cmd_cb);
 
-	__wrap_at_cmd_init_ExpectAndReturn(0);
-	__wrap_at_cmd_write_Stub(cmd_cb);
+	/* First call should fail, pass through the error code */
 	rc = modem_jwt_generate(&jwt);
-	TEST_ASSERT_EQUAL_INT(-EBADMSG, rc);
+	TEST_ASSERT_EQUAL_INT(-1, rc);
 
 	/* Success case, default parameters */
-	__wrap_at_cmd_init_ExpectAndReturn(0);
 	rc = modem_jwt_generate(&jwt);
 	TEST_ASSERT_EQUAL_INT(0, rc);
 
@@ -93,7 +85,6 @@ void test_modem_jwt_generate(void)
 	jwt.exp_delta_s = 100;
 	jwt.sec_tag = 10;
 	jwt.key = 2;
-	__wrap_at_cmd_init_ExpectAndReturn(0);
 	rc = modem_jwt_generate(&jwt);
 	TEST_ASSERT_EQUAL_INT(0, rc);
 }
@@ -104,7 +95,6 @@ void test_modem_jwt_get_uuids(void)
 	struct nrf_modem_fw_uuid mfw = {0};
 	int rc;
 
-	__wrap_at_cmd_init_IgnoreAndReturn(0);
 	rc = modem_jwt_get_uuids(&dev, &mfw);
 	TEST_ASSERT_EQUAL_INT(0, rc);
 	TEST_ASSERT_EQUAL_STRING("50363154-3930-4d96-80f9-13121cf35adc", dev.str);
