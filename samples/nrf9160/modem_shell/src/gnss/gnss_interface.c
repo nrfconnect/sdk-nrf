cc6c69c6654f2a12308833087be957a4f35c2c56
Tuomas Hiltunen <tuomas.hiltunen@nordicsemi.no>
2021-09-30 12:34:22 +0300

    samples: nrf9160: modem_shell: always set gnss command event handler
    Set the modem gnss event handler every time a MoSh gnss command is
    given. Previously this did take place if gnss was already initialized.
    
    Signed-off-by: Tuomas Hiltunen <tuomas.hiltunen@nordicsemi.no>

--- samples/nrf9160/modem_shell/src/gnss/gnss_interface.c
+++ samples/nrf9160/modem_shell/src/gnss/gnss_interface.c
@@ -832,57 +832,51 @@ static void pgps_event_handler(struct nrf_cloud_pgps_event *event)
 
 static void gnss_api_init(void)
 {
-	int err;
 	static bool gnss_api_initialized;
 
 	if (gnss_api_initialized) {
+		/* Reset event handler in case some other handler was set in the meantime */
+		(void)nrf_modem_gnss_event_handler_set(gnss_event_handler);
 		return;
 	}
 
 	/* Activate GNSS API v2 */
-	err = nrf_modem_gnss_init();
+	(void)nrf_modem_gnss_init();
+	(void)nrf_modem_gnss_event_handler_set(gnss_event_handler);
 
-	if (!err) {
-		err = nrf_modem_gnss_event_handler_set(gnss_event_handler);
-	}
+	gnss_api_initialized = true;
 
-	if (!err) {
-		gnss_api_initialized = true;
+	struct k_work_queue_config gnss_work_q_config = {
+		.name = "gnss_workq",
+		.no_yield = false
+	};
 
-		struct k_work_queue_config gnss_work_q_config = {
-			.name = "gnss_workq",
-			.no_yield = false
-		};
+	k_work_queue_start(
+		&gnss_work_q,
+		gnss_workq_stack_area,
+		K_THREAD_STACK_SIZEOF(gnss_workq_stack_area),
+		GNSS_WORKQ_THREAD_PRIORITY,
+		&gnss_work_q_config);
 
-		k_work_queue_start(
-			&gnss_work_q,
-			gnss_workq_stack_area,
-			K_THREAD_STACK_SIZEOF(gnss_workq_stack_area),
-			GNSS_WORKQ_THREAD_PRIORITY,
-			&gnss_work_q_config);
-
-		k_work_init(&gnss_stop_work, stop_gnss_work_fn);
-		k_work_init_delayable(&gnss_start_work, start_gnss_work_fn);
-		k_work_init_delayable(&gnss_timeout_work, handle_timeout_work_fn);
+	k_work_init(&gnss_stop_work, stop_gnss_work_fn);
+	k_work_init_delayable(&gnss_start_work, start_gnss_work_fn);
+	k_work_init_delayable(&gnss_timeout_work, handle_timeout_work_fn);
 
 #if defined(CONFIG_NRF_CLOUD_AGPS) || defined(CONFIG_SUPL_CLIENT_LIB)
-		k_work_init(&get_agps_data_work, get_agps_data);
+	k_work_init(&get_agps_data_work, get_agps_data);
 #endif /* CONFIG_NRF_CLOUD_AGPS || CONFIG_SUPL_CLIENT_LIB */
 
 #if defined(CONFIG_SUPL_CLIENT_LIB)
-		static struct supl_api supl_api = {
-			.read = supl_read,
-			.write = supl_write,
-			.handler = inject_agps_data,
-			.logger = NULL, /* set to "supl_logger" to enable logging */
-			.counter_ms = k_uptime_get
-		};
+	static struct supl_api supl_api = {
+		.read = supl_read,
+		.write = supl_write,
+		.handler = inject_agps_data,
+		.logger = NULL, /* set to "supl_logger" to enable logging */
+		.counter_ms = k_uptime_get
+	};
 
-		(void)supl_init(&supl_api);
+	(void)supl_init(&supl_api);
 #endif /* CONFIG_SUPL_CLIENT_LIB */
-	} else {
-		mosh_error("GNSS: Failed to initialize GNSS API");
-	}
 }
 
 int gnss_start(void)
