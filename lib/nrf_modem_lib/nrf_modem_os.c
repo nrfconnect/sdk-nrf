nrfconnect/sdk-nrf

commit fcc9ccd77c47f93e201c5927376d4161de92c9c8

lib: nrf_modem_lib: Conditionally disable trace_proxy_irq_handler
trace_proxy_irq_handler is using an IRQ that other users also want to
use. For instance the test zephyr/tests/kernel/gen_isr_table/ wants to
use this IRQ.

I don't see why we need to enable the IRQ when trace is disabled. So
this patch disables it when NRF_MODEM_LIB_TRACE_ENABLED=n.

NCSDK-11344

Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

lib/nrf_modem_lib/nrf_modem_os.c
@@ -452,6 +452,7 @@ ISR_DIRECT_DECLARE(rpc_proxy_irq_handler)
 	return 1; /* We should check if scheduling decision should be made */
 }
 
+#ifdef NRF_MODEM_LIB_TRACE_ENABLED
 ISR_DIRECT_DECLARE(trace_proxy_irq_handler)
 {
 	/*
@@ -470,6 +471,7 @@ void trace_task_create(void)
 			   trace_proxy_irq_handler, UNUSED_FLAGS);
 	irq_enable(TRACE_IRQ);
 }
+#endif
 
 void read_task_create(void)
 {
@@ -479,9 +481,9 @@ void read_task_create(void)
 	irq_enable(NRF_MODEM_APPLICATION_IRQ);
 }
 
+#ifdef CONFIG_NRF_MODEM_LIB_TRACE_MEDIUM_UART
 void trace_uart_init(void)
 {
-#ifdef CONFIG_NRF_MODEM_LIB_TRACE_MEDIUM_UART
 	/* UART pins are defined in "nrf9160dk_nrf9160.dts". */
 	const nrfx_uarte_config_t config = {
 		/* Use UARTE1 pins routed on VCOM2. */
@@ -502,18 +504,16 @@ void trace_uart_init(void)
 	/* Initialize nrfx UARTE driver in blocking mode. */
 	/* TODO: use UARTE in non-blocking mode with IRQ handler. */
 	nrfx_uarte_init(&uarte_inst, &config, NULL);
-#endif
 }
+#endif
 
 #ifdef CONFIG_NRF_MODEM_LIB_TRACE_MEDIUM_RTT
 #define RTT_BUF_SZ		(CONFIG_NRF_MODEM_LIB_TRACE_MEDIUM_RTT_BUF_SIZE)
 static int trace_rtt_channel;
 static char rtt_buffer[RTT_BUF_SZ];
-#endif
 
 static void trace_rtt_init(void)
 {
-#ifdef CONFIG_NRF_MODEM_LIB_TRACE_MEDIUM_RTT
 	trace_rtt_channel = SEGGER_RTT_AllocUpBuffer("modem_trace", rtt_buffer,
 		sizeof(rtt_buffer), SEGGER_RTT_MODE_NO_BLOCK_SKIP);
 
@@ -524,8 +524,8 @@ static void trace_rtt_init(void)
 		LOG_INF("RTT channel for modem trace is now %d",
 			trace_rtt_channel);
 	}
-#endif
 }
+#endif
 
 void *nrf_modem_os_alloc(size_t bytes)
 {
@@ -639,9 +639,17 @@ void nrf_modem_os_init(void)
 	read_task_create();
 
 	/* Configure and enable modem tracing over UART and RTT. */
+#ifdef CONFIG_NRF_MODEM_LIB_TRACE_MEDIUM_UART
 	trace_uart_init();
+#endif
+
+#ifdef CONFIG_NRF_MODEM_LIB_TRACE_MEDIUM_RTT
 	trace_rtt_init();
+#endif
+
+#ifdef NRF_MODEM_LIB_TRACE_ENABLED
 	trace_task_create();
+#endif
 
 	memset(&heap_diag, 0x00, sizeof(heap_diag));
 	memset(&shmem_diag, 0x00, sizeof(shmem_diag));

commit dae033e78d1938d1b86638e52dde03bd0a1bc13d

lib: nrf_modem: fix tracing
Trace functionality was broken in fcc9ccd77c47,
NRF_MODEM_LIB_TRACE_ENABLED should have been
CONFIG_NRF_MODEM_LIB_TRACE_ENABLED instead.

Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>

lib/nrf_modem_lib/nrf_modem_os.c
@@ -452,7 +452,7 @@ ISR_DIRECT_DECLARE(rpc_proxy_irq_handler)
 	return 1; /* We should check if scheduling decision should be made */
 }
 
-#ifdef NRF_MODEM_LIB_TRACE_ENABLED
+#ifdef CONFIG_NRF_MODEM_LIB_TRACE_ENABLED
 ISR_DIRECT_DECLARE(trace_proxy_irq_handler)
 {
 	/*
@@ -647,7 +647,7 @@ void nrf_modem_os_init(void)
 	trace_rtt_init();
 #endif
 
-#ifdef NRF_MODEM_LIB_TRACE_ENABLED
+#ifdef CONFIG_NRF_MODEM_LIB_TRACE_ENABLED
 	trace_task_create();
 #endif
 