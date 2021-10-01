oyvindronningstad/fw-nrfconnect-nrf

hw_unique_key: Improve HUK loading in the immutable bootloader
Add a separate config CONFIG_HW_UNIQUE_KEY_LOAD which doesn't enable
as many other configs, to give access only to the KDR loading API.

Modify the immutable bootloader to allow a single boot without
a HUK present. The bootloader writes the last word of the HUK flash
page to 0, to signify that it should fail if a HUK is not present.

This allows the application to write a random HUK. Random number
generation requires quite a bit of code, making it infeasible to add
to the bootloader. Therefore, this change removes the requirement to
either
 - Flash random_hw_unique_key without the bootloader, boot it, and
   then flash the bootloader and app, or
 - Generate a key off chip and programming it together with the
   bootloader and app.

Ref: NCSDK-9034

Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

samples/bootloader/src/main.c
@@ -12,19 +12,29 @@
 #include <bl_storage.h>
 #include <bl_boot.h>
 #include <bl_validation.h>
+#include <nrfx_nvmc.h>
 
-#if defined(CONFIG_HW_UNIQUE_KEY)
+#if defined(CONFIG_HW_UNIQUE_KEY_LOAD)
 #include <init.h>
 #include <hw_unique_key.h>
+
+#define HUK_FLAG_OFFSET 0xFFC /* When this word is set, expect HUK to be written. */
 
 int load_huk(const struct device *unused)
 {
 	(void)unused;
 
 	if (!hw_unique_key_is_written(HUK_KEYSLOT_KDR)) {
+		uint32_t huk_flag = PM_HW_UNIQUE_KEY_PARTITION_ADDRESS + HUK_FLAG_OFFSET;
+		if (*(uint32_t *)huk_flag == 0xFFFFFFFF) {
+			printk("First boot, expecting app to write HUK.\n");
+			nrfx_nvmc_word_write(huk_flag, 0);
+			return 0;
+		}
 		printk("Error: Hardware Unique Key not present.\n");
 		k_panic();
 		return -1;
+
 	}
 
 	hw_unique_key_load_kdr();

nrfconnect/sdk-nrf

samples/bootloader/src/main.c
@@ -12,19 +12,30 @@
 #include <bl_storage.h>
 #include <bl_boot.h>
 #include <bl_validation.h>
+#include <nrfx_nvmc.h>
 
-#if defined(CONFIG_HW_UNIQUE_KEY)
+#if defined(CONFIG_HW_UNIQUE_KEY_LOAD)
 #include <init.h>
 #include <hw_unique_key.h>
+
+#define HUK_FLAG_OFFSET 0xFFC /* When this word is set, expect HUK to be written. */
 
 int load_huk(const struct device *unused)
 {
 	(void)unused;
 
 	if (!hw_unique_key_is_written(HUK_KEYSLOT_KDR)) {
+		uint32_t huk_flag_addr = PM_HW_UNIQUE_KEY_PARTITION_ADDRESS + HUK_FLAG_OFFSET;
+
+		if (*(uint32_t *)huk_flag_addr == 0xFFFFFFFF) {
+			printk("First boot, expecting app to write HUK.\n");
+			nrfx_nvmc_word_write(huk_flag_addr, 0);
+			return 0;
+		}
 		printk("Error: Hardware Unique Key not present.\n");
 		k_panic();
 		return -1;
+
 	}
 
 	hw_unique_key_load_kdr();