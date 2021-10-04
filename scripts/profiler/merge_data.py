f8854ae8118543d5e6c11a131e48f1f24089764e
Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>
2021-10-04 02:18:03 -0700

    scripts: profiler: merge_data: Generic approach to data transfer
    Comply to new format of data.
    Only timestamp of submitting a sync event is used
    to synchronize events, even if sync event is tracked.
    
    Jira: NCSDK-10669
    
    Signed-off-by: Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>

--- scripts/profiler/merge_data.py
+++ scripts/profiler/merge_data.py
@@ -3,7 +3,8 @@
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-from events import EventsData, Event, EventType
+from processed_events import ProcessedEvents
+from events import Event, EventType, TrackedEvent
 import argparse
 import numpy as np
 
@@ -18,14 +19,14 @@ def sync_peripheral_ts(ts_peripheral, sync_ts_peripheral, sync_ts_central):
 
     new_sync_ts_peripheral = predictor(sync_ts_peripheral)
     mse = np.square(np.subtract(sync_ts_central, new_sync_ts_peripheral)).mean()
-
     if mse < MSE_THRESH:
         print('Use linear regression')
-        return predictor(ts_peripheral)
+        return predictor(ts_peripheral[list(list(elem is not None for elem in row) for row in ts_peripheral)])
     else:
         print('Use piecewise linear interpolation')
-        return np.interp(ts_peripheral, sync_ts_peripheral, sync_ts_central,
-                         left=-1, right=-1)
+        return np.interp(np.array(ts_peripheral[list(list(elem is not None for elem in row) for row in ts_peripheral)],
+                                  dtype='float64'),
+                         sync_ts_peripheral, sync_ts_central, left=-1, right=-1)
 
 
 def main():
@@ -42,27 +43,28 @@ def main():
     parser.add_argument("result_dataset", help="Name for result dataset")
     args = parser.parse_args()
 
-    evt_peripheral = EventsData([], {})
+    evt_peripheral = ProcessedEvents()
     evt_peripheral.read_data_from_files(args.peripheral_dataset + ".csv",
                                         args.peripheral_dataset + ".json")
 
-    evt_central = EventsData([], {})
+    evt_central = ProcessedEvents()
     evt_central.read_data_from_files(args.central_dataset + ".csv",
                                      args.central_dataset + ".json")
 
     # Compensating clock drift - based on synchronization events
-    sync_evt_peripheral = evt_peripheral.get_event_type_id(
-            args.peripheral_sync_event)
+    sync_evt_peripheral = evt_peripheral.get_event_type_id(args.peripheral_sync_event)
     sync_evt_central = evt_central.get_event_type_id(args.central_sync_event)
 
-    sync_peripheral = list(filter(lambda x: x.type_id == sync_evt_peripheral,
-                             evt_peripheral.events))
-    sync_central = list(filter(lambda x: x.type_id == sync_evt_central,
-                              evt_central.events))
+    sync_peripheral = list(filter(lambda x: x.submit.type_id == sync_evt_peripheral,
+                                  evt_peripheral.tracked_events))
 
-    ts_peripheral = list(map(lambda x: x.timestamp, evt_peripheral.events))
-    sync_ts_peripheral = list(map(lambda x: x.timestamp, sync_peripheral))
-    sync_ts_central = list(map(lambda x: x.timestamp, sync_central))
+    sync_central = list(filter(lambda x: x.submit.type_id == sync_evt_central,
+                               evt_central.tracked_events))
+
+    ts_peripheral = np.array(list(map(lambda x: list([x.submit.timestamp, x.proc_start_time, x.proc_end_time]),
+                                      evt_peripheral.tracked_events)))
+    sync_ts_peripheral = list(map(lambda x: x.submit.timestamp, sync_peripheral))
+    sync_ts_central = list(map(lambda x: x.submit.timestamp, sync_central))
 
     sync_diffs_central = np.subtract(sync_ts_central[1:], sync_ts_central[:-1])
     sync_diffs_peripheral = np.subtract(sync_ts_peripheral[1:], sync_ts_peripheral[:-1])
@@ -83,20 +85,19 @@ def main():
     elif len(sync_ts_peripheral) < len(sync_ts_central):
         sync_ts_central = sync_ts_central[:len(sync_ts_peripheral)]
 
-    new_ts_peripheral = sync_peripheral_ts(ts_peripheral,
-                                           sync_ts_peripheral,
-                                           sync_ts_central)
+    new_ts_peripheral = ts_peripheral.copy()
+    new_ts_peripheral[list(list(elem is not None for elem in row) for row in new_ts_peripheral)] = \
+        sync_peripheral_ts(ts_peripheral, sync_ts_peripheral, sync_ts_central)
     assert len(new_ts_peripheral) == len(ts_peripheral)
-
     # Reindexing, renaming and compensating time differences for peripheral events
     max_central_id = max([int(i) for i in evt_central.registered_events_types])
 
-    assert len(new_ts_peripheral) == len(evt_peripheral.events)
-    evt_peripheral.events = list(map(lambda x, y:
-                                 Event(x.type_id + max_central_id + 1,
-                                       y,
-                                       x.data),
-                                 evt_peripheral.events, new_ts_peripheral))
+    assert len(new_ts_peripheral) == len(evt_peripheral.tracked_events)
+    evt_peripheral.tracked_events = list(map(lambda x, y:
+                                         TrackedEvent(Event(x.submit.type_id + max_central_id + 1, y[0], x.submit.data),
+                                                      y[1],
+                                                      y[2]),
+                                             evt_peripheral.tracked_events, new_ts_peripheral))
 
     evt_peripheral.registered_events_types = {k + max_central_id + 1 :
                 EventType(v.name + "_peripheral", v.data_types, v.data_descriptions)
@@ -111,19 +112,22 @@ def main():
     TIME_DIFF = 0.5
     start_time = sync_ts_central[0] - TIME_DIFF
     end_time = sync_ts_central[-1] + TIME_DIFF
-    evt_peripheral.events = list(filter(lambda x:
-        x.timestamp >= start_time and x.timestamp <= end_time,
-        evt_peripheral.events))
+    evt_peripheral.tracked_events = list(filter(lambda x:
+        x.submit.timestamp >= start_time and (x.proc_end_time <= end_time if x.proc_end_time is not None \
+            else x.submit.timestamp <= end_time),
+        evt_peripheral.tracked_events))
 
-    evt_central.events = list(filter(lambda x:
-        x.timestamp >= start_time and x.timestamp <= end_time,
-        evt_central.events))
+    evt_central.tracked_events = list(filter(lambda x:
+        x.submit.timestamp >= start_time and (x.proc_end_time <= end_time if x.proc_end_time is not None \
+            else x.submit.timestamp <= end_time),
+        evt_central.tracked_events))
 
     all_registered_events_types = evt_peripheral.registered_events_types.copy()
     all_registered_events_types.update(evt_central.registered_events_types)
 
-    result_events = EventsData(evt_peripheral.events + evt_central.events,
-                               all_registered_events_types)
+    result_events = ProcessedEvents()
+    result_events.tracked_events = evt_peripheral.tracked_events + evt_central.tracked_events
+    result_events.registered_events_types = all_registered_events_types
 
     result_events.write_data_to_files(args.result_dataset + ".csv",
                                       args.result_dataset + ".json")
