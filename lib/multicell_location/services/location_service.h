94164a6b44afff7f50e1d762cf4bd9840ccda883
Tommi Rantanen <tommi.rantanen@nordicsemi.no>
2021-10-15 07:22:13 +0300

    lib: multicell_location: Use REST client library
    Taking new REST client library into use for HTTP REST requests.
    Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

--- lib/multicell_location/services/location_service.h
+++ lib/multicell_location/services/location_service.h
@@ -15,44 +15,16 @@
 extern "C" {
 #endif
 
-/* @brief Generate an HTTPS request in the format the location service expects.
- *
- * @param cell_data Pointer to neighbor cell data.
- * @param device_id Unique device identifier.
- * @param buf Buffer for storing the HTTP request.
- * @param buf_len Length of the provided buffer.
- *
- * @return 0 on success, or negative error code on failure.
- */
-int location_service_generate_request(const struct lte_lc_cells_info *cell_data,
-				      const char *const device_id,
-				      char *buf, size_t buf_len);
-
-/* @brief Get pointer to the location service's null-terminated hostname.
- *
- * @return A pointer to null-terminated hostname in case of success,
- *	   or NULL in case of failure.
- */
-const char *location_service_get_hostname(void);
-
-/* @brief Get pointer to certificate to location service.
+/**
+ * @brief Get pointer to certificate to location service.
  *
  * @return A pointer to null-terminated root CA certificate in case of success,
- *	   or NULL in case of failure.
+ *         or NULL in case of failure.
  */
 const char *location_service_get_certificate(void);
 
-/* @brief Parse a response from a location service, and populate the provided
- *	  location structure.
- *
- * @param response Response from location service.
- * @param location Storage for the result from response parsing.
- *
- * @return 0 on success, or -1 on failure.
- */
-int location_service_parse_response(const char *response, struct multicell_location *location);
-
-/* @brief Generate location request, send, and parse response.
+/**
+ * @brief Generate location request, send, and parse response.
  *
  * @param cell_data Pointer to neighbor cell data.
  * @param device_id Unique device identifier.
