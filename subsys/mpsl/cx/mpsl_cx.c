137a33a1f8c417e02d9bca509e8fb17c3d54ba19
Jonathan Rico <jonathan.rico@nordicsemi.no>
2021-10-18 12:22:55 +0200

    mpsl: add devicetree bindings for 3-wire coexistence
    This adds devicetree bindings for a generic 3-wire radio coexistence
    interface, and a superset for the Bluetooth-compatible implementation.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- subsys/mpsl/cx/mpsl_cx.c
+++ subsys/mpsl/cx/mpsl_cx.c
@@ -10,12 +10,19 @@
 #if IS_ENABLED(CONFIG_MPSL_CX_THREAD)
 #include <mpsl/mpsl_cx_config_thread.h>
 
+#if DT_NODE_HAS_STATUS(DT_PHANDLE(DT_NODELABEL(radio), coex), okay)
+#define CX_NODE DT_PHANDLE(DT_NODELABEL(radio), coex)
+#else
+#define CX_NODE DT_INVALID_NODE
+#error No enabled coex nodes registered in DTS.
+#endif
+
 static int cx_thread_configure(void)
 {
 	struct mpsl_cx_thread_interface_config cfg = {
-		.request_pin  = CONFIG_MPSL_CX_THREAD_PIN_REQUEST,
-		.priority_pin = CONFIG_MPSL_CX_THREAD_PIN_PRIORITY,
-		.granted_pin  = CONFIG_MPSL_CX_THREAD_PIN_GRANT,
+		.request_pin  = NRF_DT_GPIOS_TO_PSEL(CX_NODE, req_gpios),
+		.priority_pin = NRF_DT_GPIOS_TO_PSEL(CX_NODE, pri_dir_gpios),
+		.granted_pin  = NRF_DT_GPIOS_TO_PSEL(CX_NODE, grant_gpios),
 	};
 
 	return mpsl_cx_thread_interface_config_set(&cfg);
