tejlmand/fw-nrfconnect-nrf

partition_manager: use ncs_file(CONF_FILES ...) to lookup board layouts
Using `ncs_file(CONF_FILES <path> PM <list> ...)` to obtain a board
specific static partition manager layout file.

This means users can now create a static partition file, such as
pm_static_nrf52840dongle_nrf52840.yml instead of placing code like the
following in `<sample>/CMakeLists.txt`:

```
if (EXISTS "configuration/${BOARD}/pm_static.yml"
    AND ${BOARD} STREQUAL "nrf52840dongle_nrf52840")
  set(PM_STATIC_YML_FILE configuration/${BOARD}/pm_static.yml)
endif()
```

As partition manager static configurations are now looked up using
similar pattern to that of Kconfig fragments and overlay files, the
path to the selected pm_static file will be printed.

Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

cmake/partition_manager.cmake
@@ -26,8 +26,16 @@ set(user_def_pm_static ${PM_STATIC_YML_FILE})
 set(nodomain_pm_static ${APPLICATION_SOURCE_DIR}/pm_static.yml)
 set(domain_pm_static ${APPLICATION_SOURCE_DIR}/pm_static_${DOMAIN}.yml)
 
+ncs_file(CONF_FILES ${APPLICATION_SOURCE_DIR}
+         PM board_pm_static
+         DOMAIN ${DOMAIN}
+         BUILD ${CONF_FILE_BUILD_TYPE}
+)
+
 if(EXISTS "${user_def_pm_static}" AND NOT IS_DIRECTORY "${user_def_pm_static}")
   set(static_configuration_file ${user_def_pm_static})
+elseif (EXISTS ${board_pm_static})
+  set(static_configuration_file ${board_pm_static})
 elseif (EXISTS ${domain_pm_static})
   set(static_configuration_file ${domain_pm_static})
 elseif (EXISTS ${nodomain_pm_static})
@@ -35,6 +43,9 @@ elseif (EXISTS ${nodomain_pm_static})
 endif()
 
 if (EXISTS ${static_configuration_file})
+  message(STATUS "Found partition manager static configuration: "
+                 "${static_configuration_file}"
+  )
   set(static_configuration --static-config ${static_configuration_file})
 endif()
 

nrfconnect/sdk-nrf

cmake/partition_manager.cmake
@@ -26,15 +26,34 @@ set(user_def_pm_static ${PM_STATIC_YML_FILE})
 set(nodomain_pm_static ${APPLICATION_CONFIG_DIR}/pm_static.yml)
 set(domain_pm_static ${APPLICATION_CONFIG_DIR}/pm_static_${DOMAIN}.yml)
 
+ncs_file(CONF_FILES ${APPLICATION_CONFIG_DIR}
+         PM board_pm_static
+         DOMAIN ${DOMAIN}
+         BUILD ${CONF_FILE_BUILD_TYPE}
+)
+
+ncs_file(CONF_FILES ${BOARD_DIR}
+         PM board_def_pm_static
+         DOMAIN ${DOMAIN}
+         BUILD ${CONF_FILE_BUILD_TYPE}
+)
+
 if(EXISTS "${user_def_pm_static}" AND NOT IS_DIRECTORY "${user_def_pm_static}")
   set(static_configuration_file ${user_def_pm_static})
+elseif (EXISTS ${board_pm_static})
+  set(static_configuration_file ${board_pm_static})
 elseif (EXISTS ${domain_pm_static})
   set(static_configuration_file ${domain_pm_static})
 elseif (EXISTS ${nodomain_pm_static})
   set(static_configuration_file ${nodomain_pm_static})
+elseif (EXISTS ${board_def_pm_static})
+  set(static_configuration_file ${board_def_pm_static})
 endif()
 
 if (EXISTS ${static_configuration_file})
+  message(STATUS "Found partition manager static configuration: "
+                 "${static_configuration_file}"
+  )
   set(static_configuration --static-config ${static_configuration_file})
 endif()
 