fd53c26f0cc3179bb2fab11fe06163cf122ba237
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2021-10-27 13:01:59 +0200

    doc: add support for Google Analytics
    Add utility functions and scripts to embed Google Analytics to all
    docsets.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

---
+++ doc/_static/js/ga-tracker.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021 Nordic Semiconductor ASA
+// SPDX-License-Identifier: Apache-2.0
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {dataLayer.push(arguments);}
+
+gtag('js', new Date());
+gtag('config', 'G-4X57FZCTCL');
