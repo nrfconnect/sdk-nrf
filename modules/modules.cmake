c052d39ee239ff59f80b1ef39fa8be446233cb69
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2021-11-15 16:35:21 +0100

    cmake: modules: conditional sourcing of Kconfig for NCS extended modules
    nRF Connect SDK may extend Kconfig files for Zephyr modules.
    Extending Kconfig files located outside of sdk-nrf or Zephyr may contain
    dependencies to Kconfig symbol in the external repo.
    
    This creates a hard dependency to such repo, resulting in errors if
    nRF Connect SDK downstream users tries to remove such project from their
    downstream manifest.
    > warning: <SETTING> (defined at Kconfig:<n>) defined with type unknown
    >
    > warning: <SETTING> (defined at Kconfig:<n>) defined without a prompt
    >
    > error: Aborting due to Kconfig warnings
    
    This commit introduces a principle for only sourcing nRF Connect SDK
    module extensions when the external module Kconfig is available.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- modules/modules.cmake
+++ modules/modules.cmake
@@ -6,3 +6,28 @@ set(ZEPHYR_NRFXLIB_CMAKE_DIR  ${CMAKE_CURRENT_LIST_DIR}/nrfxlib)
 set(ZEPHYR_CJSON_CMAKE_DIR    ${CMAKE_CURRENT_LIST_DIR}/cjson)
 set(ZEPHYR_CJSON_KCONFIG      ${CMAKE_CURRENT_LIST_DIR}/cjson/Kconfig)
 set(ZEPHYR_TRUSTED_FIRMWARE_M_KCONFIG ${CMAKE_CURRENT_LIST_DIR}/trusted-firmware-m/Kconfig)
+
+# Those are modules with Kconfig tree's inside the module repo but where
+# nRF Connect SDK extend those trees.
+# This means the extended Kconfig module file refers to external symbols causing
+# Kconfig parsing to fail if referred module is not available.
+# Thus we set the module to `-NOTFOUND` if unavailable to avoid sourcing the
+# Kconfig file having undefined symbols.
+
+set(NCS_KCONFIG_EXTENDED_MODULES "")
+
+foreach(module_name ${NCS_KCONFIG_EXTENDED_MODULES})
+  zephyr_string(SANITIZE TOUPPER MODULE_NAME_UPPER ${module_name})
+  if(${module_name} IN_LIST ZEPHYR_MODULE_NAMES)
+    set(NCS_${MODULE_NAME_UPPER}_KCONFIG
+        "${CMAKE_CURRENT_LIST_DIR}/${module_name}/Kconfig"
+    )
+  else()
+    set(NCS_${MODULE_NAME_UPPER}_KCONFIG
+        "${CMAKE_CURRENT_LIST_DIR}/${module_name}/Kconfig-NOTFOUND"
+    )
+  endif()
+  list(APPEND ZEPHYR_KCONFIG_MODULES_DIR
+       "NCS_${MODULE_NAME_UPPER}_KCONFIG=${NCS_${MODULE_NAME_UPPER}_KCONFIG}"
+  )
+endforeach()
