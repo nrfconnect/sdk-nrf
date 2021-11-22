96ac164e3c9de16d2555c718686d3b67e590ac06
Joakim Andersson <joakim.andersson@nordicsemi.no>
2021-11-22 18:12:45 +0100

    tfm: Use TF-M core platform services instead of out-of-tree services
    Use the TF-M core platform services instead of having these as
    out-of-tree services.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- include/tfm/tfm_ioctl_api.h
+++ include/tfm/tfm_ioctl_api.h
@@ -24,90 +24,14 @@
 #include <tfm_platform_api.h>
 #include <hal/nrf_gpio.h>
 
+/* Include core IOCTL services */
+#include <tfm_ioctl_core_api.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/** @brief Supported request types.
- */
-enum tfm_platform_ioctl_reqest_types_t {
-	TFM_PLATFORM_IOCTL_READ_SERVICE,
-	TFM_PLATFORM_IOCTL_GPIO_SERVICE,
-};
-
-/** @brief Argument list for each platform read service.
- */
-struct tfm_read_service_args_t {
-	void *destination;
-	uint32_t addr;
-	size_t len;
-};
-
-
-/** @brief Output list for each read platform service
- */
-struct tfm_read_service_out_t {
-	uint32_t result;
-};
-
-enum tfm_gpio_service_type {
-	/** Select which MCU / Subsystem controls the pin */
-	TFM_GPIO_SERVICE_TYPE_PIN_MCU_SELECT = 0,
-};
-
-/** @brief Arguments for selecting the MCU to control a GPIO pin. */
-struct tfm_gpio_service_args_mcu_select {
-	uint32_t pin_number;
-	uint32_t mcu;
-};
-
-/** @brief Argument list for each platform GPIO service */
-struct tfm_gpio_service_args {
-	uint32_t type;
-	union {
-		struct tfm_gpio_service_args_mcu_select mcu_select;
-	};
-};
-
-/** @brief Output list for each GPIO platform service
- */
-struct tfm_gpio_service_out {
-	uint32_t result;
-};
-
-/**
- * @brief Perform a read operation.
- *
- * @param[out] destination   Pointer where read result is stored
- * @param[in]  addr          Address to read from
- * @param[in]  len           Number of bytes to read
- * @param[out] result        Result of operation
- *
- * @return Returns values as specified by the tfm_platform_err_t
- */
-enum tfm_platform_err_t tfm_platform_mem_read(void *destination, uint32_t addr,
-					      size_t len, uint32_t *result);
-
-/** @brief Represents an accepted read range.
- */
-struct tfm_read_service_range {
-	uint32_t start;
-	size_t size;
-};
-
-/**
- * @brief Perform a GPIO MCU select operation.
- *
- * @param pin_number         Pin_number.
- * @param mcu                MCU to control the pin, use nrf_gpio_pin_mcusel_t values.
-
- * @param[out] result        Result of operation
- *
- * @return Returns values as specified by the tfm_platform_err_t
- */
-enum tfm_platform_err_t tfm_platform_gpio_pin_mcu_select(uint32_t pin_number, uint32_t mcu,
-							 uint32_t *result);
-
+/* Board specific IOCTL services can be added here */
 
 #ifdef __cplusplus
 }
--- include/tfm/tfm_read_ranges.h
+++ include/tfm/tfm_read_ranges.h
@@ -7,8 +7,9 @@
 #ifndef TFM_READ_RANGES_H__
 #define TFM_READ_RANGES_H__
 
-#include <tfm_ioctl_api.h>
 #include <pm_config.h>
+
+#include <tfm_ioctl_core_api.h>
 
 #include "nrf.h"
 
@@ -22,6 +23,11 @@
 #define FICR_NFC_SIZE           (sizeof(FICR_NFC_Type))
 #endif
 
+#if defined(FICR_XOSC32MTRIM_SLOPE_Msk)
+#define FICR_XOSC32MTRIM_ADDR   (FICR_BASE + offsetof(NRF_FICR_Type, XOSC32MTRIM))
+#define FICR_XOSC32MTRIM_SIZE   (sizeof(uint32_t))
+#endif
+
 /* Used by nrf_erratas.h */
 #define FICR_RESTRICTED_ADDR    (FICR_BASE + 0x130)
 #define FICR_RESTRICTED_SIZE    0x8
@@ -29,17 +35,16 @@
 static const struct tfm_read_service_range ranges[] = {
 #ifdef PM_MCUBOOT_ADDRESS
 	/* Allow reads of mcuboot metadata */
-	{.start = PM_MCUBOOT_PAD_ADDRESS,
-		.size = PM_MCUBOOT_PAD_SIZE},
+	{ .start = PM_MCUBOOT_PAD_ADDRESS, .size = PM_MCUBOOT_PAD_SIZE },
 #endif
-	{.start = FICR_INFO_ADDR,
-		.size = FICR_INFO_SIZE},
+	{ .start = FICR_INFO_ADDR, .size = FICR_INFO_SIZE },
 #if defined(FICR_NFC_TAGHEADER0_MFGID_Msk)
-	{.start = FICR_NFC_ADDR,
-		.size = FICR_NFC_SIZE},
+	{ .start = FICR_NFC_ADDR, .size = FICR_NFC_SIZE },
 #endif
-	{.start = FICR_RESTRICTED_ADDR,
-		.size = FICR_RESTRICTED_SIZE},
+	{ .start = FICR_RESTRICTED_ADDR, .size = FICR_RESTRICTED_SIZE },
+#if defined(FICR_XOSC32MTRIM_SLOPE_Msk)
+	{ .start = FICR_XOSC32MTRIM_ADDR, .size = FICR_XOSC32MTRIM_SIZE },
+#endif
 };
 
 #endif /* TFM_READ_RANGES_H__ */
