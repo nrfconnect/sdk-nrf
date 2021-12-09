d8ca72bbaa8db62302aca26ff340270f73bd4272
Emanuele Di Santo <emdi@nordicsemi.no>
2021-12-09 11:27:30 +0100

    ext: iperf3: use nrf_modem_at APIs
    Port from at_cmd to nrf_modem_at APIs.
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>

--- ext/iperf3/iperf_locale.c
+++ ext/iperf3/iperf_locale.c
@@ -118,7 +118,7 @@ const char nrf_iperf3_usage_support_str[] =
                            "  -c, --client    <host>    run in client mode, connecting to <host>\n"
                            "  -u, --udp                 use UDP rather than TCP\n"
                            "  --connect-timeout #       timeout for control connection setup (ms)\n"
-#if defined (CONFIG_NRF_MODEM_LIB_TRACE_ENABLED) && defined (CONFIG_AT_CMD)
+#if defined (CONFIG_NRF_MODEM_LIB_TRACE_ENABLED)
                            "  --curr-mdm-traces         use currently configured modem traces\n"
 #endif
                            "  -b, --bitrate #[KMG][/#]  target bitrate in bits/sec (0 for unlimited)\n"
