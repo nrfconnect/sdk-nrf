86df949ac48bfa3e5744d01025fc0405fbd23a05
Pete Skeggs <peter.skeggs@nordicsemi.no>
2021-12-10 12:02:55 -0800

    lib: location: add filtered ephemerides support
    Use new NRF_CLOUD_AGPS_FILTERED and NRF_CLOUD_AGPS_ELEVATION_MASK
    Kconfig variables when initializing the nrf_cloud_rest_agps_request
    structure.
    
    Signed-off-by: Pete Skeggs <peter.skeggs@nordicsemi.no>

--- lib/location/method_gnss.c
+++ lib/location/method_gnss.c
@@ -213,9 +213,17 @@ static void method_gnss_agps_request_work_fn(struct k_work *item)
 	rest_ctx.auth = (char *)jwt_buf;
 
 	struct nrf_cloud_rest_agps_request request = {
-						      NRF_CLOUD_REST_AGPS_REQ_CUSTOM,
-						      &agps_request,
-						      NULL};
+						     NRF_CLOUD_REST_AGPS_REQ_CUSTOM,
+						     &agps_request,
+						     NULL,
+#if defined(CONFIG_NRF_CLOUD_AGPS_FILTERED_RUNTIME)
+						     true,
+						     CONFIG_NRF_CLOUD_AGPS_ELEVATION_MASK
+#else
+						     false, 0
+#endif
+						     };
+
 	struct lte_lc_cells_info net_info = {0};
 	struct location_utils_modem_params_info modem_params = { 0 };
 
