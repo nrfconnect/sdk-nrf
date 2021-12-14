4003e570dee6343197dab6440a890b3ce81b5efb
Joakim Andersson <joakim.andersson@nordicsemi.no>
2021-12-14 10:54:25 +0100

    modules: TF-M: Update out-of-tree boards to set PLATFORM_PATH variable
    Update out-of-tree boards to set the PLATFORM_PATH variable to be used
    for setting configurations.
    
    NCSDK-11973
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/tfm/tfm/boards/nrf9160/config.cmake
+++ modules/tfm/tfm/boards/nrf9160/config.cmake
@@ -5,7 +5,8 @@
 #
 #-------------------------------------------------------------------------------
 
-include(platform/ext/target/nordic_nrf/common/nrf9160/config.cmake)
+set(PLATFORM_PATH platform/ext/target/nordic_nrf/)
+include(${PLATFORM_PATH}/common/nrf9160/config.cmake)
 
 # Override the AEAD algorithm configuration since nRF9160 supports only AES_CCM
 set(PS_CRYPTO_AEAD_ALG                  PSA_ALG_CCM CACHE STRING    "The AEAD algorithm to use for authenticated encryption in Protected Storage")
