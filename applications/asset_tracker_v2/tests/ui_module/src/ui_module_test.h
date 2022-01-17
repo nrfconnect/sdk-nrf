87b311d64834cfdab9f71d2d3ee451bca718ec5c
Simen S. Røstad <simen.rostad@nordicsemi.no>
2022-01-17 11:36:38 +0100

    applications: asset_tracker_v2: Add test for UI module
    Add test for UI module.
    
    Signed-off-by: Simen S. Røstad <simen.rostad@nordicsemi.no>

---
+++ applications/asset_tracker_v2/tests/ui_module/src/ui_module_test.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef UI_MODULE_TEST_H_
+#define UI_MODULE_TEST_H_
+
+#include <init.h>
+
+/* Redefine SYS_INIT so that setup() is not run before the test runner starts. */
+#undef SYS_INIT
+#define SYS_INIT(_init_fn, _level, _prio)
+
+/* Suppress linker that warns that setup() is not used. This is because the function is run
+ * at SYS_INIT, which is now redefined.
+ */
+static int setup(const struct device *dev) __attribute__((unused));
+
+#endif /* UI_MODULE_TEST */
