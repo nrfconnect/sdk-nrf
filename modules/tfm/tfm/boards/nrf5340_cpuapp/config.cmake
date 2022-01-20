092e402e7748390c115159a41582c719e940bc8b
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-01-20 12:48:00 +0100

    tfm: Fix psa_generate_random and inclusion of TF-M default crypto keys
    Fix psa_generate_random implementatation in TF-M not having
    nrf_cc3xx_platform_ctr_drbg_init called in platform init.
    This is needed by random_ext which is active in TF-M mbedtls
    configuration.
    
    Rename PLATFORM_DUMMY_CRYPTO_KEYS to PLATFORM_DEFAULT_CRYPTO_KEYS
    according to upstream renaming. This reflects that the crypto keys
    module is implemented in upstream.
    We provide our own version of crypto_keys so this must stay disabled.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/tfm/tfm/boards/nrf5340_cpuapp/config.cmake
+++ modules/tfm/tfm/boards/nrf5340_cpuapp/config.cmake
@@ -10,7 +10,7 @@ include(${PLATFORM_PATH}/common/nrf5340/config.cmake)
 
 # Override the AEAD algorithm configuration
 set(PS_CRYPTO_AEAD_ALG                  PSA_ALG_GCM CACHE STRING    "The AEAD algorithm to use for authenticated encryption in Protected Storage")
-set(PLATFORM_DUMMY_CRYPTO_KEYS          FALSE      CACHE BOOL      "Use dummy crypto keys. Should not be used in production.")
+set(PLATFORM_DEFAULT_CRYPTO_KEYS        FALSE       CACHE BOOL      "Use default crypto keys implementation.")
 
 # Disable crypto regression tests that are not supported
 set(TFM_CRYPTO_TEST_ALG_CFB             OFF          CACHE BOOL      "Test CFB cryptography mode")
