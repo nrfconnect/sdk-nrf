a0d2315423a8453c2f0244787c5083cb9572884e
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-01-20 12:48:00 +0100

    tfm: Fix psa_generate_random and inclusion of TF-M default crypto keys
    Fix psa_generate_random implementatation in TF-M not having
    nrf_cc3xx_platform_ctr_drbg_init called in platform init.
    This is needed by random_ext which is active in TF-M mbedtls
    configuration.
    
    Rename PLATFORM_DUMMY_CRYPTO_KEYS to PLATFORM_DEFAULT_CRYPTO_KEYS
    according to upstream renaming. This reflects that the crypto keys
    module is implemented in upstream.
    We provide our own version of crypto_keys so this must stay disabled.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/tfm/tfm/boards/common/tfm_hal_platform.c
+++ modules/tfm/tfm/boards/common/tfm_hal_platform.c
@@ -13,6 +13,7 @@
 #include "tfm_spm_log.h"
 #include "hw_unique_key.h"
 #include <nrf_cc3xx_platform.h>
+#include <nrf_cc3xx_platform_ctr_drbg.h>
 
 enum tfm_hal_status_t tfm_hal_platform_init(void)
 {
@@ -23,21 +24,32 @@ enum tfm_hal_status_t tfm_hal_platform_init(void)
 		return status;
 	}
 
-#if defined(TFM_PARTITION_CRYPTO) && \
-    !defined(TFM_CRYPTO_KEY_DERIVATION_MODULE_DISABLED) && \
-    !defined(PLATFORM_DUMMY_CRYPTO_KEYS)
+#if defined(TFM_PARTITION_CRYPTO)
+	int err;
+
 	/* Initialize the nrf_cc3xx runtime */
-	int result = nrf_cc3xx_platform_init();
-	if (result != NRF_CC3XX_PLATFORM_SUCCESS) {
+	err = nrf_cc3xx_platform_init();
+	if (err != NRF_CC3XX_PLATFORM_SUCCESS) {
 		return TFM_HAL_ERROR_BAD_STATE;
 	}
 
+#if !defined(TFM_CRYPTO_RNG_MODULE_DISABLED)
+	/* Needed by mbedtls_psa_external_get_random in random_ext.c */
+	err = nrf_cc3xx_platform_ctr_drbg_init(NULL, NULL, 0);
+	if (err != NRF_CC3XX_PLATFORM_SUCCESS) {
+		return TFM_HAL_ERROR_BAD_STATE;
+	}
+#endif /* !defined(TFM_CRYPTO_RNG_MODULE_DISABLED) */
+
+#if !defined(TFM_CRYPTO_KEY_DERIVATION_MODULE_DISABLED) && \
+    !defined(PLATFORM_DEFAULT_CRYPTO_KEYS)
 	if (!hw_unique_key_are_any_written()) {
 		SPMLOG_INFMSG("Writing random Hardware Unique Keys to the KMU.\r\n");
 		hw_unique_key_write_random();
 		SPMLOG_INFMSG("Success\r\n");
 	}
 #endif
+#endif /* defined(TFM_PARTITION_CRYPTO) */
 
 	return TFM_HAL_SUCCESS;
 }
