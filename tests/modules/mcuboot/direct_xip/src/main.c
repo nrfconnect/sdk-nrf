56ad9a2823ba8061360c4657289b9bd4bcd77b1e
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2022-01-23 11:02:34 +0100

    mcuboot: add support for building direct xip variants
    Leverage the variant functionality in multi_image.cmake to add support
    for generating variants of the 'app' image which can be used by mcuboot
    in a DIRECT_XIP configuration.
    
    Ref: NCSDK-6211
    
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

---
+++ tests/modules/mcuboot/direct_xip/src/main.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <ztest.h>
+#include <stdio.h>
+
+
+void test_direct_xip(void)
+{
+	/* Checks if the test application boots up in the direct XiP mode. */
+	zassert_true(true, "Needed to pass.");
+}
+
+void test_main(void)
+{
+	ztest_test_suite(test_mcuboot_direct_xip,
+			 ztest_unit_test(test_direct_xip)
+	);
+	ztest_run_test_suite(test_mcuboot_direct_xip);
+}
