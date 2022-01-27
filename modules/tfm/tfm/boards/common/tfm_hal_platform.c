b8c00d2d3de0a502172fd72673f13dcc6b110c11
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2022-01-27 16:49:46 +0100

    crypto: Reducing TF-M Minimal config size
    -Disabling PSA_WANT_ALG_CTR_DRBG if TFM_MINIMAL is set
    -Running HMAC_DRBG function if minimal config is used
    -Initialize without RNG if RNG crypto module is not enabled.
    
    NCSDK-13530
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/tfm/tfm/boards/common/tfm_hal_platform.c
+++ modules/tfm/tfm/boards/common/tfm_hal_platform.c
@@ -6,14 +6,54 @@
  *
  */
 
+#if defined(TFM_PARTITION_CRYPTO)
+#include "common.h"
+#include <nrf_cc3xx_platform.h>
+#include <nrf_cc3xx_platform_ctr_drbg.h>
+#endif
+
 #include "tfm_hal_platform.h"
 #include "tfm_hal_platform_common.h"
 #include "cmsis.h"
 #include "uart_stdout.h"
 #include "tfm_spm_log.h"
 #include "hw_unique_key.h"
-#include <nrf_cc3xx_platform.h>
-#include <nrf_cc3xx_platform_ctr_drbg.h>
+
+#if defined(TFM_PARTITION_CRYPTO)
+static enum tfm_hal_status_t crypto_platform_init(void)
+{
+	int err;
+
+	/* Initialize the nrf_cc3xx runtime */
+#if defined(TFM_CRYPTO_RNG_MODULE_DISABLED)
+	err = nrf_cc3xx_platform_init_no_rng();
+#else
+#if defined(PSA_WANT_ALG_CTR_DRBG)
+	err = nrf_cc3xx_platform_init();
+#elif defined (PSA_WANT_ALG_HMAC_DRBG)
+	err = nrf_cc3xx_platform_init_hmac_drbg();
+#else
+	#error "Please enable either PSA_WANT_ALG_CTR_DRBG or PSA_WANT_ALG_HMAC_DRBG"
+#endif
+#endif
+
+	if (err != NRF_CC3XX_PLATFORM_SUCCESS) {
+		return TFM_HAL_ERROR_BAD_STATE;
+	}
+
+#if !defined(TFM_CRYPTO_KEY_DERIVATION_MODULE_DISABLED) && \
+    !defined(PLATFORM_DEFAULT_CRYPTO_KEYS)
+	if (!hw_unique_key_are_any_written()) {
+		SPMLOG_INFMSG("Writing random Hardware Unique Keys to the KMU.\r\n");
+		hw_unique_key_write_random();
+		SPMLOG_INFMSG("Success\r\n");
+	}
+#endif
+
+	return TFM_HAL_SUCCESS;
+}
+#endif /* defined(TFM_PARTITION_CRYPTO) */
+
 
 enum tfm_hal_status_t tfm_hal_platform_init(void)
 {
@@ -25,30 +65,11 @@ enum tfm_hal_status_t tfm_hal_platform_init(void)
 	}
 
 #if defined(TFM_PARTITION_CRYPTO)
-	int err;
-
-	/* Initialize the nrf_cc3xx runtime */
-	err = nrf_cc3xx_platform_init();
-	if (err != NRF_CC3XX_PLATFORM_SUCCESS) {
-		return TFM_HAL_ERROR_BAD_STATE;
+	status = crypto_platform_init();
+	if (status != TFM_HAL_SUCCESS)
+	{
+		return status;
 	}
-
-#if !defined(TFM_CRYPTO_RNG_MODULE_DISABLED)
-	/* Needed by mbedtls_psa_external_get_random in random_ext.c */
-	err = nrf_cc3xx_platform_ctr_drbg_init(NULL, NULL, 0);
-	if (err != NRF_CC3XX_PLATFORM_SUCCESS) {
-		return TFM_HAL_ERROR_BAD_STATE;
-	}
-#endif /* !defined(TFM_CRYPTO_RNG_MODULE_DISABLED) */
-
-#if !defined(TFM_CRYPTO_KEY_DERIVATION_MODULE_DISABLED) && \
-    !defined(PLATFORM_DEFAULT_CRYPTO_KEYS)
-	if (!hw_unique_key_are_any_written()) {
-		SPMLOG_INFMSG("Writing random Hardware Unique Keys to the KMU.\r\n");
-		hw_unique_key_write_random();
-		SPMLOG_INFMSG("Success\r\n");
-	}
-#endif
 #endif /* defined(TFM_PARTITION_CRYPTO) */
 
 	return TFM_HAL_SUCCESS;
