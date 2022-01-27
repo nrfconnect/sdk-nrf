9a2a9d5cac576e0b1615f9a052d0d598ffe44909
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2022-01-27 18:21:16 +0100

    samples: Crypto, buffer IV sizes and configurations
    -Change IV size from 16 to 12 since
     16 is not supported by Oberon
    -Change the AES_CTR sample text size to be
     a multiple of the block size
    -Changes to the correct Kconfig for enabling
     Oberon backend in the GCM example
    
    ref: NCSDK-13503
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- samples/crypto/aes_ctr/src/main.c
+++ samples/crypto/aes_ctr/src/main.c
@@ -32,7 +32,7 @@ LOG_MODULE_REGISTER(aes_ctr, LOG_LEVEL_DBG);
 /* ====================================================================== */
 /*			Global variables/defines for the AES CTR example			  */
 
-#define NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE (100)
+#define NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE (128)
 #define NRF_CRYPTO_EXAMPLE_AES_BLOCK_SIZE (16)
 
 /* AES IV buffer */
