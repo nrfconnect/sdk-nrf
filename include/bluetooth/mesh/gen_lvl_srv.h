e5e5751b5ff4c9ce4ec64891bb3e3d692b39a122
Richa Pandey <richa.pandey@nordicsemi.no>
2022-01-27 11:37:01 +0100

    doc: Added links to RST files in bluetooth_mesh_one header files
    Provided comments in the include/bluetooth/mesh/ header files
    part-1 about the location of corresponding RST files and
    rendered documentation for the particular library.
    
    Ref: NCSDK-11624
    
    Signed-off-by: Richa Pandey <richa.pandey@nordicsemi.no>

--- include/bluetooth/mesh/gen_lvl_srv.h
+++ include/bluetooth/mesh/gen_lvl_srv.h
@@ -4,6 +4,13 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
+/*
+ * The RST file for this library can be found in
+ * doc/nrf/libraries/bluetooth_services/mesh/gen_lvl_srv.rst.
+ * Rendered documentation is available at
+ * https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/nrf/libraries/bluetooth_services/mesh/gen_lvl_srv.html.
+ */
+
 /**
  * @file
  * @defgroup bt_mesh_lvl_srv Bluetooth mesh Generic Level Server model
--- include/bluetooth/mesh/gen_prop.h
+++ include/bluetooth/mesh/gen_prop.h
@@ -4,6 +4,13 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
+/*
+ * The RST file for this library can be found in
+ * doc/nrf/libraries/bluetooth_services/mesh/gen_prop.rst.
+ * Rendered documentation is available at
+ * https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/nrf/libraries/bluetooth_services/mesh/gen_prop.html.
+ */
+
 /**
  * @file
  * @defgroup bt_mesh_prop Generic Property models
