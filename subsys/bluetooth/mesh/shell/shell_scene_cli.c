40c675cc17cb8ee36f8e02512ae99b783242d532
Anders Storrø <anders.storro@nordicsemi.no>
2022-02-18 12:40:20 +0100

    Bluetooth: Mesh: Shell support for time/scene mdls
    Adds support for shell interaction with client model instances in a mesh
    application.
    
    This includes:
    - Time
    - Scene
    - Scheduler
    
    Signed-off-by: Anders Storrø <anders.storro@nordicsemi.no>

---
+++ subsys/bluetooth/mesh/shell/shell_scene_cli.c
@@ -0,0 +1,210 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <stdlib.h>
+#include <bluetooth/mesh/models.h>
+#include <shell/shell.h>
+
+#include "mesh/net.h"
+#include "mesh/access.h"
+#include "shell_utils.h"
+
+#define SHELL_SENSOR_CLI_REG_CNT_MAX 32
+
+static struct bt_mesh_model *mod;
+
+static void scene_get_print(const struct shell *shell, int err, struct bt_mesh_scene_state *rsp)
+{
+	if (!err) {
+		shell_print(shell,
+			    "Status: %d, current scene: %d "
+			    "target scene: %d, rem time: %d",
+			    rsp->status, rsp->current, rsp->target,
+			    rsp->remaining_time);
+	}
+}
+
+static int cmd_get(const struct shell *shell, size_t argc, char *argv[])
+{
+	if (!mod && !shell_model_first_get(BT_MESH_MODEL_ID_SCENE_CLI, &mod)) {
+		return -ENODEV;
+	}
+
+	struct bt_mesh_scene_cli *cli = mod->user_data;
+	struct bt_mesh_scene_state rsp;
+
+	int err = bt_mesh_scene_cli_get(cli, NULL, &rsp);
+
+	scene_get_print(shell, err, &rsp);
+	return err;
+}
+
+static void scene_print(const struct shell *shell, int err, struct bt_mesh_scene_register *rsp)
+{
+	if (!err) {
+		shell_print(shell, "Status: %d, current scene: %d", rsp->status, rsp->current);
+
+		for (int i = 0; i < rsp->count; i++) {
+			shell_print(shell, "Scene %d: %d", i, rsp->scenes[i]);
+		}
+	}
+}
+
+static int cmd_register_get(const struct shell *shell, size_t argc, char *argv[])
+{
+	if (!mod && !shell_model_first_get(BT_MESH_MODEL_ID_SCENE_CLI, &mod)) {
+		return -ENODEV;
+	}
+
+	struct bt_mesh_scene_cli *cli = mod->user_data;
+	uint16_t scenes[SHELL_SENSOR_CLI_REG_CNT_MAX];
+	struct bt_mesh_scene_register rsp = {
+		.count = SHELL_SENSOR_CLI_REG_CNT_MAX,
+		.scenes = &scenes[0],
+	};
+
+	int err = bt_mesh_scene_cli_register_get(cli, NULL, &rsp);
+
+	scene_print(shell, err, &rsp);
+	return err;
+}
+
+static int store(const struct shell *shell, size_t argc, char *argv[], bool acked)
+{
+	uint16_t scene = (uint16_t)strtol(argv[1], NULL, 0);
+
+	if (!mod && !shell_model_first_get(BT_MESH_MODEL_ID_SCENE_CLI, &mod)) {
+		return -ENODEV;
+	}
+
+	struct bt_mesh_scene_cli *cli = mod->user_data;
+
+	if (acked) {
+		uint16_t scenes[CONFIG_BT_MESH_SCENES_MAX];
+		struct bt_mesh_scene_register rsp = {
+			.count = CONFIG_BT_MESH_SCENES_MAX,
+			.scenes = &scenes[0],
+		};
+		int err = bt_mesh_scene_cli_store(cli, NULL, scene, &rsp);
+
+		scene_print(shell, err, &rsp);
+		return err;
+	} else {
+		return bt_mesh_scene_cli_store_unack(cli, NULL, scene);
+	}
+}
+
+static int cmd_store(const struct shell *shell, size_t argc, char *argv[])
+{
+	return store(shell, argc, argv, true);
+}
+
+static int cmd_store_unack(const struct shell *shell, size_t argc, char *argv[])
+{
+	return store(shell, argc, argv, false);
+}
+
+static int scene_del(const struct shell *shell, size_t argc, char *argv[], bool acked)
+{
+	uint16_t scene = (uint16_t)strtol(argv[1], NULL, 0);
+
+	if (!mod && !shell_model_first_get(BT_MESH_MODEL_ID_SCENE_CLI, &mod)) {
+		return -ENODEV;
+	}
+
+	struct bt_mesh_scene_cli *cli = mod->user_data;
+
+	if (acked) {
+		uint16_t scenes[CONFIG_BT_MESH_SCENES_MAX];
+		struct bt_mesh_scene_register rsp = {
+			.count = CONFIG_BT_MESH_SCENES_MAX,
+			.scenes = &scenes[0],
+		};
+		int err = bt_mesh_scene_cli_delete(cli, NULL, scene, &rsp);
+
+		scene_print(shell, err, &rsp);
+		return err;
+	} else {
+		return bt_mesh_scene_cli_delete_unack(cli, NULL, scene);
+	}
+}
+
+static int cmd_delete(const struct shell *shell, size_t argc, char *argv[])
+{
+	return scene_del(shell, argc, argv, true);
+}
+
+static int cmd_delete_unack(const struct shell *shell, size_t argc, char *argv[])
+{
+	return scene_del(shell, argc, argv, false);
+}
+
+static int recall(const struct shell *shell, size_t argc, char *argv[], bool acked)
+{
+	uint16_t scene = (uint16_t)strtol(argv[1], NULL, 0);
+	uint32_t time = (argc >= 3) ? (uint32_t)strtol(argv[2], NULL, 0) : 0;
+	uint32_t delay = (argc == 4) ? (uint32_t)strtol(argv[3], NULL, 0) : 0;
+
+	if (!mod && !shell_model_first_get(BT_MESH_MODEL_ID_SCENE_CLI, &mod)) {
+		return -ENODEV;
+	}
+
+	struct bt_mesh_scene_cli *cli = mod->user_data;
+	struct bt_mesh_model_transition trans = { .time = time, .delay = delay };
+
+	if (acked) {
+		struct bt_mesh_scene_state rsp;
+		int err = bt_mesh_scene_cli_recall(cli, NULL, scene, (argc > 2) ? &trans : NULL,
+						   &rsp);
+
+		scene_get_print(shell, err, &rsp);
+		return err;
+	} else {
+		return bt_mesh_scene_cli_recall_unack(cli, NULL, scene, (argc > 2) ? &trans : NULL);
+	}
+}
+
+static int cmd_recall(const struct shell *shell, size_t argc, char *argv[])
+{
+	return recall(shell, argc, argv, true);
+}
+
+static int cmd_recall_unack(const struct shell *shell, size_t argc, char *argv[])
+{
+	return recall(shell, argc, argv, false);
+}
+
+static int cmd_instance_get_all(const struct shell *shell, size_t argc, char *argv[])
+{
+	return shell_model_instances_get_all(shell, BT_MESH_MODEL_ID_SCENE_CLI);
+}
+
+static int cmd_instance_set(const struct shell *shell, size_t argc, char *argv[])
+{
+	uint8_t elem_idx = (uint8_t)strtol(argv[1], NULL, 0);
+
+	return shell_model_instance_set(shell, &mod, BT_MESH_MODEL_ID_SCENE_CLI, elem_idx);
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(instance_cmds,
+			       SHELL_CMD_ARG(set, NULL, "<elem_idx> ", cmd_instance_set, 2, 0),
+			       SHELL_CMD_ARG(get-all, NULL, NULL, cmd_instance_get_all, 1, 0),
+			       SHELL_SUBCMD_SET_END);
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	scene_cmds, SHELL_CMD_ARG(get, NULL, NULL, cmd_get, 1, 0),
+	SHELL_CMD_ARG(register-get, NULL, NULL, cmd_register_get, 1, 0),
+	SHELL_CMD_ARG(store, NULL, "<scene>", cmd_store, 2, 0),
+	SHELL_CMD_ARG(store-unack, NULL, "<scene>", cmd_store_unack, 2, 0),
+	SHELL_CMD_ARG(delete, NULL, "<scene>", cmd_delete, 2, 0),
+	SHELL_CMD_ARG(delete-unack, NULL, "<scene>", cmd_delete_unack, 2, 0),
+	SHELL_CMD_ARG(recall, NULL, "<scene> [transition_time_ms [delay_ms]]", cmd_recall, 2, 2),
+	SHELL_CMD_ARG(recall-unack, NULL, "<scene> [transition_time_ms [delay_ms]]",
+		      cmd_recall_unack, 2, 2),
+	SHELL_CMD(instance, &instance_cmds, "Instance commands", shell_model_cmds_help),
+	SHELL_SUBCMD_SET_END);
+
+SHELL_CMD_ARG_REGISTER(mdl_scene, &scene_cmds, "Scene Cli commands", shell_model_cmds_help, 1, 1);
---
+++ subsys/bluetooth/mesh/shell/shell_scheduler_cli.c
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <stdlib.h>
+#include <bluetooth/mesh/models.h>
+#include <shell/shell.h>
+#include <sys/math_extras.h>
+
+#include "mesh/net.h"
+#include "mesh/access.h"
+#include "../model_utils.h"
+
+#include "shell_utils.h"
+
+static struct bt_mesh_model *mod;
+static struct bt_mesh_schedule_entry set_entry;
+
+static int cmd_get(const struct shell *shell, size_t argc, char *argv[])
+{
+	if (!mod && !shell_model_first_get(BT_MESH_MODEL_ID_SCHEDULER_CLI, &mod)) {
+		return -ENODEV;
+	}
+
+	struct bt_mesh_scheduler_cli *cli = mod->user_data;
+	uint16_t rsp;
+
+	int err = bt_mesh_scheduler_cli_get(cli, NULL, &rsp);
+
+	if (!err) {
+		shell_print(shell, "Status: %d", rsp);
+	}
+
+	return err;
+}
+
+static void schedule_print(const struct shell *shell, int err, struct bt_mesh_schedule_entry *rsp)
+{
+	if (!err) {
+		shell_print(shell,
+			    "Day: %02d\nMonth: %02d\nYear: %02d\n"
+			    "Hour: %d\nMinute: %d\nSecond:%d\n"
+			    "Weekday: %d\nAction: %d\nTrans time: %d\nScene num: %d",
+			    rsp->day, rsp->month, rsp->year, rsp->hour, rsp->minute, rsp->second,
+			    rsp->day_of_week, rsp->action,
+			    model_transition_decode(rsp->transition_time), rsp->scene_number);
+	}
+}
+
+static int cmd_action_get(const struct shell *shell, size_t argc, char *argv[])
+{
+	uint8_t idx = (uint8_t)strtol(argv[1], NULL, 0);
+
+	if (!mod && !shell_model_first_get(BT_MESH_MODEL_ID_SCHEDULER_CLI, &mod)) {
+		return -ENODEV;
+	}
+
+	struct bt_mesh_scheduler_cli *cli = mod->user_data;
+	struct bt_mesh_schedule_entry rsp;
+
+	int err = bt_mesh_scheduler_cli_action_get(cli, NULL, idx, &rsp);
+
+	schedule_print(shell, err, &rsp);
+	return err;
+}
+
+static int cmd_action_ctx_set(const struct shell *shell, size_t argc, char *argv[])
+{
+	set_entry.year = (uint8_t)strtol(argv[1], NULL, 0);
+	set_entry.month = (uint8_t)strtol(argv[2], NULL, 0);
+	set_entry.day = (uint8_t)strtol(argv[3], NULL, 0);
+	set_entry.hour = (uint8_t)strtol(argv[4], NULL, 0);
+	set_entry.minute = (uint8_t)strtol(argv[5], NULL, 0);
+	set_entry.second = (uint8_t)strtol(argv[6], NULL, 0);
+	set_entry.day_of_week = (uint8_t)strtol(argv[7], NULL, 0);
+	set_entry.action = (enum bt_mesh_scheduler_action)strtol(argv[8], NULL, 0);
+	set_entry.scene_number = (uint16_t)strtol(argv[10], NULL, 0);
+
+	uint32_t transition_time = (uint32_t)strtol(argv[9], NULL, 0);
+
+	set_entry.transition_time = model_transition_encode(transition_time);
+
+	shell_print(shell, "Current action context:");
+	schedule_print(shell, 0, &set_entry);
+	return 0;
+}
+
+static int action_set(const struct shell *shell, size_t argc, char *argv[], bool acked)
+{
+	uint8_t idx = (uint8_t)strtol(argv[1], NULL, 0);
+
+	if (!mod && !shell_model_first_get(BT_MESH_MODEL_ID_SCHEDULER_CLI, &mod)) {
+		return -ENODEV;
+	}
+
+	struct bt_mesh_scheduler_cli *cli = mod->user_data;
+
+	if (acked) {
+		struct bt_mesh_schedule_entry rsp;
+		int err = bt_mesh_scheduler_cli_action_set(cli, NULL, idx, &set_entry, &rsp);
+
+		schedule_print(shell, 0, &set_entry);
+		return err;
+	} else {
+		return bt_mesh_scheduler_cli_action_set_unack(cli, NULL, idx, &set_entry);
+	}
+}
+
+static int cmd_action_set(const struct shell *shell, size_t argc, char *argv[])
+{
+	return action_set(shell, argc, argv, true);
+}
+
+static int cmd_action_set_unack(const struct shell *shell, size_t argc, char *argv[])
+{
+	return action_set(shell, argc, argv, false);
+}
+
+static int cmd_instance_get_all(const struct shell *shell, size_t argc, char *argv[])
+{
+	return shell_model_instances_get_all(shell, BT_MESH_MODEL_ID_SCHEDULER_CLI);
+}
+
+static int cmd_instance_set(const struct shell *shell, size_t argc, char *argv[])
+{
+	uint8_t elem_idx = (uint8_t)strtol(argv[1], NULL, 0);
+
+	return shell_model_instance_set(shell, &mod, BT_MESH_MODEL_ID_SCHEDULER_CLI, elem_idx);
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(instance_cmds,
+			       SHELL_CMD_ARG(set, NULL, "<elem_idx> ", cmd_instance_set, 2, 0),
+			       SHELL_CMD_ARG(get-all, NULL, NULL, cmd_instance_get_all, 1, 0),
+			       SHELL_SUBCMD_SET_END);
+
+SHELL_STATIC_SUBCMD_SET_CREATE(sched_cmds, SHELL_CMD_ARG(get, NULL, NULL, cmd_get, 1, 0),
+			       SHELL_CMD_ARG(action-get, NULL, "<idx>", cmd_action_get, 2, 0),
+			       SHELL_CMD_ARG(action-ctx-set, NULL,
+					     "<year> <month> <day> <hour> "
+					     "<minute> <second> <day_of_week> <action> "
+					     "<transition_time> <scene_number>",
+					     cmd_action_ctx_set, 11, 0),
+			       SHELL_CMD_ARG(action-set, NULL, "<idx>", cmd_action_set, 2, 0),
+			       SHELL_CMD_ARG(action-set-unack, NULL, "<idx>",
+					     cmd_action_set_unack, 2, 0),
+			       SHELL_CMD(instance, &instance_cmds, "Instance commands",
+					 shell_model_cmds_help),
+			       SHELL_SUBCMD_SET_END);
+
+SHELL_CMD_ARG_REGISTER(mdl_scheduled, &sched_cmds, "Scheduler Cli commands", shell_model_cmds_help,
+		       1, 1);
