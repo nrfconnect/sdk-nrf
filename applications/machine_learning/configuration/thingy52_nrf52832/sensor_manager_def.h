844568abef9353e1186252e01ca8d29e34722060
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-03-10 13:56:16 +0100

    caf: modules: sensor_manager: use device references
    Use `const struct device *` in `struct sm_sensor_config` instead of
    device name, since device references can be obtained at compile time
    using DEVICE_DT_GET().
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- applications/machine_learning/configuration/thingy52_nrf52832/sensor_manager_def.h
+++ applications/machine_learning/configuration/thingy52_nrf52832/sensor_manager_def.h
@@ -25,7 +25,7 @@ static const struct sm_sampled_channel accel_chan[] = {
 
 static const struct sm_sensor_config sensor_configs[] = {
 	{
-		.dev_name = "LIS2DH12-ACCEL",
+		.dev = DEVICE_DT_GET(DT_NODELABEL(lis2dh12)),
 		.event_descr = CONFIG_ML_APP_SENSOR_EVENT_DESCR,
 		.chans = accel_chan,
 		.chan_cnt = ARRAY_SIZE(accel_chan),
