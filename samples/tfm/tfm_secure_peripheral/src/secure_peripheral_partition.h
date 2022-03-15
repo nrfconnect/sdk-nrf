72447611896087c2ee1400a44ab64b6f2f6548b2
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-03-15 17:53:41 +0100

    samples: tfm: Add sample for secure peripheral in TF-M secure partition
    Add sample demonstrating use of secure peripherals in TF-M secure
    partition.
    
    NCSDK-12370
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

---
+++ samples/tfm/tfm_secure_peripheral/src/secure_peripheral_partition.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA.
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include "tfm_api.h"
+
+psa_status_t spp_process(void);
+
+psa_status_t spp_send(void);
