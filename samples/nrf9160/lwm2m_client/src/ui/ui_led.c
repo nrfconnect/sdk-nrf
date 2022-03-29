7ea92139cfd5e413acfa185d5576aa616c42caf1
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-03-30 00:03:43 +0200

    samples: nrf9160: lwm2m_client: use gpio_dt_spec
    The way to obtain LED information from Devicetree had a few issues, for
    example, rely on absolute paths, board related ifdeffery, etc. This
    patch simplifies the implementation by using gpio_dt_spec facilities and
    tries to follow good practices.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- samples/nrf9160/lwm2m_client/src/ui/ui_led.c
+++ samples/nrf9160/lwm2m_client/src/ui/ui_led.c
@@ -20,15 +20,16 @@ LOG_MODULE_REGISTER(ui_led, CONFIG_UI_LOG_LEVEL);
 
 #define PWM_PERIOD_USEC				(USEC_PER_SEC / 100U)
 
-#define LED_GPIO_PARENT_NODE		DT_PATH(leds)
-#define LED_GPIO_NODE(number)		DT_CHILD(LED_GPIO_PARENT_NODE, led_##number)
-#define LED_GPIO_PIN(number)		DT_GPIO_PIN(LED_GPIO_NODE(number), gpios)
-#define LED_GPIO_FLAGS				DT_GPIO_FLAGS(LED_GPIO_NODE(1), gpios)
-#define LED_GPIO_DEV_LABEL			DT_GPIO_LABEL(LED_GPIO_NODE(1), gpios)
-
 static const struct device *led_dev;
 static uint32_t pulse_width[NUM_LEDS];
 static bool state[NUM_LEDS];
+
+static const struct gpio_dt_spec leds[] = {
+	GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios, {}),
+	GPIO_DT_SPEC_GET_OR(DT_ALIAS(led1), gpios, {}),
+	GPIO_DT_SPEC_GET_OR(DT_ALIAS(led2), gpios, {}),
+	GPIO_DT_SPEC_GET_OR(DT_ALIAS(led3), gpios, {}),
+};
 
 static int get_pwm_channel(uint8_t led_num, uint32_t *pwm_channel)
 {
@@ -110,50 +111,15 @@ int ui_led_pwm_init(void)
 	return 0;
 }
 
-static int get_gpio_pin(uint8_t led_num, gpio_pin_t *gpio_pin)
-{
-	uint8_t led_id;
-
-	/* LEDs are named led_1 .. led_3 in Thingy dts, but led_0 .. led_4 in DK dts */
-	if (IS_ENABLED(CONFIG_BOARD_THINGY91_NRF9160_NS)) {
-		led_id = led_num + 1;
-	} else {
-		led_id = led_num;
-	}
-
-	switch (led_id) {
-#if defined(CONFIG_BOARD_NRF9160DK_NRF9160_NS)
-	case 0:
-		*gpio_pin = LED_GPIO_PIN(0);
-		break;
-#endif
-	case 1:
-		*gpio_pin = LED_GPIO_PIN(1);
-		break;
-	case 2:
-		*gpio_pin = LED_GPIO_PIN(2);
-		break;
-	case 3:
-		*gpio_pin = LED_GPIO_PIN(3);
-		break;
-	default:
-		LOG_ERR("LED GPIO pin %u not supported (%d)", led_id, -ENOTSUP);
-		return -ENOTSUP;
-	}
-
-	return 0;
-}
-
 int ui_led_gpio_on_off(uint8_t led_num, bool new_state)
 {
 	int ret;
-	gpio_pin_t gpio_pin;
 
-	ret = get_gpio_pin(led_num, &gpio_pin);
-	if (ret) {
-		return ret;
+	if (led_num >= ARRAY_SIZE(leds) || leds[led_num].port == NULL) {
+		return -EINVAL;
 	}
-	ret = gpio_pin_set(led_dev, gpio_pin, new_state);
+
+	ret = gpio_pin_set_dt(&leds[led_num], new_state);
 	if (ret) {
 		LOG_ERR("Set LED %u pin failed (%d)", led_num, ret);
 		return ret;
@@ -165,22 +131,15 @@ int ui_led_gpio_on_off(uint8_t led_num, bool new_state)
 int ui_led_gpio_init(void)
 {
 	int ret;
-	gpio_pin_t pin;
 
-	led_dev = device_get_binding(LED_GPIO_DEV_LABEL);
-	if (!led_dev) {
-		LOG_ERR("Could not bind to LED GPIO device (%d)", -ENODEV);
-		return -ENODEV;
-	}
-
-	for (int i = 0; i < NUM_LEDS; ++i) {
-		ret = get_gpio_pin(i, &pin);
-		if (ret) {
-			return ret;
+	for (size_t i = 0; i < ARRAY_SIZE(leds); ++i) {
+		if (leds[i].port != NULL && !device_is_ready(leds[i].port)) {
+			return -ENODEV;
 		}
-		ret = gpio_pin_configure(led_dev, pin, LED_GPIO_FLAGS | GPIO_OUTPUT_INACTIVE);
+
+		ret = gpio_pin_configure_dt(&leds[i], GPIO_OUTPUT_INACTIVE);
 		if (ret) {
-			LOG_ERR("Configure LED pin %d failed (%d)", i, ret);
+			LOG_ERR("Configure LED %d failed (%d)", i, ret);
 			return ret;
 		}
 	}
