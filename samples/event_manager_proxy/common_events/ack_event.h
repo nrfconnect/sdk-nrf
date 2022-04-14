rakons/fw-nrfconnect-nrf

samples: event_manager_proxy: Event manager proxy sample
This commit adds a sample of Event manager proxy implementation.

Signed-off-by: Radoslaw Koppel <radoslaw.koppel@nordicsemi.no>

samples/event_manager_proxy/common_events/ack_event.h
@@ -1,0 +1,37 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _ACK_EVENT_H_
+#define _ACK_EVENT_H_
+
+/**
+ * @brief ACK Event
+ * @defgroup ack_event ACK Event
+ * @{
+ */
+
+#include <event_manager.h>
+#include <event_manager_profiler_tracer.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct ack_event {
+	struct event_header header;
+};
+
+EVENT_TYPE_DECLARE(ack_event);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _ACK_EVENT_H_ */

samples/event_manager_proxy/common_events/config_event.h
@@ -1,0 +1,39 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _CONFIG_EVENT_H_
+#define _CONFIG_EVENT_H_
+
+/**
+ * @brief Config Event
+ * @defgroup config_event Config Event
+ * @{
+ */
+
+#include <event_manager.h>
+#include <event_manager_profiler_tracer.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct config_event {
+	struct event_header header;
+
+	int8_t init_value1;
+};
+
+EVENT_TYPE_DECLARE(config_event);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _CONFIG_EVENT_H_ */

samples/event_manager_proxy/common_events/measurement_event.h
@@ -1,0 +1,41 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _MEASUREMENT_EVENT_H_
+#define _MEASUREMENT_EVENT_H_
+
+/**
+ * @brief Measurement Event
+ * @defgroup measurement_event Measurement Event
+ * @{
+ */
+
+#include <event_manager.h>
+#include <event_manager_profiler_tracer.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct measurement_event {
+	struct event_header header;
+
+	int8_t value1;
+	int16_t value2;
+	int32_t value3;
+};
+
+EVENT_TYPE_DECLARE(measurement_event);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _MEASUREMENT_EVENT_H_ */

samples/event_manager_proxy/modules/controller.c
@@ -1,0 +1,54 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr.h>
+
+#include "measurement_event.h"
+#include "control_event.h"
+#include "ack_event.h"
+
+#define VALUE2_THRESH 15
+#define MODULE controller
+
+static bool ack_req;
+
+void send_control_event(void)
+{
+	ack_req = true;
+	struct control_event *event = new_control_event();
+
+	EVENT_SUBMIT(event);
+}
+
+static bool event_handler(const struct event_header *eh)
+{
+	if (is_measurement_event(eh)) {
+		__ASSERT_NO_MSG(!ack_req);
+		struct measurement_event *me = cast_measurement_event(eh);
+
+		if ((me->value2 >= VALUE2_THRESH) ||
+		    (me->value2 <= -VALUE2_THRESH)) {
+			send_control_event();
+		}
+
+		return false;
+	}
+
+	if (is_ack_event(eh)) {
+		__ASSERT_NO_MSG(ack_req);
+		ack_req = false;
+		return false;
+	}
+
+	/* If event is unhandled, unsubscribe. */
+	__ASSERT_NO_MSG(false);
+
+	return false;
+}
+
+EVENT_LISTENER(MODULE, event_handler);
+EVENT_SUBSCRIBE(MODULE, measurement_event);
+EVENT_SUBSCRIBE(MODULE, ack_event);

samples/event_manager_proxy/modules/stats.c
@@ -1,0 +1,69 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr.h>
+
+#define MODULE stats
+
+#include "measurement_event.h"
+#include "control_event.h"
+
+#include <logging/log.h>
+#define STATS_LOG_LEVEL 4
+LOG_MODULE_REGISTER(MODULE, STATS_LOG_LEVEL);
+
+#define STATS_ARR_SIZE 10
+
+static int32_t val_arr[STATS_ARR_SIZE];
+static unsigned int control_cnt;
+
+static void add_to_stats(int32_t value)
+{
+	static size_t ind;
+
+	/* Add new value */
+	val_arr[ind] = value;
+	ind++;
+
+	if (ind == ARRAY_SIZE(val_arr)) {
+		ind = 0;
+		long long sum = 0;
+
+		for (size_t i = 0; i < ARRAY_SIZE(val_arr); i++) {
+			sum += val_arr[i];
+		}
+		LOG_INF("Average value3: %d", (int)(sum/ARRAY_SIZE(val_arr)));
+	}
+}
+
+static void count_control_events(void)
+{
+	++control_cnt;
+	LOG_INF("Control event count: %u", control_cnt);
+}
+
+static bool event_handler(const struct event_header *eh)
+{
+	if (is_measurement_event(eh)) {
+		struct measurement_event *me = cast_measurement_event(eh);
+
+		add_to_stats(me->value3);
+		return false;
+	}
+	if (is_control_event(eh)) {
+		count_control_events();
+		return false;
+	}
+
+	/* If event is unhandled, unsubscribe. */
+	__ASSERT_NO_MSG(false);
+
+	return false;
+}
+
+EVENT_LISTENER(MODULE, event_handler);
+EVENT_SUBSCRIBE(MODULE, measurement_event);
+EVENT_SUBSCRIBE(MODULE, control_event);

samples/event_manager_proxy/remote/src/main.c
@@ -1,0 +1,66 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr.h>
+#include <device.h>
+
+#include <ipc/ipc_service.h>
+
+#include <event_manager.h>
+#include <event_manager_proxy.h>
+#include <config_event.h>
+#include <logging/log.h>
+
+#define MODULE main_remote
+
+LOG_MODULE_REGISTER(MODULE);
+
+#define INIT_VALUE1 3
+
+void main(void)
+{
+	int ret;
+	const struct device *ipc_instance  = DEVICE_DT_GET(DT_NODELABEL(ipc0));
+
+	printk("Event Manager Proxy remote_core started\n");
+
+	ret = event_manager_init();
+	if (ret) {
+		LOG_ERR("Event Manager not initialized, err: %d", ret);
+		__ASSERT_NO_MSG(false);
+		return;
+	}
+	LOG_INF("Event manager initialized");
+
+	ret = event_manager_proxy_add_remote(ipc_instance);
+	if (ret) {
+		LOG_ERR("Cannot add remote: %d", ret);
+		__ASSERT_NO_MSG(false);
+		return;
+	}
+	LOG_INF("Event proxy remote added");
+
+	/* Listen to the events from the remote */
+	ret = EVENT_MANAGER_PROXY_REGISTER_LISTENER(ipc_instance, config_event);
+	if (ret) {
+		LOG_ERR("Cannot register to remote config_event: %d", ret);
+		__ASSERT_NO_MSG(false);
+		return;
+	}
+	LOG_INF("Event proxy config_event registered");
+
+	ret = event_manager_proxy_start();
+	if (ret) {
+		LOG_ERR("Cannot start event manager proxy: %d", ret);
+		__ASSERT_NO_MSG(false);
+		return;
+	}
+
+	/* We do not have to wait for remotes here as we do not send any event that
+	 * would be fatal when missed.
+	 */
+	LOG_INF("Initialization finished successfully");
+}

nrfconnect/sdk-nrf

Co-authored-by: Divya Pillai <divya.pillai@nordicsemi.no>

samples/event_manager_proxy/common_events/ack_event.h
@@ -1,0 +1,37 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _ACK_EVENT_H_
+#define _ACK_EVENT_H_
+
+/**
+ * @brief ACK Event
+ * @defgroup ack_event ACK Event
+ * @{
+ */
+
+#include <app_event_manager.h>
+#include <app_event_manager_profiler_tracer.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct ack_event {
+	struct app_event_header header;
+};
+
+APP_EVENT_TYPE_DECLARE(ack_event);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _ACK_EVENT_H_ */

samples/event_manager_proxy/common_events/config_event.h
@@ -1,0 +1,39 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _CONFIG_EVENT_H_
+#define _CONFIG_EVENT_H_
+
+/**
+ * @brief Config Event
+ * @defgroup config_event Config Event
+ * @{
+ */
+
+#include <app_event_manager.h>
+#include <app_event_manager_profiler_tracer.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct config_event {
+	struct app_event_header header;
+
+	int8_t init_value1;
+};
+
+APP_EVENT_TYPE_DECLARE(config_event);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _CONFIG_EVENT_H_ */

samples/event_manager_proxy/common_events/measurement_event.h
@@ -1,0 +1,41 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _MEASUREMENT_EVENT_H_
+#define _MEASUREMENT_EVENT_H_
+
+/**
+ * @brief Measurement Event
+ * @defgroup measurement_event Measurement Event
+ * @{
+ */
+
+#include <app_event_manager.h>
+#include <app_event_manager_profiler_tracer.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct measurement_event {
+	struct app_event_header header;
+
+	int8_t value1;
+	int16_t value2;
+	int32_t value3;
+};
+
+APP_EVENT_TYPE_DECLARE(measurement_event);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _MEASUREMENT_EVENT_H_ */

samples/event_manager_proxy/modules/controller.c
@@ -1,0 +1,54 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr.h>
+
+#include "measurement_event.h"
+#include "control_event.h"
+#include "ack_event.h"
+
+#define VALUE2_THRESH 15
+#define MODULE controller
+
+static bool ack_req;
+
+void send_control_event(void)
+{
+	ack_req = true;
+	struct control_event *event = new_control_event();
+
+	APP_EVENT_SUBMIT(event);
+}
+
+static bool event_handler(const struct app_event_header *eh)
+{
+	if (is_measurement_event(eh)) {
+		__ASSERT_NO_MSG(!ack_req);
+		struct measurement_event *me = cast_measurement_event(eh);
+
+		if ((me->value2 >= VALUE2_THRESH) ||
+		    (me->value2 <= -VALUE2_THRESH)) {
+			send_control_event();
+		}
+
+		return false;
+	}
+
+	if (is_ack_event(eh)) {
+		__ASSERT_NO_MSG(ack_req);
+		ack_req = false;
+		return false;
+	}
+
+	/* If event is unhandled, unsubscribe. */
+	__ASSERT_NO_MSG(false);
+
+	return false;
+}
+
+APP_EVENT_LISTENER(MODULE, event_handler);
+APP_EVENT_SUBSCRIBE(MODULE, measurement_event);
+APP_EVENT_SUBSCRIBE(MODULE, ack_event);

samples/event_manager_proxy/modules/stats.c
@@ -1,0 +1,69 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr.h>
+
+#define MODULE stats
+
+#include "measurement_event.h"
+#include "control_event.h"
+
+#include <logging/log.h>
+#define STATS_LOG_LEVEL 4
+LOG_MODULE_REGISTER(MODULE, STATS_LOG_LEVEL);
+
+#define STATS_ARR_SIZE 10
+
+static int32_t val_arr[STATS_ARR_SIZE];
+static unsigned int control_cnt;
+
+static void add_to_stats(int32_t value)
+{
+	static size_t ind;
+
+	/* Add new value */
+	val_arr[ind] = value;
+	ind++;
+
+	if (ind == ARRAY_SIZE(val_arr)) {
+		ind = 0;
+		long long sum = 0;
+
+		for (size_t i = 0; i < ARRAY_SIZE(val_arr); i++) {
+			sum += val_arr[i];
+		}
+		LOG_INF("Average value3: %d", (int)(sum/ARRAY_SIZE(val_arr)));
+	}
+}
+
+static void count_control_events(void)
+{
+	++control_cnt;
+	LOG_INF("Control event count: %u", control_cnt);
+}
+
+static bool event_handler(const struct app_event_header *eh)
+{
+	if (is_measurement_event(eh)) {
+		struct measurement_event *me = cast_measurement_event(eh);
+
+		add_to_stats(me->value3);
+		return false;
+	}
+	if (is_control_event(eh)) {
+		count_control_events();
+		return false;
+	}
+
+	/* If event is unhandled, unsubscribe. */
+	__ASSERT_NO_MSG(false);
+
+	return false;
+}
+
+APP_EVENT_LISTENER(MODULE, event_handler);
+APP_EVENT_SUBSCRIBE(MODULE, measurement_event);
+APP_EVENT_SUBSCRIBE(MODULE, control_event);

samples/event_manager_proxy/remote/src/main.c
@@ -1,0 +1,64 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr.h>
+#include <device.h>
+
+#include <ipc/ipc_service.h>
+
+#include <app_event_manager.h>
+#include <event_manager_proxy.h>
+#include <config_event.h>
+#include <logging/log.h>
+
+#define MODULE main_remote
+
+LOG_MODULE_REGISTER(MODULE);
+
+void main(void)
+{
+	int ret;
+	const struct device *ipc_instance  = DEVICE_DT_GET(DT_NODELABEL(ipc0));
+
+	printk("Event Manager Proxy remote_core started\n");
+
+	ret = app_event_manager_init();
+	if (ret) {
+		LOG_ERR("Event Manager not initialized, err: %d", ret);
+		__ASSERT_NO_MSG(false);
+		return;
+	}
+	LOG_INF("Event manager initialized");
+
+	ret = event_manager_proxy_add_remote(ipc_instance);
+	if (ret) {
+		LOG_ERR("Cannot add remote: %d", ret);
+		__ASSERT_NO_MSG(false);
+		return;
+	}
+	LOG_INF("Event proxy remote added");
+
+	/* Listen to the events from the remote */
+	ret = EVENT_MANAGER_PROXY_SUBSCRIBE(ipc_instance, config_event);
+	if (ret) {
+		LOG_ERR("Cannot register to remote config_event: %d", ret);
+		__ASSERT_NO_MSG(false);
+		return;
+	}
+	LOG_INF("Event proxy config_event registered");
+
+	ret = event_manager_proxy_start();
+	if (ret) {
+		LOG_ERR("Cannot start event manager proxy: %d", ret);
+		__ASSERT_NO_MSG(false);
+		return;
+	}
+
+	/* We do not have to wait for remotes here as we do not send any event that
+	 * would be fatal when missed.
+	 */
+	LOG_INF("Initialization finished successfully");
+}