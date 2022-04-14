2381d03782648934f46f04b234f294b0064461a1
Marek Pieta <Marek.Pieta@nordicsemi.no>
2022-04-14 09:27:56 +0200

    samples: peripheral_fast_pair: Add HIDS to Fast Pair sample
    Change introduces HIDS and HID report related to audio volume
    control. Sending HID reports is triggered by button presses.
    
    Jira: NCSDK-14421
    Jira: NCSDK-14788
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- samples/bluetooth/peripheral_fast_pair/src/main.c
+++ samples/bluetooth/peripheral_fast_pair/src/main.c
@@ -12,9 +12,14 @@
 #include <dk_buttons_and_leds.h>
 
 #include "bt_adv_helper.h"
+#include "hids_helper.h"
 
 #define RUN_STATUS_LED			DK_LED1
 #define CON_STATUS_LED			DK_LED2
+
+#define VOLUME_UP_BUTTON_MASK		DK_BTN2_MSK
+#define VOLUME_DOWN_BUTTON_MASK		DK_BTN4_MSK
+
 #define RUN_LED_BLINK_INTERVAL_MS	1000
 
 
@@ -56,6 +61,79 @@ BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.security_changed = security_changed,
 };
 
+static const char *volume_change_to_str(enum hids_helper_volume_change volume_change)
+{
+	const char *res = NULL;
+
+	switch (volume_change) {
+	case HIDS_HELPER_VOLUME_CHANGE_DOWN:
+		res = "Decrease";
+		break;
+
+	case HIDS_HELPER_VOLUME_CHANGE_NONE:
+		break;
+
+	case HIDS_HELPER_VOLUME_CHANGE_UP:
+		res = "Increase";
+		break;
+
+	default:
+		/* Should not happen. */
+		__ASSERT_NO_MSG(false);
+		break;
+	}
+
+	return res;
+}
+
+static void hid_volume_control_send(enum hids_helper_volume_change volume_change)
+{
+	const char *operation_str = volume_change_to_str(volume_change);
+	int err = hids_helper_volume_ctrl(volume_change);
+
+	if (!err) {
+		if (operation_str) {
+			printk("%s audio volume\n", operation_str);
+		}
+	} else if (err == -ENOTCONN) {
+		/* HID host not connected. Silently drop HID data. */
+	} else {
+		printk("Failed to control audio volume (err %d)\n", err);
+	}
+}
+
+static void handle_volume_control(uint32_t button_state, uint32_t has_changed)
+{
+	static enum hids_helper_volume_change volume_change = HIDS_HELPER_VOLUME_CHANGE_NONE;
+	enum hids_helper_volume_change new_volume_change = volume_change;
+
+	if (has_changed & VOLUME_UP_BUTTON_MASK) {
+		if (button_state & VOLUME_UP_BUTTON_MASK) {
+			new_volume_change = HIDS_HELPER_VOLUME_CHANGE_UP;
+		} else if (volume_change == HIDS_HELPER_VOLUME_CHANGE_UP) {
+			new_volume_change = HIDS_HELPER_VOLUME_CHANGE_NONE;
+		}
+	}
+
+	if (has_changed & VOLUME_DOWN_BUTTON_MASK) {
+		if (button_state & VOLUME_DOWN_BUTTON_MASK) {
+			new_volume_change = HIDS_HELPER_VOLUME_CHANGE_DOWN;
+		} else if (volume_change == HIDS_HELPER_VOLUME_CHANGE_DOWN) {
+			new_volume_change = HIDS_HELPER_VOLUME_CHANGE_NONE;
+		}
+	}
+
+	if (volume_change != new_volume_change) {
+		volume_change = new_volume_change;
+		hid_volume_control_send(volume_change);
+	}
+}
+
+static void button_changed(uint32_t button_state, uint32_t has_changed)
+{
+	handle_volume_control(button_state, has_changed);
+}
+
 void main(void)
 {
 	bool run_led_on = true;
@@ -63,9 +141,9 @@ void main(void)
 
 	printk("Starting Bluetooth Fast Pair example\n");
 
-	err = dk_leds_init();
+	err = hids_helper_init();
 	if (err) {
-		printk("LEDs init failed (err %d)\n", err);
+		printk("HIDS init failed (err %d)\n", err);
 		return;
 	}
 
@@ -85,13 +163,25 @@ void main(void)
 
 	printk("Settings loaded\n");
 
-	err = bt_adv_helper_adv_start();
+	err = dk_leds_init();
 	if (err) {
+		printk("LEDs init failed (err %d)\n", err);
+		return;
+	}
+
+	err = bt_adv_helper_adv_start();
+	if (!err) {
+		printk("Discoverable advertising successfully started\n");
+	} else {
 		printk("Advertising failed to start (err %d)\n", err);
 		return;
 	}
 
-	printk("Advertising successfully started\n");
+	err = dk_buttons_init(button_changed);
+	if (err) {
+		printk("Buttons init failed (err %d)\n", err);
+		return;
+	}
 
 	for (;;) {
 		dk_set_led(RUN_STATUS_LED, run_led_on);
