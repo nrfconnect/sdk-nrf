c10ec7f874c7bf6b54bf02c99d489e9ae6a6caff
Marek Pieta <Marek.Pieta@nordicsemi.no>
2022-04-20 13:55:48 +0200

    bluetooth: fast_pair: Introduce non-discoverable advertising
    Change introduces non-discoverable advertising for empty Account
    Key list.
    
    Jira: NCSDK-14057
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- subsys/bluetooth/services/fast_pair/fp_advertising.c
+++ subsys/bluetooth/services/fast_pair/fp_advertising.c
@@ -5,51 +5,97 @@
  */
 
 #include <errno.h>
-#include <sys/byteorder.h>
+#include <net/buf.h>
 #include <bluetooth/bluetooth.h>
 
 #include <bluetooth/services/fast_pair.h>
 #include "fp_registration_data.h"
 #include "fp_common.h"
 
+static const uint16_t fast_pair_uuid = BT_FAST_PAIR_SERVICE_UUID;
 
-size_t bt_fast_pair_adv_data_size(bool fp_discoverable)
+static const uint8_t flags;
+static const uint8_t empty_account_key_list;
+
+
+static size_t bt_fast_pair_adv_data_size_non_discoverable(size_t account_key_cnt)
 {
-	size_t res;
+	size_t res = 0;
 
-	if (fp_discoverable) {
-		const uint16_t fast_pair_uuid = BT_FAST_PAIR_SERVICE_UUID;
+	res += sizeof(flags);
 
-		res = (sizeof(fast_pair_uuid) + FP_MODEL_ID_LEN);
+	if (account_key_cnt == 0) {
+		res += sizeof(empty_account_key_list);
 	} else {
 		/* Not supported yet. */
 		__ASSERT_NO_MSG(false);
-		res = 0;
 	}
 
 	return res;
 }
 
-static int fp_adv_data_fill_non_discoverable(struct bt_data *bt_adv_data,
-					     uint8_t *buf, size_t buf_size)
+static size_t bt_fast_pair_adv_data_size_discoverable(void)
 {
-	/* Not supported yet. */
-	return -ENOTSUP;
+	return FP_MODEL_ID_LEN;
 }
 
-static int fp_adv_data_fill_discoverable(struct bt_data *bt_adv_data,
-					 uint8_t *buf, size_t buf_size)
+size_t bt_fast_pair_adv_data_size(bool fp_discoverable)
 {
-	size_t adv_data_len = bt_fast_pair_adv_data_size(true);
+	size_t account_key_cnt = 0;
+	size_t res = 0;
+
+	res += sizeof(fast_pair_uuid);
+
+	if (fp_discoverable) {
+		res += bt_fast_pair_adv_data_size_discoverable();
+	} else {
+		res += bt_fast_pair_adv_data_size_non_discoverable(account_key_cnt);
+	}
+
+	return res;
+}
+
+static int fp_adv_data_fill_non_discoverable(struct net_buf_simple *buf, size_t account_key_cnt)
+{
+	net_buf_simple_add_u8(buf, flags);
+
+	if (account_key_cnt == 0) {
+		net_buf_simple_add_u8(buf, empty_account_key_list);
+	} else {
+		/* Not supported yet. */
+		__ASSERT_NO_MSG(false);
+	}
+
+	return 0;
+}
+
+static int fp_adv_data_fill_discoverable(struct net_buf_simple *buf)
+{
+	return fp_get_model_id(net_buf_simple_add(buf, FP_MODEL_ID_LEN), FP_MODEL_ID_LEN);
+}
+
+int bt_fast_pair_adv_data_fill(struct bt_data *bt_adv_data, uint8_t *buf, size_t buf_size,
+			       bool fp_discoverable)
+{
+	struct net_buf_simple nb;
+	size_t account_key_cnt = 0;
+	size_t adv_data_len = bt_fast_pair_adv_data_size(fp_discoverable);
+	int err;
 
 	if (adv_data_len > buf_size) {
 		return -EINVAL;
 	}
 
-	const uint16_t fast_pair_uuid = BT_FAST_PAIR_SERVICE_UUID;
+	net_buf_simple_init_with_data(&nb, buf, buf_size);
+	net_buf_simple_reset(&nb);
 
-	sys_put_le16(fast_pair_uuid, buf);
-	int err = fp_get_model_id(buf + sizeof(fast_pair_uuid), FP_MODEL_ID_LEN);
+	net_buf_simple_add_le16(&nb, fast_pair_uuid);
+
+	if (fp_discoverable) {
+		err = fp_adv_data_fill_discoverable(&nb);
+	} else {
+		err = fp_adv_data_fill_non_discoverable(&nb, account_key_cnt);
+	}
 
 	if (!err) {
 		bt_adv_data->type = BT_DATA_SVC_DATA16;
@@ -58,16 +104,4 @@ static int fp_adv_data_fill_discoverable(struct bt_data *bt_adv_data,
 	}
 
 	return err;
-}
-
-int bt_fast_pair_adv_data_fill(struct bt_data *bt_adv_data, uint8_t *buf, size_t buf_size,
-			       bool fp_discoverable)
-{
-	if (fp_discoverable) {
-		return fp_adv_data_fill_discoverable(bt_adv_data, buf, buf_size);
-	} else {
-		return fp_adv_data_fill_non_discoverable(bt_adv_data, buf, buf_size);
-	}
-
-	return 0;
 }
