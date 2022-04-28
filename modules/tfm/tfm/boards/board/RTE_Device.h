5e0009de5bc93d76b3c17155642e5a14e5540d38
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-04-28 13:02:35 +0200

    tfm: Remove noise in comments for RTE_Device.h
    Remove noise in the comments for RTE_Device, this provides no formatting
    or additional information.
    Keep only the part of comments that explains the meaning of the define.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/tfm/tfm/boards/board/RTE_Device.h
+++ modules/tfm/tfm/boards/board/RTE_Device.h
@@ -4,8 +4,6 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-//-------- <<< Use Configuration Wizard in Context Menu >>> --------------------
-
 #ifndef __RTE_DEVICE_H
 #define __RTE_DEVICE_H
 
@@ -20,42 +18,28 @@
 #define UARTE(idx)			DT_NODELABEL(uart##idx)
 #define UARTE_PROP(idx, prop)		DT_PROP(UARTE(idx), prop)
 
-// <i> Configuration settings for Driver_USART0 in component ::Drivers:USART
+/* Configuration settings for Driver_USART0. */
 #define   RTE_USART0                    1
-//   <h> Pin Selection (0xFFFFFFFF means Disconnected)
-//     <o> TXD
+/* Pin Selection (0xFFFFFFFF means Disconnected) */
 #define   RTE_USART0_TXD_PIN            UARTE_PROP(0, tx_pin)
-//     <o> RXD
 #define   RTE_USART0_RXD_PIN            UARTE_PROP(0, rx_pin)
-//     <o> RTS
 #define   RTE_USART0_RTS_PIN            UARTE_PROP(0, rts_pin)
-//     <o> CTS
 #define   RTE_USART0_CTS_PIN            UARTE_PROP(0, cts_pin)
-//   </h> Pin Configuration
 
-// <i> Configuration settings for Driver_USART1 in component ::Drivers:USART
+/* Configuration settings for Driver_USART1. */
 #define   RTE_USART1                    1
-//   <h> Pin Selection (0xFFFFFFFF means Disconnected)
+/* Pin Selection (0xFFFFFFFF means Disconnected) */
 #if defined(CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP_NS)
-//     <o> TXD
 #define   RTE_USART1_TXD_PIN            25 // TODO: Add to devicetree
-//     <o> RXD
 #define   RTE_USART1_RXD_PIN            26 // TODO: Add to devicetree
 #else
-//     <o> TXD
 #define   RTE_USART1_TXD_PIN            UARTE_PROP(1, tx_pin)
-//     <o> RXD
 #define   RTE_USART1_RXD_PIN            UARTE_PROP(1, rx_pin)
 #endif
-//     <o> RTS
 #define   RTE_USART1_RTS_PIN            0xFFFFFFFF
-//     <o> CTS
 #define   RTE_USART1_CTS_PIN            0xFFFFFFFF
-//   </h> Pin Configuration
 
-// <e> FLASH (Flash Memory) [Driver_FLASH0]
-// <i> Configuration settings for Driver_FLASH0 in component ::Drivers:FLASH
+/* Configuration settings for Driver_FLASH0. */
 #define   RTE_FLASH0                    1
-// </e> FLASH (Flash Memory) [Driver_FLASH0]
 
 #endif  /* __RTE_DEVICE_H */
