a48ae8ccff435917e50a5c33687c61c599857efa
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2022-04-29 17:40:07 +0200

    applications: nrf_desktop: Link doxygen API to rst
    Ensure that API documented with doxygen is visible in rst files.
    Add missing documentation.
    
    Jira: NCSDK-14306
    
    Co-developed-by: divya pillai <divya.pillai@nordicsemi.no>
    Signed-off-by: divya pillai <divya.pillai@nordicsemi.no>
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

--- applications/nrf_desktop/configuration/common/led_state.h
+++ applications/nrf_desktop/configuration/common/led_state.h
@@ -4,14 +4,12 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
+/** @file
+ * @brief Header file with LED states.
+ */
+
 #ifndef _LED_STATE_H_
 #define _LED_STATE_H_
-
-/**
- * @brief LED State
- * @defgroup led_state LED State
- * @{
- */
 
 #include <zephyr/types.h>
 #include <caf/events/led_event.h>
@@ -20,29 +18,52 @@
 extern "C" {
 #endif
 
+/**
+ * @brief LED State
+ * @defgroup nrf_desktop_led_state LED State
+ * @{
+ */
+
+/** @brief System states represented by LED. */
 enum led_system_state {
+	/** Device is idle. */
 	LED_SYSTEM_STATE_IDLE,
+	/** Device is charging. */
 	LED_SYSTEM_STATE_CHARGING,
+	/** Device in error state. */
 	LED_SYSTEM_STATE_ERROR,
 
+	/** Number of system states. */
 	LED_SYSTEM_STATE_COUNT
 };
 
+/** @brief Peer states represented by LED. */
 enum led_peer_state {
+	/** Peer is disconnected. */
 	LED_PEER_STATE_DISCONNECTED,
+	/** Peer is connected. */
 	LED_PEER_STATE_CONNECTED,
+	/** Device searches for peer. */
 	LED_PEER_STATE_PEER_SEARCH,
+	/** Device waits for user to confirm peer switch. */
 	LED_PEER_STATE_CONFIRM_SELECT,
+	/** Device waits for user to confirm bond erase. */
 	LED_PEER_STATE_CONFIRM_ERASE,
+	/** Device searches for new peer before bond erase. */
 	LED_PEER_STATE_ERASE_ADV,
 
+	/** Number of peer states. */
 	LED_PEER_STATE_COUNT
 };
 
+/** @brief LED identification. */
 enum led_id {
+	/** LED representing system state. */
 	LED_ID_SYSTEM_STATE,
+	/** LED representing peer state. */
 	LED_ID_PEER_STATE,
 
+	/** Number of LEDs. */
 	LED_ID_COUNT
 };
 
@@ -59,12 +80,12 @@ enum led_id {
 #define LED_PEER_COUNT 1
 #endif
 
-#ifdef __cplusplus
-}
-#endif
-
 /**
  * @}
  */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _LED_STATE_H_ */
