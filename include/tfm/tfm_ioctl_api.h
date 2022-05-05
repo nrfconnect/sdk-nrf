2d446eb306c3677b1b105251a422a16333f43222
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-05-05 09:25:31 +0200

    tfm: Add platform services for firmware info functions
    Add TF-M platform services for firmware info functions.
    This is needed to be able to read the s0 and s1 firmware information
    struct from the non-secure processing environment.
    
    NCSDK-14890
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- include/tfm/tfm_ioctl_api.h
+++ include/tfm/tfm_ioctl_api.h
@@ -27,11 +27,44 @@
 /* Include core IOCTL services */
 #include <tfm_ioctl_core_api.h>
 
+#include <autoconf.h>
+
+#if CONFIG_FW_INFO
+#include <fw_info_bare.h>
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+
 /* Board specific IOCTL services can be added here */
+enum tfm_platform_ioctl_reqest_types_t {
+	TFM_PLATFORM_IOCTL_FW_INFO = TFM_PLATFORM_IOCTL_CORE_LAST,
+};
+
+#if CONFIG_FW_INFO
+
+/** @brief Argument list for each platform firmware info service.
+ */
+struct tfm_fw_info_args_t {
+	void *fw_address;
+	struct fw_info *info;
+};
+
+/** @brief Output list for each platform firmware info service
+ */
+struct tfm_fw_info_out_t {
+	uint32_t result;
+};
+
+int tfm_platform_firmware_info(uint32_t fw_address, struct fw_info *info);
+
+
+int tfm_platform_s0_active(uint32_t s0_address, uint32_t s1_address,
+			   bool *s0_active);
+
+#endif /* CONFIG_FW_INFO */
 
 #ifdef __cplusplus
 }
--- modules/tfm/tfm/boards/src/tfm_platform_system.c
+++ modules/tfm/tfm/boards/src/tfm_platform_system.c
@@ -14,8 +14,10 @@
 #include <string.h>
 #include <arm_cmse.h>
 
-#include "tfm_ioctl_core_api.h"
+#include "tfm_ioctl_api.h"
 #include "tfm_platform_hal_ioctl.h"
+#include <tfm_hal_isolation.h>
+#include <tfm_memory_utils.h>
 
 /* This contains the user provided allowed ranges */
 #include <tfm_read_ranges.h>
@@ -27,6 +29,53 @@ void tfm_platform_hal_system_reset(void)
 	/* Reset the system */
 	NVIC_SystemReset();
 }
+
+#if CONFIG_FW_INFO
+enum tfm_platform_err_t
+tfm_platform_hal_fw_info_service(psa_invec  *in_vec, psa_outvec *out_vec)
+{
+	const struct fw_info *tfm_info;
+	struct tfm_fw_info_args_t *args;
+	struct tfm_fw_info_out_t *out;
+	enum tfm_hal_status_t status;
+	enum tfm_platform_err_t err;
+	uint32_t attr = TFM_HAL_ACCESS_WRITABLE |
+			TFM_HAL_ACCESS_READABLE |
+			TFM_HAL_ACCESS_NS;
+
+	if (in_vec->len != sizeof(struct tfm_fw_info_args_t) ||
+	    out_vec->len != sizeof(struct tfm_fw_info_out_t)) {
+		return TFM_PLATFORM_ERR_INVALID_PARAM;
+	}
+
+	args = (struct tfm_fw_info_args_t *)in_vec->base;
+	out = (struct tfm_fw_info_out_t *)out_vec->base;
+
+	/* Assume failure, unless valid region is hit in the loop */
+	out->result = -1;
+	err = TFM_PLATFORM_ERR_INVALID_PARAM;
+
+	if (args->info == NULL) {
+		return TFM_PLATFORM_ERR_INVALID_PARAM;
+	}
+
+	status = tfm_hal_memory_has_access((uintptr_t)args->info,
+					    sizeof(struct fw_info),
+					    attr);
+	if (status != TFM_HAL_SUCCESS) {
+		return TFM_PLATFORM_ERR_INVALID_PARAM;
+	}
+
+	tfm_info = fw_info_find((uintptr_t)args->fw_address);
+	if (tfm_info != NULL) {
+		tfm_memcpy(args->info, tfm_info, sizeof(struct fw_info));
+		out->result = 0;
+		err = TFM_PLATFORM_ERR_SUCCESS;
+	}
+
+	return err;
+}
+#endif
 
 enum tfm_platform_err_t tfm_platform_hal_ioctl(tfm_platform_ioctl_req_t request,
                                                psa_invec  *in_vec,
@@ -41,8 +90,11 @@ enum tfm_platform_err_t tfm_platform_hal_ioctl(tfm_platform_ioctl_req_t request,
 		return tfm_platform_hal_gpio_service(in_vec, out_vec);
 #endif /* defined(GPIO_PIN_CNF_MCUSEL_Msk) */
 
+#if CONFIG_FW_INFO
 	/* Board specific IOCTL services */
-
+	case TFM_PLATFORM_IOCTL_FW_INFO:
+		return tfm_platform_hal_fw_info_service(in_vec, out_vec);
+#endif
 	/* Not a supported IOCTL service.*/
 	default:
 		return TFM_PLATFORM_ERR_NOT_SUPPORTED;
