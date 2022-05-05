009a8f08839c2cc4f4645aa587ab44e822d53a95
Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>
2022-05-05 14:58:00 +0300

    samples: nrf9160: modem_shell: at_cmd_mode: runtime set for line endings
    New subcommands to change default line ending characters for receive.
    Default line termination changed from CR to CR+LF.
    This enables nRF Connect / LTE Link Monitor / Cert Manager
    usage when at_comman_mode is running on MoSh.
    Jira: MOSH-282
    
    Signed-off-by: Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>

--- samples/nrf9160/modem_shell/src/at/at_cmd_mode.h
+++ samples/nrf9160/modem_shell/src/at/at_cmd_mode.h
@@ -7,6 +7,16 @@
 #ifndef MOSH_AT_CMD_MODE_H
 #define MOSH_AT_CMD_MODE_H
 
+/** @brief Line Termination Modes. */
+enum line_termination {
+	NULL_TERM, /**< Null Termination */
+	CR_TERM, /**< CR Termination */
+	LF_TERM, /**< LF Termination */
+	CR_LF_TERM /**< CR+LF Termination */
+};
+
 int at_cmd_mode_start(const struct shell *sh);
+
+void at_cmd_mode_line_termination_set(enum line_termination line_term);
 
 #endif /* MOSH_AT_CMD_MODE_H */
