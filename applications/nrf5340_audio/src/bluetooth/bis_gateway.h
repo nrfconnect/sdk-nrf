663e06576e3ce26a61c3c726db67d222be86e84c
Erik Robstad <erik.robstad@nordicsemi.no>
2022-05-06 13:23:54 +0200

    applications: nrf5340_audio: Added new files for BAP
    One set of .c/.h files for all of the 4 possible device types
    
    Signed-off-by: Erik Robstad <erik.robstad@nordicsemi.no>

---
+++ applications/nrf5340_audio/src/bluetooth/bis_gateway.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _BIS_GATEWAY_H_
+#define _BIS_GATEWAY_H_
+
+#include <zephyr.h>
+
+/**
+ * @brief Run Bluetooth LE Audio
+ */
+void le_audio_run(void);
+
+#endif /* _BIS_GATEWAY_H_ */
---
+++ applications/nrf5340_audio/src/bluetooth/cis_headset.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _CIS_HEADSET_H_
+#define _CIS_HEADSET_H_
+
+#include <zephyr.h>
+
+/**
+ * @brief Run Bluetooth LE Audio
+ */
+void le_audio_run(void);
+
+#endif /* _CIS_HEADSET_H_ */
