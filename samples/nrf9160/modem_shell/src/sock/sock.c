e3b21aeba6fd622b48dbcda976fce9824b90178d
Stig Bjørlykke <stig.bjorlykke@nordicsemi.no>
2022-05-09 08:29:00 +0200

    samples: nrf9160: modem_shell: Use PDN ID in getaddrinfo
    Use PDN ID in getaddrinfo() when doing connect() on a specific PDN.
    
    Signed-off-by: Stig Bjørlykke <stig.bjorlykke@nordicsemi.no>

--- samples/nrf9160/modem_shell/src/sock/sock.c
+++ samples/nrf9160/modem_shell/src/sock/sock.c
@@ -19,6 +19,7 @@
 #include <zephyr/net/tls_credentials.h>
 #include <fcntl.h>
 #include <nrf_socket.h>
+#include <modem/pdn.h>
 
 #include "sock.h"
 #include "mosh_defines.h"
@@ -308,7 +309,8 @@ static int sock_getaddrinfo(
 	int family,
 	int type,
 	char *address,
-	int port)
+	int port,
+	int pdn_cid)
 {
 	int err;
 
@@ -322,7 +324,16 @@ static int sock_getaddrinfo(
 			.ai_family = family,
 			.ai_socktype = type,
 		};
-		err = getaddrinfo(address, NULL, &hints, &socket_info->addrinfo);
+		char pdn_serv[12];
+		char *service = NULL;
+
+		if (pdn_cid > 0) {
+			snprintf(pdn_serv, sizeof(pdn_serv), "%d", pdn_id_get(pdn_cid));
+			service = pdn_serv;
+			hints.ai_flags = AI_PDNSERV;
+		}
+
+		err = getaddrinfo(address, service, &hints, &socket_info->addrinfo);
 		if (err) {
 			if (err == DNS_EAI_SYSTEM) {
 				mosh_error("getaddrinfo() failed, err %d errno %d", err, errno);
@@ -533,7 +544,7 @@ int sock_open_and_connect(
 	}
 
 	/* Get address */
-	err = sock_getaddrinfo(socket_info, family, type, address, port);
+	err = sock_getaddrinfo(socket_info, family, type, address, port, pdn_cid);
 	if (err) {
 		goto connect_error;
 	}
