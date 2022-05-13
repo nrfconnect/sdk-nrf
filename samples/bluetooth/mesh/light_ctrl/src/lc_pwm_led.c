779e80c3faeed85bd2bfcb0fcfb1bc85547042b5
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2022-05-13 11:09:25 +0200

    samples: bluetooth: Use pwm_dt_spec
    ... to align with the updated PWM API and also simplify a bit
    the code of these samples.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- samples/bluetooth/mesh/light_ctrl/src/lc_pwm_led.c
+++ samples/bluetooth/mesh/light_ctrl/src/lc_pwm_led.c
@@ -11,34 +11,18 @@
 
 #define PWM_LED0_NODE	DT_ALIAS(pwm_led0)
 
-/*
- * Devicetree helper macro which gets the 'flags' cell from the node's
- * pwms property, or returns 0 if the property has no 'flags' cell.
- */
-
-#if DT_PHA_HAS_CELL(PWM_LED0_NODE, pwms, flags)
-#define PWM_FLAGS DT_PWMS_FLAGS(PWM_LED0_NODE)
-#else
-#define PWM_FLAGS 0
-#endif
-
 #if DT_NODE_HAS_STATUS(PWM_LED0_NODE, okay)
-#define PWM_CTLR	DT_PWMS_CTLR(PWM_LED0_NODE)
-#define PWM_CHANNEL	DT_PWMS_CHANNEL(PWM_LED0_NODE)
+static const struct pwm_dt_spec led0 = PWM_DT_SPEC_GET(PWM_LED0_NODE);
 #else
 #error "Unsupported board: pwm-led0 devicetree alias is not defined"
-#define PWM_CTLR	DT_INVALID_NODE
-#define PWM_CHANNEL	0
 #endif
 
-static const struct device *pwm;
 #define PWM_PERIOD 1024
 
 void lc_pwm_led_init(void)
 {
-	pwm = DEVICE_DT_GET(PWM_CTLR);
-	if (!device_is_ready(pwm)) {
-		printk("Error: PWM device %s is not ready\n", pwm->name);
+	if (!device_is_ready(led0.dev)) {
+		printk("Error: PWM device %s is not ready\n", led0.dev->name);
 		return;
 	}
 }
@@ -49,9 +33,5 @@ void lc_pwm_led_set(uint16_t desired_lvl)
 		(PWM_PERIOD * desired_lvl) /
 		BT_MESH_LIGHTNESS_MAX;
 
-	pwm_pin_set_usec(pwm,
-		PWM_CHANNEL,
-		PWM_PERIOD,
-		scaled_lvl,
-		PWM_FLAGS);
+	pwm_set_dt(&led0, PWM_USEC(PWM_PERIOD), PWM_USEC(scaled_lvl));
 }
