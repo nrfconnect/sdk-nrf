f8576167a84936280238ec4c0016824cfa8e7d71
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2022-05-16 16:50:22 +0200

    samples: Add blinky sample for upgrading APP+NET
    Add a sample that can be used for upgrading any application to the
    blinky sample, including solutions that use both cores on the nRF5340.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

---
+++ samples/blinky_mcuboot/src/main.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <drivers/gpio.h>
+
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   1000
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED1_NODE DT_ALIAS(led1)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+
+void main(void)
+{
+	int ret;
+
+	printk("Hello world from %s\n", CONFIG_BOARD);
+
+	if (!device_is_ready(led.port)) {
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return;
+	}
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return;
+		}
+		k_msleep(SLEEP_TIME_MS);
+	}
+}
