f4c67b4946c789031f81af762d0c079c56664577
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2022-05-16 16:43:51 +0200

    bl_validation: Fix pointer arithmetic
    It is incorrect to dereference addresses from the new firmware info onto
    the currently running firmware image.
    The reset vector address should be calculated based on offsets, relative
    to the beginning of the source image.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- subsys/bootloader/bl_validation/bl_validation.c
+++ subsys/bootloader/bl_validation/bl_validation.c
@@ -256,7 +256,8 @@ static bool validate_firmware(uint32_t fw_dst_address, uint32_t fw_src_address,
 	const uint32_t fwinfo_end = (fwinfo_address + fwinfo->total_size);
 	const uint32_t fw_dst_end = (fw_dst_address + fwinfo->size);
 	const uint32_t fw_src_end = (fw_src_address + fwinfo->size);
-	const uint32_t reset_vector = ((const uint32_t *)(fwinfo->boot_address))[1];
+	const uint32_t stack_ptr_offset = (fwinfo->boot_address - fw_dst_address);
+	const uint32_t reset_vector = ((const uint32_t *)(fw_src_address + stack_ptr_offset))[1];
 
 	if (!fwinfo) {
 		PRINT("NULL parameter.\n\r");
