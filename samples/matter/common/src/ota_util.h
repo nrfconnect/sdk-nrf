6b8de3956b4965fe910cf94e779d96d0f21d79e8
Marcin Kajor <marcin.kajor@nordicsemi.no>
2022-05-20 07:45:16 +0200

    samples: matter: added missing doxygen comment
    GetFlashHandler() was missing proper description.
    This commit adds the corresponding doxygen comment.
    
    Signed-off-by: Marcin Kajor <marcin.kajor@nordicsemi.no>

--- samples/matter/common/src/ota_util.h
+++ samples/matter/common/src/ota_util.h
@@ -8,6 +8,12 @@
 
 #pragma once
 
+/**
+ * Get FlashHandler static instance.
+ *
+ * Returned object can be used to control the QSPI external flash,
+ * which can be introduced into sleep mode and woken up on demand.
+ */
 chip::DeviceLayer::FlashHandler &GetFlashHandler();
 
 /**
