cb21711e5dcf3ea4f42888f4ff86031eb39ff1c6
Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>
2022-05-24 17:19:53 +0200

    lib: modem_antenna: add debugging on nrf_modem_at_printf
    Add debugging information on nrf_modem_at_printf if MAGPIO or COEX
    AT commands fails.
    
    Signed-off-by: Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>

--- lib/modem_antenna/modem_antenna.c
+++ lib/modem_antenna/modem_antenna.c
@@ -15,21 +15,25 @@ NRF_MODEM_LIB_ON_INIT(gnss_cfg_init_hook, on_modem_lib_init, NULL);
 
 static void on_modem_lib_init(int ret, void *ctx)
 {
+	int err;
+
 	if (ret != 0) {
 		return;
 	}
 
 	if (strlen(CONFIG_MODEM_ANTENNA_AT_MAGPIO) > 0) {
 		LOG_DBG("Setting MAGPIO configuration: %s", CONFIG_MODEM_ANTENNA_AT_MAGPIO);
-		if (nrf_modem_at_printf("%s", CONFIG_MODEM_ANTENNA_AT_MAGPIO) != 0) {
-			LOG_ERR("Failed to set MAGPIO configuration");
+		err = nrf_modem_at_printf("%s", CONFIG_MODEM_ANTENNA_AT_MAGPIO);
+		if (err) {
+			LOG_ERR("Failed to set MAGPIO configuration (err: %d)", err);
 		}
 	}
 
 	if (strlen(CONFIG_MODEM_ANTENNA_AT_COEX0) > 0) {
 		LOG_DBG("Setting COEX0 configuration: %s", CONFIG_MODEM_ANTENNA_AT_COEX0);
-		if (nrf_modem_at_printf("%s", CONFIG_MODEM_ANTENNA_AT_COEX0) != 0) {
-			LOG_ERR("Failed to set COEX0 configuration");
+		err = nrf_modem_at_printf("%s", CONFIG_MODEM_ANTENNA_AT_COEX0);
+		if (err) {
+			LOG_ERR("Failed to set COEX0 configuration (err: %d)", err);
 		}
 	}
 }
