ec5b2b76bed94966547148a1418f4d2c5a6a2c44
Jouni Korhonen <jouni.korhonen@nordicsemi.no>
2022-05-29 23:40:00 +0300

    samples: lwm2m_client: Batch create devices
    Support for batch creation of devices to Coiote LWM2M server
    from a CSV file of ID+PSK pairs.
    
    Signed-off-by: Jouni Korhonen <jouni.korhonen@nordicsemi.no>

--- samples/nrf9160/lwm2m_client/scripts/coiote.py
+++ samples/nrf9160/lwm2m_client/scripts/coiote.py
@@ -17,18 +17,37 @@ import requests
 class Coiote():
     """Interact with Coiote server"""
     def __init__(self):
-        self.api_url = "https://eu.iot.avsystem.cloud/api/coiotedm/v3"
+        #
+        self.base_url = os.environ.get('COIOTE_URL')
+
+        if self.base_url is None:
+            self.base_url = 'https://eu.iot.avsystem.cloud'
+
+        #
+        self.api_url = f"{self.base_url}/api/coiotedm/v3"
+
+        #
         self.user = os.environ.get("COIOTE_USER")
+
+        #
         self.passwd = os.environ.get("COIOTE_PASSWD")
+
+        #
         self.headers = {'accept': 'application/json',
                         'content-type': 'application/json'}
-        self.domain = os.environ.get("COIOTE-DOMAIN")
-        token = os.environ.get("COIOTE-TOKEN")
+
+        #
+        self.domain = os.environ.get("COIOTE_DOMAIN")
+
+        #
+        token = os.environ.get("COIOTE_TOKEN")
 
         if token is not None:
             self.headers = {'accept': 'application/json',
                             'content-type': 'application/json',
                             'authorization': f'Bearer {token}'}
+
+        #
         if self.user and self.passwd:
             self.auth = (self.user, self.passwd)
         else:
@@ -36,7 +55,7 @@ class Coiote():
 
     def authenticate(self):
         """Request authentication token"""
-        auth_url = "https://eu.iot.avsystem.cloud/api/auth/oauth_password"
+        auth_url = f"{self.base_url}/api/auth/oauth_password"
         headers = {'content-type': 'application/x-www-form-urlencoded'}
         payload = f"grant_type=password&username={self.user}&password={self.passwd}"
         resp = requests.post(auth_url, headers=headers, data=payload)
@@ -67,11 +86,14 @@ class Coiote():
                             headers=self.headers, data=data, auth=self.auth)
         return Coiote.handle_response(resp)
 
-    def post(self, path, data):
+    def post(self, path, data, handle=True):
         """Send HTTP POST query"""
         resp = requests.post(f"{self.api_url}{path}",
                              headers=self.headers, data=data, auth=self.auth)
-        return Coiote.handle_response(resp)
+        if handle:
+            return Coiote.handle_response(resp)
+        else:
+            return resp
 
     def delete(self, path):
         """Send HTTP DELETE query"""
@@ -82,15 +104,53 @@ class Coiote():
     @staticmethod
     def handle_response(resp):
         """Generic response handler for all queries"""
-        if resp.text is None or len(resp.text) == 0:
-            logging.warning('Response code {%d}', resp.status_code)
+        if resp is None:
             return None
-        obj = json.loads(resp.text)
-        if resp.status_code >= 300 or resp.status_code < 200:
-            logging.warning('Coiote: %s', obj['error'])
-            return None
-        logging.debug(json.dumps(obj, indent=4))
+
+        obj = None
+
+        if hasattr(resp,"text"):
+            if (resp.text is None or len(resp.text) == 0):
+                logging.warning('Response code {%d}', resp.status_code)
+                return None
+
+            obj = json.loads(resp.text)
+
+            if resp.status_code >= 300 or resp.status_code < 200:
+                logging.warning('Coiote: %s', obj['error'])
+                return None
+
+            logging.debug(json.dumps(obj, indent=4))
         return obj
+
+    @staticmethod
+    def handle_batch_response(resp):
+        if resp is None:
+            logging.warning("Empty response received.")
+            return None,0,0,0
+
+        status_code = resp.status_code
+        succeeded = 0
+        failed = 0
+
+        if hasattr(resp,"text") is False:
+            logging.warning("No response payload received.")
+            return None,status_code,succeeded,failed
+
+        obj = json.loads(resp.text)
+
+        if status_code < 200 or status_code >= 300:
+            logging.warning("Coiote returned HTTP error %d",status_code)
+            logging.warning('Coiote: %s', obj['error'])
+
+        if obj:
+            if "succeeded" in obj:
+                succeeded = obj["succeeded"]
+            if "failed" in obj:
+                failed = obj["failed"]
+
+        return obj,status_code,succeeded,failed
+
 
     def get_device(self, dev_id):
         """Get device information"""
@@ -103,11 +163,86 @@ class Coiote():
         else:
             logging.info('Coiote: Deleted device %s', dev_id)
 
-    def create_device(self, dev_id, psk):
+    def batch_create_device(self, dev_file, model, lines, prefix):
+        # dev_list is in form:  \"352656100030868\",\"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\"
+
+        logging.info(f"URL = {self.api_url}")
+
+        line_cnt = 0
+        line_tot = 0
+        succeeded = 0
+        failed = 0
+
+        with open(dev_file,"r") as fh:
+            objs = []
+            domain = self.get_domain()
+
+            for line in fh:
+                tmp = line.split(',')
+                dev_id = prefix+tmp[0].strip('"')
+                psk = tmp[1].strip('\n"')
+
+                objs.append({
+                    "id": dev_id,
+                    "connectorType": "bootstrap",
+                    "properties": {
+                        "endpointName": dev_id,
+                    },
+                    "domain": domain,
+                    "securityMode": "psk",
+                    "dtlsIdentity": dev_id,
+                    "dtlsPsk": {"HexadecimalPsk": psk}
+                })
+
+                if model:
+                    objs[-1]["properties"]["genericModelName"] = model
+
+                line_cnt += 1
+                line_tot += 1
+
+                if line_cnt == lines:
+                    if arg.debug:
+                        logging.debug("Sending POST:")
+                        logging.debug(json.dumps(objs,indent=2))
+
+                    resp = self.post(f"/devices/batch",json.dumps(objs),False)
+                    obj,status_code,tmp_s,tmp_f = Coiote.handle_batch_response(resp)
+
+                    if arg.debug:
+                        logging.debug("Received response (status %d):",status_code)
+                        if obj:
+                            logging.debug(json.dumps(obj,indent=2))
+
+                    succeeded += tmp_s
+                    failed += tmp_f
+
+                    objs = []
+                    line_cnt = 0
+
+            if line_cnt > 0:
+                if arg.debug:
+                    logging.debug("Sending POST:")
+                    logging.debug(json.dumps(objs,indent=2))
+
+                resp = self.post(f"/devices/batch",json.dumps(objs),False)
+                obj,status_code,tmp_s,tmp_f = Coiote.handle_batch_response(resp)
+
+                if arg.debug:
+                    logging.debug("Received response (status %d):",status_code)
+                    if obj:
+                        logging.debug(json.dumps(obj,indent=2))
+
+                succeeded += tmp_s
+                failed += tmp_f
+
+        logging.info(f"Coiote: uploaded %d devices into domain '%s', %d succeeded, %d failed.",
+                line_tot,self.domain,succeeded,failed)
+
+
+    def create_device(self, dev_id, psk, model=None, bootstrap=True):
         """Create a device to bootstrap server"""
         domain = self.get_domain()
         obj = {
-            "id": dev_id + '-bs',
             "connectorType": "bootstrap",
             "properties": {
                 "endpointName": dev_id
@@ -117,6 +252,19 @@ class Coiote():
             "dtlsIdentity": dev_id,
             "dtlsPsk": {"HexadecimalPsk": psk}
         }
+
+        if bootstrap:
+            obj["id"] = f"{dev_id}-bs"
+        else:
+            obj["id"] = dev_id
+
+        if model:
+            obj["properties"]["genericModelName"] = model
+
+        if arg.debug:
+            logging.debug(json.dumps(obj,indent=2))
+
+
         if self.post('/devices', json.dumps(obj)) is None:
             logging.error('Coiote: Failed to create device %s', dev_id)
         else:
@@ -130,7 +278,8 @@ class Coiote():
             logging.debug('Coiote: Domain is %s', self.domain)
         return self.domain
 
-
+#
+#
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
@@ -165,11 +314,16 @@ if __name__ == "__main__":
 
     def create(args):
         """Create a device"""
-        logging.info(coiote.create_device(args.id, args.psk))
+        logging.info(coiote.create_device(args.id, args.psk, args.model,args.bootstrap))
+
+    def batch_create(args):
+        """Batch create devices from a file"""
+        coiote.batch_create_device(args.file,args.model,args.lines,args.prefix)
 
     parser = argparse.ArgumentParser(
         description='Coiote device management')
     parser.set_defaults(func=None)
+    parser.add_argument("-d", "--debug", action="store_true", default=False, help="Output debug information.")
     subparsers = parser.add_subparsers(title='commands')
     login_pars = subparsers.add_parser(
         'login', help='Log in using username and password')
@@ -190,10 +344,18 @@ if __name__ == "__main__":
     create_pars.set_defaults(func=create)
     create_pars.add_argument('id', help='Device ID')
     create_pars.add_argument('psk')
-    create_pars.add_argument(
-        '-b', action='store_true', help='Use bootstrap')
+    create_pars.add_argument('-m', '--model', action='store', default=None, help='Generic Model Name')
+    create_pars.add_argument('-b', '--bootstrap', default=False, action='store_true', help='Use bootstrap')
     get_domain_pars = subparsers.add_parser('domain', help='Get domain')
     get_domain_pars.set_defaults(func=get_domain)
+    batch_pars = subparsers.add_parser('batch', help='Batch create devices from a file.')
+    batch_pars.set_defaults(func=batch_create)
+    batch_pars.add_argument('file', type=str, action='store', help='CSV file with IMEI+PSK pairs.')
+    batch_pars.add_argument('-m', '--model', action='store', default=None, help='Generic Model Name.')
+    batch_pars.add_argument(
+        '-p', '--prefix', action='store', default="urn:imei:", help="Prefix string for IDs. Default is 'urn:imei:'")
+    batch_pars.add_argument(
+        '-l', '--lines', action='store', type=int, default=100, help='Maximum batch size. Default is 100.')
 
     arg = parser.parse_args()
     if arg.func is None:
