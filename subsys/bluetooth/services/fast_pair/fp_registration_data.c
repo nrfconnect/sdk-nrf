15ae835d0e77143424ed371d86ecd56bd33daaa6
Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>
2022-05-31 14:26:25 +0200

    bluetooth: fast_pair: Add hash to verify provisioning data
    SHA-256 hash from Fast Pair provisioning data is added after data in a
    Fast Pair provisioning data partition. Fast Pair subsystem verifies
    whether hash is correct to ensure provisioning data integrity.
    
    Jira: NCSDK-15100
    
    Signed-off-by: Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>

--- subsys/bluetooth/services/fast_pair/fp_registration_data.c
+++ subsys/bluetooth/services/fast_pair/fp_registration_data.c
@@ -10,6 +10,7 @@
 #include <zephyr/storage/flash_map.h>
 #include <pm_config.h>
 
+#include "fp_crypto.h"
 #include "fp_registration_data.h"
 
 #define FP_PARTITION_ID		PM_BT_FAST_PAIR_ID
@@ -22,16 +23,19 @@ static const uint8_t fp_magic[] = {0xFA, 0x57, 0xFA, 0x57};
 #define FP_DATA_ALIGN		 4
 #define FP_DATA_START		 0
 #define FP_OFFSET(_prev_item_offset, _prev_item_len) \
-	(_prev_item_offset + ROUND_UP(_prev_item_len, FP_DATA_ALIGN))
+	((_prev_item_offset) + ROUND_UP(_prev_item_len, FP_DATA_ALIGN))
 
-#define FP_MAGIC_HEADER_OFF	 FP_DATA_START
-#define FP_MODEL_ID_OFF		 FP_OFFSET(FP_MAGIC_HEADER_OFF, FP_MAGIC_SIZE)
+#define FP_MAGIC_OFF		 FP_DATA_START
+#define FP_MODEL_ID_OFF		 FP_OFFSET(FP_MAGIC_OFF, FP_MAGIC_SIZE)
 #define FP_ANTI_SPOOFING_KEY_OFF FP_OFFSET(FP_MODEL_ID_OFF, FP_REG_DATA_MODEL_ID_LEN)
-#define FP_MAGIC_TRAILER_OFF	 FP_OFFSET(FP_ANTI_SPOOFING_KEY_OFF, \
+#define FP_HASH_OFF		 FP_OFFSET(FP_ANTI_SPOOFING_KEY_OFF, \
 					   FP_REG_DATA_ANTI_SPOOFING_PRIV_KEY_LEN)
+#define FP_END_OFF		 FP_OFFSET(FP_HASH_OFF, FP_CRYPTO_SHA256_HASH_LEN)
 
-BUILD_ASSERT(FP_OFFSET(FP_MAGIC_TRAILER_OFF, FP_MAGIC_SIZE) <= FP_PARTITION_SIZE,
-	     "Fast Pair registration data partition is too small");
+#define FP_DATA_SIZE		 (FP_END_OFF - FP_DATA_START)
+#define FP_OFFSET_TO_DATA_IDX(_offset) ((_offset) - FP_DATA_START)
+
+BUILD_ASSERT(FP_END_OFF <= FP_PARTITION_SIZE, "Fast Pair registration data partition is too small");
 
 static bool data_valid;
 
@@ -81,17 +85,26 @@ int fp_get_anti_spoofing_priv_key(uint8_t *buf, size_t size)
 	return err;
 }
 
-static int validate_fp_magic(const struct flash_area *fa, off_t off)
+static int validate_fp_magic(const uint8_t *magic_data)
+{
+	if (memcmp(magic_data, fp_magic, FP_MAGIC_SIZE)) {
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int validate_fp_hash(const uint8_t *prov_data, const uint8_t *hash)
 {
 	int err;
-	uint8_t read_buf[FP_MAGIC_SIZE];
+	uint8_t hash_calc[FP_CRYPTO_SHA256_HASH_LEN];
 
-	err = flash_area_read(fa, off, read_buf, FP_MAGIC_SIZE);
+	err = fp_crypto_sha256(hash_calc, prov_data, FP_DATA_SIZE - FP_CRYPTO_SHA256_HASH_LEN);
 	if (err) {
 		return err;
 	}
 
-	if (memcmp(read_buf, fp_magic, FP_MAGIC_SIZE)) {
+	if (memcmp(hash, hash_calc, FP_CRYPTO_SHA256_HASH_LEN)) {
 		err = -EINVAL;
 	}
 
@@ -103,15 +116,20 @@ static int fp_registration_data_validate(const struct device *unused)
 	ARG_UNUSED(unused);
 
 	int err;
+	uint8_t data[FP_DATA_SIZE];
 	const struct flash_area *fa = NULL;
 
 	err = flash_area_open(FP_PARTITION_ID, &fa);
 	if (!err) {
-		err = validate_fp_magic(fa, FP_MAGIC_HEADER_OFF);
+		err = flash_area_read(fa, FP_DATA_START, data, FP_DATA_SIZE);
 	}
 
 	if (!err) {
-		err = validate_fp_magic(fa, FP_MAGIC_TRAILER_OFF);
+		err = validate_fp_magic(&data[FP_OFFSET_TO_DATA_IDX(FP_MAGIC_OFF)]);
+	}
+
+	if (!err) {
+		err = validate_fp_hash(data, &data[FP_OFFSET_TO_DATA_IDX(FP_HASH_OFF)]);
 	}
 
 	if (fa) {
