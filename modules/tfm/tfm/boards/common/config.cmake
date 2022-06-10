d46b5a2b1f5a780495a3c489cf092c6982337782
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-06-10 16:31:05 +0200

    tfm: Add support for storage partitions in external flash
    Add support for storage partitions to be placed in external flash when
    TF-M is enabled.
    Improve the handling of the non-secure storage by always enabling it and
    pulling in a change in TF-M that excludes the non-secure storage when
    it is empty.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/tfm/tfm/boards/common/config.cmake
+++ modules/tfm/tfm/boards/common/config.cmake
@@ -10,3 +10,7 @@ set(PLATFORM_DEFAULT_CRYPTO_KEYS        FALSE       CACHE BOOL      "Use default
 # Disable crypto regression tests that are not supported
 set(TFM_CRYPTO_TEST_ALG_CFB             OFF         CACHE BOOL      "Test CFB cryptography mode")
 set(TFM_CRYPTO_TEST_ALG_OFB             OFF         CACHE BOOL      "Test OFB cryptography mode")
+
+# Always enable the nonsecure storage partition.
+# It will still be excluded if the partition manager excludes it.
+set(NRF_NS_STORAGE                      ON          CACHE BOOL      "Enable non-secure storage partition")
