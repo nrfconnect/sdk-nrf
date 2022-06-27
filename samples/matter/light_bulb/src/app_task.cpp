b562f3d2845c7df7821f145fc2d1a0b97f0aa19a
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2022-06-27 10:48:13 +0200

    samples: matter: Fixed open commissioning window condition
    After removing the fabric it is not possible to open commissioning
    window again, due to inproper advertising start condition. Replace
    condition checking IsThreadProvisioned with FabricCount.
    
    Signed-off-by: Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>

--- samples/matter/light_bulb/src/app_task.cpp
+++ samples/matter/light_bulb/src/app_task.cpp
@@ -352,9 +352,8 @@ void AppTask::StartThreadHandler()
 
 void AppTask::StartBLEAdvertisingHandler()
 {
-	/* Don't allow on starting Matter service BLE advertising after Thread provisioning. */
-	if (ConnectivityMgr().IsThreadProvisioned()) {
-		LOG_INF("NFC Tag emulation and Matter service BLE advertising not started - device is commissioned to a Thread network.");
+	if (Server::GetInstance().GetFabricTable().FabricCount() != 0) {
+		LOG_INF("Matter service BLE advertising not started - device is already commissioned");
 		return;
 	}
 
