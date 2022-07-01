53f4507141f2c39d771b43ca7547b6d6f0fadc9b
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-07-01 12:09:13 +0200

    tfm: Add common config file for all nrf platforms
    Add common config file for all nrf platforms and move common set
    configuration and values.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/tfm/tfm/boards/nrf9160/config.cmake
+++ modules/tfm/tfm/boards/nrf9160/config.cmake
@@ -5,13 +5,10 @@
 #
 #-------------------------------------------------------------------------------
 
+include(${CMAKE_CURRENT_LIST_DIR}/../common/config.cmake)
+
 set(PLATFORM_PATH platform/ext/target/nordic_nrf/)
 include(${PLATFORM_PATH}/common/nrf9160/config.cmake)
 
 # Override the AEAD algorithm configuration since nRF9160 supports only AES_CCM
 set(PS_CRYPTO_AEAD_ALG                  PSA_ALG_CCM CACHE STRING    "The AEAD algorithm to use for authenticated encryption in Protected Storage")
-set(PLATFORM_DEFAULT_CRYPTO_KEYS        FALSE       CACHE BOOL      "Use default crypto keys implementation.")
-
-# Disable crypto regression tests that are not supported
-set(TFM_CRYPTO_TEST_ALG_CFB             OFF         CACHE BOOL      "Test CFB cryptography mode")
-set(TFM_CRYPTO_TEST_ALG_OFB             OFF         CACHE BOOL      "Test OFB cryptography mode")
