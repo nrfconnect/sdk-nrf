32c827b20f3c5ab85a359e572d366da310fe2767
Marek Pieta <Marek.Pieta@nordicsemi.no>
2022-07-07 17:52:13 +0200

    samples: bluetooth: peripheral_lbs: Fix build issue
    Change fixes build issue when CONFIG_BT_LBS_SECURITY_ENABLED=n.
    
    Jira: NCSDK-16060
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- samples/bluetooth/peripheral_lbs/src/main.c
+++ samples/bluetooth/peripheral_lbs/src/main.c
@@ -141,6 +141,7 @@ static struct bt_conn_auth_info_cb conn_auth_info_callbacks = {
 };
 #else
 static struct bt_conn_auth_cb conn_auth_callbacks;
+static struct bt_conn_auth_info_cb conn_auth_info_callbacks;
 #endif
 
 static void app_led_cb(bool led_state)
