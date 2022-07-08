2ddff8e7737f5819017336a2e4205e5163d7f377
Krishna T <krishna.t@nordicsemi.no>
2022-07-08 19:47:04 +0530

    wifi: Add a Wi-Fi sample
    This sample can be used to test nRF Wi-Fi drivers, for now nrf7002
    driver is supported.
    
    Signed-off-by: Sridhar Nuvusetty <sridhar.nuvusetty@nordicsemi.no>
    Signed-off-by: Sachin Kulkarni <sachin.kulkarni@nordicsemi.no>
    Signed-off-by: Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>
    Signed-off-by: Krishna T <krishna.t@nordicsemi.no>

---
+++ doc/nrf/samples/samples_wifi.rst
@@ -0,0 +1,11 @@
+.. _wifi_samples:
+
+Wi-Fi samples
+#################
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Subpages
+   :glob:
+
+   ../../../samples/wifi/*/README
---
+++ samples/wifi/shell/src/wifi_main.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/sys/printk.h>
+#include <nrfx_clock.h>
+
+void main(void)
+{
+#ifdef CLOCK_FEATURE_HFCLK_DIVIDE_PRESENT
+	/* For now hardcode to 128MHz */
+	nrfx_clock_divider_set(NRF_CLOCK_DOMAIN_HFCLK,
+			       NRF_CLOCK_HFCLK_DIV_1);
+#endif
+	printk("Starting %s with CPU frequency: %d MHz\n", CONFIG_BOARD, SystemCoreClock/MHZ(1));
+}
