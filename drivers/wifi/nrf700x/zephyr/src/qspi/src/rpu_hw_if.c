7da634f46fdb52ce8c78eb8d7fe4dcd8aa1a5354
Krishna T <krishna.t@nordicsemi.no>
2022-07-17 23:39:47 +0530

    drivers: wifi: Move dummy IRQ to shell
    The dummy IRQ is only for testing shell functionality.
    
    Signed-off-by: Krishna T <krishna.t@nordicsemi.no>

--- drivers/wifi/nrf700x/zephyr/src/qspi/src/rpu_hw_if.c
+++ drivers/wifi/nrf700x/zephyr/src/qspi/src/rpu_hw_if.c
@@ -53,14 +53,8 @@ uint32_t rpu_7002_memmap[][3] = {
 	{ 0x300000, 0x338000, 1 }
 };
 
-static struct gpio_callback irq_callback_data;
 static const struct qspi_dev *qdev;
 static struct qspi_config *cfg;
-
-void irq_handler(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
-{
-	LOG_DBG("\n !!! IRQ Hit !!!!\n");
-}
 
 static int validate_addr_blk(uint32_t start_addr,
 							 uint32_t end_addr,
@@ -313,8 +307,6 @@ int rpu_enable(void)
 	rpu_qspi_init();
 	rpu_wakeup();
 	rpu_clks_on();
-
-	rpu_irq_config(&irq_callback_data, irq_handler);
 
 	return 0;
 }
