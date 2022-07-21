b831efef2cf9bf69f444cbe841414346c4cccf95
Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>
2022-07-21 12:40:37 +0530

    hostap: Implement nRF Wi-Fi management APIs
    These APIs will be used by nRF Wi-Fi management to interact with
    wpa_supplicant.
    
    Signed-off-by: Krishna T <krishna.t@nordicsemi.no>
    Signed-off-by: Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>

---
+++ modules/hostap/src/supp_api.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ *
+ */
+
+#ifndef ZEPHYR_SUPP_MGMT_H
+#define ZEPHYR_SUPP_MGMT_H
+
+#include <zephyr/net/wifi_mgmt.h>
+
+#define MAX_SSID_LEN 32
+#define MAC_ADDR_LEN 6
+
+/**
+ * @brief Request a scan
+ *
+ * @param iface_name: Wi-Fi interface name to use
+ * @param scan_result_cb_t: Application specific callback when the scan
+ * 			attempt is completed.
+ *
+ * @return: 0 for OK; -1 for ERROR
+ */
+int zephyr_supp_scan(const struct device *dev, scan_result_cb_t cb);
+
+/**
+ * @brief Request a connection
+ *
+ * @param iface_name: Wi-Fi interface name to use
+ * @param params: Connection details
+ *
+ * @return: 0 for OK; -1 for ERROR
+ */
+int zephyr_supp_connect(const struct device *dev,
+						struct wifi_connect_req_params *params);
+/**
+ * @brief Forces station to disconnect and stops any subsequent scan
+ *  or connection attempts
+ *
+ * @param iface_name: Wi-Fi interface name to use
+ *
+ * @return: 0 for OK; -1 for ERROR
+ */
+int zephyr_supp_disconnect(const struct device *dev);
+
+/**
+ * @brief
+ *
+ * @param iface_name: Wi-Fi interface name to use
+ * @param wifi_iface_status: Status structure to fill
+ *
+ * @return: 0 for OK; -1 for ERROR
+ */
+int zephyr_supp_status(const struct device *dev,
+	struct wifi_iface_status *status);
+#endif /* ZEPHYR_SUPP_MGMT_H */
