9901365638c78ae159a59cc42362071f78edacfe
Dominik Ermel <dominik.ermel@nordicsemi.no>
2022-07-27 14:42:43 +0000

    partition_manager: Fix mergehex.py path
    The file has been moved.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- cmake/partition_manager.cmake
+++ cmake/partition_manager.cmake
@@ -374,7 +374,7 @@ foreach(container ${containers} ${merged})
     OUTPUT ${PROJECT_BINARY_DIR}/${container}.hex
     COMMAND
     ${PYTHON_EXECUTABLE}
-    ${ZEPHYR_BASE}/scripts/mergehex.py
+    ${ZEPHYR_BASE}/scripts/build/mergehex.py
     -o ${PROJECT_BINARY_DIR}/${container}.hex
     ${${container}overlap_arg}
     ${${container}hex_files}
@@ -629,7 +629,7 @@ to the external flash")
       OUTPUT ${final_merged}
       COMMAND
       ${PYTHON_EXECUTABLE}
-      ${ZEPHYR_BASE}/scripts/mergehex.py
+      ${ZEPHYR_BASE}/scripts/build/mergehex.py
       -o ${final_merged}
       ${domain_hex_files}
       DEPENDS
