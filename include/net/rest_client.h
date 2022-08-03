314579e1291e0d93810d8d55354796a1b569dd71
Justin Morton <justin.morton@nordicsemi.no>
2022-08-03 10:00:06 -0700

    net: lib: rest_client: update timeout_ms comment
    Update timeout_ms comment to indicate that a
    zero value will result in an immediate timeout.
    
    Signed-off-by: Justin Morton <justin.morton@nordicsemi.no>

--- include/net/rest_client.h
+++ include/net/rest_client.h
@@ -86,6 +86,7 @@ struct rest_client_req_context {
 	/** User-defined timeout value for REST request. The timeout is set individually
 	 *  for socket connection creation and data transfer meaning REST request can take
 	 *  longer than this given timeout. To disable, set the timeout duration to SYS_FOREVER_MS.
+	 *  A value of zero will result in an immediate timeout.
 	 *  Default: CONFIG_REST_CLIENT_REST_REQUEST_TIMEOUT.
 	 */
 	int32_t timeout_ms;
