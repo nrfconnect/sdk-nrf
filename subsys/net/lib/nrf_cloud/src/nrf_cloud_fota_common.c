240d68dda4fccbf871e9e510cdc7ec25db1faa0d
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2022-08-04 12:58:49 +0100

    net: lib: nrf_cloud: Update paths to include zephyr prefix
    The zephyr prefix is needed to include these files
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- subsys/net/lib/nrf_cloud/src/nrf_cloud_fota_common.c
+++ subsys/net/lib/nrf_cloud/src/nrf_cloud_fota_common.c
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#include <zephyr.h>
+#include <zephyr/zephyr.h>
 #include <stdbool.h>
 #include <nrf_modem.h>
 #include <modem/nrf_modem_lib.h>
