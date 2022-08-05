e34ce645c92ad3c3c26cecbd6a1f1f488f99afb7
Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>
2022-08-05 09:06:45 +0300

    samples: nrf9160: modem_shell: link: unnecessary PDN evt subscription
    Removing unnecessary (re)subscription to PDN lib events when going to
    normal mode.
    Recent PDN lib changes caused this be causing multiple mosh
    prints of the same PDN event.
    Jira: MOSH-321
    
    Signed-off-by: Jani Hirsimäki <jani.hirsimaki@nordicsemi.no>

--- samples/nrf9160/modem_shell/src/link/link.c
+++ samples/nrf9160/modem_shell/src/link/link.c
@@ -616,9 +616,6 @@ int link_func_mode_set(enum lte_lc_func_mode fun, bool rel14_used)
 		/* Enable/disable Rel14 features before going to normal mode */
 		link_enable_disable_rel14_features(rel14_used);
 
-		/* (Re)register for PDN lib notifications */
-		link_shell_pdn_events_subscribe();
-
 		/* (Re)register for rsrp notifications: */
 		modem_info_rsrp_register(link_rsrp_signal_handler);
 
