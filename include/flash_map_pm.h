e4803fc0c9730e56067305ed917a0e3af4d7f386
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2022-08-09 14:00:36 +0100

    flash_map: pm: Update to use device instead of device name
    Updates the partition manager configuration to use the device directly
    rather than by using the device name which is no longer supported
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- include/flash_map_pm.h
+++ include/flash_map_pm.h
@@ -9,6 +9,7 @@
 
 #include <pm_config.h>
 #include <zephyr/sys/util.h>
+#include <zephyr/devicetree.h>
 
 /* Aliases for zephyr - mcuboot/ncs style naming */
 #define image_0 mcuboot_primary
@@ -34,15 +35,16 @@
 
 #define FLASH_AREA_ID(label) PM_ID(label)
 
-#define FLASH_AREA_OFFSET(label) \
-	UTIL_CAT(PM_, UTIL_CAT(UTIL_CAT(PM_, UTIL_CAT(PM_ID(label), _LABEL)), _OFFSET))
+#define FLASH_DATA_FIELD(label, x) \
+	UTIL_CAT(PM_, UTIL_CAT(UTIL_CAT(PM_, UTIL_CAT(PM_ID(label), _LABEL)), x))
 
-#define FLASH_AREA_SIZE(label) \
-	UTIL_CAT(PM_, UTIL_CAT(UTIL_CAT(PM_, UTIL_CAT(PM_ID(label), _LABEL)), _SIZE))
+#define FLASH_AREA_OFFSET(label) FLASH_DATA_FIELD(label, _OFFSET)
 
-#define FLASH_AREA_DEVICE(label) \
-	device_get_binding( \
-		UTIL_CAT(PM_, UTIL_CAT(UTIL_CAT(PM_, UTIL_CAT(PM_ID(label), _LABEL)), _DEV_NAME)))
+#define FLASH_AREA_SIZE(label) FLASH_DATA_FIELD(label, _SIZE)
+
+#define FLASH_AREA_DEVICE(label) COND_CODE_1(DT_NODE_EXISTS(FLASH_DATA_FIELD(label, _DEV)), \
+				 (DEVICE_DT_GET_OR_NULL(FLASH_DATA_FIELD(label, _DEV))), \
+				 (DEVICE_DT_GET_OR_NULL(DT_NODELABEL(FLASH_DATA_FIELD(label, _DEV)))))
 
 #define FLASH_AREA_LABEL_EXISTS(label) \
 	IS_ENABLED(PM_IS_ENABLED(label))
