b6adfc1edfbaf36519eefd2172b2180b095e3229
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2022-08-12 15:02:33 +0100

    mpsl: fem: Replace label comparison with device tree macros
    This tidies up the MPSL FEM GPIO configuration code to use device tree
    macros rather than checking the string of the GPIO port to detemine a
    pin mapping
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- subsys/mpsl/fem/common/include/mpsl_fem_utils.h
+++ subsys/mpsl/fem/common/include/mpsl_fem_utils.h
@@ -18,16 +18,6 @@
  */
 void mpsl_fem_extended_pin_to_mpsl_fem_pin(uint32_t pin_num, mpsl_fem_pin_t *p_fem_pin);
 
-#else  // !defined(CONFIG_MPSL_FEM_PIN_FORWARDER)
-
-/** @brief Embeds GPIO port number into GPIO pin number.
- *
- * @param[inout]  pin  Pointer to be filled with updated GPIO pin number.
- * @param[in]     lbl  Label property from a gpio phandle-array property the update
- *                     should be done for.
- */
-void mpsl_fem_pin_extend_with_port(uint8_t *pin, const char *lbl);
-
 #endif /* !defined(CONFIG_MPSL_FEM_PIN_FORWARDER) */
 
 #endif /* MPSL_FEM_UTILS_H__ */
