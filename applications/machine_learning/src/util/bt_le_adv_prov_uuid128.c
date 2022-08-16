2fc37c70566b10b8074db50bd5592de04088f845
Marek Pieta <Marek.Pieta@nordicsemi.no>
2022-08-16 14:31:35 +0200

    caf: ble_adv: Integrate Bluetooth LE advertising providers
    Change integrates Bluetooth LE advertising providers. The subsystem
    is used instead of def file to manage advertising and scan response
    data. Change also applies needed changes for nRF Desktop and nRF
    Machine Learning applications.
    
    Jira: NCSDK-8849
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/machine_learning/src/util/bt_le_adv_prov_uuid128.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/sys/byteorder.h>
+#include <bluetooth/services/nus.h>
+#include <bluetooth/bt_le_adv_prov.h>
+
+static const struct bt_data data = BT_DATA_BYTES(BT_DATA_UUID128_ALL,
+						 BT_UUID_NUS_VAL);
+
+
+static int get_data(struct bt_data *sd, const struct bt_le_adv_prov_adv_state *state,
+		    struct bt_le_adv_prov_feedback *fb)
+{
+	ARG_UNUSED(fb);
+
+	if (state->bond_cnt > 0) {
+		return -ENOENT;
+	}
+
+	*sd = data;
+
+	return 0;
+}
+
+BT_LE_ADV_PROV_SD_PROVIDER_REGISTER(uuid128_all, get_data);
---
+++ applications/nrf_desktop/src/util/bt_le_adv_prov_uuid16.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/sys/byteorder.h>
+#include <bluetooth/bt_le_adv_prov.h>
+
+
+static int get_data(struct bt_data *ad, const struct bt_le_adv_prov_adv_state *state,
+		    struct bt_le_adv_prov_feedback *fb)
+{
+	ARG_UNUSED(fb);
+
+	if (state->bond_cnt > 0) {
+		return -ENOENT;
+	}
+
+	static const uint8_t data[] = {
+#if CONFIG_DESKTOP_HIDS_ENABLE
+		0x12, 0x18,   /* HID Service */
+#endif
+#if CONFIG_DESKTOP_BAS_ENABLE
+		0x0f, 0x18,   /* Battery Service */
+#endif
+	};
+
+	ad->type = BT_DATA_UUID16_ALL;
+	ad->data_len = sizeof(data);
+	ad->data = data;
+
+	return 0;
+}
+
+BT_LE_ADV_PROV_AD_PROVIDER_REGISTER(uuid16_all, get_data);
