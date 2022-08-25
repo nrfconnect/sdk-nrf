180f67e4d9da17fbe4e57d0750bb26af4c53642b
Dominik Ermel <dominik.ermel@nordicsemi.no>
2022-08-25 14:12:15 +0000

    flash_map/pm: Add FIXED_PARTITION_ macros
    The upstream Zephyr is dropping usage of fixed-partitions node
    label property to identify partitions and replacing FLASH_AREA_
    macros with FIXED_PARTITION_ macros.
    This commit adds FIXED_PARTITION_ macros that are supposed
    to be available in parallel to FLASH_AREA_ macros, until the
    later get removed.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- include/flash_map_pm.h
+++ include/flash_map_pm.h
@@ -13,19 +13,29 @@
 
 /* Aliases for zephyr - mcuboot/ncs style naming */
 #define image_0 mcuboot_primary
+#define slot0_partition mcuboot_primary
 #define image_1 mcuboot_secondary
+#define slot1_partition mcuboot_secondary
 #define image_0_nonsecure mcuboot_primary
+#define slot0_ns_partition mcuboot_primary
 #define image_1_nonsecure mcuboot_secondary
+#define slot1_ns_partition mcuboot_secondary
 #define image_2 mcuboot_primary_1
 #define image_3 mcuboot_secondary_1
+#define slot2_partition mcuboot_primary_1
+#define slot3_partition mcuboot_secondary_1
+#define image_scratch mcuboot_scratch
 #define image_scratch mcuboot_scratch
 
 #if (CONFIG_SETTINGS_FCB || CONFIG_SETTINGS_NVS || defined(PM_SETTINGS_STORAGE_ID))
 #define storage settings_storage
+#define storage_partition settings_storage
 #elif CONFIG_FILE_SYSTEM_LITTLEFS
 #define storage littlefs_storage
+#define storage_partition littlefs_storage
 #elif CONFIG_NVS
 #define storage nvs_storage
+#define storage_partition nvs_storage
 #endif
 
 #define PM_ID(label) PM_##label##_ID
@@ -33,20 +43,26 @@
 
 #define FLASH_AREA_LABEL_STR(label) #label
 
-#define FLASH_AREA_ID(label) PM_ID(label)
+#define FIXED_PARTITION_ID(label) PM_ID(label)
+#define FLASH_AREA_ID(label) FIXED_PARTITION_ID(label)
 
-#define FLASH_DATA_FIELD(label, x) \
+#define FIXED_PARTITION_DATA_FIELD(label, x) \
 	UTIL_CAT(PM_, UTIL_CAT(UTIL_CAT(PM_, UTIL_CAT(PM_ID(label), _LABEL)), x))
+#define FLASH_DATA_FIELD(label, x) FIXED_PARTITION_DATA_FIELD(label, x)
 
-#define FLASH_AREA_OFFSET(label) FLASH_DATA_FIELD(label, _OFFSET)
+#define FIXED_PARTITION_OFFSET(label) FIXED_PARTITION_DATA_FIELD(label, _OFFSET)
+#define FLASH_AREA_OFFSET(label) FIXED_PARTITION_OFFSET(label)
 
-#define FLASH_AREA_SIZE(label) FLASH_DATA_FIELD(label, _SIZE)
+#define FIXED_PARTITION_SIZE(label) FIXED_PARTITION_DATA_FIELD(label, _SIZE)
+#define FLASH_AREA_SIZE(label) FIXED_PARTITION_SIZE(label)
 
-#define FLASH_AREA_DEVICE(label) COND_CODE_1(DT_NODE_EXISTS(FLASH_DATA_FIELD(label, _DEV)), \
-				 (DEVICE_DT_GET_OR_NULL(FLASH_DATA_FIELD(label, _DEV))), \
-				 (DEVICE_DT_GET_OR_NULL(DT_NODELABEL(FLASH_DATA_FIELD(label, _DEV)))))
+#define FIXED_PARTITION_DEVICE(label) \
+	COND_CODE_1(DT_NODE_EXISTS(FIXED_PARTITION_DATA_FIELD(label, _DEV)), \
+		    (DEVICE_DT_GET_OR_NULL(FIXED_PARTITION_DATA_FIELD(label, _DEV))), \
+		    (DEVICE_DT_GET_OR_NULL(DT_NODELABEL(FIXED_PARTITION_DATA_FIELD(label, _DEV)))))
+#define FLASH_AREA_DEVICE(label) FIXED_PARTITION_DEVICE(label)
 
-#define FLASH_AREA_LABEL_EXISTS(label) \
-	IS_ENABLED(PM_IS_ENABLED(label))
+#define FIXED_PARTITION_EXISTS(label) IS_ENABLED(PM_IS_ENABLED(label))
+#define FLASH_AREA_LABEL_EXISTS(label) FIXED_PARTITION_EXISTS(label)
 
 #endif /* FLASH_MAP_PM_H_*/
