265fb61da0780963a727123c62dc797f1d532ef9
Adam Wojasinski <adam.wojasinski@nordicsemi.no>
2022-08-26 15:23:16 +0200

    mpsl: cx: Replace deprecated nrfx API
    This commit replaces API that became deprecated with the release
    of nrfx2.9 - see CHANGELOG in zephyrproject-rtos:hal_nordic repository
    
    Signed-off-by: Adam Wojasinski <adam.wojasinski@nordicsemi.no>

--- subsys/mpsl/cx/thread/mpsl_cx_thread.c
+++ subsys/mpsl/cx/thread/mpsl_cx_thread.c
@@ -224,17 +224,17 @@ static int mpsl_cx_init(const struct device *dev)
 #if DT_NODE_HAS_PROP(CX_NODE, req_gpios)
 	uint8_t req_pin = NRF_DT_GPIOS_TO_PSEL(CX_NODE, req_gpios);
 
-	soc_secure_gpio_pin_mcu_select(req_pin, NRF_GPIO_PIN_MCUSEL_NETWORK);
+	soc_secure_gpio_pin_mcu_select(req_pin, NRF_GPIO_PIN_SEL_NETWORK);
 #endif
 #if DT_NODE_HAS_PROP(CX_NODE, pri_dir_gpios)
 	uint8_t pri_dir_pin = NRF_DT_GPIOS_TO_PSEL(CX_NODE, pri_dir_gpios);
 
-	soc_secure_gpio_pin_mcu_select(pri_dir_pin, NRF_GPIO_PIN_MCUSEL_NETWORK);
+	soc_secure_gpio_pin_mcu_select(pri_dir_pin, NRF_GPIO_PIN_SEL_NETWORK);
 #endif
 #if DT_NODE_HAS_PROP(CX_NODE, grant_gpios)
 	uint8_t grant_pin = NRF_DT_GPIOS_TO_PSEL(CX_NODE, grant_gpios);
 
-	soc_secure_gpio_pin_mcu_select(grant_pin, NRF_GPIO_PIN_MCUSEL_NETWORK);
+	soc_secure_gpio_pin_mcu_select(grant_pin, NRF_GPIO_PIN_SEL_NETWORK);
 #endif
 
 	return 0;
