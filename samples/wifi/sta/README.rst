5050c1b45017bebb5476ca868d564cb8cec9eb9d
Triveni Danda <triveni.danda@nordicsemi.no>
2022-09-22 07:33:11 -0700

    samples: wifi: Add STA sample
    This sample allows Nordic chipset to connect to an Access point
    
    Signed-off-by: Triveni Danda <triveni.danda@nordicsemi.no>

---
+++ samples/wifi/sta/README.rst
@@ -0,0 +1,93 @@
+.. _wifi_station_sample:
+
+Wi-Fi: Station
+##############
+
+.. contents::
+   :local:
+   :depth: 2
+
+The Station sample demonstrates how to connect the Wi-Fi station to a specified access point using DHCP.
+
+Requirements
+************
+
+The sample supports the following development kit:
+
+.. table-from-sample-yaml::
+
+Overview
+********
+
+This sample can perform Wi-Fi operations such as connect and disconnect in the 2.4GHz and 5GHz bands depending on the capabilities of an access point.
+
+Using this sample, the development kit can connect to the specified access point in :abbr:`STA (Station)` mode.
+
+Building and running
+********************
+
+.. |sample path| replace:: :file:`samples/wifi/sta`
+
+.. include:: /includes/build_and_run_ns.txt
+
+Currently, the following board(s) are supported:
+
+* nRF7002 DK
+
+You must configure the following Wi-Fi credentials in ``prj.conf``:
+
+* Network name (SSID)
+* Key management
+* Password
+
+.. note::
+   You can also use menuconfig to enable ``Key management`` option.
+
+See :ref:`zephyr:menuconfig` in the Zephyr documentation for instructions on how to run menuconfig.
+
+To build for the nRF7002 DK, use the ``nrf7002dk_nrf5340_cpuapp`` build target.
+The following is an example of the CLI command:
+
+.. code-block:: console
+
+   west build -b nrf7002dk_nrf5340_cpuapp
+
+Testing
+=======
+
+|test_sample|
+
+#. |connect_kit|
+#. |connect_terminal|
+
+   The sample shows the following output:
+
+   .. code-block:: console
+
+      <inf> sta: Connection requested
+      <inf> sta: Connected
+      Status: successful
+      ==================
+        State: COMPLETED
+        Interface Mode: STATION
+        Link Mode: WIFI 6 (802.11ax/HE)
+        SSID: NORDIC-GUEST
+        BSSID: F0:1D:2D:72:EB:EF
+        Band: 5GHz
+        Channel: 116
+        Security: WPA2-PSK
+        MFP: Optional
+        RSSI: -55
+      <inf> sta: IP address: 192.168.48.41
+      <inf> sta: Disconnect requested
+      <inf> sta: Disconnection request done (0)
+      Status: successful
+      ==================
+        State: DISCONNECTED
+
+Dependencies
+************
+
+This sample uses the following `sdk-nrfxlib`_ library:
+
+* :ref:`nrfxlib:nrf_security`
---
+++ samples/wifi/sta/src/main.c
@@ -0,0 +1,261 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ * @brief WiFi station sample
+ */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(sta, CONFIG_LOG_DEFAULT_LEVEL);
+
+#include <nrfx_clock.h>
+#include <zephyr/zephyr.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <zephyr/shell/shell.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/init.h>
+
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/net_event.h>
+
+#include "net_private.h"
+
+#define WIFI_SHELL_MODULE "wifi"
+
+#define WIFI_SHELL_MGMT_EVENTS (NET_EVENT_WIFI_CONNECT_RESULT |		\
+				NET_EVENT_WIFI_DISCONNECT_RESULT)
+
+#define MAX_SSID_LEN        32
+
+static struct net_mgmt_event_callback wifi_shell_mgmt_cb;
+static struct net_mgmt_event_callback net_shell_mgmt_cb;
+
+static struct {
+	const struct shell *sh;
+	union {
+		struct {
+			uint8_t disconnecting	: 1;
+			uint8_t _unused		: 6;
+		};
+		uint8_t all;
+	};
+} context;
+
+static int cmd_wifi_status(void)
+{
+	struct net_if *iface = net_if_get_default();
+	struct wifi_iface_status status = { 0 };
+
+	if (net_mgmt(NET_REQUEST_WIFI_IFACE_STATUS, iface, &status,
+				sizeof(struct wifi_iface_status))) {
+		printk("Status request failed\n");
+
+		return -ENOEXEC;
+	}
+
+	printk("Status: successful\n");
+	printk("==================\n");
+	printk("State: %s\n", wifi_state_txt(status.state));
+
+	if (status.state >= WIFI_STATE_ASSOCIATED) {
+		uint8_t mac_string_buf[sizeof("xx:xx:xx:xx:xx:xx")];
+
+		printk("Interface Mode: %s\n",
+		       wifi_mode_txt(status.iface_mode));
+		printk("Link Mode: %s\n",
+		       wifi_link_mode_txt(status.link_mode));
+		printk("SSID: %-32s\n", status.ssid);
+		printk("BSSID: %s\n",
+		       status.bssid ? net_sprint_ll_addr_buf(status.bssid,
+				WIFI_MAC_ADDR_LEN, mac_string_buf,
+				sizeof(mac_string_buf)) :
+				"");
+		printk("Band: %s\n", wifi_band_txt(status.band));
+		printk("Channel: %d\n", status.channel);
+		printk("Security: %s\n", wifi_security_txt(status.security));
+		printk("MFP: %s\n", wifi_mfp_txt(status.mfp));
+		printk("RSSI: %d\n", status.rssi);
+	}
+
+
+	return 0;
+}
+
+static void handle_wifi_connect_result(struct net_mgmt_event_callback *cb)
+{
+	const struct wifi_status *status =
+		(const struct wifi_status *) cb->info;
+
+	if (status->status) {
+		LOG_ERR("Connection request failed (%d)", status->status);
+	} else {
+		LOG_INF("Connected");
+	}
+
+	cmd_wifi_status();
+}
+
+static void handle_wifi_disconnect_result(struct net_mgmt_event_callback *cb)
+{
+	const struct wifi_status *status =
+		(const struct wifi_status *) cb->info;
+
+	if (context.disconnecting) {
+		LOG_INF("Disconnection request %s (%d)",
+			 status->status ? "failed" : "done",
+					status->status);
+		context.disconnecting = false;
+	} else {
+		LOG_INF("Disconnected");
+	}
+
+	cmd_wifi_status();
+}
+
+static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb,
+				     uint32_t mgmt_event, struct net_if *iface)
+{
+	switch (mgmt_event) {
+	case NET_EVENT_WIFI_CONNECT_RESULT:
+		handle_wifi_connect_result(cb);
+		break;
+	case NET_EVENT_WIFI_DISCONNECT_RESULT:
+		handle_wifi_disconnect_result(cb);
+		break;
+	default:
+		break;
+	}
+}
+
+static void print_dhcp_ip(struct net_mgmt_event_callback *cb)
+{
+	/* Get DHCP info from struct net_if_dhcpv4 and print */
+	const struct net_if_dhcpv4 *dhcpv4 = cb->info;
+	const struct in_addr *addr = &dhcpv4->requested_ip;
+	char dhcp_info[128];
+
+	net_addr_ntop(AF_INET, addr, dhcp_info, sizeof(dhcp_info));
+
+	LOG_INF("IP address: %s", dhcp_info);
+}
+
+static void net_mgmt_event_handler(struct net_mgmt_event_callback *cb,
+				    uint32_t mgmt_event, struct net_if *iface)
+{
+	switch (mgmt_event) {
+	case NET_EVENT_IPV4_DHCP_BOUND:
+		print_dhcp_ip(cb);
+		break;
+	default:
+		break;
+	}
+}
+
+static int __wifi_args_to_params(struct wifi_connect_req_params *params)
+{
+	params->timeout = SYS_FOREVER_MS;
+
+	/* SSID */
+	params->ssid = CONFIG_STA_SAMPLE_SSID;
+	params->ssid_length = strlen(params->ssid);
+
+#if defined(CONFIG_STA_KEY_MGMT_WPA2)
+	params->security = 1;
+#elif defined(CONFIG_STA_KEY_MGMT_WPA2_256)
+	params->security = 2;
+#elif defined(CONFIG_STA_KEY_MGMT_WPA3)
+	params->security = 3;
+#else
+	params->security = 0;
+#endif
+
+#if !defined(CONFIG_STA_KEY_MGMT_NONE)
+	params->psk = CONFIG_STA_SAMPLE_PASSWORD;
+	params->psk_length = strlen(params->psk);
+#endif
+	params->channel = WIFI_CHANNEL_ANY;
+
+	/* MFP (optional) */
+	params->mfp = WIFI_MFP_OPTIONAL;
+
+	return 0;
+}
+
+static int wifi_connect(void)
+{
+	struct net_if *iface = net_if_get_default();
+	static struct wifi_connect_req_params cnx_params;
+
+	__wifi_args_to_params(&cnx_params);
+
+	if (net_mgmt(NET_REQUEST_WIFI_CONNECT, iface,
+		     &cnx_params, sizeof(struct wifi_connect_req_params))) {
+		LOG_ERR("Connection request failed");
+
+		return -ENOEXEC;
+	}
+
+	LOG_INF("Connection requested");
+
+	return 0;
+}
+
+static int wifi_disconnect(void)
+{
+	struct net_if *iface = net_if_get_default();
+	int status;
+
+	context.disconnecting = true;
+
+	status = net_mgmt(NET_REQUEST_WIFI_DISCONNECT, iface, NULL, 0);
+
+	if (status) {
+		context.disconnecting = false;
+
+		if (status == -EALREADY) {
+			LOG_INF("Already disconnected");
+		} else {
+			LOG_ERR("Disconnect request failed");
+			return -ENOEXEC;
+		}
+	} else {
+		LOG_INF("Disconnect requested");
+	}
+
+	return 0;
+}
+
+void main(void)
+{
+	context.all = 0U;
+
+	net_mgmt_init_event_callback(&wifi_shell_mgmt_cb,
+				     wifi_mgmt_event_handler,
+				     WIFI_SHELL_MGMT_EVENTS);
+
+	net_mgmt_add_event_callback(&wifi_shell_mgmt_cb);
+
+
+	net_mgmt_init_event_callback(&net_shell_mgmt_cb,
+				     net_mgmt_event_handler,
+				     NET_EVENT_IPV4_DHCP_BOUND);
+
+	net_mgmt_add_event_callback(&net_shell_mgmt_cb);
+
+#ifdef CLOCK_FEATURE_HFCLK_DIVIDE_PRESENT
+	/* For now hardcode to 128MHz */
+	nrfx_clock_divider_set(NRF_CLOCK_DOMAIN_HFCLK,
+			       NRF_CLOCK_HFCLK_DIV_1);
+#endif
+	LOG_INF("Starting %s with CPU frequency: %d MHz", CONFIG_BOARD, SystemCoreClock/MHZ(1));
+	k_sleep(K_SECONDS(1));
+
+	wifi_connect();
+	k_sleep(K_SECONDS(30));
+	wifi_disconnect();
+}
