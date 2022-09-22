e76e58d683a33a8485214b0219948a061f19be66
Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>
2022-09-22 12:16:20 +0200

    bluetooth: controller: Allow providing handlers for HCI commands
    Provide a simple way to add handlers for HCI commands.
    The user can now define its own vendor specific HCI commands, or it can
    override the behavior of existing ones.
    
    I didn't create a public header file for it for now as it has not been
    requested.
    
    Signed-off-by: Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>

--- subsys/bluetooth/controller/hci_internal.h
+++ subsys/bluetooth/controller/hci_internal.h
@@ -17,12 +17,54 @@
 
 /** @brief Send an HCI command packet to the SoftDevice Controller.
  *
+ * If the application has provided a user handler, this handler get precedence
+ * above the default HCI command handlers. See @ref hci_internal_user_cmd_handler_register.
+ *
  * @param[in] cmd_in  HCI Command packet. The first byte in the buffer should correspond to
  *                    OpCode, as specified by the Bluetooth Core Specification.
  *
  * @return Zero on success or (negative) error code otherwise.
  */
 int hci_internal_cmd_put(uint8_t *cmd_in);
+
+/** A user implementable HCI command handler
+ *
+ * What is done in the command handler is up to the user.
+ * Parameters can be returned to the host through the raw_event_out output parameter.
+ *
+ * When the command handler returns, a Command Complete or Command Status event is generated.
+ *
+ * @param[in]  cmd               The HCI command itself. The first byte in the buffer corresponds
+ *                               to OpCode, as specified by the Bluetooth Core Specification.
+ * @param[out] raw_event_out     Parameters to be returned from the event as return parameters in
+ *                               the Command Complete event.
+ *                               Parameters can only be returned if the generated event is
+ *                               a Command Complete event.
+ * @param[out] param_length_out  Length of parameters to be returned.
+ * @param[out] gives_cmd_status  Set to true if the command is returning a Command Status event.
+ *
+ * @return Bluetooth status code. BT_HCI_ERR_UNKNOWN_CMD if unknown.
+ */
+typedef uint8_t (*hci_internal_user_cmd_handler_t)(uint8_t const *cmd,
+						   uint8_t *raw_event_out,
+						   uint8_t *param_length_out,
+						   bool *gives_cmd_status);
+
+/** @brief Register a user handler for HCI commands.
+ *
+ * The user handler can be used to handle custom HCI commands.
+ *
+ * The user handler will have precedence over all other command handling.
+ * Therefore, the application needs to ensure it is not using opcodes that
+ * are used for other Bluetooth or vendor specific HCI commands.
+ * See sdc_hci_vs.h for the opcodes that are reserved.
+ *
+ * @note Only one handler can be registered.
+ *
+ * @param[in] handler
+ * @return Zero on success or (negative) error code otherwise
+ */
+int hci_internal_user_cmd_handler_register(const hci_internal_user_cmd_handler_t handler);
 
 /** @brief Retrieve an HCI event packet from the SoftDevice Controller.
  *
