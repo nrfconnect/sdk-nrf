490ac4af0128cdfbed92440dc775c561104febe0
Vidar Lillebø <vidar.lillebo@nordicsemi.no>
2022-09-28 14:21:09 +0200

    tfm: Add support for configurable baud rate
    Add support for configurable baud rate for TF-M secure log.
    
    Signed-off-by: Vidar Lillebø <vidar.lillebo@nordicsemi.no>

---
+++ modules/tfm/tfm/boards/board/device_cfg.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef DEVICE_CFG_H__
+#define DEVICE_CFG_H__
+
+/**
+ * \file device_cfg.h
+ * \brief
+ * This is the default device configuration file with all peripherals
+ * defined and configured to be use via the secure and/or non-secure base
+ * address.
+ */
+/* ARRAY_SIZE causes a conflict as it is defined both by TF-M and indirectly by devicetree.h */
+#undef ARRAY_SIZE
+#include <autoconf.h>
+#include <zephyr/devicetree.h>
+
+#define DEFAULT_UART_BAUDRATE DT_PROP(DT_NODELABEL(uart1), current_speed)
+
+#endif /* DEVICE_CFG_H__ */
