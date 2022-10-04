62c9f5b0ac2c6f5105a645061eb43f1ab270261d
Marek Pieta <Marek.Pieta@nordicsemi.no>
2022-10-04 17:06:33 +0200

    bluetooth: adv_prov: Introduce Fast Pair provider
    Introduce Fast Pair advertising data provider. The provider exposes
    dedicated API to control toggling between discoverable and not
    discoverable advertising modes.
    
    Jira: NCSDK-16744
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ include/bluetooth/adv_prov/fast_pair.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef BT_ADV_PROV_FAST_PAIR_H_
+#define BT_ADV_PROV_FAST_PAIR_H_
+
+#include <bluetooth/services/fast_pair.h>
+
+/**
+ * @defgroup bt_adv_prov_fast_pair Fast Pair advertising data provider API
+ * @brief Fast Pair advertising data provider API
+ *
+ * @{
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Set Fast Pair advertising mode.
+ *
+ * User shall make sure that advertising mode is not modified while Fast Pair advertising data
+ * provider is providing advertising data.
+ *
+ * @param[in] fp_adv_mode	Fast Pair advertising mode.
+ */
+void bt_adv_prov_fast_pair_mode_set(enum bt_fast_pair_adv_mode fp_adv_mode);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* BT_ADV_PROV_FAST_PAIR_H_ */
