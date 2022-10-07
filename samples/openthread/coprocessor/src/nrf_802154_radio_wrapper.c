8eb3d892ae38dfed6606fa4ae64b6a7523e35971
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2022-10-07 15:24:12 +0200

    samples: openthread: Get rid of CONFIG_NET_CONFIG_IEEE802154_DEV_NAME
    This Kconfig option was removed and now the `zephyr,ieee802154` chosen
    node needs to be used instead.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- samples/openthread/coprocessor/src/nrf_802154_radio_wrapper.c
+++ samples/openthread/coprocessor/src/nrf_802154_radio_wrapper.c
@@ -8,7 +8,8 @@
 #include <zephyr/device.h>
 #include <zephyr/net/ieee802154_radio.h>
 
-static const struct device *radio_dev;
+static const struct device *const radio_dev =
+	DEVICE_DT_GET(DT_CHOSEN(zephyr_ieee802154));
 static struct ieee802154_radio_api *radio_api;
 
 bool nrf_802154_radio_wrapper_auto_ack_get(void)
@@ -23,8 +24,7 @@ void nrf_802154_radio_wrapper_auto_ack_set(bool enabled)
 
 uint16_t nrf_802154_radio_wrapper_hw_capabilities_get(void)
 {
-	radio_dev = device_get_binding(CONFIG_NET_CONFIG_IEEE802154_DEV_NAME);
-	__ASSERT_NO_MSG(radio_dev != NULL);
+	__ASSERT_NO_MSG(device_is_ready(radio_dev));
 
 	radio_api = (struct ieee802154_radio_api *)radio_dev->api;
 	__ASSERT_NO_MSG(radio_api != NULL);
