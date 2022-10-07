21f941bfe38fafc33352d644b5331b4611e800dc
Jarno Lämsä <jarno.lamsa@nordicsemi.no>
2022-10-07 10:12:55 +0300

    net: lib: lwm2m_client_utils: Add API for using the location objects
    The API allows sending location requests to the lwm2m server.
    
    Signed-off-by: Jarno Lämsä <jarno.lamsa@nordicsemi.no>

--- include/net/lwm2m_client_utils_location.h
+++ include/net/lwm2m_client_utils_location.h
@@ -32,6 +32,48 @@
 #define LOCATION_ASSIST_NEEDS_INTEGRITY			BIT(8)
 
 #if defined(CONFIG_LWM2M_CLIENT_UTILS_LOCATION_ASSIST_AGPS)
+/**
+ * @brief Set the A-GPS request mask
+ *
+ * @param agps_req The A-GPS request coming from the GNSS module.
+ */
+void location_assistance_agps_set_mask(const struct nrf_modem_gnss_agps_data_frame *agps_req);
+
+/**
+ * @brief Send the A-GPS assistance request to LwM2M server
+ *
+ * @param ctx LwM2M client context for sending the data.
+ * @param confirmable Boolean value to indicate should the server return confirmation.
+ * @return Returns a negative error code (errno.h) indicating
+ *         reason of failure or 0 for success.
+ */
+int location_assistance_agps_request_send(struct lwm2m_ctx *ctx, bool confirmable);
+#endif
+
+/**
+ * @brief Send the Ground Fix request to LwM2M server
+ *
+ * @param ctx LwM2M client context for sending the data.
+ * @param confirmable Boolean value to indicate should the server return confirmation.
+ * @return Returns a negative error code (errno.h) indicating
+ *         reason of failure or 0 for success.
+ */
+int location_assistance_ground_fix_request_send(struct lwm2m_ctx *ctx, bool confirmable);
+
+#if defined(CONFIG_LWM2M_CLIENT_UTILS_LOCATION_ASSIST_PGPS)
+/**
+ * @brief Send the P-GPS assistance request to LwM2M server
+ *
+ * @param ctx LwM2M client context for sending the data.
+ * @param confirmable Boolean value to indicate should the server return confirmation.
+ * @return Returns a negative error code (errno.h) indicating
+ *         reason of failure or 0 for success.
+ */
+int location_assistance_pgps_request_send(struct lwm2m_ctx *ctx, bool confirmable);
+#endif
+
+#if defined(CONFIG_LWM2M_CLIENT_UTILS_LOCATION_ASSIST_EVENTS)
+#if defined(CONFIG_LWM2M_CLIENT_UTILS_LOCATION_ASSIST_AGPS)
 struct gnss_agps_request_event {
 	struct app_event_header header;
 
@@ -40,7 +82,7 @@ struct gnss_agps_request_event {
 
 APP_EVENT_TYPE_DECLARE(gnss_agps_request_event);
 
-#endif
+#endif /* CONFIG_LWM2M_CLIENT_UTILS_LOCATION_ASSIST_AGPS */
 #if defined(CONFIG_LWM2M_CLIENT_UTILS_LOCATION_ASSIST_CELL)
 struct cell_location_request_event {
 	struct app_event_header header;
@@ -53,9 +95,18 @@ struct cell_location_inform_event {
 };
 
 APP_EVENT_TYPE_DECLARE(cell_location_inform_event);
-#endif
+#endif /* CONFIG_LWM2M_CLIENT_UTILS_LOCATION_ASSIST_CELL */
+
+#if defined(CONFIG_LWM2M_CLIENT_UTILS_LOCATION_ASSIST_PGPS)
+struct pgps_data_request_event {
+	struct app_event_header header;
+};
+
+APP_EVENT_TYPE_DECLARE(pgps_data_request_event);
+#endif /* CONFIG_LWM2M_CLIEN_UTILS_LOCATION_ASSIST_PGPS */
 
 int location_event_handler_init(struct lwm2m_ctx *ctx);
+#endif /* CONFIG_LWM2M_CLIENT_UTILS_LOCATION_ASSIST_EVENTS */
 
 #endif /* LWM2M_CLIENT_UTILS_LOCATION_H__ */
 
