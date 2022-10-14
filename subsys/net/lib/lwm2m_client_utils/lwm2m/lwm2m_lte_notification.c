c7ed9f3d7cd6c4ac119a2e3f9b7b32c5452eec2e
Juha Heiskanen <juha.heiskanen@nordicsemi.no>
2022-10-14 09:49:11 +0300

    net: lwm2m: Removed LTE registration callback
    Removed LTE notification from subsys and moved functionality
    to application for controlling LTE connection status.
    
    Signed-off-by: Juha Heiskanen <juha.heiskanen@nordicsemi.no>

--- subsys/net/lib/lwm2m_client_utils/lwm2m/lwm2m_lte_notification.c
+++ subsys/net/lib/lwm2m_client_utils/lwm2m/lwm2m_lte_notification.c
@@ -126,38 +126,6 @@ int lwm2m_ncell_handler_register(void)
 }
 #endif /* CONFIG_LWM2M_CLIENT_UTILS_NEIGHBOUR_CELL_LISTENER */
 
-#if defined(CONFIG_LWM2M_CLIENT_UTILS_NWK_REG_NOTIFICATION)
-static lwm2m_lte_nwk_reg_update_state_cb_t lte_nwk_reg_cb;
-
-int lwm2m_lte_reg_handler_register(lwm2m_lte_nwk_reg_update_state_cb_t cb)
-{
-	lte_nwk_reg_cb = cb;
-	lte_lc_register_handler(lte_notify_handler);
-
-	return 0;
-}
-
-static void lwm2m_lte_reg_handler_notify(enum lte_lc_nw_reg_status nw_reg_status)
-{
-	bool status;
-
-	if (!lte_nwk_reg_cb) {
-		return;
-	}
-
-	LOG_DBG("LTE NW status: %d", nw_reg_status);
-	if ((nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME) ||
-	    (nw_reg_status == LTE_LC_NW_REG_REGISTERED_ROAMING)) {
-		status = true;
-	} else {
-		status = false;
-	}
-
-	lte_nwk_reg_cb(status);
-}
-#endif /* CONFIG_LWM2M_CLIENT_UTILS_NWK_REG_NOTIFICATION */
-
-
 void lte_notify_handler(const struct lte_lc_evt *const evt)
 {
 	switch (evt->type) {
@@ -185,12 +153,6 @@ void lte_notify_handler(const struct lte_lc_evt *const evt)
 		}
 		break;
 #endif /* CONFIG_LWM2M_CLIENT_UTILS_NEIGHBOUR_CELL_LISTENER */
-
-#if defined(CONFIG_LWM2M_CLIENT_UTILS_NWK_REG_NOTIFICATION)
-	case LTE_LC_EVT_NW_REG_STATUS:
-		lwm2m_lte_reg_handler_notify(evt->nw_reg_status);
-		break;
-#endif /* CONFIG_LWM2M_CLIENT_UTILS_NWK_REG_NOTIFICATION */
 
 #if defined(CONFIG_LTE_LC_TAU_PRE_WARNING_NOTIFICATIONS)
 	case LTE_LC_EVT_TAU_PRE_WARNING:
