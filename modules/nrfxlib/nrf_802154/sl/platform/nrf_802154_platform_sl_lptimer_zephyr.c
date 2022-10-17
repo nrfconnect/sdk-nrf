85036ea3f78cc14bd926fb0abcb73373d1dc50cc
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2022-10-17 12:33:28 +0200

    modules: Correct license identifiers
    Correct license identifiers to LicenseRef-Nordic-5-Clause
    where applicable.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- modules/nrfxlib/nrf_802154/sl/platform/nrf_802154_platform_sl_lptimer_zephyr.c
+++ modules/nrfxlib/nrf_802154/sl/platform/nrf_802154_platform_sl_lptimer_zephyr.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2022 Nordic Semiconductor ASA
  *
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
 #include "platform/nrf_802154_platform_sl_lptimer.h"
