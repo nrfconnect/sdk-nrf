aafcc375cf616d3509e3cf8f5c10efccfeff83e9
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2022-10-17 12:20:53 +0200

    tests: nrf5340_audio: Correct license identifiers
    Correct license identifiers to LicenseRef-Nordic-5-Clause
    where applicable.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- tests/nrf5340_audio/tone/main.c
+++ tests/nrf5340_audio/tone/main.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2018 Nordic Semiconductor ASA
  *
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
 #include <zephyr/ztest.h>
