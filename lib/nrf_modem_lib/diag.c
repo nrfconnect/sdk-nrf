1811d227b6b120791c3fb2e2e92896fbaa36cb08
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2022-10-17 12:15:45 +0200

    lib: nrf_modem_lib: Add missing license information in diag.c
    Add missing license information in the diag.c file.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- lib/nrf_modem_lib/diag.c
+++ lib/nrf_modem_lib/diag.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
 #include <zephyr/kernel.h>
 #include <zephyr/init.h>
 #include <zephyr/sys/sys_heap.h>
