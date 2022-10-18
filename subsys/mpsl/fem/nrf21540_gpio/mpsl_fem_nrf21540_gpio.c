425f5ad5f121e138beb630d78ea12e51c4a39ec6
Jędrzej Ciupis <jedrzej.ciupis@nordicsemi.no>
2022-10-18 15:43:22 +0200

    mpsl: fem: extract common helper functions
    This commit removes unnecessary duplications in Front-End Module
    implementations by extracting common helper functions to a separate
    module.
    
    Signed-off-by: Jędrzej Ciupis <jedrzej.ciupis@nordicsemi.no>

--- subsys/mpsl/fem/nrf21540_gpio/mpsl_fem_nrf21540_gpio.c
+++ subsys/mpsl/fem/nrf21540_gpio/mpsl_fem_nrf21540_gpio.c
@@ -4,8 +4,6 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#if defined(CONFIG_MPSL_FEM_NRF21540_GPIO)
-
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <string.h>
@@ -17,13 +15,8 @@
 #include "mpsl_fem_power_model_interface.h"
 #endif
 #include <nrfx_gpiote.h>
-#if IS_ENABLED(CONFIG_HAS_HW_NRF_PPI)
-#include <nrfx_ppi.h>
-#elif IS_ENABLED(CONFIG_HAS_HW_NRF_DPPIC)
-#include <nrfx_dppi.h>
-#endif
-#else /* !defined(CONFIG_MPSL_FEM_PIN_FORWARDER) */
 #include <mpsl_fem_utils.h>
+#else /* !defined(CONFIG_MPSL_FEM_PIN_FORWARDER) */
 #include <soc_secure.h>
 #if IS_ENABLED(CONFIG_PINCTRL)
 #include <pinctrl_soc.h>
@@ -31,83 +24,6 @@
 #endif /* !defined(CONFIG_MPSL_FEM_PIN_FORWARDER) */
 
 #if !defined(CONFIG_MPSL_FEM_PIN_FORWARDER)
-
-#define MPSL_FEM_GPIO_POLARITY_GET(dt_property) \
-	((GPIO_ACTIVE_LOW & \
-	  DT_GPIO_FLAGS(DT_NODELABEL(nrf_radio_fem), dt_property)) \
-	 ? false : true)
-
-#define MPSL_FEM_GPIO_PORT(pin)     DT_GPIO_CTLR(DT_NODELABEL(nrf_radio_fem), pin)
-#define MPSL_FEM_GPIO_PORT_REG(pin) ((NRF_GPIO_Type *)DT_REG_ADDR(MPSL_FEM_GPIO_PORT(pin)))
-#define MPSL_FEM_GPIO_PORT_NO(pin)  DT_PROP(MPSL_FEM_GPIO_PORT(pin), port)
-#define MPSL_FEM_GPIO_PIN_NO(pin)   DT_GPIO_PIN(DT_NODELABEL(nrf_radio_fem), pin)
-
-#define MPSL_FEM_GPIO_INVALID_PIN        0xFFU
-#define MPSL_FEM_GPIOTE_INVALID_CHANNEL  0xFFU
-#define MPSL_FEM_DISABLED_GPIOTE_PIN_CONFIG_INIT	\
-	.gpio_pin      = {				\
-		.port_pin = MPSL_FEM_GPIO_INVALID_PIN,	\
-	},						\
-	.enable        = false,				\
-	.active_high   = true,				\
-	.gpiote_ch_id  = MPSL_FEM_GPIOTE_INVALID_CHANNEL
-
-#define MPSL_FEM_DISABLED_GPIO_CONFIG_INIT		\
-	.gpio_pin      = {				\
-		.port_pin = MPSL_FEM_GPIO_INVALID_PIN,	\
-	},						\
-	.enable        = false,				\
-	.active_high   = true,				\
-
-static int ppi_channel_alloc(uint8_t *ppi_channels, size_t size)
-{
-	nrfx_err_t err = NRFX_ERROR_NOT_SUPPORTED;
-
-	for (int i = 0; i < size; i++) {
-		IF_ENABLED(CONFIG_HAS_HW_NRF_PPI,
-			(err = nrfx_ppi_channel_alloc(&ppi_channels[i]);));
-		IF_ENABLED(CONFIG_HAS_HW_NRF_DPPIC,
-			(err = nrfx_dppi_channel_alloc(&ppi_channels[i]);));
-		if (err != NRFX_SUCCESS) {
-			return -ENOMEM;
-		}
-	}
-
-	return 0;
-}
-
-#if defined(CONFIG_HAS_HW_NRF_DPPIC)
-static int egu_instance_alloc(uint8_t *p_egu_instance_no)
-{
-	/* Always return EGU0. */
-	*p_egu_instance_no = 0;
-
-	return 0;
-}
-
-static int egu_channel_alloc(uint8_t *egu_channels, size_t size, uint8_t egu_instance_no)
-{
-	(void)egu_instance_no;
-
-	/* The 802.15.4 radio driver is the only user of EGU peripheral on nRF5340 network core
-	 * and it uses channels: 0, 1, 2, 3, 4, 15. Therefore starting from channel 5, a consecutive
-	 * block of at most 10 channels can be allocated.
-	 */
-	if (size > 10U) {
-		return -ENOMEM;
-	}
-
-	uint8_t starting_channel = 5U;
-
-	for (int i = 0; i < size; i++) {
-		egu_channels[i] = starting_channel + i;
-	}
-
-	return 0;
-}
-#endif
-
-#if IS_ENABLED(CONFIG_MPSL_FEM_NRF21540_GPIO)
 static int fem_nrf21540_gpio_configure(void)
 {
 	int err;
@@ -218,23 +134,25 @@ static int fem_nrf21540_gpio_configure(void)
 	};
 
 	IF_ENABLED(CONFIG_HAS_HW_NRF_PPI,
-		   (err = ppi_channel_alloc(cfg.ppi_channels, ARRAY_SIZE(cfg.ppi_channels));));
+		   (err = mpsl_fem_utils_ppi_channel_alloc(cfg.ppi_channels,
+							   ARRAY_SIZE(cfg.ppi_channels));));
 	IF_ENABLED(CONFIG_HAS_HW_NRF_DPPIC,
-		   (err = ppi_channel_alloc(cfg.dppi_channels, ARRAY_SIZE(cfg.dppi_channels));));
+		   (err = mpsl_fem_utils_ppi_channel_alloc(cfg.dppi_channels,
+							   ARRAY_SIZE(cfg.dppi_channels));));
 	if (err) {
 		return err;
 	}
 
 	IF_ENABLED(CONFIG_HAS_HW_NRF_DPPIC,
-		   (err = egu_instance_alloc(&cfg.egu_instance_no);));
+		   (err = mpsl_fem_utils_egu_instance_alloc(&cfg.egu_instance_no);));
 	if (err) {
 		return err;
 	}
 
 	IF_ENABLED(CONFIG_HAS_HW_NRF_DPPIC,
-		   (err = egu_channel_alloc(cfg.egu_channels,
-					    ARRAY_SIZE(cfg.egu_channels),
-					    cfg.egu_instance_no);))
+		   (err = mpsl_fem_utils_egu_channel_alloc(cfg.egu_channels,
+							   ARRAY_SIZE(cfg.egu_channels),
+							   cfg.egu_instance_no);))
 	if (err) {
 		return err;
 	}
@@ -279,7 +197,6 @@ static int fem_nrf21540_gpio_configure(void)
 
 	return mpsl_fem_nrf21540_gpio_interface_config_set(&cfg);
 }
-#endif
 
 static int mpsl_fem_init(const struct device *dev)
 {
@@ -346,5 +263,3 @@ static int mpsl_fem_host_init(const struct device *dev)
 SYS_INIT(mpsl_fem_host_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
 
 #endif /* !defined(CONFIG_MPSL_FEM_PIN_FORWARDER) */
-
-#endif /* defined(CONFIG_MPSL_FEM_NRF21540_GPIO) */
