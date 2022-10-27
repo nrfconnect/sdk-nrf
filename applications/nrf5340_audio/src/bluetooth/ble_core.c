9dc4c094c384aa490442aa23ef79ae770762f396
Kristoffer Rist Skøien <kristoffer.skoien@nordicsemi.no>
2022-10-27 15:15:01 +0200

    applications: nrf5340_audio: Removed pin fwd in led driver
    Pin forwarding is now performed in the devicetree
    
    Signed-off-by: Kristoffer Rist Skøien <kristoffer.skoien@nordicsemi.no>

--- applications/nrf5340_audio/src/bluetooth/ble_core.c
+++ applications/nrf5340_audio/src/bluetooth/ble_core.c
@@ -3,15 +3,17 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
-
 #include "ble_core.h"
-#include "ble_hci_vsc.h"
+
 #include <zephyr/kernel.h>
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/hci.h>
 #include <zephyr/sys/byteorder.h>
+#include <zephyr/drivers/gpio.h>
 #include <errno.h>
+
 #include "macros_common.h"
+#include "ble_hci_vsc.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(ble, CONFIG_LOG_BLE_LEVEL);
@@ -116,15 +118,15 @@ static int controller_leds_mapping(void)
 	int ret;
 
 	ret = ble_hci_vsc_map_led_pin(PAL_LED_ID_CPU_ACTIVE,
-				      DT_GPIO_FLAGS_BY_IDX(DT_NODELABEL(rgb2_green), gpios, 0),
-				      DT_GPIO_PIN_BY_IDX(DT_NODELABEL(rgb2_green), gpios, 0));
+			DT_GPIO_FLAGS_BY_IDX(DT_NODELABEL(rgb2_green), gpios, 0),
+			DT_GPIO_PIN_BY_IDX(DT_NODELABEL(rgb2_green), gpios, 0));
 	if (ret) {
 		return ret;
 	}
 
 	ret = ble_hci_vsc_map_led_pin(PAL_LED_ID_ERROR,
-				      DT_GPIO_FLAGS_BY_IDX(DT_NODELABEL(rgb2_red), gpios, 0),
-				      DT_GPIO_PIN_BY_IDX(DT_NODELABEL(rgb2_red), gpios, 0));
+			DT_GPIO_FLAGS_BY_IDX(DT_NODELABEL(rgb2_red), gpios, 0),
+			DT_GPIO_PIN_BY_IDX(DT_NODELABEL(rgb2_red), gpios, 0));
 	if (ret) {
 		return ret;
 	}
