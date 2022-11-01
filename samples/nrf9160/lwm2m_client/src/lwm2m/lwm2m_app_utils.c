caaa4c8e7c4de5ea1305aa479ebbba48ce27339d
Veijo Pesonen <veijo.pesonen@nordicsemi.no>
2022-11-01 09:38:15 +0200

    samples: lwm2m_client: check fxn return values
    Log errors when necessary.
    
    Signed-off-by: Veijo Pesonen <veijo.pesonen@nordicsemi.no>

--- samples/nrf9160/lwm2m_client/src/lwm2m/lwm2m_app_utils.c
+++ samples/nrf9160/lwm2m_client/src/lwm2m/lwm2m_app_utils.c
@@ -5,17 +5,29 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 #include <zephyr/net/lwm2m.h>
 #include <lwm2m_resource_ids.h>
 
 #include "lwm2m_app_utils.h"
+
+LOG_MODULE_REGISTER(app_lwm2m_utils, CONFIG_APP_LOG_LEVEL);
 
 void set_ipso_obj_timestamp(int ipso_obj_id, unsigned int obj_inst_id)
 {
 	int32_t timestamp;
 	char path[MAX_LWM2M_PATH_LEN];
 
-	lwm2m_engine_get_s32(LWM2M_PATH(LWM2M_OBJECT_DEVICE_ID, 0, CURRENT_TIME_RID), &timestamp);
+	int ret = lwm2m_engine_get_s32(LWM2M_PATH(LWM2M_OBJECT_DEVICE_ID, 0,
+					CURRENT_TIME_RID), &timestamp);
+	if (ret) {
+		LOG_ERR("Unable to retrieve timestamp");
+	}
+
 	snprintk(path, MAX_LWM2M_PATH_LEN, "%d/%u/%d", ipso_obj_id, obj_inst_id, TIMESTAMP_RID);
-	lwm2m_engine_set_s32(path, timestamp);
+
+	ret = lwm2m_engine_set_s32(path, timestamp);
+	if (ret) {
+		LOG_ERR("Unable to set timestamp");
+	}
 }
