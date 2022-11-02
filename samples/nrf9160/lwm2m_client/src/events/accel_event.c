68de00359b31951dc2b21041dc9fef21e374be47
Juha Heiskanen <juha.heiskanen@nordicsemi.no>
2022-11-02 13:10:24 +0200

    samples: lwm2m_client: Accelerometer orientation update
    Removed unused and uninitialized orientation data from event
    structure.
    
    Signed-off-by: Juha Heiskanen <juha.heiskanen@nordicsemi.no>

--- samples/nrf9160/lwm2m_client/src/events/accel_event.c
+++ samples/nrf9160/lwm2m_client/src/events/accel_event.c
@@ -6,35 +6,14 @@
 
 #include "accel_event.h"
 
-static const char *accel_orienation_state_to_string(enum accel_orientation_state state)
-{
-	switch (state) {
-	case ORIENTATION_NOT_KNOWN:
-		return "not known";
-
-	case ORIENTATION_NORMAL:
-		return "normal";
-
-	case ORIENTATION_UPSIDE_DOWN:
-		return "upside down";
-
-	case ORIENTATION_ON_SIDE:
-		return "on side";
-
-	default:
-		return "";
-	}
-}
-
 static void log_accel_event(const struct app_event_header *aeh)
 {
 	struct accel_event *event = cast_accel_event(aeh);
 
 	APP_EVENT_MANAGER_LOG(aeh,
-		"Accelerometer event: x = %d.%06d, y = %d.%06d, z = %d.%06d, orientation = %s.",
+		"Accelerometer event: x = %d.%06d, y = %d.%06d, z = %d.%06d.",
 		event->data.x.val1, event->data.x.val2, event->data.y.val1, event->data.y.val2,
-		event->data.z.val1, event->data.z.val2,
-		accel_orienation_state_to_string(event->orientation));
+		event->data.z.val1, event->data.z.val2);
 }
 
 APP_EVENT_TYPE_DEFINE(accel_event, log_accel_event, NULL, APP_EVENT_FLAGS_CREATE());
