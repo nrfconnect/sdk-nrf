ccf58e79311caf397c51b0777911bec79d1f81d9
Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>
2022-11-09 15:08:11 +0530

    drivers: wifi: Read additional interface statistics data
    [CAL-3022] Extend get_stats command support to read
     unicast/broadcast/multicast packets and data count.
    
    Signed-off-by: Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>

--- drivers/wifi/nrf700x/zephyr/inc/zephyr_net_if.h
+++ drivers/wifi/nrf700x/zephyr/inc/zephyr_net_if.h
@@ -16,6 +16,7 @@
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/ethernet.h>
 #include <fmac_structs.h>
+#include <zephyr/net/wifi_mgmt.h>
 
 void wifi_nrf_if_init(struct net_if *iface);
 enum ethernet_hw_caps wifi_nrf_if_caps_get(const struct device *dev);
@@ -24,5 +25,7 @@ int wifi_nrf_if_send(const struct device *dev, struct net_pkt *pkt);
 void wifi_nrf_if_rx_frm(void *os_vif_ctx, void *frm);
 
 enum wifi_nrf_status wifi_nrf_if_state_chg(void *os_vif_ctx, enum wifi_nrf_fmac_if_state if_state);
+
+int wifi_nrf_stats_get(const struct device *dev, struct net_stats_wifi *stats);
 
 #endif /* __ZEPHYR_NET_IF_H__ */
