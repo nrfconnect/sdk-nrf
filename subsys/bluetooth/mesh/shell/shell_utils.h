674a176fe914b24303c7596accc4f92b5f713033
Anders Storrø <anders.storro@nordicsemi.no>
2022-11-16 14:56:37 +0100

    Bluetooth: Mesh: Shell util for vnd models
    Refactors the mesh shell util library to also provide support
    for vendor models.
    
    Signed-off-by: Anders Storrø <anders.storro@nordicsemi.no>

--- subsys/bluetooth/mesh/shell/shell_utils.h
+++ subsys/bluetooth/mesh/shell/shell_utils.h
@@ -15,9 +15,16 @@ double shell_model_strtodbl(const char *str, int *err);
 
 bool shell_model_first_get(uint16_t id, struct bt_mesh_model **mod);
 
+bool shell_vnd_model_first_get(uint16_t cid, uint16_t id, struct bt_mesh_model **mod);
+
 int shell_model_instance_set(const struct shell *shell, struct bt_mesh_model **mod,
 			      uint16_t mod_id, uint8_t elem_idx);
 
+int shell_vnd_model_instance_set(const struct shell *shell, struct bt_mesh_model **mod,
+				 uint16_t mod_id, uint16_t cid, uint8_t elem_idx);
+
 int shell_model_instances_get_all(const struct shell *shell, uint16_t mod_id);
+
+int shell_vnd_model_instances_get_all(const struct shell *shell, uint16_t mod_id, uint16_t cid);
 
 int shell_model_cmds_help(const struct shell *shell, size_t argc, char **argv);
