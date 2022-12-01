407461911a2f7ddb41efc5cb3208bc818e3d7adc
gWacey <graham.wacey@nordicsemi.no>
2022-12-01 14:50:30 +0000

    applications: nrf5340_audio: Move UART scripts into NCS
    	Display port with associated ID for the DK
    
    Signed-off-by: Graham Wacey <graham.wacey@nordicsemi.no>

--- scripts/uart_terminal/scripts/get_serial_ports.py
+++ scripts/uart_terminal/scripts/get_serial_ports.py
@@ -7,8 +7,10 @@
 import sys
 import subprocess
 
+
 def get_serial_ports():
-    nrfjprog_com = subprocess.Popen(["nrfjprog", "--com"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    nrfjprog_com = subprocess.Popen(
+        ["nrfjprog", "--com"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     nrfjprog_com.wait()
 
     if nrfjprog_com.returncode != 0:
@@ -20,9 +22,12 @@ def get_serial_ports():
 
     ports = list()
 
-    for line in output_decoded_lines:
-        if "VCOM0" in line:
-            info = line.split("    ")
-            ports.append(info[1])
+    if len(output_decoded_lines):
+        print("%6s %11s" % ("ID", "Port"))
+        for line in output_decoded_lines:
+            if "VCOM0" in line:
+                info = line.split("    ")
+                ports.append(info[1])
+                print("%10s %8s" % (info[0], info[1]))
 
     return ports
