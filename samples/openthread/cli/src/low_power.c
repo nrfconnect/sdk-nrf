35b46ffd41c97629e3a3adcb8c1ffa08b698b4ce
Piotr Jasiński <piotr.jasinski@nordicsemi.no>
2023-01-11 14:00:26 +0100

    manifest: adapt samples to new ot cb register api
    Use new OpenThread callback register api
    (https://github.com/nrfconnect/sdk-zephyr/pull/1030).
    
    Signed-off-by: Piotr Jasiński <piotr.jasinski@nordicsemi.no>

--- samples/openthread/cli/src/low_power.c
+++ samples/openthread/cli/src/low_power.c
@@ -12,10 +12,9 @@
 
 #include "low_power.h"
 
-static void on_thread_state_changed(uint32_t flags, void *context)
+static void on_thread_state_changed(otChangedFlags flags, struct openthread_context *ot_context,
+				    void *user_data)
 {
-	struct openthread_context *ot_context = context;
-
 	if (flags & OT_CHANGED_THREAD_ROLE) {
 		if (otThreadGetDeviceRole(ot_context->instance) == OT_DEVICE_ROLE_CHILD) {
 			const struct device *cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
@@ -30,7 +29,11 @@ static void on_thread_state_changed(uint32_t flags, void *context)
 	}
 }
 
+static struct openthread_state_changed_cb ot_state_chaged_cb = {
+	.state_changed_cb = on_thread_state_changed
+};
+
 void low_power_enable(void)
 {
-	openthread_set_state_changed_cb(on_thread_state_changed);
+	openthread_state_changed_cb_register(openthread_get_default_context(), &ot_state_chaged_cb);
 }
