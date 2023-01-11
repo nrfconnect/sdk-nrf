c93aa8189a8751e2e0b4a91ff088642bc08b6102
Sachin D Kulkarni <sachin.kulkarni@nordicsemi.no>
2023-01-11 22:02:33 +0530

    drivers: wifi: Refactor TX, RX and XO commands for radio test
    In order to have separate configure and trigger commands from the
    application, refactor the commands to RPU as well.
    
    Signed-off-by: Mahammadyunus Patil <Mahammadyunus.Patil@nordicsemi.no>
    Signed-off-by: Sachin D Kulkarni <sachin.kulkarni@nordicsemi.no>

--- drivers/wifi/nrf700x/osal/hw_if/hal/inc/fw_B/phy_rf_params.h
+++ drivers/wifi/nrf700x/osal/hw_if/hal/inc/fw_B/phy_rf_params.h
@@ -163,20 +163,15 @@ struct nrf_wifi_rf_test_capture_meas {
 struct nrf_wifi_rf_test_tx_params {
 	unsigned char test;
 
-	/* Compute the normalized frequency for the tone to be transmitted
-	 * as,normFrequency =
-	 * round(toneFrequency * ((1/(DAC sampling rate /2))*(2^25)))
+	/* Desired tone frequency in MHz in steps of 1MHz from -10MHz to +10MHz.*/
+	signed char tone_freq;
+
+	/* Desired TX power in the range -16dBm to +24dBm.
+	 * in steps of 2dBm
 	 */
-	signed int norm_freq;
+	signed char tx_pow;
 
-	/* Number of samples to be transmitted. */
-	unsigned short tx_vect_len;
-
-	/* Desired amplitude of the tone (between 0 - 0x3FF). */
-	unsigned short tone_amp;
-
-	/* Desired TX power*/
-	unsigned char tx_pow;
+	/* Set 1 for staring tone transmission.*/
 	unsigned char enabled;
 } __NRF_WIFI_PKD;
 
@@ -211,7 +206,7 @@ struct nrf_wifi_rf_get_rf_rssi {
 struct nrf_wifi_rf_test_xo_calib {
 	unsigned char test;
 
-	/* Number of samples to be captured. */
+	/* XO value in the range between 0 to 127 */
 	unsigned char xo_val;
 
 } __NRF_WIFI_PKD;
@@ -220,11 +215,8 @@ struct nrf_wifi_rf_test_xo_calib {
 struct nrf_wifi_rf_get_xo_value {
 	unsigned char test;
 
-	/* Number of samples to be captured. */
+	/* Optimal XO value computed. */
 	unsigned char xo_value;
-
-	unsigned int tone_frequency;
-
 } __NRF_WIFI_PKD;
 
 #endif /* CONFIG_NRF700X_RADIO_TEST */
