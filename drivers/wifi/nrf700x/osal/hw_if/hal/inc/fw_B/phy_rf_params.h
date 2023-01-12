3032f99110aa432c1a120430b1857a1e90734352
Mahammadyunus Patil <Mahammadyunus.Patil@nordicsemi.no>
2023-01-12 15:47:12 +0530

    drivers: wifi: Enable temperature-based calibration
    This macro enables the calibrations dependent on temperature variation
    
    Signed-off-by: Mahammadyunus Patil <Mahammadyunus.Patil@nordicsemi.no>

--- drivers/wifi/nrf700x/osal/hw_if/hal/inc/fw_B/phy_rf_params.h
+++ drivers/wifi/nrf700x/osal/hw_if/hal/inc/fw_B/phy_rf_params.h
@@ -10,29 +10,10 @@
 
 #ifndef _PHY_RF_PARAMS_H_
 #define _PHY_RF_PARAMS_H_
-#include "pack_def.h"
 
 #define NRF_WIFI_RF_PARAMS_SIZE 200
 #define NRF_WIFI_RF_PARAMS_CONF_SIZE 42
 #define NRF_WIFI_DEF_RF_PARAMS "0000000000002C00000000000000004C3C443C3C3C4444440000000050EC000000000000000000000000002022003F032424001000002800323500000CF0080A7D8105010071630300EED501001F6F00003B350100F52E0000E35E0000B7B6000066EFFEFFB5F60000896200007A840200E28FFCFF08080808040A140100000000A1A10178000000080050003B020726181818181A120A140E0600"
-
-#define NRF_WIFI_RF_PARAMS_OFF_RESV_1 0
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_X0 6
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_PDADJM7 7
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_PDADJM0 11
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_PWR2G 15
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_PWR2GM0M7 16
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_PWR5GM7 18
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_PWR5GM0 21
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_RXGNOFF 24
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_MAX_TEMP 28
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_MIN_TEMP 29
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_TXP_BOFF_2GH 30
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_TXP_BOFF_2GL 31
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_TXP_BOFF_5GH 32
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_TXP_BOFF_5GL 33
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_TXP_BOFF_V 34
-#define NRF_WIFI_RF_PARAMS_OFF_CALIB_RESV_2 37
 
 
 #define NRF_WIFI_PHY_CALIB_FLAG_RXDC 1
@@ -73,7 +54,7 @@
 
 #define NRF_WIFI_TEMP_CALIB_PERIOD (1024 * 1024) /* micro seconds */
 #define NRF_WIFI_TEMP_CALIB_THRESHOLD (40)
-#define NRF_WIFI_TEMP_CALIB_ENABLE 0
+#define NRF_WIFI_TEMP_CALIB_ENABLE 1
 
 /* Battery voltage changes base calibrations and voltage thresholds */
 #define NRF_WIFI_DEF_PHY_VBAT_CALIB (NRF_WIFI_PHY_CALIB_FLAG_DPD)
@@ -81,147 +62,5 @@
 #define NRF_WIFI_VBAT_LOW  (6)  /* Correspond to (2.5+6*0.07)=2.92V */
 #define NRF_WIFI_VBAT_HIGH (12) /* Correspond to (2.5+12*0.07)=3.34V */
 
-
-
-#ifdef CONFIG_NRF700X_RADIO_TEST
-enum nrf_wifi_rf_test {
-	NRF_WIFI_RF_TEST_RX_ADC_CAP,
-	NRF_WIFI_RF_TEST_RX_STAT_PKT_CAP,
-	NRF_WIFI_RF_TEST_RX_DYN_PKT_CAP,
-	NRF_WIFI_RF_TEST_TX_TONE,
-	NRF_WIFI_RF_TEST_DPD,
-	NRF_WIFI_RF_TEST_RF_RSSI,
-	NRF_WIFI_RF_TEST_SLEEP,
-	NRF_WIFI_RF_TEST_GET_TEMPERATURE,
-	NRF_WIFI_RF_TEST_XO_CALIB,
-	NRF_WIFI_RF_TEST_XO_TUNE,
-	NRF_WIFI_RF_TEST_MAX,
-};
-
-enum nrf_wifi_rf_test_event {
-	NRF_WIFI_RF_TEST_EVENT_RX_ADC_CAP,
-	NRF_WIFI_RF_TEST_EVENT_RX_STAT_PKT_CAP,
-	NRF_WIFI_RF_TEST_EVENT_RX_DYN_PKT_CAP,
-	NRF_WIFI_RF_TEST_EVENT_TX_TONE_START,
-	NRF_WIFI_RF_TEST_EVENT_DPD_ENABLE,
-	NRF_WIFI_RF_TEST_EVENT_RF_RSSI,
-	NRF_WIFI_RF_TEST_EVENT_SLEEP,
-	NRF_WIFI_RF_TEST_EVENT_TEMP_MEAS,
-	NRF_WIFI_RF_TEST_EVENT_XO_CALIB,
-	NRF_WIFI_RF_TEST_EVENT_MAX,
-};
-
-
-
-/* Holds the RX capture related info */
-struct nrf_wifi_rf_test_capture_params {
-	unsigned char test;
-
-	/* Number of samples to be captured. */
-	unsigned short int cap_len;
-
-	/* LNA Gain to be configured. It is a 3 bit value. The mapping is,
-	 * '0' = 24dB
-	 * '1' = 18dB
-	 * '2' = 12dB
-	 * '3' = 0dB
-	 * '4' = -12dB
-	 */
-	unsigned char lna_gain;
-
-	/* Baseband Gain to be configured. It is a 5 bit value.
-	 * It supports 64dB range.The increment happens lineraly 2dB/step
-	 */
-	unsigned char bb_gain;
-} __NRF_WIFI_PKD;
-
-
-/* Struct to hold the events from RF test SW. */
-struct nrf_wifi_rf_test_capture_meas {
-	unsigned char test;
-
-	/*  Mean of I samples. Format: Q.11 */
-	signed short mean_I;
-
-	/* Mean of Q samples. Format: Q.11 */
-	signed short mean_Q;
-
-	/* RMS of I samples */
-	unsigned int rms_I;
-
-	/* RMS of Q samples */
-	unsigned int rms_Q;
-} __NRF_WIFI_PKD;
-
-
-/* Holds the transmit related info */
-struct nrf_wifi_rf_test_tx_params {
-	unsigned char test;
-
-	/* Compute the normalized frequency for the tone to be transmitted
-	 * as,normFrequency =
-	 * round(toneFrequency * ((1/(DAC sampling rate /2))*(2^25)))
-	 */
-	signed int norm_freq;
-
-	/* Number of samples to be transmitted. */
-	unsigned short tx_vect_len;
-
-	/* Desired amplitude of the tone (between 0 – 0x3FF). */
-	unsigned short tone_amp;
-
-	/* Desired TX power*/
-	unsigned char tx_pow;
-	unsigned char enabled;
-} __NRF_WIFI_PKD;
-
-struct nrf_wifi_rf_test_dpd_params {
-	unsigned char test;
-	unsigned char enabled;
-
-} __NRF_WIFI_PKD;
-
-struct nrf_wifi_temperature_params {
-	unsigned char test;
-
-	/*! current measured temperature */
-	signed int temperature;
-
-	/*! Temperature measurment status.
-	 *0: Reading successful
-	 *1: Reading failed
-	 */
-	unsigned int readTemperatureStatus;
-} __NRF_WIFI_PKD;
-
-
-struct nrf_wifi_rf_get_rf_rssi {
-	unsigned char test;
-	unsigned char lna_gain;
-	unsigned char bb_gain;
-	unsigned char agc_status_val;
-} __NRF_WIFI_PKD;
-
-
-struct nrf_wifi_rf_test_xo_calib {
-	unsigned char test;
-
-	/* Number of samples to be captured. */
-	unsigned char xo_val;
-
-} __NRF_WIFI_PKD;
-
-
-struct nrf_wifi_rf_get_xo_value {
-	unsigned char test;
-
-	/* Number of samples to be captured. */
-	unsigned char xo_value;
-
-	unsigned int tone_frequency;
-
-} __NRF_WIFI_PKD;
-
-#endif /* CONFIG_NRF700X_RADIO_TEST */
 
 #endif
