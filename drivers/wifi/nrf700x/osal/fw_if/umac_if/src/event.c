d0a5ae73d15bba90b95503efec2ec4949b0b15b6
Krishna T <krishna.t@nordicsemi.no>
2023-01-12 14:44:14 +0530

    drivers: wifi: Implement regulatory functionality
    Implement the Wi-Fi offload regulatory callback to get and set
    regulatory information, for now only alpha2/country code is supported.
    
    Signed-off-by: Krishna T <krishna.t@nordicsemi.no>

--- drivers/wifi/nrf700x/osal/fw_if/umac_if/src/event.c
+++ drivers/wifi/nrf700x/osal/fw_if/umac_if/src/event.c
@@ -428,6 +428,17 @@ static enum wifi_nrf_status umac_event_ctrl_process(struct wifi_nrf_fmac_dev_ctx
 	case NRF_WIFI_UMAC_EVENT_DISCONNECT:
 		/* Nothing to be done */
 		break;
+	case NRF_WIFI_UMAC_EVENT_GET_REG:
+		if (callbk_fns->event_get_reg)
+			callbk_fns->event_get_reg(vif_ctx->os_vif_ctx,
+						      event_data,
+						      event_len);
+		else
+			wifi_nrf_osal_log_err(fmac_dev_ctx->fpriv->opriv,
+					      "%s: No callback registered for event %d\n",
+					      __func__,
+					      umac_hdr->cmd_evnt);
+		break;
 #ifdef CONFIG_WPA_SUPP
 	case NRF_WIFI_UMAC_EVENT_NEW_STATION:
 	case NRF_WIFI_UMAC_EVENT_DEL_STATION:
