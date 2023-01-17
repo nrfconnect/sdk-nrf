bc0e963bd4c4c0df7c5329a201c2de95c7b5eb76
Jui-Chou Chung <jui-chou.chung@nordicsemi.no>
2023-01-17 14:03:03 +0800

    applications: nrf5340_audio: Fix bidirectional headset QoS set failed
    Fix the out of index for source stream which cause the headset
    cannot set QoS properly after disconnected from gateway multiple times.
    
    Signed-off-by: Jui-Chou Chung <jui-chou.chung@nordicsemi.no>

--- applications/nrf5340_audio/src/bluetooth/le_audio_cis_headset.c
+++ applications/nrf5340_audio/src/bluetooth/le_audio_cis_headset.c
@@ -130,6 +130,8 @@ static struct bt_audio_stream
 	audio_streams[CONFIG_BT_ASCS_ASE_SNK_COUNT + CONFIG_BT_ASCS_ASE_SRC_COUNT];
 
 #if CONFIG_STREAM_BIDIRECTIONAL
+BUILD_ASSERT(CONFIG_BT_ASCS_ASE_SRC_COUNT <= 1,
+	     "CIS headset only supports one source stream for now");
 static struct bt_audio_source {
 	struct bt_audio_stream *stream;
 	uint32_t seq_num;
@@ -259,10 +261,6 @@ static int lc3_config_cb(struct bt_conn *conn, const struct bt_audio_ep *ep, enu
 {
 	int ret;
 
-#if CONFIG_STREAM_BIDIRECTIONAL
-	static size_t configured_source_stream_count;
-#endif /* CONFIG_STREAM_BIDIRECTIONAL */
-
 	for (int i = 0; i < ARRAY_SIZE(audio_streams); i++) {
 		struct bt_audio_stream *audio_stream = &audio_streams[i];
 
@@ -290,7 +288,9 @@ static int lc3_config_cb(struct bt_conn *conn, const struct bt_audio_ep *ep, enu
 			else if (dir == BT_AUDIO_DIR_SOURCE) {
 				LOG_DBG("BT_AUDIO_DIR_SOURCE");
 				print_codec(codec);
-				sources[configured_source_stream_count++].stream = audio_stream;
+
+				/* CIS headset only supports one source stream for now */
+				sources[0].stream = audio_stream;
 			}
 #endif /* CONFIG_STREAM_BIDIRECTIONAL */
 			else {
@@ -677,6 +677,7 @@ int le_audio_send(uint8_t const *const data, size_t size)
 	int ret;
 	struct net_buf *buf;
 
+	/* CIS headset only supports one source stream for now */
 	if (sources[0].stream->ep->status.state != BT_AUDIO_EP_STATE_STREAMING) {
 		LOG_DBG("Return channel not connected");
 		return 0;
