780fb5165fea1a0701ca4fad19ff6ae425a80c1f
Juha Heiskanen <juha.heiskanen@nordicsemi.no>
2023-02-02 13:45:24 +0200

    applications: asset_tracker_v2: LwM2M Fota fix
    Removed duplicate CLOUD_WRAP_EVT_FOTA_DONE. Update state was
    generated first and reboot a second. Proper solution is remove
    from update trigger.
    
    Fixed Unit test.
    
    Signed-off-by: Juha Heiskanen <juha.heiskanen@nordicsemi.no>

--- applications/asset_tracker_v2/src/cloud/lwm2m_integration/lwm2m_integration.c
+++ applications/asset_tracker_v2/src/cloud/lwm2m_integration/lwm2m_integration.c
@@ -289,10 +289,9 @@ static int firmware_update_state_cb(uint8_t update_state)
 		return 0;
 	case STATE_UPDATING:
 		LOG_DBG("STATE_UPDATING, result: %d", update_result);
-		cloud_wrap_evt.type = CLOUD_WRAP_EVT_FOTA_DONE;
 		/* Disable further callbacks from FOTA */
 		lwm2m_firmware_set_update_state_cb(NULL);
-		break;
+		return 0;
 	default:
 		LOG_ERR("Unknown state: %d", update_state);
 		cloud_wrap_evt.type = CLOUD_WRAP_EVT_FOTA_ERROR;
