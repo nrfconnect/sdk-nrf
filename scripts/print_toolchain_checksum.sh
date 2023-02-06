afd8a71318fd5b7b27d0f5854645a686349377fd
Kari Hamalainen <kari.hamalainen@nordicsemi.no>
2023-02-06 13:36:55 +0200

    ci: add script for printing docker image name
    Adding helper script for printing docker image name used in CI.
    
    Signed-off-by: Kari Hamalainen <kari.hamalainen@nordicsemi.no>

---
+++ scripts/print_toolchain_checksum.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+REQUIREMENTS=$BASEDIR/requirements-fixed.txt
+TOOLS_VERSIONS=$BASEDIR/tools-versions-linux.txt
+TOOLCHAIN_VERSION=$(cat $REQUIREMENTS $TOOLS_VERSIONS | sha256sum | head -c 10)
+
+echo "${TOOLCHAIN_VERSION}"
