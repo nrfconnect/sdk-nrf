e3e7fc85b9d7662c0a9ce26f62a08c506103eca1
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-02-14 14:24:02 +0100

    tfm: Add platform OTP ids header file for minimal profile
    Add the platform_otp_ids.h header file.
    This is required to be implemented by the platform when not using
    the PLATFORM_DEFAULT_OTP option.
    The TF-M minimal profile sets this option, in this configuration
    we don't have any OTP values, so the header file is empty.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

---
+++ include/tfm/platform_otp_ids.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef __PLATFORM_OTP_IDS_H__
+#define __PLATFORM_OTP_IDS_H__
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum tfm_otp_element_id_t {
+	PLAT_OTP_ID_MAX = UINT32_MAX,
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __PLATFORM_OTP_IDS_H__ */
