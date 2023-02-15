4a8df39d02191702cae9d4ecf83979bb811446c7
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-02-15 11:52:37 +0100

    samples: hw_unique_key: Update HW unique key derivation sample
    Update HW unique key derivation sample to TF-M 1.7.0.
    The TF-M HUK key derivation algorithm is removed in instead
    uses a standard algorithm with the HUK as the key derivation key.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- samples/keys/hw_unique_key/src/derive_key_tfm.c
+++ samples/keys/hw_unique_key/src/derive_key_tfm.c
@@ -3,8 +3,9 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  *
- * Adapted from ps_crypto_setkey() in ps_crypto_interface.c from the
- * Trusted Firmware-M project.
+ * Based on "Protected Storage service key management" documentation in "Key derivation" chapter:
+ * docs/technical_references/design_docs/ps_key_management.rst
+ * in Trusted Firmware-M project.
  */
 
 #include <zephyr/kernel.h>
@@ -14,33 +15,42 @@
 
 
 psa_key_id_t derive_key(psa_key_attributes_t *attributes, uint8_t *key_label,
-			uint32_t label_size)
+			uint32_t key_label_len)
 {
 	psa_status_t status;
 	psa_key_derivation_operation_t op = PSA_KEY_DERIVATION_OPERATION_INIT;
 	psa_key_id_t key_id_out = 0;
 
-	/* Set up a key derivation operation with HUK derivation as the alg */
-	status = psa_key_derivation_setup(&op, TFM_CRYPTO_ALG_HUK_DERIVATION);
+	status = psa_key_derivation_setup(&op, PSA_ALG_HKDF(PSA_ALG_SHA_256));
 	if (status != PSA_SUCCESS) {
 		printk("psa_key_derivation_setup returned error: %d\n", status);
 		return 0;
 	}
 
-	/* Supply the key label as an input to the key derivation */
-	status = psa_key_derivation_input_bytes(&op, PSA_KEY_DERIVATION_INPUT_LABEL,
-						key_label, label_size);
+	/* Set up a key derivation operation with HUK  */
+	status = psa_key_derivation_input_key(&op, PSA_KEY_DERIVATION_INPUT_SECRET,
+					      TFM_BUILTIN_KEY_ID_HUK);
+	if (status != PSA_SUCCESS) {
+		printk("psa_key_derivation_input_key returned error: %d\n", status);
+		return 0;
+	}
+
+	/* Supply the PS key label as an input to the key derivation */
+	status = psa_key_derivation_input_bytes(&op, PSA_KEY_DERIVATION_INPUT_INFO,
+						key_label,
+						key_label_len);
 	if (status != PSA_SUCCESS) {
 		printk("psa_key_derivation_input_bytes returned error: %d\n", status);
 		return 0;
 	}
 
-	/* Create the encryption key from the key derivation operation */
+	/* Create the storage key from the key derivation operation */
 	status = psa_key_derivation_output_key(attributes, &op, &key_id_out);
 	if (status != PSA_SUCCESS) {
 		printk("psa_key_derivation_output_key returned error: %d\n", status);
 		return 0;
 	}
+
 	printk("(Key resides internally in TF-M)\n");
 
 	/* Free resources associated with the key derivation operation */
