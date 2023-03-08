6168aaa7542377c90c75c02b9079ffe13c2e185d
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-03-08 13:25:59 +0100

    tfm: Add override header for zephyr/kernel.h
    Add override header for zephyr kernel header that in TF-M exposes the
    k_panic function to the TF-M panic function.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

---
+++ modules/tfm/tfm/boards/include/zephyr/kernel.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef __ZEPHYR_KERNEL_H
+#define __ZEPHYR_KERNEL_H
+
+/* Compatebility header for using Zephyr API in TF-M.
+ *
+ * The macros and functions here can be used by code that is common for both
+ * Zephyr and TF-M RTOS.
+ *
+ * The functionality will be forwarded to TF-M equivalent of the Zephyr API.
+ */
+
+#define k_panic() tfm_core_panic()
+
+#endif /* __ZEPHYR_KERNEL_H */
