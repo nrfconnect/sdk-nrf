df9b0b17a248fd53392525732a7b8e0fe6609785
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-03-08 14:05:21 +0100

    tfm: Add override header for zephyr/sys/check.h
    Add override header for zephyr check header.
    This is useful for providing a check that can either be used
    to check and return error on an expression, or simply assert and
    save code-space on excluding the error handling.
    In TF-M we don't allow these checks to be excluded from the build.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

---
+++ modules/tfm/tfm/boards/include/zephyr/sys/check.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef __ZEPHYR_SYS_CHECK_H
+#define __ZEPHYR_SYS_CHECK_H
+
+/* Compatebility header for using Zephyr API in TF-M.
+ *
+ * The macros and functions here can be used by code that is common for both
+ * Zephyr and TF-M RTOS.
+ *
+ * The functionality will be forwarded to TF-M equivalent of the Zephyr API.
+ */
+
+#include <autoconf.h>
+#include <zephyr/sys/__assert.h>
+
+/* Note:
+ * CONFIG_NO_RUNTIME_CHECKS is not implemented for TF-M.
+ * We don't allow exclusion of these checks.
+ */
+#if defined(CONFIG_ASSERT_ON_ERRORS)
+#define CHECKIF(expr)               \
+	__ASSERT_NO_MSG(!(expr));   \
+	if (0)
+#else
+#define CHECKIF(expr)               \
+	if (expr)
+#endif
+
+
+#endif /* ZEPHYR_INCLUDE_SYS_CHECK_H_ */
+
+#endif /* __ZEPHYR_SYS_CHECK_H */
