1bca5ce9df20de3c2247316c129f74553d45bb8a
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-03-08 13:30:33 +0100

    tfm: Add override header for zephyr/sys/printk.h
    Add override header for zephyr printk header that in TF-M build will
    send the output to printf function.
    Using printf instead of LOG_ functions means that printk in TF-M
    will not be affected by the secure partition log level.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/tfm/tfm/boards/include/zephyr/sys/printk.h
+++ modules/tfm/tfm/boards/include/zephyr/sys/printk.h
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#ifndef __ZEPHYR_KERNEL_H
-#define __ZEPHYR_KERNEL_H
+#ifndef __ZEPHYR_SYS_PRINTK_H
+#define __ZEPHYR_SYS_PRINTK_H
 
 /* Compatebility header for using Zephyr API in TF-M.
  *
@@ -15,8 +15,8 @@
  * The functionality will be forwarded to TF-M equivalent of the Zephyr API.
  */
 
-#include <zephyr/sys/printk.h>
+#include "tfm_sp_log.h"
 
-#define k_panic() tfm_core_panic()
+#define printk(fmt, ...) printf(fmt, ##__VA_ARGS__)
 
-#endif /* __ZEPHYR_KERNEL_H */
+#endif /* __ZEPHYR_SYS_PRINTK_H */
