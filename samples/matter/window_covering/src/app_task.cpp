5a924164bc3e92812e7965ad55d623d63dc9ab81
Johann Fischer <johann.fischer@nordicsemi.no>
2023-03-09 11:46:17 +0100

    treewide: migrate to new MCUmgr Kconfig options
    Migrate to new MCUmgr Kconfig options using:
    python3 $ZEPHYR_BASE/scripts/utils/migrate_mcumgr_kconfigs.py -r nrf
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- samples/matter/window_covering/src/app_task.cpp
+++ samples/matter/window_covering/src/app_task.cpp
@@ -130,7 +130,7 @@ CHIP_ERROR AppTask::Init()
 	k_timer_init(&sFunctionTimer, &AppTask::FunctionTimerTimeoutCallback, nullptr);
 	k_timer_user_data_set(&sFunctionTimer, this);
 
-#ifdef CONFIG_MCUMGR_SMP_BT
+#ifdef CONFIG_MCUMGR_TRANSPORT_BT
 	/* Initialize DFU over SMP */
 	GetDFUOverSMP().Init();
 	GetDFUOverSMP().ConfirmNewImage();
@@ -309,7 +309,7 @@ void AppTask::FunctionHandler(const AppEvent &event)
 		if (Instance().mFunctionTimerActive && Instance().mFunction == FunctionEvent::SoftwareUpdate) {
 			Instance().CancelTimer();
 
-#ifdef CONFIG_MCUMGR_SMP_BT
+#ifdef CONFIG_MCUMGR_TRANSPORT_BT
 			GetDFUOverSMP().StartServer();
 #else
 			LOG_INF("Software update is disabled");
