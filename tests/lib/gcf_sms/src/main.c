c4477ac9c54c17e52b16c3bed5b9375807bd0029
Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>
2023-03-13 14:19:58 +0100

    tests: lib: gcf_sms: Use new ztest API and remove modem SYS_INIT
    Use new ztest API and remove use of NRF_MODEM_LIB_SYS_INIT.
    
    Signed-off-by: Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>

--- tests/lib/gcf_sms/src/main.c
+++ tests/lib/gcf_sms/src/main.c
@@ -14,210 +14,233 @@
 
 static char response[64];
 
-static void test_gcf_sms_cpms(void)
+#define RES_OK "OK\r\n"
+#define RES_ERROR "ERROR\r\n"
+
+#define CMD_CPMS_E_TA "AT+CPMS=\"TA\",\"TA\""
+#define RES_CPMS_E_0_3 "+CPMS: 0,3,0,3\r\nOK\r\n"
+#define RES_CPMS_E_1_3 "+CPMS: 1,3,1,3\r\nOK\r\n"
+
+#define CMD_CPMS_Q "AT+CPMS?"
+#define RES_CPMS_Q_0_3 "+CPMS: \"TA\",0,3,\"TA\",0,3\r\nOK\r\n"
+
+#define CMD_CPMS_EQ "AT+CPMS=?"
+#define RES_CPMS_EQ "+CPMS: (\"TA\"),(\"TA\")\r\nOK\r\n"
+
+#define CMD_CSMS_E_0 "AT+CSMS=0"
+#define CMD_CSMS_E_1 "AT+CSMS=1"
+#define RES_CSMS_E_0_0_0 "+CSMS: 0,0,0\r\nOK\r\n"
+#define RES_CSMS_E_1_1_0 "+CSMS: 1,1,0\r\nOK\r\n"
+
+#define CMD_CSMS_Q "AT+CSMS?"
+#define RES_CSMS_Q_0_1_1_0 "+CSMS: 0,1,1,0\r\nOK\r\n"
+
+#define CMD_CSMS_EQ "AT+CSMS=?"
+#define RES_CSMS_EQ_0 "+CSMS: 0\r\nOK\r\n"
+
+#define CMD_CSCA_E "AT+CSCA=\"+358501234567\""
+
+#define CMD_CSCA_Q "AT+CSCA?"
+#define RES_CSCA_Q "+CSMS: \"+358501234567\""
+
+#define CMD_CMGD_E_0 "AT+CMGD=0"
+#define CMD_CMGD_E_1 "AT+CMGD=1"
+#define CMD_CMGD_E_2 "AT+CMGD=2"
+
+#define CMD_CMGD_EQ "AT+CMGD=?"
+#define RES_CMGD_EQ_OK "+CMGD: (0-2)\r\nOK\r\n"
+
+#define CMD_CMGW_E "AT+CMGW=1,9<CR>010017116031621300<CR><ESC>"
+#define RES_CMGW_E_0 "+CMGW: 0"
+#define RES_CMGW_E_1 "+CMGW: 1"
+#define RES_CMGW_E_2 "+CMGW: 2"
+
+#define CMD_CMSS_E_1 "AT+CMSS=1"
+#define RES_CMSS_E "+CMSS: "
+
+#define CMD_CONCAT_1 "AT+CMGD=1;+CMGD=2"
+#define RES_CONCAT_1 "OK\r\nOK\r\n"
+
+#define CMD_CONCAT_2 "AT+CMGD=?;+CEREG?"
+#define RES_CONCAT_2 "+CMGD: (0-2)\r\nOK\r\n+CEREG:"
+
+static void *suite_setup(void)
 {
 	int err;
 
-#define CPMS_1_CMD "AT+CPMS=\"TA\",\"TA\""
-#define CPMS_1_RES "+CPMS: 0,3,0,3\r\nOK\r\n"
+	err = nrf_modem_lib_init(NORMAL_MODE);
+	zassert_ok(err, "Failed to initialize");
 
-	err = nrf_modem_at_cmd(response, sizeof(response), CPMS_1_CMD);
-	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CPMS_1_RES, strlen(CPMS_1_RES), NULL);
-
-#define CPMS_2_CMD "AT+CPMS?"
-#define CPMS_2_RES "+CPMS: \"TA\",0,3,\"TA\",0,3\r\nOK\r\n"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CPMS_2_CMD);
-	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CPMS_2_RES, strlen(CPMS_2_RES), NULL);
-
-#define CPMS_3_CMD "AT+CPMS=?"
-#define CPMS_3_RES "+CPMS: (\"TA\"),(\"TA\")\r\nOK\r\n"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CPMS_3_CMD);
-	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CPMS_3_RES, strlen(CPMS_3_RES), NULL);
+	return NULL;
 }
 
-static void test_gcf_sms_csms(void)
+static void test_after(void *f)
 {
-	int err;
-
-#define CSMS_1_CMD "AT+CSMS=0"
-#define CSMS_1_RES "+CSMS: 1,1,0\r\nOK\r\n"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CSMS_1_CMD);
-	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CSMS_1_RES, strlen(CSMS_1_RES), NULL);
-
-#define CSMS_2_CMD "AT+CSMS=1"
-#define CSMS_2_RES "+CSMS: 0,0,0\r\nOK\r\n"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CSMS_2_CMD);
-	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CSMS_2_RES, strlen(CSMS_2_RES), NULL);
-
-#define CSMS_3_CMD "AT+CSMS?"
-#define CSMS_3_RES "+CSMS: 0,1,1,0\r\nOK\r\n"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CSMS_3_CMD);
-	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CSMS_3_RES, strlen(CSMS_3_RES), NULL);
-
-#define CSMS_4_CMD "AT+CSMS=?"
-#define CSMS_4_RES "+CSMS: 0\r\nOK\r\n"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CSMS_4_CMD);
-	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CSMS_4_RES, strlen(CSMS_4_RES), NULL);
+	/* Delete storage */
+	nrf_modem_at_cmd(response, sizeof(response), CMD_CMGD_E_0);
+	nrf_modem_at_cmd(response, sizeof(response), CMD_CMGD_E_1);
+	nrf_modem_at_cmd(response, sizeof(response), CMD_CMGD_E_2);
 }
 
-static void test_gcf_sms_csca(void)
+static void suite_teardown(void *f)
 {
 	int err;
 
-#define CSCA_1_CMD "AT+CSCA=\"+358501234567\""
-#define CSCA_1_RES "OK\r\n"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CSCA_1_CMD);
-	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CSCA_1_RES, strlen(CSCA_1_RES), NULL);
-
-#define CSCA_2_CMD "AT+CSCA?"
-#define CSCA_2_RES "+CSMS: \"+358501234567\""
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CSCA_2_CMD);
-	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CSCA_2_RES, strlen(CSCA_2_RES), NULL);
+	err = nrf_modem_lib_shutdown();
+	zassert_ok(err, NULL);
 }
 
-static void test_gcf_sms_cmgd(void)
+ZTEST(gcf_sms, test_gcf_sms_cpms)
 {
 	int err;
 
-#define CMGD_1_CMD "AT+CMGD=2"
-#define CMGD_1_RES "OK\r\n"
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CPMS_E_TA);
 
-	err = nrf_modem_at_cmd(response, sizeof(response), CMGD_1_CMD);
 	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CMGD_1_RES, strlen(CMGD_1_RES), NULL);
+	zassert_mem_equal(response, RES_CPMS_E_0_3, strlen(RES_CPMS_E_0_3), NULL);
 
-#define CMGD_2_CMD "AT+CMGD=?"
-#define CMGD_2_RES "+CMGD: (0-2)\r\nOK\r\n"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CMGD_2_CMD);
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CPMS_Q);
 	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CMGD_2_RES, strlen(CMGD_2_RES), NULL);
+	zassert_mem_equal(response, RES_CPMS_Q_0_3, strlen(RES_CPMS_Q_0_3), NULL);
+
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CPMS_EQ);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_CPMS_EQ, strlen(RES_CPMS_EQ), NULL);
 }
 
-static void test_gcf_sms_cmgw(void)
+ZTEST(gcf_sms, test_gcf_sms_csms)
 {
 	int err;
 
-#define CMGW_CMD "AT+CMGW=1,9<CR>010017116031621300<CR><ESC>"
-#define CMGW_1_RES "+CMGW: 0"
-#define CMGW_2_RES "+CMGW: 1"
-
-	err = nrf_modem_at_cmd(response, sizeof(response),
-		CMGW_CMD);
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CSMS_E_0);
 	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CMGW_1_RES, strlen(CMGW_1_RES), NULL);
+	zassert_mem_equal(response, RES_CSMS_E_1_1_0, strlen(RES_CSMS_E_1_1_0), NULL);
 
-	err = nrf_modem_at_cmd(response, sizeof(response),
-		CMGW_CMD);
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CSMS_E_1);
 	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CMGW_2_RES, strlen(CMGW_2_RES), NULL);
+	zassert_mem_equal(response, RES_CSMS_E_0_0_0, strlen(RES_CSMS_E_0_0_0), NULL);
 
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CSMS_Q);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_CSMS_Q_0_1_1_0, strlen(RES_CSMS_Q_0_1_1_0), NULL);
+
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CSMS_EQ);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_CSMS_EQ_0, strlen(RES_CSMS_EQ_0), NULL);
 }
 
-static void test_gcf_sms_cmss(void)
+ZTEST(gcf_sms, test_gcf_sms_csca)
 {
-#if CONFIG_SIM_HAS_SMS_SUPPORT
 	int err;
 
-#define CMSS_1_CMD "AT+CMSS=1"
-#define CMSS_1_RES "+CMSS: "
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CSCA_E);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_OK, strlen(RES_OK), NULL);
 
-	/* Require a SIM supporting SMS and mfw 1.3.2(?) or newer. */
-	err = nrf_modem_at_cmd(response, sizeof(response), CMSS_1_CMD);
-	zassert_mem_equal(response, "+CMSS: ", strlen("+CMSS: "), NULL); //TODO
-#else
-	printk("%s skipped, no SIM SMS support", __func__);
-#endif /* CONFIG_SIM_HAS_SMS_SUPPORT */
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CSCA_Q);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_CSCA_Q, strlen(RES_CSCA_Q), NULL);
 }
 
-
-static void test_gcf_sms_cmgw_cmgd(void)
+ZTEST(gcf_sms, test_gcf_sms_cmgd)
 {
 	int err;
 
-#define CMGW_CMGD_CMD "AT+CMGW=1,9<CR>010017116031621300<CR><ESC>"
-
-#define CMGW_CMGD_1_CMD "AT+CMGD=1"
-#define CMGW_CMGD_1_RES "OK\r\n"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CMGW_CMGD_1_CMD);
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGD_E_2);
 	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CMGW_CMGD_1_RES, strlen(CMGW_CMGD_1_RES), NULL);
+	zassert_mem_equal(response, RES_OK, strlen(RES_OK), NULL);
 
-#define CMGW_CMGD_2_RES "+CMGW: 1"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CMGW_CMGD_CMD);
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGD_EQ);
 	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CMGW_CMGD_2_RES, strlen(CMGW_CMGD_2_RES), NULL);
+	zassert_mem_equal(response, RES_CMGD_EQ_OK, strlen(RES_CMGD_EQ_OK), NULL);
+}
 
-#define CMGW_CMGD_3_RES "+CMGW: 2"
+ZTEST(gcf_sms, test_gcf_sms_cmgw)
+{
+	int err;
 
-	err = nrf_modem_at_cmd(response, sizeof(response), CMGW_CMGD_CMD);
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGW_E);
 	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CMGW_CMGD_3_RES, strlen(CMGW_CMGD_3_RES), NULL);
+	zassert_mem_equal(response, RES_CMGW_E_0, strlen(RES_CMGW_E_0), NULL);
 
-#define CMGW_CMGD_4_RES "ERROR\r\n"
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGW_E);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_CMGW_E_1, strlen(RES_CMGW_E_1), NULL);
+}
 
-	err = nrf_modem_at_cmd(response, sizeof(response), CMGW_CMGD_CMD);
+ZTEST(gcf_sms, test_gcf_sms_cmss)
+{
+	int err;
+
+	/* This test require a SIM supporting SMS and mfw 1.3.2(?) or newer. */
+	Z_TEST_SKIP_IFNDEF(CONFIG_SIM_HAS_SMS_SUPPORT);
+
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMSS_E_1);
+	zassert_mem_equal(response, RES_CMSS_E, strlen(RES_CMSS_E), NULL);
+}
+
+ZTEST(gcf_sms, test_gcf_sms_cmgw_cmgd)
+{
+	int err;
+
+	/* fill slot 0 */
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGW_E);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_CMGW_E_0, strlen(RES_CMGW_E_0), NULL);
+
+	/* fill slot 1 */
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGW_E);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_CMGW_E_1, strlen(RES_CMGW_E_1), NULL);
+
+	/* delete slot 1*/
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGD_E_1);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_OK, strlen(RES_OK), NULL);
+
+	/* fill slot 1 again */
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGW_E);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_CMGW_E_1, strlen(RES_CMGW_E_1), NULL);
+
+	/* fill slot 2 */
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGW_E);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_CMGW_E_2, strlen(RES_CMGW_E_2), NULL);
+
+	/* no more slots */
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGW_E);
 	zassert_equal(1<<16, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CMGW_CMGD_4_RES, strlen(CMGW_CMGD_4_RES), NULL);
+	zassert_mem_equal(response, RES_ERROR, strlen(RES_ERROR), NULL);
 }
 
-static void test_gcf_sms_concatenated(void)
+ZTEST(gcf_sms, test_gcf_sms_concatenated)
 {
 	int err;
 
-#define CONCAT_1_CMD "AT+CMGD=1;+CMGD=2"
-#define CONCAT_1_RES "OK\r\nOK\r\n"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CONCAT_1_CMD);
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGW_E);
 	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CONCAT_1_RES, strlen(CONCAT_1_RES), NULL);
+	zassert_mem_equal(response, RES_CMGW_E_0, strlen(RES_CMGW_E_0), NULL);
 
-#define CONCAT_2_CMD "AT+CPMS=\"TA\",\"TA\""
-#define CONCAT_2_RES "+CPMS: 1,3,1,3\r\nOK\r\n"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CONCAT_2_CMD);
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGW_E);
 	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CONCAT_2_RES, strlen(CONCAT_2_RES), NULL);
+	zassert_mem_equal(response, RES_CMGW_E_1, strlen(RES_CMGW_E_1), NULL);
 
-#define CONCAT_3_CMD "AT+CMGD=?;+CEREG?"
-#define CONCAT_3_RES "+CMGD: (0-2)\r\nOK\r\n+CEREG:"
-
-	err = nrf_modem_at_cmd(response, sizeof(response), CONCAT_3_CMD);
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CMGW_E);
 	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
-	zassert_mem_equal(response, CONCAT_3_RES, strlen(CONCAT_3_RES), NULL);
+	zassert_mem_equal(response, RES_CMGW_E_2, strlen(RES_CMGW_E_2), NULL);
+
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CONCAT_1);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_CONCAT_1, strlen(RES_CONCAT_1), NULL);
+
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CPMS_E_TA);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_CPMS_E_1_3, strlen(RES_CPMS_E_1_3), NULL);
+
+	err = nrf_modem_at_cmd(response, sizeof(response), CMD_CONCAT_2);
+	zassert_equal(0, err, "nrf_modem_at_cmd failed, error: %d", err);
+	zassert_mem_equal(response, RES_CONCAT_2, strlen(RES_CONCAT_2), NULL);
 }
 
-void test_main(void)
-{
-	ztest_test_suite(gcf_sms,
-		ztest_unit_test(test_gcf_sms_cpms),
-		ztest_unit_test(test_gcf_sms_csms),
-		ztest_unit_test(test_gcf_sms_csca),
-		ztest_unit_test(test_gcf_sms_cmgd),
-		ztest_unit_test(test_gcf_sms_cmgw),
-		ztest_unit_test(test_gcf_sms_cmss),
-		ztest_unit_test(test_gcf_sms_cmgw_cmgd),
-		ztest_unit_test(test_gcf_sms_concatenated)
-	);
-
-	ztest_run_test_suite(gcf_sms);
-}
+ZTEST_SUITE(gcf_sms, NULL, suite_setup, NULL, test_after, suite_teardown);
