5dbb710a920ca3ce2986bd71fbdbef5e8ce4f38e
Krishna T <krishna.t@nordicsemi.no>
2023-03-23 13:55:39 +0530

    drivers: wifi: Fix AP mode build
    * Rename of img_ to nrf_wifi_ was missed
    * Remove duplicate function defined for STA and AP mode
    * Cmake should use CONFIG_<option>
    
    Signed-off-by: Krishna T <krishna.t@nordicsemi.no>

--- drivers/wifi/nrf700x/osal/fw_if/umac_if/src/fmac_api.c
+++ drivers/wifi/nrf700x/osal/fw_if/umac_if/src/fmac_api.c
@@ -2169,103 +2169,6 @@ out:
 	return status;
 }
 
-enum wifi_nrf_status wifi_nrf_fmac_set_wiphy_params(void *dev_ctx,
-						    unsigned char if_idx,
-						    struct nrf_wifi_umac_set_wiphy_info *wiphy_info)
-{
-	struct wifi_nrf_fmac_dev_ctx *fmac_dev_ctx = NULL;
-	struct nrf_wifi_umac_cmd_set_wiphy *set_wiphy_cmd = NULL;
-	enum wifi_nrf_status status = WIFI_NRF_STATUS_FAIL;
-	int freq_params_valid = 0;
-
-	fmac_dev_ctx = dev_ctx;
-
-	set_wiphy_cmd = wifi_nrf_osal_mem_zalloc(fmac_dev_ctx->fpriv->opriv,
-						 sizeof(*set_wiphy_cmd));
-
-	if (!set_wiphy_cmd) {
-		wifi_nrf_osal_log_err(fmac_dev_ctx->fpriv->opriv,
-				      "%s: Unable to allocate memory\n",
-				      __func__);
-		goto out;
-	}
-
-	set_wiphy_cmd->umac_hdr.cmd_evnt = NRF_WIFI_UMAC_CMD_SET_WIPHY;
-	set_wiphy_cmd->umac_hdr.ids.wdev_id = if_idx;
-	set_wiphy_cmd->umac_hdr.ids.valid_fields |= NRF_WIFI_INDEX_IDS_WDEV_ID_VALID;
-
-	if (wiphy_info->freq_params.frequency) {
-		freq_params_valid = 1;
-		wiphy_info->freq_params.valid_fields |=
-			NRF_WIFI_SET_FREQ_PARAMS_FREQ_VALID;
-	}
-
-	if (wiphy_info->freq_params.channel_width) {
-		freq_params_valid = 1;
-		wiphy_info->freq_params.valid_fields |=
-			NRF_WIFI_SET_FREQ_PARAMS_CHANNEL_WIDTH_VALID;
-	}
-
-	if (wiphy_info->freq_params.center_frequency1) {
-		freq_params_valid = 1;
-		wiphy_info->freq_params.valid_fields |=
-			NRF_WIFI_SET_FREQ_PARAMS_CENTER_FREQ1_VALID;
-	}
-
-	if (wiphy_info->freq_params.center_frequency2) {
-		freq_params_valid = 1;
-		wiphy_info->freq_params.valid_fields |=
-			NRF_WIFI_SET_FREQ_PARAMS_CENTER_FREQ2_VALID;
-	}
-
-	if (wiphy_info->freq_params.channel_type) {
-		freq_params_valid = 1;
-		wiphy_info->freq_params.valid_fields |=
-			NRF_WIFI_SET_FREQ_PARAMS_CHANNEL_TYPE_VALID;
-	}
-
-	if (freq_params_valid) {
-		set_wiphy_cmd->valid_fields |=
-			NRF_WIFI_CMD_SET_WIPHY_FREQ_PARAMS_VALID;
-	}
-
-	if (wiphy_info->rts_threshold) {
-		set_wiphy_cmd->valid_fields |=
-			NRF_WIFI_CMD_SET_WIPHY_RTS_THRESHOLD_VALID;
-	}
-
-	if (wiphy_info->frag_threshold) {
-		set_wiphy_cmd->valid_fields |=
-			NRF_WIFI_CMD_SET_WIPHY_FRAG_THRESHOLD_VALID;
-	}
-
-	if (wiphy_info->retry_long) {
-		set_wiphy_cmd->valid_fields |=
-			NRF_WIFI_CMD_SET_WIPHY_RETRY_LONG_VALID;
-	}
-
-	if (wiphy_info->retry_short) {
-		set_wiphy_cmd->valid_fields |=
-			NRF_WIFI_CMD_SET_WIPHY_RETRY_SHORT_VALID;
-	}
-
-	wifi_nrf_osal_mem_cpy(fmac_dev_ctx->fpriv->opriv,
-			      &set_wiphy_cmd->info,
-			      wiphy_info,
-			      sizeof(set_wiphy_cmd->info));
-
-	status = umac_cmd_cfg(fmac_dev_ctx,
-			      set_wiphy_cmd,
-			      sizeof(*set_wiphy_cmd));
-out:
-	if (set_wiphy_cmd) {
-		wifi_nrf_osal_mem_free(fmac_dev_ctx->fpriv->opriv,
-				       set_wiphy_cmd);
-	}
-
-	return status;
-}
-
 
 enum wifi_nrf_status wifi_nrf_fmac_start_ap(void *dev_ctx,
 					    unsigned char if_idx,
@@ -2431,10 +2334,10 @@ out:
 
 enum wifi_nrf_status wifi_nrf_fmac_del_sta(void *dev_ctx,
 					   unsigned char if_idx,
-					   struct img_umac_del_sta_info *del_sta_info)
+					   struct nrf_wifi_umac_del_sta_info *del_sta_info)
 {
 	enum wifi_nrf_status status = WIFI_NRF_STATUS_FAIL;
-	struct img_umac_cmd_del_sta *del_sta_cmd = NULL;
+	struct nrf_wifi_umac_cmd_del_sta *del_sta_cmd = NULL;
 	struct wifi_nrf_fmac_dev_ctx *fmac_dev_ctx = NULL;
 
 	fmac_dev_ctx = dev_ctx;
@@ -2489,10 +2392,10 @@ out:
 
 enum wifi_nrf_status wifi_nrf_fmac_add_sta(void *dev_ctx,
 					   unsigned char if_idx,
-					   struct img_umac_add_sta_info *add_sta_info)
+					   struct nrf_wifi_umac_add_sta_info *add_sta_info)
 {
 	enum wifi_nrf_status status = WIFI_NRF_STATUS_FAIL;
-	struct img_umac_cmd_add_sta *add_sta_cmd = NULL;
+	struct nrf_wifi_umac_cmd_add_sta *add_sta_cmd = NULL;
 	struct wifi_nrf_fmac_dev_ctx *fmac_dev_ctx = NULL;
 
 	fmac_dev_ctx = dev_ctx;
@@ -2527,7 +2430,7 @@ enum wifi_nrf_status wifi_nrf_fmac_add_sta(void *dev_ctx,
 
 	add_sta_cmd->valid_fields |= NRF_WIFI_CMD_NEW_STATION_LISTEN_INTERVAL_VALID;
 
-	if (add_sta_info->supp_rates.img_num_rates > 0) {
+	if (add_sta_info->supp_rates.nrf_wifi_num_rates > 0) {
 		add_sta_cmd->valid_fields |=
 			NRF_WIFI_CMD_NEW_STATION_SUPP_RATES_VALID;
 	}
@@ -2591,10 +2494,10 @@ out:
 
 enum wifi_nrf_status wifi_nrf_fmac_mgmt_frame_reg(void *dev_ctx,
 						  unsigned char if_idx,
-						  struct img_umac_mgmt_frame_info *frame_info)
+						  struct nrf_wifi_umac_mgmt_frame_info *frame_info)
 {
 	enum wifi_nrf_status status = WIFI_NRF_STATUS_FAIL;
-	struct img_umac_cmd_mgmt_frame_reg *frame_reg_cmd = NULL;
+	struct nrf_wifi_umac_cmd_mgmt_frame_reg *frame_reg_cmd = NULL;
 	struct wifi_nrf_fmac_dev_ctx *fmac_dev_ctx = NULL;
 
 	fmac_dev_ctx = dev_ctx;
