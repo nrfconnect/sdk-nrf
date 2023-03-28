3ce2ef694916bef1380e8a5608ac5c15aa22ec8b
Ajay Parida <ajay.parida@nordicsemi.no>
2023-03-28 16:12:12 +0530

    drivers: wifi: Set ps config params
    [SHEL-1425] Support to configure PS params
    ps parameters such as 'listen interval', 'ps timeout'
    and 'ps wake interval type' set as received from user/app.
    
    Signed-off-by: Ajay Parida <ajay.parida@nordicsemi.no>

--- drivers/wifi/nrf700x/zephyr/src/zephyr_wifi_mgmt.c
+++ drivers/wifi/nrf700x/zephyr/src/zephyr_wifi_mgmt.c
@@ -259,8 +259,10 @@ int wifi_nrf_set_power_save(const struct device *dev,
 	struct wifi_nrf_ctx_zep *rpu_ctx_zep = NULL;
 	struct wifi_nrf_vif_ctx_zep *vif_ctx_zep = NULL;
 	int ret = -1;
+	unsigned int uapsd_queue = UAPSD_Q_MIN; /* Legacy mode */
 
 	if (!dev || !params) {
+		LOG_ERR("%s: dev or params is NULL\n", __func__);
 		goto out;
 	}
 
@@ -278,63 +280,69 @@ int wifi_nrf_set_power_save(const struct device *dev,
 		goto out;
 	}
 
-	status = wifi_nrf_fmac_set_power_save(rpu_ctx_zep->rpu_ctx,
-					      vif_ctx_zep->vif_idx,
-					      params->enabled);
+	switch (params->type) {
+	case WIFI_PS_PARAM_LISTEN_INTERVAL:
+		if ((params->listen_interval <
+		     NRF_WIFI_LISTEN_INTERVAL_MIN) ||
+		    (params->listen_interval >
+		     WIFI_LISTEN_INTERVAL_MAX)) {
+			params->fail_reason =
+				WIFI_PS_PARAM_LISTEN_INTERVAL_RANGE_INVALID;
+			return -EINVAL;
+		}
+		status = wifi_nrf_fmac_set_listen_interval(
+						rpu_ctx_zep->rpu_ctx,
+						vif_ctx_zep->vif_idx,
+						params->listen_interval);
+	break;
+	case  WIFI_PS_PARAM_TIMEOUT:
+		if (vif_ctx_zep->if_type != NRF_WIFI_IFTYPE_STATION) {
+			LOG_ERR("%s: Operation supported only in STA mode\n",
+				__func__);
+			params->fail_reason =
+				WIFI_PS_PARAM_FAIL_CMD_EXEC_FAIL;
+			goto out;
+		}
 
-	if (status != WIFI_NRF_STATUS_SUCCESS) {
-		LOG_ERR("%s: wifi_nrf_fmac_ failed\n", __func__);
-		goto out;
-	}
+		status = wifi_nrf_fmac_set_power_save_timeout(
+							rpu_ctx_zep->rpu_ctx,
+							vif_ctx_zep->vif_idx,
+							params->timeout_ms);
+	break;
+	case WIFI_PS_PARAM_MODE:
+		if (params->mode == WIFI_PS_MODE_WMM) {
+			uapsd_queue = UAPSD_Q_MAX; /* WMM mode */
+		}
 
-	ret = 0;
-out:
-	return ret;
-}
-
-
-int wifi_nrf_set_power_save_mode(const struct device *dev,
-				 struct wifi_ps_mode_params *ps_mode_params)
-{
-	enum wifi_nrf_status status = WIFI_NRF_STATUS_FAIL;
-	struct wifi_nrf_ctx_zep *rpu_ctx_zep = NULL;
-	struct wifi_nrf_vif_ctx_zep *vif_ctx_zep = NULL;
-	int ret = -1;
-
-	if (!dev || !ps_mode_params) {
-		goto out;
-	}
-
-	vif_ctx_zep = dev->data;
-
-	if (!vif_ctx_zep) {
-		LOG_ERR("%s: vif_ctx_zep is NULL\n", __func__);
-		goto out;
-	}
-
-	rpu_ctx_zep = vif_ctx_zep->rpu_ctx_zep;
-
-	if (!rpu_ctx_zep) {
-		LOG_ERR("%s: rpu_ctx_zep is NULL\n", __func__);
-		goto out;
-	}
-
-	if (ps_mode_params->mode == WIFI_PS_MODE_WMM) {
-		/*WMM mode */
 		status = wifi_nrf_fmac_set_uapsd_queue(rpu_ctx_zep->rpu_ctx,
 						       vif_ctx_zep->vif_idx,
-						       UAPSD_Q_MAX);
-	} else {
-		/* Legacy mode*/
-		status = wifi_nrf_fmac_set_uapsd_queue(rpu_ctx_zep->rpu_ctx,
-						       vif_ctx_zep->vif_idx,
-						       UAPSD_Q_MIN);
+						       uapsd_queue);
+	break;
+	case  WIFI_PS_PARAM_STATE:
+		status = wifi_nrf_fmac_set_power_save(rpu_ctx_zep->rpu_ctx,
+						      vif_ctx_zep->vif_idx,
+						      params->enabled);
+	break;
+	case WIFI_PS_PARAM_WAKEUP_MODE:
+		status = wifi_nrf_fmac_set_ps_wakeup_mode(
+							rpu_ctx_zep->rpu_ctx,
+							vif_ctx_zep->vif_idx,
+							params->wakeup_mode);
+	break;
+	default:
+		params->fail_reason =
+			WIFI_PS_PARAM_FAIL_CMD_EXEC_FAIL;
+		return -ENOTSUP;
 	}
 
 	if (status != WIFI_NRF_STATUS_SUCCESS) {
-		LOG_ERR("%s: wifi_nrf_fmac_ failed\n", __func__);
+		LOG_ERR("%s: Confiuring PS param %d failed\n",
+			__func__, params->type);
+		params->fail_reason =
+			WIFI_PS_PARAM_FAIL_CMD_EXEC_FAIL;
 		goto out;
 	}
+
 
 	ret = 0;
 out:
@@ -561,9 +569,12 @@ void wifi_nrf_event_proc_get_power_save_info(void *vif_ctx,
 
 	vif_ctx_zep = vif_ctx;
 
-	vif_ctx_zep->ps_info->mode = ps_info->ps_mode;
-	vif_ctx_zep->ps_info->enabled = ps_info->enabled;
+	vif_ctx_zep->ps_info->ps_params.mode = ps_info->ps_mode;
+	vif_ctx_zep->ps_info->ps_params.enabled = ps_info->enabled;
 	vif_ctx_zep->ps_info->num_twt_flows = ps_info->num_twt_flows;
+	vif_ctx_zep->ps_info->ps_params.timeout_ms = ps_info->ps_timeout;
+	vif_ctx_zep->ps_info->ps_params.listen_interval = ps_info->listen_interval;
+	vif_ctx_zep->ps_info->ps_params.wakeup_mode = ps_info->extended_ps;
 
 	for (int i = 0; i < ps_info->num_twt_flows; i++) {
 		struct twt_interval_float twt_interval_float;
@@ -802,90 +813,4 @@ void wifi_nrf_event_proc_twt_sleep_zep(void *vif_ctx,
 	default:
 	break;
 	}
-}
-
-int wifi_nrf_set_power_save_timeout(const struct device *dev,
-			 struct wifi_ps_timeout_params *ps_timeout_params)
-{
-	enum wifi_nrf_status status = WIFI_NRF_STATUS_FAIL;
-	struct wifi_nrf_ctx_zep *rpu_ctx_zep = NULL;
-	struct wifi_nrf_vif_ctx_zep *vif_ctx_zep = NULL;
-	int ret = -1;
-
-	if (!dev || !ps_timeout_params) {
-		goto out;
-	}
-
-	vif_ctx_zep = dev->data;
-
-	if (!vif_ctx_zep) {
-		LOG_ERR("%s: vif_ctx_zep is NULL\n", __func__);
-		goto out;
-	}
-
-	if (vif_ctx_zep->if_type != NRF_WIFI_IFTYPE_STATION) {
-		LOG_ERR("%s: Operation supported only in STA mode\n",
-			__func__);
-		goto out;
-	}
-
-	rpu_ctx_zep = vif_ctx_zep->rpu_ctx_zep;
-
-	if (!rpu_ctx_zep) {
-		LOG_ERR("%s: rpu_ctx_zep is NULL\n", __func__);
-		goto out;
-	}
-
-	status = wifi_nrf_fmac_set_power_save_timeout(rpu_ctx_zep->rpu_ctx,
-					  vif_ctx_zep->vif_idx,
-					  ps_timeout_params->timeout_ms);
-
-	if (status != WIFI_NRF_STATUS_SUCCESS) {
-		LOG_ERR("%s: wifi_nrf_fmac_ failed\n", __func__);
-		goto out;
-	}
-
-	ret = 0;
-out:
-	return ret;
-}
-
-int wifi_nrf_set_listen_interval(const struct device *dev,
-				 struct wifi_listen_interval_params *params)
-{
-	enum wifi_nrf_status status = WIFI_NRF_STATUS_FAIL;
-	struct wifi_nrf_ctx_zep *rpu_ctx_zep = NULL;
-	struct wifi_nrf_vif_ctx_zep *vif_ctx_zep = NULL;
-	int ret = -1;
-
-	if (!dev || !params) {
-		goto out;
-	}
-
-	vif_ctx_zep = dev->data;
-
-	if (!vif_ctx_zep) {
-		LOG_ERR("%s: vif_ctx_zep is NULL\n", __func__);
-		goto out;
-	}
-
-	rpu_ctx_zep = vif_ctx_zep->rpu_ctx_zep;
-
-	if (!rpu_ctx_zep) {
-		LOG_ERR("%s: rpu_ctx_zep is NULL\n", __func__);
-		goto out;
-	}
-
-	status = wifi_nrf_fmac_set_listen_interval(rpu_ctx_zep->rpu_ctx,
-						   vif_ctx_zep->vif_idx,
-						   params->listen_interval);
-
-	if (status != WIFI_NRF_STATUS_SUCCESS) {
-		LOG_ERR("%s: wifi_nrf_fmac_ failed\n", __func__);
-		goto out;
-	}
-
-	ret = 0;
-out:
-	return ret;
 }
