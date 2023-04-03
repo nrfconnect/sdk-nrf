347c5a6c4149c4d0c21bcac72708ac6778e14b26
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-04-03 12:21:12 +0200

    tfm: Add configuration options for UART hardware flow control
    Add configurations options for enabling UART hardware flow control
    in the TF-M build system.
    
    Update TF-M version for TF-M hardware flow control support.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/tfm/tfm/boards/board/RTE_Device.h
+++ modules/tfm/tfm/boards/board/RTE_Device.h
@@ -19,7 +19,13 @@
 #define   RTE_USART0_RTS_PIN            CONFIG_TFM_UART0_RTS_PIN
 #define   RTE_USART0_CTS_PIN            CONFIG_TFM_UART0_CTS_PIN
 
+#if defined(CONFIG_TFM_UART0_HWFC_ENABLED)
+#define   RTE_USART0_HWFC               NRF_UARTE_HWFC_ENABLED
+#else
+#define   RTE_USART0_HWFC               NRF_UARTE_HWFC_DISABLED
 #endif
+
+#endif /* defined(CONFIG_TFM_SECURE_UART0) || DOMAIN_NS == 1U */
 
 /* Configuration settings for Driver_USART1. */
 #if defined(CONFIG_TFM_SECURE_UART1)
@@ -28,10 +34,16 @@
 /* Pin Selection (0xFFFFFFFF means Disconnected) */
 #define   RTE_USART1_TXD_PIN            CONFIG_TFM_UART1_TXD_PIN
 #define   RTE_USART1_RXD_PIN            CONFIG_TFM_UART1_RXD_PIN
-#define   RTE_USART1_RTS_PIN            0xFFFFFFFF
-#define   RTE_USART1_CTS_PIN            0xFFFFFFFF
+#define   RTE_USART1_RTS_PIN            CONFIG_TFM_UART1_RTS_PIN
+#define   RTE_USART1_CTS_PIN            CONFIG_TFM_UART1_CTS_PIN
 
+#if defined(CONFIG_TFM_UART1_HWFC_ENABLED)
+#define   RTE_USART1_HWFC               NRF_UARTE_HWFC_ENABLED
+#else
+#define   RTE_USART1_HWFC               NRF_UARTE_HWFC_DISABLED
 #endif
+
+#endif /* defined(CONFIG_TFM_SECURE_UART1) */
 
 /* Configuration settings for Driver_FLASH0. */
 #define   RTE_FLASH0                    1
