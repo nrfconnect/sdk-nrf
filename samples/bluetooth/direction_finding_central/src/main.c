77bfbf3d42ad1de57c1439b8430da004b145ac46
Martí Bolívar <marti.bolivar@nordicsemi.no>
2023-04-17 14:58:44 -0700

    treewide: update licenses
    A linter is now complaining that Apache 2.0 is not valid for
    some of our C files, so move the license lines to currently
    acceptable licenses.
    
    The tester main.c file is copy/pasted with an Intel copyright, so we
    can't change the license and have to add it to the allowlist. (It's
    small enough that maybe it should be rewritten with our own copyright
    and license at some point, though.)
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- samples/bluetooth/direction_finding_central/src/main.c
+++ samples/bluetooth/direction_finding_central/src/main.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2022 Nordic Semiconductor ASA
  *
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
 #include <zephyr/types.h>
