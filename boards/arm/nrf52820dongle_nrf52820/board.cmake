b8f255883300ab1fe72f47068ccc31faf01f01c8
Marek Pieta <Marek.Pieta@nordicsemi.no>
2023-04-18 09:31:16 +0200

    boards: Remove nrfjprog runner's nrf-family argument
    The information is taken from Kconfig. It should no longer be passed
    explicitly.
    
    Jira: NCSDK-21090
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- boards/arm/nrf52820dongle_nrf52820/board.cmake
+++ boards/arm/nrf52820dongle_nrf52820/board.cmake
@@ -4,7 +4,6 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
-board_runner_args(nrfjprog "--nrf-family=NRF52")
 board_runner_args(jlink "--device=nrf52" "--speed=4000")
 board_runner_args(pyocd "--target=nrf52820" "--frequency=4000000")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
--- boards/arm/nrf52840gmouse_nrf52840/board.cmake
+++ boards/arm/nrf52840gmouse_nrf52840/board.cmake
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
-board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+board_runner_args(nrfjprog "--softreset")
 board_runner_args(jlink "--device=nrf52" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
