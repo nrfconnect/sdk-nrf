c1bbe4b9a34f91762f64531a3f3e89d967c6557c
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2023-04-25 14:37:44 +0200

    tests: direct_xip: ZTEST_NEW_API
    Ref: NCSDK-21237
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- tests/modules/mcuboot/direct_xip/src/main.c
+++ tests/modules/mcuboot/direct_xip/src/main.c
@@ -8,16 +8,10 @@
 #include <stdio.h>
 
 
-void test_direct_xip(void)
+ZTEST(direct_xip_test, test_direct_xip)
 {
 	/* Checks if the test application boots up in the direct XiP mode. */
 	zassert_true(true, "Needed to pass.");
 }
 
-void test_main(void)
-{
-	ztest_test_suite(test_mcuboot_direct_xip,
-			 ztest_unit_test(test_direct_xip)
-	);
-	ztest_run_test_suite(test_mcuboot_direct_xip);
-}
+ZTEST_SUITE(direct_xip_test, NULL, NULL, NULL, NULL, NULL);
