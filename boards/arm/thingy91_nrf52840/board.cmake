abee8171fbd75b6c58233d46fcede7193c6985ce
Carles Cufi <carles.cufi@nordicsemi.no>
2023-04-26 19:06:42 +0200

    boards: Remove obsolete nrf-family parameter
    This param is now obsolete and not required at all.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- boards/arm/thingy91_nrf52840/board.cmake
+++ boards/arm/thingy91_nrf52840/board.cmake
@@ -1,7 +1,6 @@
 # Copyright (c) 2019 Nordic Semiconductor ASA.
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-board_runner_args(nrfjprog "--nrf-family=NRF52")
 board_runner_args(jlink "--device=nrf52" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
