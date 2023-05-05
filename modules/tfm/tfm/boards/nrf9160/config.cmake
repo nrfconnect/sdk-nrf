8875905be596e415148e4e7f75d4ee6598af5b96
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-05-05 10:41:00 +0200

    manifest: Update TF-M zephyr and nrfxlib for nrf9161 support
    Update TF-M zephyr and nrfxlib versions to pull in nrf9161 DK and
    nrf9120 SoC support.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/tfm/tfm/boards/nrf9160/config.cmake
+++ modules/tfm/tfm/boards/nrf9160/config.cmake
@@ -7,7 +7,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../common/config.cmake)
 
 set(PLATFORM_PATH platform/ext/target/nordic_nrf/)
-include(${PLATFORM_PATH}/common/nrf9160/config.cmake)
+include(${PLATFORM_PATH}/common/nrf91/config.cmake)
 
-# Override the AEAD algorithm configuration since nRF9160 supports only AES_CCM
+# Override the AEAD algorithm configuration since nRF91 series supports only AES_CCM
 set(PS_CRYPTO_AEAD_ALG                  PSA_ALG_CCM CACHE STRING    "The AEAD algorithm to use for authenticated encryption in Protected Storage")
