ab0cc77b54310121ad796edad548a6890c59123d
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-05-05 10:41:28 +0200

    tfm: Add support for boards using nrf9120 SoC
    Add support for nrf9120 SoC for the nrf9161 SIP.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

---
+++ modules/tfm/tfm/boards/nrf9120/preload.cmake
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2023, Nordic Semiconductor ASA.
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+#
+
+include(platform/ext/target/nordic_nrf/common/nrf9120/preload.cmake)
+add_compile_definitions(__NRF_TFM__)
