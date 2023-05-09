7a0040e1aa777235121d8413f7f25cc2c92ec708
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-05-09 15:13:29 +0200

    tfm: Fix license problems in out-of-tree files
    Fix license problem:
    "BSD-3-CLAUSE" license is not allowed for this file.
    Also update license year and remove dashed lines.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/tfm/tfm/boards/nrf5340_cpuapp/preload.cmake
+++ modules/tfm/tfm/boards/nrf5340_cpuapp/preload.cmake
@@ -1,9 +1,8 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2021, Nordic Semiconductor ASA.
 #
-# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2021 - 2023, Nordic Semiconductor ASA.
 #
-#-------------------------------------------------------------------------------
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+#
 
 include(platform/ext/target/nordic_nrf/common/nrf5340/preload.cmake)
 add_compile_definitions(__NRF_TFM__)
