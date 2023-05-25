4afbd11f516453820fcfd0caea2d260d7dfdad8c
Jarno Lämsä <jarno.lamsa@nordicsemi.no>
2023-05-25 10:21:57 +0300

    net: lib: lwm2m_client_utils: Minor bug fixes
    Removed unnecessary header.
    Fixed a small issue where error value was hidden and not used.
    Also restructured error handling in gnss_assist_write_pgps.
    
    Signed-off-by: Jarno Lämsä <jarno.lamsa@nordicsemi.no>

--- subsys/net/lib/lwm2m_client_utils/location/location_wifi_ap_scanner.c
+++ subsys/net/lib/lwm2m_client_utils/location/location_wifi_ap_scanner.c
@@ -19,8 +19,6 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 #include <zephyr/net/net_event.h>
 #include <zephyr/net/wifi_mgmt.h>
 
-#include "ground_fix_obj.h"
-
 #define VISIBLE_WIFI_AP_MAC_ADDRESS_ID		0
 #define VISIBLE_WIFI_AP_CHANNEL_ID		1
 #define VISIBLE_WIFI_AP_FREQUENCY_ID		2
