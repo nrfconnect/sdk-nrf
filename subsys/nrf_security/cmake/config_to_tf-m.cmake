d64c4be54abb457754e3ac2eacc904ff9ab2b81e
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2023-06-06 14:07:33 +0200

    nrf_security: cmake: Update paths
    Update paths to reflect the move from nrfxlib to nrf.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- subsys/nrf_security/cmake/config_to_tf-m.cmake
+++ subsys/nrf_security/cmake/config_to_tf-m.cmake
@@ -17,7 +17,7 @@ set_property(TARGET zephyr_property_target
 # Set the path used by TF-M to build mbedcrypto in the secure image
 # The CMakelists.txt in this folder is called when building mbed TLS
 set_property(TARGET zephyr_property_target
-  PROPERTY TFM_MBEDCRYPTO_PATH ${ZEPHYR_NRFXLIB_MODULE_DIR}/nrf_security/tfm
+  PROPERTY TFM_MBEDCRYPTO_PATH ${CMAKE_CURRENT_LIST_DIR}/../tfm
 )
 
 # Set the configuration files
