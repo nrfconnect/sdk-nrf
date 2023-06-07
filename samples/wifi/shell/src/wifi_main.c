85a24035c9ba7b9237a031607dd1c9469656fe48
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2023-06-08 01:18:41 +0530

    samples: wifi: Add net capture support
    Add netusb and netcapture overlay to support capturing packets from
    Wi-Fi interface, these will still be Ethernet packets as 802.11
    deecapsulation is done by nRF700x driver before handing over to
    networking stack.
    
    This also needs managing netusb Ethernet and Wi-Fi Ethernet interfaces,
    so, setup them accordingly.
    
    Follow procedure mentioned by [1] but using this sample instead of
    net/capture sample.
    
    [1] - https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/zephyr/samples/net/capture/README.html
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- samples/wifi/shell/src/wifi_main.c
+++ samples/wifi/shell/src/wifi_main.c
@@ -13,14 +13,71 @@
 #include <zephyr/device.h>
 #include <zephyr/net/net_config.h>
 
+#ifdef CONFIG_USB_DEVICE_STACK
+#include <zephyr/usb/usb_device.h>
+
+/* Fixed address as the static IP given from Kconfig will be
+ * applied to Wi-Fi interface.
+ */
+#define CONFIG_NET_CONFIG_USB_IPV4_ADDR "192.0.2.1"
+#define CONFIG_NET_CONFIG_USB_IPV4_MASK "255.255.255.0"
+
+int init_usb(void)
+{
+	int ret;
+
+	ret = usb_enable(NULL);
+	if (ret != 0) {
+		printk("Cannot enable USB (%d)", ret);
+		return ret;
+	}
+
+	return 0;
+}
+#endif
+
+
 int main(void)
 {
+#ifdef CONFIG_USB_DEVICE_STACK
+	struct in_addr addr;
+#endif
+
 #ifdef CLOCK_FEATURE_HFCLK_DIVIDE_PRESENT
 	/* For now hardcode to 128MHz */
 	nrfx_clock_divider_set(NRF_CLOCK_DOMAIN_HFCLK,
 			       NRF_CLOCK_HFCLK_DIV_1);
 #endif
 	printk("Starting %s with CPU frequency: %d MHz\n", CONFIG_BOARD, SystemCoreClock/MHZ(1));
+
+#ifdef CONFIG_USB_DEVICE_STACK
+	init_usb();
+
+	/* Redirect static IP address to netusb*/
+	const struct device *usb_dev = device_get_binding("eth_netusb");
+	struct net_if *iface = net_if_lookup_by_dev(usb_dev);
+
+	if (!iface) {
+		printk("Cannot find network interface: %s", "eth_netusb");
+		return -1;
+	}
+	if (sizeof(CONFIG_NET_CONFIG_USB_IPV4_ADDR) > 1) {
+		if (net_addr_pton(AF_INET, CONFIG_NET_CONFIG_USB_IPV4_ADDR, &addr)) {
+			printk("Invalid address: %s", CONFIG_NET_CONFIG_USB_IPV4_ADDR);
+			return -1;
+		}
+		net_if_ipv4_addr_add(iface, &addr, NET_ADDR_MANUAL, 0);
+	}
+
+	if (sizeof(CONFIG_NET_CONFIG_USB_IPV4_MASK) > 1) {
+		/* If not empty */
+		if (net_addr_pton(AF_INET, CONFIG_NET_CONFIG_USB_IPV4_MASK, &addr)) {
+			printk("Invalid netmask: %s", CONFIG_NET_CONFIG_USB_IPV4_MASK);
+		} else {
+			net_if_ipv4_set_netmask(iface, &addr);
+		}
+	}
+#endif
 
 #ifdef CONFIG_NET_CONFIG_SETTINGS
 	/* Without this, DHCPv4 starts on first interface and if that is not Wi-Fi or
@@ -30,6 +87,10 @@ int main(void)
 	 */
 	/* TODO: Replace device name with DTS settings later */
 	const struct device *dev = device_get_binding("wlan0");
+	struct net_if *wifi_iface = net_if_lookup_by_dev(dev);
+
+	/* As both are Ethernet, we need to set specific interface*/
+	net_if_set_default(wifi_iface);
 
 	net_config_init_app(dev, "Initializing network");
 #endif
