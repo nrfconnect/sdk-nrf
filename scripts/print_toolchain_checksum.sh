96d8718010889d9a2ba542be75efda2b5d13f65f
Jan Gałda <jan.galda@nordicsemi.no>
2023-06-14 12:51:34 +0200

    tools: Update toolchain script
    Tools txt file was replaced by yaml.
    
    Signed-off-by: Jan Gałda <jan.galda@nordicsemi.no>

--- scripts/print_toolchain_checksum.sh
+++ scripts/print_toolchain_checksum.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(dirname "$0")
 REQUIREMENTS=$BASEDIR/requirements-fixed.txt
-TOOLS_VERSIONS=$BASEDIR/tools-versions-linux.txt
+TOOLS_VERSIONS=$BASEDIR/tools-versions-linux.yml
 TOOLCHAIN_VERSION=$(cat $REQUIREMENTS $TOOLS_VERSIONS | sha256sum | head -c 10)
 
 echo "${TOOLCHAIN_VERSION}"
