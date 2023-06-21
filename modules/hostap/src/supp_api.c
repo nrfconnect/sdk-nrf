ac02eb14599c0b11f7cbc57ba8f1be3412021e0d
Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>
2023-06-21 12:36:11 +0530

    net: lib: wifi_mgmt_ext: Avoid using supplicant APIs
    Instead of invoking supplicant or CLI calls, use NET_MGMT
    interface to push the commands to supplicant.
    
    Signed-off-by: Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>

--- modules/hostap/src/supp_api.c
+++ modules/hostap/src/supp_api.c
@@ -145,6 +145,54 @@ out:
 	k_mutex_unlock(&wpa_supplicant_mutex);
 }
 
+static struct hostapd_hw_modes *get_mode_by_band(struct wpa_supplicant *wpa_s, uint8_t band)
+{
+	enum hostapd_hw_mode hw_mode;
+	bool is_6ghz = (band == WIFI_FREQ_BAND_6_GHZ) ? true : false;
+
+	if (band == WIFI_FREQ_BAND_2_4_GHZ) {
+		hw_mode = HOSTAPD_MODE_IEEE80211G;
+	} else if ((band == WIFI_FREQ_BAND_5_GHZ) ||
+		   (band == WIFI_FREQ_BAND_6_GHZ)) {
+		hw_mode = HOSTAPD_MODE_IEEE80211A;
+	} else {
+		return NULL;
+	}
+
+	return get_mode(wpa_s->hw.modes, wpa_s->hw.num_modes, hw_mode, is_6ghz);
+}
+
+static int wpa_supp_supported_channels(struct wpa_supplicant *wpa_s, uint8_t band, char **chan_list)
+{
+	struct hostapd_hw_modes *mode = NULL;
+	int i;
+	int offset, retval;
+	int size;
+	char *_chan_list;
+
+	mode = get_mode_by_band(wpa_s, band);
+	if (!mode) {
+		return -EINVAL;
+	}
+
+	size = ((mode->num_channels) * CHAN_NUM_LEN) + 1;
+	_chan_list = k_malloc(size);
+	if (!_chan_list) {
+		wpa_printf(MSG_ERROR, "Mem alloc failed for channel list\n");
+		return -ENOMEM;
+	}
+
+	retval = 0;
+	offset = 0;
+	for (i = 0; i < mode->num_channels; i++) {
+		retval = snprintf(_chan_list + offset, CHAN_NUM_LEN, " %d", mode->channels[i].freq);
+		offset += retval;
+	}
+	*chan_list = _chan_list;
+
+	return 0;
+}
+
 static inline void wpa_supp_restart_status_work(void)
 {
 	/* Terminate synchronously */
@@ -213,6 +261,7 @@ int z_wpa_supplicant_connect(const struct device *dev,
 	struct wpa_supplicant *wpa_s;
 	int ret = 0;
 	struct add_network_resp resp = {0};
+	char *chan_list = NULL;
 
 	if (!net_if_is_admin_up(net_if_lookup_by_dev(dev))) {
 		wpa_printf(MSG_ERROR,
@@ -243,6 +292,19 @@ int z_wpa_supplicant_connect(const struct device *dev,
 	_wpa_cli_cmd_v("set_network %d scan_ssid 1", resp.network_id);
 	_wpa_cli_cmd_v("set_network %d key_mgmt NONE", resp.network_id);
 	_wpa_cli_cmd_v("set_network %d ieee80211w 0", resp.network_id);
+	if (params->band) {
+		ret = wpa_supp_supported_channels(wpa_s, params->band, &chan_list);
+		if (ret < 0) {
+			_wpa_cli_cmd_v("remove_network %d", resp.network_id);
+			goto out;
+		}
+
+		if (chan_list) {
+			_wpa_cli_cmd_v("set_network %d freq_list%s", resp.network_id, chan_list);
+			k_free(chan_list);
+		}
+	}
+
 	if (params->security != WIFI_SECURITY_TYPE_NONE) {
 		if (params->security == WIFI_SECURITY_TYPE_SAE) {
 			if (params->sae_password) {
