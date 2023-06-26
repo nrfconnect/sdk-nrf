6f4c69d98aeaf2b3f80ad94c136c8e77eaa1bb70
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2023-06-26 15:15:46 +0100

    various: Enclose string if checks in quotes
    Fixes possible issues whereby string variables are checked
    without being properly encased in quote marks. This is needed
    because unescaped variables will be treated as variable names,
    then errors will be emitted when they are dereferenced and those
    variables do not exist. This fixes issue like the following:
    
           *************************
           * Running CMake for ncp *
           *************************
        CMake Error at CMakeLists.txt:10 (if):
          if given arguments:
            "STREQUAL" "nrf52840dongle_nrf52840"
          Unknown arguments specified
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- cmake/extensions.cmake
+++ cmake/extensions.cmake
@@ -66,7 +66,7 @@ endmacro()
 
 function(get_board_without_ns_suffix board_in board_out)
   string(REGEX REPLACE "(_?ns)$" "" board_in_without_suffix ${board_in})
-  if(NOT ${board_in} STREQUAL ${board_in_without_suffix})
+  if(NOT "${board_in}" STREQUAL "${board_in_without_suffix}")
     if (NOT CONFIG_ARM_NONSECURE_FIRMWARE)
       message(FATAL_ERROR "${board_in} is not a valid name for a board without "
       "'CONFIG_ARM_NONSECURE_FIRMWARE' set. This because the 'ns'/'_ns' ending "
