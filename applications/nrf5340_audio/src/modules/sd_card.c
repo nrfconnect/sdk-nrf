63a1d9c80ad2cf9b376364d7a23496b471c6216f
Marius Vårdal <marius.vardal@nordicsemi.no>
2023-06-28 15:43:37 +0200

    applications: nrf5340_audio: Expand SD card module
    To avoid unnecessary opening
    and closing of files
    
    Signed-off-by: Marius Vårdal <marius.vardal@nordicsemi.no>

--- applications/nrf5340_audio/src/modules/sd_card.c
+++ applications/nrf5340_audio/src/modules/sd_card.c
@@ -18,26 +18,37 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(sd_card, CONFIG_MODULE_SD_CARD_LOG_LEVEL);
 
-#define SD_ROOT_PATH "/SD:/"
-#define PATH_MAX_LEN 260 /* Maximum length for path support by Windows file system*/
+#define SD_ROOT_PATH	      "/SD:/"
+#define PATH_MAX_LEN	      260 /* Maximum length for path support by Windows file system*/
+#define K_SEM_OPER_TIMEOUT_MS 500
 
 static const char *sd_root_path = "/SD:";
 static FATFS fat_fs;
 static bool sd_init_success;
+K_SEM_DEFINE(m_sem_sd_oper_ongoing, 1, 1);
 
 static struct fs_mount_t mnt_pt = {
 	.type = FS_FATFS,
 	.fs_data = &fat_fs,
 };
 
-int sd_card_list_files(char *path)
+int sd_card_list_files(char const *const path, char *buf, size_t *buf_size)
 {
 	int ret;
 	struct fs_dir_t dirp;
 	static struct fs_dirent entry;
 	char abs_path_name[PATH_MAX_LEN + 1] = SD_ROOT_PATH;
+	size_t used_buf_size = 0;
+
+	if (k_sem_count_get(&m_sem_sd_oper_ongoing) <= 0) {
+		LOG_ERR("SD operation ongoing");
+		return -EPERM;
+	}
+
+	k_sem_take(&m_sem_sd_oper_ongoing, K_MSEC(K_SEM_OPER_TIMEOUT_MS));
 
 	if (!sd_init_success) {
+		k_sem_give(&m_sem_sd_oper_ongoing);
 		return -ENODEV;
 	}
 	fs_dir_t_init(&dirp);
@@ -46,11 +57,13 @@ int sd_card_list_files(char *path)
 		ret = fs_opendir(&dirp, sd_root_path);
 		if (ret) {
 			LOG_ERR("Open SD card root dir failed");
+			k_sem_give(&m_sem_sd_oper_ongoing);
 			return ret;
 		}
 	} else {
 		if (strlen(path) > CONFIG_FS_FATFS_MAX_LFN) {
 			LOG_ERR("Path is too long");
+			k_sem_give(&m_sem_sd_oper_ongoing);
 			return -FR_INVALID_NAME;
 		}
 
@@ -59,48 +72,72 @@ int sd_card_list_files(char *path)
 		ret = fs_opendir(&dirp, abs_path_name);
 		if (ret) {
 			LOG_ERR("Open assigned path failed");
+			k_sem_give(&m_sem_sd_oper_ongoing);
 			return ret;
 		}
 	}
+
 	while (1) {
 		ret = fs_readdir(&dirp, &entry);
 		if (ret) {
+			k_sem_give(&m_sem_sd_oper_ongoing);
 			return ret;
 		}
 
 		if (entry.name[0] == 0) {
 			break;
 		}
+		if (buf != NULL) {
+			size_t remaining_buf_size = *buf_size - used_buf_size;
+			ssize_t len = snprintk(
+				&buf[used_buf_size], remaining_buf_size, "[%s]\t%s\n",
+				entry.type == FS_DIR_ENTRY_DIR ? "DIR " : "FILE", entry.name);
 
-		if (entry.type == FS_DIR_ENTRY_DIR) {
-			LOG_INF("[DIR ] %s", entry.name);
-		} else {
-			LOG_INF("[FILE] %s", entry.name);
+			if (len >= remaining_buf_size) {
+				LOG_ERR("Failed to append to buffer, error: %d", len);
+				k_sem_give(&m_sem_sd_oper_ongoing);
+				return -EINVAL;
+			}
+
+			used_buf_size += len;
 		}
+		LOG_INF("[%s] %s", entry.type == FS_DIR_ENTRY_DIR ? "DIR " : "FILE", entry.name);
 	}
 
 	ret = fs_closedir(&dirp);
 	if (ret) {
 		LOG_ERR("Close SD card root dir failed");
+		k_sem_give(&m_sem_sd_oper_ongoing);
 		return ret;
 	}
 
+	*buf_size = used_buf_size;
+	k_sem_give(&m_sem_sd_oper_ongoing);
 	return 0;
 }
 
-int sd_card_write(char const *const filename, char const *const data, size_t *size)
+int sd_card_open_write_close(char const *const filename, char const *const data, size_t *size)
 {
+	int ret;
 	struct fs_file_t f_entry;
 	char abs_path_name[PATH_MAX_LEN + 1] = SD_ROOT_PATH;
-	int ret;
+
+	if (k_sem_count_get(&m_sem_sd_oper_ongoing) <= 0) {
+		LOG_ERR("SD operation ongoing");
+		return -EPERM;
+	}
+
+	k_sem_take(&m_sem_sd_oper_ongoing, K_MSEC(K_SEM_OPER_TIMEOUT_MS));
 
 	if (!sd_init_success) {
+		k_sem_give(&m_sem_sd_oper_ongoing);
 		return -ENODEV;
 	}
 
 	if (strlen(filename) > CONFIG_FS_FATFS_MAX_LFN) {
 		LOG_ERR("Filename is too long");
-		return -FR_INVALID_NAME;
+		k_sem_give(&m_sem_sd_oper_ongoing);
+		return -ENAMETOOLONG;
 	}
 
 	strcat(abs_path_name, filename);
@@ -109,6 +146,7 @@ int sd_card_write(char const *const filename, char const *const data, size_t *si
 	ret = fs_open(&f_entry, abs_path_name, FS_O_CREATE | FS_O_WRITE | FS_O_APPEND);
 	if (ret) {
 		LOG_ERR("Create file failed");
+		k_sem_give(&m_sem_sd_oper_ongoing);
 		return ret;
 	}
 
@@ -116,12 +154,14 @@ int sd_card_write(char const *const filename, char const *const data, size_t *si
 	ret = fs_seek(&f_entry, 0, FS_SEEK_END);
 	if (ret) {
 		LOG_ERR("Seek file pointer failed");
+		k_sem_give(&m_sem_sd_oper_ongoing);
 		return ret;
 	}
 
 	ret = fs_write(&f_entry, data, *size);
 	if (ret < 0) {
 		LOG_ERR("Write file failed");
+		k_sem_give(&m_sem_sd_oper_ongoing);
 		return ret;
 	}
 
@@ -130,24 +170,35 @@ int sd_card_write(char const *const filename, char const *const data, size_t *si
 	ret = fs_close(&f_entry);
 	if (ret) {
 		LOG_ERR("Close file failed");
+		k_sem_give(&m_sem_sd_oper_ongoing);
 		return ret;
 	}
 
+	k_sem_give(&m_sem_sd_oper_ongoing);
 	return 0;
 }
 
-int sd_card_read(char const *const filename, char *const data, size_t *size)
+int sd_card_open_read_close(char const *const filename, char *const buf, size_t *size)
 {
+	int ret;
 	struct fs_file_t f_entry;
 	char abs_path_name[PATH_MAX_LEN + 1] = SD_ROOT_PATH;
-	int ret;
+
+	if (k_sem_count_get(&m_sem_sd_oper_ongoing) <= 0) {
+		LOG_ERR("SD operation ongoing");
+		return -EPERM;
+	}
+
+	k_sem_take(&m_sem_sd_oper_ongoing, K_MSEC(K_SEM_OPER_TIMEOUT_MS));
 
 	if (!sd_init_success) {
+		k_sem_give(&m_sem_sd_oper_ongoing);
 		return -ENODEV;
 	}
 
 	if (strlen(filename) > CONFIG_FS_FATFS_MAX_LFN) {
 		LOG_ERR("Filename is too long");
+		k_sem_give(&m_sem_sd_oper_ongoing);
 		return -FR_INVALID_NAME;
 	}
 
@@ -157,12 +208,14 @@ int sd_card_read(char const *const filename, char *const data, size_t *size)
 	ret = fs_open(&f_entry, abs_path_name, FS_O_READ);
 	if (ret) {
 		LOG_ERR("Open file failed");
+		k_sem_give(&m_sem_sd_oper_ongoing);
 		return ret;
 	}
 
-	ret = fs_read(&f_entry, data, *size);
+	ret = fs_read(&f_entry, buf, *size);
 	if (ret < 0) {
 		LOG_ERR("Read file failed");
+		k_sem_give(&m_sem_sd_oper_ongoing);
 		return ret;
 	}
 
@@ -174,9 +227,98 @@ int sd_card_read(char const *const filename, char *const data, size_t *size)
 	ret = fs_close(&f_entry);
 	if (ret) {
 		LOG_ERR("Close file failed");
+		k_sem_give(&m_sem_sd_oper_ongoing);
 		return ret;
 	}
 
+	k_sem_give(&m_sem_sd_oper_ongoing);
+	return 0;
+}
+
+int sd_card_open(char const *const filename, struct fs_file_t *f_seg_read_entry)
+{
+	int ret;
+	char abs_path_name[PATH_MAX_LEN + 1] = SD_ROOT_PATH;
+	size_t avilable_path_space = PATH_MAX_LEN - strlen(SD_ROOT_PATH);
+
+	if (k_sem_count_get(&m_sem_sd_oper_ongoing) <= 0) {
+		LOG_ERR("SD operation not ongoing");
+		return -EPERM;
+	}
+
+	k_sem_take(&m_sem_sd_oper_ongoing, K_MSEC(K_SEM_OPER_TIMEOUT_MS));
+
+	if (!sd_init_success) {
+		k_sem_give(&m_sem_sd_oper_ongoing);
+		return -ENODEV;
+	}
+
+	if (strlen(filename) > CONFIG_FS_FATFS_MAX_LFN) {
+		LOG_ERR("Filename is too long");
+		k_sem_give(&m_sem_sd_oper_ongoing);
+		return -ENAMETOOLONG;
+	}
+
+	if ((strlen(abs_path_name) + strlen(filename)) > PATH_MAX_LEN) {
+		LOG_ERR("Filepath is too long");
+		k_sem_give(&m_sem_sd_oper_ongoing);
+		return -EINVAL;
+	}
+
+	strncat(abs_path_name, filename, avilable_path_space);
+
+	LOG_INF("abs path name:\t%s", abs_path_name);
+
+	fs_file_t_init(f_seg_read_entry);
+
+	ret = fs_open(f_seg_read_entry, abs_path_name, FS_O_READ);
+	if (ret) {
+		LOG_ERR("Open file failed: %d", ret);
+		k_sem_give(&m_sem_sd_oper_ongoing);
+		return ret;
+	}
+
+	return 0;
+}
+
+int sd_card_read(char *buf, size_t *size, struct fs_file_t *f_seg_read_entry)
+{
+	int ret;
+
+	if (!(k_sem_count_get(&m_sem_sd_oper_ongoing) <= 0)) {
+		LOG_ERR("SD operation not ongoing");
+		return -EPERM;
+	}
+
+	ret = fs_read(f_seg_read_entry, buf, *size);
+	if (ret < 0) {
+		LOG_ERR("Read file failed: %d", ret);
+		k_sem_give(&m_sem_sd_oper_ongoing);
+		return ret;
+	}
+
+	*size = ret;
+
+	return 0;
+}
+
+int sd_card_close(struct fs_file_t *f_seg_read_entry)
+{
+	int ret;
+
+	if (k_sem_count_get(&m_sem_sd_oper_ongoing) != 0) {
+		LOG_ERR("SD operation not ongoing");
+		return -EPERM;
+	}
+
+	ret = fs_close(f_seg_read_entry);
+	if (ret) {
+		LOG_ERR("Close file failed: %d", ret);
+		k_sem_give(&m_sem_sd_oper_ongoing);
+		return ret;
+	}
+
+	k_sem_give(&m_sem_sd_oper_ongoing);
 	return 0;
 }
 
@@ -199,6 +341,7 @@ int sd_card_init(void)
 		LOG_ERR("Unable to get sector count");
 		return ret;
 	}
+
 	LOG_DBG("Sector count: %d", sector_count);
 
 	ret = disk_access_ioctl(sd_dev, DISK_IOCTL_GET_SECTOR_SIZE, &sector_size);
@@ -206,12 +349,15 @@ int sd_card_init(void)
 		LOG_ERR("Unable to get sector size");
 		return ret;
 	}
+
 	LOG_DBG("Sector size: %d bytes", sector_size);
 
 	sd_card_size_bytes = (uint64_t)sector_count * sector_size;
+
 	LOG_INF("SD card volume size: %d MB", (uint32_t)(sd_card_size_bytes >> 20));
 
 	mnt_pt.mnt_point = sd_root_path;
+
 	ret = fs_mount(&mnt_pt);
 	if (ret) {
 		LOG_ERR("Mnt. disk failed, could be format issue. should be FAT/exFAT");
--- applications/nrf5340_audio/src/modules/sd_card.h
+++ applications/nrf5340_audio/src/modules/sd_card.h
@@ -8,55 +8,117 @@
 #define _SD_CARD_H_
 
 #include <stddef.h>
+#include <zephyr/fs/fs.h>
 
-/**@brief Print out the contents under SD card root path
+/**@brief	Print out the contents under SD card root path and write the content to buffer.
  *
- * @param[in] path	Path of the folder which going to list
- *			If assigned path is null, then listing the contents under root
- *			If assigned path doesn't exist, an error will be returned
+ * @param[in]		path		Path of the folder which is going to be listed.
+ *					If assigned path is null, then listing the contents under
+ *					root. If assigned path doesn't exist, an error will be
+ *					returned.
+ * @param[out]		buf		Buffer where data is written. If set to NULL, it will be
+ *					ignored.
+ * @param[in, out]	buf_size	Buffer size.
  *
- * @return	0 on success.
- *              -ENODEV SD init failed. SD card likely not inserted
- *		Otherwise, error from underlying drivers
+ * @retval	0 on success.
+ * @retval	-EPERM SD card operation is ongoing somewhere else.
+ * @retval	-ENODEV SD init failed. SD card likely not inserted.
+ * @retval	-EINVAL Failed to append to buffer.
+ * @retval	-FR_INVALID_NAME Path is too long.
+ * @retval	Otherwise, error from underlying drivers.
  */
-int sd_card_list_files(char *path);
+int sd_card_list_files(char const *const path, char *buf, size_t *buf_size);
 
-/**@brief Write data from buffer into the file
+/**@brief	Write data from buffer into the file.
  *
- * @note If the file already exists, data will be appended to the end of the file.
+ * @note	If the file already exists, data will be appended to the end of the file.
  *
- * @param[in] filename	Name of the target file for writing, the default location is the
- *			root directoy of SD card, accept absolute path under root of SD card
- * @param[in] data	Data which going to be written into the file
- * @param[in,out] size	Pointer to the number of bytes which is going to be written
- *			The actual written size will be returned
+ * @param[in]		filename	Name of the target file for writing, the default
+ *					location is the root directoy of SD card, accept
+					absolute path under root of SD card.
+ * @param[in]		data		which is going to be written into the file.
+ * @param[in, out]	size		Pointer to the number of bytes which is going to be written.
+ *					The actual written size will be returned.
  *
- * @return	0 on success.
- *              -ENODEV SD init failed. SD card likely not inserted
- *		Otherwise, error from underlying drivers
+ * @retval	0 on success.
+ * @retval	-EPERM SD card operation is ongoing somewhere else.
+ * @retval	-ENODEV SD init failed. SD card likely not inserted.
+ * @retval	Otherwise, error from underlying drivers.
  */
-int sd_card_write(char const *const filename, char const *const data, size_t *size);
+int sd_card_open_write_close(char const *const filename, char const *const data, size_t *size);
 
-/**@brief Read data from file into the buffer
+/**@brief	Read data from file into the buffer.
  *
- * @param[in] filename	Name of the target file for reading, the default location is the
- *			root directoy of SD card, accept absolute path under root of SD card
- * @param[in] data	The buffer which will be filled by read file contents
- * @param[in,out] size	Pointer to the number of bytes which wait to be read from the file
- *			The actual read size will be returned
- *			If the actual read size is 0, there will be a warning message which
- *			indicates the file is empty
- * @return	0 on success.
- *              -ENODEV SD init failed. SD card likely not inserted
- *		Otherwise, error from underlying drivers
+ * @param[in]		filename	Name of the target file for reading, the default location is
+ *					the root directoy of SD card, accept absolute path under
+ *					root of SD card.
+ * @param[out]		buf		Pointer to the buffer to write the read data into.
+ * @param[in, out]	size		Pointer to the number of bytes which wait to be read from
+ *					the file. The actual read size will be returned. If the
+ *					actual read size is 0, there will be a warning message which
+					indicates that the file is empty.
+ *
+ * @retval	0 on success.
+ * @retval	-EPERM SD card operation is ongoing somewhere else.
+ * @retval	-ENODEV SD init failed. SD card likely not inserted.
+ * @retval	Otherwise, error from underlying drivers.
  */
-int sd_card_read(char const *const filename, char *const data, size_t *size);
+int sd_card_open_read_close(char const *const filename, char *const buf, size_t *size);
 
-/**@brief  Initialize the SD card interface and print out SD card details.
+/**@brief	Open file on SD card.
  *
- * @retval	0 on success
- *              -ENODEV SD init failed. SD card likely not inserted
- *		Otherwise, error from underlying drivers
+ * @param[in]		filename		Name of file to open. Default
+ *						location is the root directoy of SD card.
+ *						Absolute path under root of SD card is accepted.
+ * @param[in, out]	f_seg_read_entry	Pointer to a file object.
+ *						The pointer gets initialized and ready for use.
+ *
+ *
+ * @retval	0 on success.
+ * @retval	-EPERM SD card operation is ongoing somewhere else.
+ * @retval	-ENODEV SD init failed. SD likely not inserted.
+ * @retval	Otherwise, error from underlying drivers.
+ */
+int sd_card_open(char const *const filename, struct fs_file_t *f_seg_read_entry);
+
+/**@brief	Read segment on the open file on the SD card.
+ *
+ * @param[out]		buf			Pointer to the buffer to write the read data into.
+ * @param[in, out]	size			Number of bytes to be read from file.
+ *						The actual read size will be returned.
+ *						If the actual read size is 0, there will be a
+ *						warning message which indicates that the file is
+ *						empty.
+ * @param[in, out]	f_seg_read_entry	Pointer to a file object. After call to this
+ *						function, the pointer gets updated and can be used
+ *						as entry in next function call.
+ *
+ * @retval	0 on success.
+ * @retval	-EPERM SD card operation is not ongoing.
+ * @retval	-ENODEV SD init failed. SD likely not inserted.
+ * @retval	Otherwise, error from underlying drivers.
+ */
+int sd_card_read(char *buf, size_t *size, struct fs_file_t *f_seg_read_entry);
+
+/**@brief	Close the file opened by the sd_card_segment_read_open function.
+ *
+ * @param[in, out]	f_seg_read_entry	Pointer to a file object. After call to this
+ *						function, the pointer is reset and can be used for
+ *						another file.
+ *
+ *
+ * @retval	0 on success.
+ * @retval	-EPERM SD card operation is not ongoing.
+ * @retval	-EBUSY Segment read operation has not started.
+ * @retval	Otherwise, error from underlying drivers.
+ */
+int sd_card_close(struct fs_file_t *f_seg_read_entry);
+
+/**@brief	Initialize the SD card interface and print out SD card details.
+ *
+ * @retval	0 on success.
+ * @retval	-ENODEV SD init failed. SD card likely not inserted.
+ * @retval	Otherwise, error from underlying drivers.
  */
 int sd_card_init(void);
 
