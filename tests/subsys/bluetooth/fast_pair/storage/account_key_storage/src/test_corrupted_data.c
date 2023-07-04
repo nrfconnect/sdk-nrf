5db84d68d4704fc9a2d484c02741b0617c4cd2e0
Marek Pieta <Marek.Pieta@nordicsemi.no>
2023-07-04 14:28:15 +0200

    tests: fast_pair: account_key_storage: Migrate to new ztest API
    Change migrates test to new ztest API. The old API is deprecated.
    Change also removes the test from quarantine.
    
    Jira: NCSDK-21698
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- tests/subsys/bluetooth/fast_pair/storage/account_key_storage/src/test_corrupted_data.c
+++ tests/subsys/bluetooth/fast_pair/storage/account_key_storage/src/test_corrupted_data.c
@@ -13,87 +13,6 @@
 #include "storage_mock.h"
 #include "common_utils.h"
 
-#include "test_corrupted_data.h"
-
-
-static void setup_fn(void)
-{
-	/* Make sure that settings are not yet loaded. Settings will be populated with data and
-	 * loaded by the unit test.
-	 */
-	cu_account_keys_validate_unloaded();
-}
-
-static void teardown_fn(void)
-{
-	int err = settings_load();
-
-	zassert_not_equal(err, 0, "Expected error in settings load");
-	cu_account_keys_validate_unloaded();
-
-	fp_storage_ak_ram_clear();
-	storage_mock_clear();
-}
-
-/* Self test is done to ensure that the test method is valid. */
-static void test_self_teardown_fn(void)
-{
-	fp_storage_ak_ram_clear();
-	storage_mock_clear();
-	cu_account_keys_validate_unloaded();
-}
-
-static void test_wrong_settings_key(void)
-{
-	static const uint8_t key_id = ACCOUNT_KEY_MIN_ID;
-
-	int err;
-	struct account_key_data data;
-
-	data.account_key_id = key_id;
-	cu_generate_account_key(key_id, &data.account_key);
-
-	err = settings_save_one(SETTINGS_AK_SUBTREE_NAME SETTINGS_NAME_SEPARATOR_STR
-				"not_account_key", &data, sizeof(data));
-	zassert_ok(err, "Unexpected error in settings save");
-}
-
-static void test_wrong_data_len(void)
-{
-	static const uint8_t key_id = ACCOUNT_KEY_MIN_ID;
-
-	int err;
-	char settings_key[SETTINGS_AK_NAME_MAX_SIZE] = SETTINGS_AK_FULL_PREFIX;
-	struct account_key_data data;
-
-	zassert_true(('0' + account_key_id_to_idx(key_id)) <= '9', "Key index out of range");
-	settings_key[ARRAY_SIZE(settings_key) - 2] = ('0' + account_key_id_to_idx(key_id));
-
-	data.account_key_id = key_id;
-	cu_generate_account_key(key_id, &data.account_key);
-
-	err = settings_save_one(settings_key, &data, sizeof(data) - 1);
-	zassert_ok(err, "Unexpected error in settings save");
-}
-
-static void test_inconsistent_key_id(void)
-{
-	static const uint8_t key_id = ACCOUNT_KEY_MIN_ID;
-
-	int err;
-	char settings_key[SETTINGS_AK_NAME_MAX_SIZE] = SETTINGS_AK_FULL_PREFIX;
-	struct account_key_data data;
-
-	zassert_true(('0' + account_key_id_to_idx(key_id)) <= '9', "Key index out of range");
-	settings_key[ARRAY_SIZE(settings_key) - 2] = ('0' + account_key_id_to_idx(key_id));
-
-	data.account_key_id = next_account_key_id(key_id);
-	cu_generate_account_key(key_id, &data.account_key);
-
-	err = settings_save_one(settings_key, &data, sizeof(data));
-	zassert_ok(err, "Unexpected error in settings save");
-}
-
 static void store_key(uint8_t key_id)
 {
 	int err;
@@ -110,7 +29,14 @@ static void store_key(uint8_t key_id)
 	zassert_ok(err, "Unexpected error in settings save");
 }
 
-static void test_self(void)
+static void self_test_after(void)
+{
+	fp_storage_ak_ram_clear();
+	storage_mock_clear();
+	cu_account_keys_validate_unloaded();
+}
+
+static void self_test(void)
 {
 	int err;
 	uint8_t key_id = ACCOUNT_KEY_MIN_ID;
@@ -133,7 +59,7 @@ static void test_self(void)
 	cu_account_keys_validate_loaded(ACCOUNT_KEY_MIN_ID, key_cnt);
 }
 
-static void test_self_rollover(void)
+static void self_test_rollover(void)
 {
 	int err;
 	uint8_t key_id = ACCOUNT_KEY_MIN_ID;
@@ -158,12 +84,109 @@ static void test_self_rollover(void)
 	cu_account_keys_validate_loaded(ACCOUNT_KEY_MIN_ID, key_cnt);
 }
 
-static void test_drop_first_key(void)
+static void *setup_fn(void)
 {
-	store_key(next_account_key_id(ACCOUNT_KEY_MIN_ID));
+	/* Perform simple self tests to ensure that the test method is valid. */
+	self_test();
+	self_test_after();
+
+	self_test_rollover();
+	self_test_after();
+
+	return NULL;
 }
 
-static void test_drop_key(void)
+static void before_fn(void *f)
+{
+	/* Make sure that settings are not yet loaded. Settings will be populated with data and
+	 * loaded by the unit test.
+	 */
+	ARG_UNUSED(f);
+
+	cu_account_keys_validate_unloaded();
+}
+
+static void after_fn(void *f)
+{
+	ARG_UNUSED(f);
+
+	fp_storage_ak_ram_clear();
+	storage_mock_clear();
+}
+
+static void settings_load_error_validate(void)
+{
+	int err = settings_load();
+
+	zassert_not_equal(err, 0, "Expected error in settings load");
+	cu_account_keys_validate_unloaded();
+}
+
+ZTEST(suite_fast_pair_storage_corrupted, test_wrong_settings_key)
+{
+	static const uint8_t key_id = ACCOUNT_KEY_MIN_ID;
+
+	int err;
+	struct account_key_data data;
+
+	data.account_key_id = key_id;
+	cu_generate_account_key(key_id, &data.account_key);
+
+	err = settings_save_one(SETTINGS_AK_SUBTREE_NAME SETTINGS_NAME_SEPARATOR_STR
+				"not_account_key", &data, sizeof(data));
+	zassert_ok(err, "Unexpected error in settings save");
+
+	settings_load_error_validate();
+}
+
+ZTEST(suite_fast_pair_storage_corrupted, test_wrong_data_len)
+{
+	static const uint8_t key_id = ACCOUNT_KEY_MIN_ID;
+
+	int err;
+	char settings_key[SETTINGS_AK_NAME_MAX_SIZE] = SETTINGS_AK_FULL_PREFIX;
+	struct account_key_data data;
+
+	zassert_true(('0' + account_key_id_to_idx(key_id)) <= '9', "Key index out of range");
+	settings_key[ARRAY_SIZE(settings_key) - 2] = ('0' + account_key_id_to_idx(key_id));
+
+	data.account_key_id = key_id;
+	cu_generate_account_key(key_id, &data.account_key);
+
+	err = settings_save_one(settings_key, &data, sizeof(data) - 1);
+	zassert_ok(err, "Unexpected error in settings save");
+
+	settings_load_error_validate();
+}
+
+ZTEST(suite_fast_pair_storage_corrupted, test_inconsistent_key_id)
+{
+	static const uint8_t key_id = ACCOUNT_KEY_MIN_ID;
+
+	int err;
+	char settings_key[SETTINGS_AK_NAME_MAX_SIZE] = SETTINGS_AK_FULL_PREFIX;
+	struct account_key_data data;
+
+	zassert_true(('0' + account_key_id_to_idx(key_id)) <= '9', "Key index out of range");
+	settings_key[ARRAY_SIZE(settings_key) - 2] = ('0' + account_key_id_to_idx(key_id));
+
+	data.account_key_id = next_account_key_id(key_id);
+	cu_generate_account_key(key_id, &data.account_key);
+
+	err = settings_save_one(settings_key, &data, sizeof(data));
+	zassert_ok(err, "Unexpected error in settings save");
+
+	settings_load_error_validate();
+}
+
+ZTEST(suite_fast_pair_storage_corrupted, test_drop_first_key)
+{
+	store_key(next_account_key_id(ACCOUNT_KEY_MIN_ID));
+
+	settings_load_error_validate();
+}
+
+ZTEST(suite_fast_pair_storage_corrupted, test_drop_key)
 {
 	uint8_t key_id = ACCOUNT_KEY_MIN_ID;
 
@@ -175,9 +198,11 @@ static void test_drop_key(void)
 	key_id = next_account_key_id(key_id);
 	key_id = next_account_key_id(key_id);
 	store_key(key_id);
+
+	settings_load_error_validate();
 }
 
-static void test_drop_keys(void)
+ZTEST(suite_fast_pair_storage_corrupted, test_drop_keys)
 {
 	uint8_t key_id = ACCOUNT_KEY_MIN_ID;
 
@@ -190,9 +215,11 @@ static void test_drop_keys(void)
 	zassert_equal(account_key_id_to_idx(ACCOUNT_KEY_MIN_ID), account_key_id_to_idx(key_id),
 		      "Test should write key exactly after rollover");
 	store_key(key_id);
+
+	settings_load_error_validate();
 }
 
-static void test_drop_rollover(void)
+ZTEST(suite_fast_pair_storage_corrupted, test_drop_rollover)
 {
 	uint8_t key_id = ACCOUNT_KEY_MIN_ID;
 
@@ -205,24 +232,8 @@ static void test_drop_rollover(void)
 		      "Test should drop key exactly after rollover");
 	key_id = next_account_key_id(key_id);
 	store_key(key_id);
+
+	settings_load_error_validate();
 }
 
-void test_corrupted_data_run(void)
-{
-	ztest_test_suite(fast_pair_storage_courrupted_data_tests,
-			 ztest_unit_test_setup_teardown(test_self, setup_fn, test_self_teardown_fn),
-			 ztest_unit_test_setup_teardown(test_self_rollover, setup_fn,
-							test_self_teardown_fn),
-			 ztest_unit_test_setup_teardown(test_wrong_settings_key, setup_fn,
-							teardown_fn),
-			 ztest_unit_test_setup_teardown(test_wrong_data_len, setup_fn, teardown_fn),
-			 ztest_unit_test_setup_teardown(test_inconsistent_key_id, setup_fn,
-							teardown_fn),
-			 ztest_unit_test_setup_teardown(test_drop_first_key, setup_fn, teardown_fn),
-			 ztest_unit_test_setup_teardown(test_drop_key, setup_fn, teardown_fn),
-			 ztest_unit_test_setup_teardown(test_drop_keys, setup_fn, teardown_fn),
-			 ztest_unit_test_setup_teardown(test_drop_rollover, setup_fn, teardown_fn)
-			 );
-
-	ztest_run_test_suite(fast_pair_storage_courrupted_data_tests);
-}
+ZTEST_SUITE(suite_fast_pair_storage_corrupted, NULL, setup_fn, before_fn, after_fn, NULL);
