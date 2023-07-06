6a6f32457029f0134f9e4f95007fdcc9caa59e93
philip fidjeland <philipfidjeland@gmail.com>
2023-07-06 13:33:31 +0200

    working logtab with timestamp

--- applications/nrf5340_audio/src/modules/display/log_tab.c
+++ applications/nrf5340_audio/src/modules/display/log_tab.c
@@ -71,29 +71,61 @@ static int backend_is_ready(const struct log_backend *const backend)
 
 	return context->active ? 0 : -EBUSY;
 }
+
+static void add_log_msg_to_log_list(lv_obj_t *log_obj, char *log_msg, lv_color_t log_msg_color)
+{
+	log_obj = lv_list_add_text(log_obj, log_msg);
+	lv_obj_set_style_bg_color(log_obj, lv_palette_darken(LV_PALETTE_GREY, 3), 0);
+	lv_obj_set_style_text_color(log_obj, log_msg_color, 0);
+	lv_obj_scroll_to_view(log_obj, LV_ANIM_OFF);
+}
 static void backend_process(const struct log_backend *const backend, union log_msg_generic *msg)
 {
-	lv_obj_t *oldest_label;
-	lv_obj_t *last_label;
+	log_timestamp_t msg_timestamp;
+	int msg_timestamp_in_us;
 	char str[100];
+	char timestamp_n_str[200];
 	char *pstr = str;
+
+	lv_obj_t *oldest_label;
+
 	struct backend_context *context = (struct backend_context *)backend->cb->ctx;
 	size_t len;
 	uint8_t *p = log_msg_get_package(&msg->log, &len);
 
-	(void)len;
-	int slen = cbpprintf(cbprintf_callback, &pstr, p);
+	msg_timestamp = log_msg_get_timestamp(&msg->log);
+	msg_timestamp_in_us = log_output_timestamp_to_us(msg_timestamp);
 
+	int slen = cbpprintf(cbprintf_callback, &pstr, p);
 	str[slen] = '\0';
-	if (log_msg_get_level(&msg->log) == LOG_LEVEL_WRN) {
+
+	snprintf(timestamp_n_str, sizeof(timestamp_n_str), "[%d] %s", msg_timestamp_in_us, str);
+
+	switch (log_msg_get_level(&msg->log)) {
+	case LOG_LEVEL_ERR:
 		cnt++;
-		last_label = lv_list_add_text(log_list, str);
-		lv_obj_scroll_to_view(last_label, LV_ANIM_OFF);
-		if (cnt > LOG_MAX_LENGTH) {
-			oldest_label = lv_obj_get_child(log_list, 0);
-			lv_group_remove_obj(oldest_label);
-			lv_obj_del(oldest_label);
-		}
+		add_log_msg_to_log_list(log_list, str, lv_palette_lighten(LV_PALETTE_RED, 1));
+		break;
+	case LOG_LEVEL_WRN:
+		cnt++;
+		add_log_msg_to_log_list(log_list, str, lv_palette_lighten(LV_PALETTE_YELLOW, 1));
+		break;
+
+	case LOG_LEVEL_INF:
+		cnt++;
+		add_log_msg_to_log_list(log_list, str, lv_palette_lighten(LV_PALETTE_YELLOW, 5));
+		break;
+	case LOG_LEVEL_DBG:
+		cnt++;
+		add_log_msg_to_log_list(log_list, str, lv_palette_lighten(LV_PALETTE_YELLOW, 5));
+		break;
+	default:
+		break;
+	}
+	if (cnt > LOG_MAX_LENGTH) {
+		oldest_label = lv_obj_get_child(log_list, 0);
+		lv_group_remove_obj(oldest_label);
+		lv_obj_del(oldest_label);
 	}
 }
 static const struct log_backend_api backend_api = {.process = backend_process,
@@ -103,7 +135,8 @@ static const struct log_backend_api backend_api = {.process = backend_process,
 void log_tab_create(lv_obj_t *screen)
 {
 	log_list = lv_list_create(screen);
-	lv_obj_set_size(log_list, lv_disp_get_hor_res(NULL), lv_disp_get_ver_res(NULL) - 70);
+	lv_obj_set_style_bg_color(log_list, lv_palette_darken(LV_PALETTE_GREY, 3), 0);
+	lv_obj_set_size(log_list, lv_disp_get_hor_res(NULL) - 20, lv_disp_get_ver_res(NULL) - 70);
 	lv_obj_align(log_list, LV_ALIGN_TOP_LEFT, 0, 0);
 
 	static struct backend_context context1 = {.delay = 500};
