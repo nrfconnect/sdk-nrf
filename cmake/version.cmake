fcecda1cf93598621b2e8e78590779f85374963f
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2023-07-11 01:30:27 +0530

    cmake: Add NCS build version
    This is similar to Zephyr build version but on NCS.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,6 +1,25 @@
+
 math(EXPR NCS_VERSION_CODE "(${NCS_VERSION_MAJOR} << 16) + (${NCS_VERSION_MINOR} << 8)  + (${NCS_VERSION_PATCH})")
 
 # to_hex is made available by ${ZEPHYR_BASE}/cmake/hex.cmake
 to_hex(${NCS_VERSION_CODE} NCS_VERSION_NUMBER)
+
+find_package(Git QUIET)
+if(GIT_FOUND)
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} describe --abbrev=12 --always
+    WORKING_DIRECTORY                ${ZEPHYR_BASE}/../nrf
+    OUTPUT_VARIABLE                  NCS_BUILD_VERSION_NAME
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_STRIP_TRAILING_WHITESPACE
+    ERROR_VARIABLE                   stderr
+    RESULT_VARIABLE                  return_code
+  )
+  if(return_code)
+    message(STATUS "git describe failed: ${stderr}")
+  elseif(NOT "${stderr}" STREQUAL "")
+    message(STATUS "git describe warned: ${stderr}")
+  endif()
+endif()
 
 configure_file(${NRF_DIR}/ncs_version.h.in ${ZEPHYR_BINARY_DIR}/include/generated/ncs_version.h)
