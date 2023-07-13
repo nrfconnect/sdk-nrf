b48d3420506283eeb8c6808b862b7caab9edd3c5
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2023-07-13 08:52:37 +0100

    samples: ipc: ipc_service: Add sysbuild files for nRF5340dk
    Adds sysbuild files to configure the remote target for nRF5340dk
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

---
+++ samples/ipc/ipc_service/sysbuild.cmake
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2023 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+#
+
+# Add remote project
+ExternalZephyrProject_Add(
+  APPLICATION remote
+  SOURCE_DIR ${APP_DIR}/remote
+  BOARD ${SB_CONFIG_REMOTE_BOARD}
+)
+list(APPEND PM_DOMAINS CPUNET)
+set(PM_CPUNET_IMAGES remote)
+set(DOMAIN_APP_CPUNET remote)
+set(CPUNET_PM_DOMAIN_DYNAMIC_PARTITION remote CACHE INTERNAL "")
+
+# Add a dependency so that the remote sample will be built and flashed first
+add_dependencies(ipc_service remote)
+# Place remote image first in the image list
+set(IMAGES "remote" ${IMAGES})
