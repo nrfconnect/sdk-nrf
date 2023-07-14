6d6c4c157f216af969d3a314e6a1bc11d5852f84
srinivasa raju chintalapati <srinivasaraju.chintalapati@nordicsemi.no>
2023-07-14 13:11:49 +0530

    drivers: wifi: NRF style Doxygen comments
    Added NRF style doxygen comments for RPU interface files.
    
    Signed-off-by: srinivasa raju chintalapati <srinivasaraju.chintalapati@nordicsemi.no>

--- drivers/wifi/nrf700x/osal/fw_if/umac_if/inc/fw/host_rpu_common_if.h
+++ drivers/wifi/nrf700x/osal/fw_if/umac_if/inc/fw/host_rpu_common_if.h
@@ -6,8 +6,9 @@
  */
 
 /**
+ * @file
+ * @brief Common interface between host and RPU
  *
- *@brief <Common interface between host and RPU>
  */
 #ifndef __NRF_WIFI_HOST_RPU_COMMON_IFACE_H__
 #define __NRF_WIFI_HOST_RPU_COMMON_IFACE_H__
@@ -36,36 +37,33 @@
 #define NRF_WIFI_UMAC_BOOT_EXCP_VECT_3 0x00000000
 
 /**
- * enum nrf_wifi_host_rpu_msg_type - RPU message type
- * @NRF_WIFI_HOST_RPU_MSG_TYPE_SYSTEM: Unused
- * @NRF_WIFI_HOST_RPU_MSG_TYPE_SUPPLICANT: Unused
- * @NRF_WIFI_HOST_RPU_MSG_TYPE_DATA: Data path and System messages
- * @NRF_WIFI_HOST_RPU_MSG_TYPE_UMAC: Control path messages
+ * @brief This enum defines the different categories of messages that can be exchanged between
+ *  the Host and the RPU.
  *
- * Different categories of messages that can passed between the Host and
- * the RPU.
  */
 enum nrf_wifi_host_rpu_msg_type {
-/* INIT command may send using system type */
+	/** System interface messages */
 	NRF_WIFI_HOST_RPU_MSG_TYPE_SYSTEM,
+	/** Unused */
 	NRF_WIFI_HOST_RPU_MSG_TYPE_SUPPLICANT,
+	/** Data path messages */
 	NRF_WIFI_HOST_RPU_MSG_TYPE_DATA,
+	/** Control path messages */
 	NRF_WIFI_HOST_RPU_MSG_TYPE_UMAC
 };
 /**
- * struct host_rpu_msg - Message header for HOST-RPU interaction
- * @hdr: Message header
- * @type: Type of the RPU message
- * @msg: Actual message
+ * @brief This structure defines the common message header used to encapsulate each message
+ *  exchanged between the Host and UMAC.
  *
- * The common message header that encapsulates each message passed between the
- * Host and UMAC.
  */
 
 struct host_rpu_msg {
+	/** Header */
 	struct host_rpu_msg_hdr hdr;
+	/** Type of the RPU message see &enum nrf_wifi_host_rpu_msg_type */
 	signed int type;
-	signed char msg[0]; /* actual message */
+	/** Actual message */
+	signed char msg[0];
 } __NRF_WIFI_PKD;
 
 #define NRF_WIFI_PENDING_FRAMES_BITMAP_AC_VO (1 << 0)
@@ -74,30 +72,50 @@ struct host_rpu_msg {
 #define NRF_WIFI_PENDING_FRAMES_BITMAP_AC_BK (1 << 3)
 
 /**
- * struct sta_pend_frames_bitmap - STA pending frames bitmap in SoftAP power save mode.
- * @mac_addr: STA MAC address
- * @pend_frames_bitmap: Pending frames bitmap for each access category
+ * @brief This structure represents the bitmap of STA (Station) pending frames in
+ *  SoftAP power save mode.
+ *
  */
 
 struct sap_pend_frames_bitmap {
+	/** STA MAC address */
 	unsigned char mac_addr[6];
+	/** Pending frames bitmap for each access category */
 	unsigned char pend_frames_bitmap;
 } __NRF_WIFI_PKD;
 
+/**
+ * @brief This structure represents the information related to UMAC.
+ *
+ */
 struct host_rpu_umac_info {
+	/** Boot status signature */
 	unsigned int boot_status;
+	/** UMAC version */
 	unsigned int version;
+	/** @ref sap_pend_frames_bitmap */
 	struct sap_pend_frames_bitmap sap_bitmap[4];
+	/** Hardware queues info &enum host_rpu_hpqm_info */
 	struct host_rpu_hpqm_info hpqm_info;
+	/** OTP params */
 	unsigned int info_part;
+	/** OTP params */
 	unsigned int info_variant;
+	/** OTP params */
 	unsigned int info_lromversion;
+	/** OTP params */
 	unsigned int info_uromversion;
+	/** OTP params */
 	unsigned int info_uuid[4];
+	/** OTP params */
 	unsigned int info_spare0;
+	/** OTP params */
 	unsigned int info_spare1;
+	/** OTP params */
 	unsigned int mac_address0[2];
+	/** OTP params */
 	unsigned int mac_address1[2];
+	/** OTP params */
 	unsigned int calib[9];
 } __NRF_WIFI_PKD;
 #endif /* __NRF_WIFI_HOST_RPU_IFACE_H__ */
