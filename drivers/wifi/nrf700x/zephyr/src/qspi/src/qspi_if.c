eeef035be2be485a2079ba71d8a9b278258486bf
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2023-07-19 19:05:42 +0530

    drivers: wifi: Fix QSPI encryption regression
    When low-power mode support for QSPI was added, it broke QSPI encryption
    because the QSPI device will be uninitialized after the use, so, the
    encrypt fails, add init/deinit to the encryption.
    
    Fixes SHEL-1765.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- drivers/wifi/nrf700x/zephyr/src/qspi/src/qspi_if.c
+++ drivers/wifi/nrf700x/zephyr/src/qspi/src/qspi_if.c
@@ -1303,6 +1303,13 @@ int qspi_enable_encryption(uint8_t *key)
 	if (qspi_config->encryption)
 		return -EALREADY;
 
+	int ret = qspi_device_init(&qspi_perip);
+
+	if (ret != 0) {
+		LOG_ERR("qspi_device_init failed: %d\n", ret);
+		return -EIO;
+	}
+
 	memcpy(qspi_config->p_cfg.key, key, 16);
 
 	err = nrfx_qspi_dma_encrypt(&qspi_config->p_cfg);
@@ -1318,6 +1325,8 @@ int qspi_enable_encryption(uint8_t *key)
 	}
 
 	qspi_config->encryption = true;
+
+	qspi_device_uninit(&qspi_perip);
 
 	return 0;
 #else
