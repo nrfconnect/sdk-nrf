1142e900a7e54793ccdddf9816eaf06a11833acc
Veli-Tapani Peltoketo <veli-tapani.peltoketo@nordicsemi.no>
2023-07-27 10:34:27 +0300

    applications: serial_lte_modem: Full modem fota support for SLM
    - new user interface parameter for #XFOTA AT command
    - full modem fota is using an external flash
    - full fota is activated via overlay
    
    Signed-off-by: Veli-Tapani Peltoketo <veli-tapani.peltoketo@nordicsemi.no>

--- applications/serial_lte_modem/src/slm_at_fota.c
+++ applications/serial_lte_modem/src/slm_at_fota.c
@@ -9,9 +9,15 @@
 #include <nrf_modem_delta_dfu.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/logging/log_ctrl.h>
 #include <zephyr/net/tls_credentials.h>
 #include <zephyr/net/http/parser_url.h>
+#include <zephyr/device.h>
+#include <zephyr/storage/stream_flash.h>
+#include <zephyr/sys/reboot.h>
 #include <net/fota_download.h>
+#include <dfu/dfu_target_full_modem.h>
+#include <dfu/fmfu_fdev.h>
 #include "slm_util.h"
 #include "slm_settings.h"
 #include "slm_at_host.h"
@@ -26,7 +32,7 @@ LOG_MODULE_REGISTER(slm_fota, CONFIG_SLM_LOG_LEVEL);
 #define SCHEMA_HTTPS	"https"
 #define URI_HOST_MAX	CONFIG_DOWNLOAD_CLIENT_MAX_HOSTNAME_SIZE
 #define URI_SCHEMA_MAX	8
-#define ERASE_WAIT_TIME 20
+#define ERASE_WAIT_TIME 30
 #define ERASE_POLL_TIME 2
 
 /* Some features need fota_download update */
@@ -36,6 +42,7 @@ enum slm_fota_operation {
 	SLM_FOTA_STOP = 0,
 	SLM_FOTA_START_APP = 1,
 	SLM_FOTA_START_MFW = 2,
+	SLM_FOTA_START_FULL_FOTA = 3,
 	SLM_FOTA_PAUSE_RESUME = 4,
 	SLM_FOTA_MFW_READ = 7,
 	SLM_FOTA_ERASE_MFW = 9
@@ -43,6 +50,20 @@ enum slm_fota_operation {
 
 static char path[FILE_URI_MAX];
 static char hostname[URI_HOST_MAX];
+
+#if defined(CONFIG_SLM_FULL_FOTA)
+/* Buffer used as temporary storage when downloading the modem firmware.
+ */
+#define FMFU_BUF_SIZE	32
+static uint8_t fmfu_buf[FMFU_BUF_SIZE];
+
+/* External flash device for full modem firmware storage */
+static const struct device *flash_dev = DEVICE_DT_GET_ONE(jedec_spi_nor);
+
+/* dfu_target specific configurations */
+static struct dfu_target_fmfu_fdev fdev;
+static struct dfu_target_full_modem_params full_modem_fota_params;
+#endif
 
 static int do_fota_mfw_read(void)
 {
@@ -274,9 +295,15 @@ int handle_at_fota(enum at_cmd_type cmd_type)
 		if (err < 0) {
 			return err;
 		}
-		if (op == SLM_FOTA_STOP) {
+		switch (op) {
+		case SLM_FOTA_STOP:
 			err = fota_download_cancel();
-		} else if (op == SLM_FOTA_START_APP || op == SLM_FOTA_START_MFW) {
+			break;
+		case SLM_FOTA_START_APP:
+		case SLM_FOTA_START_MFW:
+#if defined(CONFIG_SLM_FULL_FOTA)
+		case SLM_FOTA_START_FULL_FOTA:
+#endif
 			char uri[FILE_URI_MAX];
 			uint16_t pdn_id;
 			int size = FILE_URI_MAX;
@@ -292,7 +319,29 @@ int handle_at_fota(enum at_cmd_type cmd_type)
 			}
 			if (op == SLM_FOTA_START_APP) {
 				type = DFU_TARGET_IMAGE_TYPE_MCUBOOT;
-			} else {
+			}
+#if defined(CONFIG_SLM_FULL_FOTA)
+			else if (op == SLM_FOTA_START_FULL_FOTA)  {
+				fdev.dev = flash_dev;
+				full_modem_fota_params.buf = fmfu_buf;
+				full_modem_fota_params.len = sizeof(fmfu_buf);
+				full_modem_fota_params.dev = &fdev;
+
+				if (!device_is_ready(flash_dev)) {
+					LOG_ERR("Flash device %s not ready\n", flash_dev->name);
+					return -ENXIO;
+				}
+
+				err = dfu_target_full_modem_cfg(&full_modem_fota_params);
+				if (err != 0 && err != -EALREADY) {
+					LOG_ERR("dfu_target_full_modem_cfg failed: %d\n", err);
+					return err;
+				}
+
+				type = DFU_TARGET_IMAGE_TYPE_FULL_MODEM;
+			}
+#endif
+			else {
 				type = DFU_TARGET_IMAGE_TYPE_MODEM_DELTA;
 			}
 			if (at_params_valid_count_get(&slm_at_param_list) > 4) {
@@ -301,8 +350,9 @@ int handle_at_fota(enum at_cmd_type cmd_type)
 			} else {
 				err = do_fota_start(op, uri, sec_tag, 0, type);
 			}
+			break;
 #if FOTA_FUTURE_FEATURE
-		} else if (op == SLM_FOTA_PAUSE_RESUME) {
+		case SLM_FOTA_PAUSE_RESUME:
 			if (paused) {
 				fota_download_resume();
 				paused = false;
@@ -311,19 +361,30 @@ int handle_at_fota(enum at_cmd_type cmd_type)
 				paused = true;
 			}
 			err = 0;
+			break;
 #endif
-		} else if (op == SLM_FOTA_MFW_READ) {
+		case SLM_FOTA_MFW_READ:
 			err = do_fota_mfw_read();
-		} else if (op == SLM_FOTA_ERASE_MFW) {
+			break;
+		case SLM_FOTA_ERASE_MFW:
 			err = do_fota_erase_mfw();
-		} else {
+			break;
+		default:
 			err = -EINVAL;
-		} break;
+			break;
+		}
+		break;
 
 	case AT_CMD_TYPE_TEST_COMMAND:
+#if defined(CONFIG_SLM_FULL_FOTA)
+		rsp_send("\r\n#XFOTA: (%d,%d,%d,%d,%d,%d)[,<file_url>[,<sec_tag>[,<pdn_id>]]]\r\n",
+			SLM_FOTA_STOP, SLM_FOTA_START_APP, SLM_FOTA_START_MFW,
+			SLM_FOTA_MFW_READ, SLM_FOTA_ERASE_MFW, SLM_FOTA_START_FULL_FOTA);
+#else
 		rsp_send("\r\n#XFOTA: (%d,%d,%d,%d,%d)[,<file_url>[,<sec_tag>[,<pdn_id>]]]\r\n",
 			SLM_FOTA_STOP, SLM_FOTA_START_APP, SLM_FOTA_START_MFW,
 			SLM_FOTA_MFW_READ, SLM_FOTA_ERASE_MFW);
+#endif
 		err = 0;
 		break;
 
@@ -368,12 +429,100 @@ void slm_fota_post_process(void)
 	}
 }
 
+#if defined(CONFIG_SLM_FULL_FOTA)
+static void handle_full_fota_activation_fail(int ret)
+{
+	int err;
+	/* All errors during the new modem firmware activation are
+	 * considered irrecoverable and a reboot is needed.
+	 */
+	LOG_ERR("Modem firmware activation failed, error: %d", ret);
+	slm_fota_stage = FOTA_STAGE_COMPLETE;
+	slm_fota_status = FOTA_STATUS_ERROR;
+	slm_fota_info = ret;
+	slm_settings_fota_save();
+
+	/* Extenal flash needs to be erased and internal counters cleared */
+	err = dfu_target_reset();
+	if (err != 0)
+		LOG_ERR("dfu_target_reset() failed: %d\n", err);
+	else
+		LOG_INF("External flash erase succeeded");
+
+	/* slm_fota_post_process is executed after the reboot and an error is sent via AT reply */
+	LOG_WRN("Rebooting...");
+	LOG_PANIC();
+	sys_reboot(SYS_REBOOT_COLD);
+}
+#endif
+
 void slm_finish_modem_fota(int modem_lib_init_ret)
 {
-	if (handle_nrf_modem_lib_init_ret(modem_lib_init_ret)) {
+#if defined(CONFIG_SLM_FULL_FOTA)
+	int err;
 
-		LOG_INF("Re-initializing the modem due to"
-				" ongoing modem firmware update.");
+	if (slm_fota_type == DFU_TARGET_IMAGE_TYPE_FULL_MODEM) {
+		/* All erroneous steps in activation stage are fatal. In this case we cannot
+		 * activate the new firmware.
+		 */
+		slm_fota_status = FOTA_STATUS_ERROR;
+
+		/* Full fota activation differs from delta modem fota. */
+		LOG_INF("Applying full modem firmware update from external flash\n");
+
+		err = nrf_modem_lib_shutdown();
+		if (err != 0) {
+			LOG_ERR("nrf_modem_lib_shutdown() failed: %d\n", err);
+			/* The function will make a reboot. */
+			handle_full_fota_activation_fail(err);
+		}
+
+		err = nrf_modem_lib_bootloader_init();
+		if (err != 0) {
+			LOG_ERR("nrf_modem_lib_bootloader_init() failed: %d\n", err);
+			/* The function will make a reboot. */
+			handle_full_fota_activation_fail(err);
+		}
+
+		/* Loading data from external flash to modem's flash. */
+		err = fmfu_fdev_load(fmfu_buf, sizeof(fmfu_buf), flash_dev, 0);
+		if (err != 0) {
+			LOG_ERR("fmfu_fdev_load failed: %d\n", err);
+			/* The function will make a reboot. */
+			handle_full_fota_activation_fail(err);
+		}
+
+		err = nrf_modem_lib_shutdown();
+		if (err != 0) {
+			LOG_ERR("nrf_modem_lib_shutdown() failed: %d\n", err);
+			/* The function will make a reboot. */
+			handle_full_fota_activation_fail(err);
+		}
+
+		err = nrf_modem_lib_init();
+		if (err != 0) {
+			LOG_ERR("nrf_modem_lib_init() failed: %d\n", err);
+			/* The function will make a reboot. */
+			handle_full_fota_activation_fail(err);
+		}
+
+		slm_fota_stage = FOTA_STAGE_COMPLETE;
+		slm_fota_status = FOTA_STATUS_OK;
+		slm_fota_info = 0;
+		LOG_INF("Full modem firmware update succeeded. Will run new firmware");
+
+		/* Extenal flash needs to be erased and internal counters cleared */
+		err = dfu_target_reset();
+		if (err != 0)
+			LOG_ERR("dfu_target_reset() failed: %d\n", err);
+		else
+			LOG_INF("External flash erase succeeded");
+	}
+#endif
+	if (slm_fota_type == DFU_TARGET_IMAGE_TYPE_MODEM_DELTA &&
+	    handle_nrf_modem_lib_init_ret(modem_lib_init_ret)) {
+
+		LOG_INF("Re-initializing the modem due to ongoing delta modem firmware update.");
 
 		/* The second init needs to be done regardless of the return value.
 		 * Refer to the below link for more information on the procedure.
