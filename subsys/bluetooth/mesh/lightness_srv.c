2718971fb513e8dfb14da3542b3472a945270dd5
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2023-07-27 17:31:27 +0200

    Bluetooth: Mesh: Prevent dim to off behavior
    According to E22980, section 6.1.2.2.2 of MshMDLd1.1, Delta Set and Move
    Set can never dim the lightness value to zero.
    
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

--- subsys/bluetooth/mesh/lightness_srv.c
+++ subsys/bluetooth/mesh/lightness_srv.c
@@ -588,12 +588,11 @@ static void lvl_delta_set(struct bt_mesh_lvl_srv *lvl_srv,
 		srv->delta_start = to_actual(status.current);
 	}
 
-	/* Clamp the value to the lightness range before storing it in an
-	 * unsigned 16 bit value, as this would overflow if the target is beyond
-	 * its storage limits, causing invalid values.
+	/* Clamp the value to the lightness range to prevent it be moved back to zero in binding
+	 * with Generic Level state.
 	 */
 	target_actual = CLAMP(srv->delta_start + delta_set->delta,
-			      BT_MESH_LIGHTNESS_MIN, BT_MESH_LIGHTNESS_MAX);
+			      srv->range.min, srv->range.max);
 
 	struct bt_mesh_lightness_set set = {
 		/* Converting back to configured space: */
@@ -606,21 +605,6 @@ static void lvl_delta_set(struct bt_mesh_lvl_srv *lvl_srv,
 	 */
 	lightness_srv_disable_control(srv);
 	lightness_srv_change_lvl(srv, ctx, &set, &status, true);
-
-	/* Override the "last" value if this is dimmed all the way to off. Then
-	 * set the "last" value to the value it was before the dimming was
-	 * started.
-	 */
-	if ((set.lvl == 0) && (from_actual(srv->delta_start) != 0) &&
-	    (from_actual(srv->delta_start) != srv->transient.last)) {
-		/* Recalulate it back to light state when overriding the "last"
-		 * value.
-		 */
-		srv->transient.last = from_actual(srv->delta_start);
-		if (!IS_ENABLED(CONFIG_EMDS)) {
-			store_state(srv);
-		}
-	}
 
 	if (rsp) {
 		rsp->current = LIGHT_TO_LVL(status.current);
