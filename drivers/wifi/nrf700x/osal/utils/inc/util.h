e2ccf640b6d641a3db1c86be95b86befbe8317a4
Sachin D Kulkarni <sachin.kulkarni@nordicsemi.no>
2023-07-27 18:43:09 +0530

    drivers: wifi: Add scan extension for specifying channels
    Add scan extension to scan only on specific channels. Only 20 MHz
    channels are supported for now.
    
    Signed-off-by: Sachin D Kulkarni <sachin.kulkarni@nordicsemi.no>

--- drivers/wifi/nrf700x/osal/utils/inc/util.h
+++ drivers/wifi/nrf700x/osal/utils/inc/util.h
@@ -15,6 +15,11 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "osal_api.h"
+#include "host_rpu_umac_if.h"
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(array) (sizeof(array) / sizeof((array)[0]))
+#endif
 
 /* Convert power from mBm to dBm */
 #define MBM_TO_DBM(gain) ((gain) / 100)
@@ -25,4 +30,8 @@ int nrf_wifi_utils_hex_str_to_val(struct wifi_nrf_osal_priv *opriv,
 				  unsigned char *str);
 
 bool nrf_wifi_utils_is_mac_addr_valid(const char *mac_addr);
+
+int nrf_wifi_utils_chan_to_freq(struct wifi_nrf_osal_priv *opriv,
+				enum nrf_wifi_band band,
+				unsigned short chan);
 #endif /* __UTIL_H__ */
