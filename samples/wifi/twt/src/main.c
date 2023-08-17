1ac9591ea0e11892f43ad34df20ac89ef2d879fd
Chiranjeevi Srikakulapu <chiranjeevi.srikakulapu@nordicsemi.no>
2023-08-17 15:23:34 +0530

    samples: wifi: Add TWT sample
    This is based on STA sample, for now it only does Wi-Fi connection.
    Also, update changelog and add a codeowners entry.
    
    Signed-off-by: Chiranjeevi Srikakulapu <chiranjeevi.srikakulapu@nordicsemi.no>

---
+++ samples/wifi/twt/src/main.c
@@ -0,0 +1,269 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ * @brief WiFi TWT sample
+ */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(twt, CONFIG_LOG_DEFAULT_LEVEL);
+
+#include <nrfx_clock.h>
+#include <zephyr/kernel.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <zephyr/shell/shell.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/init.h>
+
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/net_event.h>
+#include <zephyr/drivers/gpio.h>
+
+#include "net_private.h"
+
+#define WIFI_SHELL_MODULE "wifi"
+
+#define WIFI_SHELL_MGMT_EVENTS (NET_EVENT_WIFI_CONNECT_RESULT |		\
+				NET_EVENT_WIFI_DISCONNECT_RESULT)
+
+#define MAX_SSID_LEN        32
+#define DHCP_TIMEOUT        70
+#define CONNECTION_TIMEOUT  100
+#define STATUS_POLLING_MS   300
+
+static struct net_mgmt_event_callback wifi_shell_mgmt_cb;
+static struct net_mgmt_event_callback net_shell_mgmt_cb;
+
+static struct {
+	const struct shell *sh;
+	union {
+		struct {
+			uint8_t connected	: 1;
+			uint8_t connect_result	: 1;
+			uint8_t disconnect_requested	: 1;
+			uint8_t _unused		: 5;
+		};
+		uint8_t all;
+	};
+} context;
+
+static int cmd_wifi_status(void)
+{
+	struct net_if *iface = net_if_get_default();
+	struct wifi_iface_status status = { 0 };
+	int ret;
+
+	ret = net_mgmt(NET_REQUEST_WIFI_IFACE_STATUS, iface, &status,
+				sizeof(struct wifi_iface_status));
+	if (ret) {
+		LOG_INF("Status request failed: %d\n", ret);
+		return ret;
+	}
+
+	LOG_INF("==================");
+	LOG_INF("State: %s", wifi_state_txt(status.state));
+
+	if (status.state >= WIFI_STATE_ASSOCIATED) {
+		uint8_t mac_string_buf[sizeof("xx:xx:xx:xx:xx:xx")];
+
+		LOG_INF("Interface Mode: %s",
+		       wifi_mode_txt(status.iface_mode));
+		LOG_INF("Link Mode: %s",
+		       wifi_link_mode_txt(status.link_mode));
+		LOG_INF("SSID: %-32s", status.ssid);
+		LOG_INF("BSSID: %s",
+		       net_sprint_ll_addr_buf(
+				status.bssid, WIFI_MAC_ADDR_LEN,
+				mac_string_buf, sizeof(mac_string_buf)));
+		LOG_INF("Band: %s", wifi_band_txt(status.band));
+		LOG_INF("Channel: %d", status.channel);
+		LOG_INF("Security: %s", wifi_security_txt(status.security));
+		LOG_INF("MFP: %s", wifi_mfp_txt(status.mfp));
+		LOG_INF("RSSI: %d", status.rssi);
+		LOG_INF("TWT: %s", status.twt_capable ? "Supported" : "Not supported");
+	}
+	return 0;
+}
+
+static void handle_wifi_connect_result(struct net_mgmt_event_callback *cb)
+{
+	const struct wifi_status *status =
+		(const struct wifi_status *) cb->info;
+
+	if (context.connected) {
+		return;
+	}
+
+	if (status->status) {
+		LOG_ERR("Connection failed (%d)", status->status);
+	} else {
+		LOG_INF("Connected");
+		context.connected = true;
+	}
+
+	context.connect_result = true;
+}
+
+static void handle_wifi_disconnect_result(struct net_mgmt_event_callback *cb)
+{
+	const struct wifi_status *status =
+		(const struct wifi_status *) cb->info;
+
+	if (!context.connected) {
+		return;
+	}
+
+	if (context.disconnect_requested) {
+		LOG_INF("Disconnection request %s (%d)",
+			 status->status ? "failed" : "done",
+					status->status);
+		context.disconnect_requested = false;
+	} else {
+		LOG_INF("Received Disconnected");
+		context.connected = false;
+	}
+
+	cmd_wifi_status();
+}
+
+static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb,
+				     uint32_t mgmt_event, struct net_if *iface)
+{
+	switch (mgmt_event) {
+	case NET_EVENT_WIFI_CONNECT_RESULT:
+		handle_wifi_connect_result(cb);
+		break;
+	case NET_EVENT_WIFI_DISCONNECT_RESULT:
+		handle_wifi_disconnect_result(cb);
+		break;
+	default:
+		break;
+	}
+}
+
+static void print_dhcp_ip(struct net_mgmt_event_callback *cb)
+{
+	/* Get DHCP info from struct net_if_dhcpv4 and print */
+	const struct net_if_dhcpv4 *dhcpv4 = cb->info;
+	const struct in_addr *addr = &dhcpv4->requested_ip;
+	char dhcp_info[128];
+
+	net_addr_ntop(AF_INET, addr, dhcp_info, sizeof(dhcp_info));
+
+	LOG_INF("DHCP IP address: %s", dhcp_info);
+}
+
+static void net_mgmt_event_handler(struct net_mgmt_event_callback *cb,
+				    uint32_t mgmt_event, struct net_if *iface)
+{
+	switch (mgmt_event) {
+	case NET_EVENT_IPV4_DHCP_BOUND:
+		print_dhcp_ip(cb);
+		break;
+	default:
+		break;
+	}
+}
+
+static int __wifi_args_to_params(struct wifi_connect_req_params *params)
+{
+	params->timeout = SYS_FOREVER_MS;
+
+	/* SSID */
+	params->ssid = CONFIG_TWT_SAMPLE_SSID;
+	params->ssid_length = strlen(params->ssid);
+
+#if defined(CONFIG_TWT_STA_KEY_MGMT_WPA2)
+	params->security = 1;
+#elif defined(CONFIG_TWT_STA_KEY_MGMT_WPA2_256)
+	params->security = 2;
+#elif defined(CONFIG_TWT_STA_KEY_MGMT_WPA3)
+	params->security = 3;
+#else
+	params->security = 0;
+#endif
+
+#if !defined(CONFIG_TWT_STA_KEY_MGMT_NONE)
+	params->psk = CONFIG_TWT_SAMPLE_PASSWORD;
+	params->psk_length = strlen(params->psk);
+#endif
+	params->channel = WIFI_CHANNEL_ANY;
+
+	/* MFP (optional) */
+	params->mfp = WIFI_MFP_OPTIONAL;
+
+	return 0;
+}
+
+static int wifi_connect(void)
+{
+	struct net_if *iface = net_if_get_default();
+	static struct wifi_connect_req_params cnx_params;
+
+	context.connected = false;
+	context.connect_result = false;
+	__wifi_args_to_params(&cnx_params);
+
+	if (net_mgmt(NET_REQUEST_WIFI_CONNECT, iface,
+		     &cnx_params, sizeof(struct wifi_connect_req_params))) {
+		LOG_ERR("Connection request failed");
+
+		return -ENOEXEC;
+	}
+
+	LOG_INF("Connection requested");
+
+	return 0;
+}
+
+int main(void)
+{
+	int i;
+
+	memset(&context, 0, sizeof(context));
+
+	net_mgmt_init_event_callback(&wifi_shell_mgmt_cb,
+				     wifi_mgmt_event_handler,
+				     WIFI_SHELL_MGMT_EVENTS);
+
+	net_mgmt_add_event_callback(&wifi_shell_mgmt_cb);
+
+	net_mgmt_init_event_callback(&net_shell_mgmt_cb,
+				     net_mgmt_event_handler,
+				     NET_EVENT_IPV4_DHCP_BOUND);
+
+	net_mgmt_add_event_callback(&net_shell_mgmt_cb);
+
+	LOG_INF("Starting %s with CPU frequency: %d MHz", CONFIG_BOARD, SystemCoreClock/MHZ(1));
+	k_sleep(K_SECONDS(1));
+
+	LOG_INF("Static IP address (overridable): %s/%s -> %s",
+		CONFIG_NET_CONFIG_MY_IPV4_ADDR,
+		CONFIG_NET_CONFIG_MY_IPV4_NETMASK,
+		CONFIG_NET_CONFIG_MY_IPV4_GW);
+
+	while (1) {
+
+		wifi_connect();
+
+		for (i = 0; i < CONNECTION_TIMEOUT; i++) {
+			k_sleep(K_MSEC(STATUS_POLLING_MS));
+			cmd_wifi_status();
+			if (context.connect_result) {
+				break;
+			}
+		}
+		if (context.connected) {
+			k_sleep(K_FOREVER);
+		} else if (!context.connect_result) {
+			LOG_ERR("Connection Timed Out");
+		}
+	}
+
+	return 0;
+}
