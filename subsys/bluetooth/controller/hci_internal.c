b951e84bd391dad87e7052c4465baf045c7a2268
Kyra Lengfeld <kyra.lengfeld@nordicsemi.no>
2023-08-18 13:32:59 +0200

    Bluetooth: Controller: add hci_le_transmitter_test_v4
    When selecting ICS to qualify Le Power Control the
    LE Transmitter Test command [v4] is mandatory.
    
    Signed-off-by: Kyra Lengfeld <kyra.lengfeld@nordicsemi.no>

--- subsys/bluetooth/controller/hci_internal.c
+++ subsys/bluetooth/controller/hci_internal.c
@@ -413,6 +413,10 @@ void hci_internal_supported_commands(sdc_hci_ip_supported_commands_t *cmds)
 	cmds->hci_le_enhanced_read_transmit_power_level = 1;
 	cmds->hci_le_read_remote_transmit_power_level = 1;
 	cmds->hci_le_set_transmit_power_reporting_enable = 1;
+	/* NOTE: The DTM commands are *not* supported by the SoftDevice
+	 * controller. See doc/nrf/known_issues.rst.
+	 */
+	cmds->hci_le_transmitter_test_v4 = 1;
 #endif
 
 #if defined(CONFIG_BT_CTLR_LE_POWER_CONTROL) || defined(CONFIG_BT_CTLR_ADV_EXT)
