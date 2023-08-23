a08ccbaba393f4a46b29fe0ed8e8baef2ca6627c
Markus Swarowsky <markus.swarowsky@nordicsemi.no>
2023-08-23 17:49:21 +0200

    oberon: Fix re-define of PSA_VENDOR_ECC_MAX_CURVE_BITS
    PSA_VENDOR_ECC_MAX_CURVE_BITS gets in already defined in the
    autogenerated nrf-config-user.h and if these defines don't match
    it results in a redefined warning
    
    Ref: NCSDK-21666
    
    Signed-off-by: Markus Swarowsky <markus.swarowsky@nordicsemi.no>

--- ext/oberon/psa/core/include/psa/crypto_sizes.h
+++ ext/oberon/psa/core/include/psa/crypto_sizes.h
@@ -196,6 +196,7 @@
 
 /* The maximum size of an ECC key on this implementation, in bits.
  * This is a vendor-specific macro. */
+#ifndef PSA_VENDOR_ECC_MAX_CURVE_BITS
 #if defined(PSA_WANT_ECC_SECP_R1_521)           /*!!OM*/
 #define PSA_VENDOR_ECC_MAX_CURVE_BITS 521
 #elif defined(PSA_WANT_ECC_BRAINPOOL_P_R1_512)
@@ -228,6 +229,7 @@
 #define PSA_VENDOR_ECC_MAX_CURVE_BITS 192
 #else
 #define PSA_VENDOR_ECC_MAX_CURVE_BITS 0
+#endif
 #endif
 
 /** This macro returns the maximum supported length of the PSK for the
