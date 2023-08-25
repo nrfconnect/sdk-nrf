ba31521e6d3fb08670ea65f5ffb7a9a030c3b359
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-08-25 12:51:32 +0200

    tfm: Log missing keys during provisioning
    TF-M will panic the system if the provisioning step returns an error
    code. Running a sample or application that has enabled the proviosing
    feature will crash if the device is not correctly provisioned.
    Log this error codition to provide better failure information.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/trusted-firmware-m/tfm_boards/common/nrf_provisioning.c
+++ modules/trusted-firmware-m/tfm_boards/common/nrf_provisioning.c
@@ -69,12 +69,16 @@ enum tfm_plat_err_t tfm_plat_provisioning_perform(void)
 
 	/* The Hardware Unique Keys should be already written */
 	if (!hw_unique_key_are_any_written()) {
+		SPMLOG_ERRMSG(
+			"This device has not been provisioned with Hardware Unique Keys.");
 		return TFM_PLAT_ERR_SYSTEM_ERR;
 	}
 
 #ifdef TFM_PARTITION_INITIAL_ATTESTATION
 	/* The Initial Attestation key should be already written */
 	if (!identity_key_is_written()) {
+		SPMLOG_ERRMSG(
+			"This device has not been provisioned with an Initial Attestation Key.");
 		return TFM_PLAT_ERR_SYSTEM_ERR;
 	}
 #endif
