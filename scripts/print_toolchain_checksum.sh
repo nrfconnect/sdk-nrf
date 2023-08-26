076beeb7158256fa5240acf3deecb24ae71a9276
Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>
2023-08-26 16:04:34 +0200

    tools: toolchain_checksum to be identical on unix and windows
    Fixes an issue where the toolchain_checksum script returned different
    values depending on if the files were stored with unix or windows line
    endings.
    
    Solved by always removing carriage return characters before computing
    the sha256sum.
    
    Signed-off-by: Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>

--- scripts/print_toolchain_checksum.sh
+++ scripts/print_toolchain_checksum.sh
@@ -3,6 +3,6 @@
 BASEDIR=$(dirname "$0")
 REQUIREMENTS=$BASEDIR/requirements-fixed.txt
 TOOLS_VERSIONS=$BASEDIR/tools-versions-linux.yml
-TOOLCHAIN_VERSION=$(cat $REQUIREMENTS $TOOLS_VERSIONS | sha256sum | head -c 10)
+TOOLCHAIN_VERSION=$(cat $REQUIREMENTS $TOOLS_VERSIONS | tr -d '\r' | sha256sum | head -c 10)
 
 echo "${TOOLCHAIN_VERSION}"
