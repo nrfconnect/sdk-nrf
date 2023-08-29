931dc8da736e5a91a856aa899ddf5cb2d6c6e46b
Markus Swarowsky <markus.swarowsky@nordicsemi.no>
2023-08-29 15:41:38 +0200

    nrf_security: Rename tfm_builtin_key_loader_get_builtin_key
    The function name change to tfm_builtin_key_loader_get_builtin_key in
    TF-M 1.8 to tfm_builtin_key_loader_get_builtin_key to updating it in
    nrf_security as well.
    
    Ref: NCSDK-22275
    Signed-off-by: Markus Swarowsky <markus.swarowsky@nordicsemi.no>

--- subsys/nrf_security/src/psa_crypto_driver_wrappers.c
+++ subsys/nrf_security/src/psa_crypto_driver_wrappers.c
@@ -646,7 +646,7 @@ psa_status_t psa_driver_wrapper_get_builtin_key(psa_drv_slot_number_t slot_numbe
 	switch (location) {
 #if defined(PSA_CRYPTO_DRIVER_TFM_BUILTIN_KEY_LOADER)
 	case TFM_BUILTIN_KEY_LOADER_KEY_LOCATION:
-		return tfm_builtin_key_loader_get_key_buffer(slot_number, attributes, key_buffer,
+		return tfm_builtin_key_loader_get_builtin_key(slot_number, attributes, key_buffer,
 							     key_buffer_size, key_buffer_length);
 #endif /* PSA_CRYPTO_DRIVER_TFM_BUILTIN_KEY_LOADER */
 	default:
