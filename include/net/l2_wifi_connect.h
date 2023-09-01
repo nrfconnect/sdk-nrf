172a289aadc2fb6c0273d405172ca781cfc6444c
Dominik Ermel <dominik.ermel@nordicsemi.no>
2023-09-01 13:10:42 +0000

    net/l2_wifi: Use conn_mgr_connectivity_impl.h
    The conn_mgr_connectivity.h has been renamed to
    conn_mgr_connectivity_impl.h.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- include/net/l2_wifi_connect.h
+++ include/net/l2_wifi_connect.h
@@ -7,7 +7,7 @@
 #ifndef L2_WIFI_CONNECT_H__
 #define L2_WIFI_CONNECT_H__
 
-#include <zephyr/net/conn_mgr_connectivity.h>
+#include <zephyr/net/conn_mgr_connectivity_impl.h>
 
 #define L2_CONN_WLAN0_CTX_TYPE void *
 CONN_MGR_CONN_DECLARE_PUBLIC(L2_CONN_WLAN0);
