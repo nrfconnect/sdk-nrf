1f0be58dd6bca0f177b83a70d3a87f35d1c40c17
Chiranjeevi Srikakulapu <chiranjeevi.srikakulapu@nordicsemi.no>
2023-09-05 00:25:18 +0530

    samples: wifi: twt: Add traffic gen module
    This module can be used to send traffic by establishing a control
    connection based on TCP and then sending data traffic either TCP or UDP
    and printing results at the end (both client and server).
    
    This gives us explicit control over sending traffic rather than using
    tools like Zperf, the primary user for this will be TWT sample, so, its
    under the sample, but if we find more users then this can be moved to a
    library.
    
    For now only TCP data traffic support is added as its the primary
    usecase.
    
    Signed-off-by: Chiranjeevi Srikakulapu <chiranjeevi.srikakulapu@nordicsemi.no>

---
+++ samples/wifi/twt/modules/traffic_gen/inc/traffic_gen_tcp.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+#ifndef __TRAFFIC_GEN_TCP_H__
+#define __TRAFFIC_GEN_TCP_H__
+
+extern struct traffic_gen_report remote_report;
+
+/* tcp client/server function prototypes */
+int init_tcp_client(struct traffic_gen_config *tg_config);
+int send_tcp_uplink_traffic(struct traffic_gen_config *tg_config);
+int init_tcp_server(struct traffic_gen_config *tg_config);
+int recv_tcp_downlink_traffic(struct traffic_gen_config *tg_config);
+
+#endif /* __TRAFFIC_GEN_TCP_H__ */
