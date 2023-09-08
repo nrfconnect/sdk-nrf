ea9dcc8a76b56118a8b7657f814f982431618963
Sachin D Kulkarni <sachin.kulkarni@nordicsemi.no>
2023-09-08 17:52:25 +0530

    drivers: wifi: Add an API to get factory test version
    The factory test version is needed to perform RF adjustments.
    
    Signed-off-by: Sachin D Kulkarni <sachin.kulkarni@nordicsemi.no>

--- drivers/wifi/nrf700x/osal/fw_if/umac_if/src/fmac_api_common.c
+++ drivers/wifi/nrf700x/osal/fw_if/umac_if/src/fmac_api_common.c
@@ -450,6 +450,7 @@ enum wifi_nrf_status wifi_nrf_fmac_rf_params_get(struct wifi_nrf_fmac_dev_ctx *f
 {
 	enum wifi_nrf_status status = WIFI_NRF_STATUS_FAIL;
 	struct wifi_nrf_fmac_otp_info otp_info;
+	unsigned int ft_prog_ver;
 	int ret = -1;
 
 	if (!fmac_dev_ctx || !rf_params) {
@@ -471,6 +472,16 @@ enum wifi_nrf_status wifi_nrf_fmac_rf_params_get(struct wifi_nrf_fmac_dev_ctx *f
 	if (status != WIFI_NRF_STATUS_SUCCESS) {
 		wifi_nrf_osal_log_err(fmac_dev_ctx->fpriv->opriv,
 				      "%s: Fetching of RPU OTP information failed\n",
+				      __func__);
+		goto out;
+	}
+
+	status = wifi_nrf_hal_otp_ft_prog_ver_get(fmac_dev_ctx->hal_dev_ctx,
+						  &ft_prog_ver);
+
+	if (status != WIFI_NRF_STATUS_SUCCESS) {
+		wifi_nrf_osal_log_err(fmac_dev_ctx->fpriv->opriv,
+				      "%s: Fetching of FT program version failed\n",
 				      __func__);
 		goto out;
 	}
@@ -500,6 +511,8 @@ enum wifi_nrf_status wifi_nrf_fmac_rf_params_get(struct wifi_nrf_fmac_dev_ctx *f
 				      OTP_SZ_CALIB_XO);
 
 	}
+
+	ft_prog_ver = (ft_prog_ver & FT_PROG_VER_MASK) >> 16;
 
 	status = WIFI_NRF_STATUS_SUCCESS;
 out:
