31cd444734b050ba63011b95e67f0d0dcab94664
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-09-28 16:58:15 +0200

    modules: tfm: Update Zephyr and TF-M to support UART pinctrl config
    Update TF-M to support UART pin config from pinctrl.
    Change out-of-tree support to use the device tree information and
    pass it to the TF-M UART driver.
    
    Update TF-M tests to define hw-flow-control, and alternate pin setup
    in devicetree instead of kconfig
    
    Update Zephyr to include:
    - nRF5340 DK flow control pins.
    - Revert noup commits to take upstream commits for generic NS board
    support
    - Include nrf9160 flash layout fixes and BL2 logging fixes.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- modules/tfm/tfm/boards/board/device_cfg.h
+++ modules/tfm/tfm/boards/board/device_cfg.h
@@ -7,27 +7,23 @@
 #ifndef DEVICE_CFG_H__
 #define DEVICE_CFG_H__
 
-/**
- * \file device_cfg.h
- * \brief
- * This is the default device configuration file with all peripherals
- * defined and configured to be use via the secure and/or non-secure base
- * address.
- */
+#include <autoconf.h>
+
 /* ARRAY_SIZE causes a conflict as it is defined both by TF-M and indirectly by devicetree.h */
 #undef ARRAY_SIZE
-#include <autoconf.h>
 #include <zephyr/devicetree.h>
 
-#define TFM_UART uart##NRF_SECURE_UART_INSTANCE
+#if defined(CONFIG_TFM_SECURE_UART0) || DOMAIN_NS == 1U
+#define TFM_UART uart0
+#endif /* defined(CONFIG_TFM_SECURE_UART0) || DOMAIN_NS == 1U */
+
+#if defined(CONFIG_TFM_SECURE_UART1) && DOMAIN_NS != 1U
+#define TFM_UART uart1
+#endif /* defined(CONFIG_TFM_SECURE_UART1) */
 
 #define DEFAULT_UART_BAUDRATE DT_PROP_OR(DT_NODELABEL(TFM_UART), current_speed, 115200)
 
-#if (defined(CONFIG_TFM_SECURE_UART0) || DOMAIN_NS == 1U) && \
-	defined(CONFIG_TFM_UART0_HWFC_ENABLED)
-#define DEFAULT_UART_CONTROL ARM_USART_FLOW_CONTROL_RTS_CTS
-#elif defined(CONFIG_TFM_SECURE_UART1) && \
-	defined(CONFIG_TFM_UART1_HWFC_ENABLED)
+#if DT_PROP(DT_NODELABEL(TFM_UART), hw_flow_control)
 #define DEFAULT_UART_CONTROL ARM_USART_FLOW_CONTROL_RTS_CTS
 #else
 #define DEFAULT_UART_CONTROL 0
