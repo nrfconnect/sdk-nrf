a26ba66b4e9f23413bb113f385c01ae65be4def3
Seppo Takalo <seppo.takalo@nordicsemi.no>
2023-10-03 12:42:03 +0300

    samples: lwm2m_client: Ignore new event
    Ignore new LWM2M_RD_CLIENT_EVENT_DEREGISTER that requires
    nothing from application, just a notify.
    
    Signed-off-by: Seppo Takalo <seppo.takalo@nordicsemi.no>

--- samples/cellular/lwm2m_client/src/main.c
+++ samples/cellular/lwm2m_client/src/main.c
@@ -397,6 +397,7 @@ static void rd_client_event(struct lwm2m_ctx *client, enum lwm2m_rd_client_event
 	}
 
 	switch (client_event) {
+	case LWM2M_RD_CLIENT_EVENT_DEREGISTER:
 	case LWM2M_RD_CLIENT_EVENT_NONE:
 		/* do nothing */
 		k_mutex_unlock(&lte_mutex);
