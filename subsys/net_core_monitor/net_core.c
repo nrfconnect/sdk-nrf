9939469c7fc94d70cf415535614a031dde6376fe
Marcin Jelinski <marcin.jelinski@nordicsemi.no>
2023-10-04 14:10:20 +0200

    net_core_monitor: Make it easy to use
    The `__weak ncm_net_core_event_handler` function is used to notify
    the application about the network core staus.
    This repleace the periodic call of the
    `ncm_net_status_check` function.
    This makes it easier to use and configure.
    
    Ref: NCSDK-23996
    
    Signed-off-by: Marcin Jelinski <marcin.jelinski@nordicsemi.no>

--- subsys/net_core_monitor/net_core.c
+++ subsys/net_core_monitor/net_core.c
@@ -18,12 +18,9 @@ LOG_MODULE_REGISTER(net_core_monitor, CONFIG_NET_CORE_MONITOR_LOG_LEVEL);
 /* General purpose memory IPC of the application core.
  * Access possible only through the memory address.
  */
-#define APP_IPC_GPMEM_0_S  (*(volatile uint32_t *)(0x5002A610))
-#define APP_IPC_GPMEM_0_NS (*(volatile uint32_t *)(0x4002A610))
-#define APP_IPC_GPMEM_1_S  (*(volatile uint32_t *)(0x5002A614))
-#define APP_IPC_GPMEM_1_NS (*(volatile uint32_t *)(0x4002A614))
-#define APP_IPC_GPMEM_0    APP_IPC_GPMEM_0_S
-#define APP_IPC_GPMEM_1    APP_IPC_GPMEM_1_S
+#define APP_IPC_GPMEM_0_S  ((volatile uint32_t *)(0x5002A610))
+#define APP_IPC_GPMEM_0_NS ((volatile uint32_t *)(0x4002A610))
+#define APP_IPC_GPMEM      APP_IPC_GPMEM_0_S
 
 static void ncm_work_handler(struct k_work *work);
 static K_WORK_DELAYABLE_DEFINE(ncm_work, ncm_work_handler);
@@ -31,13 +28,13 @@ static K_WORK_DELAYABLE_DEFINE(ncm_work, ncm_work_handler);
 static void ncm_work_handler(struct k_work *work)
 {
 	static uint16_t live_cnt = CNT_INIT_VAL;
-	uint32_t gpmem = APP_IPC_GPMEM_0;
+	uint32_t gpmem = APP_IPC_GPMEM[IPC_MEM_CNT_IDX];
 
 	live_cnt++;
 	gpmem = (gpmem & (~CNT_MSK)) | (live_cnt << CNT_POS);
-	APP_IPC_GPMEM_0 = gpmem;
+	APP_IPC_GPMEM[IPC_MEM_CNT_IDX] = gpmem;
 
-	k_work_reschedule(&ncm_work, K_MSEC(CONFIG_NCM_APP_FEEDING_INTERVAL_MSEC));
+	k_work_reschedule(&ncm_work, K_MSEC(CONFIG_NCM_FEEDING_INTERVAL_MSEC));
 }
 
 static int reset(void)
@@ -50,11 +47,12 @@ static int reset(void)
 	/* A notification that a core reset has occurred.
 	 * And set the non-zero value of the counter.
 	 */
-	APP_IPC_GPMEM_0 = (APP_IPC_GPMEM_0 & FLAGS_MSK)
-			| (FLAGS_RESET << FLAGS_POS)
-			| (CNT_INIT_VAL << CNT_POS);
+	APP_IPC_GPMEM[IPC_MEM_CNT_IDX] = (APP_IPC_GPMEM[IPC_MEM_CNT_IDX] & FLAGS_MSK)
+				       | (FLAGS_RESET << FLAGS_POS)
+				       | (CNT_INIT_VAL << CNT_POS);
+
 	/* Save the reason for the reset. */
-	APP_IPC_GPMEM_1 = reas;
+	APP_IPC_GPMEM[IPC_MEM_REAS_IDX] = reas;
 
 	return 0;
 }
