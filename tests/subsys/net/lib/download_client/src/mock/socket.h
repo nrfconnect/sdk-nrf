c0c937abfdee42cd7d6a742736447451f88c6a8d
Robert Lubos <robert.lubos@nordicsemi.no>
2023-10-05 17:10:38 +0200

    tests: net: download_client: Fix crashing test
    Offloaded interfaces should no longer register net_if_api, but rather
    offloaded_if_api now. Using the former can cause a crash when accessing
    offloaded-specific function, which is exactly the case in
    download_client tests.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- tests/subsys/net/lib/download_client/src/mock/socket.h
+++ tests/subsys/net/lib/download_client/src/mock/socket.h
@@ -7,9 +7,10 @@
 #define _SOCKET_H_
 
 #include <zephyr/kernel.h>
+#include <zephyr/net/offloaded_netdev.h>
 
 extern struct mock_socket_iface_data mock_socket_iface_data;
-extern struct net_if_api mock_if_api;
+extern struct offloaded_if_api mock_if_api;
 
 int mock_nrf_modem_lib_socket_offload_init(const struct device *arg);
 bool mock_socket_is_supported(int family, int type, int proto);
