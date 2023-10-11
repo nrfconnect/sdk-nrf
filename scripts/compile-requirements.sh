b55bcc32fc5fe504c460981293bb635ecbb0dad5
Sebastian Wezel <sebastian.wezel@nordicsemi.no>
2023-10-11 09:51:21 +0200

    ci: Update pip requirements-fixed compilation
    * The workflow now runs in pull_request_target context
    * The workflow now posts a comment informing the author that the PR
      resulted in changes to the pip-requirements.txt
    * If the user reacts to the comment with the :eyes: emote and reruns the
      check, the check will add the changes to the PR
    
    Github actions used by this workflow:
    https://github.com/NordicBuilder/action-script-diff
    https://github.com/NordicBuilder/action-comment
    
    Signed-off-by: Sebastian Wezel <sebastian.wezel@nordicsemi.no>

--- scripts/compile-requirements.sh
+++ scripts/compile-requirements.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
-# This script merges the py-reqs from mcuboot, zephyr, and nrf.
+# Check if an argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <path for output file>"
+    exit 1
+fi
 
-OUT_FILE=$1
-[ -z "$OUT_FILE" ] && echo "Error output file not provided" && exit 1
+# Store the path provided as the first argument
+OUT_FILE="$1"
+
+# Check if the path is absolute or relative
+if [[ "$OUT_FILE" == /* ]]; then
+    echo "Absolute path provided: $OUT_FILE"
+else
+    echo "Relative path provided: $PWD/$OUT_FILE"
+    OUT_FILE="$PWD/$OUT_FILE"
+fi
 echo "Writing frozen requirements to: $OUT_FILE"
 echo "Log python version: $(python --version)"
 
@@ -20,13 +32,17 @@ workon pip-fixed-venv  > /dev/null 2>&1
 pip3 install pip-tools > /dev/null 2>&1
 pip3 install setuptools --upgrade
 
-
-pip-compile --allow-unsafe --output-file $OUT_FILE --strip-extras \
+pip-compile \
+    --build-isolation \
+    --strip-extras \
+    --annotation-style line \
+    --allow-unsafe \
+    --output-file $OUT_FILE \
     ./bootloader/mcuboot/scripts/requirements.txt \
     ./zephyr/scripts/requirements.txt  \
     ./nrf/scripts/requirements.txt \
     ./nrf/scripts/requirements-ci.txt \
     ./nrf/scripts/requirements-extra.txt
-sed '/^$/d' -i $OUT_FILE
+
 deactivate
 rmvirtualenv pip-fixed-venv
