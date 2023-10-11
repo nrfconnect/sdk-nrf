e98c6a288bd84ac4cf606dbe502325414dff5fe3
Justin Morton <justin.morton@nordicsemi.no>
2023-10-11 12:06:26 -0700

    samples: cellular: nrf_cloud_rest_device_message: add nrf_provisioning
    Add support for nrf_provisioning library.
    IRIS-7079
    
    Signed-off-by: Justin Morton <justin.morton@nordicsemi.no>

--- subsys/net/lib/nrf_cloud/src/nrf_cloud_credentials.c
+++ subsys/net/lib/nrf_cloud/src/nrf_cloud_credentials.c
@@ -28,18 +28,24 @@ int nrf_cloud_credentials_check(struct nrf_cloud_credentials_status *const cs)
 
 	ret = modem_key_mgmt_exists(cs->sec_tag, MODEM_KEY_MGMT_CRED_TYPE_CA_CHAIN, &exists);
 	if (ret < 0) {
+		LOG_ERR("modem_key_mgmt_exists() failed for CA cert in sec tag %u, error: %d",
+			cs->sec_tag, ret);
 		return -EIO;
 	}
 	cs->ca = exists;
 
 	ret = modem_key_mgmt_exists(cs->sec_tag, MODEM_KEY_MGMT_CRED_TYPE_PUBLIC_CERT, &exists);
 	if (ret < 0) {
+		LOG_ERR("modem_key_mgmt_exists() failed for client cert in sec tag %u, error: %d",
+			cs->sec_tag, ret);
 		return -EIO;
 	}
 	cs->client_cert = exists;
 
 	ret = modem_key_mgmt_exists(cs->sec_tag, MODEM_KEY_MGMT_CRED_TYPE_PRIVATE_CERT, &exists);
 	if (ret < 0) {
+		LOG_ERR("modem_key_mgmt_exists() failed for private key in sec tag %u, error: %d",
+			cs->sec_tag, ret);
 		return -EIO;
 	}
 	cs->prv_key = exists;
