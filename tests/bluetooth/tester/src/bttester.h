e17bb5c34f12c296f0841886092432effb2009f6
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2023-10-11 16:34:29 +0200

    tests: bluetooth: tester: Fix lisence in the changed files
    Fixes CI issue:
    nrf/tests/bluetooth/tester/src/bttester.h:
    Error: "APACHE-2.0" license is not allowed for this file.
    nrf/tests/bluetooth/tester/src/mesh.c:
    Error: "APACHE-2.0" license is not allowed for this file.
    License check failed with 2 error(s) and 0 warning(s)!
    
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

--- tests/bluetooth/tester/src/bttester.h
+++ tests/bluetooth/tester/src/bttester.h
@@ -3,7 +3,7 @@
 /*
  * Copyright (c) 2015-2016 Intel Corporation
  *
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
 #include <zephyr/sys/util.h>
