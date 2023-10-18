nrfconnect/sdk-nrf

commit fdaf9d8effacad7807c740d03bc9546a98829977

Bluetooth: Mesh: Fix sensor value check and occupancy delay
Fixes the sensor value check by actually comparing against the full
floating point value, instead of just the integer part, as the tester
chooses 0.5 as one of the test input value.

Also, fixes the missing delayed occupancy processing when sensor status
contains greater than zero-value for Motion Sensed, People Count, or
Presence Detected properties.

Also, fixes STANDBY state update by propagating changed property value
back to the state machine by triggering the STANDBY transition. This
adds a user experience improvement and also helps pass
MMDL/SR/LLC/BV-12-C because initial condition first turns on the LC mode
and then updates the Lightness Standby property value.

Also, sets CONFIG_BT_MESH_LIGHT_CTRL_SRV_TIME_PROLONG to 0 in the
tester project config because MMDL/SR/LLC/BV-12-C test assumes this
value to be zero.

Signed-off-by: Omkar Kulkarni <omkar.kulkarni@nordicsemi.no>

subsys/bluetooth/mesh/light_ctrl_srv.c
@@ -53,6 +53,7 @@ struct setup_srv_storage_data {
 
 static void restart_timer(struct bt_mesh_light_ctrl_srv *srv, uint32_t delay)
 {
+	LOG_DBG("delay: %d", delay);
 	k_work_reschedule(&srv->timer, K_MSEC(delay));
 }
 
@@ -126,16 +127,15 @@ static int delayed_change(struct bt_mesh_light_ctrl_srv *srv, bool value,
 }
 
 static void delayed_occupancy(struct bt_mesh_light_ctrl_srv *srv,
-			      const struct sensor_value *time_since_motion)
+			      int32_t ms_since_motion)
 {
 	int err;
-
-	int32_t ms_since_motion = time_since_motion->val1 * MSEC_PER_SEC;
 
 	if (ms_since_motion > srv->cfg.occupancy_delay) {
 		return;
 	}
 
+	LOG_DBG("Turn On in %d ms", srv->cfg.occupancy_delay - ms_since_motion);
 	err = delayed_change(srv, true,
 			     srv->cfg.occupancy_delay - ms_since_motion);
 	if (err) {
@@ -425,8 +425,6 @@ static int turn_on(struct bt_mesh_light_ctrl_srv *srv,
 		return -EBUSY;
 	}
 
-	LOG_DBG("Light Turned On");
-
 	uint32_t fade_time;
 
 	if (transition) {
@@ -434,6 +432,8 @@ static int turn_on(struct bt_mesh_light_ctrl_srv *srv,
 	} else {
 		fade_time = srv->cfg.fade_on;
 	}
+
+	LOG_DBG("Light Turned On in %d ms", fade_time);
 
 	if (srv->state != LIGHT_CTRL_STATE_ON) {
 		enum bt_mesh_light_ctrl_srv_state prev_state = srv->state;
@@ -460,7 +460,7 @@ static void turn_off_auto(struct bt_mesh_light_ctrl_srv *srv)
 		return;
 	}
 
-	LOG_DBG("Light Automatically Turn Off");
+	LOG_DBG("Light Auto Turns Off in %d ms", srv->cfg.fade_standby_auto);
 
 	transition_start(srv, LIGHT_CTRL_STATE_STANDBY,
 			 srv->cfg.fade_standby_auto);
@@ -505,7 +505,7 @@ static void prolong(struct bt_mesh_light_ctrl_srv *srv)
 		return;
 	}
 
-	LOG_DBG("Light Prolonged");
+	LOG_DBG("Light Fades to Prolong for %d ms", srv->cfg.fade_prolong);
 
 	transition_start(srv, LIGHT_CTRL_STATE_PROLONG, srv->cfg.fade_prolong);
 }
@@ -598,11 +598,13 @@ static void timeout(struct k_work *work)
 		}
 
 		if (srv->state == LIGHT_CTRL_STATE_ON) {
+			LOG_DBG("Light stays On for %d ms", srv->cfg.on);
 			restart_timer(srv, srv->cfg.on);
 			return;
 		}
 
 		if (srv->state == LIGHT_CTRL_STATE_PROLONG) {
+			LOG_DBG("Light in Prolong for %d ms", srv->cfg.prolong);
 			restart_timer(srv, srv->cfg.prolong);
 			return;
 		}
@@ -1024,10 +1026,14 @@ static int handle_sensor_status(struct bt_mesh_model *model, struct bt_mesh_msg_
 			continue;
 		}
 
+		/* Decode entire value to float, to get actual sensor value. */
+		float val = sensor_to_float(&value);
+
+		LOG_DBG("Checking sensor val");
 		if (id == BT_MESH_PROP_ID_TIME_SINCE_MOTION_SENSED) {
-			delayed_occupancy(srv, &value);
-		} else if (value.val1 > 0) {
-			turn_on(srv, NULL, true);
+			delayed_occupancy(srv, value.val1);
+		} else if (val > 0) {
+			delayed_occupancy(srv, 0);
 		}
 	}
 
@@ -1269,6 +1275,22 @@ static int prop_set(struct net_buf_simple *buf,
 		break;
 	case BT_MESH_LIGHT_CTRL_PROP_LIGHTNESS_STANDBY:
 		srv->cfg.light[LIGHT_CTRL_STATE_STANDBY] = from_actual(val.val1);
+
+		/* If light is already in STANDBY, or transitioning to STANDBY,
+		 * update the target level to new value.
+		 */
+		if (srv->state == LIGHT_CTRL_STATE_STANDBY) {
+			/* Check if transition is in progress */
+			if (atomic_test_bit(&srv->flags, FLAG_TRANSITION)) {
+				uint32_t rem_time = remaining_fade_time(srv);
+
+				transition_start(srv, LIGHT_CTRL_STATE_STANDBY,
+						 rem_time);
+			} else {
+				transition_start(srv, LIGHT_CTRL_STATE_STANDBY,
+						 0);
+			}
+		}
 		break;
 	case BT_MESH_LIGHT_CTRL_PROP_TIME_FADE_PROLONG:
 		srv->cfg.fade_prolong = from_prop_time(&val);

tests/bluetooth/tester/prj.conf
@@ -80,6 +80,7 @@ CONFIG_BT_MESH_LIGHT_CTRL_SRV_TIME_FADE_ON=0
 CONFIG_BT_MESH_LIGHT_CTRL_SRV_TIME_FADE_PROLONG=0
 CONFIG_BT_MESH_LIGHT_CTRL_SRV_TIME_FADE_STANDBY_AUTO=0
 CONFIG_BT_MESH_LIGHT_CTRL_SRV_TIME_FADE_STANDBY_MANUAL=0
+CONFIG_BT_MESH_LIGHT_CTRL_SRV_TIME_PROLONG=0
 
 CONFIG_SETTINGS=y
 CONFIG_FLASH=y

commit 4ed6cb5b7ac1a3904d166c306f271decfce76684

Bluetooth: Mesh: Process Occupancy when OCC Mode is off
Fixes a bug that got introduced in PR #12651 due to different handling
of occupancy messages. Previously the 'delayed_action_timeout()'
function was not processing occupancy when Occupancy Mode was off.

Now the function would, additionally, process the occupancy event
in FADE, PROLONG, and FADE STANDBY AUTO.

Signed-off-by: Omkar Kulkarni <omkar.kulkarni@nordicsemi.no>

subsys/bluetooth/mesh/light_ctrl_srv.c
@@ -667,6 +667,11 @@ static void delayed_action_timeout(struct k_work *work)
 		turn_off(srv, &transition, true);
 	} else if (atomic_test_and_clear_bit(&srv->flags, FLAG_OCC_PENDING) &&
 		   (srv->state == LIGHT_CTRL_STATE_ON ||
+		    srv->state == LIGHT_CTRL_STATE_PROLONG ||
+		    (/* Fade Standby Auto */
+		     srv->state == LIGHT_CTRL_STATE_STANDBY &&
+		     !atomic_test_bit(&srv->flags, FLAG_ON) &&
+		     atomic_test_bit(&srv->flags, FLAG_TRANSITION)) ||
 		    atomic_test_bit(&srv->flags, FLAG_OCC_MODE))) {
 		turn_on(srv, NULL, true);
 	}