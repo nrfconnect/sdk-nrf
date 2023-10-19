3fc94380eac049d89db4f19b5422a1ee4bcd604d
Triveni Danda <triveni.danda@nordicsemi.no>
2023-10-19 17:42:03 +0530

    samples: wifi: raw_tx_packet: Add channel setting functionality
    Add functionality to program the channel for raw tx packet
    transmission in idle mode.
    
    Signed-off-by: Triveni Danda <triveni.danda@nordicsemi.no>

--- samples/wifi/raw_tx_packet/src/main.c
+++ samples/wifi/raw_tx_packet/src/main.c
@@ -14,6 +14,40 @@ LOG_MODULE_REGISTER(raw_tx_packet, CONFIG_LOG_DEFAULT_LEVEL);
 #include "net_private.h"
 #include "wifi_connection.h"
 
+#ifdef CONFIG_RAW_TX_PACKET_SAMPLE_NON_CONNECTED_MODE
+static void wifi_set_channel(void)
+{
+	struct net_if *iface;
+	struct wifi_channel_info channel_info = {0};
+	int ret;
+
+	channel_info.oper = WIFI_MGMT_SET;
+
+	iface = net_if_get_first_wifi();
+	if (iface == NULL) {
+		LOG_ERR("Failed to get Wi-Fi iface");
+		return;
+	}
+
+	channel_info.if_index = net_if_get_by_iface(iface);
+	channel_info.channel = CONFIG_RAW_TX_PACKET_SAMPLE_CHANNEL;
+	if ((channel_info.channel < WIFI_CHANNEL_MIN) ||
+		   (channel_info.channel > WIFI_CHANNEL_MAX)) {
+		LOG_ERR("Invalid channel number. Range is (1-233)");
+		return;
+	}
+
+	ret = net_mgmt(NET_REQUEST_WIFI_CHANNEL, iface,
+		       &channel_info, sizeof(channel_info));
+	if (ret) {
+		LOG_ERR(" Channel setting failed %d\n", ret);
+			return;
+	}
+
+	LOG_INF("Wi-Fi channel set to %d", channel_info.channel);
+}
+#endif
+
 static void wifi_set_mode(int mode_val)
 {
 	int ret;
@@ -54,6 +88,8 @@ int main(void)
 	if (status < 0) {
 		return status;
 	}
+#else
+	wifi_set_channel();
 #endif
 
 	return 0;
