370e34453b8103502e3026a08583a0121490c1b2
Knut Eldhuset <knut.eldhuset@nordicsemi.no>
2023-10-23 11:10:27 +0200

    bluetooth: controller: remove redundant hci vs commands
    The commands vs_set_auto_power_control_request_param and
    vs_set_power_control_apr_handling have been replaced by
    vs_set_power_control_request_params.
    
    Signed-off-by: Knut Eldhuset <knut.eldhuset@nordicsemi.no>

--- subsys/bluetooth/controller/hci_internal.c
+++ subsys/bluetooth/controller/hci_internal.c
@@ -637,8 +637,6 @@ static void vs_supported_commands(sdc_hci_vs_supported_vs_commands_t *cmds)
 	cmds->event_length_set = 1;
 #if defined(CONFIG_BT_CTLR_LE_POWER_CONTROL)
 	cmds->write_remote_tx_power = 1;
-	cmds->set_auto_power_control_request_param = 1;
-	cmds->set_power_control_apr_handling = 1;
 	cmds->set_power_control_request_params = 1;
 #endif
 #if defined(CONFIG_BT_CENTRAL)
@@ -1555,10 +1553,6 @@ static uint8_t vs_cmd_put(uint8_t const * const cmd,
 #if defined(CONFIG_BT_CTLR_LE_POWER_CONTROL)
 	case SDC_HCI_OPCODE_CMD_VS_WRITE_REMOTE_TX_POWER:
 		return sdc_hci_cmd_vs_write_remote_tx_power((void *)cmd_params);
-	case SDC_HCI_OPCODE_CMD_VS_SET_AUTO_POWER_CONTROL_REQUEST_PARAM:
-		return sdc_hci_cmd_vs_set_auto_power_control_request_param((void *)cmd_params);
-	case SDC_HCI_OPCODE_CMD_VS_SET_POWER_CONTROL_APR_HANDLING:
-		return sdc_hci_cmd_vs_set_power_control_apr_handling((void *)cmd_params);
 	case SDC_HCI_OPCODE_CMD_VS_SET_POWER_CONTROL_REQUEST_PARAMS:
 		return sdc_hci_cmd_vs_set_power_control_request_params((void *)cmd_params);
 #endif
