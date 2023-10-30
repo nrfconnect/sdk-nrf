4df53622ca9f2a1e17c72c20b3f73255ae8515d4
Przemyslaw Bida <przemyslaw.bida@nordicsemi.no>
2023-10-30 10:00:40 +0100

    samples: openthread: Adding HCI capabilities in RCP sample.
    Commit adds hci functionality to openthread coprocessor sample.
    
    Signed-off-by: Przemyslaw Bida <przemyslaw.bida@nordicsemi.no>

--- samples/openthread/coprocessor/src/main.c
+++ samples/openthread/coprocessor/src/main.c
@@ -1,10 +1,14 @@
 /*
- * Copyright (c) 2020 Nordic Semiconductor ASA
+ * Copyright (c) 2023 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+
+#if defined(CONFIG_RCP_SAMPLE_HCI)
+#include "rcp_hci.h"
+#endif
 
 LOG_MODULE_REGISTER(coprocessor_sample, CONFIG_OT_COPROCESSOR_LOG_LEVEL);
 
@@ -18,5 +22,10 @@ LOG_MODULE_REGISTER(coprocessor_sample, CONFIG_OT_COPROCESSOR_LOG_LEVEL);
 int main(void)
 {
 	LOG_INF(WELCOME_TEXT);
+
+#if defined(CONFIG_RCP_SAMPLE_HCI)
+	run_hci();
+#endif
+
 	return 0;
 }
