7020a659c7721a6fbceb3dbf863c394e5bca1ea3
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2023-10-31 11:42:02 +0000

    cmake: sysbuild: Add MCUboot signing script
    Adds a CMake script which allows for signing applications when
    using sysbuild which uses PM configuration
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

---
+++ cmake/sysbuild/image_signing.cmake
@@ -0,0 +1,183 @@
+# Copyright (c) 2020-2023 Nordic Semiconductor ASA
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+
+# This file includes extra build system logic that is enabled when
+# CONFIG_BOOTLOADER_MCUBOOT=y.
+#
+# It builds signed binaries using imgtool as a post-processing step
+# after zephyr/zephyr.elf is created in the build directory.
+#
+# Since this file is brought in via include(), we do the work in a
+# function to avoid polluting the top-level scope.
+
+function(zephyr_runner_file type path)
+  # Property magic which makes west flash choose the signed build
+  # output of a given type.
+  set_target_properties(runners_yaml_props_target PROPERTIES "${type}_file" "${path}")
+endfunction()
+
+function(zephyr_mcuboot_tasks)
+  set(keyfile "${CONFIG_MCUBOOT_SIGNATURE_KEY_FILE}")
+  set(keyfile_enc "${CONFIG_MCUBOOT_ENCRYPTION_KEY_FILE}")
+
+  if(NOT "${CONFIG_MCUBOOT_GENERATE_UNSIGNED_IMAGE}")
+    # Check for misconfiguration.
+    if("${keyfile}" STREQUAL "")
+      # No signature key file, no signed binaries. No error, though:
+      # this is the documented behavior.
+      message(WARNING "Neither CONFIG_MCUBOOT_GENERATE_UNSIGNED_IMAGE or "
+                      "CONFIG_MCUBOOT_SIGNATURE_KEY_FILE are set, the generated build will not be "
+                      "bootable by MCUboot unless it is signed manually/externally.")
+      return()
+    endif()
+  endif()
+
+  foreach(file keyfile keyfile_enc)
+    if(NOT "${${file}}" STREQUAL "")
+      if(NOT IS_ABSOLUTE "${${file}}")
+        # Relative paths are relative to 'west topdir'.
+        set(${file} "${WEST_TOPDIR}/${${file}}")
+      endif()
+
+      if(NOT EXISTS "${${file}}" AND NOT "${CONFIG_MCUBOOT_GENERATE_UNSIGNED_IMAGE}")
+        message(FATAL_ERROR "west sign can't find file ${${file}} (Note: Relative paths are relative to the west workspace topdir \"${WEST_TOPDIR}\")")
+      elseif(NOT (CONFIG_BUILD_OUTPUT_BIN OR CONFIG_BUILD_OUTPUT_HEX))
+        message(FATAL_ERROR "Can't sign images for MCUboot: Neither CONFIG_BUILD_OUTPUT_BIN nor CONFIG_BUILD_OUTPUT_HEX is enabled, so there's nothing to sign.")
+      endif()
+    endif()
+  endforeach()
+
+  # Find imgtool. Even though west is installed, imgtool might not be.
+  # The user may also have a custom manifest which doesn't include
+  # MCUboot.
+  #
+  # Therefore, go with an explicitly installed imgtool first, falling
+  # back on mcuboot/scripts/imgtool.py.
+  if(IMGTOOL)
+    set(imgtool_path "${IMGTOOL}")
+  elseif(DEFINED ZEPHYR_MCUBOOT_MODULE_DIR)
+    set(IMGTOOL_PY "${ZEPHYR_MCUBOOT_MODULE_DIR}/scripts/imgtool.py")
+    if(EXISTS "${IMGTOOL_PY}")
+      set(imgtool_path "${IMGTOOL_PY}")
+    endif()
+  endif()
+
+  # No imgtool, no signed binaries.
+  if(NOT DEFINED imgtool_path)
+    message(FATAL_ERROR "Can't sign images for MCUboot: can't find imgtool. To fix, install imgtool with pip3, or add the mcuboot repository to the west manifest and ensure it has a scripts/imgtool.py file.")
+    return()
+  endif()
+
+  set(imgtool_sign imgtool sign --version ${CONFIG_MCUBOOT_IMGTOOL_SIGN_VERSION} --align 4 --slot-size @PM_MCUBOOT_PRIMARY_SIZE@ --pad-header --header-size @PM_MCUBOOT_PAD_SIZE@ CACHE STRING "imgtool sign command")
+
+  # Arguments to imgtool.
+  if(NOT CONFIG_MCUBOOT_EXTRA_IMGTOOL_ARGS STREQUAL "")
+    # Separate extra arguments into the proper format for adding to
+    # extra_post_build_commands.
+    #
+    # Use UNIX_COMMAND syntax for uniform results across host
+    # platforms.
+    separate_arguments(imgtool_extra UNIX_COMMAND ${CONFIG_MCUBOOT_EXTRA_IMGTOOL_ARGS})
+  else()
+    set(imgtool_extra)
+  endif()
+
+  if(CONFIG_MCUBOOT_HARDWARE_DOWNGRADE_PREVENTION)
+    set(imgtool_extra --security-counter ${CONFIG_MCUBOOT_HW_DOWNGRADE_PREVENTION_COUNTER_VALUE} ${imgtool_extra})
+  endif()
+
+  if(NOT "${keyfile}" STREQUAL "")
+    set(imgtool_extra -k "${keyfile}" ${imgtool_extra})
+  endif()
+
+  set(imgtool_args ${imgtool_extra})
+
+  # Extensionless prefix of any output file.
+  set(output ${ZEPHYR_BINARY_DIR}/${KERNEL_NAME}.signed)
+
+  if(CONFIG_BUILD_WITH_TFM)
+    set(input ${APPLICATION_BINARY_DIR}/zephyr/tfm_merged)
+  else()
+    set(input ${APPLICATION_BINARY_DIR}/zephyr/${KERNEL_NAME})
+  endif()
+
+  # List of additional build byproducts.
+  set(byproducts)
+
+  # 'west sign' arguments for confirmed, unconfirmed and encrypted images.
+  set(unconfirmed_args)
+  set(confirmed_args)
+  set(encrypted_args)
+
+  # Set up .bin outputs.
+  if(CONFIG_BUILD_OUTPUT_BIN)
+    if(CONFIG_BUILD_WITH_TFM)
+      # TF-M does not generate a bin file, so use the hex file as an input
+      set(unconfirmed_args ${input}.hex ${output}.bin)
+    else()
+      set(unconfirmed_args ${input}.bin ${output}.bin)
+    endif()
+
+    list(APPEND byproducts ${output}.bin)
+    zephyr_runner_file(bin ${output}.bin)
+
+    # Add the west sign calls and their byproducts to the post-processing
+    # steps for zephyr.elf.
+    #
+    # CMake guarantees that multiple COMMANDs given to
+    # add_custom_command() are run in order, so adding the 'west sign'
+    # calls to the "extra_post_build_commands" property ensures they run
+    # after the commands which generate the unsigned versions.
+    set_property(GLOBAL APPEND PROPERTY extra_post_build_commands COMMAND
+      ${imgtool_sign} ${imgtool_args} ${unconfirmed_args})
+
+    if(NOT "${keyfile_enc}" STREQUAL "")
+      if(CONFIG_BUILD_WITH_TFM)
+        # TF-M does not generate a bin file, so use the hex file as an input
+        set(unconfirmed_args ${input}.hex ${output}.encrypted.bin)
+      else()
+        set(unconfirmed_args ${input}.bin ${output}.encrypted.bin)
+      endif()
+
+      list(APPEND byproducts ${output}.encrypted.bin)
+      zephyr_runner_file(bin ${output}.encrypted.bin)
+
+      set_property(GLOBAL APPEND PROPERTY extra_post_build_commands COMMAND
+        ${imgtool_sign} ${imgtool_args} --encrypt "${keyfile_enc}" ${unconfirmed_args})
+    endif()
+  endif()
+
+  # Set up .hex outputs.
+  if(CONFIG_BUILD_OUTPUT_HEX)
+    set(unconfirmed_args ${input}.hex ${output}.hex)
+    list(APPEND byproducts ${output}.hex)
+    # Do not run zephyr_runner_file here as PM will provide the merged hex file from
+    # sysbuild's scope
+    set(BYPRODUCT_KERNEL_SIGNED_HEX_NAME "${output}.hex"
+        CACHE FILEPATH "Signed kernel hex file" FORCE
+    )
+
+    # Add the west sign calls and their byproducts to the post-processing
+    # steps for zephyr.elf.
+    #
+    # CMake guarantees that multiple COMMANDs given to
+    # add_custom_command() are run in order, so adding the 'west sign'
+    # calls to the "extra_post_build_commands" property ensures they run
+    # after the commands which generate the unsigned versions.
+    set_property(GLOBAL APPEND PROPERTY extra_post_build_commands COMMAND
+      ${imgtool_sign} ${imgtool_args} ${unconfirmed_args})
+
+    if(NOT "${keyfile_enc}" STREQUAL "")
+      set(unconfirmed_args ${input}.hex ${output}.encrypted.hex)
+      list(APPEND byproducts ${output}.encrypted.hex)
+      zephyr_runner_file(hex ${output}.encrypted.hex)
+
+      set_property(GLOBAL APPEND PROPERTY extra_post_build_commands COMMAND
+        ${imgtool_sign} ${imgtool_args} --encrypt "${keyfile_enc}" ${unconfirmed_args})
+    endif()
+  endif()
+
+  set_property(GLOBAL APPEND PROPERTY extra_post_build_byproducts ${byproducts})
+endfunction()
+
+zephyr_mcuboot_tasks()
