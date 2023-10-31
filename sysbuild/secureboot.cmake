01d82ef4d097b256f27e2332ff03169e1c5768bb
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2023-10-31 11:29:02 +0000

    sysbuild: Update Kconfig and CMake code
    Adds headings and new functionality to sysbuild
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- sysbuild/secureboot.cmake
+++ sysbuild/secureboot.cmake
@@ -7,34 +7,57 @@ get_property(PM_DOMAINS GLOBAL PROPERTY PM_DOMAINS)
 if(SB_CONFIG_SECURE_BOOT)
   if(SB_CONFIG_SECURE_BOOT_NETCORE)
     set(secure_boot_source_dir ${ZEPHYR_NRF_MODULE_DIR}/samples/nrf5340/netboot)
-  else()
-    set(secure_boot_source_dir ${ZEPHYR_NRF_MODULE_DIR}/samples/bootloader)
-  endif()
-  ExternalZephyrProject_Add(
-    APPLICATION ${SB_CONFIG_SECURE_BOOT_IMAGE_NAME}
-    # ToDo: which sample is used as  secure boot sample in other cases ?
-    SOURCE_DIR ${secure_boot_source_dir}
-    BOARD ${SB_CONFIG_SECURE_BOOT_BOARD}
-  )
-  set_target_properties(${SB_CONFIG_SECURE_BOOT_IMAGE_NAME} PROPERTIES
-    IMAGE_CONF_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/image_configurations/b0_image_default.cmake
-  )
 
-  # Can we do this in a more clever way ?
-  # Should adding of hci_rpmasg be a downstream feature, or could we perhaps
-  # overload upstream image adding.
-  if(NOT "${SB_CONFIG_SECURE_BOOT_DOMAIN}" IN_LIST PM_DOMAINS)
-    list(APPEND PM_DOMAINS ${SB_CONFIG_SECURE_BOOT_DOMAIN})
-  endif()
-  set_property(GLOBAL APPEND PROPERTY
-      PM_${SB_CONFIG_SECURE_BOOT_DOMAIN}_IMAGES
-      "${SB_CONFIG_SECURE_BOOT_IMAGE_NAME}"
-  )
-
-  if(SB_CONFIG_SECURE_BOOT_DOMAIN_APP)
-    set_property(GLOBAL PROPERTY DOMAIN_APP_${SB_CONFIG_SECURE_BOOT_DOMAIN}
-                 "${SB_CONFIG_SECURE_BOOT_IMAGE_NAME}"
+    ExternalZephyrProject_Add(
+      APPLICATION b0n
+      SOURCE_DIR ${secure_boot_source_dir}
+      BOARD ${SB_CONFIG_SECURE_BOOT_NETWORK_BOARD}
+      BUILD_ONLY true
     )
+    set_target_properties(${SB_CONFIG_SECURE_BOOT_IMAGE_NAME} PROPERTIES
+      IMAGE_CONF_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/image_configurations/b0_image_default.cmake
+    )
+
+    if(NOT "CPUNET" IN_LIST PM_DOMAINS)
+      list(APPEND PM_DOMAINS CPUNET)
+    endif()
+    set_property(GLOBAL APPEND PROPERTY
+        PM_CPUNET_IMAGES
+        "b0n"
+    )
+
+    if(SB_CONFIG_SECURE_BOOT_DOMAIN_APP)
+      set_property(GLOBAL PROPERTY DOMAIN_APP_CPUNET
+                   "b0n"
+      )
+    endif()
+  endif()
+
+  if(SB_CONFIG_SECURE_BOOT_APPCORE)
+    set(secure_boot_source_dir ${ZEPHYR_NRF_MODULE_DIR}/samples/bootloader)
+
+    ExternalZephyrProject_Add(
+      APPLICATION b0
+      SOURCE_DIR ${secure_boot_source_dir}
+      BUILD_ONLY true
+    )
+    set_target_properties(${SB_CONFIG_SECURE_BOOT_IMAGE_NAME} PROPERTIES
+      IMAGE_CONF_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/image_configurations/b0_image_default.cmake
+    )
+
+    if(NOT "APP" IN_LIST PM_DOMAINS)
+      list(APPEND PM_DOMAINS APP)
+    endif()
+    set_property(GLOBAL APPEND PROPERTY
+        PM_APP_IMAGES
+        "b0"
+    )
+
+    if(SB_CONFIG_SECURE_BOOT_DOMAIN_APP)
+      set_property(GLOBAL PROPERTY DOMAIN_APP_APP
+                   "b0"
+      )
+    endif()
   endif()
 
   if(SB_CONFIG_SECURE_BOOT_BUILD_S1_VARIANT_IMAGE)
@@ -47,7 +70,7 @@ if(SB_CONFIG_SECURE_BOOT)
     endif()
 
     set_property(GLOBAL APPEND PROPERTY
-        PM_${SB_CONFIG_SECURE_BOOT_DOMAIN}_IMAGES
+        PM_APP_IMAGES
         "${image}"
     )
   endif()
