64cff07e3dc253819661b6fc7e27054f70462d6f
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2023-10-31 11:15:53 +0000

    cmake: fw_zip: Add sysbuild support
    Adds extra functionality so that the code can be called from
    a sysbuild context
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- cmake/fw_zip.cmake
+++ cmake/fw_zip.cmake
@@ -5,8 +5,8 @@
 #
 
 function(generate_dfu_zip)
-  set(oneValueArgs OUTPUT TYPE TARGET)
-  set(multiValueArgs BIN_FILES SCRIPT_PARAMS)
+  set(oneValueArgs OUTPUT TYPE TARGET IMAGE)
+  set(multiValueArgs BIN_FILES SCRIPT_PARAMS DEPENDS)
   cmake_parse_arguments(GENZIP "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
   if (NOT(
@@ -18,10 +18,24 @@ function(generate_dfu_zip)
     message(FATAL_ERROR "Missing required param")
   endif()
 
-  get_filename_component(APPNAME ${APPLICATION_SOURCE_DIR} NAME)
-  if(CONFIG_BUILD_OUTPUT_META)
-    set(meta_info_file ${PROJECT_BINARY_DIR}/${KERNEL_META_NAME})
-    set(meta_argument --meta-info-file ${meta_info_file})
+  if(SYSBUILD AND GENZIP_IMAGE)
+    set(APPNAME ${GENZIP_IMAGE})
+    sysbuild_get(CONFIG_BUILD_OUTPUT_META IMAGE ${GENZIP_IMAGE} VAR CONFIG_BUILD_OUTPUT_META KCONFIG)
+    sysbuild_get(CONFIG_BOARD IMAGE ${GENZIP_IMAGE} VAR CONFIG_BOARD KCONFIG)
+    sysbuild_get(CONFIG_SOC IMAGE ${GENZIP_IMAGE} VAR CONFIG_SOC KCONFIG)
+
+    if(CONFIG_BUILD_OUTPUT_META)
+      sysbuild_get(application_image_dir IMAGE ${GENZIP_IMAGE} VAR APPLICATION_BINARY_DIR CACHE)
+      sysbuild_get(CONFIG_KERNEL_BIN_NAME IMAGE ${GENZIP_IMAGE} VAR CONFIG_KERNEL_BIN_NAME KCONFIG)
+      set(meta_info_file ${GENZIP_IMAGE}_extra_byproducts)
+      set(meta_argument --meta-info-file ${application_image_dir}/zephyr/${CONFIG_KERNEL_BIN_NAME}.meta)
+    endif()
+  else()
+    get_filename_component(APPNAME ${APPLICATION_SOURCE_DIR} NAME)
+    if(CONFIG_BUILD_OUTPUT_META)
+      set(meta_info_file ${PROJECT_BINARY_DIR}/${KERNEL_META_NAME})
+      set(meta_argument --meta-info-file ${meta_info_file})
+    endif()
   endif()
 
   add_custom_command(
@@ -37,7 +51,7 @@ function(generate_dfu_zip)
     "type=${GENZIP_TYPE}"
     "board=${CONFIG_BOARD}"
     "soc=${CONFIG_SOC}"
-    DEPENDS ${GENZIP_BIN_FILES} ${meta_info_file}
+    DEPENDS ${GENZIP_DEPENDS} ${GENZIP_BIN_FILES} ${meta_info_file}
     )
 
   get_filename_component(TARGET_NAME ${GENZIP_OUTPUT} NAME)
@@ -48,5 +62,4 @@ function(generate_dfu_zip)
     ALL
     DEPENDS ${GENZIP_OUTPUT}
     )
-
 endfunction()
