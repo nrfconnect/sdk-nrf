4abb4a4db5813c77fbd30f687ac921a00d1a30b7
Juha Heiskanen <juha.heiskanen@nordicsemi.no>
2023-11-02 14:02:39 +0200

    tests: mcumgr_smp_client: Added Unit tests
    Unit test for validate mcumgr smp client API.
    
    Signed-off-by: Juha Heiskanen <juha.heiskanen@nordicsemi.no>

---
+++ tests/subsys/net/lib/mcumgr_smp_client/src/nrfx.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/* DUMMY FILE ONLY TO BE USED FOR TESTING */
+#ifndef NRFX_H__
+#define NRFX_H__
+bool nrfx_is_in_ram(char *const file);
+#endif /* NRFX_H__ */
