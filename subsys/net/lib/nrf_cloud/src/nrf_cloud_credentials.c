403f99f7622977716cb814f5fc05d8e1bf269365
Georges Oates_Larsen <georges.larsen@nordicsemi.no>
2023-11-15 10:16:47 -0800

    lib: net: nrf_cloud: Support native credential checking
    Add support for TLS Credentials subsystem/native network stack to the
    credentials checking feature of the nrf_cloud library.
    
    IRIS-7574
    
    Signed-off-by: Georges Oates_Larsen <georges.larsen@nordicsemi.no>

--- subsys/net/lib/nrf_cloud/src/nrf_cloud_credentials.c
+++ subsys/net/lib/nrf_cloud/src/nrf_cloud_credentials.c
@@ -6,8 +6,76 @@
 #include <net/nrf_cloud.h>
 #include <zephyr/logging/log.h>
 #include <modem/modem_key_mgmt.h>
+#include <zephyr/net/tls_credentials.h>
 
 LOG_MODULE_REGISTER(nrf_cloud_credentials, CONFIG_NRF_CLOUD_LOG_LEVEL);
+
+enum nrf_cloud_cred_type {
+	NRF_CLOUD_CRED_CA_CERT,
+	NRF_CLOUD_CRED_CLIENT_CERT,
+	NRF_CLOUD_CRED_PRIVATE_KEY
+};
+
+/**
+ * @brief Check whether a specified credential exists in any enabled credentials subsystem.
+ *
+ * @retval 0 if the specified credential exists.
+ * @retval -EIO Error while checking if specified credential exists.
+ * @retval -ENOTSUP if the specified  credential does not exist.
+ */
+static int nrf_cloud_credential_check(uint32_t sec_tag, enum nrf_cloud_cred_type cred_type) {
+	enum modem_key_mgmt_cred_type modem_type;
+	enum tls_credential_type tls_type;
+	char *debug_tag;
+	int ret;
+	bool exists;
+
+	/* Translate universal credential type into credential subsystem types. */
+	switch(cred_type) {
+	case NRF_CLOUD_CRED_CA_CERT:
+		modem_type = MODEM_KEY_MGMT_CRED_TYPE_CA_CHAIN;
+		tls_type = TLS_CREDENTIAL_CA_CERTIFICATE;
+		debug_tag = "CA cert";
+		break;
+	case NRF_CLOUD_CRED_CLIENT_CERT:
+		modem_type = MODEM_KEY_MGMT_CRED_TYPE_PUBLIC_CERT;
+		tls_type = TLS_CREDENTIAL_SERVER_CERTIFICATE;
+		debug_tag = "client cert";
+		break;
+	case NRF_CLOUD_CRED_PRIVATE_KEY:
+		modem_type = MODEM_KEY_MGMT_CRED_TYPE_PRIVATE_CERT;
+		tls_type = TLS_CREDENTIAL_PRIVATE_KEY;
+		debug_tag = "private key";
+		break;
+	default:
+		/* Invalid credential type specified. */
+		return -EIO;
+	}
+
+	/* Check each enabled credentials subsystem for the credential. */
+
+#if defined(CONFIG_MODEM_KEY_MGMT)
+	/* Modem credentials subsystem */
+	ret = modem_key_mgmt_exists(sec_tag, modem_type, &exists);
+	if (ret < 0) {
+		LOG_ERR("modem_key_mgmt_exists() failed for %s in sec tag %u, error: %d",
+			debug_tag, sec_tag, ret);
+		return -EIO;
+	}
+	if (exists) {
+		return 0;
+	}
+#endif /* CONFIG_MODEM_KEY_MGMT */
+
+#if defined(TLS_CREDENTIALS)
+	/* Native credentials subsystem */
+	if (credential_get((sec_tag_t)sec_tag, tls_type)) {
+		return 0;
+	}
+#endif /* TLS_CREDENTIALS */
+
+	return -ENOTSUP;
+}
 
 int nrf_cloud_credentials_configured_check(void)
 {
@@ -49,7 +117,6 @@ int nrf_cloud_credentials_check(struct nrf_cloud_credentials_status *const cs)
 	}
 
 	int ret;
-	bool exists = false;
 
 	memset(cs, 0, sizeof(*cs));
 
@@ -59,29 +126,26 @@ int nrf_cloud_credentials_check(struct nrf_cloud_credentials_status *const cs)
 	cs->sec_tag = CONFIG_NRF_CLOUD_COAP_SEC_TAG;
 #endif
 
-	ret = modem_key_mgmt_exists(cs->sec_tag, MODEM_KEY_MGMT_CRED_TYPE_CA_CHAIN, &exists);
-	if (ret < 0) {
-		LOG_ERR("modem_key_mgmt_exists() failed for CA cert in sec tag %u, error: %d",
-			cs->sec_tag, ret);
+	/* Check for CA cert */
+	ret = nrf_cloud_credential_check(cs->sec_tag, NRF_CLOUD_CRED_CA_CERT);
+	if (ret == -EIO) {
 		return -EIO;
 	}
-	cs->ca = exists;
+	cs->ca = (ret == 0);
 
-	ret = modem_key_mgmt_exists(cs->sec_tag, MODEM_KEY_MGMT_CRED_TYPE_PUBLIC_CERT, &exists);
-	if (ret < 0) {
-		LOG_ERR("modem_key_mgmt_exists() failed for client cert in sec tag %u, error: %d",
-			cs->sec_tag, ret);
+	/* Check for client cert */
+	ret = nrf_cloud_credential_check(cs->sec_tag, NRF_CLOUD_CRED_CLIENT_CERT);
+	if (ret == -EIO) {
 		return -EIO;
 	}
-	cs->client_cert = exists;
+	cs->client_cert = (ret == 0);
 
-	ret = modem_key_mgmt_exists(cs->sec_tag, MODEM_KEY_MGMT_CRED_TYPE_PRIVATE_CERT, &exists);
-	if (ret < 0) {
-		LOG_ERR("modem_key_mgmt_exists() failed for private key in sec tag %u, error: %d",
-			cs->sec_tag, ret);
+	/* Check for private key */
+	ret = nrf_cloud_credential_check(cs->sec_tag, NRF_CLOUD_CRED_PRIVATE_KEY);
+	if (ret == -EIO) {
 		return -EIO;
 	}
-	cs->prv_key = exists;
+	cs->prv_key = (ret == 0);
 
 	LOG_DBG("Sec Tag: %u, CA: %s, Client Cert: %s, Private Key: %s",
 		cs->sec_tag,
