c17dd45ce5ec9738b7487f647dcb6ce86d78ff90
Markus Lassila <markus.lassila@nordicsemi.no>
2023-11-16 12:57:34 +0200

    applications: serial_lte_modem: Fix indications in #XSLEEP=2
    Do not trigger the indicate pin when preparing to sleep.
    
    Signed-off-by: Markus Lassila <markus.lassila@nordicsemi.no>

--- applications/serial_lte_modem/src/slm_uart_handler.c
+++ applications/serial_lte_modem/src/slm_uart_handler.c
@@ -188,7 +188,7 @@ static void rx_process(struct k_work *work)
 	rx_recovery();
 }
 
-static int tx_start(void)
+static int tx_start(bool indicate)
 {
 	uint8_t *buf;
 	size_t ret;
@@ -197,8 +197,10 @@ static int tx_start(void)
 
 	(void)pm_device_state_get(uart_dev, &state);
 	if (state != PM_DEVICE_STATE_ACTIVE) {
-		(void)indicate_start();
-		return -ENODEV;
+		if (indicate) {
+			(void)indicate_start();
+		}
+		return 1;
 	}
 
 	ret = ring_buf_get_claim(&tx_buf, &buf, ring_buf_capacity_get(&tx_buf));
@@ -228,7 +230,7 @@ static void uart_callback(const struct device *dev, struct uart_event *evt, void
 			LOG_ERR("UART_TX_DONE failure: %d", err);
 		}
 		if (ring_buf_is_empty(&tx_buf) == false) {
-			(void)tx_start();
+			(void)tx_start(false);
 		} else {
 			k_sem_give(&tx_done_sem);
 		}
@@ -239,7 +241,7 @@ static void uart_callback(const struct device *dev, struct uart_event *evt, void
 			LOG_ERR("UART_TX_ABORTED failure: %d", err);
 		}
 		if (ring_buf_is_empty(&tx_buf) == false) {
-			(void)tx_start();
+			(void)tx_start(false);
 		} else {
 			k_sem_give(&tx_done_sem);
 		}
@@ -310,7 +312,7 @@ int slm_uart_power_on(void)
 	}
 
 	k_sem_give(&tx_done_sem);
-	(void)tx_start();
+	(void)tx_start(false);
 
 	return 0;
 }
@@ -326,7 +328,7 @@ int slm_uart_power_off(void)
 	}
 
 	/* Write sync str to buffer, so it is send first when we power UART.*/
-	(void)slm_uart_tx_write(SLM_SYNC_STR, sizeof(SLM_SYNC_STR)-1, true);
+	(void)slm_uart_tx_write(SLM_SYNC_STR, sizeof(SLM_SYNC_STR)-1, true, false);
 
 	return err;
 }
@@ -341,7 +343,7 @@ bool slm_uart_can_context_send(const uint8_t *data, size_t len)
 }
 
 /* Write the data to tx_buffer and trigger sending. */
-int slm_uart_tx_write(const uint8_t *data, size_t len, bool print_full_debug)
+int slm_uart_tx_write(const uint8_t *data, size_t len, bool print_full_debug, bool indicate)
 {
 	size_t ret;
 	size_t sent = 0;
@@ -358,7 +360,7 @@ int slm_uart_tx_write(const uint8_t *data, size_t len, bool print_full_debug)
 		} else {
 			/* Buffer full, block and start TX. */
 			k_sem_take(&tx_done_sem, K_FOREVER);
-			err = tx_start();
+			err = tx_start(indicate);
 			if (err) {
 				LOG_ERR("TX buf overflow, %d dropped. Unable to send: %d",
 					len - sent,
@@ -372,8 +374,8 @@ int slm_uart_tx_write(const uint8_t *data, size_t len, bool print_full_debug)
 	k_mutex_unlock(&mutex_tx_put);
 
 	if (k_sem_take(&tx_done_sem, K_NO_WAIT) == 0) {
-		err = tx_start();
-		if (err == -ENODEV) {
+		err = tx_start(indicate);
+		if (err == 1) {
 			k_sem_give(&tx_done_sem);
 			return 0;
 		} else if (err) {
@@ -441,7 +443,7 @@ int slm_uart_handler_init(slm_uart_rx_callback_t callback_t)
 
 	k_sem_give(&tx_done_sem);
 
-	err = slm_uart_tx_write(SLM_SYNC_STR, sizeof(SLM_SYNC_STR)-1, true);
+	err = slm_uart_tx_write(SLM_SYNC_STR, sizeof(SLM_SYNC_STR)-1, true, false);
 	if (err) {
 		return err;
 	}
