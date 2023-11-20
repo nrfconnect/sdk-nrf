f3e6d358b65e98952a40dcfdf0a4d3b573aa7c0e
Marek Pieta <Marek.Pieta@nordicsemi.no>
2023-11-20 16:36:01 +0100

    applications: nrf_desktop: Align to new SDFW API
    Change aligns the nRF Destkop application to new SDFW API.
    
    Jira: NCSDK-24660
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- applications/nrf_desktop/src/modules/dfu.c
+++ applications/nrf_desktop/src/modules/dfu.c
@@ -335,18 +335,8 @@ static void complete_dfu_data_store(void)
 #elif CONFIG_SUIT
 		update_candidate.mem = suit_plat_get_nvm_ptr(FIXED_PARTITION_OFFSET(dfu_partition));
 		update_candidate.size = img_length;
-
-		bool dfu_partition_valid = suit_validate_candidate((uintptr_t)update_candidate.mem,
-								   update_candidate.size);
-
-	        if (dfu_partition_valid) {
-			LOG_INF("Valid DFU update candidate stored in DFU partition");
-			LOG_INF("Update will be performed on configuration channel reboot request");
-		} else {
-			update_candidate.mem = 0;
-			update_candidate.size = 0;
-			LOG_WRN("SUIT image is not valid");
-		}
+		LOG_INF("DFU update candidate stored in DFU partition");
+		LOG_INF("Update will be performed on configuration channel reboot request");
 #endif
 		terminate_dfu();
 	}
@@ -818,12 +808,37 @@ static void handle_image_info_request(uint8_t *data, size_t *size)
 #elif CONFIG_SUIT
 static void handle_image_info_request(uint8_t *data, size_t *size)
 {
+	/* CBOR encoded Manifest Component ID
+	 * nRF Desktop uses default SUIT manifest file (default namespace and name).
+	 */
+	const uint8_t component_id[] = {
+		0x82, /* CBOR type: list, length: 2 */
+		  0x4c, /* CBOR type: byte string, length: 12 */
+		    0x6b, /* CBOR type: text string, length: 11 */
+		      'I', 'N', 'S', 'T',  'L', 'D',  '_',  'M',  'F',   'S',  'T',
+		  0x50, /* CBOR type: byte string, length: 16 */
+		    /* RFC4122 uuid5(uuid5(uuid.NAMESPACE_DNS, 'nordicsemi.com'),
+		     *               'nRF54H20_sample_root')
+		     */
+		    0x3f, 0x6a, 0x3a, 0x4d, 0xcd, 0xfa, 0x58, 0xc5,
+		    0xac, 0xce, 0xf9, 0xf5, 0x84, 0xc4, 0x11, 0x24
+	};
 	/* SUIT uses hash with length of 32 bytes. */
 	uint8_t hash[32] = {0};
+	int alg_id = 0;
 	unsigned int seq_num = 0;
-	suit_plat_mreg_t hash_mreg = {.mem = hash, .size = sizeof(hash)};
 
-	int err = suit_read_manifest_data(&seq_num, &hash_mreg);
+	suit_plat_mreg_t hash_mreg = {
+		.mem = hash,
+		.size = sizeof(hash)
+	};
+	suit_plat_mreg_t manifest_component_id = {
+		.mem = component_id,
+		.size = sizeof(component_id),
+	};
+
+	int err = suit_get_installed_manifest_info(&manifest_component_id, &seq_num, &alg_id,
+						   &hash_mreg);
 
 	if (!err) {
 		uint8_t flash_area_id = 0;
