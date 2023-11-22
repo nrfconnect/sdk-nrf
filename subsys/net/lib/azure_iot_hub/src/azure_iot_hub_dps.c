59bd43087bbc5471cfe6dc51374dd1e3d0d89463
Simen S. Røstad <simen.rostad@nordicsemi.no>
2023-11-22 06:39:12 +0100

    samples: net: Remove settings backend override
    The settings backend used to store the Azure hostname and device ID that
    is received during DPS (Device Provisioning Service) was overridden by
    the nRF7002 board configuration. Setting NVS as the backend instead
    of FCB.
    
    This caused the hostname and device ID to fail to be successfully
    retrieved from flash post boot, causing the DPS process to occur
    every time. Why it doesn't work with the NVS backend
    needs to be investigated.
    
    Signed-off-by: Simen S. Røstad <simen.rostad@nordicsemi.no>

--- subsys/net/lib/azure_iot_hub/src/azure_iot_hub_dps.c
+++ subsys/net/lib/azure_iot_hub/src/azure_iot_hub_dps.c
@@ -137,7 +137,7 @@ static int dps_settings_handler(const char *key, size_t len,
 
 	if (strcmp(key, DPS_SETTINGS_HOSTNAME_LEN_KEY) == 0) {
 		err = read_cb(cb_arg, &hostname_len, sizeof(hostname_len));
-		if (err < 0) {
+		if (err <= 0) {
 			LOG_ERR("Failed to read hostname length, error: %d", err);
 			return err;
 		}
@@ -150,7 +150,7 @@ static int dps_settings_handler(const char *key, size_t len,
 	if (strcmp(key, DPS_SETTINGS_HOSTNAME_KEY) == 0) {
 		err = read_cb(cb_arg, assigned_hub_buf,
 			      MIN(sizeof(assigned_hub_buf), hostname_len));
-		if (err < 0) {
+		if (err <= 0) {
 			LOG_ERR("Failed to read hostname, error: %d", err);
 			return err;
 		}
@@ -166,7 +166,7 @@ static int dps_settings_handler(const char *key, size_t len,
 
 	if (strcmp(key, DPS_SETTINGS_DEVICE_ID_LEN_KEY) == 0) {
 		err = read_cb(cb_arg, &device_id_len, sizeof(device_id_len));
-		if (err < 0) {
+		if (err <= 0) {
 			LOG_ERR("Failed to read device ID length, error: %d", err);
 			return err;
 		}
@@ -179,7 +179,7 @@ static int dps_settings_handler(const char *key, size_t len,
 	if (strcmp(key, DPS_SETTINGS_DEVICE_ID_KEY) == 0) {
 		err = read_cb(cb_arg, assigned_device_id_buf,
 			      MIN(sizeof(assigned_device_id_buf), device_id_len));
-		if (err < 0) {
+		if (err <= 0) {
 			LOG_ERR("Failed to read device ID, error: %d", err);
 			return err;
 		}
