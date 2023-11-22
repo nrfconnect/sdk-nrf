c7a8bed9ecf3393537c2045c758fea2934a741e4
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2023-11-22 13:04:49 +0100

    samples: matter: Added support for mcuboot and DFU with sysbuild
    Introduced configuration that allows to enable mcuboot and DFU
    (over BT SMP) for the Matter samples build with sysbuild.
    Additionally the support for sysbuild in Matter weather station
    and Matter bridge was added.
    
    Signed-off-by: Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>

---
+++ applications/matter_bridge/sysbuild.cmake
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2023 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+#
+
+# Set the paritions configuration
+set(PM_STATIC_YML_FILE ${ZEPHYR_NRF_MODULE_DIR}/applications/matter_bridge/configuration/${BOARD}/pm_static_dfu.yml PARENT_SCOPE)
