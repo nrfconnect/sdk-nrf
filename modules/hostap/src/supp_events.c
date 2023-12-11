099188796e42ab749b46d594478feb526f21416d
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2023-12-11 16:17:06 +0530

    modules: hostapd: Move to data driven design
    Instead of assuming that the array is perfectly aligned with enum, moved
    to a data driven design, this decouples from the enum numbering and also
    helps avoid repetition of the event string while parsing.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- modules/hostap/src/supp_events.c
+++ modules/hostap/src/supp_events.c
@@ -15,23 +15,25 @@
 /* Re-defines MAC2STR with address of the element */
 #define MACADDR2STR(a) &(a)[0], &(a)[1], &(a)[2], &(a)[3], &(a)[4], &(a)[5]
 
-static char *wpa_supp_event_map[] = {
-	"CTRL-EVENT-CONNECTED",
-	"CTRL-EVENT-DISCONNECTED",
-	"CTRL-EVENT-ASSOC-REJECT",
-	"CTRL-EVENT-AUTH-REJECT",
-	"CTRL-EVENT-TERMINATING",
-	"CTRL-EVENT-SSID-TEMP-DISABLED",
-	"CTRL-EVENT-SSID-REENABLED",
-	"CTRL-EVENT-SCAN-STARTED",
-	"CTRL-EVENT-SCAN-RESULTS",
-	"CTRL-EVENT-SCAN-FAILED",
-	"CTRL-EVENT-BSS-ADDED",
-	"CTRL-EVENT-BSS-REMOVED",
-	"CTRL-EVENT-NETWORK-NOT-FOUND",
-	"CTRL-EVENT-NETWORK-ADDED",
-	"CTRL-EVENT-NETWORK-REMOVED",
-	"CTRL-EVENT-DSCP-POLICY",
+static const struct wpa_supp_event_info {
+	const char *event_str;
+	enum supp_event_num event;
+} wpa_supp_event_info[] = {
+	{ "CTRL-EVENT-CONNECTED", WPA_SUPP_EVENT_CONNECTED },
+	{ "CTRL-EVENT-DISCONNECTED", WPA_SUPP_EVENT_DISCONNECTED },
+	{ "CTRL-EVENT-ASSOC-REJECT", WPA_SUPP_EVENT_ASSOC_REJECT },
+	{ "CTRL-EVENT-AUTH-REJECT", WPA_SUPP_EVENT_AUTH_REJECT },
+	{ "CTRL-EVENT-SSID-TEMP-DISABLED", WPA_SUPP_EVENT_SSID_TEMP_DISABLED },
+	{ "CTRL-EVENT-SSID-REENABLED", WPA_SUPP_EVENT_SSID_REENABLED },
+	{ "CTRL-EVENT-BSS-ADDED", WPA_SUPP_EVENT_BSS_ADDED },
+	{ "CTRL-EVENT-BSS-REMOVED", WPA_SUPP_EVENT_BSS_REMOVED },
+	{ "CTRL-EVENT-TERMINATING", WPA_SUPP_EVENT_TERMINATING },
+	{ "CTRL-EVENT-SCAN-STARTED", WPA_SUPP_EVENT_SCAN_STARTED },
+	{ "CTRL-EVENT-SCAN-FAILED", WPA_SUPP_EVENT_SCAN_FAILED },
+	{ "CTRL-EVENT-NETWORK-NOT-FOUND", WPA_SUPP_EVENT_NETWORK_NOT_FOUND },
+	{ "CTRL-EVENT-NETWORK-ADDED", WPA_SUPP_EVENT_NETWORK_ADDED },
+	{ "CTRL-EVENT-NETWORK-REMOVED", WPA_SUPP_EVENT_NETWORK_REMOVED },
+	{ "CTRL-EVENT-DSCP-POLICY", WPA_SUPP_EVENT_DSCP_POLICY },
 };
 
 static void copy_mac_addr(const unsigned int *src, uint8_t *dst)
@@ -75,38 +77,43 @@ static enum wifi_disconn_reason wpas_to_wifi_mgmt_diconn_status(int status)
 static int wpa_supp_process_status(struct supp_int_event_data *event_data, char *wpa_supp_status)
 {
 	int ret = 1; /* For cases where parsing is not being done*/
-	int event = -1;
 	int i;
 	union supp_event_data *data;
 	unsigned int tmp_mac_addr[ETH_ALEN];
+	unsigned int event_prefix_len;
+	char *event_no_prefix;
+	struct wpa_supp_event_info event_info;
 
 	data = (union supp_event_data *)event_data->data;
 
-	for (i = 0; i < ARRAY_SIZE(wpa_supp_event_map); i++) {
-		if (strncmp(wpa_supp_status, wpa_supp_event_map[i],
-		    strlen(wpa_supp_event_map[i])) == 0) {
-			event = i;
+	for (i = 0; i < ARRAY_SIZE(wpa_supp_event_info); i++) {
+		if (strncmp(wpa_supp_status, wpa_supp_event_info[i].event_str,
+		    strlen(wpa_supp_event_info[i].event_str)) == 0) {
+			event_info = wpa_supp_event_info[i];
 			break;
 		}
 	}
 
-	if (i >= ARRAY_SIZE(wpa_supp_event_map)) {
+	if (i >= ARRAY_SIZE(wpa_supp_event_info)) {
 		/* This is not a bug but rather implementation gap (intentional or not) */
 		wpa_printf(MSG_DEBUG, "Event not supported: %s\n", wpa_supp_status);
 		return -ENOTSUP;
 	}
 
-	event_data->event = event;
+	/* Skip the event prefix and a space */
+	event_prefix_len = strlen(event_info.event_str) + 1;
+	event_no_prefix = wpa_supp_status + event_prefix_len;
+	event_data->event = event_info.event;
 
 	switch (event_data->event) {
 	case WPA_SUPP_EVENT_CONNECTED:
-		ret = sscanf(wpa_supp_status + strlen("CTRL-EVENT-CONNECTED - Connection to"),
+		ret = sscanf(event_no_prefix, "- Connection to"
 			MACSTR, MACADDR2STR(tmp_mac_addr));
 		event_data->data_len = sizeof(data->connected);
 		copy_mac_addr(tmp_mac_addr, data->connected.bssid);
 		break;
 	case WPA_SUPP_EVENT_DISCONNECTED:
-		ret = sscanf(wpa_supp_status + strlen("CTRL-EVENT-DISCONNECTED bssid="),
+		ret = sscanf(event_no_prefix,
 			MACSTR" reason=%d", MACADDR2STR(tmp_mac_addr),
 			&data->disconnected.reason_code);
 		event_data->data_len = sizeof(data->disconnected);
@@ -116,7 +123,7 @@ static int wpa_supp_process_status(struct supp_int_event_data *event_data, char 
 		/* TODO */
 		break;
 	case WPA_SUPP_EVENT_AUTH_REJECT:
-		ret = sscanf(wpa_supp_status + strlen("CTRL-EVENT-AUTH-REJECT "), MACSTR
+		ret = sscanf(event_no_prefix, MACSTR
 			" auth_type=%u auth_transaction=%u status_code=%u",
 			MACADDR2STR(tmp_mac_addr),
 			&data->auth_reject.auth_type,
@@ -126,7 +133,7 @@ static int wpa_supp_process_status(struct supp_int_event_data *event_data, char 
 		copy_mac_addr(tmp_mac_addr, data->auth_reject.bssid);
 		break;
 	case WPA_SUPP_EVENT_SSID_TEMP_DISABLED:
-		ret = sscanf(wpa_supp_status + strlen("CTRL-EVENT-SSID-TEMP-DISABLED "),
+		ret = sscanf(event_no_prefix,
 			"id=%d ssid=%s auth_failures=%u duration=%d reason=%s",
 			&data->temp_disabled.id, data->temp_disabled.ssid,
 			&data->temp_disabled.auth_failures,
@@ -135,20 +142,20 @@ static int wpa_supp_process_status(struct supp_int_event_data *event_data, char 
 		event_data->data_len = sizeof(data->temp_disabled);
 		break;
 	case WPA_SUPP_EVENT_SSID_REENABLED:
-		ret = sscanf(wpa_supp_status + strlen("CTRL-EVENT-SSID-REENABLED "),
+		ret = sscanf(event_no_prefix,
 			"id=%d ssid=%s", &data->reenabled.id,
 			data->reenabled.ssid);
 		event_data->data_len = sizeof(data->reenabled);
 		break;
 	case WPA_SUPP_EVENT_BSS_ADDED:
-		ret = sscanf(wpa_supp_status + strlen("CTRL-EVENT-BSS-ADDED "), "%u "MACSTR,
+		ret = sscanf(event_no_prefix, "%u "MACSTR,
 			&data->bss_added.id,
 			MACADDR2STR(tmp_mac_addr));
 		copy_mac_addr(tmp_mac_addr, data->bss_added.bssid);
 		event_data->data_len = sizeof(data->bss_added);
 		break;
 	case WPA_SUPP_EVENT_BSS_REMOVED:
-		ret = sscanf(wpa_supp_status + strlen("CTRL-EVENT-BSS-REMOVED "),
+		ret = sscanf(event_no_prefix,
 			"%u "MACSTR,
 			&data->bss_removed.id,
 			MACADDR2STR(tmp_mac_addr));
@@ -161,7 +168,7 @@ static int wpa_supp_process_status(struct supp_int_event_data *event_data, char 
 	case WPA_SUPP_EVENT_NETWORK_NOT_FOUND:
 	case WPA_SUPP_EVENT_NETWORK_ADDED:
 	case WPA_SUPP_EVENT_NETWORK_REMOVED:
-		strncpy(data->supp_event_str, wpa_supp_event_map[event],
+		strncpy(data->supp_event_str, event_info.event_str,
 				 sizeof(data->supp_event_str));
 		event_data->data_len = strlen(data->supp_event_str) + 1;
 	case WPA_SUPP_EVENT_DSCP_POLICY:
@@ -173,7 +180,7 @@ static int wpa_supp_process_status(struct supp_int_event_data *event_data, char 
 
 	if (ret <= 0) {
 		wpa_printf(MSG_ERROR, "%s Parse failed: %s",
-			   wpa_supp_event_map[event_data->event], strerror(errno));
+			   event_info.event_str, strerror(errno));
 	}
 
 	return ret;
