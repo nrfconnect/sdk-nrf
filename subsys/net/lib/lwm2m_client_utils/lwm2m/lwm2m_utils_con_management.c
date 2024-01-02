401657e51ed611c6d78efc8c834dd880205ba970
Juha Ylinen <juha.ylinen@nordicsemi.no>
2024-01-02 13:37:10 +0200

    net: lwm2m_client_utils: Conneval fixes
    Set dtls_stored flag to false also when loading DTLS connection fails.
    Otherwise saving the DTLS connection is not possibe anymore.
    
    Fix issue when LwM2M engine thread is resumed and the estimated energy
    consumption is still below the threshold.
    
    Update test cases.
    
    Signed-off-by: Juha Ylinen <juha.ylinen@nordicsemi.no>

--- subsys/net/lib/lwm2m_client_utils/lwm2m/lwm2m_utils_con_management.c
+++ subsys/net/lib/lwm2m_client_utils/lwm2m/lwm2m_utils_con_management.c
@@ -50,11 +50,11 @@ static void lwm2m_utils_dtls_load(struct lwm2m_ctx *ctx)
 			       sizeof(load_dtls));
 	if (ret) {
 		ret = -errno;
-		LOG_ERR("Failed to load %d DTLS session: %d", ctx->sock_fd, ret);
+		LOG_WRN("Failed to load %d DTLS session: %d", ctx->sock_fd, ret);
 	} else {
 		LOG_DBG("DTLS session load ok");
-		dtls_stored = false;
 	}
+	dtls_stored = false;
 }
 
 void lwm2m_utils_connection_manage(struct lwm2m_ctx *client,
@@ -83,7 +83,7 @@ void lwm2m_utils_connection_manage(struct lwm2m_ctx *client,
 	case LWM2M_RD_CLIENT_EVENT_DISCONNECT:
 	case LWM2M_RD_CLIENT_EVENT_DEREGISTER_FAILURE:
 		if (IS_ENABLED(CONFIG_LWM2M_CLIENT_UTILS_DTLS_CON_MANAGEMENT)) {
-			/* Socket is cloded and dtls session also */
+			/* Socket is closed and dtls session also */
 			dtls_stored = false;
 		}
 		break;
