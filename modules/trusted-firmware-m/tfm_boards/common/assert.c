4e8a2cc887ac5f212387f28d8d8abb3c5ea44ae0
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-01-04 13:14:46 +0000

    modules: tfm: assert: Change space indent to tab
    Fixes a compliance issue by replacing space indents with tabs
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- modules/trusted-firmware-m/tfm_boards/common/assert.c
+++ modules/trusted-firmware-m/tfm_boards/common/assert.c
@@ -14,11 +14,11 @@
 /* Making sure the borders between secure and non-secure regions are aligned with the SPU regions */
 #if !(IS_ALIGNED_POW2(PM_TFM_NONSECURE_ADDRESS, CONFIG_NRF_SPU_FLASH_REGION_SIZE))
 #pragma message "\n\n!!!Partition alignment error!!!"\
-                "\nThe non-secure start address in pm_static.yml"\
-                " or generated partition.yml is: " M2S(PM_TFM_NONSECURE_ADDRESS)\
-                "\nwhich is not aligned with the SPU region size."\
-                "\nRefer to the documentation section 'TF-M partition alignment requirements'"\
-                "\nfor more information.\n\n"
+		"\nThe non-secure start address in pm_static.yml"\
+		" or generated partition.yml is: " M2S(PM_TFM_NONSECURE_ADDRESS)\
+		"\nwhich is not aligned with the SPU region size."\
+		"\nRefer to the documentation section 'TF-M partition alignment requirements'"\
+		"\nfor more information.\n\n"
 #error "TF-M non-secure start address is not aligned on SPU region size"
 #endif
 
