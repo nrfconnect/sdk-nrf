b1e2d9d850affda225a1c33a51c33095c86b2214
Graham Wacey <graham.wacey@nordicsemi.no>
2024-01-17 10:33:17 +0000

    applications: nrf5340_audio: Issues after upmerge
    	The get frequency and duration now return the enums directly from
    	the codec configuration structure. Before these were converted to
    	Hz and us values in the call. Added a call to the conversion
    	functions in codec.c and now display the correct values.
    
    Signed-off-by: Graham Wacey <graham.wacey@nordicsemi.no>

--- applications/nrf5340_audio/src/bluetooth/bt_stream/le_audio.c
+++ applications/nrf5340_audio/src/bluetooth/bt_stream/le_audio.c
@@ -34,3 +34,76 @@ bool le_audio_ep_state_check(struct bt_bap_ep *ep, enum bt_bap_ep_state state)
 
 	return false;
 }
+
+int le_audio_freq_hz_get(const struct bt_audio_codec_cfg *codec, int *freq_hz)
+{
+	int ret;
+
+	ret = bt_audio_codec_cfg_get_freq(codec);
+	if (ret < 0) {
+		*freq_hz = 0;
+		return ret;
+	}
+
+	ret = bt_audio_codec_cfg_freq_to_freq_hz(ret);
+	if (ret < 0) {
+		*freq_hz = 0;
+		return ret;
+	}
+
+	*freq_hz = ret;
+
+	return 0;
+}
+
+int le_audio_duration_us_get(const struct bt_audio_codec_cfg *codec, int *frame_dur_us)
+{
+	int ret;
+
+	ret = bt_audio_codec_cfg_get_frame_dur(codec);
+	if (ret < 0) {
+		*frame_dur_us = 0;
+		return ret;
+	}
+
+	ret = bt_audio_codec_cfg_frame_dur_to_frame_dur_us(ret);
+	if (ret < 0) {
+		*frame_dur_us = 0;
+		return ret;
+	}
+
+	*frame_dur_us = ret;
+
+	return 0;
+}
+
+int le_audio_octets_per_frame_get(const struct bt_audio_codec_cfg *codec, uint32_t *octets_per_sdu)
+{
+	int ret;
+
+	ret = bt_audio_codec_cfg_get_octets_per_frame(codec);
+	if (ret < 0) {
+		*octets_per_sdu = 0;
+		return ret;
+	}
+
+	*octets_per_sdu = ret;
+
+	return 0;
+}
+
+int le_audio_frame_blocks_per_sdu_get(const struct bt_audio_codec_cfg *codec,
+				      uint32_t *frame_blks_per_sdu)
+{
+	int ret;
+
+	ret = bt_audio_codec_cfg_get_frame_blocks_per_sdu(codec, true);
+	if (ret < 0) {
+		*frame_blks_per_sdu = 0;
+		return ret;
+	}
+
+	*frame_blks_per_sdu = ret;
+
+	return 0;
+}
