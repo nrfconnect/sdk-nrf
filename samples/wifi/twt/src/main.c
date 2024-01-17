8ff0bb0cbc6dc4819c6a7ac4767741f28fa8e9a2
Ajay Parida <ajay.parida@nordicsemi.no>
2024-01-17 19:00:19 +0530

    samples: wifi; twt: Log TWT teardown status
    TWT teardown status is notified on with teardown event.
    
    Signed-off-by: Ajay Parida <ajay.parida@nordicsemi.no>

--- samples/wifi/twt/src/main.c
+++ samples/wifi/twt/src/main.c
@@ -245,8 +245,13 @@ static void handle_wifi_twt_event(struct net_mgmt_event_callback *cb)
 		(const struct wifi_twt_params *)cb->info;
 
 	if (resp->operation == WIFI_TWT_TEARDOWN) {
-		LOG_INF("TWT teardown received for flow ID %d\n",
-		      resp->flow_id);
+		if (resp->teardown_status == WIFI_TWT_TEARDOWN_SUCCESS) {
+			LOG_INF("TWT teardown success received for flow ID %d\n",
+				resp->flow_id);
+		} else {
+			LOG_INF("TWT teardown failed for flow ID %d\n",
+				resp->flow_id);
+		}
 		return;
 	}
 
