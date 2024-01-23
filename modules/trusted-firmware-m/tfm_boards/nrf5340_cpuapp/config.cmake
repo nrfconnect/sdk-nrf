a49bd9c4ce298e1d814eee7d8e04a586c5af8405
Markus Swarowsky <markus.swarowsky@nordicsemi.no>
2024-01-23 15:59:48 +0100

    tf-m: Add NS build support for zephyr out-of-tree nordic_nrf platform.
    Due to the TF-M split build feature the separate build logic is needed
    to support Non-secure builds with the out-of-tree boards of the TF-M
    nordic_nrf platforms
    
    Signed-off-by: Markus Swarowsky <markus.swarowsky@nordicsemi.no>

--- modules/trusted-firmware-m/tfm_boards/nrf5340_cpuapp/config.cmake
+++ modules/trusted-firmware-m/tfm_boards/nrf5340_cpuapp/config.cmake
@@ -1,12 +1,13 @@
 #
-# Copyright (c) 2021 - 2023, Nordic Semiconductor ASA.
+# Copyright (c) 2021 - 2024, Nordic Semiconductor ASA.
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
 include(${CMAKE_CURRENT_LIST_DIR}/../common/config.cmake)
 
-set(PLATFORM_PATH platform/ext/target/nordic_nrf/)
+set(NRF_SOC_VARIANT nrf5340 CACHE STRING "nRF SoC Variant")
+
 include(${PLATFORM_PATH}/common/nrf5340/config.cmake)
 
 # Override the AEAD algorithm configuration
