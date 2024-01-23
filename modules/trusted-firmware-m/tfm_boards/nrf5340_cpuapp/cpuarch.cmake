18561a3aacffc299c9ffe3a3b5561d9f6c5e4d08
Markus Swarowsky <markus.swarowsky@nordicsemi.no>
2024-01-23 15:59:48 +0100

    tf-m: Add NS build support for zephyr out-of-tree nordic_nrf platform.
    Due to the TF-M split build feature the separate build logic is needed
    to support Non-secure builds with the out-of-tree boards of the TF-M
    nordic_nrf platforms
    
    Signed-off-by: Markus Swarowsky <markus.swarowsky@nordicsemi.no>

--- modules/trusted-firmware-m/tfm_boards/nrf5340_cpuapp/cpuarch.cmake
+++ modules/trusted-firmware-m/tfm_boards/nrf5340_cpuapp/cpuarch.cmake
@@ -1,8 +1,10 @@
 #
-# Copyright (c) 2021 - 2023, Nordic Semiconductor ASA.
+# Copyright (c) 2021 - 2024, Nordic Semiconductor ASA.
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
-include(platform/ext/target/nordic_nrf/common/nrf5340/cpuarch.cmake)
+set(PLATFORM_PATH platform/ext/target/nordic_nrf)
+
+include(${PLATFORM_PATH}/common/nrf5340/cpuarch.cmake)
 add_compile_definitions(__NRF_TFM__)
--- modules/trusted-firmware-m/tfm_boards/nrf9120/config.cmake
+++ modules/trusted-firmware-m/tfm_boards/nrf9120/config.cmake
@@ -1,13 +1,14 @@
 #
-# Copyright (c) 2023, Nordic Semiconductor ASA.
+# Copyright (c) 2023-2024, Nordic Semiconductor ASA.
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
 include(${CMAKE_CURRENT_LIST_DIR}/../common/config.cmake)
 
-set(PLATFORM_PATH platform/ext/target/nordic_nrf/)
-include(${PLATFORM_PATH}/common/nrf91/config.cmake)
+set(NRF_SOC_VARIANT nrf91 CACHE STRING "nRF SoC Variant")
+
+include(${PLATFORM_PATH}/common/${NRF_SOC_VARIANT}/config.cmake)
 
 # Override the AEAD algorithm configuration since nRF91 series supports only AES_CCM
 set(PS_CRYPTO_AEAD_ALG                  PSA_ALG_CCM CACHE STRING    "The AEAD algorithm to use for authenticated encryption in Protected Storage")
