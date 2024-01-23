b34561c0f5ca7cc8b7a23a3fa4395a5dcc0fec22
Markus Swarowsky <markus.swarowsky@nordicsemi.no>
2024-01-23 15:59:48 +0100

    tf-m: Add NS build support for zephyr out-of-tree nordic_nrf platform.
    Due to the TF-M split build feature the separate build logic is needed
    to support Non-secure builds with the out-of-tree boards of the TF-M
    nordic_nrf platforms
    
    Signed-off-by: Markus Swarowsky <markus.swarowsky@nordicsemi.no>

--- modules/trusted-firmware-m/tfm_boards/nrf9120/cpuarch.cmake
+++ modules/trusted-firmware-m/tfm_boards/nrf9120/cpuarch.cmake
@@ -1,8 +1,10 @@
 #
-# Copyright (c) 2023, Nordic Semiconductor ASA.
+# Copyright (c) 2023-2024, Nordic Semiconductor ASA.
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
-include(platform/ext/target/nordic_nrf/common/nrf9120/cpuarch.cmake)
+set(PLATFORM_PATH platform/ext/target/nordic_nrf)
+
+include(${PLATFORM_PATH}/common/nrf9120/cpuarch.cmake)
 add_compile_definitions(__NRF_TFM__)
