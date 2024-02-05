c71a95bed9709bb008d3f7c3e32860f2df264d7d
Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>
2024-02-05 16:12:16 +0100

    Bluetooth: Controller: Kconfig: Add entry for connection event extension
    Define CONFIG_BT_CTLR_SDC_CONN_EVENT_EXTEND_DEFAULT to allow
    users to enable and disable connection event extension in a simpler
    way.
    
    Signed-off-by: Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>

--- subsys/bluetooth/controller/hci_driver.c
+++ subsys/bluetooth/controller/hci_driver.c
@@ -1192,6 +1192,15 @@ static int hci_driver_open(void)
 			MULTITHREADING_LOCK_RELEASE();
 			return -ENOTSUP;
 		}
+
+		sdc_hci_cmd_vs_conn_event_extend_t event_extend_params = {
+			.enable = CONFIG_BT_CTLR_SDC_CONN_EVENT_EXTEND_DEFAULT
+		};
+		err = sdc_hci_cmd_vs_conn_event_extend(&event_extend_params);
+		if (err) {
+			MULTITHREADING_LOCK_RELEASE();
+			return -ENOTSUP;
+		}
 	}
 
 	if (IS_ENABLED(CONFIG_BT_CENTRAL)) {
