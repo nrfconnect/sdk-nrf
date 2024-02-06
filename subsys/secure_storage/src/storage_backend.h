e2d0b651b4176d09f4256042bbb4b4d95a67e5a5
Vidar Lillebø <vidar.lillebo@nordicsemi.no>
2024-02-06 16:26:49 +0100

    secure_storage: Store data in one file
    Instead of spreading nonce, size, flags and data over several files
    these are now stored in one blob. This should solve issues related
    to cleanup when one of the storage operation fails.
    
    Ref: NCSDK-25493
    Signed-off-by: Vidar Lillebø <vidar.lillebo@nordicsemi.no>

--- subsys/secure_storage/src/storage_backend.h
+++ subsys/secure_storage/src/storage_backend.h
@@ -10,16 +10,15 @@
 #include <psa/error.h>
 #include <psa/storage_common.h>
 
-/* Gets an object of the exact object_size size */
-psa_status_t storage_get_object(const psa_storage_uid_t uid, const char *prefix, const char *suffix,
-				uint8_t *object_data, const size_t object_size);
+/* Gets an object up to object_size size */
+psa_status_t storage_get_object(const psa_storage_uid_t uid, const char *prefix, void *object_data,
+				const size_t object_size, size_t *object_length);
 
 /* Writes an object */
-psa_status_t storage_set_object(const psa_storage_uid_t uid, const char *prefix, char *suffix,
-				const uint8_t *object_data, const size_t object_size);
+psa_status_t storage_set_object(const psa_storage_uid_t uid, const char *prefix,
+				const void *object_data, const size_t object_size);
 
 /* Deletes an object */
-psa_status_t storage_remove_object(const psa_storage_uid_t uid, const char *prefix,
-				   const char *suffix);
+psa_status_t storage_remove_object(const psa_storage_uid_t uid, const char *prefix);
 
 #endif /* __STORAGE_BACKEND_H_*/
