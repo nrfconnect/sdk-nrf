4bc7e3f32791fc97061871ff798d712bdafd1d61
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2024-02-25 22:27:18 +0530

    drivers: wifi: Fix formatting
    Improves readability esp. for init.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- drivers/wifi/nrf700x/src/qspi/src/device.c
+++ drivers/wifi/nrf700x/src/qspi/src/device.c
@@ -21,15 +21,19 @@
 static struct qspi_config config;
 
 #if defined(CONFIG_NRF700X_ON_QSPI)
-static struct qspi_dev qspi = { .init = qspi_init,
-			 .read = qspi_read,
-			 .write = qspi_write,
-			 .hl_read = qspi_hl_read};
+static struct qspi_dev qspi = {
+	.init = qspi_init,
+	.read = qspi_read,
+	.write = qspi_write,
+	.hl_read = qspi_hl_read
+};
 #else
-static struct qspi_dev spim = { .init = spim_init,
-			 .read = spim_read,
-			 .write = spim_write,
-			 .hl_read = spim_hl_read};
+static struct qspi_dev spim = {
+	.init = spim_init,
+	.read = spim_read,
+	.write = spim_write,
+	.hl_read = spim_hl_read
+};
 #endif
 
 struct qspi_config *qspi_defconfig(void)
