ed95bd2a5b97412f4b5b6f1104df51d681144163
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2024-02-27 01:42:39 +0530

    location: Fix CMock unit test
    Now that we are usign DT macros for nRF70 Wi-Fi, the CMock which runs on
    native posix board fails as there is no Wi-Fi interface on native posix
    and no mocking for DT APIs.
    
    To solve this add a board specific overlay with the Wi-Fi DT label
    defined and also add a dummy Wi-Fi device instance.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
    Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

--- tests/subsys/net/lib/lwm2m_client_utils/src/dummy_wlan.c
+++ tests/subsys/net/lib/lwm2m_client_utils/src/dummy_wlan.c
@@ -3,33 +3,10 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
-#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 
-
-#define DUMMY_WLAN_NAME	"wlan0"
-
-typedef int (*dummy_api_configure_t)(const struct device *dev,
-				     uint32_t dev_config);
-
-
-struct dummy_wlan_api {
-	dummy_api_configure_t configure;
-};
-
-static int dummy_configure(const struct device *dev, uint32_t config)
-{
-	return 0;
-}
-
-static const struct dummy_wlan_api funcs = {
-	.configure = dummy_configure,
-};
-
-int dummy_init(const struct device *dev)
-{
-	return 0;
-}
-
-DEVICE_DEFINE(dummy_wlan, DUMMY_WLAN_NAME, dummy_init, NULL, NULL, NULL,
-	      POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &funcs);
+/* Define a dummy driver just that the linker finds it. Otherwise we get a complaint like:
+ *     undefined reference to `__device_dts_ord_12'
+ */
+#define DT_DRV_COMPAT nordic_wlan0
+DEVICE_DT_INST_DEFINE(0, NULL, NULL, NULL, NULL, POST_KERNEL, 0, NULL);
