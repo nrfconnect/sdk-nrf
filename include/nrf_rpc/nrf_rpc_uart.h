9b0a8279c5f290eb98d071061b6d0aead16f5412
Przemyslaw Bida <przemyslaw.bida@nordicsemi.no>
2024-03-04 17:32:04 +0100

    nrf_rpc_uart: transport refactoring
    nrf_rpc_uart: fix hdlc encoding
    
    The code couldn't properly encode and decode a frame that
    contained 7d byte (delimiter). Change the encoder to XOR
    an escaped byte with 0x20 like in the original HDLC spec
    and fix the decoder to properly handle escaped bytes.
    
    Also, increase the max frame size to 1536 to be able to
    send large IPv6 packets over RPC and move the frame buffer
    to the transport context.
    
    nrf_rpc_uart: fix RX buffer usage
    
    Use a different buffer for the RX ring buffer and for
    reading bytes from UART peripherals. This fixes the issue
    with losing first byte on one side of the communication
    after reboot.
    
    Signed-off-by: Przemyslaw Bida <przemyslaw.bida@nordicsemi.no>
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- include/nrf_rpc/nrf_rpc_uart.h
+++ include/nrf_rpc/nrf_rpc_uart.h
@@ -10,11 +10,35 @@
 #include <zephyr/device.h>
 #include <nrf_rpc.h>
 #include <nrf_rpc_tr.h>
+#include <zephyr/sys/ring_buffer.h>
+
+#define NRF_RPC_MAX_FRAME_SIZE	1536
+#define NRF_RPC_RX_RINGBUF_SIZE 64
+
+typedef enum {
+	hdlc_state_unsync,
+	hdlc_state_frame_start,
+	hdlc_state_frame_found,
+	hdlc_state_escape,
+} hdlc_state_t;
 
 typedef struct nrf_rpc_uart {
-    const struct device *uart;
+	const struct device *uart;
 	nrf_rpc_tr_receive_handler_t receive_callback;
+	void *receive_ctx;
 	const struct nrf_rpc_tr *transport;
+
+	/* RX buffer populated by UART ISR */
+	uint8_t rx_buffer[NRF_RPC_RX_RINGBUF_SIZE];
+	struct ring_buf rx_ringbuf;
+
+	/* ISR function callback worker */
+	struct k_work cb_work;
+
+	/* HDLC frame parsing state */
+	hdlc_state_t hdlc_state;
+	uint8_t frame_buffer[NRF_RPC_MAX_FRAME_SIZE];
+	size_t frame_len;
 } nrf_rpc_uart;
 
 extern const struct nrf_rpc_tr_api nrf_rpc_uart_service_api;
