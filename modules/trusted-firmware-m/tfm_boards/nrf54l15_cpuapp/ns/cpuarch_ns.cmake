2888238941b2b666b00b3f96466bd56fe09e7a27
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2024-03-18 14:55:05 +0100

    tfm: rewrite 54L TF-M CMake support
    We move most of the 54L TF-M CMake support to TF-M.
    
    This was done to make 54L align as much as possible with other
    platforms.
    
    This makes comparing different platforms easier as all files are in
    similarly placed directories.
    
    We also port the remaining existing CMake code to the new TF-M
    revision.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- modules/trusted-firmware-m/tfm_boards/nrf54l15_cpuapp/ns/cpuarch_ns.cmake
+++ modules/trusted-firmware-m/tfm_boards/nrf54l15_cpuapp/ns/cpuarch_ns.cmake
@@ -4,4 +4,7 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
-include(../cpuarch.cmake)
+set(PLATFORM_DIR  ${CMAKE_CURRENT_LIST_DIR})
+set(PLATFORM_PATH ${CMAKE_CURRENT_LIST_DIR})
+
+include(${CMAKE_CURRENT_LIST_DIR}/common/nrf54l15/cpuarch.cmake)
