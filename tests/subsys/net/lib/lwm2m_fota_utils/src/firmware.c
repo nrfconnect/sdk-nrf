ed839022434ddc5efde976dec2619ef6d11d7b48
Seppo Takalo <seppo.takalo@nordicsemi.no>
2024-04-03 13:42:30 +0300

    net: lib_ fota_download: Return errors from URI parsing
    Parse URI before starting the download client
    and return parsing failures from _start().
    
    Also, check that download_client parses the protocol,
    which means that it knows the protocol.
    
    Signed-off-by: Seppo Takalo <seppo.takalo@nordicsemi.no>

--- tests/subsys/net/lib/lwm2m_fota_utils/src/firmware.c
+++ tests/subsys/net/lib/lwm2m_fota_utils/src/firmware.c
@@ -543,7 +543,7 @@ ZTEST(lwm2m_client_utils_firmware, test_firmware_pull_failures_cb)
 	printf("State %d\r\n", state);
 	zassert_equal(state, STATE_DOWNLOADING, "wrong result value");
 	zassert_not_null(firmware_fota_download_cb, "Fota client cb is NULL");
-	pull_callback_event_stub(FOTA_DOWNLOAD_EVT_ERROR, FOTA_DOWNLOAD_ERROR_CAUSE_NO_ERROR);
+	pull_callback_event_stub(FOTA_DOWNLOAD_EVT_ERROR, FOTA_DOWNLOAD_ERROR_CAUSE_CONNECT_FAILED);
 	result = get_app_result();
 	state = get_app_state();
 	zassert_equal(result, RESULT_CONNECTION_LOST, "wrong result value");
