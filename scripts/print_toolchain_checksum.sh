cd56080d6bce328a5e98322ecab876ee716098b6
Jan Gałda <jan.galda@nordicsemi.no>
2024-04-08 10:26:58 +0200

    tools: Use yaml files to calculate toolchain id
    If yaml files are present, toolchain bundler uses them instead of txt
     files as source of required tools.
    
    Signed-off-by: Jan Gałda <jan.galda@nordicsemi.no>

--- scripts/print_toolchain_checksum.sh
+++ scripts/print_toolchain_checksum.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(dirname "$0")
 REQUIREMENTS=$BASEDIR/requirements-fixed.txt
-TOOLS_VERSIONS=$BASEDIR/tools-versions-linux.txt
-TOOLCHAIN_VERSION=$(cat $REQUIREMENTS $TOOLS_VERSIONS | sha256sum | head -c 10)
+TOOLS_VERSIONS=$BASEDIR/tools-versions-linux.yml
+TOOLCHAIN_VERSION=$(cat $REQUIREMENTS $TOOLS_VERSIONS | tr -d '\r' | sha256sum | head -c 10)
 
 echo "${TOOLCHAIN_VERSION}"
