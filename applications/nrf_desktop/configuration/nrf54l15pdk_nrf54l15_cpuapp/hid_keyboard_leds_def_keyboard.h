3748e962103c9135517f1e4134b79f2e1842189d
Marek Pieta <Marek.Pieta@nordicsemi.no>
2024-04-22 23:14:53 +0200

    applications: nrf_desktop: Use PWM LEDs for nRF54L PDK
    Change switches to using PWM LEDs for nRF54L PDK to improve user
    experience.
    
    Jira: NCSDK-25056
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>
    Signed-off-by: Anna Wojdylo <anna.wojdylo@nordicsemi.no>

--- applications/nrf_desktop/configuration/nrf54l15pdk_nrf54l15_cpuapp/hid_keyboard_leds_def_keyboard.h
+++ applications/nrf_desktop/configuration/nrf54l15pdk_nrf54l15_cpuapp/hid_keyboard_leds_def_keyboard.h
@@ -15,13 +15,23 @@
  */
 const struct {} hid_keyboard_leds_def_include_once;
 
+/* On the nRF54L15 SoC, you can only use the **GPIO1** port for PWM hardware peripheral output.
+ * Because of that, the PDK PCA10156 has the following limitations:
+ *
+ * - On the PDK revision v0.2.1, **LED 1** cannot be used for PWM output.
+ * - On the PDK revision v0.3.0, **LED 0** and **LED 2** cannot be used for PWM output.
+ *
+ * You can still use these LEDs with the PWM LED driver, but you must set the LED color to
+ * ``LED_COLOR(255, 255, 255)`` or ``LED_COLOR(0, 0, 0)``. This ensures the PWM peripheral is not
+ * used for the mentioned LEDs.
+ */
 static const struct led_effect keyboard_led_on = LED_EFFECT_LED_ON(LED_COLOR(255, 255, 255));
 static const struct led_effect keyboard_led_off = LED_EFFECT_LED_OFF();
 
 /* Map HID keyboard LEDs to application LED IDs. */
 static const uint8_t keyboard_led_map[] = {
 	[HID_KEYBOARD_LEDS_NUM_LOCK] = 2,
-	[HID_KEYBOARD_LEDS_CAPS_LOCK] = 3,
+	[HID_KEYBOARD_LEDS_CAPS_LOCK] = LED_UNAVAILABLE,
 	[HID_KEYBOARD_LEDS_SCROLL_LOCK] = LED_UNAVAILABLE,
 	[HID_KEYBOARD_LEDS_COMPOSE] = LED_UNAVAILABLE,
 	[HID_KEYBOARD_LEDS_KANA] = LED_UNAVAILABLE,
