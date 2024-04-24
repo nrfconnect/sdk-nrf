9ee681116e4e7a7dba5f2db2aeb14bcc587170f2
Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>
2024-04-24 12:37:05 +0200

    samples: Power_Management: Add hello world con for 3 cores
    Add configuration for app + rad + ppr cores for hello world on nrf54h20
    
    Jira: none
    
    Signed-off-by: Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>

--- samples/multicore/hello_world/sysbuild.cmake
+++ samples/multicore/hello_world/sysbuild.cmake
@@ -14,6 +14,16 @@ ExternalZephyrProject_Add(
     SOURCE_DIR ${APP_DIR}/remote
     BOARD ${SB_CONFIG_REMOTE_BOARD}
   )
+if(NOT "${SB_CONFIG_REMOTE_BOARD_2}" STREQUAL "")
+  message( WARNING "Using settup for power management tests")
+  # Add remote project
+  ExternalZephyrProject_Add(
+    APPLICATION remote_2
+    SOURCE_DIR ${APP_DIR}/remote
+    BOARD ${SB_CONFIG_REMOTE_BOARD_2}
+  )
+endif()
+
 set_property(GLOBAL APPEND PROPERTY PM_DOMAINS CPUNET)
 set_property(GLOBAL APPEND PROPERTY PM_CPUNET_IMAGES remote)
 set_property(GLOBAL PROPERTY DOMAIN_APP_CPUNET remote)
