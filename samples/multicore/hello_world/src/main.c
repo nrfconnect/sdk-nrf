2deae075876832a04a7160fead4372b69c97246f
Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>
2024-04-24 12:37:05 +0200

    samples: Power_Management: Add hello world con for 3 cores
    Add configuration for app + rad + ppr cores for hello world on nrf54h20
    
    Jira: none
    
    Signed-off-by: Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>

--- samples/multicore/hello_world/src/main.c
+++ samples/multicore/hello_world/src/main.c
@@ -7,10 +7,17 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/logging/log.h>
+#include <hal/nrf_memconf.h>
 LOG_MODULE_REGISTER(app);
 
 int main(void)
 {
+
+#if defined(CONFIG_BOARD_NRF54H20DK_NRF54H20_CPUPPR)
+		csr_write(VPRCSR_NORDIC_VPRNORDICSLEEPCTRL,
+			VPRCSR_NORDIC_VPRNORDICSLEEPCTRL_SLEEPSTATE_HIBERNATE);
+#endif
+
 	int cnt = 0;
 
 	while (1) {
