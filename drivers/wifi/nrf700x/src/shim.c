59d9bd86d020714041adc8bc20c926ffdf728b4b
Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>
2024-04-29 22:46:24 +0530

    drivers: wifi: Create dedicated mem pool for Wi-Fi driver
    Create dedicated memory pools for Wi-Fi management and
    data operations (defaults: 20KB for management and 60KB for data).
    
    Reduce the heap memory pool size since the Wi-Fi driver will
    be allocating memory from a dedicated pool.
    
    Signed-off-by: Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>

--- drivers/wifi/nrf700x/src/shim.c
+++ drivers/wifi/nrf700x/src/shim.c
@@ -18,6 +18,7 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/__assert.h>
+#include <zephyr/sys/math_extras.h>
 
 #include "rpu_hw_if.h"
 #include "shim.h"
@@ -27,19 +28,73 @@
 #include "qspi_if.h"
 
 LOG_MODULE_REGISTER(wifi_nrf, CONFIG_WIFI_NRF700X_LOG_LEVEL);
+K_HEAP_DEFINE(wifi_drv_mgmt_mem_pool, CONFIG_NRF_WIFI_CONFIG_HEAP_MEM_POOL_SIZE);
+K_HEAP_DEFINE(wifi_drv_pkt_mem_pool, CONFIG_NRF_WIFI_PKT_HEAP_MEM_POOL_SIZE);
 
 struct zep_shim_intr_priv *intr_priv;
 
 static void *zep_shim_mem_alloc(size_t size)
 {
 	size = (size + 4) & 0xfffffffc;
-	return k_malloc(size);
+	return k_heap_alloc(&wifi_drv_mgmt_mem_pool, size, K_FOREVER);
+}
+
+static void *zep_shim_pkt_mem_alloc(size_t size)
+{
+	size = (size + 4) & 0xfffffffc;
+	return k_heap_alloc(&wifi_drv_pkt_mem_pool, size, K_FOREVER);
 }
 
 static void *zep_shim_mem_zalloc(size_t size)
 {
+	void *ret;
+    size_t bounds;
+
 	size = (size + 4) & 0xfffffffc;
-	return k_calloc(size, sizeof(char));
+
+	if (size_mul_overflow(size, sizeof(char), &bounds)) {
+		return NULL;
+	}
+
+	ret = zep_shim_mem_alloc(bounds);
+	if (ret != NULL) {
+		(void)memset(ret, 0, bounds);
+	}
+
+	return ret;
+}
+
+static void *zep_shim_pkt_mem_zalloc(size_t size)
+{
+	void *ret;
+    size_t bounds;
+
+	size = (size + 4) & 0xfffffffc;
+
+	if (size_mul_overflow(size, sizeof(char), &bounds)) {
+		return NULL;
+	}
+
+	ret = zep_shim_pkt_mem_alloc(bounds);
+	if (ret != NULL) {
+		(void)memset(ret, 0, bounds);
+	}
+
+	return ret;
+}
+
+static void zep_shim_mem_free(void *buf)
+{
+	if (buf) {
+		k_heap_free(&wifi_drv_mgmt_mem_pool, buf);
+	}
+}
+
+static void zep_shim_pkt_mem_free(void *buf)
+{
+	if (buf) {
+		k_heap_free(&wifi_drv_pkt_mem_pool, buf);
+	}
 }
 
 static void *zep_shim_mem_cpy(void *dest, const void *src, size_t count)
@@ -877,7 +932,9 @@ static unsigned int zep_shim_strlen(const void *str)
 static const struct nrf_wifi_osal_ops nrf_wifi_os_zep_ops = {
 	.mem_alloc = zep_shim_mem_alloc,
 	.mem_zalloc = zep_shim_mem_zalloc,
-	.mem_free = k_free,
+	.pkt_mem_zalloc = zep_shim_pkt_mem_zalloc,
+	.mem_free = zep_shim_mem_free,
+	.pkt_mem_free = zep_shim_pkt_mem_free,
 	.mem_cpy = zep_shim_mem_cpy,
 	.mem_set = zep_shim_mem_set,
 	.mem_cmp = zep_shim_mem_cmp,
