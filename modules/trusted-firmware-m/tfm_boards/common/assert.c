f8c6e60ad55c6bccb64bba655ba8014a6d537675
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2024-05-07 15:07:28 +0200

    tf-m: Update non-secure region definitions
    In nRF53 and nRF91 series the security attributes (TrustZone)
    for the flash and the RAM regions are configured using the SPU
    peripheral. The SPU peripheral adds some hardware limitations regarding
    the partitions.
    These are:
    1) The veneers needs to be placed at the end of a secure partition
    2) The size of veneers region is limited by the FLASHNSC[n].SIZE options
    3) The flash/RAM regions need to be aligned to the SPU region size
    
    In nRF54L15 the flash and the RAM security attributes are configured
    using the SAU and the MPC which have different limitations.
    The MPC has smaller region size size of 4K allowing better granularity.
    The non-secure callable region doesn't have the limitations 1 and 2.
    It only needs to be aligned by 32 bytes as the SAU requires.
    
    This change updates the region_definition header to remove the
    non-secure callable limitations for nRF54L15. It also updates
    the assertion logic and the relevant documentation.
    
    Ref: NCSDK-25023
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

--- modules/trusted-firmware-m/tfm_boards/common/assert.c
+++ modules/trusted-firmware-m/tfm_boards/common/assert.c
@@ -12,21 +12,28 @@
 #define IS_ALIGNED_POW2(value, align) (((value) & ((align)-1)) == 0)
 
 /* Making sure the borders between secure and non-secure regions are
- * aligned with the SPU regions
+ * aligned with the TrustZone SPU/MPC requirements.
  */
-#if !(IS_ALIGNED_POW2(PM_TFM_NONSECURE_ADDRESS, CONFIG_NRF_SPU_FLASH_REGION_ALIGNMENT))
+#if !(IS_ALIGNED_POW2(PM_TFM_NONSECURE_ADDRESS, CONFIG_NRF_TRUSTZONE_FLASH_REGION_SIZE))
 #pragma message \
 	"\n\n!!!Partition alignment error!!!" \
 	"\nThe non-secure start address in pm_static.yml" \
 	" or generated partition.yml is: " \
 	M2S(PM_TFM_NONSECURE_ADDRESS) \
-	"\nwhich is not aligned with the SPU HW requirements."				\
+	"\nwhich is not aligned with the SPU/MPC HW requirements."				\
+	"\nIn nRF53/nRF91 series the flash region need to be aligned with the SPU region size." \
+	"\nIn nRF54L15 the flash region need to be aligned with the MPC region size." \
 	"\nRefer to the documentation section 'TF-M partition alignment requirements'" \
 	"\nfor more information.\n\n"
 
-#error "TF-M non-secure start address is not aligned to SPU HW requirements"
+#error "TF-M non-secure start address is not aligned to SPU/MPC HW requirements"
 #endif
 
-#if !(IS_ALIGNED_POW2(PM_SRAM_NONSECURE_ADDRESS, CONFIG_NRF_SPU_RAM_REGION_ALIGNMENT))
-#error "SRAM non-secure address is not aligned to SPU HW requirements"
+#if !(IS_ALIGNED_POW2(PM_SRAM_NONSECURE_ADDRESS, CONFIG_NRF_TRUSTZONE_RAM_REGION_SIZE))
+#pragma message \
+	"SRAM non-secure address is not aligned to SPU/MPC HW requirements" \
+	"\nIn nRF53/nRF91 series the RAM region need to be aligned with the SPU region size." \
+	"\nIn nRF54L15 the RAM region need to be aligned with the MPC region size.\n\n"
+
+#error "SRAM non-secure start address is not aligned to SPU/MPC HW requirements"
 #endif
