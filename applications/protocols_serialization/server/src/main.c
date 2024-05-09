e7f27585b968fefd127ff905efb9c566469c9ce4
Damian Krolik <damian.krolik@nordicsemi.no>
2024-05-09 20:32:24 +0200

    applications: remove delay in protocols_serialization startup
    Remove the now unnecessary startup delay after adding
    support for 2-way handshake nRF RPC group initialization.
    
    Also, remove unnecessary code from the application and
    switch all printk() occurrences to logging macros.
    
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- applications/protocols_serialization/server/src/main.c
+++ applications/protocols_serialization/server/src/main.c
@@ -4,60 +4,31 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#include <zephyr/device.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/drivers/uart.h>
 
-#include <zephyr/kernel.h>
-#include <zephyr/device.h>
-
-#include <nrf_rpc_tr.h>
-#include <nrf_rpc/nrf_rpc_uart.h>
-#include <nrf_rpc_errno.h>
-#include <nrf_rpc_cbor.h>
-#include <sys/errno.h>
+#include <nrf_rpc.h>
 
 LOG_MODULE_REGISTER(nrf_rpc_remote, CONFIG_NRF_RPC_REMOTE_LOG_LEVEL);
 
 static void err_handler(const struct nrf_rpc_err_report *report)
 {
-	printk("nRF RPC error %d ocurred. See nRF RPC logs for more details.",
-	       report->code);
+	LOG_ERR("nRF RPC error %d ocurred. See nRF RPC logs for more details", report->code);
 	//k_oops();
 }
-
-static int serialization_init(void)
-{
-
-	int err;
-
-	printk("Init begin\n");
-
-	err = nrf_rpc_init(err_handler);
-	if (err) {
-		LOG_ERR("Init failed %d\n", err);
-		return -NRF_EINVAL;
-	}
-
-	printk("Init done\n");
-
-	return 0;
-}
-
-extern volatile int num_interrupts;
 
 int main(void)
 {
 	int ret;
 
-	k_sleep(K_MSEC(3000));
-	ret = serialization_init();
+	LOG_INF("Initializing RPC server");
+
+	ret = nrf_rpc_init(err_handler);
 
 	if (ret != 0) {
-		LOG_ERR("Init RPC Failed.");
+		LOG_ERR("RPC init failed");
 	}
 
-	printk("Welcome to RPC server\r\n");
+	LOG_INF("RPC server ready");
 
 	return 0;
 }
