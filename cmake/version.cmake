1e7ac0a57f145ddd9d5eed4564c5e721d885b8e2
Gregers Gram Rygg <gregers.gram.rygg@nordicsemi.no>
2024-05-14 10:02:44 +0200

    kernel: Fix git commit banner when using worktrees
    The gitdir is in a different location when using git worktrees. This
    commit looks up the absolute path to the gitdir before adding the
    gitdir dependencies.
    
    Signed-off-by: Gregers Gram Rygg <gregers.gram.rygg@nordicsemi.no>

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -21,6 +21,29 @@ add_custom_command(
 add_custom_target(ncs_version_h DEPENDS ${PROJECT_BINARY_DIR}/include/generated/ncs_version.h)
 add_dependencies(version_h ncs_version_h)
 
+find_package(Git QUIET)
+if(GIT_FOUND)
+  # The gitdir folder will not be under NRF_DIR when using git worktrees. Use git rev-parse to find
+  # the absolute path to the git directory.
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} rev-parse --absolute-git-dir
+    WORKING_DIRECTORY                ${NRF_DIR}
+    OUTPUT_VARIABLE                  ABSOLUTE_GIT_DIR
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_STRIP_TRAILING_WHITESPACE
+    ERROR_VARIABLE                   stderr
+    RESULT_VARIABLE                  return_code
+  )
+
+  if(return_code)
+    message(STATUS "git rev-parse failed: ${stderr}")
+  elseif(NOT "${stderr}" STREQUAL "")
+    message(STATUS "git rev-parse warned: ${stderr}")
+  endif()
+else()
+  set(ABSOLUTE_GIT_DIR ${NRF_DIR}/".git")
+endif()
+
 add_custom_command(
   OUTPUT ${PROJECT_BINARY_DIR}/include/generated/ncs_commit.h
   COMMAND ${CMAKE_COMMAND} -DZEPHYR_BASE=${ZEPHYR_BASE} -DNRF_DIR=${NRF_DIR}
@@ -28,7 +51,7 @@ add_custom_command(
     -DCOMMIT_TYPE=NCS
     -DCOMMIT_PATH=${NRF_DIR}
     -P ${ZEPHYR_NRF_MODULE_DIR}/cmake/gen_commit_h.cmake
-    DEPENDS ${NRF_DIR}/VERSION ${NRF_DIR}/.git ${NRF_DIR}/.git/index
+    DEPENDS ${NRF_DIR}/VERSION ${ABSOLUTE_GIT_DIR} ${ABSOLUTE_GIT_DIR}/index
 )
 add_custom_target(ncs_commit_h DEPENDS ${PROJECT_BINARY_DIR}/include/generated/ncs_commit.h)
 add_dependencies(version_h ncs_commit_h)
