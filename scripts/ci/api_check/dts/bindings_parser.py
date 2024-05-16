doki-nordic/fw-nrfconnect-nrf

scripts: ci: Detect API-breaking changes in the PRs
This script will check the PR's changes in the header files and DTS
bindings that may affects compatibility of the public API. This will
reduce risk of accidentally breaking the API. Workflow that runs
on each PR will add a comment with analysis summary.

Signed-off-by: Dominik Kilian <Dominik.Kilian@nordicsemi.no>

scripts/ci/api_check/dts/bindings_parser.py
@@ -1,0 +1,110 @@
+# Copyright (c) 2024 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+
+import sys
+import pickle
+from pathlib import Path
+from utils import devicetree_sources, warning
+
+if devicetree_sources:
+    sys.path.insert(0, devicetree_sources)
+
+from devicetree import edtlib
+
+
+class ParseResult:
+    bindings: 'list[Binding]'
+    binding_by_name: 'dict[str, Binding]'
+    def __init__(self):
+        self.bindings = []
+        self.binding_by_name = {}
+
+class Property:
+    name: str
+    type: str
+    description: str
+    enum: 'set[str]'
+    const: 'str | None'
+    default: 'str | None'
+    deprecated: bool
+    required: bool
+    specifier_space: str
+
+    def __init__(self, prop: edtlib.PropertySpec):
+        self.name = prop.name
+        self.type = prop.type or ''
+        self.description = prop.description or ''
+        self.enum = set([ str(x) for x in (prop.enum or []) ])
+        self.const = str(prop.const) if prop.const else None
+        self.default = str(prop.default) if prop.default else None
+        self.deprecated = prop.deprecated or False
+        self.required = prop.required or False
+        self.specifier_space = str(prop.specifier_space or '')
+
+class Binding:
+    path: str
+    name: str
+    description: str
+    cells: str
+    buses: str
+    properties: 'dict[str, Property]'
+
+    def __init__(self, binding: edtlib.Binding, file: Path):
+        self.path = str(file)
+        self.name = binding.compatible or self.path
+        if binding.on_bus is not None:
+            self.name += '@' + binding.on_bus
+        self.description = binding.description or ''
+        cells_array = [
+            f'{name}={";".join(value)}' for name, value in (binding.specifier2cells or {}).items()
+        ]
+        cells_array.sort()
+        self.cells = '&'.join(cells_array)
+        busses_array = list(binding.buses or [])
+        busses_array.sort()
+        self.buses = ';'.join(busses_array)
+        self.properties = {}
+        for key, value in (binding.prop2specs or {}).items():
+            prop = Property(value)
+            self.properties[key] = prop
+
+
+def get_binding_files(bindings_dirs: 'list[Path]') -> 'list[Path]':
+    binding_files = []
+    for bindings_dir in bindings_dirs:
+        if not bindings_dir.is_dir():
+            raise FileNotFoundError(f'Bindings directory "{bindings_dir}" not found.')
+        for file in bindings_dir.glob('**/*.yaml'):
+            binding_files.append(file)
+        for file in bindings_dir.glob('**/*.yml'):
+            binding_files.append(file)
+    return binding_files
+
+
+def parse_bindings(dirs_or_pickle: 'list[Path]|Path') -> ParseResult:
+    result = ParseResult()
+    if isinstance(dirs_or_pickle, list):
+        yaml_files = get_binding_files(dirs_or_pickle)
+        fname2path: 'dict[str, str]' = {
+            path.name: str(path) for path in yaml_files
+        }
+        for binding_file in yaml_files:
+            try:
+                binding = Binding(edtlib.Binding(str(binding_file), fname2path, None, False, False), binding_file)
+                if binding.name in result.binding_by_name:
+                    warning(f'Repeating binding {binding.name}: {binding.path}  {result.binding_by_name[binding.name].path}')
+                result.bindings.append(binding)
+                result.binding_by_name[binding.name] = binding
+            except edtlib.EDTError as err:
+                warning(err)
+    else:
+        with open(dirs_or_pickle, 'rb') as fd:
+            result = pickle.load(fd)
+    return result
+
+
+def save_bindings(parse_result: ParseResult, file: Path):
+    with open(file, 'wb') as fd:
+        pickle.dump(parse_result, fd)
+

scripts/ci/api_check/dts/main.py
@@ -1,0 +1,132 @@
+# Copyright (c) 2024 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+
+import os
+import sys
+import json
+import out_text
+from json import JSONEncoder
+from pathlib import Path
+from utils import error
+from args import args
+from bindings_parser import parse_bindings, save_bindings
+from compare import compare
+
+
+def collect_zephyr_inputs() -> 'list[Path]':
+    zephyr_base = os.getenv('ZEPHYR_BASE')
+    if zephyr_base is None:
+        error('You must specify "ZEPHYR_BASE" if you using "-" argument as an input.')
+        sys.exit(1)
+    zephyr_base = Path(zephyr_base)
+    west_root = zephyr_base.parent
+    bindings_dirs: 'list[Path]' = []
+    for bindings_dir in west_root.glob('**/dts/bindings'):
+        rel = bindings_dir.relative_to(west_root)
+        if str(rel).count('test') or str(rel).count('sample'):
+            continue
+        bindings_dirs.append(bindings_dir)
+    return bindings_dirs
+
+
+def collect_inputs(arguments: 'list[list[str]]') -> 'list[Path]|Path':
+    result_list: 'list[Path]' = []
+    result_pickle: 'Path|None' = None
+    for arg_list in arguments:
+        for arg in arg_list:
+            if arg == '-':
+                result_list.extend(collect_zephyr_inputs())
+            else:
+                arg = Path(arg)
+                if arg.is_file():
+                    result_pickle = arg
+                else:
+                    result_list.append(arg)
+    if len(result_list) and result_pickle is not None:
+        error('Expecting pickled file or list of directories. Not both.')
+        sys.exit(1)
+    return result_pickle or result_list
+
+
+def dump_json(file: Path, **kwargs):
+    def default_encode(o):
+        this_id = id(o)
+        if this_id in ids:
+            return f'__id__{this_id}'
+        if isinstance(o, set):
+            return list(o)
+        else:
+            ids.add(this_id)
+            d = {'__id__': f'__id__{this_id}'}
+            for name in tuple(dir(o)):
+                if not name.startswith('_'):
+                    value = getattr(o, name)
+                    if not callable(value):
+                        d[name] = value
+            return d
+    ids = set()
+    with open(file, 'w') as fd:
+        fd.write('{\n')
+        first = True
+        for name, value in kwargs.items():
+            json = JSONEncoder(sort_keys=False, indent=2, default=default_encode).encode(value)
+            if not first:
+                fd.write(',\n')
+            fd.write(f'"{name}": {json}')
+            first = False
+        fd.write('\n}\n')
+
+def main():
+    new_input = parse_bindings(collect_inputs(args.new))
+
+    if args.old:
+        old_input = parse_bindings(collect_inputs(args.old))
+    else:
+        old_input = None
+
+    if args.save_input:
+        save_bindings(new_input, args.save_input)
+
+    if args.save_old_input and old_input:
+        save_bindings(old_input, args.save_old_input)
+
+    if args.dump_json:
+        if old_input:
+            dump_json(args.dump_json,
+                      new_bindings=new_input.bindings,
+                      new_binding_by_name=new_input.binding_by_name,
+                      old_bindings=old_input.bindings,
+                      old_binding_by_name=old_input.binding_by_name)
+        else:
+            dump_json(args.dump_json,
+                      bindings=new_input.bindings,
+                      binding_by_name=new_input.binding_by_name)
+
+    level = 0
+
+    if old_input:
+        changes = compare(new_input, old_input)
+        if args.dump_json:
+            dump_json(args.dump_json,
+                      new_bindings=new_input.bindings,
+                      new_binding_by_name=new_input.binding_by_name,
+                      old_bindings=old_input.bindings,
+                      old_binding_by_name=old_input.binding_by_name,
+                      changes=changes)
+        stats = out_text.generate(changes)
+        if args.save_stats:
+            args.save_stats.write_text(json.dumps({
+                'notice': stats[1],
+                'warning': stats[2],
+                'critical': stats[3],
+            }, indent=2))
+        for i in range(len(stats)):
+            if stats[i] > 0:
+                level = i
+
+    sys.exit(level)
+
+
+if __name__ == '__main__':
+    main()

nrfconnect/sdk-nrf

scripts/ci/api_check/dts/bindings_parser.py
@@ -1,0 +1,109 @@
+# Copyright (c) 2024 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+
+import sys
+import pickle
+from pathlib import Path
+from dts_tools import devicetree_sources, warning
+
+if devicetree_sources:
+    sys.path.insert(0, devicetree_sources)
+
+from devicetree import edtlib
+
+
+class ParseResult:
+    bindings: 'list[Binding]'
+    binding_by_name: 'dict[str, Binding]'
+    def __init__(self):
+        self.bindings = []
+        self.binding_by_name = {}
+
+class Property:
+    name: str
+    type: str
+    description: str
+    enum: 'set[str]'
+    const: 'str | None'
+    default: 'str | None'
+    deprecated: bool
+    required: bool
+    specifier_space: str
+
+    def __init__(self, prop: edtlib.PropertySpec):
+        self.name = prop.name
+        self.type = prop.type or ''
+        self.description = prop.description or ''
+        self.enum = { str(x) for x in (prop.enum or []) }
+        self.const = str(prop.const) if prop.const else None
+        self.default = str(prop.default) if prop.default else None
+        self.deprecated = prop.deprecated or False
+        self.required = prop.required or False
+        self.specifier_space = str(prop.specifier_space or '')
+
+class Binding:
+    path: str
+    name: str
+    description: str
+    cells: str
+    buses: str
+    properties: 'dict[str, Property]'
+
+    def __init__(self, binding: edtlib.Binding, file: Path):
+        self.path = str(file)
+        self.name = binding.compatible or self.path
+        if binding.on_bus is not None:
+            self.name += '@' + binding.on_bus
+        self.description = binding.description or ''
+        cells_array = [
+            f'{name}={";".join(value)}' for name, value in (binding.specifier2cells or {}).items()
+        ]
+        cells_array.sort()
+        self.cells = '&'.join(cells_array)
+        busses_array = list(binding.buses or [])
+        busses_array.sort()
+        self.buses = ';'.join(busses_array)
+        self.properties = {}
+        for key, value in (binding.prop2specs or {}).items():
+            prop = Property(value)
+            self.properties[key] = prop
+
+
+def get_binding_files(bindings_dirs: 'list[Path]') -> 'list[Path]':
+    binding_files = []
+    for bindings_dir in bindings_dirs:
+        if not bindings_dir.is_dir():
+            raise FileNotFoundError(f'Bindings directory "{bindings_dir}" not found.')
+        for file in bindings_dir.glob('**/*.yaml'):
+            binding_files.append(file)
+        for file in bindings_dir.glob('**/*.yml'):
+            binding_files.append(file)
+    return binding_files
+
+
+def parse_bindings(dirs_or_pickle: 'list[Path]|Path') -> ParseResult:
+    result = ParseResult()
+    if isinstance(dirs_or_pickle, list):
+        yaml_files = get_binding_files(dirs_or_pickle)
+        fname2path: 'dict[str, str]' = {
+            path.name: str(path) for path in yaml_files
+        }
+        for binding_file in yaml_files:
+            try:
+                binding = Binding(edtlib.Binding(str(binding_file), fname2path, None, False, False), binding_file)
+                if binding.name in result.binding_by_name:
+                    warning(f'Repeating binding {binding.name}: {binding.path}  {result.binding_by_name[binding.name].path}')
+                result.bindings.append(binding)
+                result.binding_by_name[binding.name] = binding
+            except edtlib.EDTError as err:
+                warning(err)
+    else:
+        with open(dirs_or_pickle, 'rb') as fd:
+            result = pickle.load(fd)
+    return result
+
+
+def save_bindings(parse_result: ParseResult, file: Path):
+    with open(file, 'wb') as fd:
+        pickle.dump(parse_result, fd)

scripts/ci/api_check/dts/main.py
@@ -1,0 +1,132 @@
+# Copyright (c) 2024 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+
+import os
+import sys
+import json
+import out_text
+from json import JSONEncoder
+from pathlib import Path
+from dts_tools import error
+from args import args
+from bindings_parser import parse_bindings, save_bindings
+from dts_compare import compare
+
+
+def collect_zephyr_inputs() -> 'list[Path]':
+    zephyr_base = os.getenv('ZEPHYR_BASE')
+    if zephyr_base is None:
+        error('You must specify "ZEPHYR_BASE" if you using "-" argument as an input.')
+        sys.exit(1)
+    zephyr_base = Path(zephyr_base)
+    west_root = zephyr_base.parent
+    bindings_dirs: 'list[Path]' = []
+    for bindings_dir in west_root.glob('**/dts/bindings'):
+        rel = bindings_dir.relative_to(west_root)
+        if str(rel).count('test') or str(rel).count('sample'):
+            continue
+        bindings_dirs.append(bindings_dir)
+    return bindings_dirs
+
+
+def collect_inputs(arguments: 'list[list[str]]') -> 'list[Path]|Path':
+    result_list: 'list[Path]' = []
+    result_pickle: 'Path|None' = None
+    for arg_list in arguments:
+        for arg in arg_list:
+            if arg == '-':
+                result_list.extend(collect_zephyr_inputs())
+            else:
+                arg = Path(arg)
+                if arg.is_file():
+                    result_pickle = arg
+                else:
+                    result_list.append(arg)
+    if len(result_list) > 0 and result_pickle is not None:
+        error('Expecting pickled file or list of directories. Not both.')
+        sys.exit(1)
+    return result_pickle or result_list
+
+
+def dump_json(file: Path, **kwargs):
+    def default_encode(o):
+        this_id = id(o)
+        if this_id in ids:
+            return f'__id__{this_id}'
+        if isinstance(o, set):
+            return list(o)
+        else:
+            ids.add(this_id)
+            d = {'__id__': f'__id__{this_id}'}
+            for name in tuple(dir(o)):
+                if not name.startswith('_'):
+                    value = getattr(o, name)
+                    if not callable(value):
+                        d[name] = value
+            return d
+    ids = set()
+    with open(file, 'w') as fd:
+        fd.write('{\n')
+        first = True
+        for name, value in kwargs.items():
+            json = JSONEncoder(sort_keys=False, indent=2, default=default_encode).encode(value)
+            if not first:
+                fd.write(',\n')
+            fd.write(f'"{name}": {json}')
+            first = False
+        fd.write('\n}\n')
+
+def main():
+    new_input = parse_bindings(collect_inputs(args.new))
+
+    if args.old:
+        old_input = parse_bindings(collect_inputs(args.old))
+    else:
+        old_input = None
+
+    if args.save_input:
+        save_bindings(new_input, args.save_input)
+
+    if args.save_old_input and old_input:
+        save_bindings(old_input, args.save_old_input)
+
+    if args.dump_json:
+        if old_input:
+            dump_json(args.dump_json,
+                      new_bindings=new_input.bindings,
+                      new_binding_by_name=new_input.binding_by_name,
+                      old_bindings=old_input.bindings,
+                      old_binding_by_name=old_input.binding_by_name)
+        else:
+            dump_json(args.dump_json,
+                      bindings=new_input.bindings,
+                      binding_by_name=new_input.binding_by_name)
+
+    level = 0
+
+    if old_input:
+        changes = compare(new_input, old_input)
+        if args.dump_json:
+            dump_json(args.dump_json,
+                      new_bindings=new_input.bindings,
+                      new_binding_by_name=new_input.binding_by_name,
+                      old_bindings=old_input.bindings,
+                      old_binding_by_name=old_input.binding_by_name,
+                      changes=changes)
+        stats = out_text.generate(changes)
+        if args.save_stats:
+            args.save_stats.write_text(json.dumps({
+                'notice': stats[1],
+                'warning': stats[2],
+                'critical': stats[3],
+            }, indent=2))
+        for i, count in enumerate(stats):
+            if count > 0:
+                level = i
+
+    sys.exit(level)
+
+
+if __name__ == '__main__':
+    main()