819a0f7c79113c9e300a13d668153ee3176747f7
Vidar Lillebø <vidar.lillebo@nordicsemi.no>
2024-05-21 12:08:39 +0200

    cracen: PRNG optimizations
    Removes sicrypto PRNG and replaces with implementation that has smaller
    context size.
    
    Signed-off-by: Vidar Lillebø <vidar.lillebo@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/sicrypto/src/util.h
+++ subsys/nrf_security/src/drivers/cracen/sicrypto/src/util.h
@@ -9,16 +9,6 @@
 
 #include <sicrypto/util.h>
 
-/* Compute v = v + summand.
- *
- * v is an unsigned integer stored as a big endian byte array of sz bytes.
- * Summand must be less than or equal to the maximum value of a size_t minus 255.
- * The final carry is discarded: addition is modulo 2^(sz*8).
- */
-void si_be_add(unsigned char *v, size_t sz, size_t summand);
-
-void si_xorbytes(char *a, const char *b, size_t sz);
-
 int si_status_on_prng(struct sitask *t);
 
 int si_wait_on_prng(struct sitask *t);
