09ced85e22116dd754c8faf3fdf15f7146511c8f
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2024-05-27 17:06:56 +0200

    boards: thingy91x: more sysbuild fixes
    Fix mcuboot build and usage of static partition table.
    
    Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>

--- boards/nordic/thingy91x/sysbuild.cmake
+++ boards/nordic/thingy91x/sysbuild.cmake
@@ -8,7 +8,7 @@ if(SB_CONFIG_BOARD_THINGY91X_NRF9151_NS)
   # Use static partition layout to ensure the partition layout remains
   # unchanged after DFU. This needs to be made globally available
   # because it is used in other CMake files.
-  set(PM_STATIC_YML_FILE ${CMAKE_CURRENT_LIST_DIR}/thingy91x_pm_static.yml CACHE INTERNAL "")
+  set(PM_STATIC_YML_FILE ${CMAKE_CURRENT_LIST_DIR}/thingy91x_nrf9151_pm_static.yml CACHE INTERNAL "")
 endif()
 
 if(SB_CONFIG_BOARD_THINGY91X_NRF5340_CPUAPP OR SB_CONFIG_BOARD_THINGY91X_NRF5340_CPUAPP_NS)
