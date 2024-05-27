f0a260415df8cb617973fb54d23a4bcf0982441b
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2024-05-27 15:19:22 +0200

    suit: fix: Add a basic check for component addresses
    Add a basic checks that will prevent local domains from booting if the
    UICRs are not present.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- subsys/suit/mci/src/suit_mci_nrf54h20.c
+++ subsys/suit/mci/src/suit_mci_nrf54h20.c
@@ -9,6 +9,7 @@
 #include <suit_execution_mode.h>
 #include <sdfw/lcs.h>
 #include <zephyr/logging/log.h>
+#include <sdfw/arbiter.h>
 
 #define MANIFEST_PUBKEY_NRF_TOP_GEN0	 0x4000BB00
 #define MANIFEST_PUBKEY_SYSCTRL_GEN0	 0x40082100
@@ -402,17 +403,43 @@ mci_err_t suit_mci_memory_access_rights_validate(const suit_manifest_class_id_t 
 	case SUIT_MANIFEST_APP_RECOVERY:
 	case SUIT_MANIFEST_APP_LOCAL_1:
 	case SUIT_MANIFEST_APP_LOCAL_2:
-	case SUIT_MANIFEST_APP_LOCAL_3:
-		/* App manifest - TODO - implement checks based on UICR
-		 */
+	case SUIT_MANIFEST_APP_LOCAL_3: {
+		struct arbiter_mem_params_access mem_params = {
+			.allowed_types = ARBITER_MEM_TYPE(RESERVED, FIXED),
+			.access = {
+					.owner = NRF_OWNER_APPLICATION,
+					.permissions = ARBITER_MEM_PERM(READ, SECURE),
+					.address = (uintptr_t)address,
+					.size = mem_size,
+				},
+		};
+
+		if (arbiter_mem_access_check(&mem_params) != ARBITER_STATUS_OK) {
+			return MCI_ERR_NOACCESS;
+		}
+
 		return SUIT_PLAT_SUCCESS;
+	}
 
 	case SUIT_MANIFEST_RAD_RECOVERY:
 	case SUIT_MANIFEST_RAD_LOCAL_1:
-	case SUIT_MANIFEST_RAD_LOCAL_2:
-		/* Rad manifest - TODO - implement checks based on UICR
-		 */
+	case SUIT_MANIFEST_RAD_LOCAL_2: {
+		struct arbiter_mem_params_access mem_params = {
+			.allowed_types = ARBITER_MEM_TYPE(RESERVED, FIXED),
+			.access = {
+					.owner = NRF_OWNER_RADIOCORE,
+					.permissions = ARBITER_MEM_PERM(READ, SECURE),
+					.address = (uintptr_t)address,
+					.size = mem_size,
+				},
+		};
+
+		if (arbiter_mem_access_check(&mem_params) != ARBITER_STATUS_OK) {
+			return MCI_ERR_NOACCESS;
+		}
+
 		return SUIT_PLAT_SUCCESS;
+	}
 
 	default:
 		break;
