3865e88bd6d0735de89699822f9c7660eaee3351
Vidar Lillebø <vidar.lillebo@nordicsemi.no>
2024-05-28 13:44:51 +0200

    cracen: Fix cmake identation
    Fixes indentation in cmake to use 2 spaces.
    
    Signed-off-by: Vidar Lillebø <vidar.lillebo@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/cracenpsa.cmake
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/cracenpsa.cmake
@@ -7,7 +7,7 @@
 # This directory contains C sources for the CRACEN PSA crypto driver
 
 list(APPEND cracen_driver_include_dirs
-    ${CMAKE_CURRENT_LIST_DIR}/include
+  ${CMAKE_CURRENT_LIST_DIR}/include
 )
 
 list(APPEND cracen_driver_sources
@@ -24,79 +24,79 @@ list(APPEND cracen_driver_sources
 
 if(CONFIG_CRACEN_LIB_KMU)
   list(APPEND cracen_driver_sources
-	${CMAKE_CURRENT_LIST_DIR}/src/lib_kmu.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/lib_kmu.c
   )
 endif()
 
 if(CONFIG_PSA_NEED_CRACEN_AEAD_DRIVER)
   list(APPEND cracen_driver_sources
-	${CMAKE_CURRENT_LIST_DIR}/src/aead.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/aead.c
   )
 endif()
 
 if(CONFIG_PSA_NEED_CRACEN_ASYMMETRIC_ENCRYPTION_DRIVER)
   list(APPEND cracen_driver_sources
-	${CMAKE_CURRENT_LIST_DIR}/src/asymmetric.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/asymmetric.c
   )
 endif()
 
 if(CONFIG_PSA_NEED_CRACEN_ASYMMETRIC_SIGNATURE_DRIVER)
   list(APPEND cracen_driver_sources
-	${CMAKE_CURRENT_LIST_DIR}/src/sign.c
-	)
+    ${CMAKE_CURRENT_LIST_DIR}/src/sign.c
+  )
 endif()
 
 if(CONFIG_PSA_NEED_CRACEN_HASH_DRIVER)
   list(APPEND cracen_driver_sources
-	${CMAKE_CURRENT_LIST_DIR}/src/hash.c
-	)
+    ${CMAKE_CURRENT_LIST_DIR}/src/hash.c
+  )
 endif()
 
 if(CONFIG_PSA_NEED_CRACEN_MAC_DRIVER)
   list(APPEND cracen_driver_sources
-	${CMAKE_CURRENT_LIST_DIR}/src/mac.c
-	)
+    ${CMAKE_CURRENT_LIST_DIR}/src/mac.c
+  )
 endif()
 
 if(CONFIG_PSA_NEED_CRACEN_KEY_MANAGEMENT_DRIVER OR CONFIG_PSA_NEED_CRACEN_KMU_DRIVER)
   list(APPEND cracen_driver_sources
-	${CMAKE_CURRENT_LIST_DIR}/src/key_management.c
-	)
+    ${CMAKE_CURRENT_LIST_DIR}/src/key_management.c
+  )
 endif()
 
 if(CONFIG_PSA_NEED_CRACEN_PAKE_DRIVER)
   list(APPEND cracen_driver_sources
-	${CMAKE_CURRENT_LIST_DIR}/src/pake.c
-	${CMAKE_CURRENT_LIST_DIR}/src/spake2p.c
-	)
+    ${CMAKE_CURRENT_LIST_DIR}/src/pake.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/spake2p.c
+  )
 endif()
 
 if(CONFIG_PSA_NEED_CRACEN_ECJPAKE_SECP_R1_256)
   list(APPEND cracen_driver_sources
-	${CMAKE_CURRENT_LIST_DIR}/src/jpake.c
-	)
+    ${CMAKE_CURRENT_LIST_DIR}/src/jpake.c
+  )
 endif()
 
 if(CONFIG_PSA_NEED_CRACEN_SRP_6)
   list(APPEND cracen_driver_sources
-	${CMAKE_CURRENT_LIST_DIR}/src/srp.c
-	)
+    ${CMAKE_CURRENT_LIST_DIR}/src/srp.c
+  )
 endif()
 
 if(CONFIG_PSA_NEED_CRACEN_KMU_DRIVER)
   list(APPEND cracen_driver_sources
-	${CMAKE_CURRENT_LIST_DIR}/src/kmu.c
-	)
+    ${CMAKE_CURRENT_LIST_DIR}/src/kmu.c
+  )
 endif()
 
 if(CONFIG_PSA_NEED_CRACEN_KEY_AGREEMENT_DRIVER OR CONFIG_PSA_NEED_CRACEN_KEY_DERIVATION_DRIVER)
   list(APPEND cracen_driver_sources
-	${CMAKE_CURRENT_LIST_DIR}/src/key_derivation.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/key_derivation.c
   )
 endif()
 
 if(CONFIG_PSA_NEED_CRACEN_PLATFORM_KEYS)
   list(APPEND cracen_driver_sources
-  ${CMAKE_CURRENT_LIST_DIR}/src/platform_keys/platform_keys.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/platform_keys/platform_keys.c
   )
 endif()
