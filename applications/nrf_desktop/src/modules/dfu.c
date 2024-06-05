aa035ba1993982b07b8aa25a6a99784be95abb46
Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>
2024-06-05 10:53:26 +0200

    applications: nrf_desktop: Add SUIT support to dfu module
    The commit adds support for configuration channel DFU image transport
    with SUIT.
    
    Jira: NCSDK-24197
    Jira: NCSDK-24660
    JIRA: NCSDK-27583
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>
    Signed-off-by: Jan Zyczkowski <jan.zyczkowski@nordicsemi.no>

--- applications/nrf_desktop/src/modules/dfu.c
+++ applications/nrf_desktop/src/modules/dfu.c
@@ -91,6 +91,17 @@ LOG_MODULE_REGISTER(MODULE, CONFIG_DESKTOP_CONFIG_CHANNEL_DFU_LOG_LEVEL);
 	#else
 		#error Missing partition definitions.
 	#endif
+#elif CONFIG_SUIT
+	BUILD_ASSERT(!IS_ENABLED(CONFIG_PARTITION_MANAGER_ENABLED),
+		     "SUIT DFU is supported only without Partition Manager");
+	BUILD_ASSERT(DT_NODE_EXISTS(DT_NODELABEL(dfu_partition)),
+		     "DFU partition must be defined in devicetree");
+	#include <sdfw/sdfw_services/suit_service.h>
+	#include <suit_plat_mem_util.h>
+	#define BOOTLOADER_NAME	"SUIT"
+	#define DFU_SLOT_ID		FIXED_PARTITION_ID(dfu_partition)
+	#define UPDATE_CANDIDATE_CNT	1
+	static suit_plat_mreg_t update_candidate;
 #else
 	#error Bootloader not supported.
 #endif
@@ -220,6 +231,17 @@ static void reboot_request_handler(struct k_work *work)
 	}
 
 	LOG_PANIC();
+
+#if CONFIG_SUIT
+	if ((update_candidate.mem != 0) && (update_candidate.size != 0) &&
+	    (cur_offset == img_length) && !slot_was_used_by_other_transport) {
+		LOG_INF("Configuration channel reboot request triggered SUIT update");
+		int ret = suit_trigger_update(&update_candidate, UPDATE_CANDIDATE_CNT);
+
+		LOG_INF("SUIT update triggered with status: %d", ret);
+	}
+#endif
+
 	sys_reboot(SYS_REBOOT_WARM);
 
 	if (!k_work_delayable_is_pending(&background_erase)) {
@@ -310,6 +332,12 @@ static void complete_dfu_data_store(void)
 		if (err) {
 			LOG_ERR("Cannot request the image upgrade (err:%d)", err);
 		}
+#elif CONFIG_SUIT
+		update_candidate.mem
+			= suit_plat_mem_nvm_ptr_get(FIXED_PARTITION_OFFSET(dfu_partition));
+		update_candidate.size = img_length;
+		LOG_INF("DFU update candidate stored in DFU partition");
+		LOG_INF("Update will be performed on configuration channel reboot request");
 #endif
 		terminate_dfu();
 	}
@@ -776,6 +804,60 @@ static void handle_image_info_request(uint8_t *data, size_t *size)
 		pos += sizeof(header.h.v1.sem_ver.build_num);
 	} else {
 		LOG_ERR("Cannot obtain image information");
+	}
+}
+#elif CONFIG_SUIT
+static void handle_image_info_request(uint8_t *data, size_t *size)
+{
+	unsigned int seq_num = 0;
+	suit_ssf_manifest_class_info_t class_info;
+
+	int err = suit_get_supported_manifest_info(SUIT_MANIFEST_APP_ROOT, &class_info);
+
+	if (!err) {
+		err = suit_get_installed_manifest_info(&(class_info.class_id),
+				&seq_num, NULL, NULL, NULL, NULL);
+	}
+	if (!err) {
+		uint8_t flash_area_id = 0;
+		/* SUIT supports multiple images, return zero as a special image size value. */
+		uint32_t image_size = 0;
+		uint32_t build_num = seq_num;
+		uint8_t major = 0;
+		uint8_t minor = 0;
+		uint16_t revision = 0;
+
+		LOG_INF("Booted application sequence number: %" PRIu32, seq_num);
+
+		size_t data_size = sizeof(flash_area_id) +
+				   sizeof(image_size) +
+				   sizeof(major) +
+				   sizeof(minor) +
+				   sizeof(revision) +
+				   sizeof(build_num);
+		size_t pos = 0;
+
+		*size = data_size;
+
+		data[pos] = flash_area_id;
+		pos += sizeof(flash_area_id);
+
+		sys_put_le32(image_size, &data[pos]);
+		pos += sizeof(image_size);
+
+		data[pos] = major;
+		pos += sizeof(major);
+
+		data[pos] = minor;
+		pos += sizeof(minor);
+
+		sys_put_le16(revision, &data[pos]);
+		pos += sizeof(revision);
+
+		sys_put_le32(build_num, &data[pos]);
+		pos += sizeof(build_num);
+	} else {
+		LOG_ERR("suit retrieve manifest seq num failed (err: %d)", err);
 	}
 }
 #endif
