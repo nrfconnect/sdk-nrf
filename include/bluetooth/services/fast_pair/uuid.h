b58886a2e91ceab0b0394c3ca3f75cee5b2ff2da
Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>
2024-06-05 14:59:55 +0200

    bluetooth: fast_pair: uuid: fix missing dots in doxygen
    Fixed missing dots in the Fast Pair UUID header to make the doxygen
    documentation more consistent and to align it with the main Fast Pair
    header.
    
    Signed-off-by: Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>

--- include/bluetooth/services/fast_pair/uuid.h
+++ include/bluetooth/services/fast_pair/uuid.h
@@ -20,50 +20,50 @@
 extern "C" {
 #endif
 
-/** Fast Pair Service UUID value */
+/** Fast Pair Service UUID value. */
 #define BT_FAST_PAIR_UUID_FPS_VAL (0xFE2C)
-/** Fast Pair Service UUID */
+/** Fast Pair Service UUID. */
 #define BT_FAST_PAIR_UUID_FPS BT_UUID_DECLARE_16(BT_FAST_PAIR_UUID_FPS_VAL)
 
-/** Fast Pair Model ID Characteristic UUID value */
+/** Fast Pair Model ID Characteristic UUID value. */
 #define BT_FAST_PAIR_UUID_MODEL_ID_VAL \
 	BT_UUID_128_ENCODE(0xFE2C1233, 0x8366, 0x4814, 0x8EB0, 0x01DE32100BEA)
-/** Fast Pair Model ID Characteristic UUID */
+/** Fast Pair Model ID Characteristic UUID. */
 #define BT_FAST_PAIR_UUID_MODEL_ID \
 	BT_UUID_DECLARE_128(BT_FAST_PAIR_UUID_MODEL_ID_VAL)
 
-/** Fast Pair Key-based Pairing Characteristic UUID value */
+/** Fast Pair Key-based Pairing Characteristic UUID value. */
 #define BT_FAST_PAIR_UUID_KEY_BASED_PAIRING_VAL \
 	BT_UUID_128_ENCODE(0xFE2C1234, 0x8366, 0x4814, 0x8EB0, 0x01DE32100BEA)
-/** Fast Pair Key-based Pairing Characteristic UUID */
+/** Fast Pair Key-based Pairing Characteristic UUID. */
 #define BT_FAST_PAIR_UUID_KEY_BASED_PAIRING \
 	BT_UUID_DECLARE_128(BT_FAST_PAIR_UUID_KEY_BASED_PAIRING_VAL)
 
-/** Fast Pair Passkey Characteristic UUID value */
+/** Fast Pair Passkey Characteristic UUID value. */
 #define BT_FAST_PAIR_UUID_PASSKEY_VAL \
 	BT_UUID_128_ENCODE(0xFE2C1235, 0x8366, 0x4814, 0x8EB0, 0x01DE32100BEA)
-/** Fast Pair Passkey Characteristic UUID */
+/** Fast Pair Passkey Characteristic UUID. */
 #define BT_FAST_PAIR_UUID_PASSKEY \
 	BT_UUID_DECLARE_128(BT_FAST_PAIR_UUID_PASSKEY_VAL)
 
-/** Fast Pair Account Key Characteristic UUID value */
+/** Fast Pair Account Key Characteristic UUID value. */
 #define BT_FAST_PAIR_UUID_ACCOUNT_KEY_VAL \
 	BT_UUID_128_ENCODE(0xFE2C1236, 0x8366, 0x4814, 0x8EB0, 0x01DE32100BEA)
-/** Fast Pair Account Key Characteristic UUID */
+/** Fast Pair Account Key Characteristic UUID. */
 #define BT_FAST_PAIR_UUID_ACCOUNT_KEY \
 	BT_UUID_DECLARE_128(BT_FAST_PAIR_UUID_ACCOUNT_KEY_VAL)
 
-/** Fast Pair Additional Data Characteristic UUID value */
+/** Fast Pair Additional Data Characteristic UUID value. */
 #define BT_FAST_PAIR_UUID_ADDITIONAL_DATA_VAL \
 	BT_UUID_128_ENCODE(0xFE2C1237, 0x8366, 0x4814, 0x8EB0, 0x01DE32100BEA)
-/** Fast Pair Additional Data Characteristic UUID */
+/** Fast Pair Additional Data Characteristic UUID. */
 #define BT_FAST_PAIR_UUID_ADDITIONAL_DATA \
 	BT_UUID_DECLARE_128(BT_FAST_PAIR_UUID_ADDITIONAL_DATA_VAL)
 
-/** Fast Pair Beacon Actions Characteristic UUID value (used in the FMDN extension) */
+/** Fast Pair Beacon Actions Characteristic UUID value (used in the FMDN extension). */
 #define BT_FAST_PAIR_UUID_BEACON_ACTIONS_VAL \
 	BT_UUID_128_ENCODE(0xFE2C1238, 0x8366, 0x4814, 0x8EB0, 0x01DE32100BEA)
-/** Fast Pair Beacon Actions Characteristic UUID (used in the FMDN extension) */
+/** Fast Pair Beacon Actions Characteristic UUID (used in the FMDN extension). */
 #define BT_FAST_PAIR_UUID_BEACON_ACTIONS \
 	BT_UUID_DECLARE_128(BT_FAST_PAIR_UUID_BEACON_ACTIONS_VAL)
 
