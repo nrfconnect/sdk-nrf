f0004b586e41c154d7f6fe848d6f03de8ff074a8
alperen sener <alperen.sener@nordicsemi.no>
2024-06-18 12:19:56 +0200

    samples: bluetooth: mesh: fix bin path for dfu target sample
    Updating the bin path with correct directory for dfu target sample.
    
    Signed-off-by: alperen sener <alperen.sener@nordicsemi.no>

--- samples/bluetooth/mesh/dfu/target/sysbuild.cmake
+++ samples/bluetooth/mesh/dfu/target/sysbuild.cmake
@@ -8,7 +8,7 @@ if(SB_CONFIG_DFU_ZIP_BLUETOOTH_MESH_METADATA)
 add_custom_target(ble_mesh_print_dfu_fwid ALL
   ${PYTHON_EXECUTABLE}
   ${ZEPHYR_NRF_MODULE_DIR}/samples/bluetooth/mesh/dfu/common/script/ble_mesh_dfu_fwid.py
-  --bin-path ${PROJECT_BINARY_DIR}/${DEFAULT_IMAGE}/zephyr
+  --bin-path ${CMAKE_BINARY_DIR}/${DEFAULT_IMAGE}/zephyr
   DEPENDS parse_mesh_metadata
 )
 endif()
