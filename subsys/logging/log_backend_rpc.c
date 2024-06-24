0a7393fd4bfbf7da3ce69d5a367b70bda1d9f7ce
Damian Krolik <damian.krolik@nordicsemi.no>
2024-06-24 09:35:10 +0200

    logging: crash log support for nRF RPC backend
    Add Kconfig option that enables saving the crash log in the
    "crash_log" retained RAM partition, and adds an nRF RPC
    command that allows a remote user to read the crash log
    after the device reboots.
    
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- subsys/logging/log_backend_rpc.c
+++ subsys/logging/log_backend_rpc.c
@@ -10,7 +10,7 @@
  *
  */
 
-#include "log_rpc_internal.h"
+#include "log_rpc_group.h"
 
 #include <nrf_rpc_cbor.h>
 
@@ -18,8 +18,11 @@
 #include <zephyr/logging/log_backend.h>
 #include <zephyr/logging/log_backend_std.h>
 #include <zephyr/logging/log_output.h>
+#include <zephyr/retention/retention.h>
 
 #include <string.h>
+
+#define CRASH_LOG_DEV DEVICE_DT_GET(DT_NODELABEL(crash_log))
 
 /*
  * Drop messages coming from nRF RPC to avoid the avalanche of logs:
@@ -37,11 +40,140 @@ static const char *const filtered_out_sources[] = {
 static bool panic_mode;
 static uint32_t log_format = LOG_OUTPUT_TEXT;
 static uint8_t current_level;
-static uint8_t tx_buffer[CONFIG_LOG_BACKEND_RPC_BUFFER_SIZE];
+static uint8_t log_output_buffer[CONFIG_LOG_BACKEND_RPC_BUFFER_SIZE];
 
-extern struct k_thread logging_thread;
+#ifdef CONFIG_LOG_BACKEND_RPC_CRASH_LOG
+/* Header for the crash log data stored in the retained RAM partition. */
+typedef struct crash_log_header {
+	size_t size;
+} crash_log_header_t;
 
-static int send(uint8_t *data, size_t length, void *context)
+static size_t crash_log_cur_size;
+static size_t crash_log_max_size;
+
+static void crash_log_clear(void)
+{
+	const struct device *const crash_log = CRASH_LOG_DEV;
+	ssize_t rc;
+
+	crash_log_cur_size = 0;
+	crash_log_max_size = 0;
+
+	rc = retention_clear(crash_log);
+
+	if (rc) {
+		return;
+	}
+
+	rc = retention_size(crash_log);
+
+	if (rc <= (ssize_t)sizeof(crash_log_header_t)) {
+		/* Either failed to obtain the retained partition size, or it is too small. */
+		return;
+	}
+
+	crash_log_max_size = (size_t)rc - sizeof(crash_log_header_t);
+}
+
+static void retain(const uint8_t *data, size_t length)
+{
+	const struct device *const crash_log = CRASH_LOG_DEV;
+	crash_log_header_t header;
+	int rc;
+
+	if (crash_log_cur_size >= crash_log_max_size) {
+		/* Crash log partition overflow. */
+		return;
+	}
+
+	length = MIN(length, crash_log_max_size - crash_log_cur_size);
+
+	rc = retention_write(crash_log, sizeof(header) + crash_log_cur_size, data, length);
+
+	if (rc) {
+		return;
+	}
+
+	crash_log_cur_size += length;
+	header.size = crash_log_cur_size;
+
+	(void)retention_write(crash_log, 0, (const uint8_t *)&header, sizeof(header));
+}
+
+static void log_rpc_get_crash_log_handler(const struct nrf_rpc_group *group,
+					  struct nrf_rpc_cbor_ctx *ctx, void *handler_data)
+{
+	const struct device *const crash_log = CRASH_LOG_DEV;
+	size_t offset;
+	size_t length;
+	bool decoded_ok;
+	crash_log_header_t header;
+	struct nrf_rpc_cbor_ctx rsp_ctx;
+
+	decoded_ok = zcbor_uint_decode(ctx->zs, &offset, sizeof(offset));
+	decoded_ok = decoded_ok && zcbor_uint_decode(ctx->zs, &length, sizeof(length));
+	nrf_rpc_cbor_decoding_done(group, ctx);
+
+	if (!decoded_ok) {
+		goto err;
+	}
+
+	if (retention_is_valid(crash_log) != 1) {
+		goto err;
+	}
+
+	if (retention_read(crash_log, 0, (uint8_t *)&header, sizeof(header))) {
+		goto err;
+	}
+
+	if (offset >= header.size) {
+		goto err;
+	}
+
+	length = MIN(length, header.size - offset);
+	NRF_RPC_CBOR_ALLOC(group, rsp_ctx, 5 + length);
+
+	if (!zcbor_bstr_start_encode(rsp_ctx.zs)) {
+		goto discard;
+	}
+
+	if (retention_read(crash_log, sizeof(header) + offset, rsp_ctx.zs[0].payload_mut, length)) {
+		goto discard;
+	}
+
+	rsp_ctx.zs[0].payload_mut += length;
+
+	if (!zcbor_bstr_end_encode(rsp_ctx.zs, NULL)) {
+		goto discard;
+	}
+
+	goto send;
+
+discard:
+	NRF_RPC_CBOR_DISCARD(group, rsp_ctx);
+err:
+	/* Send empty string */
+	NRF_RPC_CBOR_ALLOC(group, rsp_ctx, 1);
+	zcbor_bstr_encode_ptr(rsp_ctx.zs, NULL, 0);
+send:
+	nrf_rpc_cbor_rsp_no_err(group, &rsp_ctx);
+}
+
+NRF_RPC_CBOR_CMD_DECODER(log_rpc_group, log_rpc_get_crash_log_handler, LOG_RPC_CMD_GET_CRASH_LOG,
+			 log_rpc_get_crash_log_handler, NULL);
+#else
+static void crash_log_clear(void)
+{
+}
+
+static void retain(const uint8_t *data, size_t length)
+{
+	ARG_UNUSED(data);
+	ARG_UNUSED(length);
+}
+#endif /* CONFIG_LOG_BACKEND_RPC_CRASH_LOG */
+
+static void send(const uint8_t *data, size_t length)
 {
 	struct nrf_rpc_cbor_ctx ctx;
 	const struct zcbor_string data_string = {
@@ -53,20 +185,32 @@ static int send(uint8_t *data, size_t length, void *context)
 
 	if (!zcbor_uint_encode(ctx.zs, &current_level, sizeof(current_level)) ||
 	    !zcbor_bstr_encode(ctx.zs, &data_string)) {
-		/*
-		 * Log output does not handle error codes gracefully, so pretend the buffer has been
-		 * flushed to move on and not enter an infinite loop.
-		 */
 		NRF_RPC_CBOR_DISCARD(&log_rpc_group, ctx);
-		return length;
+		return;
 	}
 
 	nrf_rpc_cbor_evt_no_err(&log_rpc_group, LOG_RPC_EVT_MSG, &ctx);
-
-	return length;
 }
 
-LOG_OUTPUT_DEFINE(log_output_rpc, send, tx_buffer, sizeof(tx_buffer));
+static int flush(uint8_t *data, size_t length, void *context)
+{
+	ARG_UNUSED(context);
+
+	if (panic_mode) {
+		retain(data, length);
+	} else {
+		send(data, length);
+	}
+
+	/*
+	 * Log output does not handle error codes gracefully, so pretend the buffer has been
+	 * flushed to move on and not enter an infinite loop.
+	 */
+
+	return (int)length;
+}
+
+LOG_OUTPUT_DEFINE(log_output_rpc, flush, log_output_buffer, sizeof(log_output_buffer));
 
 static const char *log_msg_source_name_get(struct log_msg *msg)
 {
@@ -89,19 +233,26 @@ static const char *log_msg_source_name_get(struct log_msg *msg)
 	return TYPE_SECTION_START(log_const)[source_id].name;
 }
 
-static inline bool starts_with(const char *str, const char *prefix)
+static bool starts_with(const char *str, const char *prefix)
 {
 	return strncmp(str, prefix, strlen(prefix)) == 0;
 }
 
 static void process(const struct log_backend *const backend, union log_msg_generic *msg)
 {
-	const uint32_t flags = LOG_OUTPUT_FLAG_TIMESTAMP | LOG_OUTPUT_FLAG_FORMAT_TIMESTAMP |
-			       LOG_OUTPUT_FLAG_CRLF_NONE;
 	const log_format_func_t log_formatter = log_format_func_t_get(log_format);
 	const char *source_name = log_msg_source_name_get(&msg->log);
+	uint32_t flags = LOG_OUTPUT_FLAG_TIMESTAMP | LOG_OUTPUT_FLAG_FORMAT_TIMESTAMP;
 
-	if (!NRF_RPC_GROUP_STATUS(log_rpc_group)) {
+	/*
+	 * Panic mode: logs are appended to the buffer in retained RAM, so include LF characters.
+	 * Otherwise:  if nRF RPC group is ready, logs are sent in separate messages, so skip LFs.
+	 */
+	if (panic_mode) {
+		flags |= LOG_OUTPUT_FLAG_CRLF_LFONLY;
+	} else if (NRF_RPC_GROUP_STATUS(log_rpc_group)) {
+		flags |= LOG_OUTPUT_FLAG_CRLF_NONE;
+	} else {
 		return;
 	}
 
@@ -113,11 +264,6 @@ static void process(const struct log_backend *const backend, union log_msg_gener
 		}
 	}
 
-	if (panic_mode) {
-		/* TODO: retain the crash log and make it available after reboot. */
-		return;
-	}
-
 	current_level = log_msg_get_level(&msg->log);
 	log_formatter(&log_output_rpc, &msg->log, flags);
 }
@@ -126,6 +272,7 @@ static void panic(struct log_backend const *const backend)
 {
 	ARG_UNUSED(backend);
 
+	crash_log_clear();
 	panic_mode = true;
 }
 
--- subsys/logging/log_forwarder_rpc.c
+++ subsys/logging/log_forwarder_rpc.c
@@ -10,13 +10,15 @@
  *
  */
 
-#include "log_rpc_internal.h"
+#include "log_rpc_group.h"
+
+#include <logging/log_rpc.h>
 
 #include <nrf_rpc_cbor.h>
 
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(rpc);
+LOG_MODULE_REGISTER(remote);
 
 static void log_rpc_msg_handler(const struct nrf_rpc_group *group, struct nrf_rpc_cbor_ctx *ctx,
 				void *handler_data)
@@ -30,7 +32,7 @@ static void log_rpc_msg_handler(const struct nrf_rpc_group *group, struct nrf_rp
 	nrf_rpc_cbor_decoding_done(group, ctx);
 
 	if (!decoded_ok) {
-		goto out;
+		return;
 	}
 
 	switch (level) {
@@ -49,10 +51,36 @@ static void log_rpc_msg_handler(const struct nrf_rpc_group *group, struct nrf_rp
 	default:
 		break;
 	}
-
-out:
-	return;
 }
 
 NRF_RPC_CBOR_EVT_DECODER(log_rpc_group, log_rpc_msg_handler, LOG_RPC_EVT_MSG, log_rpc_msg_handler,
 			 NULL);
+
+int log_rpc_get_crash_log(size_t offset, char *buffer, size_t buffer_length)
+{
+	struct nrf_rpc_cbor_ctx ctx;
+	struct zcbor_string log_chunk;
+	bool decoded_ok;
+
+	NRF_RPC_CBOR_ALLOC(&log_rpc_group, ctx, 10);
+
+	if (!zcbor_uint32_put(ctx.zs, offset) || !zcbor_uint32_put(ctx.zs, buffer_length)) {
+		NRF_RPC_CBOR_DISCARD(&log_rpc_group, ctx);
+		return -ENOBUFS;
+	}
+
+	nrf_rpc_cbor_cmd_rsp_no_err(&log_rpc_group, LOG_RPC_CMD_GET_CRASH_LOG, &ctx);
+
+	/* Parse response */
+	decoded_ok = zcbor_bstr_decode(ctx.zs, &log_chunk);
+	nrf_rpc_cbor_decoding_done(&log_rpc_group, &ctx);
+
+	if (!decoded_ok) {
+		return -EINVAL;
+	}
+
+	log_chunk.len = MIN(log_chunk.len, buffer_length);
+	memcpy(buffer, log_chunk.value, log_chunk.len);
+
+	return log_chunk.len;
+}
