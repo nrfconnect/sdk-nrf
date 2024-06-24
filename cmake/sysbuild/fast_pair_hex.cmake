e824eba461d2aa85ad613961481aeb77377fd845
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-06-24 09:26:21 +0100

    sysbuild: Remove include functions
    Removes include functions and just includes files directly where
    this is appropriate
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- cmake/sysbuild/fast_pair_hex.cmake
+++ cmake/sysbuild/fast_pair_hex.cmake
@@ -4,43 +4,47 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
-set(FP_PARTITION_NAME bt_fast_pair)
+function(fast_pair_hex)
+  set(fp_partition_name bt_fast_pair)
 
-set(
-  FP_PROVISIONING_DATA_HEX
-  ${CMAKE_BINARY_DIR}/modules/nrf/subsys/bluetooth/services/fast_pair/fp_provisioning_data.hex
-  )
+  set(
+    fp_provisioning_data_hex
+    ${CMAKE_BINARY_DIR}/modules/nrf/subsys/bluetooth/services/fast_pair/fp_provisioning_data.hex
+    )
 
-set(FP_PROVISIONING_DATA_ADDRESS $<TARGET_PROPERTY:partition_manager,PM_BT_FAST_PAIR_ADDRESS>)
+  set(fp_provisioning_data_address $<TARGET_PROPERTY:partition_manager,PM_BT_FAST_PAIR_ADDRESS>)
 
-add_custom_command(
-  OUTPUT
-  ${FP_PROVISIONING_DATA_HEX}
-  DEPENDS
-  "${APPLICATION_BINARY_DIR}/pm.config"
-  COMMAND
-  ${PYTHON_EXECUTABLE} ${ZEPHYR_NRF_MODULE_DIR}/scripts/nrf_provision/fast_pair/fp_provision_cli.py
-                       -o ${FP_PROVISIONING_DATA_HEX} -a ${FP_PROVISIONING_DATA_ADDRESS}
-                       -m ${FP_MODEL_ID} -k ${FP_ANTI_SPOOFING_KEY}
-  COMMENT
-  "Generating Fast Pair provisioning data hex file"
-  USES_TERMINAL
-  )
+  add_custom_command(
+    OUTPUT
+    ${fp_provisioning_data_hex}
+    DEPENDS
+    "${APPLICATION_BINARY_DIR}/pm.config"
+    COMMAND
+    ${PYTHON_EXECUTABLE} ${ZEPHYR_NRF_MODULE_DIR}/scripts/nrf_provision/fast_pair/fp_provision_cli.py
+                         -o ${fp_provisioning_data_hex} -a ${fp_provisioning_data_address}
+                         -m ${FP_MODEL_ID} -k ${FP_ANTI_SPOOFING_KEY}
+    COMMENT
+    "Generating Fast Pair provisioning data hex file"
+    USES_TERMINAL
+    )
 
-add_custom_target(
-  ${FP_PARTITION_NAME}_target
-  DEPENDS
-  "${FP_PROVISIONING_DATA_HEX}"
-  )
+  add_custom_target(
+    ${fp_partition_name}_target
+    DEPENDS
+    "${fp_provisioning_data_hex}"
+    )
 
-set_property(
-  GLOBAL PROPERTY
-  ${FP_PARTITION_NAME}_PM_HEX_FILE
-  "${FP_PROVISIONING_DATA_HEX}"
-  )
+  set_property(
+    GLOBAL PROPERTY
+    ${fp_partition_name}_PM_HEX_FILE
+    "${fp_provisioning_data_hex}"
+    )
 
-set_property(
-  GLOBAL PROPERTY
-  ${FP_PARTITION_NAME}_PM_TARGET
-  ${FP_PARTITION_NAME}_target
-  )
+  set_property(
+    GLOBAL PROPERTY
+    ${fp_partition_name}_PM_TARGET
+    ${fp_partition_name}_target
+    )
+endfunction()
+
+fast_pair_hex()
