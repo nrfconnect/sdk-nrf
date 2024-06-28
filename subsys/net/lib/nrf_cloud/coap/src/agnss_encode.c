0dbb07df03d5e011218853e5bd38c6b248682a5c
Pete Skeggs <peter.skeggs@nordicsemi.no>
2024-06-28 11:46:17 -0700

    net: lib: nrf_cloud_coap: Net info optional for AGNSS
    Move all the network info fields to an optional section of the
    AGNSS CDDL. Rebuild codecs with zcbor. Adjust
    coap_codec_agnss_encode() accordingly.
    
    Signed-off-by: Pete Skeggs <peter.skeggs@nordicsemi.no>

--- subsys/net/lib/nrf_cloud/coap/src/agnss_encode.c
+++ subsys/net/lib/nrf_cloud/coap/src/agnss_encode.c
@@ -21,12 +21,16 @@
 
 static bool encode_repeated_agnss_req_types(zcbor_state_t *state,
 					    const struct agnss_req_types_r *input);
+static bool encode_repeated_agnss_req_eci(zcbor_state_t *state, const struct agnss_req_eci *input);
 static bool encode_repeated_agnss_req_filtered(zcbor_state_t *state,
 					       const struct agnss_req_filtered *input);
 static bool encode_repeated_agnss_req_mask(zcbor_state_t *state,
 					   const struct agnss_req_mask *input);
+static bool encode_repeated_agnss_req_mcc(zcbor_state_t *state, const struct agnss_req_mcc *input);
+static bool encode_repeated_agnss_req_mnc(zcbor_state_t *state, const struct agnss_req_mnc *input);
 static bool encode_repeated_agnss_req_rsrp(zcbor_state_t *state,
 					   const struct agnss_req_rsrp *input);
+static bool encode_repeated_agnss_req_tac(zcbor_state_t *state, const struct agnss_req_tac *input);
 static bool encode_agnss_req(zcbor_state_t *state, const struct agnss_req *input);
 
 static bool encode_repeated_agnss_req_types(zcbor_state_t *state,
@@ -42,6 +46,23 @@ static bool encode_repeated_agnss_req_types(zcbor_state_t *state,
 					       (&(*input).agnss_req_types_int), sizeof(int32_t))) ||
 		    (zcbor_list_map_end_force_encode(state), false)) &&
 		   zcbor_list_end_encode(state, 13))));
+
+	if (!tmp_result) {
+		zcbor_trace_file(state);
+		zcbor_log("%s error: %s\r\n", __func__, zcbor_error_str(zcbor_peek_error(state)));
+	} else {
+		zcbor_log("%s success\r\n", __func__);
+	}
+
+	return tmp_result;
+}
+
+static bool encode_repeated_agnss_req_eci(zcbor_state_t *state, const struct agnss_req_eci *input)
+{
+	zcbor_log("%s\r\n", __func__);
+
+	bool tmp_result = ((((zcbor_uint32_put(state, (2)))) &&
+			    (zcbor_uint32_encode(state, (&(*input).agnss_req_eci)))));
 
 	if (!tmp_result) {
 		zcbor_trace_file(state);
@@ -88,12 +109,63 @@ static bool encode_repeated_agnss_req_mask(zcbor_state_t *state, const struct ag
 	return tmp_result;
 }
 
+static bool encode_repeated_agnss_req_mcc(zcbor_state_t *state, const struct agnss_req_mcc *input)
+{
+	zcbor_log("%s\r\n", __func__);
+
+	bool tmp_result = ((((zcbor_uint32_put(state, (5)))) &&
+			    (zcbor_uint32_encode(state, (&(*input).agnss_req_mcc)))));
+
+	if (!tmp_result) {
+		zcbor_trace_file(state);
+		zcbor_log("%s error: %s\r\n", __func__, zcbor_error_str(zcbor_peek_error(state)));
+	} else {
+		zcbor_log("%s success\r\n", __func__);
+	}
+
+	return tmp_result;
+}
+
+static bool encode_repeated_agnss_req_mnc(zcbor_state_t *state, const struct agnss_req_mnc *input)
+{
+	zcbor_log("%s\r\n", __func__);
+
+	bool tmp_result = ((((zcbor_uint32_put(state, (6)))) &&
+			    (zcbor_uint32_encode(state, (&(*input).agnss_req_mnc)))));
+
+	if (!tmp_result) {
+		zcbor_trace_file(state);
+		zcbor_log("%s error: %s\r\n", __func__, zcbor_error_str(zcbor_peek_error(state)));
+	} else {
+		zcbor_log("%s success\r\n", __func__);
+	}
+
+	return tmp_result;
+}
+
 static bool encode_repeated_agnss_req_rsrp(zcbor_state_t *state, const struct agnss_req_rsrp *input)
 {
 	zcbor_log("%s\r\n", __func__);
 
 	bool tmp_result = ((((zcbor_uint32_put(state, (7)))) &&
 			    (zcbor_int32_encode(state, (&(*input).agnss_req_rsrp)))));
+
+	if (!tmp_result) {
+		zcbor_trace_file(state);
+		zcbor_log("%s error: %s\r\n", __func__, zcbor_error_str(zcbor_peek_error(state)));
+	} else {
+		zcbor_log("%s success\r\n", __func__);
+	}
+
+	return tmp_result;
+}
+
+static bool encode_repeated_agnss_req_tac(zcbor_state_t *state, const struct agnss_req_tac *input)
+{
+	zcbor_log("%s\r\n", __func__);
+
+	bool tmp_result = ((((zcbor_uint32_put(state, (8)))) &&
+			    (zcbor_uint32_encode(state, (&(*input).agnss_req_tac)))));
 
 	if (!tmp_result) {
 		zcbor_trace_file(state);
@@ -113,20 +185,20 @@ static bool encode_agnss_req(zcbor_state_t *state, const struct agnss_req *input
 		(((zcbor_map_start_encode(state, 8) &&
 		   (((!(*input).agnss_req_types_present ||
 		      encode_repeated_agnss_req_types(state, (&(*input).agnss_req_types))) &&
-		     (((zcbor_uint32_put(state, (2)))) &&
-		      (zcbor_uint32_encode(state, (&(*input).agnss_req_eci)))) &&
+		     (!(*input).agnss_req_eci_present ||
+		      encode_repeated_agnss_req_eci(state, (&(*input).agnss_req_eci))) &&
 		     (!(*input).agnss_req_filtered_present ||
 		      encode_repeated_agnss_req_filtered(state, (&(*input).agnss_req_filtered))) &&
 		     (!(*input).agnss_req_mask_present ||
 		      encode_repeated_agnss_req_mask(state, (&(*input).agnss_req_mask))) &&
-		     (((zcbor_uint32_put(state, (5)))) &&
-		      (zcbor_uint32_encode(state, (&(*input).agnss_req_mcc)))) &&
-		     (((zcbor_uint32_put(state, (6)))) &&
-		      (zcbor_uint32_encode(state, (&(*input).agnss_req_mnc)))) &&
+		     (!(*input).agnss_req_mcc_present ||
+		      encode_repeated_agnss_req_mcc(state, (&(*input).agnss_req_mcc))) &&
+		     (!(*input).agnss_req_mnc_present ||
+		      encode_repeated_agnss_req_mnc(state, (&(*input).agnss_req_mnc))) &&
 		     (!(*input).agnss_req_rsrp_present ||
 		      encode_repeated_agnss_req_rsrp(state, (&(*input).agnss_req_rsrp))) &&
-		     (((zcbor_uint32_put(state, (8)))) &&
-		      (zcbor_uint32_encode(state, (&(*input).agnss_req_tac))))) ||
+		     (!(*input).agnss_req_tac_present ||
+		      encode_repeated_agnss_req_tac(state, (&(*input).agnss_req_tac)))) ||
 		    (zcbor_list_map_end_force_encode(state), false)) &&
 		   zcbor_map_end_encode(state, 8))));
 
