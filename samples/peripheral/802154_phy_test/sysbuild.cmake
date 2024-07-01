fe67aee0e36791a537f75cf335049d67d08d1d26
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-07-01 10:08:58 +0100

    samples: peripheral: Fix selecting app core image from network core
    Fixes some issues with the selection of the application core board
    target on samples so that it will work with any board which has an
    nRF5340 instead of just the nrf5340dk
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- samples/peripheral/802154_phy_test/sysbuild.cmake
+++ samples/peripheral/802154_phy_test/sysbuild.cmake
@@ -9,11 +9,18 @@ zephyr_get(EXTRA_ZEPHYR_MODULES)
 set(EXTRA_ZEPHYR_MODULES "${EXTRA_ZEPHYR_MODULES};${CMAKE_CURRENT_LIST_DIR}/modules/app_rpc" CACHE INTERNAL "extra modules directories")
 
 # Include app core image if enabled
-if(SB_CONFIG_BOARD_NRF5340DK_NRF5340_CPUNET)
+if(SB_CONFIG_SOC_NRF5340_CPUNET)
+  # Get application core board target
+  string(REPLACE "/" ";" split_board_qualifiers "${BOARD_QUALIFIERS}")
+  list(GET split_board_qualifiers 1 target_soc)
+  set(board_target_appcore "${BOARD}/${target_soc}/cpuapp")
+  set(target_soc)
+
   ExternalZephyrProject_Add(
     APPLICATION remote_shell
     SOURCE_DIR ${ZEPHYR_NRF_MODULE_DIR}/samples/nrf5340/remote_shell
-    BOARD "nrf5340dk/nrf5340/cpuapp"
+    BOARD ${board_target_appcore}
+    BOARD_REVISION ${BOARD_REVISION}
   )
 
   if(NOT "CPUAPP" IN_LIST PM_DOMAINS)
