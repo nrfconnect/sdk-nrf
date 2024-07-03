e43a7e04c28767ff2c06e4fa1ad72d2f85ea079c
Damian Krolik <damian.krolik@nordicsemi.no>
2024-07-03 16:27:56 +0200

    net: openthread: rpc: interface management functions
    Implement serialization of OpenThread APIs for managing
    the Thread interface state.
    
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- subsys/net/openthread/rpc/common/ot_rpc_types.h
+++ subsys/net/openthread/rpc/common/ot_rpc_types.h
@@ -15,3 +15,11 @@ enum ot_rpc_netif_address_offsets {
 	OT_RPC_NETIF_ADDRESS_RLOC_OFFSET = 7,
 	OT_RPC_NETIF_ADDRESS_MESH_LOCAL_OFFSET = 8,
 };
+
+/** @brief Offsets used to encode otLinkModeConfig bit-fields.
+ */
+enum ot_rpc_link_mode_offsets {
+	OT_RPC_LINK_MODE_RX_ON_WHEN_IDLE_OFFSET = 0,
+	OT_RPC_LINK_MODE_DEVICE_TYPE_OFFSET = 1,
+	OT_RPC_LINK_MODE_NETWORK_DATA_OFFSET = 2,
+};
