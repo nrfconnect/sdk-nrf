b9ad8fa01e4ef8f4891e47c6a8584ddfb8828bfa
Damian Krolik <damian.krolik@nordicsemi.no>
2024-07-05 12:14:31 +0200

    tests: net: openthread: rpc: initial client unit tests
    1. Add mock nRF RPC transport for unit testing serialization
       and deserialization function.
    2. Add initial unit tests for OpenThread RPC client APIs.
    
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- subsys/net/openthread/rpc/common/ot_rpc_group.c
+++ subsys/net/openthread/rpc/common/ot_rpc_group.c
@@ -7,6 +7,8 @@
 #include <nrf_rpc/nrf_rpc_ipc.h>
 #elif CONFIG_NRF_RPC_UART_TRANSPORT
 #include <nrf_rpc/nrf_rpc_uart.h>
+#elif CONFIG_MOCK_NRF_RPC_TRANSPORT
+#include <mock_nrf_rpc_transport.h>
 #endif
 #include <nrf_rpc_cbor.h>
 
@@ -17,6 +19,8 @@
 NRF_RPC_IPC_TRANSPORT(ot_rpc_tr, DEVICE_DT_GET(DT_NODELABEL(ipc0)), "ot_rpc_ept");
 #elif defined(CONFIG_NRF_RPC_UART_TRANSPORT)
 #define ot_rpc_tr NRF_RPC_UART_TRANSPORT(DT_CHOSEN(nordic_rpc_uart))
+#elif defined(CONFIG_MOCK_NRF_RPC_TRANSPORT)
+#define ot_rpc_tr mock_nrf_rpc_tr
 #endif
 NRF_RPC_GROUP_DEFINE(ot_group, "ot", &ot_rpc_tr, NULL, NULL, NULL);
 LOG_MODULE_REGISTER(ot_rpc, LOG_LEVEL_DBG);
