151461d5b0757edfffe083931540dedc7b81e30f
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-07-08 10:48:00 +0100

    samples: bluetooth: direct_test_mode: Update for sysbuild app core
    Updates the sample to use the newly added application core
    sysbuild Kconfig options
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- samples/bluetooth/direct_test_mode/sysbuild.cmake
+++ samples/bluetooth/direct_test_mode/sysbuild.cmake
@@ -3,32 +3,8 @@
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-get_property(PM_DOMAINS GLOBAL PROPERTY PM_DOMAINS)
-
 # Include app core image if enabled
 if(SB_CONFIG_SOC_NRF5340_CPUNET AND DEFINED SB_CONFIG_APPCORE_IMAGE_NAME)
-  # Get application core board target
-  string(REPLACE "/" ";" split_board_qualifiers "${BOARD_QUALIFIERS}")
-  list(GET split_board_qualifiers 1 target_soc)
-  set(board_target_appcore "${BOARD}/${target_soc}/cpuapp")
-  set(target_soc)
-
-  ExternalZephyrProject_Add(
-    APPLICATION ${SB_CONFIG_APPCORE_IMAGE_NAME}
-    SOURCE_DIR ${SB_CONFIG_APPCORE_IMAGE_PATH}
-    BOARD ${board_target_appcore}
-    BOARD_REVISION ${BOARD_REVISION}
-  )
-
-  if(NOT "CPUAPP" IN_LIST PM_DOMAINS)
-    list(APPEND PM_DOMAINS CPUAPP)
-  endif()
-
-  set_property(GLOBAL APPEND PROPERTY PM_CPUAPP_IMAGES "${SB_CONFIG_APPCORE_IMAGE_NAME}")
-  set_property(GLOBAL PROPERTY DOMAIN_APP_CPUAPP "${SB_CONFIG_APPCORE_IMAGE_NAME}")
-  set(CPUAPP_PM_DOMAIN_DYNAMIC_PARTITION ${SB_CONFIG_APPCORE_IMAGE_NAME} CACHE INTERNAL "")
-
-  string(REPLACE " " ";" shield_list "${SHIELD}")
   if("nrf21540ek" IN_LIST shield_list)
     add_overlay_dts(
       ${SB_CONFIG_APPCORE_IMAGE_NAME}
@@ -47,5 +23,3 @@ if(SB_CONFIG_DTM_TRANSPORT_HCI)
 else()
   set_config_bool(direct_test_mode CONFIG_DTM_TRANSPORT_HCI n)
 endif()
-
-set_property(GLOBAL PROPERTY PM_DOMAINS ${PM_DOMAINS})
