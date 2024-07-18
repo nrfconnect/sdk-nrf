583c3306d2399b795bcab168da83ce1083630ec5
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2024-07-18 20:52:19 +0530

    drivers: wifi: Add PS state debugs
    These are very frequent, so, a separate debug is added for debugging
    host RPU recovery logic.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- drivers/wifi/nrf700x/osal/hw_if/hal/src/hal_api.c
+++ drivers/wifi/nrf700x/osal/hw_if/hal/src/hal_api.c
@@ -416,6 +416,11 @@ enum nrf_wifi_status hal_rpu_ps_wake(struct nrf_wifi_hal_dev_ctx *hal_dev_ctx)
 #ifdef CONFIG_NRF_WIFI_RPU_RECOVERY
 	did_rpu_had_sleep_opp(hal_dev_ctx);
 #endif /* CONFIG_NRF_WIFI_RPU_RECOVERY */
+#ifdef CONFIG_NRF_WIFI_RPU_RECOVERY_PS_STATE_DEBUG
+	nrf_wifi_osal_log_info(hal_dev_ctx->hpriv->opriv,
+			       "%s: RPU PS state is AWAKE\n",
+			       __func__);
+#endif /* CONFIG_NRF_WIFI_RPU_RECOVERY_PS_STATE_DEBUG */
 
 out:
 	if (!hal_dev_ctx->irq_ctx) {
@@ -446,6 +451,11 @@ static void hal_rpu_ps_sleep(unsigned long data)
 #endif /* CONFIG_NRF_WIFI_RPU_RECOVERY */
 	hal_dev_ctx->rpu_ps_state = RPU_PS_STATE_ASLEEP;
 
+#ifdef CONFIG_NRF_WIFI_RPU_RECOVERY_PS_STATE_DEBUG
+	nrf_wifi_osal_log_info(hal_dev_ctx->hpriv->opriv,
+			       "%s: RPU PS state is ASLEEP\n",
+			       __func__);
+#endif /* CONFIG_NRF_WIFI_RPU_RECOVERY_PS_STATE_DEBUG */
 	nrf_wifi_osal_spinlock_irq_rel(hal_dev_ctx->hpriv->opriv,
 				       hal_dev_ctx->rpu_ps_lock,
 				       &flags);
