bf8467cecbf61644bbc940f938e3ee684968e9d5
Marek Pieta <Marek.Pieta@nordicsemi.no>
2024-07-24 11:11:26 +0200

    applications: nrf_desktop: Define biggest supported HID report sizes
    Change adds static definitions of the biggest supported HID input and
    output report sizes.
    
    Jira: NCSDK-28375
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- applications/nrf_desktop/configuration/common/hid_report_desc.h
+++ applications/nrf_desktop/configuration/common/hid_report_desc.h
@@ -84,6 +84,35 @@ static const uint8_t output_reports[] = {
 	REPORT_ID_KEYBOARD_LEDS,
 };
 
+/* Internal definitions used to calculate size of the biggest supported HID input report. */
+#define _REPORT_BUFFER_SIZE_MOUSE \
+	(IS_ENABLED(CONFIG_DESKTOP_HID_REPORT_MOUSE_SUPPORT) ? (REPORT_SIZE_MOUSE) : (0))
+#define _REPORT_BUFFER_SIZE_KEYBOARD_KEYS \
+	(IS_ENABLED(CONFIG_DESKTOP_HID_REPORT_KEYBOARD_SUPPORT) ? (REPORT_SIZE_KEYBOARD_KEYS) : (0))
+#define _REPORT_BUFFER_SIZE_SYSTEM_CTRL								\
+	(IS_ENABLED(CONFIG_DESKTOP_HID_REPORT_SYSTEM_CTRL_SUPPORT) ?				\
+	 (REPORT_SIZE_SYSTEM_CTRL) : (0))
+#define _REPORT_BUFFER_SIZE_CONSUMER_CTRL							\
+	(IS_ENABLED(CONFIG_DESKTOP_HID_REPORT_CONSUMER_CTRL_SUPPORT) ?				\
+	 (REPORT_SIZE_CONSUMER_CTRL) : (0))
+#define _REPORT_BUFFER_SIZE_BOOT_MOUSE \
+	(IS_ENABLED(CONFIG_DESKTOP_HID_BOOT_INTERFACE_MOUSE) ? (REPORT_SIZE_MOUSE_BOOT) : (0))
+#define _REPORT_BUFFER_SIZE_BOOT_KEYBOARD \
+	(IS_ENABLED(CONFIG_DESKTOP_HID_BOOT_INTERFACE_KEYBOARD) ? (REPORT_SIZE_KEYBOARD_BOOT) : (0))
+#define _MAX6(a, b, c, d, e, f)	MAX(MAX(a, b), MAX(MAX(c, d), MAX(e, f)))
+
+/* Size of the biggest supported HID input report that is part of input reports map. */
+#define REPORT_BUFFER_SIZE_INPUT_REPORT								\
+	_MAX6(_REPORT_BUFFER_SIZE_MOUSE, _REPORT_BUFFER_SIZE_KEYBOARD_KEYS,			\
+	      _REPORT_BUFFER_SIZE_SYSTEM_CTRL, _REPORT_BUFFER_SIZE_CONSUMER_CTRL,		\
+	      _REPORT_BUFFER_SIZE_BOOT_MOUSE, _REPORT_BUFFER_SIZE_BOOT_KEYBOARD)
+
+/* Internal definitions used to calculate size of the biggest supported HID output report. */
+#define _REPORT_BUFFER_SIZE_KEYBOARD_LEDS \
+	(IS_ENABLED(CONFIG_DESKTOP_HID_REPORT_KEYBOARD_SUPPORT) ? (REPORT_SIZE_KEYBOARD_LEDS) : (0))
+
+/* Size of the biggest supported HID output report that is part of output reports map. */
+#define REPORT_BUFFER_SIZE_OUTPUT_REPORT	_REPORT_BUFFER_SIZE_KEYBOARD_LEDS
 
 extern const uint8_t hid_report_desc[];
 extern const size_t hid_report_desc_size;
