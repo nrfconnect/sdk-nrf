eeab88c444248bb2d111e62b04facad9a190ad6e
Przemyslaw Bida <przemyslaw.bida@nordicsemi.no>
2024-07-24 11:23:03 +0200

    net: openthread: rpc: support net interface over RPC
    Add support for OpenThread L2 that sends outgoing IP
    packets and receives incoming packets over RPC. The RPC
    server uses AF_PACKET net context to capture packets
    received by the actual OpenThread interface.
    
    The OpenThread network interface on the RPC server is
    not auto started and must be explicitly brought up by the
    RPC client.
    
    Signed-off-by: Przemyslaw Bida <przemyslaw.bida@nordicsemi.no>
    Co-authored-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- subsys/net/openthread/rpc/common/ot_rpc_ids.h
+++ subsys/net/openthread/rpc/common/ot_rpc_ids.h
@@ -7,7 +7,9 @@
 /** @brief Command IDs accepted by the OpenThread over RPC client.
  */
 enum ot_rpc_cmd_client {
-	OT_RPC_CMD_CLI_OUTPUT,
+	OT_RPC_CMD_CLI_OUTPUT = 0,
+	OT_RPC_CMD_STATE_CHANGED,
+	OT_RPC_CMD_IF_RECEIVE,
 };
 
 /** @brief Command IDs accepted by the OpenThread over RPC server.
@@ -15,4 +17,10 @@ enum ot_rpc_cmd_client {
 enum ot_rpc_cmd_server {
 	OT_RPC_CMD_CLI_INIT = 0,
 	OT_RPC_CMD_CLI_INPUT_LINE,
+	OT_RPC_CMD_IP6_GET_UNICAST_ADDRESSES,
+	OT_RPC_CMD_SET_STATE_CHANGED_CALLBACK,
+	OT_RPC_CMD_REMOVE_STATE_CHANGED_CALLBACK,
+	OT_RPC_CMD_IF_ENABLE,
+	OT_RPC_CMD_IF_SEND,
+	OT_RPC_CMD_IF_EXTADDR,
 };
