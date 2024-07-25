ffd103298da8cc2d8faf279523c6765b90356605
Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>
2024-07-25 12:56:09 +0200

    samples: bluetooth: iso_time_sync: Fix timed led toggle on 52/53
    The implementations were not correctly handling RTC overflows.
    Now the implementation matches the one used in conn_time_sync.
    A separate PR will extract out common logic.
    
    Signed-off-by: Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>

--- samples/bluetooth/iso_time_sync/src/controller_time_nrf52.c
+++ samples/bluetooth/iso_time_sync/src/controller_time_nrf52.c
@@ -12,6 +12,7 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/sys/barrier.h>
 #include <mpsl_clock.h>
 #include <nrfx_rtc.h>
 #include <nrfx_timer.h>
@@ -221,7 +222,7 @@ static uint32_t us_to_rtc_ticks(uint32_t timestamp_us)
 
 uint64_t controller_time_us_get(void)
 {
-	const uint32_t rtc_overflow_time_us = 512000000UL;
+	const uint64_t rtc_overflow_time_us = 512000000UL;
 
 	/* On the 52 series the RTC has to task to capture the current RTC value.
 	 * Therefore we cannot capture the TIMER and RTC value simultaneously.
@@ -229,9 +230,30 @@ uint64_t controller_time_us_get(void)
 	 * This will result in an error of maximum one RTC tick.
 	 */
 
-	uint32_t rtc_ticks = nrf_rtc_counter_get(app_rtc_instance.p_reg);
+	uint32_t captured_rtc_overflows;
+	uint32_t captured_rtc_ticks;
 
-	return rtc_ticks_to_us(rtc_ticks) + (num_rtc_overflows * rtc_overflow_time_us) +
+	while (true) {
+		captured_rtc_overflows = num_rtc_overflows;
+
+		/* Read out RTC ticks after reading number of overflows. */
+		barrier_isync_fence_full();
+
+		captured_rtc_ticks = nrf_rtc_counter_get(app_rtc_instance.p_reg);
+
+		/* Read out number of overflows after reading number of RTC ticks  */
+		barrier_isync_fence_full();
+
+		if (captured_rtc_overflows == num_rtc_overflows) {
+			/* There were no new overflows after reading ticks.
+			 * That is, we can use the captured value.
+			 */
+			break;
+		}
+	}
+
+	return rtc_ticks_to_us(captured_rtc_ticks) +
+	       (captured_rtc_overflows * rtc_overflow_time_us) +
 	       rtc_ticks_to_us(offset_ticks_and_controller_to_app_rtc);
 }
 
@@ -240,7 +262,10 @@ void controller_time_trigger_set(uint64_t timestamp_us)
 	uint64_t timestamp_without_rtc_offset =
 		timestamp_us - rtc_ticks_to_us(offset_ticks_and_controller_to_app_rtc);
 
-	uint32_t rtc_remainder_time_us = timestamp_without_rtc_offset % 512000000UL;
+	uint32_t num_overflows = timestamp_without_rtc_offset / 512000000UL;
+	uint64_t overflow_time_us = num_overflows * 512000000UL;
+
+	uint32_t rtc_remainder_time_us = timestamp_without_rtc_offset - overflow_time_us;
 	uint32_t rtc_val = us_to_rtc_ticks(rtc_remainder_time_us);
 	uint8_t timer_val =	timestamp_without_rtc_offset - rtc_ticks_to_us(rtc_val);
 
