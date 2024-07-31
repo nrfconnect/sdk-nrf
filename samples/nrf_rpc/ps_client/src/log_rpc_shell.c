f4173118c1cabc04e976afe1a298d02629801d2c
Damian Krolik <damian.krolik@nordicsemi.no>
2024-07-31 13:09:41 +0200

    samples: ps_client: shell command for log_rpc level
    Add "log_rpc stream_level <level>" shell for configuring
    the nRF RPC logging level.
    
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- samples/nrf_rpc/ps_client/src/log_rpc_shell.c
+++ samples/nrf_rpc/ps_client/src/log_rpc_shell.c
@@ -11,7 +11,29 @@
 
 #include <stdlib.h>
 
-static int log_rpc_get_crash_log_cmd(const struct shell *sh, size_t argc, char *argv[])
+static int cmd_log_rpc_stream_level(const struct shell *sh, size_t argc, char *argv[])
+{
+	int rc = 0;
+	enum log_rpc_level level;
+
+	level = (enum log_rpc_level)shell_strtol(argv[1], 10, &rc);
+
+	if (rc) {
+		shell_error(sh, "Invalid argument: %d", rc);
+		return -EINVAL;
+	}
+
+	rc = log_rpc_set_stream_level(level);
+
+	if (rc) {
+		shell_error(sh, "Error: %d", rc);
+		return -ENOEXEC;
+	}
+
+	return 0;
+}
+
+static int cmd_log_rpc_crash(const struct shell *sh, size_t argc, char *argv[])
 {
 	int rc;
 	char buffer[CONFIG_RPC_CRASH_LOG_READ_BUFFER_SIZE];
@@ -29,12 +51,19 @@ static int log_rpc_get_crash_log_cmd(const struct shell *sh, size_t argc, char *
 
 	shell_fprintf(sh, SHELL_NORMAL, "\n");
 
-	return rc;
+	if (rc) {
+		shell_error(sh, "Error: %d", rc);
+		return -ENOEXEC;
+	}
+
+	return 0;
 }
 
 SHELL_STATIC_SUBCMD_SET_CREATE(log_rpc_cmds,
+			       SHELL_CMD_ARG(stream_level, NULL, "Set stream logging level",
+					     cmd_log_rpc_stream_level, 2, 0),
 			       SHELL_CMD_ARG(crash, NULL, "Retrieve remote device crash log",
-					     log_rpc_get_crash_log_cmd, 1, 0),
+					     cmd_log_rpc_crash, 1, 0),
 			       SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_ARG_REGISTER(log_rpc, &log_rpc_cmds, "RPC logging commands", NULL, 1, 0);
