4e1d663850412cdd300cf4bced57e073d676f58c
Sylwester Konczyk <sylwester.konczyk@nordicsemi.no>
2024-08-20 12:46:17 +0200

    mgmt: Extension of SUIT-Firmware-Update service
    Support for DFU Cache raw image upload added.
    Support for DFU cleanup added.
    
    Ref: NCSDK-28335
    
    Signed-off-by: Sylwester Konczyk <sylwester.konczyk@nordicsemi.no>

--- subsys/mgmt/suitfu/src/suitfu_mgmt_suit_cache_raw_upload.c
+++ subsys/mgmt/suitfu/src/suitfu_mgmt_suit_cache_raw_upload.c
@@ -20,7 +20,7 @@
 #include <zephyr/mgmt/mcumgr/mgmt/mgmt.h>
 #include <zephyr/mgmt/mcumgr/mgmt/handlers.h>
 #include "suitfu_mgmt_priv.h"
-#include <suit_envelope_info.h>
+#include <suit_dfu_cache_rw.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(suitfu_mgmt, CONFIG_MGMT_SUITFU_LOG_LEVEL);
@@ -29,17 +29,17 @@ LOG_MODULE_DECLARE(suitfu_mgmt, CONFIG_MGMT_SUITFU_LOG_LEVEL);
 typedef struct {
 	size_t off;  /* SIZE_MAX if unspecified. */
 	size_t size; /* SIZE_MAX if unspecified. */
-	bool defer_install;
+	uint32_t target_id;
 	struct zcbor_string img_data;
 } suitfu_mgmt_envelope_upload_req_t;
 
-int suitfu_mgmt_suit_envelope_upload(struct smp_streamer *ctx)
+int suitfu_mgmt_suit_cache_raw_upload(struct smp_streamer *ctx)
 {
 	zcbor_state_t *zsd = ctx->reader->zs;
 	zcbor_state_t *zse = ctx->writer->zs;
+	static uint32_t target_id;
 	static size_t image_size;
 	static size_t offset_in_image;
-	static bool defer_install;
 
 	static struct suit_nvm_device_info device_info;
 
@@ -47,15 +47,15 @@ int suitfu_mgmt_suit_envelope_upload(struct smp_streamer *ctx)
 	suitfu_mgmt_envelope_upload_req_t req = {
 		.off = SIZE_MAX,
 		.size = SIZE_MAX,
-		.defer_install = false,
+		.target_id = ~0,
 		.img_data = {0},
 	};
 
 	struct zcbor_map_decode_key_val envelope_upload_decode[] = {
 		ZCBOR_MAP_DECODE_KEY_VAL(data, zcbor_bstr_decode, &req.img_data),
 		ZCBOR_MAP_DECODE_KEY_VAL(len, zcbor_size_decode, &req.size),
-		ZCBOR_MAP_DECODE_KEY_VAL(defer_install, zcbor_bool_decode, &req.defer_install),
-		ZCBOR_MAP_DECODE_KEY_VAL(off, zcbor_size_decode, &req.off)};
+		ZCBOR_MAP_DECODE_KEY_VAL(off, zcbor_size_decode, &req.off),
+		ZCBOR_MAP_DECODE_KEY_VAL(target_id, zcbor_uint32_decode, &req.target_id)};
 
 	if (zcbor_map_decode_bulk(zsd, envelope_upload_decode, ARRAY_SIZE(envelope_upload_decode),
 				  &decoded) != 0) {
@@ -64,47 +64,38 @@ int suitfu_mgmt_suit_envelope_upload(struct smp_streamer *ctx)
 	}
 
 	if (req.off == 0) {
-		LOG_INF("New envelope transfer started");
+		LOG_INF("New Cache RAW upload, cache pool %d, image size: %d bytes", req.target_id,
+			req.size);
 		image_size = 0;
 		offset_in_image = 0;
 
 		if (req.size == 0) {
-			if (req.defer_install) {
-				LOG_ERR("Candidate envelope empty");
-				return MGMT_ERR_EINVAL;
-			}
-
-			LOG_INF("Triggering an update");
-			int rc = suitfu_mgmt_candidate_envelope_process();
-
-			if (zcbor_tstr_put_lit(zse, "rc") && zcbor_int32_put(zse, rc) &&
-			    zcbor_tstr_put_lit(zse, "off") &&
-			    zcbor_size_put(zse, offset_in_image)) {
-				return MGMT_ERR_EOK;
-			}
-
-			return MGMT_ERR_EMSGSIZE;
+			LOG_ERR("New Cache RAW upload, cache pool %d, empty image", req.target_id);
+			return MGMT_ERR_EINVAL;
 		}
 
-		suit_plat_err_t err = suit_dfu_partition_device_info_get(&device_info);
-
+		suit_plat_err_t err =
+			suit_dfu_cache_rw_device_info_get(req.target_id, &device_info);
 		if (err != SUIT_PLAT_SUCCESS) {
-			LOG_ERR("DFU Partition not found");
+			LOG_ERR("Cache pool %d not found", req.target_id);
 			return MGMT_ERR_ENOENT;
 		}
 
-		/* Erases dfu_partition and dfu_cache_partition_0, leaves
-		 * dfu_partition_1..dfu_partition_n intact. suitfu_mgmt_claenup() may be used
-		 * instead.
-		 */
+		if (req.size > device_info.partition_size) {
+			LOG_ERR("Image too large: %d bytes. Cache pool %d size: %d bytes", req.size,
+				req.target_id, device_info.partition_size);
+			return MGMT_ERR_ENOMEM;
+		}
+
 		int rc = suitfu_mgmt_erase(&device_info, req.size);
+
 		if (rc != MGMT_ERR_EOK) {
-			LOG_ERR("Erasing DFU partition failed");
-			return rc;
+			LOG_ERR("Cache pool %d, erasing partition failed", req.target_id);
+			return MGMT_ERR_EBADSTATE;
 		}
 
 		image_size = req.size;
-		defer_install = req.defer_install;
+		target_id = req.target_id;
 	}
 
 	bool last = false;
@@ -140,13 +131,7 @@ int suitfu_mgmt_suit_envelope_upload(struct smp_streamer *ctx)
 		req.off += req.img_data.len;
 		offset_in_image += req.img_data.len;
 		if (last) {
-
-			LOG_INF("Candidate envelope stored");
-			rc = suitfu_mgmt_candidate_envelope_stored();
-			if (rc == MGMT_ERR_EOK && !defer_install) {
-				rc = suitfu_mgmt_candidate_envelope_process();
-			}
-
+			LOG_INF("Cache pool %d updated with RAW image", target_id);
 			image_size = 0;
 		}
 	}
@@ -157,9 +142,4 @@ int suitfu_mgmt_suit_envelope_upload(struct smp_streamer *ctx)
 	}
 
 	return MGMT_ERR_EMSGSIZE;
-}
-
-int suitfu_mgmt_suit_cleanup(struct smp_streamer *ctx)
-{
-	return suitfu_mgmt_cleanup();
 }
