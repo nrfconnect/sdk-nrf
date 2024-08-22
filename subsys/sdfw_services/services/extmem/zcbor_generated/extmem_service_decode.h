aabc00f03f2cf1b190f4e7bf3d9d0ac40eafcf21
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2024-08-22 16:34:26 +0200

    sdfw_services: Regenerate zcbor-generated client code
    Using zcbor 0.9.0
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- subsys/sdfw_services/services/extmem/zcbor_generated/extmem_service_decode.h
+++ subsys/sdfw_services/services/extmem/zcbor_generated/extmem_service_decode.h
@@ -5,7 +5,7 @@
  */
 
 /*
- * Generated using zcbor version 0.8.1
+ * Generated using zcbor version 0.9.0
  * https://github.com/NordicSemiconductor/zcbor
  * Generated with a --default-max-qty of 3
  */
@@ -13,28 +13,29 @@
 #ifndef EXTMEM_SERVICE_DECODE_H__
 #define EXTMEM_SERVICE_DECODE_H__
 
-#include <stdint.h>
+#include "extmem_service_types.h"
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <string.h>
-#include "extmem_service_types.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #if DEFAULT_MAX_QTY != 3
-#error "The type file was generated with a different default_max_qty than this file"
+#error                                                                         \
+    "The type file was generated with a different default_max_qty than this file"
 #endif
 
-int cbor_decode_extmem_req(const uint8_t *payload, size_t payload_len, struct extmem_req *result,
-			   size_t *payload_len_out);
+int cbor_decode_extmem_req(const uint8_t *payload, size_t payload_len,
+                           struct extmem_req *result, size_t *payload_len_out);
 
-int cbor_decode_extmem_rsp(const uint8_t *payload, size_t payload_len, struct extmem_rsp *result,
-			   size_t *payload_len_out);
+int cbor_decode_extmem_rsp(const uint8_t *payload, size_t payload_len,
+                           struct extmem_rsp *result, size_t *payload_len_out);
 
-int cbor_decode_extmem_nfy(const uint8_t *payload, size_t payload_len, struct extmem_nfy *result,
-			   size_t *payload_len_out);
+int cbor_decode_extmem_nfy(const uint8_t *payload, size_t payload_len,
+                           struct extmem_nfy *result, size_t *payload_len_out);
 
 #ifdef __cplusplus
 }
--- subsys/sdfw_services/services/extmem/zcbor_generated/extmem_service_types.h
+++ subsys/sdfw_services/services/extmem/zcbor_generated/extmem_service_types.h
@@ -5,7 +5,7 @@
  */
 
 /*
- * Generated using zcbor version 0.8.1
+ * Generated using zcbor version 0.9.0
  * https://github.com/NordicSemiconductor/zcbor
  * Generated with a --default-max-qty of 3
  */
@@ -13,9 +13,9 @@
 #ifndef EXTMEM_SERVICE_TYPES_H__
 #define EXTMEM_SERVICE_TYPES_H__
 
-#include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -31,101 +31,105 @@ extern "C" {
 #define DEFAULT_MAX_QTY 3
 
 struct extmem_read_done_req {
-	uint32_t extmem_read_done_req_request_id;
-	uint32_t extmem_read_done_req_error;
-	uint32_t extmem_read_done_req_addr;
+  uint32_t extmem_read_done_req_request_id;
+  uint32_t extmem_read_done_req_error;
+  uint32_t extmem_read_done_req_addr;
 };
 
 struct extmem_write_setup_req {
-	uint32_t extmem_write_setup_req_request_id;
-	uint32_t extmem_write_setup_req_error;
-	uint32_t extmem_write_setup_req_addr;
+  uint32_t extmem_write_setup_req_request_id;
+  uint32_t extmem_write_setup_req_error;
+  uint32_t extmem_write_setup_req_addr;
 };
 
 struct extmem_write_done_req {
-	uint32_t extmem_write_done_req_request_id;
-	uint32_t extmem_write_done_req_error;
+  uint32_t extmem_write_done_req_request_id;
+  uint32_t extmem_write_done_req_error;
 };
 
 struct extmem_erase_done_req {
-	uint32_t extmem_erase_done_req_request_id;
-	uint32_t extmem_erase_done_req_error;
+  uint32_t extmem_erase_done_req_request_id;
+  uint32_t extmem_erase_done_req_error;
 };
 
 struct extmem_get_capabilities_req {
-	uint32_t extmem_get_capabilities_req_request_id;
-	uint32_t extmem_get_capabilities_req_error;
-	uint32_t extmem_get_capabilities_req_base_addr;
-	uint32_t extmem_get_capabilities_req_capacity;
-	uint32_t extmem_get_capabilities_req_erase_size;
-	uint32_t extmem_get_capabilities_req_write_size;
-	uint32_t extmem_get_capabilities_req_chunk_size;
-	bool extmem_get_capabilities_req_memory_mapped;
+  uint32_t extmem_get_capabilities_req_request_id;
+  uint32_t extmem_get_capabilities_req_error;
+  uint32_t extmem_get_capabilities_req_base_addr;
+  uint32_t extmem_get_capabilities_req_capacity;
+  uint32_t extmem_get_capabilities_req_erase_size;
+  uint32_t extmem_get_capabilities_req_write_size;
+  uint32_t extmem_get_capabilities_req_chunk_size;
+  bool extmem_get_capabilities_req_memory_mapped;
 };
 
 struct extmem_req {
-	union {
-		struct extmem_read_done_req extmem_req_msg_extmem_read_done_req_m;
-		struct extmem_write_setup_req extmem_req_msg_extmem_write_setup_req_m;
-		struct extmem_write_done_req extmem_req_msg_extmem_write_done_req_m;
-		struct extmem_erase_done_req extmem_req_msg_extmem_erase_done_req_m;
-		struct extmem_get_capabilities_req extmem_req_msg_extmem_get_capabilities_req_m;
-	};
-	enum {
-		extmem_req_msg_extmem_read_done_req_m_c,
-		extmem_req_msg_extmem_write_setup_req_m_c,
-		extmem_req_msg_extmem_write_done_req_m_c,
-		extmem_req_msg_extmem_erase_done_req_m_c,
-		extmem_req_msg_extmem_get_capabilities_req_m_c,
-	} extmem_req_msg_choice;
+  union {
+    struct extmem_read_done_req extmem_req_msg_extmem_read_done_req_m;
+    struct extmem_write_setup_req extmem_req_msg_extmem_write_setup_req_m;
+    struct extmem_write_done_req extmem_req_msg_extmem_write_done_req_m;
+    struct extmem_erase_done_req extmem_req_msg_extmem_erase_done_req_m;
+    struct extmem_get_capabilities_req
+        extmem_req_msg_extmem_get_capabilities_req_m;
+  };
+  enum {
+    extmem_req_msg_extmem_read_done_req_m_c,
+    extmem_req_msg_extmem_write_setup_req_m_c,
+    extmem_req_msg_extmem_write_done_req_m_c,
+    extmem_req_msg_extmem_erase_done_req_m_c,
+    extmem_req_msg_extmem_get_capabilities_req_m_c,
+  } extmem_req_msg_choice;
 };
 
 struct extmem_rsp {
-	enum {
-		extmem_rsp_msg_extmem_read_done_rsp_m_c = 1,
-		extmem_rsp_msg_extmem_write_setup_rsp_m_c = 3,
-		extmem_rsp_msg_extmem_write_done_rsp_m_c = 5,
-		extmem_rsp_msg_extmem_erase_done_rsp_m_c = 7,
-		extmem_rsp_msg_extmem_get_capabilities_rsp_m_c = 9,
-	} extmem_rsp_msg_choice;
+  enum {
+    extmem_rsp_msg_extmem_read_done_rsp_m_c = 1,
+    extmem_rsp_msg_extmem_write_setup_rsp_m_c = 3,
+    extmem_rsp_msg_extmem_write_done_rsp_m_c = 5,
+    extmem_rsp_msg_extmem_erase_done_rsp_m_c = 7,
+    extmem_rsp_msg_extmem_get_capabilities_rsp_m_c = 9,
+  } extmem_rsp_msg_choice;
 };
 
 struct extmem_read_pending_notify {
-	uint32_t extmem_read_pending_notify_request_id;
-	uint32_t extmem_read_pending_notify_offset;
-	uint32_t extmem_read_pending_notify_size;
+  uint32_t extmem_read_pending_notify_request_id;
+  uint32_t extmem_read_pending_notify_offset;
+  uint32_t extmem_read_pending_notify_size;
 };
 
 struct extmem_write_pending_notify {
-	uint32_t extmem_write_pending_notify_request_id;
-	uint32_t extmem_write_pending_notify_offset;
-	uint32_t extmem_write_pending_notify_size;
+  uint32_t extmem_write_pending_notify_request_id;
+  uint32_t extmem_write_pending_notify_offset;
+  uint32_t extmem_write_pending_notify_size;
 };
 
 struct extmem_erase_pending_notify {
-	uint32_t extmem_erase_pending_notify_request_id;
-	uint32_t extmem_erase_pending_notify_offset;
-	uint32_t extmem_erase_pending_notify_size;
+  uint32_t extmem_erase_pending_notify_request_id;
+  uint32_t extmem_erase_pending_notify_offset;
+  uint32_t extmem_erase_pending_notify_size;
 };
 
 struct extmem_get_capabilities_notify_pending {
-	uint32_t extmem_get_capabilities_notify_pending_request_id;
+  uint32_t extmem_get_capabilities_notify_pending_request_id;
 };
 
 struct extmem_nfy {
-	union {
-		struct extmem_read_pending_notify extmem_nfy_msg_extmem_read_pending_notify_m;
-		struct extmem_write_pending_notify extmem_nfy_msg_extmem_write_pending_notify_m;
-		struct extmem_erase_pending_notify extmem_nfy_msg_extmem_erase_pending_notify_m;
-		struct extmem_get_capabilities_notify_pending
-			extmem_nfy_msg_extmem_get_capabilities_notify_pending_m;
-	};
-	enum {
-		extmem_nfy_msg_extmem_read_pending_notify_m_c,
-		extmem_nfy_msg_extmem_write_pending_notify_m_c,
-		extmem_nfy_msg_extmem_erase_pending_notify_m_c,
-		extmem_nfy_msg_extmem_get_capabilities_notify_pending_m_c,
-	} extmem_nfy_msg_choice;
+  union {
+    struct extmem_read_pending_notify
+        extmem_nfy_msg_extmem_read_pending_notify_m;
+    struct extmem_write_pending_notify
+        extmem_nfy_msg_extmem_write_pending_notify_m;
+    struct extmem_erase_pending_notify
+        extmem_nfy_msg_extmem_erase_pending_notify_m;
+    struct extmem_get_capabilities_notify_pending
+        extmem_nfy_msg_extmem_get_capabilities_notify_pending_m;
+  };
+  enum {
+    extmem_nfy_msg_extmem_read_pending_notify_m_c,
+    extmem_nfy_msg_extmem_write_pending_notify_m_c,
+    extmem_nfy_msg_extmem_erase_pending_notify_m_c,
+    extmem_nfy_msg_extmem_get_capabilities_notify_pending_m_c,
+  } extmem_nfy_msg_choice;
 };
 
 #ifdef __cplusplus
