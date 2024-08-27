58d8c150cdcef35fb1486a6b1553beade6cd0ef3
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-08-27 15:29:43 +0100

    tests: nrf_compress: decompression: Add mcuboot_update test
    Adds a test which ensures compressed update image support works
    in MCUbot
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

---
+++ tests/subsys/nrf_compress/decompression/mcuboot_update/sysbuild.cmake
@@ -0,0 +1,18 @@
+#
+# Copyright (c) 2024 Nordic Semiconductor
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+#
+
+ExternalZephyrProject_Add(
+  APPLICATION compressed_app
+  SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/compressed_app
+)
+
+set_config_bool(compressed_app CONFIG_BOOTLOADER_MCUBOOT "${SB_CONFIG_BOOTLOADER_MCUBOOT}")
+set_config_string(compressed_app CONFIG_MCUBOOT_SIGNATURE_KEY_FILE
+                  "${SB_CONFIG_BOOT_SIGNATURE_KEY_FILE}"
+)
+set_config_bool(compressed_app CONFIG_MCUBOOT_BOOTLOADER_MODE_OVERWRITE_ONLY y)
+set(compressed_app_SIGNING_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/modified_signing.cmake" CACHE INTERNAL "MCUboot signing script" FORCE)
+set_config_bool(compressed_app CONFIG_MCUBOOT_COMPRESSED_IMAGE_SUPPORT_ENABLED y)
