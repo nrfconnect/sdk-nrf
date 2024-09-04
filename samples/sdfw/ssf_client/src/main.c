931f9a3e86da44dd657f29c11e6444ae603ae6ae
Andreas Moltumyr <andreas.moltumyr@nordicsemi.no>
2024-09-04 13:40:26 +0200

    samples: sdfw: ssf_client: remove deprecated sdfw_update service req
    Remove deprecated sdfw_update service request from the sample.
    
    Signed-off-by: Andreas Moltumyr <andreas.moltumyr@nordicsemi.no>

--- samples/sdfw/ssf_client/src/main.c
+++ samples/sdfw/ssf_client/src/main.c
@@ -6,17 +6,9 @@
 
 #include <sdfw/sdfw_services/echo_service.h>
 #include <sdfw/sdfw_services/reset_evt_service.h>
-#include <sdfw/sdfw_services/sdfw_update_service.h>
 
-#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(ssf_client_sample, CONFIG_SSF_CLIENT_SAMPLE_LOG_LEVEL);
-
-#if CONFIG_IMPRIMATUR_ENABLED
-#define IMPRIMATUR_UPDATE_IMAGE_OFFSET CONFIG_IMPRIMATUR_UPDATE_IMAGE_OFFSET
-#else
-#define IMPRIMATUR_UPDATE_IMAGE_OFFSET (0xe155000)
-#endif
 
 static void echo_request(void)
 {
@@ -52,24 +44,6 @@ static void reset_evt_subscribe(void)
 	}
 }
 
-static void sdfw_update(void)
-{
-	int err;
-
-	uintptr_t *image_data = (uintptr_t *)IMPRIMATUR_UPDATE_IMAGE_OFFSET;
-
-	if (IS_ENABLED(CONFIG_UPDATE_REQUEST_CHECK_DATA) && *image_data == 0xFFFFFFFF) {
-		LOG_WRN("No update download data.");
-		return;
-	}
-
-	err = ssf_sdfw_update(IMPRIMATUR_UPDATE_IMAGE_OFFSET);
-	if (err != 0) {
-		LOG_ERR("Unable to perform sdfw update, err: %d", err);
-		return;
-	}
-}
-
 int main(void)
 {
 	LOG_INF("ssf client sample (%s)", CONFIG_BOARD);
@@ -80,10 +54,6 @@ int main(void)
 
 	if (IS_ENABLED(CONFIG_ENABLE_RESET_EVT_SUBSCRIBE_REQUEST)) {
 		reset_evt_subscribe();
-	}
-
-	if (IS_ENABLED(CONFIG_ENABLE_SDFW_UPDATE_REQUEST)) {
-		sdfw_update();
 	}
 
 	return 0;
