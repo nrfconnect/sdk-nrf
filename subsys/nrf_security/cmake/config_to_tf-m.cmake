88e3a498dea2e64bdac11251a3f670b69fa88f4a
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2024-09-04 13:39:18 +0200

    crypto: PSA core: Add psa_crypto_config + _library_config
    -This commit adds usage of psa_crypto_config and
     psa_crypto_config_library and it is a major refactoring on
     the PSA core in build
    -Previously we have made choices on whether or not to build
     specific features dependent on CMake variables constructed
     by nrf_security (BUILD_INSIDE_TFM and/or COMPILE_PSA_APIS).
     These have been removed in favor of the standard signals
     that exists in Mbed TLS and TF-M
     (MBEDTLS_PSA_CRYPTO_SPM and MBEDTLS_PSA_CRYPTO_C). This
     commit tries to fix a technical debt issue of using
     multiple competing signals for the same meaning. From this
     commit on, the standard signals will be used in all cases.
    -Added early exit in PSA crypto core build in case
     MBEDTLS_PSA_CRYPTO_C is not set (meaning it doesn't need
     to be built)
    -Added an early exit in all PSA driver build in case
     MBEDTLS_PSA_CRYPTO_C is not set (meaning it doesn't build
     the PSA core and no drivers are needed)
    -Adding cmake/generate_configs.cmake that includes macros
     generate_mbedcrypto_interface_configs and
     generate_mbedcrypto_library_configs
     which is used to ensure
    -Adding macros to store/restore specific configurations
     that needs to be changed depending on whether you have
     an interface build or a library build (generated in two
     sets)
    -Removing Kconfig GENERATE_MBEDTLS_CFG_FILE as it is
     unsupported
    -Removing MBEDTLS_USER_CONFIG_FILE as we are now using
     MBEDTLS_PSA_CRYPTO_USER_CONFIG_FILE
    -Changing the default names for Mbed TLS config files
     according to the generated files named earlier in this
     commit-message
    -Removed all references to mbedcrypto_common which was
     used to transfer information both to PSA crypto interface
     and library build. This is now handled by psa_crypto_config
     and psa_crypto_library_config
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- subsys/nrf_security/cmake/config_to_tf-m.cmake
+++ subsys/nrf_security/cmake/config_to_tf-m.cmake
@@ -20,22 +20,6 @@ set_property(TARGET zephyr_property_target
   PROPERTY TFM_MBEDCRYPTO_PATH ${CMAKE_CURRENT_LIST_DIR}/../tfm
 )
 
-# Set the configuration files
-set_property(TARGET zephyr_property_target
-  APPEND
-    PROPERTY TFM_CMAKE_OPTIONS
-      -DTFM_MBEDCRYPTO_CONFIG_PATH:STRING=${CONFIG_MBEDTLS_CFG_FILE}
-      -DTFM_MBEDCRYPTO_PSA_CRYPTO_CONFIG_PATH:STRING=${CMAKE_CURRENT_BINARY_DIR}/src/include/generated/${CONFIG_MBEDTLS_PSA_CRYPTO_CONFIG_FILE}
-)
-
-if(NOT ${CONFIG_MBEDTLS_USER_CONFIG_FILE} STREQUAL "nrf-config-user-empty.h" )
-  set_property(TARGET zephyr_property_target
-  APPEND
-    PROPERTY TFM_CMAKE_OPTIONS
-      -DTFM_MBEDCRYPTO_USER_CONFIG_PATH:STRING=${CMAKE_CURRENT_BINARY_DIR}/src/include/generated/${CONFIG_MBEDTLS_USER_CONFIG_FILE}
-  )
-endif()
-
 if(CONFIG_PSA_ITS_ENCRYPTED)
   set_property(TARGET zephyr_property_target
     APPEND PROPERTY TFM_CMAKE_OPTIONS
