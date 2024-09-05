f24ba847acf68b793b0cf662a6fed1dfca211804
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2024-09-05 08:54:37 +0200

    crypto: Adding threading support for PSA core
    -Add support for threading in PSA core to align with Mbed TLS 3.6.0
     Mbed TLS 3.6.0 requires 3 mutex-symbols being globally accessible:
     - mbedtls_threading_key_slot_mutex
     - mbedtls_threading_psa_globaldata_mutex
     - mbedtls_threading_psa_rngdata_mutex
    
    Note: This is not used when CryptoCell is enabled, which has a
    different solution for threading support available in platform
    integration files found in nrfxlib.
    
    Note: This commit will likely be replaced with a implementation
    upstream, but is added here temporarily
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

---
+++ subsys/nrf_security/src/threading/include/threading_alt.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MBEDTLS_THREADING_ALT_H
+#define MBEDTLS_THREADING_ALT_H
+
+#include <zephyr/kernel.h>
+#include "mbedtls/build_info.h"
+
+typedef struct k_mutex mbedtls_threading_mutex_t;
+
+#endif /* MBEDTLS_THREADING_ALT_H */
