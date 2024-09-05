be8be2313fdd30790902a40e4971cde9afc5315b
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2024-09-05 11:52:59 +0200

    tf-m: Add support for building PSA core externally
    -TF-M has no active support for out-of-tree builds of PSA core, and they
     have decided to place every PSA core (and interface file) in a common
     folder that we will have problem to use directly, as it conflicts with
     changed files on our end or files provided by the Oberon PSA core.
     This commit likely will be adjusted at some later point, but it tries
     to control include-order towards our replacement files and our strategy
     of handling generated crypto configurations for the build of
     the PSA interface and the library containing crypto support
    -This commit relies on the CMake variable PSA_CRYPTO_EXTERNAL_CORE
     which is added as a [nrf noup] in TF-M
    -This commit adds external_core.cmake which handles the following:
     - Ensure includes happen right after TF-M creates library targets
       to ensure that include-order dependency is resolved for taking in
       out-of-tree include folder from nrf_security, generated config file
       folder, Oberon PSA core include folders and out-of-tree addition
       of Arm Mbed TLS distribution (include and library folder)
     - Ensure that the generated config files (configured in NCS) are
       propagated to the TF-M build and is used for every library that
       depends on either PSA interface or library version of these
    -This commit adds external_core_install.cmake which will be used instead
     of the TF-M install target for specific PSA interface files.
    -Adding lots of files from TF-M tree into nrf_security include folder.
     nrf_security/include is added early in the include order to override
     equivalently named files from Arm Mbed TLS distribution in the PSA
     interface usage
    
    Note: It is frustrating that TF-M is include order dependent, and parts
    of the work to fix this will go towards ensuring that this is not
    in effect in upstream TF-M code. The files copied here are unfortunate
    and is only done to accelerate TF-M 2.1 integration...
    
    Please see NCSDK-XXXXX for details on how to fix this in upstream TF-M
    
    ref: NCSDK-XXXXX
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

---
+++ modules/trusted-firmware-m/tfm_boards/external_core.cmake
@@ -0,0 +1,126 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2024, Arm Limited. All rights reserved.
+# Copyright (c) 2024, Nordic Semiconductor ASA.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+
+#=================== Build logic for external PSA core build ==================#
+
+# Break out of this if PSA_CRYPTO_EXTERNAL_CORE is not set
+if(NOT PSA_CRYPTO_EXTERNAL_CORE)
+    return()
+endif()
+
+# Note that 
+
+# Adjusting includes from spe-CMakeLists.txt which has the following heading:
+#
+# This CMake script is prepard by TF-M for building the non-secure side
+# application and not used in secure build a tree being for export only.
+# This file is renamed to spe/CMakeList.txt during installation phase
+#
+if(TARGET tfm_api_ns)
+    set(EXTERNAL_CRYPTO_CORE_HANDLED_TFM_API_NS True)
+    target_include_directories(tfm_api_ns
+            PUBLIC
+                    ${OBERON_PSA_CORE_PATH}/include
+                    ${INTERFACE_INC_DIR}/crypto_keys
+    )
+endif()
+
+# Duplicates that can be removed
+#set(TFM_MBEDCRYPTO_CONFIG_PATH ${MBEDTLS_CFG_FILE})
+#set(TFM_MBEDCRYPTO_PSA_CRYPTO_CONFIG_PATH ${MBEDTLS_PSA_CRYPTO_CONFIG_FILE})
+#set(TFM_MBEDCRYPTO_PSA_CRYPTO_USER_CONFIG_PATH ${MBEDTLS_PSA_CRYPTO_USER_CONFIG_FILE})
+
+# Note: This is a duplicate from nrf_security/CMakeLists.txt
+#       with additions of the install-target for Oberon-psa-core includes
+if(TARGET psa_interface)
+    set(EXTERNAL_CRYPTO_CORE_HANDLED_PSA_INTERFACE True)
+    target_include_directories(psa_interface
+        INTERFACE
+            $<BUILD_INTERFACE:${OBERON_PSA_CORE_PATH}/include>
+            # Oberon library
+            ${OBERON_PSA_CORE_PATH}/library
+            # Mbed TLS (mbedcrypto) PSA headers
+            ${ARM_MBEDTLS_PATH}/library
+            ${ARM_MBEDTLS_PATH}/include
+            ${ARM_MBEDTLS_PATH}/include/library 
+    )
+endif()
+
+# Constructing config libraries in partition/crypto/CMakeLists.txt
+
+# Configurations files for the users of the client interface
+if(TARGET psa_crypto_config)
+    set(EXTERNAL_CRYPTO_CORE_HANDLED_PSA_CRYPTO_CONFIG True)
+    target_compile_definitions(psa_crypto_config
+        INTERFACE
+            MBEDTLS_CONFIG_FILE="${MBEDTLS_CONFIG_FILE}"
+            MBEDTLS_PSA_CRYPTO_CONFIG_FILE="${MBEDTLS_PSA_CRYPTO_CONFIG_FILE}"
+            # Give a signal that we are inside TF-M build to prevent check_config.h
+            # complaining about lacking legacy features for Mbed TLS wrapper APIs, TLS/DTLS and X.509.
+            INSIDE_TFM_BUILD
+    )
+
+    target_include_directories(psa_crypto_config
+        INTERFACE
+            ${PSA_CRYPTO_CONFIG_INTERFACE_PATH}
+            ${NRF_SECURITY_ROOT}/include
+            ${OBERON_PSA_CORE_PATH}/include/
+    )
+endif()
+
+# This defines the configuration files for the users of the library directly
+if(TARGET psa_crypto_library_config)
+    set(EXTERNAL_CRYPTO_CORE_HANDLED_PSA_CRYPTO_LIBRARY_CONFIG True)
+    target_compile_definitions(psa_crypto_library_config
+        INTERFACE
+            MBEDTLS_CONFIG_FILE="${MBEDTLS_CONFIG_FILE}"
+            MBEDTLS_PSA_CRYPTO_CONFIG_FILE="${MBEDTLS_PSA_CRYPTO_CONFIG_FILE}"
+            MBEDTLS_PSA_CRYPTO_USER_CONFIG_FILE="${MBEDTLS_PSA_CRYPTO_USER_CONFIG_FILE}"
+    )
+
+    target_include_directories(psa_crypto_library_config
+        INTERFACE
+            ${PSA_CRYPTO_CONFIG_LIBRARY_PATH}
+            ${NRF_SECURITY_ROOT}/include
+            ${OBERON_PSA_CORE_PATH}/include/
+    )
+endif()
+
+if(TARGET tfm_psa_rot_partition_crypto)
+    set(EXTERNAL_CRYPTO_CORE_HANDLED_TFM_PSA_ROT_PARTITION_CRYPTO True)
+    target_link_libraries(tfm_psa_rot_partition_crypto
+        PRIVATE
+            psa_crypto_library_config
+            psa_interface
+    )
+endif()
+
+if(TARGET ${MBEDTLS_TARGET_PREFIX}mbedcrypto)
+    # Can't store the state as there might be multiple calls depending on MBEDTLS_TARGET_PREFIX
+    target_include_directories(${MBEDTLS_TARGET_PREFIX}mbedcrypto
+        PUBLIC
+            # The following is required for psa/error.h
+            $<BUILD_INTERFACE:${OBERON_PSA_CORE_PATH}/include>
+    )
+endif()
+
+if(TARGET tfm_sprt)
+    set(EXTERNAL_CRYPTO_CORE_HANDLED_TFM_SPRT True)
+    target_compile_definitions(tfm_sprt
+        PRIVATE
+            MBEDTLS_CONFIG_FILE="${MBEDTLS_CONFIG_FILE}"
+            MBEDTLS_PSA_CRYPTO_CONFIG_FILE="${MBEDTLS_PSA_CRYPTO_CONFIG_FILE}"
+            INSIDE_TFM_BUILD
+    )
+
+    target_link_libraries(tfm_sprt
+        PRIVATE
+            psa_crypto_config
+            psa_interface
+    )
+endif()
---
+++ subsys/nrf_security/include/tfm_attest_defs.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __TFM_ATTEST_DEFS_H__
+#define __TFM_ATTEST_DEFS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Initial Attestation message types that distinguish Attest services. */
+#define TFM_ATTEST_GET_TOKEN       1001
+#define TFM_ATTEST_GET_TOKEN_SIZE  1002
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __TFM_ATTEST_DEFS_H__ */
