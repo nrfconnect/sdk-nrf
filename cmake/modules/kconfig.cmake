2454fff88db2bf9418a6247edcffa5d1442841b9
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-09-12 10:17:59 +0100

    cmake: modules: kconfig: Update autoconf path
    Updates the path to the autoconf file as this has changed in
    Zephyr
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- cmake/modules/kconfig.cmake
+++ cmake/modules/kconfig.cmake
@@ -9,14 +9,14 @@ include_guard(GLOBAL)
 if(CONFIG_NCS_IS_VARIANT_IMAGE)
   # A variant build should reuse same .config and thus autoconf.h, therefore
   # copy files from original and bypass Kconfig invocation.
-  set(AUTOCONF_H ${PROJECT_BINARY_DIR}/include/generated/autoconf.h)
+  set(AUTOCONF_H ${PROJECT_BINARY_DIR}/include/generated/zephyr/autoconf.h)
   set(DOTCONFIG  ${PROJECT_BINARY_DIR}/.config)
 
-  set(preload_autoconf_h ${PRELOAD_BINARY_DIR}/zephyr/include/generated/autoconf.h)
+  set(preload_autoconf_h ${PRELOAD_BINARY_DIR}/zephyr/include/generated/zephyr/autoconf.h)
   set(preload_dotconfig  ${PRELOAD_BINARY_DIR}/zephyr/.config)
 
   file(COPY ${preload_dotconfig}  DESTINATION ${PROJECT_BINARY_DIR})
-  file(COPY ${preload_autoconf_h} DESTINATION ${PROJECT_BINARY_DIR}/include/generated)
+  file(COPY ${preload_autoconf_h} DESTINATION ${PROJECT_BINARY_DIR}/include/generated/zephyr)
   file(APPEND ${AUTOCONF_H} "#define CONFIG_NCS_IS_VARIANT_IMAGE 1")
 
   set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${AUTOCONF_H})
