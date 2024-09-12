d6d7594972866e0e37f2d3d11570e121c04b749a
Jakub Zymelka <jakub.zymelka@nordicsemi.no>
2024-09-12 15:03:33 +0200

    applications: sdp: add mbox backend for eGPIO
    Add an mbox-based shared struct type to the possible
    ipc communication channels.
    
    Signed-off-by: Jakub Zymelka <jakub.zymelka@nordicsemi.no>

--- applications/sdp/gpio/src/main.c
+++ applications/sdp/gpio/src/main.c
@@ -4,24 +4,13 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
+#include "../include/main.h"
+
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/dt-bindings/gpio/nordic-nrf-gpio.h>
-#include <zephyr/ipc/ipc_service.h>
-
-#include <drivers/gpio/nrfe_gpio.h>
-
 #include <hal/nrf_vpr_csr.h>
 #include <hal/nrf_vpr_csr_vio.h>
 #include <haly/nrfy_gpio.h>
-
-static struct ipc_ept ep;
-
-volatile uint32_t bound_sem = 1;
-
-static void ep_bound(void *priv)
-{
-	bound_sem = 0;
-}
 
 static nrf_gpio_pin_pull_t get_pull(gpio_flags_t flags)
 {
@@ -122,10 +111,8 @@ static void gpio_nrfe_port_toggle_bits(uint16_t toggle_mask)
 	nrf_vpr_csr_vio_out_toggle_set(toggle_mask);
 }
 
-static void ep_recv(const void *data, size_t len, void *priv)
+void process_packet(nrfe_gpio_data_packet_t *packet)
 {
-	nrfe_gpio_data_packet_t *packet = (nrfe_gpio_data_packet_t *)data;
-
 	if (packet->port != 2) {
 		return;
 	}
@@ -153,41 +140,14 @@ static void ep_recv(const void *data, size_t len, void *priv)
 	}
 }
 
-static struct ipc_ept_cfg ep_cfg = {
-	.cb = {
-		.bound = ep_bound,
-		.received = ep_recv,
-	},
-};
-
 int main(void)
 {
-	int ret;
-	const struct device *ipc0_instance;
-	volatile uint32_t delay = 0;
+	int ret = 0;
 
-#if !defined(CONFIG_SYS_CLOCK_EXISTS)
-	/* Wait a little bit for IPC service to be ready on APP side */
-	while (delay < 1000) {
-		delay++;
-	}
-#endif
-
-	ipc0_instance = DEVICE_DT_GET(DT_NODELABEL(ipc0));
-
-	ret = ipc_service_open_instance(ipc0_instance);
-	if ((ret < 0) && (ret != -EALREADY)) {
-		return ret;
-	}
-
-	ret = ipc_service_register_endpoint(ipc0_instance, &ep, &ep_cfg);
+	ret = backend_init();
 	if (ret < 0) {
-		return ret;
+		return 0;
 	}
-
-	/* Wait for endpoint to be bound */
-	while (bound_sem != 0) {
-	};
 
 	if (!nrf_vpr_csr_rtperiph_enable_check()) {
 		nrf_vpr_csr_rtperiph_enable_set(true);
