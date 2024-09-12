b226268decf8097925b415c584ef34a3209651c1
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2024-09-12 13:53:02 +0200

    mgmt: Use bootloader info hooks in SUIT
    Use bootloader info hook to provide SUIT bootloader identification.
    
    Ref: NCSDK-NONE
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- subsys/mgmt/suitfu/src/suitfu_mgmt_suit_os.c
+++ subsys/mgmt/suitfu/src/suitfu_mgmt_suit_os.c
@@ -5,37 +5,45 @@
  */
 
 #include <zcbor_common.h>
-#include <zcbor_decode.h>
 #include <zcbor_encode.h>
-#include <mgmt/mcumgr/util/zcbor_bulk.h>
 
-#include <zephyr/mgmt/mcumgr/mgmt/mgmt.h>
-#include <zephyr/mgmt/mcumgr/mgmt/handlers.h>
+#include <zephyr/mgmt/mcumgr/mgmt/callbacks.h>
+#include <zephyr/mgmt/mcumgr/grp/os_mgmt/os_mgmt.h>
 #include "suitfu_mgmt_priv.h"
 
-int suitfu_mgmt_suit_bootloader_info_read(struct smp_streamer *ctxt)
+static enum mgmt_cb_return bootloader_info_hook(uint32_t event, enum mgmt_cb_return prev_status,
+					int32_t *rc, uint16_t *group, bool *abort_more,
+					void *data, size_t data_size)
 {
-	zcbor_state_t *zse = ctxt->writer->zs;
-	zcbor_state_t *zsd = ctxt->reader->zs;
-	struct zcbor_string query = { 0 };
-	size_t decoded = 0;
-	bool ok;
+	struct os_mgmt_bootloader_info_data *bootloader_info_data;
 
-	struct zcbor_map_decode_key_val bootloader_info[] = {
-		ZCBOR_MAP_DECODE_KEY_DECODER("query", zcbor_tstr_decode, &query),
-	};
-
-	if (zcbor_map_decode_bulk(zsd, bootloader_info, ARRAY_SIZE(bootloader_info), &decoded)) {
-		return MGMT_ERR_EINVAL;
+	if ((event != MGMT_EVT_OP_OS_MGMT_BOOTLOADER_INFO)
+	    || (data_size != sizeof(*bootloader_info_data))) {
+		*rc = MGMT_ERR_EUNKNOWN;
+		return MGMT_CB_ERROR_RC;
 	}
+
+	bootloader_info_data = data;
 
 	/* If no parameter is recognized then just introduce the bootloader. */
-	if (decoded == 0) {
-		ok = zcbor_tstr_put_lit(zse, "bootloader") &&
-		     zcbor_tstr_put_lit(zse, "SUIT");
+	if (*(bootloader_info_data->decoded) == 0) {
+		bool ok = zcbor_tstr_put_lit(bootloader_info_data->zse, "bootloader") &&
+		     zcbor_tstr_put_lit(bootloader_info_data->zse, "SUIT");
+
+		*rc = (ok ? MGMT_ERR_EOK : MGMT_ERR_EMSGSIZE);
 	} else {
-		return MGMT_ERR_ENOTSUP;
+		*rc = MGMT_ERR_ENOTSUP;
 	}
 
-	return ok ? MGMT_ERR_EOK : MGMT_ERR_EMSGSIZE;
+	return MGMT_CB_ERROR_RC;
+}
+
+static struct mgmt_callback cmd_bootloader_info_cb = {
+	.callback = bootloader_info_hook,
+	.event_id = MGMT_EVT_OP_OS_MGMT_BOOTLOADER_INFO,
+};
+
+void suitfu_mgmt_register_bootloader_info_hook(void)
+{
+	mgmt_callback_register(&cmd_bootloader_info_cb);
 }
