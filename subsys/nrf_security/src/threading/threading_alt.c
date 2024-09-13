a4736d8a05cba86fa0a6589fdffd7533910f2d79
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2024-09-13 11:01:14 +0200

    crypto: Adjusting threading to also work with no multithreading support
    fixup! crypto: Adding threading support for PSA core
    
    -This commit changes from threading/CMakeLists.txt to
     threading/threading.cmake to ensure that include can be used
     instead of add_subdirectory. This is necessary to keep the
     variables used to pass to build the mbedcrypto lib working inside
     and outside
    -Added support for the faux no threading suppport mode by reusing
     the nrf_security_mutexes.h API
    -Adding init/free to nrf_security_mutexes.h APIs
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- subsys/nrf_security/src/threading/threading_alt.c
+++ subsys/nrf_security/src/threading/threading_alt.c
@@ -10,11 +10,7 @@
 #include <stddef.h>
 #include <string.h>
 
-#include <zephyr/kernel.h>
-
-BUILD_ASSERT(IS_ENABLED(CONFIG_MULTITHREADING),
-	"This file is intended for multi-threading, but single-threading is enabled. "
-	"Please check your config build configuration!");
+#include <nrf_security_mutexes.h>
 
 #if !defined(MBEDTLS_CONFIG_FILE)
 #include "mbedtls/config.h"
@@ -22,52 +18,26 @@ BUILD_ASSERT(IS_ENABLED(CONFIG_MULTITHREADING),
 #include MBEDTLS_CONFIG_FILE
 #endif
 
-K_MUTEX_DEFINE(mbedtls_threading_key_slot_mutex);
-K_MUTEX_DEFINE(mbedtls_threading_psa_globaldata_mutex);
-K_MUTEX_DEFINE(mbedtls_threading_psa_rngdata_mutex);
+NRF_SECURITY_MUTEX_DEFINE(mbedtls_threading_key_slot_mutex);
+NRF_SECURITY_MUTEX_DEFINE(mbedtls_threading_psa_globaldata_mutex);
+NRF_SECURITY_MUTEX_DEFINE(mbedtls_threading_psa_rngdata_mutex);
 
-#if defined(MBEDTLS_THREADING_ALT) && !(defined(CONFIG_CC3XX_BACKEND) || defined(CONFIG_PSA_CRYPTO_DRIVER_CC3XX))
-
-void mbedtls_threading_set_alt( void (*mutex_init)( mbedtls_threading_mutex_t * ),
-                       void (*mutex_free)( mbedtls_threading_mutex_t * ),
-                       int (*mutex_lock)( mbedtls_threading_mutex_t * ),
-                       int (*mutex_unlock)( mbedtls_threading_mutex_t * ) )
+void mbedtls_mutex_init(mbedtls_threading_mutex_t *mutex)
 {
-    // Do nothing
+    nrf_security_mutex_init(mutex);
 }
 
-void mbedtls_threading_free_alt( void )
+void mbedtls_mutex_free(mbedtls_threading_mutex_t *mutex)
 {
-    // Do nothing
+    nrf_security_mutex_free(mutex);
 }
 
-#endif /* MBEDTLS_THREADING_ALT */
-
-#if defined(MBEDTLS_THREADING_C)
-
-void mutex_init(mbedtls_threading_mutex_t *mutex)
+int mbedtls_mutex_lock(mbedtls_threading_mutex_t *mutex)
 {
-    (void)k_mutex_init((struct k_mutex*)mutex);
+    return nrf_security_mutex_lock(mutex);
 }
 
-void mutex_free(mbedtls_threading_mutex_t *mutex)
+int mbedtls_mutex_unlock(mbedtls_threading_mutex_t *mutex)
 {
-    // Do nothing
+    return nrf_security_mutex_unlock(mutex);
 }
-
-int mutex_lock(mbedtls_threading_mutex_t *mutex)
-{
-    return k_mutex_lock((struct k_mutex*)mutex, K_FOREVER);
-}
-
-int mutex_unlock(mbedtls_threading_mutex_t *mutex)
-{
-    return k_mutex_unlock((struct k_mutex*)mutex);
-}
-
-void (*mbedtls_mutex_init)(mbedtls_threading_mutex_t *mutex) = mutex_init;
-void (*mbedtls_mutex_free)(mbedtls_threading_mutex_t *mutex) = mutex_free;
-int (*mbedtls_mutex_lock)(mbedtls_threading_mutex_t *mutex) = mutex_lock;
-int (*mbedtls_mutex_unlock)(mbedtls_threading_mutex_t *mutex) = mutex_unlock;
-
-#endif /* MBEDTLS_THREADING_C */
