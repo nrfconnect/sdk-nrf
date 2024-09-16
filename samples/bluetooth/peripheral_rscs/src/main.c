9c829d13d69077388f6b3fac07e5b72bb05f2454
Piotr Pryga <piotr.pryga@nordicsemi.no>
2024-09-16 09:37:34 +0200

    Dirty changes to peripheral_rscs to debu PM hanging issue
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- samples/bluetooth/peripheral_rscs/src/main.c
+++ samples/bluetooth/peripheral_rscs/src/main.c
@@ -80,32 +80,42 @@ static void rsc_simulation(struct bt_rscs_measurement *measurement)
 
 int set_cumulative(uint32_t total_distance)
 {
+#if IS_ENABLED(CONFIG_SERIAL)
 	printk("Set total distance: %d\n", total_distance);
+#endif /* CONFIG_SERIAL */
 	measurement.total_distance = total_distance;
 	return 0;
 }
 
 int calibration(void)
 {
+#if IS_ENABLED(CONFIG_SERIAL)
 	printk("Start the calibration procedure\n");
+#endif /* CONFIG_SERIAL */
+
 	return 0;
 }
 
 void update_loc(uint8_t location)
 {
 	__ASSERT_NO_MSG(location < ARRAY_SIZE(sensor_location));
-
+#if IS_ENABLED(CONFIG_SERIAL)
 	printk("Update sensor location: %u - %s\n", location, sensor_location[location]);
+#endif /* CONFIG_SERIAL */
 }
 
 void evt_handler(enum bt_rscs_evt evt)
 {
 	switch (evt) {
 	case RSCS_EVT_MEAS_NOTIFY_ENABLE:
+#if IS_ENABLED(CONFIG_SERIAL)
 		printk("Measurement notify enable\n");
+#endif /* CONFIG_SERIAL */
 		break;
 	case RSCS_EVT_MEAS_NOTIFY_DISABLE:
+#if IS_ENABLED(CONFIG_SERIAL)
 		printk("Measurement notify disable\n");
+#endif /* CONFIG_SERIAL */
 	default:
 		break;
 	}
@@ -120,26 +130,32 @@ static const struct bt_rscs_cp_cb control_point_cb = {
 static void connected(struct bt_conn *conn, uint8_t err)
 {
 	if (err) {
+#if IS_ENABLED(CONFIG_SERIAL)
 		printk("Connection failed, err 0x%02x %s\n", err, bt_hci_err_to_str(err));
+#endif /* CONFIG_SERIAL */
 	} else {
+#if IS_ENABLED(CONFIG_SERIAL)
 		printk("Connected\n");
+#endif /* CONFIG_SERIAL */
 	}
 
 	current_conn = bt_conn_ref(conn);
 
-	dk_set_led_on(CON_STATUS_LED);
+	//dk_set_led_on(CON_STATUS_LED);
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
+#if IS_ENABLED(CONFIG_SERIAL)
 	printk("Disconnected, reason 0x%02x %s\n", reason, bt_hci_err_to_str(reason));
+#endif /* CONFIG_SERIAL */
 
 	if (current_conn) {
 		bt_conn_unref(current_conn);
 		current_conn = NULL;
 	}
 
-	dk_set_led_off(CON_STATUS_LED);
+	//dk_set_led_off(CON_STATUS_LED);
 }
 
 #ifdef CONFIG_BT_RSCS_SECURITY_ENABLED
@@ -150,10 +166,14 @@ static void security_changed(struct bt_conn *conn, bt_security_t level, enum bt_
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
 	if (!err) {
+#if IS_ENABLED(CONFIG_SERIAL)
 		printk("Security changed: %s level %u\n", addr, level);
+#endif /* CONFIG_SERIAL */
 	} else {
+#if IS_ENABLED(CONFIG_SERIAL)
 		printk("Security failed: %s level %u err %d %s\n", addr, level, err,
 		       bt_security_err_to_str(err));
+#endif /* CONFIG_SERIAL */
 	}
 }
 #endif
@@ -173,7 +193,9 @@ static void auth_passkey_display(struct bt_conn *conn, unsigned int passkey)
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
+#if IS_ENABLED(CONFIG_SERIAL)
 	printk("Passkey for %s: %06u\n", addr, passkey);
+#endif /* CONFIG_SERIAL */
 }
 
 static void auth_cancel(struct bt_conn *conn)
@@ -182,7 +204,9 @@ static void auth_cancel(struct bt_conn *conn)
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
+#if IS_ENABLED(CONFIG_SERIAL)
 	printk("Pairing cancelled: %s\n", addr);
+#endif /* CONFIG_SERIAL */
 }
 
 static void pairing_complete(struct bt_conn *conn, bool bonded)
@@ -191,7 +215,9 @@ static void pairing_complete(struct bt_conn *conn, bool bonded)
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
+#if IS_ENABLED(CONFIG_SERIAL)
 	printk("Pairing completed: %s, bonded: %d\n", addr, bonded);
+#endif /* CONFIG_SERIAL */
 }
 
 static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
@@ -200,8 +226,10 @@ static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
+#if IS_ENABLED(CONFIG_SERIAL)
 	printk("Pairing failed conn: %s, reason %d %s\n", addr, reason,
 	       bt_security_err_to_str(reason));
+#endif /* CONFIG_SERIAL */
 }
 
 static struct bt_conn_auth_cb conn_auth_callbacks = {
@@ -225,82 +253,120 @@ static const struct bt_data ad[] = {
 	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1)
 };
 
+#define BT_LE_ADV_CONN BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE, \
+				       BT_GAP_PER_ADV_SLOW_INT_MIN*2, \
+				       BT_GAP_PER_ADV_SLOW_INT_MAX*2, NULL)
+
+#pragma GCC push_options
+#pragma GCC optimize("-O0")
+//bool wait = false;
+
 int main(void)
 {
 	int err;
 	uint32_t blink_status = 0;
 
+#if IS_ENABLED(CONFIG_SERIAL)
 	printk("Starting Running Speed and Cadence peripheral example\n");
+#endif /* CONFIG_SERIAL */
 
-	err = dk_leds_init();
-	if (err) {
-		printk("LEDs init failed (err %d)\n", err);
-		return 0;
-	}
+	//err = dk_leds_init();
+// 	if (err) {
+// #if IS_ENABLED(CONFIG_SERIAL)
+// 		printk("LEDs init failed (err %d)\n", err);
+// #endif /* CONFIG_SERIAL */
+// 		return 0;
+// 	}
 
-	if (IS_ENABLED(CONFIG_BT_RSCS_SECURITY_ENABLED)) {
-		err = bt_conn_auth_cb_register(&conn_auth_callbacks);
-		if (err) {
-			printk("Failed to register authorization callbacks.\n");
-			return 0;
-		}
+// 	if (IS_ENABLED(CONFIG_BT_RSCS_SECURITY_ENABLED)) {
+// 		err = bt_conn_auth_cb_register(&conn_auth_callbacks);
+// 		if (err) {
+// #if IS_ENABLED(CONFIG_SERIAL)
+// 			printk("Failed to register authorization callbacks.\n");
+// #endif /* CONFIG_SERIAL */
+// 			return 0;
+// 		}
 
-		err = bt_conn_auth_info_cb_register(&conn_auth_info_callbacks);
-		if (err) {
-			printk("Failed to register authorization info callbacks.\n");
-			return 0;
-		}
-	}
+// 		err = bt_conn_auth_info_cb_register(&conn_auth_info_callbacks);
+// 		if (err) {
+// #if IS_ENABLED(CONFIG_SERIAL)
+// 			printk("Failed to register authorization info callbacks.\n");
+// #endif /* CONFIG_SERIAL */
+// 			return 0;
+// 		}
+// 	}
+
+	//while(wait);
+	//k_sleep(K_FOREVER);
+	//k_msleep(10000);
+	printk("back\n");
 
 	err = bt_enable(NULL);
 	if (err) {
+#if IS_ENABLED(CONFIG_SERIAL)
 		printk("Bluetooth init failed (err %d)\n", err);
+#endif /* CONFIG_SERIAL */
 		return 0;
 	}
 
-	printk("Bluetooth initialized\n");
+// #if IS_ENABLED(CONFIG_SERIAL)
+// 	printk("Bluetooth initialized\n");
+// #endif /* CONFIG_SERIAL */
 
-	if (IS_ENABLED(CONFIG_SETTINGS)) {
-		settings_load();
-	}
+// 	if (IS_ENABLED(CONFIG_SETTINGS)) {
+// 		settings_load();
+// 	}
 
-	struct bt_rscs_init_params rscs_init = {0};
+// 	struct bt_rscs_init_params rscs_init = {0};
 
-	rscs_init.features.inst_stride_len = 1;
-	rscs_init.features.multi_sensor_loc = 1;
-	rscs_init.features.sensor_calib_proc = 1;
-	rscs_init.features.total_distance = 1;
-	rscs_init.features.walking_or_running = 1;
+// 	rscs_init.features.inst_stride_len = 1;
+// 	rscs_init.features.multi_sensor_loc = 1;
+// 	rscs_init.features.sensor_calib_proc = 1;
+// 	rscs_init.features.total_distance = 1;
+// 	rscs_init.features.walking_or_running = 1;
 
-	rscs_init.supported_locations.other = 1;
-	rscs_init.supported_locations.top_of_shoe = 1;
-	rscs_init.supported_locations.in_shoe = 1;
-	rscs_init.supported_locations.hip = 1;
+// 	rscs_init.supported_locations.other = 1;
+// 	rscs_init.supported_locations.top_of_shoe = 1;
+// 	rscs_init.supported_locations.in_shoe = 1;
+// 	rscs_init.supported_locations.hip = 1;
 
-	rscs_init.location = RSC_LOC_HIP;
+// 	rscs_init.location = RSC_LOC_HIP;
 
-	rscs_init.cp_cb = &control_point_cb;
-	rscs_init.evt_handler = evt_handler;
+// 	rscs_init.cp_cb = &control_point_cb;
+// 	rscs_init.evt_handler = evt_handler;
 
-	err = bt_rscs_init(&rscs_init);
-	if (err) {
-		printk("Failed to init RSCS (err %d)\n", err);
-		return 0;
-	}
+// 	err = bt_rscs_init(&rscs_init);
+// 	if (err) {
+// #if IS_ENABLED(CONFIG_SERIAL)
+// 		printk("Failed to init RSCS (err %d)\n", err);
+// #endif /* CONFIG_SERIAL */
+// 		return 0;
+// 	}
+	printk("start adv\n");
 
 	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), NULL, 0);
 	if (err) {
+		printk("start adv failed\n");
+
+#if IS_ENABLED(CONFIG_SERIAL)
 		printk("Advertising failed to start (err %d)\n", err);
+#endif /* CONFIG_SERIAL */
 		return 0;
 	}
 
+#if IS_ENABLED(CONFIG_SERIAL)
 	printk("Advertising successfully started\n");
+#endif /* CONFIG_SERIAL */
 
 	for (;;) {
-		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
-		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+		printk("Loop\n");
+		k_msleep(2000);
 
-		rsc_simulation(&measurement);
-		bt_rscs_measurement_send(current_conn, &measurement);
+	// 	//dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
+	// 	k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+
+	// 	rsc_simulation(&measurement);
+	// 	bt_rscs_measurement_send(current_conn, &measurement);
 	}
 }
+#pragma GCC pop_options
