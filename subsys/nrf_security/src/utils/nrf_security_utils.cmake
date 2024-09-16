5b2d60c72afcadb65d734d6a60e19e5acaca5135
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2024-09-16 21:30:49 +0200

    crypto: Threading: Ensure linking against libkernel.a by path
    fixup! crypto: Adding threading support for PSA core
    
    -This resolves an issue with CRACEN builds where trying to get a proper
     link to kernel involves using kernel library by full path to avoid
     issues in build.
    -Make includes local for nrf_security_mutexes/events
    -Establish nrf_security_utils library to localize the issue and to
     try to find a better way to resolve this in the future
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- subsys/nrf_security/src/utils/nrf_security_utils.cmake
+++ subsys/nrf_security/src/utils/nrf_security_utils.cmake
@@ -4,17 +4,23 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
-append_with_prefix(src_crypto_base ${CMAKE_CURRENT_LIST_DIR}
-  nrf_security_mutexes.c
-  nrf_security_events.c
+add_library(nrf_security_utils STATIC
+  ${CMAKE_CURRENT_LIST_DIR}/nrf_security_mutexes.c
+  ${CMAKE_CURRENT_LIST_DIR}/nrf_security_events.c
 )
 
-target_include_directories(psa_crypto_library_config
+target_include_directories(nrf_security_utils
   INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
-target_include_directories(psa_crypto_config
-  INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}
+# This special linking is done to give access to the zephyr kernel library
+# which possibly isn't --whole-archived in the build. Trying to link to the 
+# kernel library directly will give cyclic dependency. The only way to avoid
+# it seems to be to link with a full path instead.
+target_link_libraries(nrf_security_utils
+  PRIVATE
+    ${CMAKE_BINARY_DIR}/zephyr/kernel/libkernel.a
 )
+
+nrf_security_add_zephyr_options_library(nrf_security_utils)
