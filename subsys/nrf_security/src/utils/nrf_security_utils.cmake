909835f6a3c84040f8bf7e95e8b6352559022f7a
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2024-09-18 23:01:20 +0200

    crypto: Handling threading support a bit more accurately
    fixup! crypto: Adding threading support for PSA core
    
    -This commit ensures the appropriate linking from nrf-security_utils
     to libraries in tf-M related to low-level OS integration
     like the addition of __assert.h and the debvug log used whe
     asserts happen
    -This commit also moves nrf_security_utils into private scope
     for mbedcrypto-base and cracen PSA crypto library
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- subsys/nrf_security/src/utils/nrf_security_utils.cmake
+++ subsys/nrf_security/src/utils/nrf_security_utils.cmake
@@ -14,13 +14,24 @@ target_include_directories(nrf_security_utils
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
-# This special linking is done to give access to the zephyr kernel library
-# which possibly isn't --whole-archived in the build. Trying to link to the 
-# kernel library directly will give cyclic dependency. The only way to avoid
-# it seems to be to link with a full path instead.
-target_link_libraries(nrf_security_utils
-  PRIVATE
-    ${CMAKE_BINARY_DIR}/zephyr/kernel/libkernel.a
-)
+if(BUILD_INSIDE_TFM)
+  # This gives access to cmsis, nrfx and mdk and the tfm_sp_log used by 
+  # __assert.h
+  target_link_libraries(nrf_security_utils
+    PUBLIC
+      platform_s
+      tfm_sp_log
+      tfm_psa_rot_partition_crypto
+  )
+else()
+  # This special linking is done to give access to the zephyr kernel library
+  # which possibly isn't --whole-archived in the build. Trying to link to the 
+  # kernel library directly will give cyclic dependency. The only way to avoid
+  # it seems to be to link with a full path instead.
+  target_link_libraries(nrf_security_utils
+    PRIVATE
+      ${CMAKE_BINARY_DIR}/zephyr/kernel/libkernel.a
+  )
+endif()
 
 nrf_security_add_zephyr_options_library(nrf_security_utils)
