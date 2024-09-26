9d5026b1a98f1a4826c6ed8c525aee625e4a983b
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2024-09-26 17:08:31 +0200

    crypto: Defer mutex operation in pre-kernel
    fixup! crypto: Adding threading support for PSA core
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- subsys/nrf_security/src/threading/threading_alt.c
+++ subsys/nrf_security/src/threading/threading_alt.c
@@ -9,6 +9,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
 
 #include "nrf_security_mutexes.h"
 
@@ -22,27 +24,49 @@ NRF_SECURITY_MUTEX_DEFINE(mbedtls_threading_key_slot_mutex);
 NRF_SECURITY_MUTEX_DEFINE(mbedtls_threading_psa_globaldata_mutex);
 NRF_SECURITY_MUTEX_DEFINE(mbedtls_threading_psa_rngdata_mutex);
 
-static void mbedtls_mutex_init_fn(mbedtls_threading_mutex_t *mutex)
+static void mbedtls_mutex_init_fn(mbedtls_threading_mutex_t * mutex)
 {
-    nrf_security_mutex_init(mutex);
+    if(!k_is_pre_kernel() && !k_is_in_isr()) {
+        nrf_security_mutex_init(mutex);
+    }
 }
 
-static void mbedtls_mutex_free_fn(mbedtls_threading_mutex_t *mutex)
+static void mbedtls_mutex_free_fn(mbedtls_threading_mutex_t * mutex)
 {
-    nrf_security_mutex_free(mutex);
+    if(!k_is_pre_kernel() && !k_is_in_isr()) {
+        nrf_security_mutex_free(mutex);
+    }
 }
 
-static int mbedtls_mutex_lock_fn(mbedtls_threading_mutex_t *mutex)
+static int mbedtls_mutex_lock_fn(mbedtls_threading_mutex_t * mutex)
 {
-    return nrf_security_mutex_lock(mutex);
+    if(!k_is_pre_kernel() && !k_is_in_isr()) {
+        return nrf_security_mutex_lock(mutex);
+    } else {
+        return 0;
+    }
 }
 
-static int mbedtls_mutex_unlock_fn(mbedtls_threading_mutex_t *mutex)
+static int mbedtls_mutex_unlock_fn(mbedtls_threading_mutex_t * mutex)
 {
-    return nrf_security_mutex_unlock(mutex);
+    if(!k_is_pre_kernel() && !k_is_in_isr()) {
+        return nrf_security_mutex_unlock(mutex);
+    } else {
+        return 0;
+    }
 }
 
 void (*mbedtls_mutex_init)(mbedtls_threading_mutex_t *mutex) = mbedtls_mutex_init_fn;
 void (*mbedtls_mutex_free)(mbedtls_threading_mutex_t *mutex) = mbedtls_mutex_free_fn;
 int (*mbedtls_mutex_lock)(mbedtls_threading_mutex_t *mutex) = mbedtls_mutex_lock_fn;
 int (*mbedtls_mutex_unlock)(mbedtls_threading_mutex_t *mutex) = mbedtls_mutex_unlock_fn;
+
+static int post_kernel_init(void)
+{
+    mbedtls_mutex_init(&mbedtls_threading_key_slot_mutex);
+    mbedtls_mutex_init(&mbedtls_threading_psa_globaldata_mutex);
+    mbedtls_mutex_init(&mbedtls_threading_psa_rngdata_mutex);
+    return 0;
+}
+
+SYS_INIT(post_kernel_init, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
