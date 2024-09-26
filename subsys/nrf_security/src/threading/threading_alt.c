501e1186eba98072f4a4f0acd3de53ec8ceb63e5
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2024-09-26 14:07:00 +0200

    crypto: Fix aliasing issues of mbedtl_mutex_xxxx APIs
    fixup! crypto: Adding threading support for PSA core
    
    -Needs to be pointer to function not functions
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- subsys/nrf_security/src/threading/threading_alt.c
+++ subsys/nrf_security/src/threading/threading_alt.c
@@ -22,22 +22,27 @@ NRF_SECURITY_MUTEX_DEFINE(mbedtls_threading_key_slot_mutex);
 NRF_SECURITY_MUTEX_DEFINE(mbedtls_threading_psa_globaldata_mutex);
 NRF_SECURITY_MUTEX_DEFINE(mbedtls_threading_psa_rngdata_mutex);
 
-void mbedtls_mutex_init(mbedtls_threading_mutex_t *mutex)
+static void mbedtls_mutex_init_fn(mbedtls_threading_mutex_t *mutex)
 {
     nrf_security_mutex_init(mutex);
 }
 
-void mbedtls_mutex_free(mbedtls_threading_mutex_t *mutex)
+static void mbedtls_mutex_free_fn(mbedtls_threading_mutex_t *mutex)
 {
     nrf_security_mutex_free(mutex);
 }
 
-int mbedtls_mutex_lock(mbedtls_threading_mutex_t *mutex)
+static int mbedtls_mutex_lock_fn(mbedtls_threading_mutex_t *mutex)
 {
     return nrf_security_mutex_lock(mutex);
 }
 
-int mbedtls_mutex_unlock(mbedtls_threading_mutex_t *mutex)
+static int mbedtls_mutex_unlock_fn(mbedtls_threading_mutex_t *mutex)
 {
     return nrf_security_mutex_unlock(mutex);
 }
+
+void (*mbedtls_mutex_init)(mbedtls_threading_mutex_t *mutex) = mbedtls_mutex_init_fn;
+void (*mbedtls_mutex_free)(mbedtls_threading_mutex_t *mutex) = mbedtls_mutex_free_fn;
+int (*mbedtls_mutex_lock)(mbedtls_threading_mutex_t *mutex) = mbedtls_mutex_lock_fn;
+int (*mbedtls_mutex_unlock)(mbedtls_threading_mutex_t *mutex) = mbedtls_mutex_unlock_fn;
