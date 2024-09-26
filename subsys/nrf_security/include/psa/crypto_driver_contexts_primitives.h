cf8382cbfe591053390a28508764a8f5c7ecbbf7
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2024-09-26 18:52:26 +0200

    nrf_security: Use the renamed header file
    The common header file is now renamed with the module prefix.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- subsys/nrf_security/include/psa/crypto_driver_contexts_primitives.h
+++ subsys/nrf_security/include/psa/crypto_driver_contexts_primitives.h
@@ -24,7 +24,7 @@
 #ifndef PSA_CRYPTO_DRIVER_CONTEXTS_PRIMITIVES_H
 #define PSA_CRYPTO_DRIVER_CONTEXTS_PRIMITIVES_H
 
-#include "common.h"
+#include "oberon_psa_common.h"
 
 #include "psa/crypto_driver_common.h"
 
