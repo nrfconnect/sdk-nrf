7c1027a1dc45c325e7a324b8a295cebd7ccfa274
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2024-09-26 17:08:31 +0200

    crypto: Defer mutex operation in pre-kernel
    fixup! crypto: Adding threading support for PSA core
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- subsys/nrf_security/src/utils/nrf_security_mutexes.c
+++ subsys/nrf_security/src/utils/nrf_security_mutexes.c
@@ -17,45 +17,55 @@
 
 #if defined(CONFIG_MULTITHREADING) && !defined(__NRF_TFM__)
 
-void nrf_security_mutex_init(nrf_security_mutex_t mutex)
+void nrf_security_mutex_init(mbedtls_threading_mutex_t * mutex)
 {
-	(void)k_mutex_init(mutex);
+	if((mutex->flags & NRF_SECURITY_MUTEX_FLAGS_INITIALIZED) != 0) {
+		k_mutex_init(&mutex->mutex);
+	}
 }
 
-void nrf_security_mutex_free(nrf_security_mutex_t mutex)
+void nrf_security_mutex_free(mbedtls_threading_mutex_t * mutex)
 {
 	(void)mutex;
 }
 
-int nrf_security_mutex_lock(nrf_security_mutex_t mutex)
+int nrf_security_mutex_lock(mbedtls_threading_mutex_t * mutex)
 {
-	return k_mutex_lock(mutex, K_FOREVER);
+	if((mutex->flags & NRF_SECURITY_MUTEX_FLAGS_INITIALIZED) != 0) {
+		return k_mutex_lock(&mutex->mutex, K_FOREVER);
+	} else {
+		return -EINVAL;
+	}
 }
 
-int nrf_security_mutex_unlock(nrf_security_mutex_t mutex)
+int nrf_security_mutex_unlock(mbedtls_threading_mutex_t * mutex)
 {
-	return k_mutex_unlock(mutex);
+	if((mutex->flags & NRF_SECURITY_MUTEX_FLAGS_INITIALIZED) != 0) {
+		return k_mutex_unlock(&mutex->mutex);
+	} else {
+		return -EINVAL;
+	}
 }
 
 #else
 
-void nrf_security_mutex_init(nrf_security_mutex_t mutex)
+void nrf_security_mutex_init(mbedtls_threading_mutex_t * mutex)
 {
 	(void)mutex;
 }
 
-void nrf_security_mutex_free(nrf_security_mutex_t mutex)
+void nrf_security_mutex_free(mbedtls_threading_mutex_t * mutex)
 {
 	(void)mutex;
 }
 
-int nrf_security_mutex_lock(nrf_security_mutex_t mutex)
+int nrf_security_mutex_lock(mbedtls_threading_mutex_t * mutex)
 {
 	(void)mutex;
 	return 0;
 }
 
-int nrf_security_mutex_unlock(nrf_security_mutex_t mutex)
+int nrf_security_mutex_unlock(mbedtls_threading_mutex_t * mutex)
 {
 	(void)mutex;
 	return 0;
