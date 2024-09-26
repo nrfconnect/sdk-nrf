47ef653854df9ed7b5c015864580bc2e9c349be9
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2024-09-26 16:25:43 +0300

    nrf54l15dk: add TF-M (/ns) support
    Bring in changes needed in Zephyr and TF-M, and make needed
    adjustments.
    
    Support for TF-M in `nrf54l15pdk` is removed at the same time.
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

--- modules/trusted-firmware-m/tfm_boards/board/device_cfg.h
+++ modules/trusted-firmware-m/tfm_boards/board/device_cfg.h
@@ -13,7 +13,8 @@
 #undef ARRAY_SIZE
 #include <zephyr/devicetree.h>
 
-#if !defined(NRF54L15_ENGA_XXAA)
+#if !defined(NRF54L15_XXAA)
+
 #if defined(CONFIG_TFM_SECURE_UART0) || DOMAIN_NS == 1U
 #define TFM_UART uart0
 #endif /* defined(CONFIG_TFM_SECURE_UART0) || DOMAIN_NS == 1U */
@@ -22,7 +23,8 @@
 #define TFM_UART uart1
 #endif /* defined(CONFIG_TFM_SECURE_UART1) */
 
-#else /* NRF54L15_ENGA_XXAA */
+#else /* NRF54L15_XXAA */
+
 #if defined(CONFIG_TFM_SECURE_UART00) && DOMAIN_NS != 1U
 #define TFM_UART uart00
 #endif /* defined(CONFIG_TFM_SECURE_UART00) */
@@ -42,7 +44,8 @@
 #if defined(CONFIG_TFM_SECURE_UART30) && DOMAIN_NS != 1U
 #define TFM_UART uart30
 #endif /* defined(CONFIG_TFM_SECURE_UART30) */
-#endif /* NRF54L15_ENGA_XXAA */
+
+#endif /* NRF54L15_XXAA */
 
 #define DEFAULT_UART_BAUDRATE DT_PROP_OR(DT_NODELABEL(TFM_UART), current_speed, 115200)
 
