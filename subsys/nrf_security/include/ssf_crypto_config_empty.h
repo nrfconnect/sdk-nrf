5e03ee91f670a22ef6a09ef03b88cfb43d964701
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2024-09-27 13:09:22 +0200

    nrf_security: Make PSA SSF client independent
    Add configuration to allow enabling the SSF PSA client
    when nrf_security is not enabled.
    This is particularly useful for the applications that only
    want to use the PSA rng and no other crypto. Enabling
    nrf_security in these applications will result to an
    increased application footprint and configuration complexity
    without any reason.
    
    This configuration provides the PSA implementation
    from the secure domain through the SSF client and
    it has no configurability yet. So there is no need
    to enforce NRF_SECURITY with this configuration.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

---
+++ subsys/nrf_security/include/ssf_crypto_config_empty.h
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/* This is intentionally empty since the SSF doesn't support any configuration yet. */
