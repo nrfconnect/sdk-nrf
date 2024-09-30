d9d7f8a459fa3742f73f50e70be5f147ca3498f4
Marek Pieta <Marek.Pieta@nordicsemi.no>
2024-09-30 15:32:36 +0200

    scripts: hid_configurator: Add nRF54L15 DK support
    Change adds nRF54L15 DK support.
    
    Jira: NCSDK-29114
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- scripts/hid_configurator/NrfHidManager.py
+++ scripts/hid_configurator/NrfHidManager.py
@@ -9,7 +9,7 @@ NORDIC_VID = 0x1915
 
 class NrfHidManager():
     TYPE2BOARDLIST = {
-        'gaming_mouse' : ['nrf52840gmouse', 'nrf52840dk', 'nrf54l15pdk', 'nrf54h20dk'],
+        'gaming_mouse' : ['nrf52840gmouse', 'nrf52840dk', 'nrf54l15dk', 'nrf54l15pdk', 'nrf54h20dk'],
         'dongle' : ['nrf52840dongle', 'nrf52833dongle', 'nrf52820dongle', 'nrf5340dk'],
         'keyboard' : ['nrf52kbd'],
         'desktop_mouse' : ['nrf52dmouse', 'nrf52810dmouse'],
