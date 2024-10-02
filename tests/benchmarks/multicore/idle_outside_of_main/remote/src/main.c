2d070398b291b4dce8d22514e4d743d9be5b5e16
Piotr Kosycarz <piotr.kosycarz@nordicsemi.no>
2024-10-02 11:32:37 +0200

    tests: benchmarks: multicore: add idle_outside_of_main
    Check sleeping outside of the main.
    
    Signed-off-by: Piotr Kosycarz <piotr.kosycarz@nordicsemi.no>

---
+++ tests/benchmarks/multicore/idle_outside_of_main/remote/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+
+	k_msleep(2000);
+
+	return 0;
+}
