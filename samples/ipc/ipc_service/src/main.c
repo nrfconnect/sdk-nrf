600332233dffd26f35f6841878b996dfb4372a32
Piotr Kosycarz <piotr.kosycarz@nordicsemi.no>
2024-10-07 15:01:28 +0200

    samples: ipc: ipc_service: show full board and target name
    CONFIG_BOARD does not allow to distiguish between cores.
    
    Signed-off-by: Piotr Kosycarz <piotr.kosycarz@nordicsemi.no>

--- samples/ipc/ipc_service/src/main.c
+++ samples/ipc/ipc_service/src/main.c
@@ -101,7 +101,7 @@ int main(void)
 	p_payload->size = CONFIG_APP_IPC_SERVICE_MESSAGE_LEN;
 	p_payload->cnt = 0;
 
-	printk("IPC-service %s demo started\n", CONFIG_BOARD);
+	printk("IPC-service %s demo started\n", CONFIG_BOARD_TARGET);
 
 	ipc0_instance = DEVICE_DT_GET(DT_NODELABEL(ipc0));
 
