nrfconnect/sdk-nrf

commit 5611086056c1112f1a35ebc6d8b55c99a9e7560a

lib: lte_link_control: deprecate reduced mobility
Deprecate reduced mobility API. This functionality is
trivial to implement and doesn't need a whole API within the
Link Controller.

Signed-off-by: Mirko Covizzi <mirko.covizzi@nordicsemi.no>

samples/cellular/modem_shell/src/link/link_shell.c
@@ -1998,7 +1998,7 @@ static int link_shell_redmob(const struct shell *shell, size_t argc, char **argv
 {
 	int ret;
 	enum link_shell_operation operation = LINK_OPERATION_NONE;
-	enum lte_lc_reduced_mobility_mode redmob_mode = LINK_REDMOB_NONE;
+	enum link_reduced_mobility_mode redmob_mode = LINK_REDUCED_MOBILITY_NONE;
 	char snum[10];
 
 	optreset = 1;
@@ -2017,10 +2017,10 @@ static int link_shell_redmob(const struct shell *shell, size_t argc, char **argv
 			break;
 
 		case LINK_SHELL_OPT_REDMOB_DEFAULT:
-			redmob_mode = LTE_LC_REDUCED_MOBILITY_DEFAULT;
+			redmob_mode = LINK_REDUCED_MOBILITY_DEFAULT;
 			break;
 		case LINK_SHELL_OPT_REDMOB_NORDIC:
-			redmob_mode = LTE_LC_REDUCED_MOBILITY_NORDIC;
+			redmob_mode = LINK_REDUCED_MOBILITY_NORDIC;
 			break;
 
 		case 'h':
@@ -2038,21 +2038,21 @@ static int link_shell_redmob(const struct shell *shell, size_t argc, char **argv
 	}
 
 	if (operation == LINK_OPERATION_DISABLE) {
-		redmob_mode = LTE_LC_REDUCED_MOBILITY_DISABLED;
+		redmob_mode = LINK_REDUCED_MOBILITY_DISABLED;
 	}
 	if (operation == LINK_OPERATION_READ) {
-		enum lte_lc_reduced_mobility_mode mode;
+		enum link_reduced_mobility_mode mode;
 
-		ret = lte_lc_reduced_mobility_get(&mode);
-		if (ret) {
+		ret = nrf_modem_at_scanf("AT%REDMOB?", "%%REDMOB: %hu", &mode);
+		if (ret != 1) {
 			mosh_error("Cannot get reduced mobility mode: %d", ret);
 		} else {
 			mosh_print(
 				"Reduced mobility mode read successfully: %s",
 				link_shell_redmob_mode_to_string(mode, snum));
 		}
-	} else if (redmob_mode != LINK_REDMOB_NONE) {
-		ret = lte_lc_reduced_mobility_set(redmob_mode);
+	} else if (redmob_mode != LINK_REDUCED_MOBILITY_NONE) {
+		ret = nrf_modem_at_printf("AT%%REDMOB=%d", redmob_mode);
 		if (ret) {
 			mosh_error("Cannot set reduced mobility mode: %d", ret);
 		} else {

include/modem/lte_lc.h
@@ -652,7 +652,10 @@ enum lte_lc_ce_level {
 	LTE_LC_CE_LEVEL_UNKNOWN	= UINT8_MAX,
 };
 
-/** Reduced mobility mode. */
+/** Reduced mobility mode.
+ *
+ *  @deprecated since v2.8.0.
+ */
 enum lte_lc_reduced_mobility_mode {
 	/** Functionality according to the 3GPP relaxed monitoring feature. */
 	LTE_LC_REDUCED_MOBILITY_DEFAULT = 0,
@@ -1767,6 +1770,8 @@ int lte_lc_periodic_search_request(void);
  * @retval 0 if a mode was found and written to the provided pointer.
  * @retval -EINVAL if input parameter was @c NULL.
  * @retval -EFAULT if an AT command failed.
+ *
+ * @deprecated since v2.8.0.
  */
 int lte_lc_reduced_mobility_get(enum lte_lc_reduced_mobility_mode *mode);
 
@@ -1779,6 +1784,8 @@ int lte_lc_reduced_mobility_get(enum lte_lc_reduced_mobility_mode *mode);
  *
  * @retval 0 if the new reduced mobility mode was accepted by the modem.
  * @retval -EFAULT if an AT command failed.
+ *
+ * @deprecated since v2.8.0.
  */
 int lte_lc_reduced_mobility_set(enum lte_lc_reduced_mobility_mode mode);
 

samples/cellular/modem_shell/src/link/link.h
@@ -18,7 +18,6 @@ enum link_ncellmeas_modes {
 
 #define LINK_FUNMODE_NONE 99
 #define LINK_SYSMODE_NONE 99
-#define LINK_REDMOB_NONE 99
 
 /** Type of factory reset to perform. */
 enum link_factory_reset_type {
@@ -30,6 +29,25 @@ enum link_factory_reset_type {
 
 	/** Invalid type. */
 	LINK_FACTORY_RESET_INVALID = 99
+};
+
+/** Reduced mobility mode. */
+enum link_reduced_mobility_mode {
+	/** Functionality according to the 3GPP relaxed monitoring feature. */
+	LINK_REDUCED_MOBILITY_DEFAULT = 0,
+
+	/** Enable Nordic-proprietary reduced mobility feature. */
+	LINK_REDUCED_MOBILITY_NORDIC = 1,
+
+	/**
+	 * Full measurements for best possible mobility.
+	 *
+	 * Disable the 3GPP relaxed monitoring and Nordic-proprietary reduced mobility features.
+	 */
+	LINK_REDUCED_MOBILITY_DISABLED = 2,
+
+	/** Invalid type. */
+	LINK_REDUCED_MOBILITY_NONE = 99
 };
 
 void link_init(void);

commit 5acb7340125434de2a5ea3d74a06f7414077feac

samples: cellular: modem_shell: redmob: Don't pass enum to scanf
When passing enum type into scanf, there will be a compilation warning:

nrf/samples/cellular/modem_shell/src/link/link_shell.c:2046:69:
warning: format '%hu' expects argument of type 'short unsigned int *',
but argument 3 has type 'enum link_reduced_mobility_mode *' [-Wformat=]
2046 | ret = nrf_modem_at_scanf("AT%REDMOB?", "%%REDMOB: %hu", &mode);

Regression from PR #17579

Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

samples/cellular/modem_shell/src/link/link_shell.c
@@ -2042,11 +2042,13 @@ static int link_shell_redmob(const struct shell *shell, size_t argc, char **argv
 	}
 	if (operation == LINK_OPERATION_READ) {
 		enum link_reduced_mobility_mode mode;
+		uint16_t mode_tmp;
 
-		ret = nrf_modem_at_scanf("AT%REDMOB?", "%%REDMOB: %hu", &mode);
+		ret = nrf_modem_at_scanf("AT%REDMOB?", "%%REDMOB: %hu", &mode_tmp);
 		if (ret != 1) {
 			mosh_error("Cannot get reduced mobility mode: %d", ret);
 		} else {
+			mode = mode_tmp;
 			mosh_print(
 				"Reduced mobility mode read successfully: %s",
 				link_shell_redmob_mode_to_string(mode, snum));