88380cea90a6f4481ee20b258afe1290f632473f
Adam Szczygieł <adam.szczygiel@nordicsemi.no>
2024-10-09 14:48:47 +0200

    suit: Rework SDFW and SDFW Recovery sinks
    New implementation of SDFW and SDFW Recovery sinks.
    Supports fixed installation order (SDFW, then SDFW Recovery).
    Treats SDFW Recovery installation as optional.
    Envelope is saved when SDFW slot is successfully installed.
    
    Ref: NCSDK-29561
    
    Signed-off-by: Adam Szczygieł <adam.szczygiel@nordicsemi.no>

--- subsys/suit/platform/src/suit_plat_error_convert.c
+++ subsys/suit/platform/src/suit_plat_error_convert.c
@@ -24,6 +24,9 @@ int suit_plat_err_to_processor_err_convert(suit_plat_err_t plat_err)
 	case SUIT_PLAT_ERR_CBOR_DECODING:
 		proc_err = SUIT_ERR_DECODING;
 		break;
+	case SUIT_PLAT_ERR_SDRFW_FAILURE:
+		proc_err = SUIT_FAIL_CONDITION;
+		break;
 	/* To be extended */
 	default:
 		/* Return SUIT_ERR_CRASH */
--- subsys/suit/utils/src/suit_plat_mem_util.c
+++ subsys/suit/utils/src/suit_plat_mem_util.c
@@ -8,6 +8,9 @@
 #ifdef CONFIG_FLASH_SIMULATOR
 #include <zephyr/drivers/flash/flash_simulator.h>
 #endif /* CONFIG_FLASH_SIMULATOR */
+#ifdef CONFIG_SDFW_IS_UROT
+#include <hal/nrf_mramc.h>
+#endif /* CONFIG_SDFW_IS_UROT */
 
 uint8_t *suit_plat_mem_ptr_get(uintptr_t address)
 {
@@ -71,9 +74,9 @@ uintptr_t suit_plat_mem_nvm_offset_get(uint8_t *ptr)
 	uintptr_t address = suit_plat_mem_address_get(ptr);
 
 #if (DT_NODE_EXISTS(DT_NODELABEL(mram1x)))
-	address = (((address)&0xEFFFFFFFUL) - (DT_REG_ADDR(DT_NODELABEL(mram1x)) & 0xEFFFFFFFUL));
+	address = (((address) & 0xEFFFFFFFUL) - (DT_REG_ADDR(DT_NODELABEL(mram1x)) & 0xEFFFFFFFUL));
 #elif (DT_NODE_EXISTS(DT_NODELABEL(mram10)))
-	address = (((address)&0xEFFFFFFFUL) - (DT_REG_ADDR(DT_NODELABEL(mram10)) & 0xEFFFFFFFUL));
+	address = (((address) & 0xEFFFFFFFUL) - (DT_REG_ADDR(DT_NODELABEL(mram10)) & 0xEFFFFFFFUL));
 #endif
 
 	return address;
@@ -84,12 +87,61 @@ uint8_t *suit_plat_mem_nvm_ptr_get(uintptr_t offset)
 	uintptr_t address;
 
 #if (DT_NODE_EXISTS(DT_NODELABEL(mram1x)))
-	address = (((offset)&0xEFFFFFFFUL) + (DT_REG_ADDR(DT_NODELABEL(mram1x)) & 0xEFFFFFFFUL));
+	address = (((offset) & 0xEFFFFFFFUL) + (DT_REG_ADDR(DT_NODELABEL(mram1x)) & 0xEFFFFFFFUL));
 #elif (DT_NODE_EXISTS(DT_NODELABEL(mram10)))
-	address = (((offset)&0xEFFFFFFFUL) + (DT_REG_ADDR(DT_NODELABEL(mram10)) & 0xEFFFFFFFUL));
+	address = (((offset) & 0xEFFFFFFFUL) + (DT_REG_ADDR(DT_NODELABEL(mram10)) & 0xEFFFFFFFUL));
 #else
 	address = offset;
 #endif
 
 	return suit_plat_mem_ptr_get(address);
 }
+
+#ifdef CONFIG_SDFW_IS_UROT
+static nrf_mramc_mode_write_t set_mramc_write_mode(nrf_mramc_mode_write_t new_write_mode)
+{
+	NRF_MRAMC_Type * const mramc = (NRF_MRAMC_Type * const)DT_REG_ADDR(DT_NODELABEL(mramc));
+	nrf_mramc_config_t mramc_config;
+
+	nrf_mramc_config_get(mramc, &mramc_config);
+
+	nrf_mramc_mode_write_t previous_write_mode = mramc_config.mode_write;
+
+	if (new_write_mode != previous_write_mode) {
+		mramc_config.mode_write = new_write_mode;
+		nrf_mramc_config_set(mramc, &mramc_config);
+	}
+
+	return previous_write_mode;
+}
+
+bool suit_plat_mem_clear_sicr_update_registers(void)
+{
+	bool cleared = true;
+
+	if (NRF_SICR->UROT.UPDATE.OPERATION != SICR_UROT_UPDATE_OPERATION_OPCODE_Nop ||
+	    NRF_SICR->UROT.UPDATE.STATUS != SICR_UROT_UPDATE_STATUS_CODE_None) {
+
+		const nrf_mramc_mode_write_t new_write_mode = NRF_MRAMC_MODE_WRITE_DIRECT;
+		const nrf_mramc_mode_write_t previous_write_mode =
+			set_mramc_write_mode(new_write_mode);
+
+		NRF_SICR->UROT.UPDATE.OPERATION = SICR_UROT_UPDATE_OPERATION_OPCODE_Nop;
+		NRF_SICR->UROT.UPDATE.STATUS = SICR_UROT_UPDATE_STATUS_CODE_None;
+
+		/* Trigger MRAM write */
+		NRF_SICR->UROT.UPDATE.SM.TBS.RFU[1] = (uint32_t)0xFFFFFFFF;
+
+		if (previous_write_mode != new_write_mode) {
+			set_mramc_write_mode(previous_write_mode);
+		}
+
+		if (NRF_SICR->UROT.UPDATE.OPERATION != SICR_UROT_UPDATE_OPERATION_OPCODE_Nop ||
+		    NRF_SICR->UROT.UPDATE.STATUS != SICR_UROT_UPDATE_STATUS_CODE_None) {
+			cleared = false;
+		}
+	}
+
+	return cleared;
+}
+#endif /* CONFIG_SDFW_IS_UROT */
