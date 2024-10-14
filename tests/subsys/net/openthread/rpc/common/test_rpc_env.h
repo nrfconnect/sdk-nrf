19d0d53422ad5d82d22e6d1aadb828573b6372e2
Konrad Derda <konrad.derda@nordicsemi.no>
2024-10-14 07:24:19 +0200

    tests: subsys: net: openthread: add DNS client tests
    This commit add tests suite for OpenThread RPC DNS client
    
    Signed-off-by: Konrad Derda <konrad.derda@nordicsemi.no>

--- tests/subsys/net/openthread/rpc/common/test_rpc_env.h
+++ tests/subsys/net/openthread/rpc/common/test_rpc_env.h
@@ -28,11 +28,12 @@
 		.data = (uint8_t[]){bytes}, .len = sizeof((uint8_t[]){bytes}),                     \
 	}
 
-#define RPC_INIT_REQ      RPC_PKT(0x04, 0x00, 0xff, 0x00, 0xff, 0x00, 'o', 't')
-#define RPC_INIT_RSP      RPC_PKT(0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 'o', 't')
-#define RPC_CMD(cmd, ...) RPC_PKT(0x80, cmd, 0xff, 0x00, 0x00 __VA_OPT__(,) __VA_ARGS__, 0xf6)
-#define RPC_RSP(...)      RPC_PKT(0x01, 0xff, 0x00, 0x00, 0x00 __VA_OPT__(,) __VA_ARGS__, 0xf6)
-#define NO_RSP            RPC_PKT()
+#define RPC_INIT_REQ         RPC_PKT(0x04, 0x00, 0xff, 0x00, 0xff, 0x00, 'o', 't')
+#define RPC_INIT_RSP         RPC_PKT(0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 'o', 't')
+#define RPC_CMD(cmd, ...)    RPC_PKT(0x80, cmd, 0xff, 0x00, 0x00 __VA_OPT__(,) __VA_ARGS__, 0xf6)
+#define RPC_CB_CMD(cmd, ...) RPC_PKT(0x80, cmd, 0x00, 0x00, 0x00 __VA_OPT__(,) __VA_ARGS__, 0xf6)
+#define RPC_RSP(...)         RPC_PKT(0x01, 0xff, 0x00, 0x00, 0x00 __VA_OPT__(,) __VA_ARGS__, 0xf6)
+#define NO_RSP               RPC_PKT()
 
 /* Dummy test data */
 
@@ -81,3 +82,32 @@
 #define CBOR_ADDR1 0x50, ADDR_1
 
 #define CBOR_SOC_ADDR CBOR_ADDR1, CBOR_UINT16(1024)
+
+#define DNS_HOSTNAME "example.com"
+#define DNS_SERVICE_NAME "_test._udp.example.com"
+#define DNS_SERVICE_INSTANCE "Test"
+#define DNS_IPV6_ADDR 0x20, 0x01, 0xd, 0xb8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x1
+#define DNS_PORT 53
+#define DNS_TIMEOUT 1000
+#define DNS_ATTEMPTS 5
+
+#define CBOR_IPV6_ADDR 0x50, DNS_IPV6_ADDR
+#define CBOR_DNS_QUERY_CONFIG CBOR_IPV6_ADDR, CBOR_UINT8(DNS_PORT), CBOR_UINT16(DNS_TIMEOUT), \
+			      DNS_ATTEMPTS, OT_DNS_FLAG_RECURSION_DESIRED, OT_DNS_NAT64_DISALLOW,  \
+			      OT_DNS_SERVICE_MODE_SRV_TXT_OPTIMIZE, OT_DNS_TRANSPORT_UDP
+
+#define CBOR_HOSTNAME 0x6C, 'e', 'x', 'a', 'm', 'p', 'l', 'e', '.', 'c', 'o', 'm', 0x0
+#define CBOR_SERVICE_NAME 0x77, '_', 't', 'e', 's', 't', '.',                                   \
+				'_', 'u', 'd', 'p', '.', 'e', 'x', 'a', 'm', 'p', 'l', 'e', '.' \
+				, 'c', 'o', 'm', 0x0
+#define CBOR_SERVICE_INSTANCE 0x65, 'T', 'e', 's', 't', 0x0
+#define CBOR_TXT_DATA 0x50, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x20, 0x54, 0x58, 0x54, \
+		      0x20, 0x44, 0x41, 0x54, 0x41 /* "Service TXT DATA" */
+
+#define CBOR_TTL CBOR_UINT8(120)
+#define CBOR_PORT CBOR_UINT16(12345)
+#define CBOR_PRIORITY 10
+#define CBOR_WEIGHT 12
+
+#define CBOR_SERVICE_INFO CBOR_TTL, CBOR_PORT, CBOR_PRIORITY, CBOR_WEIGHT, CBOR_HOSTNAME, \
+			  CBOR_IPV6_ADDR, CBOR_TTL, CBOR_TXT_DATA, CBOR_FALSE, CBOR_TTL
