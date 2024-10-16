d517017a5943122a23da8aba1f9d962beb6d07e3
Bartlomiej Buczek <bartlomiej.buczek@nordicsemi.no>
2024-10-16 15:17:59 +0200

    tests: current_consumption: Add adc & s2ram test.
    Test is using adc and should reach s2ram power state between sampling.
    Require loopback on P1.02 and P1.03 to work properly.
    
    Signed-off-by: Bartlomiej Buczek <bartlomiej.buczek@nordicsemi.no>

---
+++ tests/benchmarks/multicore/idle_adc/remote/src/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+	k_msleep(500);
+	while (1) {
+		k_msleep(2000);
+	}
+
+	return 0;
+}
