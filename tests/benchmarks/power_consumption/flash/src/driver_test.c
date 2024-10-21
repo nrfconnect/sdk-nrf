6191cef45f3e73fc19b3124b6ed634500aef2891
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2024-10-21 13:11:55 +0200

    tests: benchmarks: Add support for "jedec,mspi-nor" flash devices
    ... so that the tests can be built for nRF54H20 and nRF9280 after they
    are switched to use the new MSPI driver for EXMIF.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- tests/benchmarks/power_consumption/flash/src/driver_test.c
+++ tests/benchmarks/power_consumption/flash/src/driver_test.c
@@ -8,11 +8,20 @@
 #include <zephyr/storage/flash_map.h>
 #include <zephyr/kernel.h>
 
+#if DT_HAS_COMPAT_STATUS_OKAY(jedec_spi_nor)
+#define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(jedec_spi_nor)
+#elif DT_HAS_COMPAT_STATUS_OKAY(jedec_mspi_nor)
+#define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(jedec_mspi_nor)
+#else
+#error Unsupported flash driver
+#define FLASH_NODE DT_INVALID_NODE
+#endif
+
 #define TEST_AREA_OFFSET	0xff000
 #define EXPECTED_SIZE	512
 uint8_t buf[EXPECTED_SIZE];
 
-static const struct device *flash_dev = DEVICE_DT_GET(DT_INST(0, jedec_spi_nor));
+static const struct device *flash_dev = DEVICE_DT_GET(FLASH_NODE);
 
 
 void thread_definition(void)
