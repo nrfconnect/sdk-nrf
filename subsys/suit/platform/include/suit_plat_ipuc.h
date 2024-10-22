67daadf7bd797ed1f4aa1b934a92479bf8b1b99e
Sylwester Konczyk <sylwester.konczyk@nordicsemi.no>
2024-10-22 14:37:55 +0200

    suit: in-place updateable components
    Now it is possible to:
    - Write to declared IPUC
    - Get size of image written to IPUC
    - Get a sdfw_mirror address if appropriate IPUC is declared
    
    Signed-off-by: Sylwester Konczyk <sylwester.konczyk@nordicsemi.no>

--- subsys/suit/platform/include/suit_plat_ipuc.h
+++ subsys/suit/platform/include/suit_plat_ipuc.h
@@ -17,6 +17,7 @@
 #define SUIT_PLAT_IPUC_H__
 
 #include <suit_types.h>
+#include <suit_plat_err.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -26,28 +27,64 @@ extern "C" {
  *
  * @param[in] handle  Handle to the updateable component.
  *
- * @retval SUIT_ERR_UNSUPPORTED_COMPONENT_ID  if component handle is invalid.
- * @retval SUIT_ERR_OVERFLOW  if the maximum number of updateable components has been reached.
- * @retval SUIT_SUCCESS if updateable component was successfully declared.
+ * @retval SUIT_PLAT_ERR_INVAL  component handle is invalid or component is not supported.
+ * @retval SUIT_PLAT_ERR_NOMEM  the maximum number of updateable components has been reached.
+ * @retval SUIT_PLAT_SUCCESS updateable component was successfully declared.
  */
-int suit_plat_ipuc_declare(suit_component_t handle);
+suit_plat_err_t suit_plat_ipuc_declare(suit_component_t handle);
 
 /** @brief Revoke declaration of updateable component.
  *
  * @param[in] handle  Handle to the component that is no longer updateable.
  *
- * @retval SUIT_ERR_UNSUPPORTED_COMPONENT_ID  if component handle is invalid.
- * @retval SUIT_SUCCESS if declaration of updateable component was successfully revoked.
+ * @retval SUIT_PLAT_ERR_NOT_FOUND component is not declared as IPUC
+ * @retval SUIT_PLAT_SUCCESS declaration of updateable component was successfully revoked.
  */
-int suit_plat_ipuc_revoke(suit_component_t handle);
+suit_plat_err_t suit_plat_ipuc_revoke(suit_component_t handle);
 
-/** @brief Find an updateable component ID, which is not smaller than specified size.
+/** @brief Writes a chunk of data to IPUC
  *
- * @param[in] min_size  Minimal size of the updateable component.
+ * @note offset parameter equal to 0 resets the data transfer. That allows to restart a image
+ * transfer, or even to transfer a new image.
  *
- * @returns Pointer to the updateable component ID, NULL otherwise.
+ * @param[in] handle		Handle to the updateable component.
+ * @param[in] offset		Offset to write data chunk. Discontinuous writes are not supported.
+ * @param[in] buffer		Pointer to source data chunk.
+ * @param[in] chunk_size	Size of data chunk.
+ * @param[in] last_chunk	True if this is the last chunk of image.
+ *
+ * @retval SUIT_PLAT_ERR_NOT_FOUND component is not declared as IPUC
+ * @retval SUIT_PLAT_ERR_INCORRECT_STATE discontinued write attempt
+ * @retval SUIT_PLAT_ERR_IO sink not found, sink does not support seek operation, seek operation
+ * failed, streaming to sink failed
+ * @retval SUIT_PLAT_SUCCESS opration completed successfully
+ *
  */
-struct zcbor_string *suit_plat_find_sdfw_mirror_ipuc(size_t min_size);
+suit_plat_err_t suit_plat_ipuc_write(suit_component_t handle, size_t offset, uintptr_t buffer,
+				     size_t chunk_size, bool last_chunk);
+
+/** @brief Gets size of image written to IPUC
+ *
+ * @param[in] handle		Handle to the updateable component.
+ * @param[out] size		Size of image stored in IPUC
+ *
+ * @retval SUIT_PLAT_ERR_INVAL invalid parameter
+ * @retval SUIT_PLAT_ERR_NOT_FOUND component is not declared as IPUC
+ * @retval SUIT_PLAT_ERR_INCORRECT_STATE writing to IPUC is still in progress
+ * @retval SUIT_PLAT_SUCCESS opration completed successfully
+ */
+suit_plat_err_t suit_plat_ipuc_get_stored_img_size(suit_component_t handle, size_t *size);
+
+/** @brief Find an updateable component which is capable to store SDFW update candidate.
+ *
+ * @details Since Secure ROM imposes requirements related to SDFW candidate location, returned
+ * pointer does not necessarily points to the begin of updateable component.
+ *
+ * @param[in] required_size Required size of area capable to store SDFW candidate.
+ *
+ * @returns Pointer to area capable to store SDFW candidate, 0 otherwise.
+ */
+uintptr_t suit_plat_ipuc_sdfw_mirror_addr(size_t required_size);
 
 #ifdef __cplusplus
 }
