6f5c90c4f9947cec34b8d913efd990a9cd12e383
Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
2024-10-24 17:07:37 +0200

    cmake: sysbuild: sdp: cleanup and simplify
    Use a common function for all cases.
    This will scale better for future SDPs.
    
    Signed-off-by: Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>

--- cmake/sysbuild/sdp.cmake
+++ cmake/sysbuild/sdp.cmake
@@ -2,20 +2,23 @@
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-function(sdp_apply_overlays)
-  if(SB_CONFIG_EGPIO_BACKEND_MBOX)
-    sysbuild_cache_set(VAR ${DEFAULT_IMAGE}_SNIPPET APPEND REMOVE_DUPLICATES "sdp-gpio-mbox")
-  elseif(SB_CONFIG_EGPIO_BACKEND_ICMSG)
-    sysbuild_cache_set(VAR ${DEFAULT_IMAGE}_SNIPPET APPEND REMOVE_DUPLICATES "sdp-gpio-icmsg")
-    sysbuild_cache_set(VAR sdp_SNIPPET APPEND REMOVE_DUPLICATES "sdp-gpio-icmsg")
-  elseif(SB_CONFIG_EGPIO_BACKEND_ICBMSG)
-    sysbuild_cache_set(VAR ${DEFAULT_IMAGE}_SNIPPET APPEND REMOVE_DUPLICATES "sdp-gpio-icbmsg")
-    sysbuild_cache_set(VAR sdp_SNIPPET APPEND REMOVE_DUPLICATES "sdp-gpio-icbmsg")
-  endif()
+# Apply snippet to both images. Assumption is that SDP image is called 'sdp'.
+function(sdp_apply_snippets snippet)
+  sysbuild_cache_set(VAR ${DEFAULT_IMAGE}_SNIPPET APPEND REMOVE_DUPLICATES ${snippet})
+  sysbuild_cache_set(VAR sdp_SNIPPET APPEND REMOVE_DUPLICATES ${snippet})
 endfunction()
 
 if(SB_CONFIG_SDP)
   if(SB_CONFIG_SDP_GPIO)
-    sdp_apply_overlays()
+    if(SB_CONFIG_SDP_GPIO_BACKEND_MBOX)
+      set(snippet_name "sdp-gpio-mbox")
+    elseif(SB_CONFIG_SDP_GPIO_BACKEND_ICMSG)
+      set(snippet_name "sdp-gpio-icmsg")
+    elseif(SB_CONFIG_SDP_GPIO_BACKEND_ICBMSG)
+      set(snippet_name "sdp-gpio-icbmsg")
+    endif()
   endif()
+
+  sdp_apply_snippets(${snippet_name})
+  set(snippet_name)
 endif()
