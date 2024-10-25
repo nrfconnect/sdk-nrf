0f67c08165c1499ea47d3240ea306978446eedff
Sebastian Głąb <sebastian.glab@nordicsemi.no>
2024-10-25 11:23:55 +0200

    tests: drivers: gpio: egpio_basic_api: Remove redundant code
    Header file contains definitions that are no longer required.
    
    Signed-off-by: Sebastian Głąb <sebastian.glab@nordicsemi.no>

--- tests/drivers/gpio/egpio_basic_api/src/test_egpio.h
+++ tests/drivers/gpio/egpio_basic_api/src/test_egpio.h
@@ -9,7 +9,6 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/util.h>
 #include <zephyr/ztest.h>
 
 #if DT_NODE_HAS_STATUS(DT_INST(0, test_egpio), okay)
@@ -20,7 +19,6 @@
  */
 #define DEV_OUT DT_GPIO_CTLR(DT_INST(0, test_egpio), out_gpios)
 #define DEV_IN DT_GPIO_CTLR(DT_INST(0, test_egpio), in_gpios)
-#define DEV DEV_OUT
 #define PIN_OUT DT_GPIO_PIN(DT_INST(0, test_egpio), out_gpios)
 #define PIN_OUT_FLAGS DT_GPIO_FLAGS(DT_INST(0, test_egpio), out_gpios)
 #define PIN_IN DT_GPIO_PIN(DT_INST(0, test_egpio), in_gpios)
@@ -34,23 +32,5 @@
 #define PIN_OUT 10
 #define PIN_IN 14
 #endif
-
-#define MAX_INT_CNT 3
-struct drv_data {
-	struct gpio_callback gpio_cb;
-	gpio_flags_t mode;
-	int index;
-	int aux;
-};
-
-void test_egpio_pin_read_write(void);
-void test_egpio_callback_add_remove(void);
-void test_egpio_callback_self_remove(void);
-void test_egpio_callback_enable_disable(void);
-void test_egpio_callback_variants(void);
-
-void test_egpio_port(void);
-
-void test_egpio_deprecated(void);
 
 #endif /* __TEST_EGPIO_H__ */
