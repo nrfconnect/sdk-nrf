029db1256b5087c0be91ab8a35782230fb3df349
Bartosz Miller <bartosz.miller@nordicsemi.no>
2024-10-31 08:07:36 +0100

    tests: benchmarks: Verify HPU feature temperature wakeups
    SCFW HPU feature measures temperature periodically
    Verify the presence and period of the measurements
    
    Signed-off-by: Bartosz Miller <bartosz.miller@nordicsemi.no>
    (cherry picked from commit e15c6f88f55bc62332261e5349ea8e9da6cc3417)

---
+++ tests/benchmarks/multicore/idle_hpu_temp_meas/src/main.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/logging/log.h>
+#include <nrfs_backend_ipc_service.h>
+#include <nrfs_gdpwr.h>
+
+LOG_MODULE_REGISTER(idle_hpu_temp_meas);
+
+/* Required to power off the GD2 and GD3 domains
+ * Will be removed when GD handling
+ * is implemented in sdk-zephyr
+ */
+static void gdpwr_handler(nrfs_gdpwr_evt_t const *p_evt, void *context)
+{
+	switch (p_evt->type) {
+	case NRFS_GDPWR_REQ_APPLIED:
+		LOG_INF("GDPWR handler - response received: 0x%x, CTX=%d", p_evt->type,
+			(uint32_t)context);
+		break;
+	case NRFS_GDPWR_REQ_REJECTED:
+		LOG_ERR("GDPWR handler - request rejected: 0x%x, CTX=%d", p_evt->type,
+			(uint32_t)context);
+		break;
+	default:
+		LOG_ERR("GDPWR handler - unexpected event: 0x%x, CTX=%d", p_evt->type,
+			(uint32_t)context);
+		break;
+	}
+}
+
+/* Required to power off the GD2 and GD3 domains
+ * Will be removed when GD handling
+ * is implemented in sdk-zephyr
+ */
+static void clear_global_power_domains_requests(void)
+{
+	int service_status;
+	int tst_ctx = 1;
+
+	service_status = nrfs_gdpwr_init(gdpwr_handler);
+	LOG_INF("Response: %d", service_status);
+	LOG_INF("Sending GDPWR DISABLE request for: GDPWR_POWER_DOMAIN_ACTIVE_SLOW");
+	service_status = nrfs_gdpwr_power_request(GDPWR_POWER_DOMAIN_ACTIVE_SLOW,
+						  GDPWR_POWER_REQUEST_CLEAR, (void *)tst_ctx++);
+	LOG_INF("Response: %d", service_status);
+	LOG_INF("Sending GDPWR DISABLE request for: GDPWR_POWER_DOMAIN_ACTIVE_FAST");
+	service_status = nrfs_gdpwr_power_request(GDPWR_POWER_DOMAIN_ACTIVE_FAST,
+						  GDPWR_POWER_REQUEST_CLEAR, (void *)tst_ctx++);
+	LOG_INF("Response: %d", service_status);
+	LOG_INF("Sending GDPWR DISABLE request for: GDPWR_POWER_DOMAIN_MAIN_SLOW");
+	service_status = nrfs_gdpwr_power_request(GDPWR_POWER_DOMAIN_MAIN_SLOW,
+						  GDPWR_POWER_REQUEST_CLEAR, (void *)tst_ctx);
+	LOG_INF("Response: %d", service_status);
+}
+
+int main(void)
+{
+	LOG_INF("SCFW HPU temperature measurement feature test, %s", CONFIG_BOARD_TARGET);
+	nrfs_backend_wait_for_connection(K_FOREVER);
+	clear_global_power_domains_requests();
+	while (1) {
+		LOG_INF("Going to sleep forever");
+		k_sleep(K_FOREVER);
+	}
+
+	return 0;
+}
