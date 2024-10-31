f1e16defe2bfcb55bea6846de2e41cb309e61dc2
Piotr Krzyzanowski <piotr.krzyzanowski@nordicsemi.no>
2024-10-31 17:49:52 +0100

    tests: benchmarks: power_consumption: cleanup
    Use common source file for additional "remote" image
    Change nrf54l15pdk to nrf54l15dk as "pdk" is no longer
    supported
    
    Signed-off-by: Piotr Krzyzanowski <piotr.krzyzanowski@nordicsemi.no>
    (cherry picked from commit 479a68b6ce108599e601d858526ba5044849de5e)

--- tests/benchmarks/power_consumption/gpio/sysbuild.cmake
+++ tests/benchmarks/power_consumption/gpio/sysbuild.cmake
@@ -7,14 +7,14 @@
 if(NOT "${SB_CONFIG_REMOTE_BOARD}" STREQUAL "")
   # Add remote project
   ExternalZephyrProject_Add(
-      APPLICATION remote
-      SOURCE_DIR ${APP_DIR}/remote
+      APPLICATION remote_sleep_forever
+      SOURCE_DIR ${APP_DIR}/../common/remote_sleep_forever
       BOARD ${SB_CONFIG_REMOTE_BOARD}
       BOARD_REVISION ${BOARD_REVISION}
     )
 
   # Add a dependency so that the remote image will be built and flashed first
-  add_dependencies(gpio remote)
+  add_dependencies(gpio remote_sleep_forever)
   # Add dependency so that the remote image is flashed first.
-  sysbuild_add_dependencies(FLASH gpio remote)
+  sysbuild_add_dependencies(FLASH gpio remote_sleep_forever)
 endif()
