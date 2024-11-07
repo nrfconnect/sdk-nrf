696ed1e01f5205ea0fdf57ffa857304ade5405c0
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2024-11-07 13:24:20 +0100

    scripts: west_commands: genboard: support current NCS only
    Maintaining multiversion compatibility is a nightmare, so lets make the
    tool support the current NCS version, ie, the one in the same tree.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- scripts/west_commands/genboard/west-ncs-genboard-test.sh
+++ scripts/west_commands/genboard/west-ncs-genboard-test.sh
@@ -7,52 +7,29 @@ NCS_BASE="$SCRIPTDIR/../../.."
 
 SOC=${1}
 
-declare -a NCS_VERSIONS=(
-	"2.0.0"
-	"2.1.0"
-	"2.2.0"
-	"2.3.0"
-	"2.4.0"
-	"2.5.0"
-	"2.6.0"
-	"2.7.0"
-	"2.8.0"
-	"2.8.99"
-)
-
 declare -a SOCS=(
-	"nrf52805 caaa 2.0.0"
-	"nrf52810 qfaa 2.0.0"
-	"nrf52811 qfaa 2.0.0"
-	"nrf52820 qdaa 2.0.0"
-	"nrf52832 ciaa 2.0.0"
-	"nrf52832 qfaa 2.0.0"
-	"nrf52832 qfab 2.0.0"
-	"nrf52833 qdaa 2.5.0"
-	"nrf52833 qiaa 2.0.0"
-	"nrf52840 qfaa 2.5.0"
-	"nrf52840 qiaa 2.0.0"
-	"nrf5340 qkaa 2.0.0"
-	"nrf54l15 qfaa 2.7.0"
-	"nrf9131 laca 2.5.0"
-	"nrf9151 laca 2.6.0"
-	"nrf9160 sica 2.0.0"
-	"nrf9161 laca 2.5.0"
+	"nrf52805 caaa"
+	"nrf52810 qfaa"
+	"nrf52811 qfaa"
+	"nrf52820 qdaa"
+	"nrf52832 ciaa"
+	"nrf52832 qfaa"
+	"nrf52832 qfab"
+	"nrf52833 qdaa"
+	"nrf52833 qiaa"
+	"nrf52840 qfaa"
+	"nrf52840 qiaa"
+	"nrf5340 qkaa"
+	"nrf54l15 qfaa"
+	"nrf9131 laca"
+	"nrf9151 laca"
+	"nrf9160 sica"
+	"nrf9161 laca"
 )
 
 HELLO_WORLD="$NCS_BASE/../zephyr/samples/hello_world"
 
-# https://stackoverflow.com/questions/4023830
-verlte() {
-	printf '%s\n' "$1" "$2" | sort -C -V
-}
-
-verlt() {
-	! verlte "$2" "$1"
-}
-
-rm -rf $NCS_BASE/boards/arm/testvnd $NCS_BASE/boards/testvnd
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+rm -rf $NCS_BASE/boards/testvnd
 
 for soc in "${SOCS[@]}"; do
 	read -a socarr <<< "$soc"
@@ -62,91 +39,34 @@ for soc in "${SOCS[@]}"; do
 		continue
 	fi
 
-	for ncs_version in "${NCS_VERSIONS[@]}"; do
-		ncs_version_esc="${ncs_version//./_}"
+	board=brd_${socarr[0]}_${socarr[1]}
 
-		if verlt $ncs_version ${socarr[2]}; then
-			echo "Skipping ${socarr[0]}-${socarr[1]} for NCS $ncs_version (unsupported)"
-			continue;
-		fi
+	echo "Generating board: $board"
 
-		board=brd_${socarr[0]}_${socarr[1]}_$ncs_version_esc
+	west ncs-genboard \
+		-o $NCS_BASE/boards \
+		-e "testvnd" \
+		-b $board \
+		-d "Test Board" \
+		-s ${socarr[0]} \
+		-v ${socarr[1]} \
 
-		echo "Generating board: $board"
+	echo "Building hello_world for: $board"
 
-		west ncs-genboard \
-			-o $NCS_BASE/boards \
-			-e "testvnd" \
-			-b $board \
-			-d "Test Board" \
-			-s ${socarr[0]} \
-			-v ${socarr[1]} \
-			-n "$ncs_version"
-	done
-done
-
-for ncs_version in "${NCS_VERSIONS[@]}"
-do
-	ncs_version_esc="${ncs_version//./_}"
-
-	echo "Switching to NCS $ncs_version"
-
-	LATEST=".*99"
-	if [[ ${ncs_version} =~ $LATEST ]]; then
-		git checkout main
-	else
-		git checkout v$ncs_version
+	if [[ ${socarr[0]} == nrf52* ]]; then
+		west build -p -b $board $HELLO_WORLD
+	elif [[ ${socarr[0]} == nrf53* ]]; then
+		west build -p -b $board/${socarr[0]}/cpuapp $HELLO_WORLD
+		west build -p -b $board/${socarr[0]}/cpuapp/ns $HELLO_WORLD
+		west build -p -b $board/${socarr[0]}/cpunet $HELLO_WORLD
+	elif [[ ${socarr[0]} == nrf54l* ]]; then
+		west build -p -b $board/${socarr[0]}/cpuapp $HELLO_WORLD
+		# west build -p -b $board/${socarr[0]}/cpuflpr $HELLO_WORLD
+		# west build -p -b $board/${socarr[0]}/cpuflpr/xip $HELLO_WORLD
+	elif [[ ${socarr[0]} == nrf91* ]]; then
+		west build -p -b $board/${socarr[0]} $HELLO_WORLD
+		west build -p -b $board/${socarr[0]}/ns $HELLO_WORLD
 	fi
-	west update
 
-	for soc in "${SOCS[@]}"; do
-		read -a socarr <<< "$soc"
-
-		if [ ! -z "$SOC" ] && [ $SOC != ${socarr[0]} ]; then
-			echo "Skipping ${socarr[0]} (not requested)"
-			continue
-		fi
-
-		if verlt $ncs_version ${socarr[2]}; then
-			continue;
-		fi
-
-		board=brd_${socarr[0]}_${socarr[1]}_$ncs_version_esc
-
-		echo "Building for: $board"
-
-		if [[ ${socarr[0]} == nrf52* ]]; then
-			west build -p -b $board $HELLO_WORLD
-		elif [[ ${socarr[0]} == nrf53* ]]; then
-			if verlt $ncs_version "2.7.0"; then
-				west build -p -b ${board}_cpuapp $HELLO_WORLD
-				if verlte "2.6.0" $ncs_version; then
-					west build -p -b ${board}_cpuapp_ns $HELLO_WORLD
-				fi
-				west build -p -b ${board}_cpunet $HELLO_WORLD
-			else
-				west build -p -b $board/${socarr[0]}/cpuapp $HELLO_WORLD
-				west build -p -b $board/${socarr[0]}/cpuapp/ns $HELLO_WORLD
-				west build -p -b $board/${socarr[0]}/cpunet $HELLO_WORLD
-			fi
-		elif [[ ${socarr[0]} == nrf54l* ]]; then
-			west build -p -b $board/${socarr[0]}/cpuapp $HELLO_WORLD
-			# west build -p -b $board/${socarr[0]}/cpuflpr $HELLO_WORLD
-			# west build -p -b $board/${socarr[0]}/cpuflpr/xip $HELLO_WORLD
-		elif [[ ${socarr[0]} == nrf91* ]]; then
-			if verlt $ncs_version "2.7.0"; then
-				west build -p -b $board $HELLO_WORLD
-				if verlte "2.6.0" $ncs_version; then
-					west build -p -b ${board}_ns $HELLO_WORLD
-				fi
-			else
-				west build -p -b $board/${socarr[0]} $HELLO_WORLD
-				west build -p -b $board/${socarr[0]}/ns $HELLO_WORLD
-			fi
-		fi
-	done
+	rm -rf $NCS_BASE/boards/testvnd
 done
-
-rm -rf $NCS_BASE/boards/arm/brd_* $NCS_BASE/boards/testvnd
-git checkout $BRANCH
-west update
