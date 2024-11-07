a7453645236bc19bd9d3f648fc48ec3cc2b3e386
Piotr Krzyzanowski <piotr.krzyzanowski@nordicsemi.no>
2024-11-07 11:36:11 +0100

    tests: benchmarks: remove GPD workaround
    Remove GDP workaround.
    
    Signed-off-by: Piotr Krzyzanowski <piotr.krzyzanowski@nordicsemi.no>

--- tests/benchmarks/power_consumption/common/remote_sleep_forever/src/main.c
+++ tests/benchmarks/power_consumption/common/remote_sleep_forever/src/main.c
@@ -3,39 +3,11 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
-
 #include <zephyr/kernel.h>
 
-#ifdef CONFIG_BOARD_NRF54H20DK
-#include <nrfs_backend_ipc_service.h>
-#include <nrfs_gdpwr.h>
-
-static void gdpwr_handler(nrfs_gdpwr_evt_t const *p_evt, void *context)
-{
-}
-
-static void clear_global_power_domains_requests(void)
-{
-	int service_status;
-	int tst_ctx = 1;
-
-	service_status = nrfs_gdpwr_init(gdpwr_handler);
-	service_status = nrfs_gdpwr_power_request(GDPWR_POWER_DOMAIN_ACTIVE_SLOW,
-						  GDPWR_POWER_REQUEST_CLEAR, (void *)tst_ctx++);
-	service_status = nrfs_gdpwr_power_request(GDPWR_POWER_DOMAIN_ACTIVE_FAST,
-						  GDPWR_POWER_REQUEST_CLEAR, (void *)tst_ctx++);
-	service_status = nrfs_gdpwr_power_request(GDPWR_POWER_DOMAIN_MAIN_SLOW,
-						  GDPWR_POWER_REQUEST_CLEAR, (void *)tst_ctx);
-}
-#endif
 
 int main(void)
 {
-#ifdef CONFIG_BOARD_NRF54H20DK
-	nrfs_backend_wait_for_connection(K_FOREVER);
-	clear_global_power_domains_requests();
-#endif
-
 	k_sleep(K_FOREVER);
 
 	return 0;
--- tests/benchmarks/power_consumption/qdec/src/main.c
+++ tests/benchmarks/power_consumption/qdec/src/main.c
@@ -9,10 +9,6 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/pm/device_runtime.h>
 
-#ifdef CONFIG_BOARD_NRF54H20DK
-#include <nrfs_backend_ipc_service.h>
-#include <nrfs_gdpwr.h>
-#endif
 
 #define LOG_LEVEL LOG_LEVEL_DBG
 
@@ -120,34 +116,9 @@ void init_device(void)
 	__ASSERT_NO_MSG(rc == 0);
 }
 
-#ifdef CONFIG_BOARD_NRF54H20DK
-static void gdpwr_handler(nrfs_gdpwr_evt_t const *p_evt, void *context)
-{
-}
-
-static void clear_global_power_domains_requests(void)
-{
-	int service_status;
-	int tst_ctx = 1;
-
-	service_status = nrfs_gdpwr_init(gdpwr_handler);
-	service_status = nrfs_gdpwr_power_request(GDPWR_POWER_DOMAIN_ACTIVE_SLOW,
-						  GDPWR_POWER_REQUEST_CLEAR, (void *)tst_ctx++);
-	service_status = nrfs_gdpwr_power_request(GDPWR_POWER_DOMAIN_ACTIVE_FAST,
-						  GDPWR_POWER_REQUEST_CLEAR, (void *)tst_ctx++);
-	service_status = nrfs_gdpwr_power_request(GDPWR_POWER_DOMAIN_MAIN_SLOW,
-						  GDPWR_POWER_REQUEST_CLEAR, (void *)tst_ctx);
-}
-#endif
-
 int main(void)
 {
 	int rc;
-
-#ifdef CONFIG_BOARD_NRF54H20DK
-	nrfs_backend_wait_for_connection(K_FOREVER);
-	clear_global_power_domains_requests();
-#endif
 
 	init_device();
 
