96c7ea30e125181a0472ef222d7b10a41edbf287
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-11-07 13:02:12 +0000

    bootloader: bl_crypto: Add SHA512 support
    Adds support for SHA512 as the hash algorithm in the bl_crypto
    module using PSA
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- include/bl_crypto.h
+++ include/bl_crypto.h
@@ -14,7 +14,6 @@ extern "C" {
 #include <zephyr/types.h>
 #include <fw_info.h>
 
-
 /** @defgroup bl_crypto Bootloader crypto functions
  * @{
  */
@@ -25,18 +24,33 @@ extern "C" {
 #define ESIGINV  102
 
 
-#if CONFIG_SB_CRYPTO_OBERON_SHA256
-	#include <ocrypto_sha256.h>
-	#define SHA256_CTX_SIZE sizeof(ocrypto_sha256_ctx)
-	typedef ocrypto_sha256_ctx bl_sha256_ctx_t;
-#elif CONFIG_SB_CRYPTO_CC310_SHA256
-	#include <nrf_cc310_bl_hash_sha256.h>
-	#define SHA256_CTX_SIZE sizeof(nrf_cc310_bl_hash_context_sha256_t)
-	typedef nrf_cc310_bl_hash_context_sha256_t bl_sha256_ctx_t;
+#if defined(CONFIG_SB_CRYPTO_OBERON_SHA256)
+/* SHA256 (oberon) */
+#include <ocrypto_sha256.h>
+
+#define SHA256_CTX_SIZE sizeof(ocrypto_sha256_ctx)
+
+typedef ocrypto_sha256_ctx bl_sha256_ctx_t;
+#elif defined(CONFIG_SB_CRYPTO_CC310_SHA256)
+/* SHA256 (cc310) */
+#include <nrf_cc310_bl_hash_sha256.h>
+
+#define SHA256_CTX_SIZE sizeof(nrf_cc310_bl_hash_context_sha256_t)
+
+typedef nrf_cc310_bl_hash_context_sha256_t bl_sha256_ctx_t;
+#elif defined(CONFIG_SB_CRYPTO_PSA_SHA512)
+/* SHA512 (PSA) */
+#include <psa/crypto.h>
+#include <psa/crypto_extra.h>
+
+#define SHA512_CTX_SIZE sizeof(psa_hash_operation_t)
+
+typedef psa_hash_operation_t bl_sha512_ctx_t;
 #else
-	#define SHA256_CTX_SIZE 256
-	// uint32_t to make sure it is aligned equally as the other contexts.
-	typedef uint32_t bl_sha256_ctx_t[SHA256_CTX_SIZE/4];
+/* SHA256 fallback */
+#define SHA256_CTX_SIZE 256
+/* uint32_t to make sure it is aligned equally as the other contexts. */
+typedef uint32_t bl_sha256_ctx_t[SHA256_CTX_SIZE/4];
 #endif
 
 /**
@@ -95,7 +109,6 @@ int bl_root_of_trust_verify_external(const uint8_t *public_key,
 				     const uint8_t *signature,
 				     const uint8_t *firmware,
 				     const uint32_t firmware_len);
-
 
 /**
  * @brief Initialize a sha256 operation context variable.
@@ -169,6 +182,60 @@ int bl_sha256_verify(const uint8_t *data, uint32_t data_len, const uint8_t *expe
 typedef int (*bl_sha256_verify_t)(const uint8_t *data, uint32_t data_len,
 				const uint8_t *expected);
 
+/**
+ * @brief Initialize a sha512 operation context variable.
+ *
+ * @param[out]  ctx  Context to be initialized.
+ *
+ * @retval 0         On success.
+ * @retval -EINVAL   If @p ctx was NULL.
+ */
+int bl_sha512_init(bl_sha512_ctx_t *ctx);
+
+/**
+ * @brief Hash a portion of data.
+ *
+ * @note @p ctx must be initialized before being used in this function.
+ *       An uninitialized @p ctx might not be reported as an error. Also,
+ *       @p ctx must not be used if it has been finalized, though this might
+ *       also not be reported as an error.
+ *
+ * @param[in]  ctx       Context variable. Must have been initialized.
+ * @param[in]  data      Data to hash.
+ * @param[in]  data_len  Length of @p data.
+ *
+ * @retval 0         On success.
+ * @retval -EINVAL   If @p ctx was NULL, uninitialized, or corrupted.
+ * @retval -ENOSYS   If the context has already been finalized.
+ */
+int bl_sha512_update(bl_sha512_ctx_t *ctx, const uint8_t *data, uint32_t data_len);
+
+/**
+ * @brief Finalize a hash result.
+ *
+ * @param[in]  ctx       Context variable.
+ * @param[out] output    Where to put the resulting digest. Must be at least
+ *                       64 bytes long.
+ *
+ * @retval 0         On success.
+ * @retval -EINVAL   If @p ctx was NULL or corrupted, or @p output was NULL.
+ */
+int bl_sha512_finalize(bl_sha512_ctx_t *ctx, uint8_t *output);
+
+/**
+ * @brief Calculate a digest and verify it directly.
+ *
+ * @param[in]  data      The data to hash.
+ * @param[in]  data_len  The length of @p data.
+ * @param[in]  expected  The expected digest over @p data.
+ *
+ * @retval 0          If the procedure succeeded and the resulting digest is
+ *                    identical to @p expected.
+ * @retval -EHASHINV  If the procedure succeeded, but the digests don't match.
+ * @return Any error code from @ref bl_sha512_init, @ref bl_sha512_update, or
+ *         @ref bl_sha512_finalize if something else went wrong.
+ */
+int bl_sha512_verify(const uint8_t *data, uint32_t data_len, const uint8_t *expected);
 
 /**
  * @brief Validate a secp256r1 signature.
@@ -195,6 +262,23 @@ typedef int (*bl_secp256r1_validate_t)(
 			  const uint8_t *signature,
 			  const uint8_t *public_key);
 
+/**
+ * @brief Validate an ed25519 signature.
+ *
+ * @param[in]  hash        The hash to validate against.
+ * @param[in]  hash_len    The length of the hash.
+ * @param[in]  signature   The signature to validate.
+ * @param[in]  public_key  The public key to validate with.
+ *
+ * @retval 0         The operation succeeded and the signature is valid for the
+ *                   hash.
+ * @retval -EINVAL   A parameter was NULL, or the @p hash_len was not 32 bytes.
+ * @retval -ESIGINV  The signature validation failed.
+ */
+int bl_ed25519_validate(const uint8_t *hash,
+			  uint32_t hash_len,
+			  const uint8_t *signature,
+			  const uint8_t *public_key);
 
 /**
  * @brief Structure describing the BL_ROT_VERIFY EXT_API.
