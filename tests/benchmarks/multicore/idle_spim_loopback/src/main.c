b28b1e4351e330688f3e9d650dd5f13f76d1c143
Piotr Krzyzanowski <piotr.krzyzanowski@nordicsemi.no>
2024-11-07 11:36:11 +0100

    tests: benchmarks: remove GPD workaround
    Remove GDP workaround.
    
    Signed-off-by: Piotr Krzyzanowski <piotr.krzyzanowski@nordicsemi.no>
    (cherry picked from commit ae0935a72b7acc18da88b52b4505eb41e6abbb2c)

--- tests/benchmarks/multicore/idle_spim_loopback/src/main.c
+++ tests/benchmarks/multicore/idle_spim_loopback/src/main.c
@@ -12,9 +12,6 @@ LOG_MODULE_REGISTER(idle_spim_loopback, LOG_LEVEL_INF);
 #include <zephyr/linker/devicetree_regions.h>
 #include <zephyr/pm/device_runtime.h>
 
-#include <nrfs_backend_ipc_service.h>
-#include <nrfs_gdpwr.h>
-
 #define	DELTA			(1)
 
 #define SPI_MODE (SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_LINES_SINGLE | SPI_TRANSFER_MSB \
@@ -40,52 +37,6 @@ void my_timer_handler(struct k_timer *dummy)
 	timer_expired = true;
 }
 
-/* Required to power off the GD2 and GD3 domains
- * Will be removed when GD handling
- * is implemented in sdk-zephyr
- */
-static void gdpwr_handler(nrfs_gdpwr_evt_t const *p_evt, void *context)
-{
-	switch (p_evt->type) {
-	case NRFS_GDPWR_REQ_APPLIED:
-		printk("GDPWR handler - response received: 0x%x, CTX=%d\n", p_evt->type,
-			(uint32_t)context);
-		break;
-	case NRFS_GDPWR_REQ_REJECTED:
-		printk("GDPWR handler - request rejected: 0x%x, CTX=%d\n", p_evt->type,
-			(uint32_t)context);
-		break;
-	default:
-		printk("GDPWR handler - unexpected event: 0x%x, CTX=%d\n", p_evt->type,
-			(uint32_t)context);
-		break;
-	}
-}
-
-/* Required to power off the GD2 and GD3 domains
- * Will be removed when GD handling
- * is implemented in sdk-zephyr
- */
-static void clear_global_power_domains_requests(void)
-{
-	int service_status;
-	int tst_ctx = 1;
-
-	service_status = nrfs_gdpwr_init(gdpwr_handler);
-	printk("Response: %d\n", service_status);
-	printk("Sending GDPWR DISABLE request for: GDPWR_POWER_DOMAIN_ACTIVE_SLOW\n");
-	service_status = nrfs_gdpwr_power_request(GDPWR_POWER_DOMAIN_ACTIVE_SLOW,
-						GDPWR_POWER_REQUEST_CLEAR, (void *)tst_ctx++);
-	printk("Response: %d\n", service_status);
-	printk("Sending GDPWR DISABLE request for: GDPWR_POWER_DOMAIN_ACTIVE_FAST\n");
-	service_status = nrfs_gdpwr_power_request(GDPWR_POWER_DOMAIN_ACTIVE_FAST,
-						GDPWR_POWER_REQUEST_CLEAR, (void *)tst_ctx++);
-	printk("Response: %d\n", service_status);
-	printk("Sending GDPWR DISABLE request for: GDPWR_POWER_DOMAIN_MAIN_SLOW\n");
-	service_status = nrfs_gdpwr_power_request(GDPWR_POWER_DOMAIN_MAIN_SLOW,
-						GDPWR_POWER_REQUEST_CLEAR, (void *)tst_ctx);
-	printk("Response: %d\n", service_status);
-}
 
 int main(void)
 {
@@ -112,9 +63,6 @@ int main(void)
 		.buffers = &rx_spi_buf,
 		.count = 1
 	};
-
-	nrfs_backend_wait_for_connection(K_FOREVER);
-	clear_global_power_domains_requests();
 
 	LOG_INF("%s runs as a SPI HOST", CONFIG_BOARD_TARGET);
 	LOG_INF("%d bytes of data exchanged at once", CONFIG_DATA_FIELD);
