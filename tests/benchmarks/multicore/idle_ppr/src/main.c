2b09a32aee737e65205bfe1ce1dc850f2b6c7a29
Sebastian Głąb <sebastian.glab@nordicsemi.no>
2024-11-12 08:52:24 +0100

    tests: benchmarks: multicore: Add test for S2RAM and PPR
    Add non-functional check (current consumption) for sample
    application that:
    - runs on cpuapp, cpurad and cpuppr;
    - enters low power state.
    
    Confirm that when PPR core is used:
    - SOC may enter low power states like IDLE or S2RAM.
    
    Signed-off-by: Sebastian Głąb <sebastian.glab@nordicsemi.no>
    (cherry picked from commit 7504e21df95756c5fabe8968a50ecca6729f8d69)

---
+++ tests/benchmarks/multicore/idle_ppr/src/main.c
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(idle_ppr, LOG_LEVEL_INF);
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+
+/* Variables used to make CPU active for ~1 second */
+static struct k_timer my_timer;
+static bool timer_expired;
+
+void my_timer_handler(struct k_timer *dummy)
+{
+	timer_expired = true;
+}
+
+int main(void)
+{
+	int counter = 0;
+	int ret;
+
+	LOG_INF("Multicore idle_ppr test on %s", CONFIG_BOARD_TARGET);
+	LOG_INF("Main sleeps for %d ms", CONFIG_TEST_SLEEP_DURATION_MS);
+
+	ret = gpio_is_ready_dt(&led);
+	if (!ret) {
+		LOG_ERR("LED is not ready");
+	}
+	__ASSERT(ret, "LED is not ready\n");
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		LOG_ERR("Unable to configure GPIO as output");
+	}
+	__ASSERT(ret == 0, "Unable to configure GPIO as output\n");
+
+	k_timer_init(&my_timer, my_timer_handler, NULL);
+
+	/* Run test forever */
+	while (1) {
+		timer_expired = false;
+
+		/* start a one-shot timer that expires after 1 second */
+		k_timer_start(&my_timer, K_MSEC(1000), K_NO_WAIT);
+
+		/* Turn ON LED */
+		ret = gpio_pin_set_dt(&led, 1);
+		if (ret < 0) {
+			LOG_ERR("Unable to turn on LED");
+		}
+		__ASSERT(ret == 0, "Unable to turn on LED\n");
+
+		/* Keep CPU active for ~ 1 second */
+		while (!timer_expired) {
+			k_busy_wait(10000);
+			k_yield();
+		}
+
+		/* Turn OFF LED */
+		ret = gpio_pin_set_dt(&led, 0);
+		if (ret < 0) {
+			LOG_ERR("Unable to turn off LED");
+		}
+		__ASSERT(ret == 0, "Unable to turn off LED\n");
+
+		LOG_INF("Run %d", counter);
+		counter++;
+
+		/* Sleep / enter low power state */
+		k_msleep(CONFIG_TEST_SLEEP_DURATION_MS);
+	}
+
+	return 0;
+}
