4f1c09023681fd5c6b43443cf6449ae4c6eafa63
Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>
2024-11-14 13:34:00 +0100

    nrf54l: Removing hardcoded clock frequency
    Adding in config that takes clock-frequency from device tree and passes
    it to TF-m
    Removing hardcoded NRF_CONFIG_CPU_FREQ_MHZ
    
    Signed-off-by: Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>

--- modules/trusted-firmware-m/tfm_boards/nrf54l15_cpuapp/ns/cpuarch_ns.cmake
+++ modules/trusted-firmware-m/tfm_boards/nrf54l15_cpuapp/ns/cpuarch_ns.cmake
@@ -7,6 +7,4 @@
 set(PLATFORM_DIR  ${CMAKE_CURRENT_LIST_DIR})
 set(PLATFORM_PATH ${CMAKE_CURRENT_LIST_DIR})
 
-add_compile_definitions(NRF_CONFIG_CPU_FREQ_MHZ=128)
-
 include(${CMAKE_CURRENT_LIST_DIR}/common/nrf54l15/cpuarch.cmake)
