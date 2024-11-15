aeac638eb5cf1f9ed6578d714ffa08f353c37481
Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>
2024-11-15 11:47:25 +0100

    applications: nrf_desktop: failsafe: migrate to Zephyr hwinfo driver
    Updated the nRF Desktop Failsafe module to use the Zephyr HWinfo
    driver as its dependency instead of relying on the nrfx reset reason
    helper.
    
    Ref: NCSDK-25090
    
    Signed-off-by: Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>

--- applications/nrf_desktop/src/modules/failsafe.c
+++ applications/nrf_desktop/src/modules/failsafe.c
@@ -5,7 +5,7 @@
  */
 
 #include <zephyr/kernel.h>
-#include <helpers/nrfx_reset_reason.h>
+#include <zephyr/drivers/hwinfo.h>
 #include <zephyr/storage/flash_map.h>
 
 #define MODULE failsafe
@@ -15,17 +15,27 @@
 LOG_MODULE_REGISTER(MODULE, CONFIG_DESKTOP_FAILSAFE_LOG_LEVEL);
 
 
-static bool failsafe_check(void)
+static int failsafe_check(bool *failure_detected)
 {
-	uint32_t mask = NRFX_RESET_REASON_DOG_MASK |
-			NRFX_RESET_REASON_LOCKUP_MASK;
+	int err;
+	uint32_t reas = 0;
+	static const uint32_t mask = RESET_WATCHDOG | RESET_CPU_LOCKUP;
 
-	uint32_t reas = nrfx_reset_reason_get();
+	err = hwinfo_get_reset_cause(&reas);
+	if (err) {
+		LOG_ERR("Failed to fetch reset cause: %d", err);
+		return err;
+	}
 
-	return (reas & mask) != 0;
+	*failure_detected = ((reas & mask) != 0);
+
+	LOG_INF("Reset reason (0x%08X) trigger %s", reas,
+		*failure_detected ? "active" : "inactive");
+
+	return err;
 }
 
-static void failsafe_erase(void)
+static int failsafe_erase(void)
 {
 	const struct flash_area *flash_area;
 	int err = flash_area_open(FIXED_PARTITION_ID(storage_partition),
@@ -37,15 +47,24 @@ static void failsafe_erase(void)
 	}
 
 	if (err) {
-		LOG_ERR("Failsafe cannot erase settings");
+		LOG_ERR("Failsafe cannot erase settings: %d", err);
 	} else {
 		LOG_WRN("Failsafe erased settings");
 	}
+
+	return err;
 }
 
-static void failsafe_clear(void)
+static int failsafe_clear(void)
 {
-	nrfx_reset_reason_clear(nrfx_reset_reason_get());
+	int err;
+
+	err = hwinfo_clear_reset_cause();
+	if (err) {
+		LOG_ERR("Failed to clear reset cause: %d", err);
+	}
+
+	return err;
 }
 
 static bool app_event_handler(const struct app_event_header *aeh)
@@ -55,17 +74,27 @@ static bool app_event_handler(const struct app_event_header *aeh)
 				cast_module_state_event(aeh);
 
 		if (check_state(event, MODULE_ID(main), MODULE_STATE_READY)) {
+			int err;
+			bool failure_detected;
 			static bool initialized;
 
 			__ASSERT_NO_MSG(!initialized);
 			initialized = true;
 
-			if (failsafe_check()) {
-				failsafe_erase();
+			err = failsafe_check(&failure_detected);
+			if (!err && failure_detected) {
+				err = failsafe_erase();
 			}
-			failsafe_clear();
 
-			module_set_state(MODULE_STATE_READY);
+			if (!err) {
+				err = failsafe_clear();
+			}
+
+			if (!err) {
+				module_set_state(MODULE_STATE_READY);
+			} else {
+				module_set_state(MODULE_STATE_ERROR);
+			}
 		}
 
 		return false;
