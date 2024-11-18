6f35a511e986589e545e256d8f7c322146ed173c
Jakub Zymelka <jakub.zymelka@nordicsemi.no>
2024-11-18 10:38:21 +0100

    sysbuild: sdp: Add SDP MSPI to cmake
    Add cmake files to be able to include the SDP MSPI
    application in solutions where SDP MSPI is required.
    
    Signed-off-by: Jakub Zymelka <jakub.zymelka@nordicsemi.no>

--- sysbuild/sdp.cmake
+++ sysbuild/sdp.cmake
@@ -2,18 +2,29 @@
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-# If it is enabled, include the SDP GPIO application in the build
-if(SB_CONFIG_SDP_GPIO)
+if(SB_CONFIG_SDP)
   # Extract SoC name from related variables
   string(REPLACE "/" ";" split_board_qualifiers "${BOARD_QUALIFIERS}")
   list(GET split_board_qualifiers 1 target_soc)
   set(board_target_flpr "${BOARD}/${target_soc}/cpuflpr")
   set(target_soc)
 
+  # Select the SDP application
+  if(SB_CONFIG_SDP_GPIO)
+    set(sdp_app_dir "${ZEPHYR_NRF_MODULE_DIR}/applications/sdp/gpio")
+  elseif(SB_CONFIG_SDP_MSPI)
+    set(sdp_app_dir "${ZEPHYR_NRF_MODULE_DIR}/applications/sdp/mspi")
+  else()
+    message(FATAL_ERROR "Unknown SDP application type")
+  endif()
+
+  # Include the SDP application in the build
   ExternalZephyrProject_Add(
     APPLICATION sdp
-    SOURCE_DIR ${ZEPHYR_NRF_MODULE_DIR}/applications/sdp/gpio
+    SOURCE_DIR ${sdp_app_dir}
     BOARD ${board_target_flpr}
     BOARD_REVISION ${BOARD_REVISION}
   )
+  set(sdp_app_dir)
+  set(board_target_flpr)
 endif()
