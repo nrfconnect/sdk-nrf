6275f3cde7164da9f229c82800c524ebe2d2f1d2
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2024-11-19 15:05:26 +0200

    nrf_security: drivers: integrate updated sxsymcrypt headers
    source: /sxsymcrypt-r20220504/include/sxsymcrypt/
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/sxsymcrypt/include/sxsymcrypt/sha1.h
+++ subsys/nrf_security/src/drivers/cracen/sxsymcrypt/include/sxsymcrypt/sha1.h
@@ -18,6 +18,21 @@ extern "C" {
 
 struct sxhash;
 
+#define SX_HASH_DIGESTSZ_SHA1 20
+#define SX_HASH_BLOCKSZ_SHA1 64
+
+
+#if SX_HASH_DIGESTSZ_MAX < SX_HASH_DIGESTSZ_SHA1
+#undef SX_HASH_DIGESTSZ_MAX
+#define SX_HASH_DIGESTSZ_MAX SX_HASH_DIGESTSZ_SHA1
+#endif
+
+
+#if SX_HASH_BLOCKSZ_MAX < SX_HASH_BLOCKSZ_SHA1
+#undef SX_HASH_BLOCKSZ_MAX
+#define SX_HASH_BLOCKSZ_MAX SX_HASH_BLOCKSZ_SHA1
+#endif
+
 /** Hash algorithm SHA-1 (Secure Hash Algorithm 1)
  *
  * Deprecated algorithm. NIST formally deprecated use of SHA-1 in 2011
