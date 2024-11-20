6b091d1b1aacd51d5c427c29fc4459b7f3295af1
Adrian Gielniewski <adrian.gielniewski@nordicsemi.no>
2024-11-20 10:47:35 +0100

    samples: matter: Update `Basic Information` cluster revision to 4
    Update `Basic Information` cluster revision to 4.
    
    Signed-off-by: Adrian Gielniewski <adrian.gielniewski@nordicsemi.no>

--- applications/matter_weather_station/src/default_zap/zap-generated/endpoint_config.h
+++ applications/matter_weather_station/src/default_zap/zap-generated/endpoint_config.h
@@ -153,7 +153,7 @@
 			  ZAP_ATTRIBUTE_MASK(EXTERNAL_STORAGE) | ZAP_ATTRIBUTE_MASK(SINGLETON) }, /* MaxPathsPerInvoke                   \
 												   */                                    \
 			{ ZAP_SIMPLE_DEFAULT(0), 0x0000FFFC, 4, ZAP_TYPE(BITMAP32), 0 }, /* FeatureMap */                                \
-			{ ZAP_SIMPLE_DEFAULT(3), 0x0000FFFD, 2, ZAP_TYPE(INT16U),                                                        \
+			{ ZAP_SIMPLE_DEFAULT(4), 0x0000FFFD, 2, ZAP_TYPE(INT16U),                                                        \
 			  ZAP_ATTRIBUTE_MASK(SINGLETON) }, /* ClusterRevision */                                                         \
                                                                                                                                          \
 			/* Endpoint: 0, Cluster: OTA Software Update Requestor (server) */                                               \
