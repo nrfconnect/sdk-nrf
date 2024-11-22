024c537ed69df07b6b5b850d8f1d009abed1ea4f
Andrzej Kuros <andrzej.kuros@nordicsemi.no>
2024-11-22 15:23:59 +0100

    samples: dtm: remove special handling of nrf21540ek on nrf5340
    Direct handling of pin forwarding for nrf21540ek on the nrf5340 in
    dtm sample is not necessary and is removed. The pin forwarding
    related to the pins used by the shield on the nrf5340 is handled
    by the shield code.
    
    Signed-off-by: Andrzej Kuros <andrzej.kuros@nordicsemi.no>

--- samples/bluetooth/direct_test_mode/sysbuild.cmake
+++ samples/bluetooth/direct_test_mode/sysbuild.cmake
@@ -3,15 +3,6 @@
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-# Include app core image if enabled
-if(SB_CONFIG_SOC_NRF5340_CPUNET AND DEFINED SB_CONFIG_APPCORE_IMAGE_NAME)
-  if("nrf21540ek" IN_LIST shield_list)
-    add_overlay_dts(
-      ${SB_CONFIG_APPCORE_IMAGE_NAME}
-      ${CMAKE_CURRENT_LIST_DIR}/sysbuild/remote_shell/nrf21540ek.overlay)
-  endif()
-endif()
-
 if(SB_CONFIG_DTM_NO_DFE)
   add_overlay_dts(
     direct_test_mode
