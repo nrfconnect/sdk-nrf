6bedc931ca016a996630bac7a090deedc76dd703
Sigurd Hellesvik <sigurd.hellesvik@nordicsemi.no>
2024-11-22 15:59:18 +0100

    tf-m: Add Attestation support for nRF54L15
    Add support for PSA Attestation to the nRF54L15. Includes noup changes
    to TF-M repository.
    
    Ref: NCSDK-22598
    Signed-off-by: Sigurd Hellesvik <sigurd.hellesvik@nordicsemi.no>

--- modules/trusted-firmware-m/tfm_boards/nrf54l15_cpuapp/config.cmake
+++ modules/trusted-firmware-m/tfm_boards/nrf54l15_cpuapp/config.cmake
@@ -13,3 +13,9 @@ include(${PLATFORM_PATH}/common/${NRF_SOC_VARIANT}/config.cmake)
 
 # Override PS_CRYPTO_KDF_ALG
 set(PS_CRYPTO_KDF_ALG                  PSA_ALG_SP800_108_COUNTER_CMAC CACHE STRING    "KDF Algorithm to use")
+
+set(CONFIG_NRFX_RRAMC                ON          CACHE BOOL      "Enable nrfx drivers for RRAMC")
+add_compile_definitions(CONFIG_NRFX_RRAMC)
+
+# Override attestation to sign message instead of hash, because CRACEN drivers can not sign a hash.
+set(ATTEST_SIGN_MESSAGE               ON         CACHE BOOL      "Sign message instead of hash")
