6147ddf0b6a3f6206b5cd4be445f3f5dc2841fb8
Damian Krolik <damian.krolik@nordicsemi.no>
2024-11-27 15:59:00 +0100

    samples: nrf_rpc: ps: fix shell commands after server restart
    When server restarts, clear the flag indicating that the CLI
    has already been initialized on the server, so that it is
    re-initialized before the next otCliInputLine().
    
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

---
+++ samples/nrf_rpc/protocols_serialization/client/src/ot_shell.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef OT_SHELL_H_
+#define OT_SHELL_H_
+
+void ot_shell_server_restarted(void);
+
+#endif
