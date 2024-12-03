70afed9ba4134e8fd299826aa780d09263412e83
Ivan Iushkov <ivan.iushkov@nordicsemi.no>
2024-12-03 16:41:03 +0100

    bluetooth: add Kconfig options to enable optional CS capabilities
    added 3 new options to allow developers to use step mode-3 and
    multiple antenna during CS procedure
    
    Signed-off-by: Ivan Iushkov <ivan.iushkov@nordicsemi.no>
    (cherry picked from commit 02f9716596f378d60b0d9290756a738eedd58cad)

--- subsys/bluetooth/controller/hci_driver.c
+++ subsys/bluetooth/controller/hci_driver.c
@@ -1225,6 +1225,19 @@ static int configure_memory_usage(void)
 	}
 #endif
 
+#if defined(CONFIG_BT_CTLR_SDC_CS_COUNT)
+	cfg.cs_cfg.max_antenna_paths_supported = CONFIG_BT_CTLR_SDC_CS_MAX_ANTENNA_PATHS;
+	cfg.cs_cfg.num_antennas_supported = CONFIG_BT_CTLR_SDC_CS_NUM_ANTENNAS;
+	cfg.cs_cfg.step_mode3_supported = IS_ENABLED(CONFIG_BT_CTLR_SDC_CS_STEP_MODE3);
+
+	required_memory = sdc_cfg_set(SDC_DEFAULT_RESOURCE_CFG_TAG,
+									SDC_CFG_TYPE_CS_CFG,
+									&cfg);
+	if (required_memory < 0) {
+		return required_memory;
+	}
+#endif
+
 	LOG_DBG("BT mempool size: %u, required: %u",
 	       sizeof(sdc_mempool), required_memory);
 
