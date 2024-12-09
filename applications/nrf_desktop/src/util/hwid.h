f2e3acec45222a7996301f43b1678bbcf9dd34a6
Pawel Dunaj <pawel.dunaj@nordicsemi.no>
2024-12-09 12:40:35 +0100

    applications: nrf_desktop: fix c++ compatibility in util headers
    Make util headers includable from a c++ code.
    
    Signed-off-by: Pawel Dunaj <pawel.dunaj@nordicsemi.no>

--- applications/nrf_desktop/src/util/hwid.h
+++ applications/nrf_desktop/src/util/hwid.h
@@ -7,8 +7,16 @@
 #ifndef _HWID_H_
 #define _HWID_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define HWID_LEN 8
 
 void hwid_get(uint8_t *buf, size_t buf_size);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _HWID_H_ */
