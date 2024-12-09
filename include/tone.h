e3b399d54e0894db749b3cf374dab2339831fe79
Graham Wacey <graham.wacey@nordicsemi.no>
2024-12-09 13:02:24 +0000

    lib: tone: Generate tones of differing sample and carrier widths
    OCT-3436
    
    Update the library to allow tones of 8-, 16-, 24- and 32-bit samples
    and 8-, 16 and 32-bit carrier widths to be generated.
    Twister test cases included.
    
    Signed-off-by: Graham Wacey <graham.wacey@nordicsemi.no>

--- include/tone.h
+++ include/tone.h
@@ -25,9 +25,12 @@
  * @brief               Generates one full pulse-code modulation (PCM) period of a tone with the
  *                      given parameters.
  *
+ * @note                The returned frequency of the tone is the quotient of the sample_freq_hz
+ *                      divided by the tone_freq_hz.
+ *
  * @param tone          User provided buffer. Must be large enough to hold
  *                      the generated PCM tone, depending on settings.
- * @param tone_size     Resulting tone size.
+ * @param tone_size     Resulting tone size in bytes.
  * @param tone_freq_hz  The desired tone frequency in the range [100..10000] Hz.
  * @param smpl_freq_hz  Sampling frequency.
  * @param amplitude     Amplitude in the range [0..1].
@@ -39,6 +42,28 @@
  */
 int tone_gen(int16_t *tone, size_t *tone_size, uint16_t tone_freq_hz, uint32_t smpl_freq_hz,
 	     float amplitude);
+
+/**
+ * @brief                 Generates one full pulse-code modulation (PCM) period of a tone with the
+ *                        given parameters.
+ *
+ * @note                  The returned frequency of the tone is the quotient of the sample_freq_hz
+ *                        divided by the tone_freq_hz.
+ * @note                  The routine returns the tone samples left aligned in the carrier.
+ *
+ * @param tone            User provided buffer. Must be large enough to hold
+ *                        the generated PCM tone, depending on settings.
+ * @param tone_size       Resulting tone size in bytes.
+ * @param tone_freq_hz    The desired tone frequency in the range [100..10000] Hz.
+ * @param sample_freq_hz  Sampling frequency.
+ * @param sample_bits     Number of bits to represent a sample (i.e. 8, 16, 24 or 32 bits).
+ * @param carrier_bits    Number of bits to carry a sample (i.e. 8, 16 or 32 bit).
+ * @param amplitude       Amplitude in the range [0..1].
+ *
+ * @return 0 if successful, error otherwise.
+ */
+int tone_gen_size(void *tone, size_t *tone_size, uint16_t tone_freq_hz, uint32_t sample_freq_hz,
+		  uint8_t sample_bits, uint8_t carrier_bits, float amplitude);
 
 /**
  * @}
