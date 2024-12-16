nrfconnect/sdk-nrf

commit 062d82d01135fdc23d60f895735efb7543f6bbe4

ext: curl: fix compilation with nRF92
Fixed compilation with nRF92.

Signed-off-by: Tommi Kangas <tommi.kangas@nordicsemi.no>

ext/curl/lib/url.h
@@ -43,7 +43,11 @@
 #if defined(CONFIG_NRF_CURL_INTEGRATION)
 /* In embedded this needs to be a lot of smaller */
 #define UPLOADBUFFER_DEFAULT (708)
+#if defined(CONFIG_SOC_SERIES_NRF92X)
+#define UPLOADBUFFER_MAX (DT_REG_ADDR(DT_NODELABEL(cpuapp_cpucell_ipc_shm_heap)))
+#else
 #define UPLOADBUFFER_MAX (CONFIG_NRF_MODEM_LIB_SHMEM_TX_SIZE)
+#endif
 #define UPLOADBUFFER_MIN (CURL_MAX_WRITE_SIZE)
 #else
 #define UPLOADBUFFER_DEFAULT 65536

commit 3cd81c7caa8fe84971f970b92325e7a8aede70e9

ext: curl: fix usage of incorrect macro
Corrected use of incorrect macro introduced in commit 062d82d.

Signed-off-by: Tommi Kangas <tommi.kangas@nordicsemi.no>

ext/curl/lib/url.h
@@ -44,7 +44,7 @@
 /* In embedded this needs to be a lot of smaller */
 #define UPLOADBUFFER_DEFAULT (708)
 #if defined(CONFIG_SOC_SERIES_NRF92X)
-#define UPLOADBUFFER_MAX (DT_REG_ADDR(DT_NODELABEL(cpuapp_cpucell_ipc_shm_heap)))
+#define UPLOADBUFFER_MAX (DT_REG_SIZE(DT_NODELABEL(cpuapp_cpucell_ipc_shm_heap)))
 #else
 #define UPLOADBUFFER_MAX (CONFIG_NRF_MODEM_LIB_SHMEM_TX_SIZE)
 #endif