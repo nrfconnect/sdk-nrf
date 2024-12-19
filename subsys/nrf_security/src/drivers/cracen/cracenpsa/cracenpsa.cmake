66dcd8a02bf009340ac7f7e98ec45430e06fb743
Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>
2024-12-19 15:59:55 +0100

    nrf_security: drivers: cracen: Remove sicrypto for ed25519
    Add support for Ed25519 and Ed25519ph in cracenpsa
    directly using silexpk/sxsymcrypt.
    This bypasses sicrypto, which saves on flash usage
    Remove sicrypto implementation of Ed25519 from
    cracenpsa.
    
    Signed-off-by: Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/cracenpsa.cmake
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/cracenpsa.cmake
@@ -44,6 +44,7 @@ endif()
 if(CONFIG_PSA_NEED_CRACEN_ASYMMETRIC_SIGNATURE_DRIVER)
   list(APPEND cracen_driver_sources
     ${CMAKE_CURRENT_LIST_DIR}/src/sign.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/ed25519.c
   )
 endif()
 
@@ -61,6 +62,7 @@ endif()
 
 if(CONFIG_PSA_NEED_CRACEN_KEY_MANAGEMENT_DRIVER OR CONFIG_PSA_NEED_CRACEN_KMU_DRIVER OR CONFIG_MBEDTLS_PSA_CRYPTO_BUILTIN_KEYS)
   list(APPEND cracen_driver_sources
+    ${CMAKE_CURRENT_LIST_DIR}/src/ed25519.c
     ${CMAKE_CURRENT_LIST_DIR}/src/key_management.c
   )
 endif()
