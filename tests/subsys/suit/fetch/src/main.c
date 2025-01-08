da79f1ca142dde5df223bad161a7e367153c73df
Michal Kozikowski <michal.kozikowski@nordicsemi.no>
2025-01-08 14:32:13 +0100

    tests: Extend SUIT tests for fetch, copy and write with decryption
    This commit extends integration tests for fetch, copy and write
    SUIT directives with test cases for payload decryption.
    Common test module decrypt_test_utils.c was added with default
    constants and utilities for decryption tests to use.
    Refactor of decrypt_filter/src/main.c was made because of this
    common module.
    
    Ref: NCSDK-31276
    
    Signed-off-by: Michal Kozikowski <michal.kozikowski@nordicsemi.no>

--- tests/subsys/suit/fetch/src/main.c
+++ tests/subsys/suit/fetch/src/main.c
@@ -11,6 +11,9 @@
 #include <suit_ipuc_sdfw.h>
 #include <suit_dfu_cache.h>
 #include <mocks_sdfw.h>
+#include <psa/crypto.h>
+#include <suit_decrypt_filter.h>
+#include <decrypt_test_utils.h>
 
 #define TEST_DATA_SIZE 64
 #define WRITE_ADDR     0x1A00080000
@@ -57,9 +60,21 @@ static const uint8_t cache2[] = {
 	0x90, 0x70, 0x18, 0x92, 0x36, 0x51, 0x92, 0x83, 0x09, 0x86, 0x70, 0x19, 0x23};
 static const size_t cache2_len = sizeof(cache2);
 
+/* clang-format off */
+static const uint8_t valid_manifest_component[] = {
+	0x82, /* array: 2 elements */
+		0x4c, /* byte string: 12 bytes */
+			0x6b, /* string: 11 characters */
+				'I', 'N', 'S', 'T', 'L', 'D', '_', 'M', 'F', 'S', 'T',
+		0x50, /* byte string: 16 bytes */
+			0x5b, 0x46, 0x9f, 0xd1, 0x90, 0xee, 0x53, 0x9c,
+			0xa3, 0x18, 0x68, 0x1b, 0x03, 0x69, 0x5e, 0x36
+};
+/* clang-format on */
+
 static struct zcbor_string valid_manifest_component_id = {
-	.value = (const uint8_t *)0x1234,
-	.len = 123,
+	.value = valid_manifest_component,
+	.len = sizeof(valid_manifest_component),
 };
 
 static void setup_cache_with_sample_entries(void)
@@ -340,4 +355,208 @@ ZTEST(fetch_tests, test_integrated_fetch_to_memptr_NOK_handle_NULL)
 
 	ret = suit_plat_release_component_handle(component_handle);
 	zassert_equal(ret, SUIT_SUCCESS, "Handle release failed - error %i", ret);
+}
+
+ZTEST(fetch_tests, test_integrated_fetch_to_msink_encrypted_OK)
+{
+	suit_component_t component_handle;
+	memptr_storage_handle_t handle = NULL;
+	struct zcbor_string source = {
+		.value = decrypt_test_ciphertext_direct,
+		.len = sizeof(decrypt_test_ciphertext_direct)
+	};
+
+	/* [h'MEM', h'02', h'1A00080000', h'191000'] */
+	uint8_t valid_value[] = {0x84, 0x44, 0x63, 'M',  'E',  'M',	 0x41, 0x02, 0x45,
+				 0x1A, 0x00, 0x08, 0x00, 0x00, 0x43, 0x19, 0x10, 0x00};
+
+	struct zcbor_string valid_component_id = {
+		.value = valid_value,
+		.len = sizeof(valid_value),
+	};
+
+	psa_key_id_t cek_key_id;
+	uint8_t cek_key_id_cbor[] = {
+		0x1A, 0x00, 0x00, 0x00, 0x00,
+	};
+
+	psa_status_t status = psa_crypto_init();
+
+	zassert_equal(status, PSA_SUCCESS, "Failed to init psa crypto");
+
+	status = decrypt_test_init_encryption_key(decrypt_test_key_data,
+				sizeof(decrypt_test_key_data), &cek_key_id,
+				PSA_ALG_GCM, cek_key_id_cbor);
+	zassert_equal(status, PSA_SUCCESS, "Failed to import key");
+
+	struct suit_encryption_info enc_info =
+				DECRYPT_TEST_ENC_INFO_DEFAULT_INIT(cek_key_id_cbor);
+
+	int ret = suit_plat_create_component_handle(&valid_component_id, false, &component_handle);
+
+	zassert_equal(ret, SUIT_SUCCESS, "create_component_handle failed - error %i", ret);
+
+	ret = suit_plat_fetch_integrated(component_handle, &source,
+						&valid_manifest_component_id, &enc_info);
+	zassert_equal(ret, SUIT_SUCCESS, "suit_plat_fetch failed - error %i", ret);
+
+	ret = suit_plat_component_impl_data_get(component_handle, &handle);
+	zassert_equal(ret, SUIT_SUCCESS, "suit_plat_component_impl_data_get failed - error %i",
+		      ret);
+
+	const uint8_t *payload;
+	size_t payload_size = 0;
+
+
+	ret = suit_memptr_storage_ptr_get(handle, &payload, &payload_size);
+
+	zassert_equal(ret, SUIT_PLAT_SUCCESS, "storage.get failed - error %i", ret);
+	zassert_equal(memcmp(decrypt_test_plaintext, payload,
+					strlen(decrypt_test_plaintext)), 0,
+			"Retrieved decrypted payload doesn't mach decrypt_test_plaintext");
+	zassert_equal(sizeof(decrypt_test_plaintext), payload_size,
+			"Retrieved payload_size doesn't mach size of decrypt_test_plaintext");
+
+	ret = suit_plat_release_component_handle(component_handle);
+	zassert_equal(ret, SUIT_SUCCESS, "Handle release failed - error %i", ret);
+
+	psa_destroy_key(cek_key_id);
+}
+
+ZTEST(fetch_tests, test_integrated_fetch_to_memptr_encrypted_NOK)
+{
+	suit_component_t component_handle;
+	memptr_storage_handle_t handle = NULL;
+	struct zcbor_string source = {
+		.value = decrypt_test_ciphertext_direct,
+		.len = sizeof(decrypt_test_ciphertext_direct)
+	};
+
+	/* [h'CAND_IMG', h'02'] */
+	uint8_t valid_value[] = {0x82, 0x49, 0x68, 'C', 'A',  'N', 'D',
+				 '_',  'I',  'M',  'G', 0x41, 0x02};
+
+	struct zcbor_string valid_component_id = {
+		.value = valid_value,
+		.len = sizeof(valid_value),
+	};
+
+	psa_key_id_t cek_key_id;
+	uint8_t cek_key_id_cbor[] = {
+		0x1A, 0x00, 0x00, 0x00, 0x00,
+	};
+
+	psa_status_t status = psa_crypto_init();
+
+	zassert_equal(status, PSA_SUCCESS, "Failed to init psa crypto");
+
+	status = decrypt_test_init_encryption_key(decrypt_test_key_data,
+				sizeof(decrypt_test_key_data), &cek_key_id,
+				PSA_ALG_GCM, cek_key_id_cbor);
+	zassert_equal(status, PSA_SUCCESS, "Failed to import key");
+
+	struct suit_encryption_info enc_info =
+				DECRYPT_TEST_ENC_INFO_DEFAULT_INIT(cek_key_id_cbor);
+
+	int ret = suit_plat_create_component_handle(&valid_component_id, false, &component_handle);
+
+	zassert_equal(ret, SUIT_SUCCESS, "create_component_handle failed - error %i", ret);
+
+	ret = suit_plat_fetch_integrated(component_handle, &source,
+						&valid_manifest_component_id, &enc_info);
+	zassert_equal(ret, SUIT_SUCCESS, "suit_plat_fetch failed - error %i", ret);
+
+	ret = suit_plat_component_impl_data_get(component_handle, &handle);
+	zassert_equal(ret, SUIT_SUCCESS, "suit_plat_component_impl_data_get failed - error %i",
+		      ret);
+
+	const uint8_t *payload;
+	size_t payload_size = 0;
+
+
+	ret = suit_memptr_storage_ptr_get(handle, &payload, &payload_size);
+
+	zassert_equal(ret, SUIT_PLAT_SUCCESS, "storage.get failed - error %i", ret);
+	zassert_not_equal(memcmp(decrypt_test_plaintext, payload,
+						strlen(decrypt_test_plaintext)), 0,
+			"Retrieved decrypted payload should not mach decrypt_test_plaintext");
+	zassert_equal(sizeof(decrypt_test_plaintext), payload_size,
+			"Retrieved payload_size doesn't mach size of decrypt_test_plaintext");
+
+	ret = suit_plat_release_component_handle(component_handle);
+	zassert_equal(ret, SUIT_SUCCESS, "Handle release failed - error %i", ret);
+
+	psa_destroy_key(cek_key_id);
+}
+
+ZTEST(fetch_tests, test_integrated_fetch_to_msink_encrypted_wrong_key_NOK)
+{
+	suit_component_t component_handle;
+	memptr_storage_handle_t handle = NULL;
+	struct zcbor_string source = {
+		.value = decrypt_test_ciphertext_direct,
+		.len = sizeof(decrypt_test_ciphertext_direct)
+	};
+
+	/* [h'MEM', h'02', h'1A00080000', h'191000'] */
+	uint8_t valid_value[] = {0x84, 0x44, 0x63, 'M',  'E',  'M',	 0x41, 0x02, 0x45,
+				     0x1A, 0x00, 0x08, 0x00, 0x00, 0x43, 0x19, 0x10, 0x00};
+
+	struct zcbor_string valid_component_id = {
+		.value = valid_value,
+		.len = sizeof(valid_value),
+	};
+
+	psa_key_id_t cek_key_id;
+	uint8_t cek_key_id_cbor[] = {
+		0x1A, 0x00, 0x00, 0x00, 0x00,
+	};
+
+	psa_status_t status = psa_crypto_init();
+
+	zassert_equal(status, PSA_SUCCESS, "Failed to init psa crypto");
+
+	uint8_t wrong_test_key_data[sizeof(decrypt_test_key_data)];
+
+	memcpy(wrong_test_key_data, decrypt_test_key_data, sizeof(wrong_test_key_data));
+	/* Corrupt CEK that we store.*/
+	wrong_test_key_data[10] = 0x00;
+
+	status = decrypt_test_init_encryption_key(wrong_test_key_data,
+				sizeof(wrong_test_key_data), &cek_key_id,
+				PSA_ALG_GCM, cek_key_id_cbor);
+	zassert_equal(status, PSA_SUCCESS, "Failed to import key");
+
+	struct suit_encryption_info enc_info =
+				DECRYPT_TEST_ENC_INFO_DEFAULT_INIT(cek_key_id_cbor);
+
+	int ret = suit_plat_create_component_handle(&valid_component_id, false, &component_handle);
+
+	zassert_equal(ret, SUIT_SUCCESS, "create_component_handle failed - error %i", ret);
+
+	ret = suit_plat_fetch_integrated(component_handle, &source,
+						&valid_manifest_component_id, &enc_info);
+	zassert_equal(ret, SUIT_ERR_AUTHENTICATION,
+			"suit_plat_fetch should have failed with SUIT_ERR_AUTHENTICATION");
+
+	ret = suit_plat_component_impl_data_get(component_handle, &handle);
+	zassert_equal(ret, SUIT_SUCCESS, "suit_plat_component_impl_data_get failed - error %i",
+		      ret);
+
+	const uint8_t *payload;
+	size_t payload_size = 0;
+
+	ret = suit_memptr_storage_ptr_get(handle, &payload, &payload_size);
+
+	zassert_equal(ret, SUIT_PLAT_SUCCESS, "storage.get failed - error %i", ret);
+	zassert_not_equal(memcmp(decrypt_test_plaintext, payload,
+						strlen(decrypt_test_plaintext)), 0,
+			"Retrieved decrypted payload should not mach decrypt_test_plaintext");
+	zassert_equal(payload_size, 0,
+			"Retrieved payload_size is not 0");
+
+	ret = suit_plat_release_component_handle(component_handle);
+	zassert_equal(ret, SUIT_SUCCESS, "Handle release failed - error %i", ret);
+
+	psa_destroy_key(cek_key_id);
 }
