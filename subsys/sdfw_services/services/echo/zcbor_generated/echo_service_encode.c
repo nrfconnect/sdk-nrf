48d68a36d3468b04c0324eccffd8b191fe4fc666
Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>
2025-01-08 17:14:26 +0100

    sdfw_services: regenerate zcbor encoders/decoders with current version
    Regenerate code using the current zcbor version.
    
    Signed-off-by: Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>

--- subsys/sdfw_services/services/echo/zcbor_generated/echo_service_encode.c
+++ subsys/sdfw_services/services/echo/zcbor_generated/echo_service_encode.c
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Nordic Semiconductor ASA
+ * Copyright (c) 2025 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
 /*
- * Generated using zcbor version 0.8.1
+ * Generated using zcbor version 0.9.0
  * https://github.com/NordicSemiconductor/zcbor
  * Generated with a --default-max-qty of 3
  */
@@ -22,6 +22,17 @@
 #error "The type file was generated with a different default_max_qty than this file"
 #endif
 
+#define log_result(state, result, func)                                                            \
+	do {                                                                                       \
+		if (!result) {                                                                     \
+			zcbor_trace_file(state);                                                   \
+			zcbor_log("%s error: %s\r\n", func,                                        \
+				  zcbor_error_str(zcbor_peek_error(state)));                       \
+		} else {                                                                           \
+			zcbor_log("%s success\r\n", func);                                         \
+		}                                                                                  \
+	} while (0)
+
 static bool encode_echo_service_rsp(zcbor_state_t *state, const struct echo_service_rsp *input);
 static bool encode_echo_service_req(zcbor_state_t *state, const struct zcbor_string *input);
 
@@ -29,40 +40,27 @@ static bool encode_echo_service_rsp(zcbor_state_t *state, const struct echo_serv
 {
 	zcbor_log("%s\r\n", __func__);
 
-	bool tmp_result =
-		(((zcbor_list_start_encode(state, 2) &&
-		   ((((zcbor_int32_encode(state, (&(*input).echo_service_rsp_ret)))) &&
-		     ((zcbor_tstr_encode(state, (&(*input).echo_service_rsp_str_out))))) ||
-		    (zcbor_list_map_end_force_encode(state), false)) &&
-		   zcbor_list_end_encode(state, 2))));
+	bool res = (((zcbor_list_start_encode(state, 2) &&
+		      ((((zcbor_int32_encode(state, (&(*input).echo_service_rsp_ret)))) &&
+			((zcbor_tstr_encode(state, (&(*input).echo_service_rsp_str_out))))) ||
+		       (zcbor_list_map_end_force_encode(state), false)) &&
+		      zcbor_list_end_encode(state, 2))));
 
-	if (!tmp_result) {
-		zcbor_trace_file(state);
-		zcbor_log("%s error: %s\r\n", __func__, zcbor_error_str(zcbor_peek_error(state)));
-	} else {
-		zcbor_log("%s success\r\n", __func__);
-	}
-
-	return tmp_result;
+	log_result(state, res, __func__);
+	return res;
 }
 
 static bool encode_echo_service_req(zcbor_state_t *state, const struct zcbor_string *input)
 {
 	zcbor_log("%s\r\n", __func__);
 
-	bool tmp_result = (((zcbor_list_start_encode(state, 1) &&
-			     ((((zcbor_tstr_encode(state, (&(*input)))))) ||
-			      (zcbor_list_map_end_force_encode(state), false)) &&
-			     zcbor_list_end_encode(state, 1))));
+	bool res = (((zcbor_list_start_encode(state, 1) &&
+		      ((((zcbor_tstr_encode(state, (&(*input)))))) ||
+		       (zcbor_list_map_end_force_encode(state), false)) &&
+		      zcbor_list_end_encode(state, 1))));
 
-	if (!tmp_result) {
-		zcbor_trace_file(state);
-		zcbor_log("%s error: %s\r\n", __func__, zcbor_error_str(zcbor_peek_error(state)));
-	} else {
-		zcbor_log("%s success\r\n", __func__);
-	}
-
-	return tmp_result;
+	log_result(state, res, __func__);
+	return res;
 }
 
 int cbor_encode_echo_service_req(uint8_t *payload, size_t payload_len,
