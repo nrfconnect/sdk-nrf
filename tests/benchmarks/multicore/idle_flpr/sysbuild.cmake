cb16172bae5dcb13198deaa33035d7e578284bdb
Sebastian Głąb <sebastian.glab@nordicsemi.no>
2025-01-13 12:58:28 +0100

    tests: benchmarks: multicore: Add PM tests with FLPR core
    Add test that confirms Power Management on FLPR core.
    
    Signed-off-by: Sebastian Głąb <sebastian.glab@nordicsemi.no>

---
+++ tests/benchmarks/multicore/idle_flpr/sysbuild.cmake
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2024 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+#
+
+ExternalZephyrProject_Add(
+  APPLICATION remote_flpr
+  SOURCE_DIR  ${APP_DIR}/remote
+  BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpuflpr
+  BOARD_REVISION ${BOARD_REVISION}
+)
+
+ExternalZephyrProject_Add(
+  APPLICATION remote_rad
+  SOURCE_DIR ${SYSBUILD_NRF_MODULE_DIR}/tests/benchmarks/power_consumption/common/remote_sleep_forever
+  BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpurad
+  BOARD_REVISION ${BOARD_REVISION}
+)
