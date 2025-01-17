fa8dd00db7a36dec2fc2b155ae04d4d56bfaccc3
Andrey Smirnov <andrew.smirnov@gmail.com>
2025-01-17 11:16:56 -0800

    hids: Replace repeating code in bt_hids_inp_rep_send()
    There's already rep_attr that contains the value of
    &hids_obj->gp.svc.attrs[hids_inp_rep->att_ind] so let's re-use that.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

--- subsys/bluetooth/services/hids.c
+++ subsys/bluetooth/services/hids.c
@@ -1152,7 +1152,7 @@ int bt_hids_inp_rep_send_userdata(struct bt_hids *hids_obj,
 
 	struct bt_gatt_notify_params params = {0};
 
-	params.attr = &hids_obj->gp.svc.attrs[hids_inp_rep->att_ind];
+	params.attr = rep_attr;
 	params.data = rep;
 	params.len = hids_inp_rep->size;
 	params.func = cb;
