439d064a73bfc61e3bb5b9cd82e3e2dabacec392
Bartosz Miller <bartosz.miller@nordicsemi.no>
2025-01-22 14:53:32 +0100

    tests: benchmarks: Add UARTE suspend test with current measurement
    PM suspend have not been tested with power measurement so far.
    
    Signed-off-by: Bartosz Miller <bartosz.miller@nordicsemi.no>

---
+++ tests/benchmarks/current_consumption/uarte_suspend/sysbuild.cmake
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2025 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+#
+
+if(SB_CONFIG_SOC_NRF54H20)
+  # Add remote project
+  ExternalZephyrProject_Add(
+      APPLICATION remote
+      SOURCE_DIR ${ZEPHYR_NRF_MODULE_DIR}/tests/benchmarks/power_consumption/common/remote_sleep_forever
+      BOARD ${SB_CONFIG_REMOTE_BOARD}
+      BOARD_REVISION ${BOARD_REVISION}
+    )
+  # Add a dependency so that the remote image will be built and flashed first
+  add_dependencies(uarte_suspend remote)
+  # Add dependency so that the remote image is flashed first.
+  sysbuild_add_dependencies(FLASH uarte_suspend remote)
+endif()
