ee9504f44d48348b5f93be00f28e16bcb02b3cec
Dominik Ermel <dominik.ermel@nordicsemi.no>
2025-01-24 15:16:49 +0000

    tests: nrf_compress: Make space for large MCUboot
    Increases size of MCUboot by taking away one page of primary
    image. Changes signing CMake to properly change address of a
    binary.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- tests/subsys/nrf_compress/decompression/mcuboot_update/modified_signing.cmake
+++ tests/subsys/nrf_compress/decompression/mcuboot_update/modified_signing.cmake
@@ -12,6 +12,6 @@ set(BYPRODUCT_KERNEL_SIGNED_HEX_NAME "${output}"
     CACHE FILEPATH "Signed kernel hex file" FORCE
     )
 set_property(GLOBAL APPEND PROPERTY extra_post_build_commands COMMAND
-             ${CMAKE_OBJCOPY} -I ihex -O ihex --change-addresses 0x92000
+             ${CMAKE_OBJCOPY} -I ihex -O ihex --change-addresses 0x91000
              ${ZEPHYR_BINARY_DIR}/${KERNEL_NAME}.signed.hex ${output})
 set_property(GLOBAL APPEND PROPERTY extra_post_build_byproducts ${output})
