f04a383ed53cfb4201671f550aaac49962067ca1
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2025-01-27 09:41:28 -0500

    manifest: Update Memfault SDK to 1.22.0
    In Memfault SDK 1.22.0, the config
    `MEMFAULT_METRICS_HEARTBEAT_INTERVAL_SECS` set via
    `memfault_platform_config.h` was replaced with the Kconfig option
    `CONFIG_MEMFAULT_METRICS_HEARTBEAT_INTERVAL_SECS`.
    
    This commit updates the SDK version and:
     - Removes debug module Kconfig in Asset Tracker v2 for the hearbeat
       interval, this is unnecessary wrapping now that the Memfault Zephyr
       module has a Kconfig
     - Removes references to the old config
     - Adds example usage of
       `CONFIG_MEMFAULT_METRICS_HEARTBEAT_INTERVAL_SECS`
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

--- samples/debug/memfault/config/memfault_platform_config.h
+++ samples/debug/memfault/config/memfault_platform_config.h
@@ -11,11 +11,3 @@
  * memfault-firmware-sdk. Default configuration settings can be found in
  * "<NCS folder>/modules/lib/memfault-firmware-sdk/components/include/memfault/default_config.h"
  */
-
-/* Uncomment the definition below to override the default setting for
- * heartbeat interval. This will prepare the captured metric data for upload
- * to Memfault cloud at the specified interval.
- */
-/*
- * #define MEMFAULT_METRICS_HEARTBEAT_INTERVAL_SECS 1800
- */
