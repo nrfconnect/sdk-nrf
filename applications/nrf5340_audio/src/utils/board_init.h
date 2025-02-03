a50c58cc90332e84d0ae72d69e38894052e131d6
Graham Wacey <graham.wacey@nordicsemi.no>
2025-02-03 13:30:46 +0000

    applications: nrf5340_audio: LED module for kits with other LED setup
    	The LED module is currently written in a way
    	that’s highly dependent on all of the LEDs present
    	on the audio DK, which prevents the application from
    	running on the nRF5340 DK. The module must be refactored
    	so that it can still show the state of the application with
    	a different selection of LEDs.
    
    Signed-off-by: Graham Wacey <graham.wacey@nordicsemi.no>

---
+++ applications/nrf5340_audio/src/utils/board_init.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+/**
+ * @brief	Initialize the hardware related modules on the nRF board.
+ *
+ * @return	0 if successful, error otherwise.
+ */
+int board_init(void);
+
+#endif /* _BOARD_H_ */
