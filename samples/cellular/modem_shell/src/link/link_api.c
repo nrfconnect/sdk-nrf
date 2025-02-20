nrfconnect/sdk-nrf

commit 8caef9de8300989f034575a3b7fe9a52c631a2b4

samples: cellular: modem_shell: Fix errors when printing link status
When registration had already been dropped, a couple of errors
were shown when trying to print registration and PDN status.
AT response parsing has been improved to handle also this
situation.

MOSH-640

Signed-off-by: Tommi Kangas <tommi.kangas@nordicsemi.no>

samples/cellular/modem_shell/src/link/link_api.c
@@ -227,6 +227,7 @@ int link_api_xmonitor_read(struct lte_xmonitor_resp_t *resp)
 {
 	int ret = 0;
 	int len;
+	uint32_t reg_status;
 	char tmp_cell_id_str[OP_CELL_ID_STR_MAX_LEN + 1] = { 0 };
 	char tmp_tac_str[OP_TAC_STR_MAX_LEN + 1] = { 0 };
 
@@ -241,9 +242,12 @@ int link_api_xmonitor_read(struct lte_xmonitor_resp_t *resp)
 	resp->cell_id = -1;
 	resp->tac = -1;
 
+	/* reg_status is not used but can not be ignored, otherwise the function returns an error
+	 * for a notification which only contains the registration status.
+	 */
 	ret = nrf_modem_at_scanf("AT%XMONITOR",
 				 "%%XMONITOR: "
-				 "%*u,"                                /* <reg_status>: ignored */
+				 "%u,"                                 /* <reg_status> */
 				 "%"L(OP_FULL_NAME_STR_MAX_LEN)"[^,]," /* <full_name> with quotes */
 				 "%"L(OP_SHORT_NAME_STR_MAX_LEN)"[^,],"/* <short_name> with quotes*/
 				 "%"L(OP_PLMN_STR_MAX_LEN)"[^,],"      /* <plmn> */
@@ -255,6 +259,7 @@ int link_api_xmonitor_read(struct lte_xmonitor_resp_t *resp)
 				 "%*u,"                                /* <EARFCN>: ignored */
 				 "%d,"                                 /* <rsrp> */
 				 "%d",                                 /* <snr> */
+					&reg_status,
 					resp->full_name_str,
 					resp->short_name_str,
 					resp->plmn_str,
@@ -403,6 +408,10 @@ int link_api_pdp_contexts_read(struct pdp_context_info_array *pdp_info)
 	/* Allocate array of PDP info accordingly */
 	pdp_info->array = calloc(pdp_cnt, sizeof(struct pdp_context_info));
 	pdp_info->size = pdp_cnt;
+
+	if (pdp_cnt == 0) {
+		goto clean_exit;
+	}
 
 	/* Parse the response */
 	ret = at_parser_init(&parser, at_ptr);

samples/cellular/modem_shell/src/link/link.c
@@ -183,7 +183,7 @@ static void link_registered_work(struct k_work *unused)
 	/* PDN activation may take some time. Thus, let modem have some time
 	 * before reading the PDN information.
 	 */
-	k_sleep(K_MSEC(2000));
+	k_sleep(K_MSEC(1500));
 
 	link_api_modem_info_get_for_shell(true);
 }

commit cad600a861cad80a29d1549e6ed7b1e61a81d366

samples: cellular: modem_shell: Fix LTE cell printing during search
Commit 8caef9d caused regression when printing LTE cell information
during network search. This PR reverts the original functionality in
link_api_xmonitor_read() and instead removes an error log which
was printed when link_api_xmonitor_read() returned an error. It's
simpler to let link_api_xmonitor_read() fail if cell ID and TAC
can not be parsed.

Signed-off-by: Tommi Kangas <tommi.kangas@nordicsemi.no>

samples/cellular/modem_shell/src/link/link_api.c
@@ -227,7 +227,6 @@ int link_api_xmonitor_read(struct lte_xmonitor_resp_t *resp)
 {
 	int ret = 0;
 	int len;
-	uint32_t reg_status;
 	char tmp_cell_id_str[OP_CELL_ID_STR_MAX_LEN + 1] = { 0 };
 	char tmp_tac_str[OP_TAC_STR_MAX_LEN + 1] = { 0 };
 
@@ -242,12 +241,9 @@ int link_api_xmonitor_read(struct lte_xmonitor_resp_t *resp)
 	resp->cell_id = -1;
 	resp->tac = -1;
 
-	/* reg_status is not used but can not be ignored, otherwise the function returns an error
-	 * for a notification which only contains the registration status.
-	 */
 	ret = nrf_modem_at_scanf("AT%XMONITOR",
 				 "%%XMONITOR: "
-				 "%u,"                                 /* <reg_status> */
+				 "%*u,"                                /* <reg_status>: ignored */
 				 "%"L(OP_FULL_NAME_STR_MAX_LEN)"[^,]," /* <full_name> with quotes */
 				 "%"L(OP_SHORT_NAME_STR_MAX_LEN)"[^,],"/* <short_name> with quotes*/
 				 "%"L(OP_PLMN_STR_MAX_LEN)"[^,],"      /* <plmn> */
@@ -259,7 +255,6 @@ int link_api_xmonitor_read(struct lte_xmonitor_resp_t *resp)
 				 "%*u,"                                /* <EARFCN>: ignored */
 				 "%d,"                                 /* <rsrp> */
 				 "%d",                                 /* <snr> */
-					&reg_status,
 					resp->full_name_str,
 					resp->short_name_str,
 					resp->plmn_str,
@@ -300,7 +295,9 @@ static void link_api_modem_operator_info_read_for_shell(void)
 	int ret = link_api_xmonitor_read(&xmonitor_resp);
 
 	if (ret) {
-		mosh_error("link_api_xmonitor_read failed, result: ret %d", ret);
+		/* Reading the information fails if modem is no longer registered, but
+		 * don't want to print an error in this case.
+		 */
 		return;
 	}
 