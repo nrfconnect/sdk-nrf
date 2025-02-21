77211bd963ffff99af28978bdd49edfe8438c9df
Marek Pieta <Marek.Pieta@nordicsemi.no>
2025-02-21 15:10:58 +0100

    scripts: nrf_profiler: Improve stats calculating script
    Clean up the script implementation. Allow defining presets using a
    *.json file. Allow to filter outliers to improve UX for histogram plot.
    
    Jira: NCSDK-31712
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- scripts/nrf_profiler/calc_stats.py
+++ scripts/nrf_profiler/calc_stats.py
@@ -7,15 +7,19 @@ from stats_nordic import StatsNordic
 
 import argparse
 import logging
+import json
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Calculating stats for events.',
+    parser = argparse.ArgumentParser(description="nRF Profiler event propagation statistics",
                                      allow_abbrev=False)
-    parser.add_argument('dataset_name', help='Name of dataset')
-    parser.add_argument('--start_time', help='Measurement start time[s]')
-    parser.add_argument('--end_time', help='Measurement end time[s]')
-    parser.add_argument('--log', help='Log level')
+    parser.add_argument("dataset_name", help="Name of nRF Profiler dataset")
+    parser.add_argument("test_presets", help="Test preset [*.json file]")
+    parser.add_argument("--start_time", type=float, default=0.0,
+                        help="Measurement start time [s]")
+    parser.add_argument("--end_time", type=float, default=float('inf'),
+                        help="Measurement end time [s]")
+    parser.add_argument("--log", help="Log level")
     args = parser.parse_args()
 
     if args.log is not None:
@@ -23,19 +27,20 @@ def main():
     else:
         log_lvl_number = logging.INFO
 
-    if args.start_time is None:
-        args.start_time = 0
-    else:
-        args.start_time = float(args.start_time)
+    try:
+        with open(args.test_presets, "r") as test_preset_file:
+            test_preset_dict = json.load(test_preset_file)
+    except FileNotFoundError:
+        print("File {} not found".format(args.test_presets))
+        return
+    except Exception as e:
+        print("Exception while opening {}".format(args.test_presets))
+        print(e)
+        return
 
-    if args.end_time is None:
-        args.end_time = float('inf')
-    else:
-        args.end_time = float(args.end_time)
-
-    sn = StatsNordic(args.dataset_name + ".csv", args.dataset_name + ".json",
+    sn = StatsNordic("{}.csv".format(args.dataset_name), "{}.json".format(args.dataset_name),
                      log_lvl_number)
-    sn.calculate_stats_preset1(args.start_time, args.end_time)
+    sn.calculate_stats(test_preset_dict, args.start_time, args.end_time)
 
 if __name__ == "__main__":
     main()
