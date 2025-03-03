1fa6e41615278574ad19db3412dd2d3a43696dac
Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>
2025-03-03 14:59:24 +0100

    sysbuild: fast_pair: add Model ID and Anti-Spoofing Key Kconfig options
    Replaced the SB_CONFIG_BT_FAST_PAIR sysbuild Kconfig option with the
    following Kconfig options:
    
    * SB_CONFIG_BT_FAST_PAIR_MODEL_ID
    * SB_CONFIG_BT_FAST_PAIR_ANTI_SPOOFING_PRIVATE_KEY
    
    If both options are populated, the sysbuild sets the promptless
    SB_CONFIG_BT_FAST_PAIR_PROV_DATA Kconfig option. This Kconfig option
    triggers generation of the Fast Pair provisioning data hex, which
    is merged with the final hex used during firmware upload (merged.hex).
    
    This change limits the sysbuild scope for the Fast Pair category.
    Sysbuild can only be used to manage the Fast Pair provisioning data
    New options do not enable the overall Fast Pair subsystem anymore
    in the default image like the old SB_CONFIG_BT_FAST_PAIR Kconfig did.
    
    Improved validation of the Fast Pair partition definition.
    
    Aligned all Fast Pair projects to follow the new scheme.
    
    Ref: NCSDK-32057
    
    Signed-off-by: Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>

--- samples/bluetooth/fast_pair/locator_tag/sysbuild.cmake
+++ samples/bluetooth/fast_pair/locator_tag/sysbuild.cmake
@@ -1,21 +1,8 @@
 #
-# Copyright (c) 2024 Nordic Semiconductor
+# Copyright (c) 2024-2025 Nordic Semiconductor
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
-
-if(NOT DEFINED FP_MODEL_ID AND NOT DEFINED FP_ANTI_SPOOFING_KEY)
-  message(WARNING "
-  -------------------------------------------------------
-  --- WARNING: Using demo Fast Pair Model ID and Fast ---
-  --- Pair Anti Spoofing Key, it should not be used   ---
-  --- for production.                                 ---
-  -------------------------------------------------------
-  \n"
-  )
-  set(FP_MODEL_ID "0x4A436B" PARENT_SCOPE)
-  set(FP_ANTI_SPOOFING_KEY "rie10A7ONqwd77VmkxGsblPUbMt384qjDgcEJ/ctT9Y=" PARENT_SCOPE)
-endif()
 
 if(SB_CONFIG_APP_DFU)
   set_config_bool(${DEFAULT_IMAGE} CONFIG_APP_DFU y)
