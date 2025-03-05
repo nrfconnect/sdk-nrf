939bf1a61c0b01c0de94198d7b48cafea216a7e0
Andrzej Kuros <andrzej.kuros@nordicsemi.no>
2025-03-05 17:13:35 +0100

    mpsl: fem: nrf2220: add temperature compensation
    For the nRF2220 the temperature compensation feature is added.
    It allows to achieve better output power accuracy among the whole
    supported temperature range.
    
    The feature is enabled by the MPSL_FEM_NRF2220_TEMPERATURE_COMPENSATION
    Kconfig option. It relies on functions provided by MPSL in the
    sdk-nrfxlib.
    The temperature source can be taken from the SoC or can be set
    by custom implementation.
    
    Signed-off-by: Andrzej Kuros <andrzej.kuros@nordicsemi.no>

--- subsys/mpsl/fem/common/mpsl_fem_twi_drv.c
+++ subsys/mpsl/fem/common/mpsl_fem_twi_drv.c
@@ -6,7 +6,7 @@
 
 #include <mpsl_fem_twi_drv.h>
 #include <zephyr/drivers/i2c.h>
-
+#include <zephyr/drivers/i2c/i2c_nrfx_twim.h>
 
 int32_t mpsl_fem_twi_drv_impl_xfer_read(void *p_instance, uint8_t slave_address,
 			uint8_t internal_address, uint8_t *p_data, uint8_t data_length)
@@ -22,4 +22,41 @@ int32_t mpsl_fem_twi_drv_impl_xfer_write(void *p_instance, uint8_t slave_address
 	const struct device *dev = (const struct device *)p_instance;
 
 	return i2c_burst_write(dev, slave_address, internal_address, p_data, data_length);
+}
+
+int32_t mpsl_fem_twi_drv_impl_xfer_write_async(void *p_instance, uint8_t slave_address,
+	const uint8_t *p_data, uint8_t data_length, mpsl_fem_twi_async_xfer_write_cb_t p_callback,
+	void *p_context)
+{
+	const struct device *dev = (const struct device *)p_instance;
+
+	struct i2c_msg msg = {
+		.buf = (uint8_t *)p_data,
+		.len = data_length,
+		.flags = I2C_MSG_WRITE | I2C_MSG_STOP
+	};
+
+	return i2c_nrfx_twim_async_transfer_begin(dev, &msg, slave_address,
+		(i2c_nrfx_twim_async_transfer_handler_t)p_callback, p_context);
+}
+
+uint32_t mpsl_fem_twi_drv_impl_xfer_write_async_time_get(void *p_instance, uint8_t data_length)
+{
+	(void)p_instance;
+
+	static const uint32_t sw_overhead_safety_margin_time_us = 10U;
+	/* Note: on nRF devices the first bit of each data octet is delayed by one period,
+	 * thus +1 below.
+	 */
+	static const uint32_t i2c_data_byte_periods_with_ack = 1U + (8U + 1U);
+	static const uint32_t i2c_start_bit_periods = 2U;
+	static const uint32_t i2c_stop_bit_periods = 2U;
+	static const uint32_t i2c_speed_hz = 100000;
+
+	/* Total number of sck periods needed to perform a write transfer. */
+	uint32_t total_periods = i2c_start_bit_periods
+				+ (data_length + 1U) * i2c_data_byte_periods_with_ack
+				+ i2c_stop_bit_periods;
+
+	return (total_periods * 1000000 / i2c_speed_hz) + sw_overhead_safety_margin_time_us;
 }
