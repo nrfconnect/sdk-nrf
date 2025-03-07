5afcccb59c9470c7b20ff92d65a74d0daf13cbbc
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2025-03-07 08:19:37 +0100

    crypto: Add PSA core lite
    -This adds a PSA core that is size optimized. Its intended usage is
     builds that are severely size-limited like bootloaders (NSIB and
     MCUboot).
    -The functionality of the PSA core lite is limited to:
     -ECDSA/EdDSA verification (hash/message)
     -one-shot hash calculation
     -FW encryption/decryption (multi part operations)
    -This core only supports resolving keys on PSA crypto driver level
     and only a limited set of key management functions (resolve, lock and
     destroy). There is no volatile key support or PSA ITS support.
    -Added Kconfig PSA_CORE_LITE
    -Added psa_core_lite (.c/.h)
    
    NOTE: This commit requires a subsequent commit to allow for key and
    attribute retrieval from KMU.
    
    NOTE: PSA core lite is currently limited to nRF54L series devices
    
    ref: NCSDK-30323
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

---
+++ subsys/nrf_security/src/core/lite/psa_core_lite.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+#ifndef PSA_CORE_LITE_H_
+#define PSA_CORE_LITE_H_
+
+#include "util/util_macro.h"
+#include <psa/crypto.h>
+
+/* Macros to verify different signature algorithms */
+
+#define VERIFY_ALG_ED25519(_alg)						\
+	UTIL_CONCAT_AND(IS_ENABLED_ALL(PSA_WANT_ALG_PURE_EDDSA,			\
+				       PSA_WANT_ECC_TWISTED_EDWARDS_255),	\
+			_alg == PSA_ALG_PURE_EDDSA)
+
+#define VERIFY_ALG_ED25519PH(_alg)						\
+	UTIL_CONCAT_AND(IS_ENABLED_ALL(PSA_WANT_ALG_ED25519PH,			\
+				       PSA_WANT_ECC_TWISTED_EDWARDS_255),	\
+			_alg == PSA_ALG_ED25519PH)
+
+#define VERIFY_ALG_ECDSA_SECP_R1_256(_alg)					\
+	UTIL_CONCAT_AND(							\
+		IS_ENABLED_ALL(PSA_WANT_ALG_ECDSA, PSA_WANT_ECC_SECP_R1_256),	\
+		PSA_ALG_IS_ECDSA(_alg))
+
+#define VERIFY_ALG_DETERMINISTIC_ECDSA_SECP_R1_256(_alg)			\
+	UTIL_CONCAT_AND(IS_ENABLED_ALL(PSA_WANT_ALG_DETERMINISTIC_ECDSA,	\
+				       PSA_WANT_ECC_SECP_R1_256),		\
+			PSA_ALG_ECDSA_IS_DETERMINISTIC(_alg))
+
+/* Macros to verify different hash algorithms */
+
+#define VERIFY_ALG_SHA_256(_alg) \
+	(IS_ENABLED(PSA_WANT_ALG_SHA_256) && _alg == PSA_ALG_SHA_256)
+
+#define VERIFY_ALG_SHA_512(_alg) \
+	(IS_ENABLED(PSA_WANT_ALG_SHA_512) && _alg == PSA_ALG_SHA_512)
+
+/* Macros to verify different encryption algorithms */
+
+#define VERIFY_ALG_CTR(_alg) \
+	(IS_ENABLED(PSA_WANT_ALG_CTR) && _alg == PSA_ALG_CTR)
+
+#endif /* PSA_CORE_LITE_H_ */
