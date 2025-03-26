7d93bc310567642e326a92d492279c021f6bb52d
Kacper Radoszewski <kacper.radoszewski@nordicsemi.no>
2025-03-26 10:34:34 +0100

    applications: serial_lte_modem: fix LwM2M carrier event assert
    The check for a valid carrier error or deferred reason can cause a crash
    because PART_OF_ARRAY was not used correctly.
    
    Signed-off-by: Kacper Radoszewski <kacper.radoszewski@nordicsemi.no>

--- applications/serial_lte_modem/src/lwm2m_carrier/slm_at_carrier.c
+++ applications/serial_lte_modem/src/lwm2m_carrier/slm_at_carrier.c
@@ -50,7 +50,7 @@ static void print_err(const lwm2m_carrier_event_t *evt)
 			"Connection failure",
 	};
 
-	__ASSERT(PART_OF_ARRAY(strerr, strerr[err->type]),
+	__ASSERT(PART_OF_ARRAY(strerr, &strerr[err->type]),
 		 "Unhandled liblwm2m_carrier error");
 
 	LOG_ERR("LWM2M_CARRIER_EVENT_ERROR: %s, reason %d", strerr[err->type], err->value);
@@ -86,7 +86,7 @@ static void print_deferred(const lwm2m_carrier_event_t *evt)
 			"Waiting for SIM MSISDN",
 	};
 
-	__ASSERT(PART_OF_ARRAY(strdef, strdef[def->reason]),
+	__ASSERT(PART_OF_ARRAY(strdef, &strdef[def->reason]),
 		 "Unhandled deferred reason");
 
 	LOG_INF("LWM2M_CARRIER_EVENT_DEFERRED: reason %s, timeout %d seconds",
