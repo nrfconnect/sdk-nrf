dd9677c1449b37d111858269a0fe773d944fca28
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2025-03-28 10:44:35 +0100

    nrf_security: Remove Cracen unused hash funcs/macros
    Remove unused macros and functions.
    The functions sx_hash_create_(algorithm)
    are not used by the PSA driver, only the
    sx_hash_create_(engine) are used.
    
    Also the macros SX_HASH_BLOCKSZ_MAX and
    SX_HASH_DIGESTSZ_MAX are also unused so
    remove them as well.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
    (cherry picked from commit b86bb7375239519373609cfed0c5bb969a5e860d)

--- subsys/nrf_security/src/drivers/cracen/sxsymcrypt/include/sxsymcrypt/sm3.h
+++ subsys/nrf_security/src/drivers/cracen/sxsymcrypt/include/sxsymcrypt/sm3.h
@@ -21,38 +21,8 @@ struct sxhash;
 #define SX_HASH_DIGESTSZ_SM3 32
 #define SX_HASH_BLOCKSZ_SM3 64
 
-
-#if SX_HASH_DIGESTSZ_MAX < SX_HASH_DIGESTSZ_SM3
-#undef SX_HASH_DIGESTSZ_MAX
-#define SX_HASH_DIGESTSZ_MAX SX_HASH_DIGESTSZ_SM3
-#endif
-
-
-#if SX_HASH_BLOCKSZ_MAX < SX_HASH_BLOCKSZ_SM3
-#undef SX_HASH_BLOCKSZ_MAX
-#define SX_HASH_BLOCKSZ_MAX SX_HASH_BLOCKSZ_SM3
-#endif
-
 /** GM/T 0004-2012: SM3 cryptographic hash algorithm */
 extern const struct sxhashalg sxhashalg_sm3;
-
-/** Prepares a SM3 hash operation context
- *
- * This function initializes the user allocated object \p c with a new hash
- * operation context and reserves the HW resource.
- *
- * After successful execution of this function, the context \p c can be passed
- * to any of the hashing functions.
- *
- * @param[out] c hash operation context
- * @param[in] csz size of the hash operation context
- * @return ::SX_OK
- * @return ::SX_ERR_INCOMPATIBLE_HW
- * @return ::SX_ERR_RETRY
- *
- * @remark - SM3 digest size is 32 bytes
- */
-int sx_hash_create_sm3(struct sxhash *c, size_t csz);
 
 #ifdef __cplusplus
 }
