85744d444858ffa7f578caf76caf0f562073fde1
Grzegorz Chwierut <grzegorz.chwierut@nordicsemi.no>
2025-03-31 17:56:51 +0200

    tests: kmu: pytest: verify ncs-provision policies
    Refactor tests for KMU provisioning. Add scenarios
    to verify ncs-provision policies: revekable, lock
    and lock-last. Use application with runtime revocation.
    
    Signed-off-by: Grzegorz Chwierut <grzegorz.chwierut@nordicsemi.no>

--- tests/subsys/kmu/pytest/common.py
+++ tests/subsys/kmu/pytest/common.py
@@ -4,6 +4,7 @@
 from __future__ import annotations
 
 import logging
+import os
 import shlex
 import subprocess
 
@@ -14,6 +15,13 @@ logger = logging.getLogger(__name__)
 APP_KEYS_FOR_KMU = Path(__file__).resolve().parent.parent / 'keys'
 
 
+def normalize_path(path: str | None) -> str:
+    if path is not None:
+        path = os.path.expanduser(os.path.expandvars(path))
+        path = os.path.normpath(os.path.abspath(path))
+    return path
+
+
 def run_command(command: list[str], timeout: int = 30):
     logger.info(f"CMD: {shlex.join(command)}")
     ret: subprocess.CompletedProcess = subprocess.run(
@@ -21,8 +29,17 @@ def run_command(command: list[str], timeout: int = 30):
         stderr=subprocess.STDOUT, timeout=timeout)
     if ret.returncode:
         logger.error(f"Failed command: {shlex.join(command)}")
-        logger.error(ret.stdout)
+        logger.info(ret.stdout)
         raise subprocess.CalledProcessError(ret.returncode, command)
+
+
+def reset_board(dev_id: str | None = None):
+    command = [
+        'nrfutil', 'device', 'reset'
+    ]
+    if dev_id:
+        command.extend(['--serial-number', dev_id])
+    run_command(command)
 
 
 def erase_board(dev_id: str | None):
@@ -34,8 +51,17 @@ def erase_board(dev_id: str | None):
     run_command(command)
 
 
+def flash_with_nrfutil(firmware: Path | str, dev_id: str):
+    command = [
+        'nrfutil', 'device', 'program',
+        '--firmware', str(firmware)
+    ]
+    if dev_id:
+        command.extend(['--serial-number', dev_id])
+    run_command(command)
+
+
 def flash_board(build_dir: Path | str, dev_id: str | None, erase: bool = False):
-    logger.info("Flash the board.")
     command = [
         'west', 'flash', '--skip-rebuild',
         '-d', str(build_dir)
@@ -47,21 +73,27 @@ def flash_board(build_dir: Path | str, dev_id: str | None, erase: bool = False):
     run_command(command)
 
 
-def provision_keys_for_kmu(dev_id: str | None, key1: str | Path,
-                           key2: str | Path | None = None,
-                           key3: str | Path | None = None):
-    logger.info("Provision keys using west command. Erase the board first")
-    erase_board(dev_id)
+def provision_keys_for_kmu(
+        keys: list[str] | str,
+        keyname: str = "UROT_PUBKEY",  # UROT_PUBKEY, BL_PUBKEY, APP_PUBKEY
+        soc: str = "nrf54l15",  # nrf54l15, nrf54l10, nrf54l05
+        policy: str | None = None,  # revokable, lock, lock-last (default)
+        dev_id: str | None = None
+):
+    logger.info("Provision keys using west command.")
     command = [
         'west', 'ncs-provision', 'upload',
-        '--soc', 'nrf54l15',
-        '--key', str(key1)
+        '--keyname', keyname,
+        '--soc', soc
     ]
-    if key2:
-        command.extend(['--key', str(key2)])
-    if key3:
-        command.extend(['--key', str(key3)])
+    if policy:
+        command += ['--policy', policy]
     if dev_id:
-        command.extend(['--dev-id', dev_id])
+        command += ['--dev-id', dev_id]
+    if not isinstance(keys, list):
+        keys = [keys]
+    for key in keys:
+        command += ['--key', normalize_path(str(key))]
+
     run_command(command)
     logger.info("Keys provisioned successfully")
---
+++ tests/subsys/kmu/pytest/test_kmu_provision.py
@@ -0,0 +1,93 @@
+# Copyright (c) 2024 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+from __future__ import annotations
+
+import logging
+
+from pathlib import Path
+
+import pytest
+from twister_harness import DeviceAdapter
+from twister_harness.helpers.utils import match_lines, find_in_config
+from common import (
+    provision_keys_for_kmu,
+    reset_board,
+    APP_KEYS_FOR_KMU
+)
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.parametrize(
+    'test_option', ['one_key', 'three_keys_second_used', 'three_keys_last_used']
+)
+def test_kmu_use_key_from_config(dut: DeviceAdapter, test_option):
+    """
+    Upload proper key using west ncs-provision command,
+    verify that the application boots successfully.
+    """
+    logger.info("Provision same key that was used during building")
+    sysbuild_config = Path(dut.device_config.build_dir) / 'zephyr' / '.config'
+    key_file = find_in_config(sysbuild_config, 'SB_CONFIG_BOOT_SIGNATURE_KEY_FILE')
+
+    if test_option == 'three_keys_second_used':
+        keys = [
+            APP_KEYS_FOR_KMU / 'root-ed25519-1.pem',
+            key_file,
+            APP_KEYS_FOR_KMU / 'root-ed25519-2.pem'
+        ]
+    elif test_option == 'three_keys_last_used':
+        keys = [
+            APP_KEYS_FOR_KMU / 'root-ed25519-1.pem',
+            APP_KEYS_FOR_KMU / 'root-ed25519-2.pem',
+            key_file
+        ]
+    else:
+        keys = [key_file]
+
+    provision_keys_for_kmu(
+        keys=keys,
+        keyname="UROT_PUBKEY",
+        dev_id=dut.device_config.id
+    )
+    dut.clear_buffer()
+    reset_board(dut.device_config.id)
+
+    lines = dut.readlines_until(
+        regex='Unable to find bootable image|Jumping to the first image slot',
+        print_output=True, timeout=20)
+
+    match_lines(lines, ['Jumping to the first image slot'])
+    logger.info("Passed: Booted successfully after provisioning the same key that was used during building")
+
+
+def test_kmu_use_wrong_key(dut: DeviceAdapter):
+    """
+    Upload keys using west ncs-provision command,
+    verify that the application does not boot if the keys are incorrect.
+    """
+    logger.info("Provision wrong keys")
+    provision_keys_for_kmu(
+        keys=[
+            APP_KEYS_FOR_KMU / 'root-ed25519-1.pem',
+            APP_KEYS_FOR_KMU / 'root-ed25519-2.pem',
+            APP_KEYS_FOR_KMU / 'root-ed25519-w.pem'
+        ],
+        keyname="UROT_PUBKEY",
+        dev_id=dut.device_config.id
+    )
+
+    dut.clear_buffer()
+    reset_board(dut.device_config.id)
+
+    lines = dut.readlines_until(
+        regex='Unable to find bootable image|Jumping to the first image slot',
+        print_output=True, timeout=20)
+
+    match_lines(lines, [
+        'ED25519 signature verification failed',
+        'Image in the primary slot is not valid',
+        'Unable to find bootable image'
+    ])
+    logger.info("Passed: Not booted when used wrong keys")
