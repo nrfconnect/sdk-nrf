nrfconnect/sdk-nrf

commit 5e39c5f48c2b80df7f3dca5a289ed17c226de7d9

lib: location: Remove support for HERE location services
Removed support for HERE location services.

Signed-off-by: Tommi Kangas <tommi.kangas@nordicsemi.no>

lib/location/location_core.c
@@ -205,31 +205,6 @@ static const char *location_core_gnss_accuracy_str(enum location_accuracy accura
 	return accuracy_str;
 }
 
-static const char LOCATION_SERVICE_ANY_STR[] = "Any";
-static const char LOCATION_SERVICE_NRF_CLOUD_STR[] = "nRF Cloud";
-static const char LOCATION_SERVICE_HERE_STR[] = "HERE";
-
-static const char *location_core_service_str(enum location_service service)
-{
-	const char *service_str = NULL;
-
-	switch (service) {
-	case LOCATION_SERVICE_ANY:
-		service_str = LOCATION_SERVICE_ANY_STR;
-		break;
-	case LOCATION_SERVICE_NRF_CLOUD:
-		service_str = LOCATION_SERVICE_NRF_CLOUD_STR;
-		break;
-	case LOCATION_SERVICE_HERE:
-		service_str = LOCATION_SERVICE_HERE_STR;
-		break;
-	default:
-		__ASSERT_NO_MSG(0);
-		break;
-	}
-	return service_str;
-}
-
 #endif
 
 int location_core_init(void)
@@ -328,16 +303,10 @@ void location_core_config_log(const struct location_config *config)
 				config->methods[i].gnss.accuracy);
 		} else if (type == LOCATION_METHOD_CELLULAR) {
 			LOG_DBG("      Timeout: %dms", config->methods[i].cellular.timeout);
-			LOG_DBG("      Service: %s (%d)",
-				location_core_service_str(config->methods[i].cellular.service),
-				config->methods[i].cellular.service);
 			LOG_DBG("      Cell count: %d", config->methods[i].cellular.cell_count);
 #if defined(CONFIG_LOCATION_METHOD_WIFI)
 		} else if (type == LOCATION_METHOD_WIFI) {
 			LOG_DBG("      Timeout: %dms", config->methods[i].wifi.timeout);
-			LOG_DBG("      Service: %s (%d)",
-				location_core_service_str(config->methods[i].wifi.service),
-				config->methods[i].wifi.service);
 #endif
 		}
 	}
@@ -428,14 +397,6 @@ static void location_request_info_create(const struct location_config *config)
 		if (abs(method_wifi_index - method_cellular_index) == 1) {
 			__ASSERT_NO_MSG(loc_req_info.cellular != NULL);
 			__ASSERT_NO_MSG(loc_req_info.wifi != NULL);
-
-			/* If Wi-Fi and cellular are using same service */
-			if (loc_req_info.cellular->service == loc_req_info.wifi->service) {
-				combine_wifi_cell = true;
-			} else {
-				LOG_DBG("Wi-Fi and cellular methods have different service "
-					"so they are not combined");
-			}
 		} else if (loc_req_info.cellular != NULL && loc_req_info.wifi != NULL) {
 			LOG_DBG("Wi-Fi and cellular methods are not one after the other "
 				"in method list so they are not combined");

include/modem/location.h
@@ -142,21 +142,6 @@ enum location_accuracy {
 	LOCATION_ACCURACY_HIGH,
 };
 
-/** Location service. */
-enum location_service {
-	/**
-	 * Use any location service that has been configured to be available.
-	 *
-	 * This is useful when only one service is configured but can also be used
-	 * even if several services are available.
-	 */
-	LOCATION_SERVICE_ANY,
-	/** nRF Cloud location service. */
-	LOCATION_SERVICE_NRF_CLOUD,
-	/** HERE location service. */
-	LOCATION_SERVICE_HERE
-};
-
 /** Date and time (UTC). */
 struct location_datetime {
 	/** True if date and time are valid, false if not. */
@@ -467,16 +452,6 @@ struct location_cellular_config {
 	int32_t timeout;
 
 	/**
-	 * @brief Used cellular positioning service.
-	 *
-	 * @details Default value is @ref LOCATION_SERVICE_ANY. It is applied when
-	 * location_config_defaults_set() function is called.
-	 *
-	 * This parameter is ignored when @kconfig{CONFIG_LOCATION_SERVICE_EXTERNAL} is enabled.
-	 */
-	enum location_service service;
-
-	/**
 	 * @brief Number of cells to be requested for cellular positioning.
 	 *
 	 * @details Default value is 4. It is applied when
@@ -519,16 +494,6 @@ struct location_wifi_config {
 	 * @ref location_config structure is still valid.
 	 */
 	int32_t timeout;
-
-	/**
-	 * @brief Used Wi-Fi positioning service.
-	 *
-	 * @details Default value is @ref LOCATION_SERVICE_ANY. It is applied when
-	 * location_config_defaults_set() function is called.
-	 *
-	 * This parameter is ignored when @kconfig{CONFIG_LOCATION_SERVICE_EXTERNAL} is enabled.
-	 */
-	enum location_service service;
 };
 
 /** Location method configuration. */
@@ -560,7 +525,6 @@ struct location_config {
 	 * these methods are handled together, if the following conditions are met:
 	 *   - Methods are one after the other in location request method list
 	 *   - @ref mode is @ref LOCATION_REQ_MODE_FALLBACK
-	 *   - Requested cloud service for Wi-Fi and cellular is the same
 	 */
 	struct location_method_config methods[CONFIG_LOCATION_METHODS_LIST_SIZE];
 

lib/location/CMakeLists.txt
@@ -17,7 +17,7 @@ endif()
 
 if(CONFIG_LOCATION_METHOD_CELLULAR OR CONFIG_LOCATION_METHOD_WIFI)
 zephyr_library_sources(method_cloud_location.c)
-add_subdirectory(cloud_service)
+zephyr_library_sources_ifdef(CONFIG_LOCATION_SERVICE_NRF_CLOUD cloud_service.c)
 endif()
 
 zephyr_library_compile_definitions(_POSIX_C_SOURCE=200809L)

lib/location/Kconfig
@@ -117,16 +117,9 @@ if LOCATION_METHOD_CELLULAR || LOCATION_METHOD_WIFI
 
 config LOCATION_SERVICE_CLOUD_RECV_BUF_SIZE
 	int "Receive buffer size"
-	default 1024 if LOCATION_SERVICE_HERE
 	default 512
 	help
 	  Size of the buffer used to store the response from the location service.
-
-config LOCATION_SERVICE_HERE
-	bool "HERE location service"
-	select CJSON_LIB
-	help
-	  Use HERE location service.
 
 config LOCATION_SERVICE_NRF_CLOUD
 	bool "nRF Cloud location service"
@@ -136,47 +129,6 @@ config LOCATION_SERVICE_NRF_CLOUD
 	select NRF_CLOUD_REST_AUTOGEN_JWT if NRF_CLOUD_REST
 	help
 	  Use nRF Cloud location service.
-
-if LOCATION_SERVICE_HERE
-
-config LOCATION_REST_CLIENT
-	bool
-	default y
-	select REST_CLIENT
-	help
-	  Use the REST client library for cellular service. This is
-	  separated here from other dependencies to allow disabling of
-	  REST client library in tests for mocking purposes.
-
-config LOCATION_MODEM_KEY_MGMT
-	bool
-	default y
-	select MODEM_KEY_MGMT
-	help
-	  Use the Modem key management library for cellular service. This is
-	  separated here from other dependencies to allow disabling of
-	  Modem key management library in tests for mocking purposes.
-
-config LOCATION_SERVICE_HERE_API_KEY
-	string "HERE API key"
-	help
-	  HERE API key used for authentication.
-
-config LOCATION_SERVICE_HERE_HOSTNAME
-	string "Server hostname for HERE"
-	default "positioning.hereapi.com"
-	help
-	  Server hostname to use when connecting to the HERE location service.
-
-config LOCATION_SERVICE_HERE_TLS_SEC_TAG
-	int "Security tag for HERE TLS certificates"
-	default 175
-	help
-	  Security tag where the required TLS certificates for the HERE location
-	  service are stored. This sec tag will also be used to provision CA
-	  certificates.
-
-endif # LOCATION_SERVICE_HERE
 
 endif # LOCATION_METHOD_CELLULAR || LOCATION_METHOD_WIFI
 

lib/location/cloud_service.c
@@ -26,6 +26,7 @@ BUILD_ASSERT(
 	"CONFIG_NRF_CLOUD_MQTT, CONFIG_NRF_CLOUD_REST or CONFIG_NRF_CLOUD_COAP transport "
 	"must be enabled");
 
+static char recv_buf[CONFIG_LOCATION_SERVICE_CLOUD_RECV_BUF_SIZE];
 
 #if defined(CONFIG_NRF_CLOUD_MQTT)
 /* Verify that LOCATION is enabled if MQTT is enabled */
@@ -33,33 +34,33 @@ BUILD_ASSERT(
 	IS_ENABLED(CONFIG_NRF_CLOUD_LOCATION),
 	"If CONFIG_NRF_CLOUD_MQTT is enabled also CONFIG_NRF_CLOUD_LOCATION must be enabled");
 
-static struct location_data nrf_cloud_location;
-static K_SEM_DEFINE(nrf_location_ready, 0, 1);
+static struct location_data cloud_location;
+static K_SEM_DEFINE(location_ready, 0, 1);
 #endif
 
 #if defined(CONFIG_NRF_CLOUD_MQTT)
-static void cloud_service_nrf_location_ready_cb(
+static void cloud_service_location_ready_cb(
 	const struct nrf_cloud_location_result *const result)
 {
 	if ((result != NULL) && (result->err == NRF_CLOUD_ERROR_NONE)) {
-		nrf_cloud_location.latitude = result->lat;
-		nrf_cloud_location.longitude = result->lon;
-		nrf_cloud_location.accuracy = (double)result->unc;
+		cloud_location.latitude = result->lat;
+		cloud_location.longitude = result->lon;
+		cloud_location.accuracy = (double)result->unc;
 
-		k_sem_give(&nrf_location_ready);
+		k_sem_give(&location_ready);
 	} else {
 		if (result) {
 			LOG_ERR("Unable to determine location from cloud service, error: %d",
 				result->err);
 		}
-		/* Reset the semaphore to unblock cloud_service_nrf_pos_get()
+		/* Reset the semaphore to unblock cloud_service_pos_get()
 		 * and make it return an error.
 		 */
-		k_sem_reset(&nrf_location_ready);
+		k_sem_reset(&location_ready);
 	}
 }
 
-int cloud_service_nrf_pos_get(
+static int cloud_service_pos_get(
 	const struct cloud_service_pos_req *params,
 	char * const rcv_buf,
 	const size_t rcv_buf_len,
@@ -70,11 +71,11 @@ int cloud_service_nrf_pos_get(
 
 	int err;
 
-	k_sem_reset(&nrf_location_ready);
+	k_sem_reset(&location_ready);
 
 	LOG_DBG("Sending positioning request (MQTT)");
 	err = nrf_cloud_location_request(
-		params->cell_data, params->wifi_data, NULL, cloud_service_nrf_location_ready_cb);
+		params->cell_data, params->wifi_data, NULL, cloud_service_location_ready_cb);
 	if (err == -EACCES) {
 		LOG_ERR("Cloud connection is not established");
 		return err;
@@ -85,18 +86,18 @@ int cloud_service_nrf_pos_get(
 
 	LOG_DBG("Positioning request sent");
 
-	if (k_sem_take(&nrf_location_ready, K_MSEC(params->timeout_ms)) == -EAGAIN) {
+	if (k_sem_take(&location_ready, K_MSEC(params->timeout_ms)) == -EAGAIN) {
 		LOG_ERR("Positioning data request timed out or "
 			"cloud did not return a location");
 		return -ETIMEDOUT;
 	}
 
-	*location = nrf_cloud_location;
+	*location = cloud_location;
 
 	return err;
 }
 #else /* defined(CONFIG_NRF_CLOUD_MQTT) */
-int cloud_service_nrf_pos_get(
+static int cloud_service_pos_get(
 	const struct cloud_service_pos_req *params,
 	char * const rcv_buf,
 	const size_t rcv_buf_len,
@@ -136,3 +137,18 @@ int cloud_service_nrf_pos_get(
 	return err;
 }
 #endif /* defined(CONFIG_NRF_CLOUD_MQTT) */
+
+int cloud_service_location_get(
+	const struct cloud_service_pos_req *params,
+	struct location_data *location)
+{
+	__ASSERT_NO_MSG(params != NULL);
+	__ASSERT_NO_MSG(params->cell_data != NULL || params->wifi_data != NULL);
+	__ASSERT_NO_MSG(location != NULL);
+
+	LOG_DBG("Cloud service location parameters:");
+	LOG_DBG("  Timeout: %dms", params->timeout_ms);
+
+	return cloud_service_pos_get(
+		params, recv_buf, sizeof(recv_buf), location);
+}

lib/location/cloud_service.h
@@ -17,8 +17,6 @@ extern "C" {
 
 /** Cloud positioning input parameters. */
 struct cloud_service_pos_req {
-	/** Cloud positioning service to be used. */
-	enum location_service service;
 	/** Neighbor cell data. */
 	struct lte_lc_cells_info *cell_data;
 	/** Wi-Fi scanning results data. */
@@ -43,11 +41,6 @@ struct cloud_service_pos_req {
 int cloud_service_location_get(
 	const struct cloud_service_pos_req *params,
 	struct location_data *location);
-
-/**
- * @brief Initialize cloud services.
- */
-void cloud_service_init(void);
 
 #ifdef __cplusplus
 }

lib/location/cloud_service/CMakeLists.txt
@@ -1,11 +1,0 @@
-#
-# Copyright (c) 2023 Nordic Semiconductor
-#
-# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
-#
-
-zephyr_library()
-zephyr_library_sources(cloud_service.c)
-zephyr_library_sources_ifdef(CONFIG_LOCATION_SERVICE_NRF_CLOUD cloud_service_nrf.c)
-zephyr_library_sources_ifdef(CONFIG_LOCATION_SERVICE_HERE cloud_service_here_rest.c)
-zephyr_library_sources_ifdef(CONFIG_LOCATION_SERVICE_HERE cloud_service_utils.c)

lib/location/cloud_service/cloud_service.c
@@ -1,82 +1,0 @@
-/*
- * Copyright (c) 2023 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
- */
-
-#include <stdio.h>
-#include <zephyr/kernel.h>
-#include <assert.h>
-
-#include <zephyr/logging/log.h>
-#include <zephyr/net/tls_credentials.h>
-
-#include "cloud_service.h"
-#include "cloud_service_nrf.h"
-#include "cloud_service_here_rest.h"
-#include "cloud_service_utils.h"
-
-LOG_MODULE_DECLARE(location, CONFIG_LOCATION_LOG_LEVEL);
-
-#if defined(CONFIG_LOCATION_SERVICE_EXTERNAL)
-BUILD_ASSERT(
-	!IS_ENABLED(CONFIG_LOCATION_SERVICE_NRF_CLOUD) &&
-	!IS_ENABLED(CONFIG_LOCATION_SERVICE_HERE),
-	"CONFIG_LOCATION_SERVICE_NRF_CLOUD and CONFIG_LOCATION_SERVICE_HERE "
-	"must be disabled when CONFIG_LOCATION_SERVICE_EXTERNAL is set");
-
-#else
-
-BUILD_ASSERT(
-	IS_ENABLED(CONFIG_LOCATION_SERVICE_NRF_CLOUD) ||
-	IS_ENABLED(CONFIG_LOCATION_SERVICE_HERE),
-	"At least one location service must be enabled");
-
-static char recv_buf[CONFIG_LOCATION_SERVICE_CLOUD_RECV_BUF_SIZE];
-
-int cloud_service_location_get(
-	const struct cloud_service_pos_req *params,
-	struct location_data *location)
-{
-	__ASSERT_NO_MSG(params != NULL);
-	__ASSERT_NO_MSG(params->cell_data != NULL || params->wifi_data != NULL);
-	__ASSERT_NO_MSG(location != NULL);
-
-	LOG_DBG("Cloud service location parameters:");
-	LOG_DBG("  Service: %d", params->service);
-	LOG_DBG("  Timeout: %dms", params->timeout_ms);
-
-#if defined(CONFIG_LOCATION_SERVICE_NRF_CLOUD)
-	if (params->service == LOCATION_SERVICE_NRF_CLOUD ||
-	    params->service == LOCATION_SERVICE_ANY) {
-		return cloud_service_nrf_pos_get(
-			params, recv_buf, sizeof(recv_buf), location);
-	}
-#endif
-#if defined(CONFIG_LOCATION_SERVICE_HERE)
-	if (params->service == LOCATION_SERVICE_HERE ||
-	    params->service == LOCATION_SERVICE_ANY) {
-		return cloud_service_here_rest_pos_get(
-			params, recv_buf, sizeof(recv_buf), location);
-	}
-#endif
-	/* We should never get here as at least one service must be enabled */
-	return -ENOTSUP;
-}
-
-void cloud_service_init(void)
-{
-#if defined(CONFIG_LOCATION_SERVICE_HERE)
-	int ret = cloud_service_utils_provision_ca_certificates();
-
-	if (ret) {
-		LOG_ERR("Certificate provisioning failed, ret %d", ret);
-		if (ret == -EACCES) {
-			LOG_WRN("err: -EACCES, that might indicate that modem is in state where "
-				"cert cannot be written, i.e. not in pwroff or offline");
-		}
-	}
-#endif
-}
-
-#endif /* defined(CONFIG_LOCATION_SERVICE_EXTERNAL) */

commit ddef283be324e06ef2b0ad748ba719a7bd582eab

lib: location: Fix combining of cell and Wi-Fi methods
Fixed combining of cellular and Wi-Fi methods broken by
commit 5e39c5f.

Signed-off-by: Tommi Kangas <tommi.kangas@nordicsemi.no>

lib/location/location_core.c
@@ -397,6 +397,8 @@ static void location_request_info_create(const struct location_config *config)
 		if (abs(method_wifi_index - method_cellular_index) == 1) {
 			__ASSERT_NO_MSG(loc_req_info.cellular != NULL);
 			__ASSERT_NO_MSG(loc_req_info.wifi != NULL);
+
+			combine_wifi_cell = true;
 		} else if (loc_req_info.cellular != NULL && loc_req_info.wifi != NULL) {
 			LOG_DBG("Wi-Fi and cellular methods are not one after the other "
 				"in method list so they are not combined");