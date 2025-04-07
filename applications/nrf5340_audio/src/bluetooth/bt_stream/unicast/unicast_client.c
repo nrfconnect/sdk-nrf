7f52a3ffe334419103ab48a804e1b4aeafa6dc86
Alexander Svensen <alexander.svensen@nordicsemi.no>
2025-04-07 15:22:17 +0200

    applications: nrf5340_audio: Set sdu size according to cap
    - Set frame length no higher than what the capabilities support
     - Use BAP preset if value is supported
     - Use max frame len supported by unicast server below preset
    - OCT-3349
    
    Signed-off-by: Alexander Svensen <alexander.svensen@nordicsemi.no>

--- applications/nrf5340_audio/src/bluetooth/bt_stream/unicast/unicast_client.c
+++ applications/nrf5340_audio/src/bluetooth/bt_stream/unicast/unicast_client.c
@@ -706,33 +706,6 @@ static bool valid_codec_cap_check(struct bt_audio_codec_cap cap_array[], uint8_t
 	return valid_result;
 }
 
-/**
- * @brief Set the allocation to a preset codec configuration.
- *
- * @param codec The preset codec configuration.
- * @param loc   Location bitmask setting.
- *
- */
-static void bt_audio_codec_allocation_set(struct bt_audio_codec_cfg *codec_cfg,
-					  enum bt_audio_location loc)
-{
-	for (size_t i = 0U; i < codec_cfg->data_len;) {
-		const uint8_t len = codec_cfg->data[i++];
-		const uint8_t type = codec_cfg->data[i++];
-		uint8_t *value = &codec_cfg->data[i];
-		const uint8_t value_len = len - sizeof(type);
-
-		if (type == BT_AUDIO_CODEC_CFG_CHAN_ALLOC) {
-			const uint32_t loc_32 = loc;
-
-			sys_put_le32(loc_32, value);
-
-			return;
-		}
-		i += value_len;
-	}
-}
-
 static int update_cap_sink_stream_qos(struct le_audio_unicast_server *unicast_server,
 				      uint32_t pres_delay_us)
 {
@@ -1004,8 +977,62 @@ static void discover_cb(struct bt_conn *conn, int err, enum bt_audio_dir dir)
 		if (valid_codec_cap_check(unicast_server->sink_codec_cap,
 					  temp_cap[temp_cap_index].num_caps, BT_AUDIO_DIR_SINK,
 					  idx.lvl3)) {
-			bt_audio_codec_allocation_set(&lc3_preset_sink.codec_cfg,
-						      unicast_server->location);
+
+			ret = bt_audio_codec_cfg_set_val(&lc3_preset_sink.codec_cfg,
+							 BT_AUDIO_CODEC_CFG_CHAN_ALLOC,
+							 (uint8_t *)&unicast_server->location,
+							 sizeof(unicast_server->location));
+			if (ret < 0) {
+				LOG_ERR("Failed to set codec channel allocation: %d", ret);
+				return;
+			}
+
+			const uint8_t *codec_frame_len_ptr = NULL;
+
+			ret = bt_audio_codec_cfg_get_val(&lc3_preset_sink.codec_cfg,
+							 BT_AUDIO_CODEC_CFG_FRAME_LEN,
+							 &codec_frame_len_ptr);
+			if (ret < 0) {
+				LOG_ERR("Failed to get codec frame length: %d", ret);
+				return;
+			}
+
+			uint16_t cap_max_frame_len = 0;
+			uint16_t codec_frame_len = sys_get_le16(codec_frame_len_ptr);
+
+			for (int i = 0; i < temp_cap[temp_cap_index].num_caps; i++) {
+				uint16_t cap_max_frame_len_temp = 0;
+				const uint8_t *cap_max_frame_len_temp_ptr = NULL;
+
+				ret = bt_audio_codec_cap_get_val(&temp_cap[temp_cap_index].codec[i],
+								 BT_AUDIO_CODEC_CFG_FRAME_LEN,
+								 &cap_max_frame_len_temp_ptr);
+				if (ret < 0) {
+					LOG_ERR("Failed to get codec cap frame length: %d", ret);
+					return;
+				}
+
+				/* The first 16 bits are the min frame length */
+				cap_max_frame_len_temp =
+					sys_get_le16(cap_max_frame_len_temp_ptr + sizeof(uint16_t));
+
+				if (cap_max_frame_len_temp > cap_max_frame_len) {
+					cap_max_frame_len = cap_max_frame_len_temp;
+				}
+			}
+
+			LOG_DBG("Codec_frame_len: %d, cap_max_frame_len: %d", codec_frame_len,
+				cap_max_frame_len);
+
+			if (codec_frame_len > cap_max_frame_len) {
+				ret = bt_audio_codec_cfg_set_val(
+					&lc3_preset_sink.codec_cfg, BT_AUDIO_CODEC_CFG_FRAME_LEN,
+					(uint8_t *)&cap_max_frame_len, sizeof(cap_max_frame_len));
+				if (ret < 0) {
+					LOG_ERR("Failed to set codec frame length: %d", ret);
+					return;
+				}
+			}
 		} else {
 			/* NOTE: The string below is used by the Nordic CI system */
 			LOG_WRN("No valid codec capability found for %s sink",
@@ -1016,8 +1043,14 @@ static void discover_cb(struct bt_conn *conn, int err, enum bt_audio_dir dir)
 		if (valid_codec_cap_check(unicast_server->source_codec_cap,
 					  temp_cap[temp_cap_index].num_caps, BT_AUDIO_DIR_SOURCE,
 					  idx.lvl3)) {
-			bt_audio_codec_allocation_set(&lc3_preset_source.codec_cfg,
-						      unicast_server->location);
+			ret = bt_audio_codec_cfg_set_val(&lc3_preset_source.codec_cfg,
+							 BT_AUDIO_CODEC_CFG_CHAN_ALLOC,
+							 (uint8_t *)&unicast_server->location,
+							 sizeof(unicast_server->location));
+			if (ret < 0) {
+				LOG_ERR("Failed to set codec channel allocation: %d", ret);
+				return;
+			}
 		} else {
 			LOG_WRN("No valid codec capability found for %s source",
 				unicast_server->ch_name);
