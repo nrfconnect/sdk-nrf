674083ad46c417717e1d59e67ce9a1cc4529c9ac
Magdalena Pastula <magdalena.pastula@nordicsemi.no>
2025-04-08 12:41:46 +0200

    sysbuild: rename sdp to hpf
    Change names sdp, nrfe and egpio to the new ones: hpf and hpf_gpio.
    
    Signed-off-by: Magdalena Pastula <magdalena.pastula@nordicsemi.no>

--- sysbuild/hpf.cmake
+++ sysbuild/hpf.cmake
@@ -2,17 +2,17 @@
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-if(SB_CONFIG_SDP)
+if(SB_CONFIG_HPF)
   # Extract SoC name from related variables
   string(REPLACE "/" ";" split_board_qualifiers "${BOARD_QUALIFIERS}")
   list(GET split_board_qualifiers 1 target_soc)
   set(board_target_flpr "${BOARD}/${target_soc}/cpuflpr")
   set(target_soc)
 
-  # Include the SDP application in the build
+  # Include the High Performance Framework in the build
   ExternalZephyrProject_Add(
-    APPLICATION sdp
-    SOURCE_DIR ${SB_CONFIG_SDP_IMAGE_PATH}
+    APPLICATION hpf
+    SOURCE_DIR ${SB_CONFIG_HPF_IMAGE_PATH}
     BOARD ${board_target_flpr}
     BOARD_REVISION ${BOARD_REVISION}
   )
