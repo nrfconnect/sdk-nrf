192b6f51186a2e5b4bb7d7fa8173e98e198977f6
Magdalena Pastula <magdalena.pastula@nordicsemi.no>
2025-04-08 12:38:43 +0200

    drivers: change sdp name to hpf
    Change names sdp, nrfe and egpio to the new ones: hpf and hpf_gpio.
    
    Signed-off-by: Magdalena Pastula <magdalena.pastula@nordicsemi.no>

---
+++ include/drivers/gpio/hpf_gpio.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef HPF_GPIO_H
+#define HPF_GPIO_H
+
+#include <drivers/nrfx_common.h>
+#include <hpf/hpf_common.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @brief eGPIO opcodes. */
+typedef enum {
+	HPF_GPIO_PIN_CONFIGURE = 0, /* Configure eGPIO pin. */
+	HPF_GPIO_PIN_CLEAR     = 1, /* Clear eGPIO pin. */
+	HPF_GPIO_PIN_SET       = 2, /* Set eGPIO pin. */
+	HPF_GPIO_PIN_TOGGLE    = 3, /* Toggle eGPIO pin. */
+} hpf_gpio_opcode_t;
+
+/** @brief eGPIO data packet. */
+typedef struct __packed {
+	uint8_t opcode; /* eGPIO opcode. */
+	uint32_t pin; /* Pin number when opcode is HPF_GPIO_PIN_CONFIGURE, pin mask for others. */
+	uint8_t port; /* Port number. */
+	uint32_t flags; /* Configuration flags when opcode
+			 * is HPF_GPIO_PIN_CONFIGURE (gpio_flags_t).
+			 * Not used in other cases.
+			 */
+} hpf_gpio_data_packet_t;
+
+typedef struct {
+	struct hpf_shared_data_lock lock;
+	hpf_gpio_data_packet_t data;
+} hpf_gpio_mbox_data_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HPF_GPIO_H */
