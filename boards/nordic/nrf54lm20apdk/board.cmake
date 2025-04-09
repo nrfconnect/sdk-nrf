05ce097ff002b3019d1c70e1b9a7ed563e60ea70
Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>
2025-04-09 17:57:05 +0200

    add nrf54lm20a flpr
    Signed-off-by: Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>

--- boards/nordic/nrf54lm20apdk/board.cmake
+++ boards/nordic/nrf54lm20apdk/board.cmake
@@ -1,7 +1,11 @@
 # Copyright (c) 2024 Nordic Semiconductor ASA
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
+if(CONFIG_BOARD_NRF54LM20APDK_NRF54LM20A_CPUAPP)
+  board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
+elseif(CONFIG_BOARD_NRF54LM20APDK_NRF54LM20A_CPUFLPR)
+  board_runner_args(jlink "--speed=4000")
+endif()
 
 include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
