2a462be9d530d9c40dc8392226a960e4adc2f22d
Sebastian Głąb <sebastian.glab@nordicsemi.no>
2025-04-24 11:10:03 +0200

    tests: zephyr: drivers: clock_control: Align to 54LM20A
    Copy sources from sdk-zephyr.
    
    Make similar changes to this PR:
    https://github.com/nrfconnect/sdk-zephyr/pull/2806
    but for 54LM20A and 54LV10A.
    
    Signed-off-by: Sebastian Głąb <sebastian.glab@nordicsemi.no>

---
+++ tests/zephyr/drivers/clock_control/clock_control_api/src/nrf_device_subsys.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2019, Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include "device_subsys.h"
+#include <zephyr/drivers/clock_control/nrf_clock_control.h>
+
+static const struct device_subsys_data subsys_data[] = {
+	{
+		.subsys = CLOCK_CONTROL_NRF_SUBSYS_HF,
+		.startup_us =
+			(IS_ENABLED(CONFIG_SOC_NRF54LM20A_ENGA) ||
+			 IS_ENABLED(CONFIG_SOC_NRF54LV10A_ENGA)) ?
+				1000 : (IS_ENABLED(CONFIG_SOC_SERIES_NRF91X) ? 3000 : 500)
+	},
+#ifndef CONFIG_SOC_NRF52832
+	/* On nrf52832 LF clock cannot be stopped because it leads
+	 * to RTC COUNTER register reset and that is unexpected by
+	 * system clock which is disrupted and may hang in the test.
+	 */
+	{
+		.subsys = CLOCK_CONTROL_NRF_SUBSYS_LF,
+		.startup_us = (CLOCK_CONTROL_NRF_K32SRC ==
+			NRF_CLOCK_LFCLK_RC) ? 1000 : 500000
+	}
+#endif /* !CONFIG_SOC_NRF52832 */
+};
+
+static const struct device_data devices[] = {
+	{
+		.dev = DEVICE_DT_GET_ONE(nordic_nrf_clock),
+		.subsys_data =  subsys_data,
+		.subsys_cnt = ARRAY_SIZE(subsys_data)
+	}
+};
