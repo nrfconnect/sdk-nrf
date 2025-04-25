8424e2ccf06aa5432b8756b87c45dd92d87ebb31
Juha Ylinen <juha.ylinen@nordicsemi.no>
2025-04-25 12:56:14 +0300

    net: nrf_provisioning: Improve error handling
    Handle negative coap response codes. Retry provisioning if coap
    client returns timeout error.
    
    Add config NRF_PROVISIONING_INITIAL_BACKOFF.
    
    Signed-off-by: Juha Ylinen <juha.ylinen@nordicsemi.no>

--- subsys/net/lib/nrf_provisioning/src/nrf_provisioning_coap.c
+++ subsys/net/lib/nrf_provisioning/src/nrf_provisioning_coap.c
@@ -469,6 +469,8 @@ static int authenticate(struct coap_client *client, const char *auth_token,
 			   coap_ctx->code == COAP_RESPONSE_CODE_FORBIDDEN) {
 			LOG_ERR("Unauthorized, code %d", coap_ctx->code);
 			return -EACCES;
+		} else if (coap_ctx->code < 0) {
+			return coap_ctx->code;
 		}
 		LOG_ERR("Unknown result code %d", coap_ctx->code);
 		return -ENOTSUP;
@@ -532,6 +534,8 @@ static int send_response(struct coap_client *client,
 		} else if (coap_ctx->code == COAP_RESPONSE_CODE_BAD_REQUEST) {
 			LOG_ERR("Bad request");
 			return -EINVAL;
+		} else if (coap_ctx->code < 0) {
+			return coap_ctx->code;
 		}
 		LOG_ERR("Unknown result code %d", coap_ctx->code);
 		return -ENOTSUP;
