6a0f90a14e9102aec5c6f7e21f9c551e5280fe91
Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
2025-05-05 13:47:14 +0200

    boards: nordic: Set default version of nRF54LM20PDK board to newest one
    Set default board version to 0.2.0.
    
    Signed-off-by: Karol Lasończyk <karol.lasonczyk@nordicsemi.no>

--- boards/nordic/nrf54lm20pdk/revision.cmake
+++ boards/nordic/nrf54lm20pdk/revision.cmake
@@ -3,7 +3,7 @@
 
 set(REVISIONS "0.0.0" "0.2.0" "0.2.0.csp")
 if(NOT DEFINED BOARD_REVISION)
-  set(BOARD_REVISION "0.0.0")
+  set(BOARD_REVISION "0.2.0")
 else()
   if(NOT BOARD_REVISION IN_LIST REVISIONS)
     message(FATAL_ERROR "${BOARD_REVISION} is not a valid nRF54LM20 PDK board revision. Accepted revisions: ${REVISIONS}")
