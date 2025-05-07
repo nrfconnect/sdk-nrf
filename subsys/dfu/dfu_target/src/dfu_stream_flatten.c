ba2e8847f67c838f82583e7cc58d3ce56472bccc
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2025-05-07 13:58:11 +0200

    dfu: dfu_target: use proper flatten function
    This is a cleanup after issues with updates to stream_flash library.
    Scrambling flash after FOTA done/fail is corrected.
    
    Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>

--- subsys/dfu/dfu_target/src/dfu_stream_flatten.c
+++ subsys/dfu/dfu_target/src/dfu_stream_flatten.c
@@ -22,11 +22,11 @@ int stream_flash_flatten_page(struct stream_flash_ctx *ctx, off_t off)
 	}
 
 #if defined(CONFIG_STREAM_FLASH_ERASE)
-	if (ctx->erased_up_to == page.start_offset) {
+	if (ctx->erased_up_to >= (page.start_offset + page.size)) {
 		return 0;
 	}
 #else
-	if (ctx->bytes_written + ctx->offset > page.start_offset) {
+	if (ctx->bytes_written > (page.start_offset + page.size)) {
 		return 0;
 	}
 #endif
@@ -39,7 +39,7 @@ int stream_flash_flatten_page(struct stream_flash_ctx *ctx, off_t off)
 		LOG_ERR("Error %d while flattening page", rc);
 #if defined(CONFIG_STREAM_FLASH_ERASE)
 	} else {
-		ctx->erased_up_to = page.start_offset;
+		ctx->erased_up_to = page.start_offset + page.size - ctx->offset;
 #endif
 	}
 
