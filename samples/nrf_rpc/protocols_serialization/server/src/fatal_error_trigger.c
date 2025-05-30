747105e904d7b3c475195dcb19d22466555c52f2
Damian Krolik <damian.krolik@nordicsemi.no>
2025-05-30 11:25:54 +0200

    samples: nrf_rpc: ps_server: update fatal error trigger
    1. Use /alias/fatal-error-trigger DTS property to select
       button (or GPIO pin) that triggers the fatal error.
    2. Trigger fatal error from GPIO interrupt handler instead
       of DK_LIBRARY handler as the latter is run from the
       system workqueue context and we want the fatal error
       trigger to work even if the system work queue is stuck.
    
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- samples/nrf_rpc/protocols_serialization/server/src/fatal_error_trigger.c
+++ samples/nrf_rpc/protocols_serialization/server/src/fatal_error_trigger.c
@@ -4,25 +4,53 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#include <dk_buttons_and_leds.h>
-
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/init.h>
-#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/sys/util_macro.h>
 
 LOG_MODULE_DECLARE(nrf_ps_server, CONFIG_NRF_PS_SERVER_LOG_LEVEL);
 
-static void button_handler(uint32_t button_state, uint32_t has_changed)
+const struct gpio_dt_spec gpio_pin = GPIO_DT_SPEC_GET(DT_ALIAS(fatal_error_trigger), gpios);
+static struct gpio_callback gpio_cb;
+
+static void trigger_handler(const struct device *port, struct gpio_callback *cb,
+			    gpio_port_pins_t pins)
 {
-	if (button_state & DK_BTN1_MSK) {
-		LOG_ERR("Fatal error button pressed - triggering k_oops");
-		k_oops();
+	ARG_UNUSED(port);
+	ARG_UNUSED(cb);
+	ARG_UNUSED(pins);
+
+	LOG_ERR("Fatal error button pressed - triggering k_oops");
+	k_oops();
+}
+
+static int trigger_init(void)
+{
+	int rc;
+
+	if (!gpio_is_ready_dt(&gpio_pin)) {
+		return -ENODEV;
 	}
+
+	rc = gpio_pin_configure_dt(&gpio_pin, GPIO_INPUT);
+	if (rc) {
+		return rc;
+	}
+
+	gpio_init_callback(&gpio_cb, trigger_handler, BIT(gpio_pin.pin));
+
+	rc = gpio_add_callback_dt(&gpio_pin, &gpio_cb);
+	if (rc) {
+		return rc;
+	}
+
+	rc = gpio_pin_interrupt_configure_dt(&gpio_pin, GPIO_INT_EDGE_TO_ACTIVE);
+	if (rc) {
+		return rc;
+	}
+
+	return 0;
 }
 
-static int button_handler_init(void)
-{
-	return dk_buttons_init(button_handler);
-}
-
-SYS_INIT(button_handler_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+SYS_INIT(trigger_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
