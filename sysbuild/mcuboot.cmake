2eb1690fb0422a8183958e8408fc002a9df88a6b
Artur Hadasz <artur.hadasz@nordicsemi.no>
2025-05-30 16:18:15 +0200

    mcuboot: Support for DIRECT XIP on nRF54H - app core only
    This commit adds support for the DIRECT_XIP modes
    on nRF54H20
    
    Signed-off-by: Artur Hadasz <artur.hadasz@nordicsemi.no>

--- sysbuild/mcuboot.cmake
+++ sysbuild/mcuboot.cmake
@@ -5,10 +5,24 @@
 
 if(SB_CONFIG_MCUBOOT_BUILD_DIRECT_XIP_VARIANT)
   set(image mcuboot_secondary_app)
-  ExternalNcsVariantProject_Add(APPLICATION ${DEFAULT_IMAGE} VARIANT ${image})
+  if(SB_CONFIG_PARTITION_MANAGER)
+    ExternalNcsVariantProject_Add(APPLICATION ${DEFAULT_IMAGE} VARIANT ${image})
 
-  set_property(GLOBAL APPEND PROPERTY
-      PM_APP_IMAGES
-      "${image}"
-  )
+    set_property(GLOBAL APPEND PROPERTY
+        PM_APP_IMAGES
+        "${image}"
+    )
+  else()
+    # TODO: NCSDK-33774 add a general way to pass configuration options to the variant
+    # image
+    zephyr_get(extra_conf_file SYSBUILD LOCAL VAR EXTRA_CONF_FILE)
+
+    ExternalNcsVariantProject_Add(APPLICATION ${DEFAULT_IMAGE} VARIANT ${image} SPLIT_KCONFIG true)
+    set(${image}_EXTRA_CONF_FILE "${extra_conf_file}" CACHE INTERNAL "")
+    set_target_properties(${image} PROPERTIES
+      IMAGE_CONF_SCRIPT ${ZEPHYR_BASE}/share/sysbuild/image_configurations/MAIN_image_default.cmake
+    )
+    set(secondary_overlay "${ZEPHYR_NRF_MODULE_DIR}/subsys/mcuboot/mcuboot_secondary_app.overlay")
+    add_overlay_dts(${image} "${secondary_overlay}")
+  endif()
 endif()
