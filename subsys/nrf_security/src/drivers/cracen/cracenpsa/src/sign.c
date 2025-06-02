8c44b85184594e35454a66db3582491069ccb7aa
Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>
2025-06-02 10:56:43 +0200

    nrf_security: cracen: Add RSA to cracen_psa
    add support for RSA directly in cracen_psa
    remove support for RSA through sicrypto
    
    Signed-off-by: Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/src/sign.c
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/src/sign.c
@@ -30,14 +30,16 @@
 #include "cracen_psa_ecdsa.h"
 #include "cracen_psa_eddsa.h"
 #include <cracen_psa_ikg.h>
+#include "cracen_psa_rsa_signature_pss.h"
+#include "cracen_psa_rsa_signature_pkcs1v15.h"
 #include "ecc.h"
 #define SI_IS_MESSAGE	   (1)
 #define SI_IS_HASH	   (0)
 #define SI_EXTRACT_PUBKEY  (1)
 #define SI_EXTRACT_PRIVKEY (0)
 
-#define SI_PSA_IS_KEY_FLAG(flag, attr) ((flag) == (psa_get_key_usage_flags((attr)) & (flag)))
-#define SI_PSA_IS_KEY_TYPE(flag, attr) ((flag) == (psa_get_key_type((attr)) & (flag)))
+#define CRACEN_PSA_IS_KEY_FLAG(flag, attr) ((flag) == (psa_get_key_usage_flags((attr)) & (flag)))
+#define CRACEN_PSA_IS_KEY_TYPE(flag, attr) ((flag) == (psa_get_key_type((attr)) & (flag)))
 
 #if PSA_MAX_RSA_KEY_BITS > 0
 static int cracen_signature_set_hashalgo(const struct sxhashalg **hashalg, psa_algorithm_t alg)
@@ -173,11 +175,11 @@ static psa_status_t validate_signing_conditions(bool is_message, psa_algorithm_t
 		return PSA_ERROR_INVALID_ARGUMENT;
 	}
 
-	if (is_message && !SI_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_SIGN_MESSAGE, attributes)) {
+	if (is_message && !CRACEN_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_SIGN_MESSAGE, attributes)) {
 		return PSA_ERROR_INVALID_ARGUMENT;
 	}
 
-	if (!is_message && !SI_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_SIGN_HASH, attributes)) {
+	if (!is_message && !CRACEN_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_SIGN_HASH, attributes)) {
 		return PSA_ERROR_INVALID_ARGUMENT;
 	}
 
@@ -348,8 +350,8 @@ static psa_status_t validate_ec_signature_inputs(bool is_message,
 		return PSA_ERROR_NOT_SUPPORTED;
 	}
 
-	if ((is_message && !SI_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_VERIFY_MESSAGE, attributes)) ||
-	    (!is_message && !SI_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_VERIFY_HASH, attributes))) {
+	if ((is_message && !CRACEN_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_VERIFY_MESSAGE, attributes)) ||
+	    (!is_message && !CRACEN_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_VERIFY_HASH, attributes))) {
 		return PSA_ERROR_INVALID_ARGUMENT;
 	}
 
@@ -436,49 +438,36 @@ static psa_status_t cracen_signature_rsa_sign(bool is_message,
 #if PSA_MAX_RSA_KEY_BITS > 0
 	int sx_status;
 	size_t key_bits_attr = psa_get_key_bits(attributes);
-	struct si_sig_privkey privkey = {0};
-	struct si_sig_signature sign = {0};
+	struct cracen_rsa_key privkey = {0};
+	struct cracen_signature sign = {0};
+	struct sxhashalg hashalg = {0};
+	const struct sxhashalg *hashalgpointer = &hashalg;
 	struct sx_buf modulus;
 	struct sx_buf exponent;
-	struct sitask t;
-
-	/* Workmem for RSA signature task is rsa_modulus_size + 2*hash_digest_size + 4 */
-	char workmem[PSA_BITS_TO_BYTES(PSA_MAX_RSA_KEY_BITS) + 2 * PSA_HASH_MAX_SIZE + 4];
-
-	si_task_init(&t, workmem, sizeof(workmem));
 
 	if (alg == PSA_ALG_RSA_PKCS1V15_SIGN_RAW || key_bits_attr < 2048) {
 		return PSA_ERROR_NOT_SUPPORTED;
 	}
 
-	if (((is_message) && (!SI_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_SIGN_MESSAGE, attributes))) ||
-	    ((!is_message) && (!SI_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_SIGN_HASH, attributes)))) {
+	if ((is_message && (!CRACEN_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_SIGN_MESSAGE, attributes))) ||
+	    ((!is_message) && (!CRACEN_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_SIGN_HASH, attributes)))) {
 		return PSA_ERROR_INVALID_ARGUMENT;
 	}
 
 	if (is_message) {
-		sx_status = cracen_signature_set_hashalgo(&privkey.hashalg, alg);
+		sx_status = cracen_signature_set_hashalgo(&hashalgpointer, alg);
 	} else {
-		sx_status = cracen_signature_set_hashalgo_from_digestsz(&privkey.hashalg, alg,
+		sx_status = cracen_signature_set_hashalgo_from_digestsz(&hashalgpointer, alg,
 									input_length);
 	}
 	if (sx_status) {
 		return silex_statuscodes_to_psa(sx_status);
 	}
 
-	sx_status = cracen_signature_get_rsa_key(&privkey.key.rsa, 0, true, key_buffer,
-						 key_buffer_size, &modulus, &exponent);
+	sx_status = cracen_signature_get_rsa_key(&privkey, false, true, key_buffer,
+						      key_buffer_size, &modulus, &exponent);
 	if (sx_status) {
 		return silex_statuscodes_to_psa(sx_status);
-	}
-
-	if (PSA_ALG_IS_RSA_PSS(alg)) {
-		privkey.def = si_sig_def_rsa_pss;
-		privkey.saltsz = sx_hash_get_alg_digestsz(privkey.hashalg);
-	} else if (PSA_ALG_IS_RSA_PKCS1V15_SIGN(alg)) {
-		privkey.def = si_sig_def_rsa_pkcs1v15;
-	} else {
-		return PSA_ERROR_NOT_SUPPORTED;
 	}
 
 	sign.sz = PSA_SIGN_OUTPUT_SIZE(PSA_KEY_TYPE_RSA_KEY_PAIR, key_bits_attr, alg);
@@ -487,24 +476,30 @@ static psa_status_t cracen_signature_rsa_sign(bool is_message,
 	if ((size_t)signature_size < sign.sz) {
 		return PSA_ERROR_BUFFER_TOO_SMALL;
 	}
+	if (PSA_ALG_IS_RSA_PSS(alg) && IS_ENABLED(CONFIG_PSA_NEED_CRACEN_RSA_PSS)) {
+		size_t saltsz = sx_hash_get_alg_digestsz(hashalgpointer);
 
-	if (is_message) {
-		si_sig_create_sign(&t, &privkey, &sign);
-		si_task_consume(&t, (char *)input, input_length);
+		if (is_message) {
+			sx_status = cracen_rsa_pss_sign_message(&privkey, &sign, hashalgpointer,
+								input, input_length, saltsz);
+		} else {
+			sx_status = cracen_rsa_pss_sign_digest(&privkey, &sign, hashalgpointer,
+							       input, input_length, saltsz);
+		}
+	} else if (PSA_ALG_IS_RSA_PKCS1V15_SIGN(alg) && IS_ENABLED(PSA_NEED_CRACEN_RSA_PKCS1V15_SIGN)) {
+		if (is_message) {
+			sx_status = cracen_rsa_pkcs1v15_sign_message(
+				&privkey, &sign, hashalgpointer, input, input_length);
+		} else {
+			sx_status = cracen_rsa_pkcs1v15_sign_digest(&privkey, &sign, hashalgpointer,
+								    input, input_length);
+			}
 	} else {
-		si_sig_create_sign_digest(&t, &privkey, &sign);
-		si_task_consume(&t, (char *)input, sx_hash_get_alg_digestsz(privkey.hashalg));
-	}
-	si_task_run(&t);
-	sx_status = si_task_wait(&t);
-
-	safe_memzero(workmem, sizeof(workmem));
-	if (sx_status != SX_OK) {
-		return silex_statuscodes_to_psa(sx_status);
+		return PSA_ERROR_NOT_SUPPORTED;
 	}
 
 	*signature_length = sign.sz;
-	return PSA_SUCCESS;
+	return silex_statuscodes_to_psa(sx_status);
 #else
 	return PSA_ERROR_NOT_SUPPORTED;
 #endif /* PSA_MAX_RSA_KEY_BITS > 0 */
@@ -518,31 +513,29 @@ static psa_status_t cracen_signature_rsa_verify(bool is_message,
 						size_t signature_length)
 {
 #if PSA_MAX_RSA_KEY_BITS > 0
-	int sx_status = 0;
+	int sx_status;
 	size_t key_bits_attr = psa_get_key_bits(attributes);
-	struct si_sig_pubkey pubkey = {0};
-	struct si_sig_signature sign = {0};
+	struct cracen_rsa_key privkey = {0};
+	struct cracen_signature sign = {0};
+	struct sxhashalg hashalg = {0};
+	const struct sxhashalg *hashalgpointer = &hashalg;
 	struct sx_buf modulus;
 	struct sx_buf exponent;
-	struct sitask t;
-
-	/* Workmem for RSA verify task is  rsa_modulus_size + 2 * hash_digest_size + 4 */
-	char workmem[PSA_BITS_TO_BYTES(PSA_MAX_RSA_KEY_BITS) + 2 * PSA_HASH_MAX_SIZE + 4];
-
-	si_task_init(&t, workmem, sizeof(workmem));
+	size_t saltsz;
 
 	if (alg == PSA_ALG_RSA_PKCS1V15_SIGN_RAW || key_bits_attr < 2048) {
 		return PSA_ERROR_NOT_SUPPORTED;
 	}
-	if (((is_message) && (!SI_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_VERIFY_MESSAGE, attributes))) ||
-	    ((!is_message) && (!SI_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_VERIFY_HASH, attributes)))) {
+
+	if (is_message && (!CRACEN_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_VERIFY_MESSAGE, attributes)) ||
+	    (!is_message && (!CRACEN_PSA_IS_KEY_FLAG(PSA_KEY_USAGE_VERIFY_HASH, attributes)))) {
 		return PSA_ERROR_INVALID_ARGUMENT;
 	}
 
 	if (is_message) {
-		sx_status = cracen_signature_set_hashalgo(&pubkey.hashalg, alg);
+		sx_status = cracen_signature_set_hashalgo(&hashalgpointer, alg);
 	} else {
-		sx_status = cracen_signature_set_hashalgo_from_digestsz(&pubkey.hashalg, alg,
+		sx_status = cracen_signature_set_hashalgo_from_digestsz(&hashalgpointer, alg,
 									input_length);
 	}
 	if (sx_status) {
@@ -550,44 +543,43 @@ static psa_status_t cracen_signature_rsa_verify(bool is_message,
 	}
 
 	sx_status = cracen_signature_get_rsa_key(
-		&pubkey.key.rsa, true, SI_PSA_IS_KEY_TYPE(PSA_KEY_TYPE_RSA_KEY_PAIR, attributes),
+		&privkey, true, CRACEN_PSA_IS_KEY_TYPE(PSA_KEY_TYPE_RSA_KEY_PAIR, attributes),
 		key_buffer, key_buffer_size, &modulus, &exponent);
-
 	if (sx_status) {
 		return silex_statuscodes_to_psa(sx_status);
 	}
-
-	if (PSA_ALG_IS_RSA_PSS(alg)) {
-		pubkey.def = si_sig_def_rsa_pss;
-		pubkey.saltsz = sx_hash_get_alg_digestsz(pubkey.hashalg);
-	} else if (PSA_ALG_IS_RSA_PKCS1V15_SIGN(alg)) {
-		pubkey.def = si_sig_def_rsa_pkcs1v15;
-	} else {
-		return PSA_ERROR_NOT_SUPPORTED;
-	}
-
-	if ((size_t)signature_length !=
+	if (signature_length !=
 	    PSA_SIGN_OUTPUT_SIZE(PSA_KEY_TYPE_RSA_KEY_PAIR, key_bits_attr, alg)) {
 		return PSA_ERROR_INVALID_SIGNATURE;
 	}
-	sign.sz = signature_length;
-	sign.r = (char *)signature;
-
-	if (is_message) {
-		si_sig_create_verify(&t, &pubkey, &sign);
-		si_task_consume(&t, (char *)input, input_length);
+	if (PSA_ALG_IS_RSA_PSS(alg)) {
+		saltsz = sx_hash_get_alg_digestsz(hashalgpointer);
+	} else if (PSA_ALG_IS_RSA_PKCS1V15_SIGN(alg)) {
+		saltsz = 0;
 	} else {
-		if (sx_hash_get_alg_digestsz(pubkey.hashalg) != input_length) {
-			return PSA_ERROR_INVALID_ARGUMENT;
-		}
-		si_sig_create_verify_digest(&t, &pubkey, &sign);
-		si_task_consume(&t, (char *)input, sx_hash_get_alg_digestsz(pubkey.hashalg));
+		return PSA_ERROR_NOT_SUPPORTED;
 	}
-
-	si_task_run(&t);
-	sx_status = si_task_wait(&t);
-
-	safe_memzero(workmem, sizeof(workmem));
+	sign.sz = signature_length;
+	sign.r = (uint8_t *)signature;
+	if (PSA_ALG_IS_RSA_PSS(alg) && IS_ENABLED(PSA_NEED_CRACEN_RSA_PSS)) {
+		if (is_message) {
+			sx_status = cracen_rsa_pss_verify_message(&privkey, &sign, hashalgpointer,
+								  input, input_length, saltsz);
+		} else {
+			sx_status = cracen_rsa_pss_verify_digest(&privkey, &sign, hashalgpointer,
+								 input, input_length, saltsz);
+		}
+	} else if (PSA_ALG_IS_RSA_PKCS1V15_SIGN(alg) && IS_ENABLED(PSA_NEED_CRACEN_RSA_PKCS1V15_SIGN)) {
+		if (is_message) {
+			sx_status = cracen_rsa_pkcs1v15_verify_message(
+				&privkey, &sign, hashalgpointer, input, input_length);
+		} else {
+			sx_status = cracen_rsa_pkcs1v15_verify_digest(
+				&privkey, &sign, hashalgpointer, input, input_length);
+		}
+	} else {
+		return PSA_ERROR_NOT_SUPPORTED;
+	}
 	return silex_statuscodes_to_psa(sx_status);
 #else
 	return PSA_ERROR_NOT_SUPPORTED;
