e1c7346450e764e980cfbc5658e0e7605aee7126
Artur Hadasz <artur.hadasz@nordicsemi.no>
2025-06-10 16:50:01 +0200

    cmake: NCS-only fix for variant configuration with no partition manager
    This commit introduces a (probably temporary) way of fixing passing
    Kconfigs to a variant image.
    
    In the solution, when not using partition manager, both the .config
    and autoconf.h files are copied into the variant image. However,
    the CONFIG_FLASH_LOAD_OFFSET and CONFIG_FLASH_LOAD_SIZE are modified
    to match the values from zephyr,code-partition - as this are
    the values used for linking.
    
    Signed-off-by: Artur Hadasz <artur.hadasz@nordicsemi.no>

--- sysbuild/mcuboot.cmake
+++ sysbuild/mcuboot.cmake
@@ -14,32 +14,18 @@ if(SB_CONFIG_MCUBOOT_BUILD_DIRECT_XIP_VARIANT)
       set(image "${primary_image}_secondary_app")
     endif()
 
-    if(SB_CONFIG_PARTITION_MANAGER)
-      ExternalNcsVariantProject_Add(APPLICATION ${primary_image} VARIANT ${image})
+    ExternalNcsVariantProject_Add(APPLICATION ${primary_image} VARIANT ${image})
 
+    if(SB_CONFIG_PARTITION_MANAGER)
       set_property(GLOBAL APPEND PROPERTY
           PM_APP_IMAGES
           "${image}"
       )
     else()
-      # TODO: NCSDK-33774 add a general way to pass configuration options to the variant
-      # image
-      if(${primary_image} STREQUAL ${DEFAULT_IMAGE})
-        zephyr_get(extra_conf_file SYSBUILD LOCAL VAR EXTRA_CONF_FILE)
-      else()
-        # All variables with ${image}_ prefix are passed to the variant image automatically.
-        set(extra_conf_file)
-      endif()
-
-      ExternalNcsVariantProject_Add(APPLICATION ${primary_image} VARIANT ${image} SPLIT_KCONFIG true)
       UpdateableImage_Add(APPLICATION ${image} GROUP "VARIANT")
-      set(${image}_EXTRA_CONF_FILE "${extra_conf_file}" CACHE INTERNAL "")
-      set_target_properties(${image} PROPERTIES
-        IMAGE_CONF_SCRIPT ${ZEPHYR_BASE}/share/sysbuild/image_configurations/MAIN_image_default.cmake
-      )
       set(secondary_overlay "${ZEPHYR_NRF_MODULE_DIR}/subsys/mcuboot/mcuboot_secondary_app.overlay")
-
       add_overlay_dts(${image} "${secondary_overlay}")
     endif()
+
   endforeach()
 endif()
