085c88e3003541b2b224bc3aec25401d0c936ed4
Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>
2025-06-25 16:47:48 +0200

    applications: nrf_desktop: Align behavior on USB disconnection
    Adds power manager restriction and forces power down on USB
    disconnected event for consistency as it can also be done on
    disconnection by USB suspended event. Adds option to delay
    system off after USB disconnection by few seconds to allow
    user to take actions like restart advertising without going
    through reboot.
    
    Jira: NCSDK-17701
    
    Signed-off-by: Aleksander Strzebonski <aleksander.strzebonski@nordicsemi.no>

--- applications/nrf_desktop/src/modules/usb_state_pm.c
+++ applications/nrf_desktop/src/modules/usb_state_pm.c
@@ -19,6 +19,16 @@ LOG_MODULE_REGISTER(MODULE, CONFIG_DESKTOP_USB_STATE_LOG_LEVEL);
 #include <caf/events/force_power_down_event.h>
 
 
+static bool initialized;
+static struct k_work_delayable pm_restrict_remove_work;
+
+static void pm_restrict_remove_work_fn(struct k_work *work)
+{
+	ARG_UNUSED(work);
+
+	power_manager_restrict(MODULE_IDX(MODULE), POWER_MANAGER_LEVEL_MAX);
+}
+
 static void update_pm_policy_latency_req(bool enable)
 {
 	static bool enabled;
@@ -45,9 +55,16 @@ static bool app_event_handler(const struct app_event_header *aeh)
 		const struct usb_state_event *event = cast_usb_state_event(aeh);
 
 		LOG_DBG("USB state change detected");
+		__ASSERT_NO_MSG(initialized);
 
 		if (IS_ENABLED(CONFIG_DESKTOP_USB_PM_REQ_NO_PM_LATENCY)) {
 			update_pm_policy_latency_req(event->state == USB_STATE_ACTIVE);
+		}
+
+		if ((CONFIG_DESKTOP_USB_PM_RESTRICT_REMOVE_DELAY_MS > 0) &&
+		    (event->state != USB_STATE_DISCONNECTED)) {
+			/* Cancel the work if it is scheduled. */
+			(void)k_work_cancel_delayable(&pm_restrict_remove_work);
 		}
 
 		switch (event->state) {
@@ -58,7 +75,28 @@ static bool app_event_handler(const struct app_event_header *aeh)
 			power_manager_restrict(MODULE_IDX(MODULE), POWER_MANAGER_LEVEL_ALIVE);
 			break;
 		case USB_STATE_DISCONNECTED:
-			power_manager_restrict(MODULE_IDX(MODULE), POWER_MANAGER_LEVEL_MAX);
+			if (CONFIG_DESKTOP_USB_PM_RESTRICT_REMOVE_DELAY_MS > 0) {
+				/* Remove the restriction after the delay to allow the user to
+				 * take actions like e.g. restart BLE advertising without going
+				 * through system off.
+				 */
+				(void)k_work_schedule(
+					&pm_restrict_remove_work,
+					K_MSEC(CONFIG_DESKTOP_USB_PM_RESTRICT_REMOVE_DELAY_MS));
+			} else {
+				power_manager_restrict(MODULE_IDX(MODULE), POWER_MANAGER_LEVEL_MAX);
+			}
+			/* When disconnecting USB cable, the USB_STATE_SUSPENDED event may
+			 * or may not appear before the USB_STATE_DISCONNECTED event. Add power
+			 * manager restriction and force power down for application to behave
+			 * consistently even if the USB_STATE_SUSPENDED event doesn't appear. Don't
+			 * add restriction if the restriction removal delay is set to 0.
+			 */
+			if (CONFIG_DESKTOP_USB_PM_RESTRICT_REMOVE_DELAY_MS > 0) {
+				power_manager_restrict(MODULE_IDX(MODULE),
+						       POWER_MANAGER_LEVEL_SUSPENDED);
+			}
+			force_power_down();
 			break;
 		case USB_STATE_SUSPENDED:
 			LOG_DBG("USB suspended");
@@ -76,9 +114,11 @@ static bool app_event_handler(const struct app_event_header *aeh)
 		const struct module_state_event *event = cast_module_state_event(aeh);
 
 		if (check_state(event, MODULE_ID(main), MODULE_STATE_READY)) {
-			static bool initialized;
-
 			__ASSERT_NO_MSG(!initialized);
+			if (CONFIG_DESKTOP_USB_PM_RESTRICT_REMOVE_DELAY_MS > 0) {
+				k_work_init_delayable(&pm_restrict_remove_work,
+						      pm_restrict_remove_work_fn);
+			}
 			initialized = true;
 		}
 		return false;
