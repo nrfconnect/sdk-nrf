f15f2232d1a49e15db6a44e938bb0910cc00cff0
Magne Værnes <magne.varnes@nordicsemi.no>
2025-06-30 09:44:46 +0200

    nrf_security: drivers: cracen: Enable KMU key generation for ChaCha20
    Enabled support for KMU key generation of ChaCha20 keys.
    
    Signed-off-by: Magne Værnes <magne.varnes@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/src/key_management.c
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/src/key_management.c
@@ -1206,7 +1206,8 @@ psa_status_t generate_key_for_kmu(const psa_key_attributes_t *attributes, uint8_
 		if (status != PSA_SUCCESS) {
 			return status;
 		}
-	} else if (key_type == PSA_KEY_TYPE_AES || key_type == PSA_KEY_TYPE_HMAC) {
+	} else if (key_type == PSA_KEY_TYPE_AES || key_type == PSA_KEY_TYPE_HMAC ||
+		   key_type == PSA_KEY_TYPE_CHACHA20) {
 		status = psa_generate_random(key, PSA_BITS_TO_BYTES(psa_get_key_bits(attributes)));
 		if (status != PSA_SUCCESS) {
 			return status;
