34183d76a1aff15a3df44a7bfea60414a6c7abca
Travis Lam <travis.lam@nordicsemi.no>
2025-07-03 13:07:27 +0200

    nrf_security: add nrf7120 to CRACEN driver
    Add nrf7120 for CRACEN core drivers
    
    Signed-off-by: Travis Lam <travis.lam@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/src/lib_kmu.c
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/src/lib_kmu.c
@@ -12,7 +12,11 @@
 
 #include <nrf.h>
 
+#if defined(CONFIG_SOC_SERIES_NRF54LX)
 #include <nrfx_rramc.h>
+#elif defined(CONFIG_SOC_SERIES_NRF71X)
+#include <nrfx_mramc.h>
+#endif
 
 #include <cracen/lib_kmu.h>
 
@@ -33,6 +37,36 @@ void lib_kmu_clear_all_events(void)
 	NRF_KMU_S->EVENTS_PUSHBLOCKED = 0;
 #endif
 }
+
+#if defined(CONFIG_SOC_SERIES_NRF71X)
+static void mram_sicr_perm_set(bool permission)
+{
+	if (permission) {
+		nrfx_mramc_config_write_mode_set(NRF_MRAMC_MODE_WRITE_DIRECT);
+		nrf_mramc_readynext_timeout_t readynext_timeout = {
+			.value		= NRF_MRAMC_READYNEXTTIMEOUT_DEFAULT,
+			.direct_write = true,
+		};
+		nrfy_mramc_readynext_timeout_set(NRF_MRAMC, &readynext_timeout);
+
+		/* CONFIGNVR.PAGE[3] page 4 contain the SICR info block
+		 * setting info block read and write permission to
+		 * allow KMU to write to SICR in MRAM.
+		 */
+		nrfy_mramc_confignvr_perm_set(true, 3);
+	} else {
+		nrfx_mramc_config_write_mode_set(NRF_MRAMC_MODE_WRITE_DISABLE);
+
+		nrf_mramc_readynext_timeout_t readynext_timeout = {
+			.value		= NRF_MRAMC_READYNEXTTIMEOUT_DEFAULT,
+			.direct_write = false,
+		};
+		nrfy_mramc_readynext_timeout_set(NRF_MRAMC, &readynext_timeout);
+
+		nrfy_mramc_confignvr_perm_set(false, 3);
+	}
+}
+#endif
 
 static int trigger_task_and_wait_for_event_or_error(volatile uint32_t *task,
 						    volatile uint32_t *event)
@@ -80,7 +114,7 @@ int lib_kmu_provision_slot(int slot_id, struct kmu_src *kmu_src)
 	__ASSERT(IS_PTR_ALIGNED_BYTES(kmu_src->dest, 16), "DEST misaligned");
 
 	int result = 1;
-
+#if defined(CONFIG_SOC_SERIES_NRF54LX)
 #if defined(__NRF_TFM__)
 	nrf_rramc_config_t rramc_config;
 
@@ -92,6 +126,10 @@ int lib_kmu_provision_slot(int slot_id, struct kmu_src *kmu_src)
 #else
 	nrfx_rramc_write_enable_set(true, 0);
 #endif
+#elif defined(CONFIG_SOC_SERIES_NRF71X)
+	/* Enable read and write from KMU to SICR in MRAM */
+	mram_sicr_perm_set(true);
+#endif
 
 	NRF_KMU_S->KEYSLOT = slot_id;
 	NRF_KMU_S->SRC = (uint32_t)kmu_src;
@@ -99,11 +137,16 @@ int lib_kmu_provision_slot(int slot_id, struct kmu_src *kmu_src)
 	result = trigger_task_and_wait_for_event_or_error(&(NRF_KMU_S->TASKS_PROVISION),
 							  &(NRF_KMU_S->EVENTS_PROVISIONED));
 
+#if defined(CONFIG_SOC_SERIES_NRF54LX)
 #if defined(__NRF_TFM__)
 	rramc_config.write_buff_size = orig_write_buf_size;
 	nrf_rramc_config_set(NRF_RRAMC_S, &rramc_config);
 #else
 	nrfx_rramc_write_enable_set(false, 0);
+#endif
+#elif defined(CONFIG_SOC_SERIES_NRF71X)
+	/* Disable write from KMU to SICR in MRAM */
+	mram_sicr_perm_set(false);
 #endif
 
 	return result;
@@ -145,8 +188,11 @@ int lib_kmu_block_slot_range(int slot_id, unsigned int slot_count)
 
 int lib_kmu_revoke_slot(int slot_id)
 {
-#if !defined(__NRF_TFM__)
+#if !defined(__NRF_TFM__) && defined(CONFIG_SOC_SERIES_NRF54LX)
 	nrfx_rramc_write_enable_set(true, 0);
+#elif !defined(__NRF_TFM__) && defined(CONFIG_SOC_SERIES_NRF71X)
+	/* Enable read and write from KMU to SICR in MRAM */
+	mram_sicr_perm_set(true);
 #endif
 
 	NRF_KMU_S->KEYSLOT = slot_id;
@@ -154,8 +200,11 @@ int lib_kmu_revoke_slot(int slot_id)
 	int result = trigger_task_and_wait_for_event_or_error(&(NRF_KMU_S->TASKS_REVOKE),
 							      &(NRF_KMU_S->EVENTS_REVOKED));
 
-#if !defined(__NRF_TFM__)
+#if !defined(__NRF_TFM__) && defined(CONFIG_SOC_SERIES_NRF54LX)
 	nrfx_rramc_write_enable_set(false, 0);
+#elif !defined(__NRF_TFM__) && defined(CONFIG_SOC_SERIES_NRF71X)
+	/* Disable read and write from KMU to SICR in MRAM */
+	mram_sicr_perm_set(false);
 #endif
 
 	return result;
