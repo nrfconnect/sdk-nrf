c6e6daeca24e4a55316fa2f7113c79bfb91971c9
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2025-07-07 15:43:49 +0300

    doc: nrf_security: remove outdated Mbed TLS configuration page
    Looking at the Kconfig and CMake logic it appears that providing your
    own configuration header file is not a possibility anymore.
    CONFIG_GENERATE_MBEDTLS_CFG_FILE doesn't exist anymore.
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

--- doc/_utils/redirects.py
+++ doc/_utils/redirects.py
@@ -553,7 +553,8 @@ NRF = [
     ("libraries/nrf_security/doc/configuration", "libraries/security/nrf_security/doc/configuration"), # Configuration
     ("libraries/nrf_security/doc/driver_config", "security/crypto/driver_config"), # Feature configurations and driver support (moved to security/crypto for v3.1.0)
     ("libraries/security/nrf_security/doc/driver_config", "security/crypto/driver_config"),
-    ("libraries/nrf_security/doc/mbed_tls_header", "libraries/security/nrf_security/doc/mbed_tls_header"), # User-provided Mbed TLS configuration header
+    ("libraries/nrf_security/doc/mbed_tls_header", "libraries/security/nrf_security/index"), # User-provided Mbed TLS configuration header (removed in v3.1.0)
+    ("libraries/security/nrf_security/doc/mbed_tls_header", "libraries/security/nrf_security/index"), # User-provided Mbed TLS configuration header (removed in v3.1.0)
     ("libraries/nrf_security/doc/backend_config", "libraries/security/nrf_security/doc/backend_config"), # Legacy configurations and supported features
     ("libraries/tfm/index", "libraries/security/tfm/index"), # TF-M libraries (landing)
     ("libraries/tfm/tfm_ioctl_api", "libraries/security/tfm/tfm_ioctl_api"), # TF-M input/output control (IOCTL)
