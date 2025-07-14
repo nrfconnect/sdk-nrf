08f5ba8a685cb442fc4d808331493d5118cd3472
Robert Robinson <robert.robinson@nordicsemi.no>
2025-07-14 12:19:32 +0100

    nrf: scripts: runners: Add support for nrfutil_next out of tree runner
    Nordic devices that are yet to be upstreamed, but have nrfutil device
    support, requie modifications to the runner in upstream zephyr in order
    to support west flash. These cannot be applied becasuse config
    defintions only exist in nrf. Therefore creating a runner that modifies
    nrfutil so that changes are contained to sdk-nrf.
    
    Signed-off-by: Robert Robinson <robert.robinson@nordicsemi.no>

---
+++ boards/nordic/common/nrfutil_next.board.cmake
@@ -0,0 +1,5 @@
+# Copyright (c) 2025 Nordic Semiconductor ASA
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+
+board_set_flasher_ifnset(nrfutil_next)
+board_finalize_runner_args(nrfutil_next) # No default arguments to provide.
