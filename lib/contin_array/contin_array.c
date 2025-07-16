96af81eaf37f3ebedf767e3736c1de26a1dd0281
Graham Wacey <graham.wacey@nordicsemi.no>
2025-07-16 11:55:45 +0100

    lib: contin_array: Support multiple channels and data types
    Update the library to allow building of multi-channel arrays
    with 8-, 24- and 32-bit samples and 8-, 16 and 32-bit carrier
    widths.
    Twister test cases included.
    
    Signed-off-by: Graham Wacey <graham.wacey@nordicsemi.no>

--- lib/contin_array/contin_array.c
+++ lib/contin_array/contin_array.c
@@ -22,7 +22,7 @@ int contin_array_create(void *const pcm_cont, uint32_t pcm_cont_size, void const
 	}
 
 	if (!pcm_cont_size || !pcm_finite_size) {
-		LOG_ERR("size cannot be zero");
+		LOG_ERR("Size cannot be zero");
 		return -EPERM;
 	}
 
@@ -35,4 +35,144 @@ int contin_array_create(void *const pcm_cont, uint32_t pcm_cont_size, void const
 	}
 
 	return 0;
+}
+
+int contin_array_buf_create(struct net_buf *pcm_contin, void const *const pcm_finite,
+			    uint16_t pcm_finite_size, uint32_t locations,
+			    uint16_t *const finite_pos)
+{
+	struct audio_metadata *meta_contin;
+	uint8_t num_ch;
+	uint8_t count_ch;
+	uint8_t *output;
+	uint8_t carrier_bytes;
+	uint16_t step;
+	uint16_t frame_bytes;
+	uint16_t finite_start_pos;
+	uint32_t out_locs;
+
+	if (pcm_contin == NULL || pcm_finite == NULL || finite_pos == NULL) {
+		LOG_ERR("Invalid parameter");
+		return -ENXIO;
+	}
+
+	if ((pcm_contin->size == 0) || (pcm_finite_size == 0) || (*finite_pos >= pcm_finite_size)) {
+		LOG_ERR("Size or finite position out of range");
+		return -EPERM;
+	}
+
+	meta_contin = net_buf_user_data(pcm_contin);
+	if (meta_contin == NULL) {
+		LOG_ERR("No metadata");
+		return -ENXIO;
+	}
+
+	/* Here the number of common output locations is determined */
+	if (meta_contin->locations == 0 && locations == 0) {
+		/* Both are mono buffers and hence have a single output location in common */
+		out_locs = 0x01;
+	} else {
+		out_locs = meta_contin->locations & locations;
+	}
+
+	if (out_locs == 0) {
+		LOG_ERR("Locations error");
+		return -EPERM;
+	}
+
+	if ((meta_contin->carried_bits_per_sample == 0) ||
+	    (meta_contin->carried_bits_per_sample > PCM_CONT_MAX_CARRIER_BIT_DEPTH) ||
+	    (meta_contin->carried_bits_per_sample % 8)) {
+		LOG_ERR("Carrier bits invalid: %d", meta_contin->carried_bits_per_sample);
+		return -EINVAL;
+	}
+
+	carrier_bytes = meta_contin->carried_bits_per_sample / 8;
+
+	num_ch = metadata_num_ch_get(meta_contin);
+
+	if (pcm_contin->len == 0) {
+		memset(pcm_contin->data, 0, meta_contin->bytes_per_location * num_ch);
+		net_buf_add(pcm_contin, meta_contin->bytes_per_location * num_ch);
+	}
+
+	finite_start_pos = *finite_pos;
+
+	if (meta_contin->interleaved) {
+		step = carrier_bytes * (num_ch - 1);
+		frame_bytes = carrier_bytes;
+	} else {
+		step = 0;
+		frame_bytes = meta_contin->bytes_per_location;
+	}
+
+	count_ch = 0;
+
+	/* While there are common output locations */
+	while (out_locs) {
+		if (out_locs & 0x01) {
+			output = &((uint8_t *)pcm_contin->data)[frame_bytes * count_ch];
+
+			*finite_pos = finite_start_pos;
+
+			for (size_t j = 0; j < meta_contin->bytes_per_location;
+			     j += carrier_bytes) {
+				for (size_t k = 0; k < carrier_bytes; k++) {
+					*output++ = ((uint8_t *)pcm_finite)[(*finite_pos)++];
+
+					if (*finite_pos >= pcm_finite_size) {
+						*finite_pos = 0;
+					}
+				}
+
+				output += step;
+			}
+
+			count_ch++;
+		}
+
+		out_locs >>= 1;
+	}
+
+	return 0;
+}
+
+int contin_array_net_buf_create(struct net_buf *pcm_contin, struct net_buf const *const pcm_finite,
+				uint32_t locations, uint16_t *const finite_pos)
+{
+	struct audio_metadata *meta_contin;
+	struct audio_metadata *meta_finite;
+
+	if (pcm_contin == NULL || pcm_finite == NULL) {
+		LOG_ERR("Error in input parameter");
+		return -ENXIO;
+	}
+
+	if (pcm_finite->len == 0) {
+		LOG_ERR("Finite buffer size error");
+		return -EPERM;
+	}
+
+	meta_contin = net_buf_user_data(pcm_contin);
+	if (meta_contin == NULL) {
+		LOG_ERR("No metadata");
+		return -ENXIO;
+	}
+
+	meta_finite = net_buf_user_data(pcm_finite);
+	if (meta_finite == NULL) {
+		LOG_ERR("No metadata");
+		return -ENXIO;
+	}
+
+	if ((meta_contin->sample_rate_hz != meta_finite->sample_rate_hz) ||
+	    (meta_contin->bits_per_sample != meta_finite->bits_per_sample) ||
+	    (meta_contin->carried_bits_per_sample != meta_finite->carried_bits_per_sample) ||
+	    metadata_num_ch_get(meta_finite) > 1) {
+		LOG_ERR("Sample/Carrier/Finite locations mismatch");
+		return -EINVAL;
+	}
+
+	return contin_array_buf_create(pcm_contin, (void const *const)pcm_finite->data,
+				       meta_finite->bytes_per_location, locations, finite_pos);
 }
