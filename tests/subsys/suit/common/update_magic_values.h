ffe5a23017ef2b5d8891d2fe3e8b1904d389113e
Adam Szczygieł <adam.szczygiel@nordicsemi.no>
2025-07-17 09:46:43 +0200

    tests: subsys: suit: Remove SUIT tests
    Part of SUIT removal activities
    
    Signed-off-by: Adam Szczygieł <adam.szczygiel@nordicsemi.no>

--- tests/subsys/suit/common/update_magic_values.h
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2023 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
- */
-
-#define UPDATE_MAGIC_VALUE_AVAILABLE 0x55AA55AA
-#define UPDATE_MAGIC_VALUE_EMPTY     0xFFFFFFFF
