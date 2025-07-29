1d451276746ac59ebf57f97b45004bd94d253e13
Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>
2025-07-29 17:41:48 +0200

    west_commands: ncs_ironside_se_update: improve robustness of script
    Add various improvements to the west command used to update IronSide SE:
    
    The script now updates both the main and recovery firmwares by default,
    unless you explictly tell it to update just one slot using
    --firmware-slot <slot>. This new default behavior should be more
    intuitive and fit what most users of the script want.
    
    The script now also reads the target firmware version from each
    firmware DFU blob and compares it against the firmware read from the
    device after the update, erroring out if the update was not successful.
    A descriptive error message is output based on the last update status.
    
    Instead of waiting for a fixed time for the device to boot after a
    reset, the script now polls the boot status using nrfutil device
    x-boot-mode-get and continues when it sees a non-zero boot status.
    This should make the script a bit quicker.
    
    The --serial argument is now actually optional like the CLI indicated.
    
    Some of the log messages have been rewritten to try to make them a bit
    clearer.
    
    Ref: NCSDK-34614
    
    Signed-off-by: Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>

--- scripts/west_commands/ncs_ironside_se_update.py
+++ scripts/west_commands/ncs_ironside_se_update.py
@@ -9,163 +9,374 @@ import struct
 import subprocess
 import time
 from ctypes import c_char_p
+from enum import Enum
+from intelhex import IntelHex
 from pathlib import Path, PosixPath
 from tempfile import TemporaryDirectory
+from textwrap import indent
+from typing import Any
 from zipfile import ZipFile
 
 from west.commands import WestCommand
 
-IRONSIDE_SE_VERSION_ADDR = 0x2F88FD04
-IRONSIDE_SE_RECOVERY_VERSION_ADDR = 0x2F88FD14
-UPDATE_STATUS_ADDR = 0x2F88FD24
+IRONSIDE_SE_VERSION_ADDR = 0x2F88_FD04
+IRONSIDE_SE_RECOVERY_VERSION_ADDR = 0x2F88_FD14
 
-UPDATE_STATUS_MSG = {
-    0xFFFFFFFF: "None",
-    0xF0000001: "UnknownOperation",
-    0xF0000002: "InvalidManifest",
-    0xF0000003: "StaleFW",
-    0xF0000005: "VerifyFailure",
-    0xF0000006: "VerifyOK",
-    0xF0000007: "UROTUpdateDisabled",
-    0xF0000008: "UROTActivated",
-    0xF0000009: "RecoveryActivated",
-    0xF000000A: "RecoveryUpdateDisabled",
-    0xF100000A: "AROTRecovery",
-}
+MANIFEST_VERSION_OFFSET = 0x60
+IRONSIDE_VERSION_LEN = 16
+
+UPDATE_STATUS_ADDR = 0x2F88_FD24
+
+
+class UpdateStatus(int, Enum):
+    NONE = 0xFFFF_FFFF
+    INVALID_MANIFEST = 0xF000_0002
+    STALE_FW = 0xF000_0003
+    VERIFY_FAILURE = 0xF000_0005
+    UROT_UPDATE_DISABLED = 0xF000_0007
+    UROT_ACTIVATED = 0xF000_0008
+    RECOVERY_ACTIVATED = 0xF000_0009
+    RECOVERY_UPDATE_DISABLED = 0xF000_000A
+
+    @property
+    def description(self) -> str:
+        match self:
+            case UpdateStatus.NONE:
+                return "no update status"
+            case UpdateStatus.INVALID_MANIFEST:
+                return "the manifest in the update blob contains invalid data"
+            case UpdateStatus.STALE_FW:
+                return (
+                    "the firmware is older than the installed firmware, "
+                    "and firmware downgrades are disabled on the device"
+                )
+            case UpdateStatus.VERIFY_FAILURE:
+                return (
+                    "failed to verify the signature of the firmware, possibly "
+                    "due to it being signed using different keys than those installed in the device"
+                )
+            case UpdateStatus.UROT_UPDATE_DISABLED:
+                return "updates of the IronSide SE firmware are disabled on the device"
+            case UpdateStatus.UROT_ACTIVATED:
+                return "the IronSide SE firmware was successfully updated"
+            case UpdateStatus.RECOVERY_ACTIVATED:
+                return "the IronSide SE Recovery firmware was successfully updated"
+            case UpdateStatus.RECOVERY_UPDATE_DISABLED:
+                return "updates of the IronSide SE Recovery firmware are disabled on the device"
+            case _:
+                return "unrecognized update status"
+
+
+class FirmwareSlot(str, Enum):
+    USLOT = "uslot"
+    RSLOT = "rslot"
+
+    @property
+    def description(self) -> str:
+        match self:
+            case FirmwareSlot.USLOT:
+                return "IronSide SE"
+            case FirmwareSlot.RSLOT:
+                return "IronSide SE Recovery"
 
 
 class NcsIronSideSEUpdate(WestCommand):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(
             name="ncs-ironside-se-update",
             help="NCS IronSide SE update",
             description="Update IronSide SE.",
         )
 
-    def do_add_parser(self, parser_adder):
+    def do_add_parser(self, parser_adder: Any) -> argparse.ArgumentParser:
         parser = parser_adder.add_parser(
             self.name, help=self.help, description=self.description
         )
-
         parser.add_argument(
             "--zip",
+            required=True,
             help="Path to IronSide SE release ZIP",
             type=argparse.FileType(mode="r"),
         )
-
         parser.add_argument(
-            "--recovery",
-            help="Update IronSide SE recovery instead of IronSide SE itself",
+            "--serial",
+            dest="serial_number",
+            type=str,
+            help="Serial number",
+        )
+        parser.add_argument(
+            "--firmware-slot",
+            help="Only update the given firmware slot instead of updating both slots",
+            choices=[m.value for m in FirmwareSlot.__members__.values()],
+            type=FirmwareSlot,
+        )
+        parser.add_argument(
+            "--allow-erase",
             action="store_true",
+            help="Allow erasing the device (this option is currently required)",
         )
-
         parser.add_argument(
-            "--allow-erase", action="store_true", help="Allow erasing the device"
-        )
-        parser.add_argument("--serial", type=str, help="serial number", default=None)
-        parser.add_argument(
-            "--wait", help="Wait time (in seconds) between resets", default=2
+            "--wait-time",
+            type=float,
+            help="Timeout in seconds to wait for the device to boot",
+            default=2.0,
         )
 
         return parser
 
-    def _decode_version(self, version_bytes: bytes) -> str:
-        # First word is semantic versioned 8 bit each
-        seqnum, patch, minor, major = struct.unpack("bbbb", version_bytes[0:4])
-
-        extraversion = c_char_p(version_bytes[4:]).value.decode("utf-8")
-
-        return f"{major}.{minor}.{patch}-{extraversion}+{seqnum}"
-
-    def _decode_status(self, status_bytes: bytes) -> str:
-        status = int.from_bytes(status_bytes, "little")
-        try:
-            return f"{hex(status)} - {UPDATE_STATUS_MSG[status]}"
-        except KeyError:
-            return f"{hex(status)} - (unknown)"
-
-    def do_run(self, args, unknown_args):
-        def nrfutil_device(cmd: str) -> str:
-            cmd = f"nrfutil device {cmd} --serial-number {args.serial}"
-            self.dbg(cmd)
-
-            result = subprocess.run(cmd, shell=True, text=True, capture_output=True)
-
-            self.dbg(result.stdout)
-
-            if result.returncode != 0:
-                self.die(f"{cmd} returned '{result.returncode}' and '{result.stderr.strip()}'")
-
-            return result.stdout
-
-        def nrfutil_read(address: int, num_bytes: int) -> bytes:
-            json_out = json.loads(
-                nrfutil_device(
-                    f"x-read --direct --json --skip-overhead --address 0x{address:x} --bytes {num_bytes}"
-                )
-            )
-            return bytes(json_out["devices"][0]["memoryData"][0]["values"])
-
-        def get_version() -> str:
-            address = (
-                IRONSIDE_SE_RECOVERY_VERSION_ADDR
-                if args.recovery
-                else IRONSIDE_SE_VERSION_ADDR
-            )
-            return self._decode_version(nrfutil_read(address, 16))
-
-        def get_status() -> str:
-            return self._decode_status(nrfutil_read(UPDATE_STATUS_ADDR, 4))
-
-        def program(hex_file: PosixPath) -> None:
-            if not hex_file.exists():
-                self.die(f"Firmware file does not exist: {hex_file}")
-
-            nrfutil_device(
-                f"program --options chip_erase_mode=ERASE_NONE --firmware {hex_file}"
-            )
+    def do_run(self, args: argparse.Namespace, unknown: list[str]) -> None:
+        common_kwargs = {
+            "serial_number": args.serial_number,
+            "wait_time": args.wait_time,
+        }
 
         if not args.allow_erase:
             self.die(
                 "Unable to perform update without erasing the device, set '--allow-erase'"
             )
+
         with TemporaryDirectory() as tmpdir:
             with ZipFile(args.zip.name, "r") as zip_ref:
                 zip_ref.extractall(tmpdir)
-                update_app = Path(tmpdir, "update/update_application.hex")
-                if args.recovery:
-                    update_hex = "ironside_se_recovery_update.hex"
-                else:
-                    update_hex = "ironside_se_update.hex"
+                update_dir = Path(tmpdir, "update")
+                update_app = update_dir / "update_application.hex"
+                match getattr(args, "firmware_slot"):
+                    case FirmwareSlot.USLOT:
+                        update_hex_files = [
+                            (FirmwareSlot.USLOT, update_dir / "ironside_se_update.hex")
+                        ]
+                    case FirmwareSlot.RSLOT:
+                        update_hex_files = [
+                            (
+                                FirmwareSlot.RSLOT,
+                                update_dir / "ironside_se_recovery_update.hex",
+                            )
+                        ]
+                    case _:
+                        update_hex_files = [
+                            (FirmwareSlot.USLOT, update_dir / "ironside_se_update.hex"),
+                            (
+                                FirmwareSlot.RSLOT,
+                                update_dir / "ironside_se_recovery_update.hex",
+                            ),
+                        ]
 
-                update_to_install = Path(tmpdir, "update", update_hex)
-
-                # Check if required files exist in the extracted ZIP
                 if not update_app.exists():
-                    self.die(f"Update application file not found in ZIP: {update_app}")
+                    self.die(
+                        f"Update application file not found in ZIP: "
+                        f"{update_app.relative_to(update_dir)}"
+                    )
 
-                if not update_to_install.exists():
-                    self.die(f"Update firmware file not found in ZIP: {update_to_install}")
+                target_versions = {}
+                for slot, update_file in update_hex_files:
+                    if not update_file.exists():
+                        self.die(
+                            f"Update firmware file for {slot.description} ({slot.value}) "
+                            f"not found in ZIP: {update_file.relative_to(update_dir)}"
+                        )
+                    target_versions[slot] = self._load_update_version(update_file)
 
-                self.inf(
-                    f"Version before update: {get_version()}, status: {get_status()}"
-                )
-
-                program(update_app)
-                program(update_to_install)
-
-                self.dbg("Reset to execute update service")
-                nrfutil_device(f"reset --reset-kind RESET_PIN")
-                time.sleep(args.wait)
-
-                self.dbg("Reset to trigger update installation")
-                nrfutil_device(f"reset --reset-kind RESET_PIN")
-                self.dbg("Waiting for update to complete")
-                time.sleep(args.wait)
-
-                self.inf(
-                    f"Version after update: {get_version()}, status: {get_status()}"
+                fw_version_before = self._read_versions_from_device(**common_kwargs)
+                target_version_diff = self._fmt_versions(
+                    before=fw_version_before,
+                    after=target_versions,
                 )
                 self.inf(
-                    "The update application is still programmed, please program proper image"
+                    f"Updating IronSide SE firmware:\n{indent(target_version_diff, '  ')}\n"
                 )
+
+                self.dbg("Programming application firmware used to trigger the update")
+                self._program(update_app, erase=True, **common_kwargs)
+
+                update_failed = False
+                last_update_status = UpdateStatus.NONE
+
+                for slot, update_file in update_hex_files:
+                    slot_target_version = target_versions[slot]
+                    self.inf(
+                        f"Updating {slot.description} ({slot.name}) "
+                        f"to {slot_target_version}"
+                    )
+
+                    update_status = self._read_update_status(**common_kwargs)
+                    self.dbg(f"Status before triggering the update: {update_status}")
+
+                    self._program(update_file, **common_kwargs)
+
+                    self.dbg("Reset to execute update service")
+                    self._nrfutil_device("reset", **common_kwargs)
+                    # Wait for the application to boot
+                    self._wait_for_bootstatus(**common_kwargs)
+                    # Wait for the application to execute
+                    time.sleep(0.200)
+
+                    self.dbg("Reset to trigger update installation")
+                    self._nrfutil_device(
+                        "reset --reset-kind RESET_VIA_SECDOM", **common_kwargs
+                    )
+                    self.dbg("Waiting for update to complete")
+                    self._wait_for_bootstatus(**common_kwargs)
+
+                    slot_version = self._read_firmware_version(slot, **common_kwargs)
+                    last_update_status = self._read_update_status(**common_kwargs)
+                    self.dbg(
+                        f"Version after update: {slot_version}, status: {update_status.name}"
+                    )
+
+                    if slot_version != slot_target_version:
+                        update_failed = True
+                        self.err(
+                            f"Failed to update {slot.description} ({slot.name}) "
+                            f"to {slot_target_version}"
+                        )
+                        break
+
+                self.dbg("Erasing application firmware used to trigger the update")
+                self._nrfutil_device("erase --all", **common_kwargs)
+
+                if update_failed:
+                    fw_version_after = self._read_versions_from_device(**common_kwargs)
+                    version_str = self._fmt_versions(
+                        before=None, after=fw_version_after
+                    )
+                    self.inf(
+                        f"The device is left with IronSide SE firmware:\n"
+                        f"{indent(version_str, '  ')}\n"
+                    )
+                    if last_update_status != UpdateStatus.NONE:
+                        fail_status_str = (
+                            f"{last_update_status.name} - "
+                            f"{last_update_status.description}"
+                        )
+                    else:
+                        fail_status_str = (
+                            "The update status did not "
+                            "indicate why the failure happened"
+                        )
+                    self.die(fail_status_str)
+
+    def _program(self, hex_file: PosixPath, erase: bool = False, **kwargs: Any) -> None:
+        if not hex_file.exists():
+            self.die(f"Firmware file does not exist: {hex_file}")
+
+        options = " --options chip_erase_mode=ERASE_NONE" if not erase else ""
+        self._nrfutil_device(f"program{options} --firmware {hex_file}", **kwargs)
+
+    def _read_versions_from_device(self, **kwargs: Any) -> dict:
+        return {
+            FirmwareSlot.USLOT: self._read_firmware_version(
+                FirmwareSlot.USLOT, **kwargs
+            ),
+            FirmwareSlot.RSLOT: self._read_firmware_version(
+                FirmwareSlot.RSLOT, **kwargs
+            ),
+        }
+
+    def _nrfutil_device(
+        self,
+        cmd: str,
+        serial_number: str | None = None,
+        die_on_error: bool = True,
+        **kwargs: Any,
+    ) -> str:
+        if serial_number is not None:
+            optional_args = f" --serial-number {serial_number}"
+        else:
+            optional_args = ""
+
+        cmd = f"nrfutil device {cmd}{optional_args} --x-sdfw-variant ironside"
+        self.dbg(cmd)
+
+        result = subprocess.run(cmd, shell=True, text=True, capture_output=True)
+
+        self.dbg(result.stdout)
+
+        if result.returncode != 0:
+            if die_on_error:
+                self.die(
+                    f"{cmd} returned '{result.returncode}' and '{result.stderr.strip()}'"
+                )
+            else:
+                return ""
+
+        return result.stdout
+
+    def _nrfutil_read(self, address: int, num_bytes: int, **kwargs: Any) -> bytes:
+        json_out = json.loads(
+            self._nrfutil_device(
+                f"x-read --direct --address 0x{address:x} --bytes {num_bytes} "
+                "--json --skip-overhead",
+                **kwargs,
+            )
+        )
+        return bytes(json_out["devices"][0]["memoryData"][0]["values"])
+
+    def _wait_for_bootstatus(self, wait_time: float, **kwargs: Any) -> int:
+        boot_status = None
+        start = time.perf_counter()
+        while not boot_status:
+            output_raw = self._nrfutil_device(
+                "x-boot-status-get --json --skip-overhead",
+                die_on_error=False,
+                **kwargs,
+            )
+            if not output_raw:
+                continue
+
+            output_json = json.loads(output_raw)
+            boot_status = output_json["devices"][0]["boot_status"]
+            if (time.perf_counter() - start) >= wait_time:
+                break
+
+        if not boot_status:
+            self.die("Timed out waiting for a non-zero bootstatus")
+
+        return boot_status
+
+    def _read_firmware_version(self, slot: FirmwareSlot, **kwargs: Any) -> str:
+        address = (
+            IRONSIDE_SE_RECOVERY_VERSION_ADDR
+            if slot == FirmwareSlot.RSLOT
+            else IRONSIDE_SE_VERSION_ADDR
+        )
+        return self._decode_version(self._nrfutil_read(address, 16, **kwargs))
+
+    def _read_update_status(self, **kwargs: Any) -> UpdateStatus:
+        return self._decode_status(self._nrfutil_read(UPDATE_STATUS_ADDR, 4, **kwargs))
+
+    def _load_update_version(self, update_file: Path) -> str:
+        self.dbg(f"Reading firmware version from {update_file}")
+        ihex = IntelHex(str(update_file))
+        version_addr = ihex.minaddr() + MANIFEST_VERSION_OFFSET
+        version_bytes = bytes(
+            [ihex[a] for a in range(version_addr, version_addr + IRONSIDE_VERSION_LEN)]
+        )
+        return self._decode_version(version_bytes)
+
+    def _decode_version(self, version_bytes: bytes) -> str:
+        # First word is semantic versioned 8 bit each
+        seqnum, patch, minor, major = struct.unpack("bbbb", version_bytes[0:4])
+        extraversion = c_char_p(version_bytes[4:]).value.decode("utf-8")
+        return f"{major}.{minor}.{patch}-{extraversion}+{seqnum}"
+
+    def _decode_status(self, status_bytes: bytes) -> UpdateStatus:
+        status = int.from_bytes(status_bytes, "little")
+        try:
+            return UpdateStatus(status)
+        except ValueError:
+            self.die(
+                f"Read unrecognized update status from the device: 0x{status:09_X}"
+            )
+
+    def _fmt_versions(
+        self, *, before: dict[FirmwareSlot, str] | None, after: dict[FirmwareSlot, str]
+    ) -> str:
+        lines = []
+        for slot in after:
+            if before is not None:
+                slot_versions = f"{before[slot]} -> {after[slot]}"
+            else:
+                slot_versions = after[slot]
+            slot_name = f"{slot.description} ({slot.name})"
+            lines.append(f"{slot_versions} - {slot_name}")
+        return "\n".join(lines)
