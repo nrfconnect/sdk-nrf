48d91fe528e677d6f1bb573bba851fc91d58c6b6
Mateusz Michalek <mateusz.michalek@nordicsemi.no>
2025-07-31 13:37:50 +0200

    tests: bootloader: NSIB MCUBoot locks
    complete set of tests for region 3 and 4 lockouts.
    
    Signed-off-by: Mateusz Michalek <mateusz.michalek@nordicsemi.no>

--- tests/subsys/bootloader/b0_lock_rwx/src/main.c
+++ tests/subsys/bootloader/b0_lock_rwx/src/main.c
@@ -8,7 +8,8 @@
 #include <hal/nrf_rramc.h>
 #include <nrfx_rramc.h>
 
-#define RRAMC_REGION_FOR_BOOTCONF 3
+#define RRAMC_REGION_FOR_TEST CONFIG_TEST_B0_LOCK_REGION
+
 static uint32_t expected_fatal;
 static uint32_t actual_fatal;
 static nrf_rramc_region_config_t config;
@@ -29,21 +30,46 @@ void check_fatal(void *unused)
 void *get_config(void)
 {
 	nrf_rramc_region_config_get(NRF_RRAMC,
-			RRAMC_REGION_FOR_BOOTCONF,
+			RRAMC_REGION_FOR_TEST,
 			&config);
+#if defined(CONFIG_TEST_B0_LOCK_READS)
+	zassert_equal(0, config.permissions &
+			(NRF_RRAMC_REGION_PERM_READ_MASK |
+			 NRF_RRAMC_REGION_PERM_WRITE_MASK |
+			 NRF_RRAMC_REGION_PERM_EXECUTE_MASK),
+			"Read Write and eXecute permissions aren't cleared");
+#else
 	zassert_equal(0, config.permissions &
 			(NRF_RRAMC_REGION_PERM_WRITE_MASK),
 			"Write permission isn't cleared");
+#endif
 	zassert_true(config.size_kb > 0, "Protected region has zero size.");
 	return NULL;
 }
 
 ZTEST(b0_self_lock_test, test_reading_b0_image)
 {
+	printk("Region %d\n", RRAMC_REGION_FOR_TEST);
 	uint32_t protected_end_address = 1024 * config.size_kb;
 	volatile uint32_t *unprotected_word = (volatile uint32_t *)protected_end_address;
 	volatile uint32_t *protected_word =
 		(volatile uint32_t *)protected_end_address - sizeof(uint32_t);
+
+	config.permissions = NRF_RRAMC_REGION_PERM_READ_MASK |
+			     NRF_RRAMC_REGION_PERM_WRITE_MASK |
+			     NRF_RRAMC_REGION_PERM_EXECUTE_MASK;
+	/* Try unlocking. This should take no effect at this point */
+	nrf_rramc_region_config_set(NRF_RRAMC, RRAMC_REGION_FOR_TEST, &config);
+
+#if defined(CONFIG_TEST_B0_LOCK_READS)
+	printk("Legal read\n");
+	int val = *unprotected_word;
+
+	printk("Illegal read\n");
+	expected_fatal++;
+	__DSB();
+	val = *protected_word;
+#else
 	uint32_t test_value = ~(*unprotected_word);
 
 	printk("Legal write\n");
@@ -56,15 +82,13 @@ ZTEST(b0_self_lock_test, test_reading_b0_image)
 	nrf_rramc_word_write((uint32_t)unprotected_word, test_value);
 	zassert_equal(test_value, *unprotected_word,
 			"Legal write value doesn't match.");
-	config.permissions = NRF_RRAMC_REGION_PERM_READ_MASK |
-			     NRF_RRAMC_REGION_PERM_WRITE_MASK |
-			     NRF_RRAMC_REGION_PERM_EXECUTE_MASK;
-	/* Try unlocking. This should take no effect at this point */
-	nrf_rramc_region_config_set(NRF_RRAMC, RRAMC_REGION_FOR_BOOTCONF, &config);
 	printk("Illegal write\n");
 	expected_fatal++;
 	__DSB();
 	nrf_rramc_word_write((uint32_t)protected_word, test_value);
+
+#endif
+
 }
 
 ZTEST_SUITE(b0_self_lock_test, NULL, get_config, NULL, check_fatal, NULL);
