de87fe2b321b0b6a8b56991fdcc84456d7383d9a
Kari Hamalainen <kari.hamalainen@nordicsemi.no>
2025-08-11 09:10:14 +0300

    ci: add nightly twister run
    Add twister nightly workflow for Mac and Linux.
    
    Signed-off-by: Kari Hamalainen <kari.hamalainen@nordicsemi.no>

---
+++ scripts/ci/twister_runner.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+EXTRA_FLAGS=""
+NRFUTIL=""
+OS="$(uname)"
+TOOLCHAIN_BUNDLE_ID=$(./nrf/scripts/print_toolchain_checksum.sh)
+
+echo "Detected OS: $OS"
+echo "Toolchain bundle: $TOOLCHAIN_BUNDLE_ID"
+echo "Using twister arguments: $@"
+
+# Detect OS and set OS-specific commands
+case "$(uname -s)" in
+    MINGW*)
+        EXTRA_FLAGS="--short-build-path"
+        export PYTHONUTF8=1
+        NRFUTIL="../nrfutil.exe sdk-manager toolchain launch --toolchain-bundle-id $TOOLCHAIN_BUNDLE_ID -- python zephyr\\scripts\\twister"
+        ;;
+    Linux*)
+        NRFUTIL="../nrfutil sdk-manager toolchain launch --toolchain-bundle-id $TOOLCHAIN_BUNDLE_ID -- python zephyr/scripts/twister"
+        ;;
+    Darwin*)
+        EXTRA_FLAGS=""
+        NRFUTIL="sudo ../nrfutil sdk-manager toolchain launch --toolchain-bundle-id $TOOLCHAIN_BUNDLE_ID -- python zephyr/scripts/twister"
+        ;;
+    *)
+        echo "Unsupported OS: $(uname -s)"
+        exit 1
+        ;;
+esac
+
+set -x
+
+$NRFUTIL "$@" $EXTRA_FLAGS
