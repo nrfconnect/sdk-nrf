93cd302b2cba53901835ffed04b2ba579b78737b
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2025-08-13 14:14:27 +0200

    nrf_security: Avoid adding Cracen ctr_drbg for tests
    Don't add the file ctr_drbg.c when you build the tests.
    While testing a modified version of this file is used
    which implements the same symbols so this file cannot
    be included.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/cracenpsa.cmake
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/cracenpsa.cmake
@@ -21,9 +21,14 @@ list(APPEND cracen_driver_sources
   # Note: We always need to have cipher.c and ctr_drbg.c since it
   # is used directly by many Cracen drivers.
   ${CMAKE_CURRENT_LIST_DIR}/src/cipher.c
-  ${CMAKE_CURRENT_LIST_DIR}/src/ctr_drbg.c
   ${CMAKE_CURRENT_LIST_DIR}/src/prng_pool.c
 )
+
+if(NOT CONFIG_PSA_CRYPTO_DRIVER_ALG_PRNG_TEST)
+  list(APPEND cracen_driver_sources
+    ${CMAKE_CURRENT_LIST_DIR}/src/ctr_drbg.c
+  )
+endif()
 
 if(CONFIG_CRACEN_IKG)
   list(APPEND cracen_driver_sources
