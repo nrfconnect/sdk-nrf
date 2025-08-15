c9bee60b92dfeb6d69e91033b192dfd9ee36744c
Olivier Lesage <olivier.lesage@nordicsemi.no>
2025-08-15 12:45:42 +0200

    samples: clarify printout for distance estimates
    There was some confusion about what these printouts are.
    It's the latest measurements, so event if no new measurements have been
    made, the sample will continue printing out the old estimates.
    
    Signed-off-by: Olivier Lesage <olivier.lesage@nordicsemi.no>

--- samples/bluetooth/channel_sounding_ras_initiator/src/main.c
+++ samples/bluetooth/channel_sounding_ras_initiator/src/main.c
@@ -710,7 +710,7 @@ int main(void)
 			for (uint8_t ap = 0; ap < MAX_AP; ap++) {
 				cs_de_dist_estimates_t distance_on_ap = get_distance(ap);
 
-				LOG_INF("Distance estimates on antenna path %u: ifft: %f, "
+				LOG_INF("Latest distance estimates on antenna path %u: ifft: %f, "
 					"phase_slope: %f, rtt: %f",
 					ap, (double)distance_on_ap.ifft,
 					(double)distance_on_ap.phase_slope,
