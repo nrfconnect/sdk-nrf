206de6b66920501ae14b0b0aadffd16f02f4af21
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2025-08-18 14:05:32 +0200

    CRACEN: Avoid "unused function" for RSA helper functions
    -Change cracen_ffkey_write_sz and cracen_ffkey_write functions from
     static to static inline to avoid "unused functions" warning when these
     are not used
    
     See sdk-nrf PR-22619 for details
    
    ref: NCSDK-28901
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/src/rsa_key.h
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/src/rsa_key.h
@@ -12,7 +12,7 @@
 #include <silexpk/sxbuf/sxbufop.h>
 
 /** Write the sizes of the elements of a RSA key. */
-static void cracen_ffkey_write_sz(const struct cracen_rsa_key *key, int *sizes)
+static inline void cracen_ffkey_write_sz(const struct cracen_rsa_key *key, int *sizes)
 {
 	int slotidx = 0;
 	int i = 0;
@@ -29,7 +29,7 @@ static void cracen_ffkey_write_sz(const struct cracen_rsa_key *key, int *sizes)
 }
 
 /** Write the elements of a RSA key into the input slots. */
-static void cracen_ffkey_write(const struct cracen_rsa_key *key, struct sx_pk_slot *inputs)
+static inline void cracen_ffkey_write(const struct cracen_rsa_key *key, struct sx_pk_slot *inputs)
 {
 	int slotidx = 0;
 	int i = 0;
