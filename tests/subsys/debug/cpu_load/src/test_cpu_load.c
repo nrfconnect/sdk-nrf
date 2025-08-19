45b5e6b693d23be31e0bd63e3e6d14121e36b324
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2025-08-19 14:13:17 +0200

    tests: debug: cpu_load: Align test to module updates
    Align test to cpu_load_init deprecation. Limit testing with shared
    DPPI channels to nRF91 only because nRF54L does not support it.
    Enable test for nrf54l15dk//cpuapp.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- tests/subsys/debug/cpu_load/src/test_cpu_load.c
+++ tests/subsys/debug/cpu_load/src/test_cpu_load.c
@@ -12,29 +12,17 @@
 #include <nrfx_timer.h>
 #include <hal/nrf_power.h>
 
-#ifdef DPPI_PRESENT
+#ifdef CONFIG_NRF_CPU_LOAD_USE_SHARED_DPPI_CHANNELS
 #include <nrfx_dppi.h>
 
 static void timer_handler(nrf_timer_event_t event_type, void *context)
 {
 	/*empty*/
 }
-#endif
 
-#define FULL_LOAD 100000
-#define SMALL_LOAD 3000
-
-ZTEST(cpu_load, test_cpu_load)
+static int dppi_shared_resources_init(void)
 {
-	int err;
-	uint32_t load;
-
-	if (IS_ENABLED(CONFIG_SOC_NRF9160)) {
-		/* Wait for LF clock stabilization. */
-		k_busy_wait(500000);
-	}
-
-#ifdef DPPI_PRESENT
+	nrfx_err_t err;
 	static nrfx_timer_t timer = NRFX_TIMER_INSTANCE(1);
 	uint32_t base_frequency = NRF_TIMER_BASE_FREQUENCY_GET(timer.p_reg);
 	nrfx_timer_config_t config = NRFX_TIMER_DEFAULT_CONFIG(base_frequency);
@@ -46,30 +34,32 @@ ZTEST(cpu_load, test_cpu_load)
 	config.frequency = NRFX_MHZ_TO_HZ(1);
 	config.bit_width = NRF_TIMER_BIT_WIDTH_32;
 
-	nrfx_dppi_t dppi = NRFX_DPPI_INSTANCE(0);
-
 	err = nrfx_timer_init(&timer, &config, timer_handler);
 	zassert_equal(err, NRFX_SUCCESS, "Unexpected error:%d", err);
 
-	err = nrfx_dppi_channel_alloc(&dppi, &ch);
+	err = nrfx_gppi_channel_alloc(&ch);
 	zassert_equal(err, NRFX_SUCCESS, "Unexpected error:%d", err);
 
 	nrfx_gppi_channel_endpoints_setup(ch, evt, tsk);
 	nrfx_gppi_channels_enable(BIT(ch));
 
-	if (!IS_ENABLED(CONFIG_NRF_CPU_LOAD_USE_SHARED_DPPI_CHANNELS)) {
-		err = cpu_load_init();
-		zassert_equal(err, -ENODEV, "Unexpected err:%d", err);
+	return 0;
+}
 
-		nrfx_gppi_channels_disable(BIT(ch));
-		nrfx_gppi_event_endpoint_clear(ch, evt);
-		nrfx_gppi_task_endpoint_clear(ch, tsk);
-		err = nrfx_dppi_channel_free(&dppi, ch);
+SYS_INIT(dppi_shared_resources_init, PRE_KERNEL_1, 0);
+#endif /* CONFIG_NRF_CPU_LOAD_USE_SHARED_DPPI_CHANNELS */
+
+#define FULL_LOAD 100000
+#define SMALL_LOAD 3000
+
+ZTEST(cpu_load, test_cpu_load)
+{
+	int load;
+
+	if (IS_ENABLED(CONFIG_SOC_NRF9160)) {
+		/* Wait for LF clock stabilization. */
+		k_busy_wait(500000);
 	}
-#endif
-
-	err = cpu_load_init();
-	zassert_equal(err, 0, "Unexpected err:%d", err);
 
 	/* Busy wait for 10 ms */
 	k_busy_wait(10000);
