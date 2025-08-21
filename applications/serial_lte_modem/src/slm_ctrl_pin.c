nrfconnect/sdk-nrf

commit 317368495528f3c96a94c1d63c9b53ae30aff189

applications: serial_lte_modem: Renaming some slm_ctrl_pin functions
Renaming some slm_ctrl_pin functions after moving them here from main.c.

Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

applications/serial_lte_modem/src/slm_ctrl_pin.c
@@ -78,28 +78,6 @@ static int configure_gpio(gpio_pin_t pin, gpio_flags_t flags)
 }
 #endif
 
-static int init_gpios(void)
-{
-	if (!device_is_ready(gpio_dev)) {
-		LOG_ERR("GPIO controller not ready");
-		return -ENODEV;
-	}
-
-#if POWER_PIN_IS_ENABLED
-	if (configure_gpio(CONFIG_SLM_POWER_PIN, GPIO_INPUT | GPIO_PULL_UP | GPIO_ACTIVE_LOW)) {
-		return -1;
-	}
-#endif
-
-#if INDICATE_PIN_IS_ENABLED
-	if (configure_gpio(CONFIG_SLM_INDICATE_PIN, GPIO_OUTPUT_INACTIVE | GPIO_ACTIVE_LOW)) {
-		return -1;
-	}
-#endif
-
-	return 0;
-}
-
 #if POWER_PIN_IS_ENABLED
 
 static int configure_power_pin_interrupt(gpio_callback_handler_t handler, gpio_flags_t flags)
@@ -135,15 +113,15 @@ static int configure_power_pin_interrupt(gpio_callback_handler_t handler, gpio_f
 	return 0;
 }
 
-static void slm_enter_sleep_work_fn(struct k_work *)
+static void slm_ctrl_pin_enter_sleep_work_fn(struct k_work *)
 {
-	slm_enter_sleep();
+	slm_ctrl_pin_enter_sleep();
 }
 
 static void power_pin_callback_poweroff(const struct device *dev,
 					struct gpio_callback *gpio_callback, uint32_t)
 {
-	static K_WORK_DEFINE(work, slm_enter_sleep_work_fn);
+	static K_WORK_DEFINE(work, slm_ctrl_pin_enter_sleep_work_fn);
 
 	LOG_INF("Power off triggered.");
 	gpio_remove_callback(dev, gpio_callback);
@@ -224,19 +202,7 @@ static void power_pin_callback_wakeup(const struct device *dev,
 	k_work_submit(&work);
 }
 
-static void enter_sleep_no_uninit(void)
-{
-	LOG_INF("Entering sleep.");
-	LOG_PANIC();
-	nrf_gpio_cfg_sense_set(CONFIG_SLM_POWER_PIN, NRF_GPIO_PIN_SENSE_LOW);
-
-	k_sleep(K_MSEC(100));
-
-	nrf_regulators_system_off(NRF_REGULATORS_NS);
-	assert(false);
-}
-
-int slm_indicate(void)
+int slm_ctrl_pin_indicate(void)
 {
 	int err = 0;
 
@@ -255,7 +221,7 @@ int slm_indicate(void)
 	return err;
 }
 
-void slm_enter_idle(void)
+void slm_ctrl_pin_enter_idle(void)
 {
 	LOG_INF("Entering idle.");
 	int err;
@@ -273,7 +239,7 @@ void slm_enter_idle(void)
 	}
 }
 
-void slm_enter_sleep(void)
+void slm_ctrl_pin_enter_sleep(void)
 {
 	slm_at_host_uninit();
 
@@ -283,12 +249,24 @@ void slm_enter_sleep(void)
 	if (!slm_is_modem_functional_mode(LTE_LC_FUNC_MODE_OFFLINE)) {
 		slm_power_off_modem();
 	}
-	enter_sleep_no_uninit();
+	slm_ctrl_pin_enter_sleep_no_uninit();
+}
+
+void slm_ctrl_pin_enter_sleep_no_uninit(void)
+{
+	LOG_INF("Entering sleep.");
+	LOG_PANIC();
+	nrf_gpio_cfg_sense_set(CONFIG_SLM_POWER_PIN, NRF_GPIO_PIN_SENSE_LOW);
+
+	k_sleep(K_MSEC(100));
+
+	nrf_regulators_system_off(NRF_REGULATORS_NS);
+	assert(false);
 }
 
 #endif /* POWER_PIN_IS_ENABLED */
 
-void slm_enter_shutdown(void)
+void slm_ctrl_pin_enter_shutdown(void)
 {
 	LOG_INF("Entering shutdown.");
 
@@ -309,9 +287,18 @@ void slm_enter_shutdown(void)
 
 void slm_ctrl_pin_init_gpios(void)
 {
-	if (init_gpios() != 0) {
-		LOG_WRN("Failed to init GPIO pins.");
+	if (!device_is_ready(gpio_dev)) {
+		LOG_ERR("GPIO controller not ready");
+		return;
 	}
+
+#if POWER_PIN_IS_ENABLED
+	(void)configure_gpio(CONFIG_SLM_POWER_PIN, GPIO_INPUT | GPIO_PULL_UP | GPIO_ACTIVE_LOW);
+#endif
+
+#if INDICATE_PIN_IS_ENABLED
+	(void)configure_gpio(CONFIG_SLM_INDICATE_PIN, GPIO_OUTPUT_INACTIVE | GPIO_ACTIVE_LOW);
+#endif
 }
 
 int slm_ctrl_pin_init(void)
@@ -332,9 +319,6 @@ int slm_ctrl_pin_init(void)
 	 */
 	err = configure_power_pin_interrupt(power_pin_callback_enable_poweroff,
 					    GPIO_INT_EDGE_FALLING);
-	if (err) {
-		return err;
-	}
 #endif
 	return err;
 }

applications/serial_lte_modem/src/main.c
@@ -260,7 +260,7 @@ int main(void)
 
 	if (!(rr & NRF_POWER_RESETREAS_OFF_MASK)) { /* DETECT signal from GPIO */
 
-		enter_sleep_no_uninit();
+		slm_ctrl_pin_enter_sleep_no_uninit();
 	}
 #endif /* CONFIG_SLM_START_SLEEP */
 

applications/serial_lte_modem/src/slm_at_commands.c
@@ -133,12 +133,12 @@ static void go_sleep_wk(struct k_work *)
 {
 	if (sleep_control.mode == SLEEP_MODE_IDLE) {
 		if (slm_at_host_power_off() == 0) {
-			slm_enter_idle();
+			slm_ctrl_pin_enter_idle();
 		} else {
 			LOG_ERR("failed to power off UART");
 		}
 	} else if (sleep_control.mode == SLEEP_MODE_DEEP) {
-		slm_enter_sleep();
+		slm_ctrl_pin_enter_sleep();
 	}
 }
 
@@ -183,7 +183,7 @@ static void slm_shutdown(void)
 	slm_at_host_uninit();
 	slm_power_off_modem();
 	LOG_PANIC();
-	slm_enter_shutdown();
+	slm_ctrl_pin_enter_shutdown();
 }
 
 SLM_AT_CMD_CUSTOM(xshutdown, "AT#XSHUTDOWN", handle_at_shutdown);

applications/serial_lte_modem/src/slm_at_host.c
@@ -510,7 +510,7 @@ static int slm_at_send_indicate(const uint8_t *data, size_t len,
 
 		pm_device_state_get(slm_uart_dev, &state);
 		if (state != PM_DEVICE_STATE_ACTIVE) {
-			slm_indicate();
+			slm_ctrl_pin_indicate();
 		}
 	}
 

applications/serial_lte_modem/src/slm_ctrl_pin.h
@@ -13,14 +13,43 @@
  * @{
  */
 
-void slm_enter_idle(void);
-void slm_enter_sleep(void);
-void slm_enter_shutdown(void);
+/**
+ * @brief Enter idle.
+ */
+void slm_ctrl_pin_enter_idle(void);
 
-/** @brief Temporarily sets the indicate pin high. */
-int slm_indicate(void);
+/**
+ * @brief Enter sleep.
+ */
+void slm_ctrl_pin_enter_sleep(void);
 
+/**
+ * @brief Enter sleep without uninitializing AT host.
+ */
+void slm_ctrl_pin_enter_sleep_no_uninit(void);
+
+/**
+ * @brief nRF91 Series SiP enters System OFF mode.
+ */
+void slm_ctrl_pin_enter_shutdown(void);
+
+/**
+ * @brief Temporarily sets the indicate pin high.
+ *
+ * @retval 0 on success, nonzero otherwise.
+ */
+int slm_ctrl_pin_indicate(void);
+
+/**
+ * @brief Initialize SLM control pin module.
+ *
+ * @retval 0 on success, nonzero otherwise.
+ */
 int slm_ctrl_pin_init(void);
+
+/**
+ * @brief Initialize SLM control pins, that is, power and indicate pins.
+ */
 void slm_ctrl_pin_init_gpios(void);
 
 /** @} */

applications/serial_lte_modem/src/slm_ppp.c
@@ -726,7 +726,7 @@ static void ppp_data_passing_thread(void*, void*, void*)
 				pm_device_state_get(ppp_uart_dev, &state);
 				if (state != PM_DEVICE_STATE_ACTIVE) {
 					LOG_DBG("PPP data received but UART not active");
-					slm_indicate();
+					slm_ctrl_pin_indicate();
 				}
 			}
 			const ssize_t len =

commit 2f3fc3df81f0a957aaf6dd28320d3004cbe87674

applications: serial_lte_modem: Fix compilation without pins
Fix compilation errors without power or indicate pins configured, i.e.,
having them -1. This is regression from PR #22870.
Also added these configurations into twister runs.

Jira: SLM-121

Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

applications/serial_lte_modem/sample.yaml
@@ -22,6 +22,46 @@ tests:
       - ci_build
       - sysbuild
       - ci_applications_serial_lte_modem
+  applications.serial_lte_modem_no_power_indicate:
+    sysbuild: true
+    build_only: true
+    extra_configs:
+      - CONFIG_SLM_POWER_PIN=-1
+      - CONFIG_SLM_INDICATE_PIN=-1
+    platform_allow:
+      - nrf9151dk/nrf9151/ns
+    integration_platforms:
+      - nrf9151dk/nrf9151/ns
+    tags:
+      - ci_build
+      - sysbuild
+      - ci_applications_serial_lte_modem
+  applications.serial_lte_modem_no_power:
+    sysbuild: true
+    build_only: true
+    extra_configs:
+      - CONFIG_SLM_POWER_PIN=-1
+    platform_allow:
+      - nrf9151dk/nrf9151/ns
+    integration_platforms:
+      - nrf9151dk/nrf9151/ns
+    tags:
+      - ci_build
+      - sysbuild
+      - ci_applications_serial_lte_modem
+  applications.serial_lte_modem_no_indicate:
+    sysbuild: true
+    build_only: true
+    extra_configs:
+      - CONFIG_SLM_INDICATE_PIN=-1
+    platform_allow:
+      - nrf9151dk/nrf9151/ns
+    integration_platforms:
+      - nrf9151dk/nrf9151/ns
+    tags:
+      - ci_build
+      - sysbuild
+      - ci_applications_serial_lte_modem
   applications.serial_lte_modem.extmcu:
     sysbuild: true
     build_only: true

applications/serial_lte_modem/src/slm_ctrl_pin.c
@@ -29,8 +29,12 @@ BUILD_ASSERT(!IS_ENABLED(CONFIG_SLM_START_SLEEP),
 	"CONFIG_SLM_START_SLEEP requires CONFIG_SLM_POWER_PIN to be defined.");
 #endif
 
+#if INDICATE_PIN_IS_ENABLED
 static struct k_work_delayable indicate_work;
+#endif
+#if POWER_PIN_IS_ENABLED
 static atomic_t callback_wakeup_running;
+#endif
 
 static int ext_xtal_control(bool xtal_on)
 {
@@ -136,14 +140,14 @@ static void power_pin_callback_poweroff(const struct device *dev,
 
 #endif /* POWER_PIN_IS_ENABLED */
 
+#if INDICATE_PIN_IS_ENABLED
+
 static void indicate_stop(void)
 {
-#if (INDICATE_PIN_IS_ENABLED)
 	if (gpio_pin_set(gpio_dev, CONFIG_SLM_INDICATE_PIN, 0) != 0) {
 		LOG_WRN("GPIO_0 set error");
 	}
 	LOG_DBG("Stop indicating");
-#endif
 }
 
 static void indicate_wk(struct k_work *work)
@@ -152,6 +156,8 @@ static void indicate_wk(struct k_work *work)
 
 	indicate_stop();
 }
+
+#endif /* INDICATE_PIN_IS_ENABLED */
 
 #if POWER_PIN_IS_ENABLED
 
@@ -177,10 +183,13 @@ static void power_pin_callback_wakeup_work_fn(struct k_work *)
 	int err;
 
 	LOG_INF("Resuming from idle.");
+
+#if INDICATE_PIN_IS_ENABLED
 	if (k_work_delayable_is_pending(&indicate_work)) {
 		k_work_cancel_delayable(&indicate_work);
 		indicate_stop();
 	}
+#endif /* INDICATE_PIN_IS_ENABLED */
 
 	err = ext_xtal_control(true);
 	if (err < 0) {
@@ -214,25 +223,6 @@ static void power_pin_callback_wakeup(const struct device *dev,
 	configure_power_pin_interrupt(power_pin_callback_enable_poweroff, GPIO_INT_EDGE_FALLING);
 
 	k_work_submit(&work);
-}
-
-int slm_ctrl_pin_indicate(void)
-{
-	int err = 0;
-
-#if (INDICATE_PIN_IS_ENABLED)
-	if (k_work_delayable_is_pending(&indicate_work)) {
-		return 0;
-	}
-	LOG_DBG("Start indicating");
-	err = gpio_pin_set(gpio_dev, CONFIG_SLM_INDICATE_PIN, 1);
-	if (err) {
-		LOG_ERR("GPIO_0 set error: %d", err);
-	} else {
-		k_work_reschedule(&indicate_work, K_MSEC(CONFIG_SLM_INDICATE_TIME));
-	}
-#endif
-	return err;
 }
 
 void slm_ctrl_pin_enter_idle(void)
@@ -280,6 +270,25 @@ void slm_ctrl_pin_enter_sleep_no_uninit(void)
 
 #endif /* POWER_PIN_IS_ENABLED */
 
+int slm_ctrl_pin_indicate(void)
+{
+	int err = 0;
+
+#if INDICATE_PIN_IS_ENABLED
+	if (k_work_delayable_is_pending(&indicate_work)) {
+		return 0;
+	}
+	LOG_DBG("Start indicating");
+	err = gpio_pin_set(gpio_dev, CONFIG_SLM_INDICATE_PIN, 1);
+	if (err) {
+		LOG_ERR("GPIO_0 set error: %d", err);
+	} else {
+		k_work_reschedule(&indicate_work, K_MSEC(CONFIG_SLM_INDICATE_TIME));
+	}
+#endif
+	return err;
+}
+
 void slm_ctrl_pin_enter_shutdown(void)
 {
 	LOG_INF("Entering shutdown.");
@@ -319,7 +328,9 @@ int slm_ctrl_pin_init(void)
 {
 	int err;
 
+#if INDICATE_PIN_IS_ENABLED
 	k_work_init_delayable(&indicate_work, indicate_wk);
+#endif
 
 	err = ext_xtal_control(true);
 	if (err < 0) {