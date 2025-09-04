eea2329432bbb5b3533c0c2151f569396eeb6a1d
Bartosz Miller <bartosz.miller@nordicsemi.no>
2025-09-04 13:36:59 +0200

    tests: benchmarks: multicore: Test idle benchmarks with MCUBoot
    Add testing configuration with MCUBoot enabled for the idle benchmarks.
    
    Signed-off-by: Bartosz Miller <bartosz.miller@nordicsemi.no>

--- tests/benchmarks/multicore/idle_ppr/sysbuild.cmake
+++ tests/benchmarks/multicore/idle_ppr/sysbuild.cmake
@@ -4,14 +4,6 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
-# Add remote project
-ExternalZephyrProject_Add(
-  APPLICATION remote_rad
-  SOURCE_DIR ${APP_DIR}/remote
-  BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpurad
-  BOARD_REVISION ${BOARD_REVISION}
-)
-
 ExternalZephyrProject_Add(
   APPLICATION remote_ppr
   SOURCE_DIR  ${APP_DIR}/remote
