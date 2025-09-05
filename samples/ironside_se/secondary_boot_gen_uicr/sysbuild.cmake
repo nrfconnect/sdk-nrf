ec0ba2caf2f780904b88c7bd0aa196ac7e9f3f4a
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2025-09-05 11:55:28 +0200

    ironside: secondary: Re-organize secondary images
    Create infrastructure for secondary iamges that is akin to how netcore
    adds sysbuild images.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- samples/ironside_se/secondary_boot_gen_uicr/sysbuild.cmake
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024 Nordic Semiconductor ASA
-# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
-
-# Build secondary application core image
-ExternalZephyrProject_Add(
-  APPLICATION secondary
-  SOURCE_DIR ${APP_DIR}/secondary
-  BOARD nrf54h20dk/nrf54h20/cpuapp
-)
