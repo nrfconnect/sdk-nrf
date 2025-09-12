c0f07a0d9eb0d4f0652b3f223ec1b08f572ff0f4
Piotr Pryga <piotr.pryga@nordicsemi.no>
2025-09-12 08:32:38 +0200

    bluetooth: In hci_internal_msg_get add msg_type_out cast to avoid warn
    In the hci_internal_msg_get() there is a call to sdc_hci_get()
    that second parameter is uint8_t pointer.
    The provided msg_type_out argument is an enum sdc_hci_msg_type_t
    pointer that must fit into the uint8_t. Durign build compiler
    generates an error about incompatible pointer types.
    
    The commit adds a cast to uint8_t to fix the compilation error.
    
    Note: the enum sdc_hci_msg_type_t is defined in SoftDevice
    Controller headers. It is SoftDevice responsibility to make
    sure the enum doesn't contain values that don't fit into
    uint8_t.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- subsys/bluetooth/controller/hci_internal.c
+++ subsys/bluetooth/controller/hci_internal.c
@@ -1789,7 +1789,7 @@ int hci_internal_msg_get(uint8_t *msg_out, sdc_hci_msg_type_t *msg_type_out)
 		return 0;
 	}
 
-	const int retval = sdc_hci_get(msg_out, msg_type_out);
+	const int retval = sdc_hci_get(msg_out, (uint8_t*)msg_type_out);
 
 #if defined(CONFIG_BT_CTLR_SDC_PAWR_SYNC)
 	if (retval == 0 && *msg_type_out == SDC_HCI_MSG_TYPE_EVT
