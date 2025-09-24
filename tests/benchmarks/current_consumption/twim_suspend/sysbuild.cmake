a520592c3d05963cb827db099d82a57d734c7948
Bartosz Miller <bartosz.miller@nordicsemi.no>
2025-09-24 14:45:30 +0200

    benchmarks: Run PM i2c tests on nrf54h20 PPR core
    Run Power Management tests on nrf54h20/cpuppr core
    
    Signed-off-by: Bartosz Miller <bartosz.miller@nordicsemi.no>

--- tests/benchmarks/current_consumption/twim_suspend/sysbuild.cmake
+++ tests/benchmarks/current_consumption/twim_suspend/sysbuild.cmake
@@ -5,15 +5,10 @@
 #
 
 if(SB_CONFIG_REMOTE_BOARD)
-  # Add remote project
   ExternalZephyrProject_Add(
-      APPLICATION remote
-      SOURCE_DIR ${APP_DIR}/remote
-      BOARD ${SB_CONFIG_REMOTE_BOARD}
-      BOARD_REVISION ${BOARD_REVISION}
-    )
-  # Add a dependency so that the remote image will be built and flashed first
-  add_dependencies(twim_suspend remote)
-  # Add dependency so that the remote image is flashed first.
-  sysbuild_add_dependencies(FLASH twim_suspend remote)
+    APPLICATION remote
+    SOURCE_DIR ${APP_DIR}/remote
+    BOARD ${SB_CONFIG_REMOTE_BOARD}
+    BOARD_REVISION ${BOARD_REVISION}
+  )
 endif()
