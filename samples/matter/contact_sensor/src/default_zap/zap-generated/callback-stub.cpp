522a0d6a42271642755a7016df1552b4d78122d9
Michał Szablowski <michal.szablowski@nordicsemi.no>
2025-10-01 16:14:36 +0200

    samples: matter: fix data model
    Fix data model for contact and temperature sensor.
    
    Signed-off-by: Michał Szablowski <michal.szablowski@nordicsemi.no>

--- samples/matter/contact_sensor/src/default_zap/zap-generated/callback-stub.cpp
+++ samples/matter/contact_sensor/src/default_zap/zap-generated/callback-stub.cpp
@@ -61,9 +61,6 @@ void emberAfClusterInitCallback(EndpointId endpoint, ClusterId clusterId)
 	case app::Clusters::Identify::Id:
 		emberAfIdentifyClusterInitCallback(endpoint);
 		break;
-	case app::Clusters::LocalizationConfiguration::Id:
-		emberAfLocalizationConfigurationClusterInitCallback(endpoint);
-		break;
 	case app::Clusters::NetworkCommissioning::Id:
 		emberAfNetworkCommissioningClusterInitCallback(endpoint);
 		break;
@@ -139,11 +136,6 @@ void __attribute__((weak)) emberAfIcdManagementClusterInitCallback(EndpointId en
 	(void)endpoint;
 }
 void __attribute__((weak)) emberAfIdentifyClusterInitCallback(EndpointId endpoint)
-{
-	// To prevent warning
-	(void)endpoint;
-}
-void __attribute__((weak)) emberAfLocalizationConfigurationClusterInitCallback(EndpointId endpoint)
 {
 	// To prevent warning
 	(void)endpoint;
