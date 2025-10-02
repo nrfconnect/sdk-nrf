6a831e1fed89d788c854135766a6b80afacad20a
Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
2025-10-02 16:32:58 +0200

    treewide: align to modified include paths in nrfx
    Include nrfx instead of MDK.
    
    Signed-off-by: Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>

--- samples/bluetooth/direct_test_mode/src/dtm_hw.c
+++ samples/bluetooth/direct_test_mode/src/dtm_hw.c
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#include "nrf.h"
+#include "nrfx.h"
 
 #include "dtm_hw.h"
 #include "dtm_hw_config.h"
--- samples/crypto/psa_tls/src/psa_tls_common.c
+++ samples/crypto/psa_tls/src/psa_tls_common.c
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#include <nrf.h>
+#include <nrfx.h>
 #include <errno.h>
 #include <zephyr/kernel.h>
 #include <zephyr/net/socket.h>
