daa9b94a01678297223c58635e1f8b77d63d28c3
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2025-10-03 13:23:04 +0200

    nrf_ironside: Move Ironside outside of nrf_security
    Create a separate subsystem called nrf_ironside instead
    of having the logic in nrf_security. Ironside is completely
    separate from nrf_security and it should not be placed there.
    
    Make sure that nrf_security cannot be enabled at the same time
    as nrf_ironside as their configurations might collide.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

---
+++ subsys/nrf_ironside/ironside_config.h
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#define MBEDTLS_PSA_CRYPTO_CLIENT
---
+++ subsys/nrf_ironside/psa/crypto_driver_config.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef PSA_CRYPTO_DRIVER_CONFIG_H
+#define PSA_CRYPTO_DRIVER_CONFIG_H
+
+#if defined(MBEDTLS_PSA_CRYPTO_CONFIG_FILE)
+#include MBEDTLS_PSA_CRYPTO_CONFIG_FILE
+#else
+#include "psa/crypto_config.h"
+#endif
+
+#endif /* PSA_CRYPTO_DRIVER_CONFIG_H */
