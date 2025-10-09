55b17bb4e12a9fc2a30a36b9d3162439406431b3
Bartosz Miller <bartosz.miller@nordicsemi.no>
2025-10-09 12:51:12 +0200

    NRFX-NONE: GRTC playground (timer CLK pin out testing) Signed-off-by: Bartosz Miller <bartosz.miller@nordicsemi.no>

--- tests/drivers/timer/grtc_timer_lfrc/src/main.c
+++ tests/drivers/timer/grtc_timer_lfrc/src/main.c
@@ -9,6 +9,7 @@
 #include <zephyr/drivers/counter.h>
 #include <zephyr/devicetree/clocks.h>
 #include <zephyr/drivers/clock_control/nrf_clock_control.h>
+#include<dk_buttons_and_leds.h>
 
 #define REQUEST_SERVING_WAIT_TIME_US 10000
 #if defined(CONFIG_COVERAGE)
@@ -32,6 +33,8 @@ struct accuracy_test_limit {
 static volatile uint8_t compare_isr_call_counter;
 static volatile uint64_t compare_count_value;
 static volatile uint64_t reference_timer_value_us;
+
+static int32_t grtc_channel;
 
 const struct device *const tst_timer_dev = DEVICE_DT_GET(DT_ALIAS(tst_timer));
 const struct device *const lfclk_dev = DEVICE_DT_GET(DT_NODELABEL(lfclk));
@@ -93,175 +96,61 @@ static int request_clock_spec(const struct device *clk_dev, const struct nrf_clo
 	return 0;
 }
 
-static void timer_compare_interrupt_handler(int32_t id, uint64_t expire_time, void *user_data)
+static void timer_compare_interrupt_continous(int32_t id, uint64_t expire_time, void *user_data)
 {
-	uint32_t tst_timer_value;
 
-	compare_count_value = z_nrf_grtc_timer_read();
+	int err;
+	int key;
+	uint32_t *timer_count_time_ms = (uint32_t *)user_data;
+	uint64_t test_ticks = 0;
+
+	if(compare_isr_call_counter % 2)
+	{
+		dk_set_led_on(DK_LED1);
+	}
+	else
+	{
+		dk_set_led_off(DK_LED1);
+	}
 	compare_isr_call_counter++;
 
-	counter_get_value(tst_timer_dev, &tst_timer_value);
-	counter_stop(tst_timer_dev);
-	reference_timer_value_us = counter_ticks_to_us(tst_timer_dev, tst_timer_value);
+	key = z_nrf_grtc_timer_compare_int_lock(grtc_channel);
+	test_ticks = z_nrf_grtc_timer_get_ticks(K_MSEC(*timer_count_time_ms));
+	err = z_nrf_grtc_timer_set(grtc_channel, test_ticks, timer_compare_interrupt_continous, (void *)timer_count_time_ms);
+	if (err != 0) {
+		printk("Unexpected error raised when setting timer, err: %d\n", err);
+		return;
+	}
+
+	z_nrf_grtc_timer_compare_int_unlock(grtc_channel, key);
 }
 
-static void configure_test_timer(const struct device *timer_dev, uint32_t count_time_ms)
-{
-	struct counter_alarm_cfg counter_cfg;
-
-	counter_cfg.flags = 0;
-	counter_cfg.ticks = counter_us_to_ticks(timer_dev, (uint64_t)count_time_ms * 1000);
-	counter_cfg.user_data = &counter_cfg;
-}
-
-static int test_timer_compare(int32_t channel, const struct accuracy_test_limit *test_limit,
-			      lfclk_source_change_option tst_dynamic_clk_change_option)
+static void grtc_continous_count(int32_t channel, uint32_t timer_count_time_ms)
 {
 	int err;
 	uint64_t test_ticks = 0;
 	uint64_t compare_value = 0;
-	int64_t time_delta_us = 0;
 
-	printk("Reference timer (HFCLK sourced) is enabled\n");
-	configure_test_timer(tst_timer_dev, test_limit->grtc_timer_count_time_ms);
-	counter_reset(tst_timer_dev);
-
-	reference_timer_value_us = 0;
-	compare_isr_call_counter = 0;
-	test_ticks = z_nrf_grtc_timer_get_ticks(K_MSEC(test_limit->grtc_timer_count_time_ms));
-	err = z_nrf_grtc_timer_set(channel, test_ticks, timer_compare_interrupt_handler, NULL);
+	test_ticks = z_nrf_grtc_timer_get_ticks(K_MSEC(timer_count_time_ms));
+	err = z_nrf_grtc_timer_set(channel, test_ticks, timer_compare_interrupt_continous, (void *)&timer_count_time_ms);
 	if (err != 0) {
 		printk("Unexpected error raised when setting timer, err: %d\n", err);
-		return -1;
+		return;
 	}
-	counter_start(tst_timer_dev);
 	z_nrf_grtc_timer_compare_read(channel, &compare_value);
 	if (K_TIMEOUT_EQ(K_TICKS(compare_value), K_TICKS(test_ticks)) == false) {
 		printk("Compare register set failed\n");
-		return -2;
-	}
-	if (tst_dynamic_clk_change_option == CHANGE_TO_SYNTH) {
-		printk("Changing clock source of the GRTC (LFCLK) to SYNTH while waiting for "
-		       "compare callback\n");
-		request_clock_spec(lfclk_dev, &lfclk_synth_mode);
-	} else if (tst_dynamic_clk_change_option == CHANGE_TO_LFRC) {
-		printk("Changing clock source of the GRTC (LFCLK) to LFRC while waiting for "
-		       "compare callback\n");
-		request_clock_spec(lfclk_dev, &lfclk_lfrc_mode);
-	} else {
-		printk("Clock source change not requested\n");
+		return;
 	}
 
-	k_sleep(K_MSEC(2 * test_limit->grtc_timer_count_time_ms));
-	if (compare_isr_call_counter != 1) {
-		printk("Invalid number of triggered GRTC intertupts: %u", compare_isr_call_counter);
-		return -3;
-	}
+	printk("grtc_continous_count START\n");
 
-	printk("Reference timer value [us]: %llu\n", reference_timer_value_us);
-	printk("Compare count value [ticks]: %llu\n", compare_count_value);
-	time_delta_us =
-		(int64_t)test_limit->grtc_timer_count_time_ms * 1000 - reference_timer_value_us;
-	printk("Time delta (Specified (GRTC) - reference timer) [us]: %lld\n", time_delta_us);
+	k_sleep(K_FOREVER);
 
-	if (time_delta_us < 0) {
-		time_delta_us *= -1;
-	}
-
-	if (time_delta_us > (int64_t)(test_limit->time_delta_abs_tolerance_us * DIFF_TOLERANCE)) {
-		printk("Absolute time delta is over the limit\n");
-		return -4;
-	}
-
-	return 0;
 }
-
-/*
- * These tests use HFCLK based timerXXX as a reference.
- * The timers start is not ideally synchronised,
- * this is taken into account in the limit
- */
-static int test_timer_count_in_compare_mode_lfclk_source_from_lfrc(int32_t grtc_channel)
-{
-	int ret = 0;
-	const struct accuracy_test_limit test_limit = {.grtc_timer_count_time_ms = 1000,
-						       .time_delta_abs_tolerance_us = 250};
-
-	printk("test_timer_count_in_compare_mode_lfclk_source_from_lfrc\n");
-	request_clock_spec(lfclk_dev, &lfclk_lfrc_mode);
-	if (ret != 0) {
-		printk("Clock request failed: %d\n", ret);
-		return -1;
-	}
-
-	for (int i = 0; i < NUMBER_OF_REPETITIONS; i++) {
-		ret += test_timer_compare(grtc_channel, &test_limit, NO_CHANGE);
-	}
-	return ret;
-}
-
-#if !defined(CONFIG_SOC_NRF54H20_CPURAD)
-static int test_timer_count_in_compare_mode_lfclk_source_from_synth(int32_t grtc_channel)
-{
-	int ret = 0;
-	const struct accuracy_test_limit test_limit = {.grtc_timer_count_time_ms = 50,
-						       .time_delta_abs_tolerance_us = 20};
-
-	printk("test_timer_count_in_compare_mode_lfclk_source_from_synth\n");
-	ret = request_clock_spec(lfclk_dev, &lfclk_synth_mode);
-	if (ret != 0) {
-		printk("Clock request failed: %d\n", ret);
-		return -1;
-	}
-
-	for (int i = 0; i < NUMBER_OF_REPETITIONS; i++) {
-		ret += test_timer_compare(grtc_channel, &test_limit, NO_CHANGE);
-	}
-	return ret;
-}
-
-static int test_timer_count_in_compare_mode_lfclk_runtime_update_lfrc_to_synth(int32_t grtc_channel)
-{
-	int ret = 0;
-	const struct accuracy_test_limit test_limit = {.grtc_timer_count_time_ms = 50,
-						       .time_delta_abs_tolerance_us = 26};
-
-	printk("test_timer_count_in_compare_mode_lfclk_runtime_update_lfrc_to_synth\n");
-	ret = request_clock_spec(lfclk_dev, &lfclk_lfrc_mode);
-	if (ret != 0) {
-		printk("Clock request failed: %d\n", ret);
-		return -1;
-	}
-
-	for (int i = 0; i < NUMBER_OF_REPETITIONS; i++) {
-		ret += test_timer_compare(grtc_channel, &test_limit, CHANGE_TO_SYNTH);
-	}
-	return ret;
-}
-
-static int test_timer_count_in_compare_mode_lfclk_runtime_update_synth_to_lfrc(int32_t grtc_channel)
-{
-	int ret = 0;
-	const struct accuracy_test_limit test_limit = {.grtc_timer_count_time_ms = 50,
-						       .time_delta_abs_tolerance_us = 26};
-
-	printk("test_timer_count_in_compare_mode_lfclk_runtime_update_synth_to_lfrc\n");
-	ret = request_clock_spec(lfclk_dev, &lfclk_synth_mode);
-	if (ret != 0) {
-		printk("Clock request failed: %d\n", ret);
-		return -1;
-	}
-
-	for (int i = 0; i < NUMBER_OF_REPETITIONS; i++) {
-		ret += test_timer_compare(grtc_channel, &test_limit, CHANGE_TO_LFRC);
-	}
-	return ret;
-}
-#endif /* !CONFIG_SOC_NRF54H20_CPURAD */
 
 int main(void)
 {
-	int32_t grtc_channel;
 	int ret = 0;
 
 	ret = request_clock_spec(fll16m_dev, &fll16m_bypass_mode);
@@ -278,19 +167,11 @@ int main(void)
 		return -2;
 	}
 
-	ret = test_timer_count_in_compare_mode_lfclk_source_from_lfrc(grtc_channel);
-#if !defined(CONFIG_SOC_NRF54H20_CPURAD)
-	/* Swiching to SYNTH via Clock Control Api is not possible from CPURAD */
-	ret += test_timer_count_in_compare_mode_lfclk_source_from_synth(grtc_channel);
-	ret += test_timer_count_in_compare_mode_lfclk_runtime_update_lfrc_to_synth(grtc_channel);
-	ret += test_timer_count_in_compare_mode_lfclk_runtime_update_synth_to_lfrc(grtc_channel);
-#endif /* !CONFIG_SOC_NRF54H20_CPURAD */
+	dk_leds_init();
+	dk_set_led_off(DK_LED1);
+	dk_set_led_off(DK_LED2);
 
-	if (ret != 0) {
-		printk("Tests status: FAIL\n");
-	} else {
-		printk("Tests status: PASS\n");
-	}
+	grtc_continous_count(grtc_channel, 500);
 
 	z_nrf_grtc_timer_chan_free(grtc_channel);
 
