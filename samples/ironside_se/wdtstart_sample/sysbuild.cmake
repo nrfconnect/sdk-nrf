612ca5fe120bc00770079863f599b21057c910e9
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2025-10-17 11:44:54 +0200

    samples: ironside_se: Add sample demonstrating use of UICR.WDTSTART
    Add a sample demonstrating how to use UICR.WDTSTART.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

---
+++ samples/ironside_se/wdtstart_sample/sysbuild.cmake
@@ -0,0 +1,5 @@
+# Copyright (c) 2025 Nordic Semiconductor ASA
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+
+# This sample demonstrates UICR.WDTSTART configuration
+# No additional sysbuild configuration needed for this sample
