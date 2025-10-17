b34dd504c274b54ac3405f094b95063771efc196
Dominik Kilian <dominik.kilian@nordicsemi.no>
2025-10-17 17:05:16 +0200

    esb: fix radio staying in RX when no pending send requests
    When fast switching is enabled, the radio stays in RXIDLE state
    even if there are no more packets to send because SW part switches
    to idle state to early, before radio gets to a DISABLED state.
    
    Ref: NCSDK-35281
    
    Signed-off-by: Dominik Kilian <dominik.kilian@nordicsemi.no>

--- subsys/esb/esb.c
+++ subsys/esb/esb.c
@@ -1206,6 +1206,7 @@ static void start_tx_transaction(void)
 
 		if (fast_switching) {
 			nrf_radio_shorts_set(NRF_RADIO, radio_shorts_common);
+			nrf_radio_event_clear(NRF_RADIO, ESB_RADIO_EVENT_END);
 			nrf_radio_int_enable(NRF_RADIO, ESB_RADIO_INT_END_MASK);
 		} else {
 			nrf_radio_shorts_set(NRF_RADIO,
@@ -1235,6 +1236,7 @@ static void start_tx_transaction(void)
 		if (ack) {
 			if (fast_switching) {
 				nrf_radio_shorts_set(NRF_RADIO, radio_shorts_common);
+				nrf_radio_event_clear(NRF_RADIO, ESB_RADIO_EVENT_END);
 				nrf_radio_int_enable(NRF_RADIO, ESB_RADIO_INT_END_MASK);
 			} else {
 				nrf_radio_shorts_set(NRF_RADIO,
@@ -1407,6 +1409,8 @@ static void on_radio_disabled_tx(void)
 
 	nrf_radio_packetptr_set(NRF_RADIO, rx_payload_buffer);
 	if (fast_switching) {
+		nrf_radio_int_disable(NRF_RADIO, ESB_RADIO_INT_END_MASK);
+		nrf_radio_shorts_set(NRF_RADIO, (radio_shorts_common | ESB_SHORT_DISABLE_MASK));
 		nrf_radio_task_trigger(NRF_RADIO, NRF_RADIO_TASK_RXEN);
 	}
 	on_radio_disabled = on_radio_disabled_tx_wait_for_ack;
@@ -1440,6 +1444,10 @@ static void on_radio_disabled_tx_wait_for_ack(void)
 				nrf_radio_txaddress_get(NRF_RADIO), rx_pdu->type.dpl_pdu.pid)) {
 				interrupt_flags |= INT_RX_DATA_RECEIVED_MSK;
 			}
+		}
+
+		if (fast_switching) {
+			nrf_radio_shorts_set(NRF_RADIO, radio_shorts_common);
 		}
 
 		if ((tx_fifo.count == 0) || (esb_cfg.tx_mode == ESB_TXMODE_MANUAL)) {
@@ -1479,6 +1487,8 @@ static void on_radio_disabled_tx_wait_for_ack(void)
 			 */
 			if (fast_switching) {
 				nrf_radio_shorts_set(NRF_RADIO, radio_shorts_common);
+				nrf_radio_event_clear(NRF_RADIO, ESB_RADIO_EVENT_END);
+				nrf_radio_int_enable(NRF_RADIO, ESB_RADIO_INT_END_MASK);
 			} else {
 				nrf_radio_shorts_set(NRF_RADIO,
 					(radio_shorts_common | NRF_RADIO_SHORT_DISABLED_RXEN_MASK));
@@ -2257,11 +2267,13 @@ int esb_start_rx(void)
 
 	if (esb_cfg.mode == ESB_MODE_MONITOR) {
 		nrf_radio_shorts_set(NRF_RADIO, RADIO_SHORTS_MONITOR);
+		nrf_radio_event_clear(NRF_RADIO, ESB_RADIO_EVENT_END);
 		nrf_radio_int_enable(NRF_RADIO, ESB_RADIO_INT_END_MASK);
 		on_radio_disabled = NULL;
 	} else {
 		if (fast_switching) {
 			nrf_radio_shorts_set(NRF_RADIO, radio_shorts_common);
+			nrf_radio_event_clear(NRF_RADIO, ESB_RADIO_EVENT_END);
 			nrf_radio_int_enable(NRF_RADIO, ESB_RADIO_INT_END_MASK);
 		} else {
 			nrf_radio_shorts_set(NRF_RADIO, (radio_shorts_common |
