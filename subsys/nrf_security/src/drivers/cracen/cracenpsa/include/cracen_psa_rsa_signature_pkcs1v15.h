bc1c37fa10646c1b7118b2255753788a08fe9f09
Anton Zyma <anton.zyma@nordicsemi.no>
2025-10-20 14:59:34 +0300

    nrf_security: CRACEN: Decrease number of typecasts
    The number of const casts decreased.
    
    Ref: NCSDK-32018
    
    Signed-off-by: Anton Zyma <anton.zyma@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/include/cracen_psa_rsa_signature_pkcs1v15.h
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/include/cracen_psa_rsa_signature_pkcs1v15.h
@@ -20,12 +20,12 @@ int cracen_rsa_pkcs1v15_sign_digest(struct cracen_rsa_key *rsa_key,
 				    size_t digest_length);
 
 int cracen_rsa_pkcs1v15_verify_message(struct cracen_rsa_key *rsa_key,
-				       struct cracen_signature *signature,
+				       struct cracen_const_signature *signature,
 				       const struct sxhashalg *hashalg, const uint8_t *message,
 				       size_t message_length);
 
 int cracen_rsa_pkcs1v15_verify_digest(struct cracen_rsa_key *rsa_key,
-				      struct cracen_signature *signature,
+				      struct cracen_const_signature *signature,
 				      const struct sxhashalg *hashalg, const uint8_t *digest,
 				      size_t digest_length);
 
