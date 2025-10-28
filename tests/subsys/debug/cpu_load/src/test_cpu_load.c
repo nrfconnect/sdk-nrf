a4d05658313a4acfbe149b866e884bde44d002fd
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2025-10-28 13:44:56 +0100

    debug: cpu_load: Align to the new GPPI API
    Additionally, removed support for PPI resources cleanup in case
    of failed allocation. cpu_load is a debug feature and if allocation
    fails then there is something wrong with the configuration and
    configuration shall be fixed or cpu_load disabled.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- tests/subsys/debug/cpu_load/src/test_cpu_load.c
+++ tests/subsys/debug/cpu_load/src/test_cpu_load.c
@@ -13,7 +13,6 @@
 #include <hal/nrf_power.h>
 
 #ifdef CONFIG_NRF_CPU_LOAD_USE_SHARED_DPPI_CHANNELS
-#include <nrfx_dppi.h>
 
 static void timer_handler(nrf_timer_event_t event_type, void *context)
 {
@@ -26,10 +25,11 @@ static int dppi_shared_resources_init(void)
 	static nrfx_timer_t timer = NRFX_TIMER_INSTANCE(NRF_TIMER1);
 	uint32_t base_frequency = NRF_TIMER_BASE_FREQUENCY_GET(timer.p_reg);
 	nrfx_timer_config_t config = NRFX_TIMER_DEFAULT_CONFIG(base_frequency);
-	uint8_t ch;
+	nrfx_gppi_handle_t handle;
 	uint32_t evt = nrf_power_event_address_get(NRF_POWER,
 						NRF_POWER_EVENT_SLEEPENTER);
 	uint32_t tsk = nrfx_timer_task_address_get(&timer, NRF_TIMER_TASK_COUNT);
+	int rv;
 
 	config.frequency = NRFX_MHZ_TO_HZ(1);
 	config.bit_width = NRF_TIMER_BIT_WIDTH_32;
@@ -37,11 +37,10 @@ static int dppi_shared_resources_init(void)
 	err = nrfx_timer_init(&timer, &config, timer_handler);
 	zassert_equal(err, NRFX_SUCCESS, "Unexpected error:%d", err);
 
-	err = nrfx_gppi_channel_alloc(&ch);
-	zassert_equal(err, NRFX_SUCCESS, "Unexpected error:%d", err);
+	rv = nrfx_gppi_conn_alloc(evt, tsk, &handle);
+	zassert_equal(rv, 0);
 
-	nrfx_gppi_channel_endpoints_setup(ch, evt, tsk);
-	nrfx_gppi_channels_enable(BIT(ch));
+	nrfx_gppi_conn_enable(handle);
 
 	return 0;
 }
