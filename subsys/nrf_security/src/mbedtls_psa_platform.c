882d767559c3dbed12121a70fc714a39aaefc39b
Grzegorz Swiderski <grzegorz.swiderski@nordicsemi.no>
2025-10-29 15:01:19 +0100

    nrf_security: Integrate IronSide PSA crypto driver
    Plug the newly defined interface functions into `psa_driver_wrapper_*()`
    and `mbedtls_psa_platform_get_builtin_key()`.
    
    The actual driver implementation will have to reside out of tree.
    
    Ref: NCSDK-35399
    Signed-off-by: Grzegorz Swiderski <grzegorz.swiderski@nordicsemi.no>

--- subsys/nrf_security/src/mbedtls_psa_platform.c
+++ subsys/nrf_security/src/mbedtls_psa_platform.c
@@ -9,10 +9,24 @@
 #include "cracen_psa.h"
 #endif
 
+#if defined(PSA_CRYPTO_DRIVER_IRONSIDE)
+#include "ironside_psa.h"
+#endif
+
 psa_status_t mbedtls_psa_platform_get_builtin_key(mbedtls_svc_key_id_t key_id,
 						  psa_key_lifetime_t *lifetime,
 						  psa_drv_slot_number_t *slot_number)
 {
+	psa_status_t status;
+	(void)status;
+
+#if defined(PSA_CRYPTO_DRIVER_IRONSIDE)
+	status = ironside_psa_get_key_slot(key_id, lifetime, slot_number);
+	if (status != PSA_ERROR_DOES_NOT_EXIST) {
+		return status;
+	}
+#endif
+
 #if defined(PSA_CRYPTO_DRIVER_CRACEN)
 	return cracen_get_key_slot(key_id, lifetime, slot_number);
 #endif
