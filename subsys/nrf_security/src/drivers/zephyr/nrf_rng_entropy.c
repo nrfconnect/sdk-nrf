f96148d8973811ae85c569f4b38efa1b3ef09d5e
Sergey Korotkov <sergey.korotkov@nordicsemi.no>
2025-10-30 12:37:20 +0100

    Revert "nrf_security: Enable NRF_RNG_ENTROPY_DRIVER for fake entropy"
    This reverts commit ed8447946818c4964c6ee69a347518b208685a8f.
    
    Signed-off-by: Sergey Korotkov <sergey.korotkov@nordicsemi.no>

--- subsys/nrf_security/src/drivers/zephyr/nrf_rng_entropy.c
+++ subsys/nrf_security/src/drivers/zephyr/nrf_rng_entropy.c
@@ -17,29 +17,19 @@
  * It uses a "Zephyr entropy driver" and can therefore only be used in
  * Zephyr images.
  *
- * This is used for two uses cases, the first use case is when hardware
- * crypto/entropy is not yet supported. This enables running software crypto
- * with a non cryptographically secure random generator to unblock development
- * when the device tree node with the DT label 'prng' is enabled.
- *
- * The second use case is for instance nrf52820 which has an NRF_RNG
- * peripheral, but does not have a HW crypto trng like cryptocell.
- * In this use case the device driver with the DT label 'rng'
- * is supported and this rng label is only applied for the Zephyr
+ * Note that it is only the device driver with the DT label 'rng' that
+ * is supported and that this rng label is only applied for the Zephyr
  * driver that uses the HW peripheral NRF_RNG (entropy_nrf5.c).
+ *
+ * An intended use-case is for instance nrf52820 which has an NRF_RNG
+ * peripheral, but does not have a HW crypto trng like cryptocell.
  *
  * Note that NRF_RNG produces TRNG, not CSPRNG.
  */
-#ifdef CONFIG_FAKE_ENTROPY_NRF_PRNG
-#define DTS_RNG_NODE_LABEL prng
-#else
-#define DTS_RNG_NODE_LABEL rng
-#endif
-
 psa_status_t nrf_rng_get_entropy(uint32_t flags, size_t *estimate_bits, uint8_t *output,
 				 size_t output_size)
 {
-	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(DTS_RNG_NODE_LABEL));
+	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(rng));
 	uint16_t request_len = MIN(UINT16_MAX, output_size);
 	int err;
 
