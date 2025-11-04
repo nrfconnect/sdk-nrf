4f4dd3d8ccde51914146bf840d57605e6b0b0f96
Sebastian Głąb <sebastian.glab@nordicsemi.no>
2025-11-04 08:38:42 +0100

    tests: benchmarks: power_consumption: spi: Verify communication
    Add check that confirms correct communication
    with adxl362 SPI device.
    
    Signed-off-by: Sebastian Głąb <sebastian.glab@nordicsemi.no>

--- tests/benchmarks/power_consumption/spi/src/driver_test.c
+++ tests/benchmarks/power_consumption/spi/src/driver_test.c
@@ -7,6 +7,12 @@
 #include <zephyr/drivers/spi.h>
 #include <zephyr/kernel.h>
 
+#define READ_COMMAND (0x0B)
+#define DEVID_AD_ADDRESS (0x0)
+#define DEVID_AD_VAL (0xAD)
+#define DEVID_MST_VAL (0x1D)
+#define PARTID_VAL (0xF2)
+
 #define SPIOP	SPI_WORD_SET(8) | SPI_TRANSFER_MSB
 
 static bool suspend_req;
@@ -22,11 +28,11 @@ void thread_definition(void)
 	int ret;
 	struct spi_dt_spec spispec = SPI_DT_SPEC_GET(DT_ALIAS(accel0), SPIOP, 0);
 
-	uint8_t tx_buffer = 0x15;
-	uint8_t data[3] = {1, 2, 3};
-	struct spi_buf tx_spi_buf		= {.buf = (void *)&tx_buffer, .len = 1};
+	uint8_t tx_buffer[5] = {READ_COMMAND, DEVID_AD_ADDRESS, 0xFF, 0xFF, 0xFF};
+	uint8_t rx_buffer[5] = {0, 0, 0, 0, 0};
+	struct spi_buf tx_spi_buf		= {.buf = tx_buffer, .len = 5};
 	struct spi_buf_set tx_spi_buf_set	= {.buffers = &tx_spi_buf, .count = 1};
-	struct spi_buf rx_spi_bufs		= {.buf = data, .len = 3};
+	struct spi_buf rx_spi_bufs		= {.buf = rx_buffer, .len = 5};
 	struct spi_buf_set rx_spi_buf_set	= {.buffers = &rx_spi_bufs, .count = 1};
 
 	while (1) {
@@ -36,9 +42,17 @@ void thread_definition(void)
 		}
 
 		ret = spi_transceive_dt(&spispec, &tx_spi_buf_set, &rx_spi_buf_set);
-		if (ret < 0) {
-			printk("Error during SPI transfer");
-			return;
-		}
+		__ASSERT(ret == 0, "spi_transceive_dt() returned %d", ret);
+
+		/* Check if communication with adxl362 was successful. */
+		__ASSERT(rx_buffer[2] == DEVID_AD_VAL,
+			"Invalid DEVID_AD, got %u, expected %u\n", rx_buffer[2], DEVID_AD_VAL);
+		__ASSERT(rx_buffer[3] == DEVID_MST_VAL,
+			"Invalid DEVID_MST, got %u, expected %u\n", rx_buffer[3], DEVID_MST_VAL);
+		__ASSERT(rx_buffer[4] == PARTID_VAL,
+			"Invalid PARTID, got %u, expected %u\n", rx_buffer[4], PARTID_VAL);
+		rx_buffer[2] = 0;
+		rx_buffer[3] = 0;
+		rx_buffer[4] = 0;
 	};
 };
