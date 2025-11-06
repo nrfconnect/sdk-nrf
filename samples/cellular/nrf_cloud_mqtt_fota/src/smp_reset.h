1aa0e012f37375282635716676c5b72499d486a1
Pascal Hernandez <pascal.hernandez@nordicsemi.no>
2025-11-06 14:22:57 +0100

    samples: cellular: Add nrf_cloud_mqtt_fota
    This demonstrates how to use FOTA over MQTT with nRF Cloud
    
    JIRA: IRIS-10200
    
    Signed-off-by: Pascal Hernandez <pascal.hernandez@nordicsemi.no>

---
+++ samples/cellular/nrf_cloud_mqtt_fota/src/smp_reset.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _SMP_RESET_H_
+#define _SMP_RESET_H_
+
+/**
+ * Reset the nRF52840 for SMP FOTA.
+ */
+int nrf52840_reset_api(void);
+
+#endif /* _SMP_RESET_H_ */
