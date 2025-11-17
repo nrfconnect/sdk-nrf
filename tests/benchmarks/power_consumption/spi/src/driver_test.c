ff3a013c1147a90fdfd77963582b40e724e4515f
Bartosz Miller <bartosz.miller@nordicsemi.no>
2025-11-17 10:50:06 +0100

    tests: drivers: Verify MLTPAN-8 workaround in SPIM pm test
    SPIM PM test add MLTPAN-8 workaround verification.
    
    Signed-off-by: Bartosz Miller <bartosz.miller@nordicsemi.no>

--- tests/benchmarks/power_consumption/spi/src/driver_test.c
+++ tests/benchmarks/power_consumption/spi/src/driver_test.c
@@ -6,6 +6,7 @@
 
 #include <zephyr/drivers/spi.h>
 #include <zephyr/kernel.h>
+#include "nrfx_spim.h"
 
 #define READ_COMMAND (0x0B)
 #define DEVID_AD_ADDRESS (0x0)
@@ -14,6 +15,21 @@
 #define PARTID_VAL (0xF2)
 
 #define SPIOP	SPI_WORD_SET(8) | SPI_TRANSFER_MSB
+
+/*
+ * Additonaly to the standard test procedure, PAN woraround is verified
+ *
+ * Required condtiton to verify MLTPAN-8 workaround:
+ * SPIM is configured with CPHA to 0,
+ * PRESCALER is larger than 2
+ * and the first transmitted bit is 1.
+ *
+ * Test SPIM confuration:
+ * COPOL = 0
+ * CPHA = 0 (OK)
+ * With 1MHz overlay PRESCALER = 10 (OK)
+ * First transmitted bit is 1 (0xB = 1011, OK)
+ */
 
 static bool suspend_req;
 
@@ -27,6 +43,7 @@ void thread_definition(void)
 {
 	int ret;
 	struct spi_dt_spec spispec = SPI_DT_SPEC_GET(DT_ALIAS(accel0), SPIOP, 0);
+	NRF_SPIM_Type *spi_reg = (NRF_SPIM_Type *)DT_REG_ADDR(DT_NODELABEL(dut_spi));
 
 	uint8_t tx_buffer[5] = {READ_COMMAND, DEVID_AD_ADDRESS, 0xFF, 0xFF, 0xFF};
 	uint8_t rx_buffer[5] = {0, 0, 0, 0, 0};
@@ -35,8 +52,10 @@ void thread_definition(void)
 	struct spi_buf rx_spi_bufs		= {.buf = rx_buffer, .len = 5};
 	struct spi_buf_set rx_spi_buf_set	= {.buffers = &rx_spi_bufs, .count = 1};
 
+
 	while (1) {
 		if (suspend_req) {
+			printk("SPI PRESCALER %x\n", spi_reg->PRESCALER);
 			suspend_req = false;
 			k_thread_suspend(k_current_get());
 		}
@@ -51,6 +70,7 @@ void thread_definition(void)
 			"Invalid DEVID_MST, got %u, expected %u\n", rx_buffer[3], DEVID_MST_VAL);
 		__ASSERT(rx_buffer[4] == PARTID_VAL,
 			"Invalid PARTID, got %u, expected %u\n", rx_buffer[4], PARTID_VAL);
+
 		rx_buffer[2] = 0;
 		rx_buffer[3] = 0;
 		rx_buffer[4] = 0;
--- tests/benchmarks/power_consumption/spi/sysbuild.cmake
+++ tests/benchmarks/power_consumption/spi/sysbuild.cmake
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
-if(NOT "${SB_CONFIG_REMOTE_BOARD}" STREQUAL "")
+if(SB_CONFIG_REMOTE_BOARD)
   ExternalZephyrProject_Add(
     APPLICATION remote_sleep_forever
     SOURCE_DIR ${APP_DIR}/../common/remote_sleep_forever
