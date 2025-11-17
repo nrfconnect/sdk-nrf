445ebb3fc58df6ae2f5f4beac511c2f5c34a4d63
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2025-11-17 13:06:40 +0100

    ab_split: Log bootloader version and counter value
    Extend the app with a logic that reads the current security counter
    and bootloader version values from the bootloader info.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- samples/dfu/ab_split/src/main.c
+++ samples/dfu/ab_split/src/main.c
@@ -10,12 +10,57 @@
 #include <common.h>
 #include "ab_utils.h"
 
+#ifdef CONFIG_RETENTION_BOOTLOADER_INFO
+#include <zephyr/retention/blinfo.h>
+#include <bootutil/image.h>
+#endif
+
 #define LOG_LEVEL LOG_LEVEL_DBG
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(ab_sample);
 
+#ifdef CONFIG_RETENTION_BOOTLOADER_INFO
+static void blinfo_bootloader_version(void)
+{
+	struct image_version version = {0x00};
+
+	int ret = blinfo_lookup(BLINFO_BOOTLOADER_VERSION, (char *)&version,
+				sizeof(struct image_version));
+
+	if (ret < 0) {
+		LOG_INF("blinfo_lookup error: %d", ret);
+	} else {
+		LOG_INF("Bootloader version: %d.%d.%d+%d", version.iv_major,
+			version.iv_minor, version.iv_revision, version.iv_build_num);
+	}
+}
+
+#ifdef CONFIG_MCUBOOT_HARDWARE_DOWNGRADE_PREVENTION
+static void blinfo_security_counter(void)
+{
+	uint32_t counter = 0;
+
+	int ret = blinfo_lookup(BLINFO_SECURITY_COUNTER_IMAGE_0, (char *)&counter,
+				sizeof(counter));
+
+	if (ret < 0) {
+		LOG_INF("blinfo_lookup error: %d", ret);
+	} else {
+		LOG_INF("Security counter: %d", counter);
+	}
+}
+#endif /* CONFIG_MCUBOOT_HARDWARE_DOWNGRADE_PREVENTION */
+#endif /* CONFIG_RETENTION_BOOTLOADER_INFO */
+
 int main(void)
 {
+#ifdef CONFIG_RETENTION_BOOTLOADER_INFO
+	blinfo_bootloader_version();
+#ifdef CONFIG_MCUBOOT_HARDWARE_DOWNGRADE_PREVENTION
+	blinfo_security_counter();
+#endif /* CONFIG_MCUBOOT_HARDWARE_DOWNGRADE_PREVENTION */
+#endif
+
 #ifdef CONFIG_MCUMGR_TRANSPORT_BT
 	start_smp_bluetooth_adverts();
 #endif
