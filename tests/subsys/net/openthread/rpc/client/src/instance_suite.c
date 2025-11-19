8458f8ba63f382f67d6e76ef739054cb5cb8e1f7
Damian Krolik <damian.krolik@nordicsemi.no>
2025-11-19 10:09:19 +0100

    openthread: rpc: avoid sending instance pointer over RPC
    All functions except otInstanceXXX skipped sending a pointer
    to otInstance and simply assumed that only a single instance
    is supported. Rework otInstanceXXX APIs to make them
    consistent with the rest and more portable.
    
    Signed-off-by: Damian Krolik <damian.krolik@nordicsemi.no>

--- tests/subsys/net/openthread/rpc/client/src/instance_suite.c
+++ tests/subsys/net/openthread/rpc/client/src/instance_suite.c
@@ -13,9 +13,6 @@
 
 #include <openthread/instance.h>
 
-/* Instance address used when testing serialization of a function that takes otInstance* */
-#define INSTANCE_ADDR UINT32_MAX
-
 static void nrf_rpc_err_handler(const struct nrf_rpc_err_report *report)
 {
 	zassert_ok(report->code);
@@ -28,40 +25,34 @@ static void tc_setup(void *f)
 	mock_nrf_rpc_tr_expect_reset();
 }
 
-/* Test serialization of otInstanceInitSingle() returning 0 */
+/* Test serialization of otInstanceInitSingle() */
 ZTEST(ot_rpc_instance, test_otInstanceInitSingle_0)
 {
 	otInstance *instance;
+	otInstance *instance2;
 
-	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_INIT_SINGLE), RPC_RSP(0));
+	/* Verify a non-null instance is returned from the function. */
+	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_INIT_SINGLE), RPC_RSP());
 	instance = otInstanceInitSingle();
 	mock_nrf_rpc_tr_expect_done();
 
-	zassert_equal(instance, NULL);
-}
+	zassert_not_null(instance, NULL);
 
-/* Test serialization of otInstanceInitSingle() returning max allowed 0xffffffff */
-ZTEST(ot_rpc_instance, test_otInstanceInitSingle_max)
-{
-	otInstance *instance;
-
-	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_INIT_SINGLE),
-				   RPC_RSP(CBOR_UINT32(UINT32_MAX)));
-	instance = otInstanceInitSingle();
+	/* Verify that the same instance is returned for subsequent calls. */
+	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_INIT_SINGLE), RPC_RSP());
+	instance2 = otInstanceInitSingle();
 	mock_nrf_rpc_tr_expect_done();
 
-	zassert_equal(instance, (void *)UINT32_MAX);
+	zassert_equal(instance, instance2);
 }
 
 /* Test serialization of otInstanceGetId() returning 0 */
 ZTEST(ot_rpc_instance, test_otInstanceGetId_0)
 {
-	otInstance *instance = (otInstance *)INSTANCE_ADDR;
 	uint32_t id;
 
-	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_GET_ID, CBOR_UINT32(INSTANCE_ADDR)),
-				   RPC_RSP(0));
-	id = otInstanceGetId(instance);
+	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_GET_ID), RPC_RSP(0));
+	id = otInstanceGetId(NULL);
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_equal(id, 0);
@@ -70,12 +61,11 @@ ZTEST(ot_rpc_instance, test_otInstanceGetId_0)
 /* Test serialization of otInstanceGetId() returning max allowed UINT32_MAX */
 ZTEST(ot_rpc_instance, test_otInstanceGetId_max)
 {
-	otInstance *instance = (otInstance *)INSTANCE_ADDR;
 	uint32_t id;
 
-	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_GET_ID, CBOR_UINT32(INSTANCE_ADDR)),
+	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_GET_ID),
 				   RPC_RSP(CBOR_UINT32(UINT32_MAX)));
-	id = otInstanceGetId(instance);
+	id = otInstanceGetId(NULL);
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_equal(id, UINT32_MAX);
@@ -84,13 +74,11 @@ ZTEST(ot_rpc_instance, test_otInstanceGetId_max)
 /* Test serialization of otInstanceIsInitialized() returning false */
 ZTEST(ot_rpc_instance, test_otInstanceIsInitialized_false)
 {
-	otInstance *instance = (otInstance *)INSTANCE_ADDR;
 	bool initialized;
 
-	mock_nrf_rpc_tr_expect_add(
-		RPC_CMD(OT_RPC_CMD_INSTANCE_IS_INITIALIZED, CBOR_UINT32(INSTANCE_ADDR)),
-		RPC_RSP(CBOR_FALSE));
-	initialized = otInstanceIsInitialized(instance);
+	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_IS_INITIALIZED),
+				   RPC_RSP(CBOR_FALSE));
+	initialized = otInstanceIsInitialized(NULL);
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_false(initialized);
@@ -99,13 +87,10 @@ ZTEST(ot_rpc_instance, test_otInstanceIsInitialized_false)
 /* Test serialization of otInstanceIsInitialized() returning true */
 ZTEST(ot_rpc_instance, test_otInstanceIsInitialized_true)
 {
-	otInstance *instance = (otInstance *)INSTANCE_ADDR;
 	bool initialized;
 
-	mock_nrf_rpc_tr_expect_add(
-		RPC_CMD(OT_RPC_CMD_INSTANCE_IS_INITIALIZED, CBOR_UINT32(INSTANCE_ADDR)),
-		RPC_RSP(CBOR_TRUE));
-	initialized = otInstanceIsInitialized(instance);
+	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_IS_INITIALIZED), RPC_RSP(CBOR_TRUE));
+	initialized = otInstanceIsInitialized(NULL);
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_true(initialized);
@@ -114,24 +99,19 @@ ZTEST(ot_rpc_instance, test_otInstanceIsInitialized_true)
 /* Test serialization of otInstanceFinalize() */
 ZTEST(ot_rpc_instance, test_otInstanceFinalize)
 {
-	otInstance *instance = (otInstance *)INSTANCE_ADDR;
-
-	mock_nrf_rpc_tr_expect_add(
-		RPC_CMD(OT_RPC_CMD_INSTANCE_FINALIZE, CBOR_UINT32(INSTANCE_ADDR)), RPC_RSP());
-	otInstanceFinalize(instance);
+	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_FINALIZE), RPC_RSP());
+	otInstanceFinalize(NULL);
 	mock_nrf_rpc_tr_expect_done();
 }
 
 /* Test serialization of otInstanceErasePersistentInfo() returning success */
 ZTEST(ot_rpc_instance, test_otInstanceErasePersistentInfo_ok)
 {
-	otInstance *instance = (otInstance *)INSTANCE_ADDR;
 	otError error;
 
-	mock_nrf_rpc_tr_expect_add(
-		RPC_CMD(OT_RPC_CMD_INSTANCE_ERASE_PERSISTENT_INFO, CBOR_UINT32(INSTANCE_ADDR)),
-		RPC_RSP(OT_ERROR_NONE));
-	error = otInstanceErasePersistentInfo(instance);
+	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_ERASE_PERSISTENT_INFO),
+				   RPC_RSP(OT_ERROR_NONE));
+	error = otInstanceErasePersistentInfo(NULL);
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_equal(error, OT_ERROR_NONE);
@@ -140,13 +120,11 @@ ZTEST(ot_rpc_instance, test_otInstanceErasePersistentInfo_ok)
 /* Test serialization of otInstanceErasePersistentInfo() returning error */
 ZTEST(ot_rpc_instance, test_otInstanceErasePersistentInfo_error)
 {
-	otInstance *instance = (otInstance *)INSTANCE_ADDR;
 	otError error;
 
-	mock_nrf_rpc_tr_expect_add(
-		RPC_CMD(OT_RPC_CMD_INSTANCE_ERASE_PERSISTENT_INFO, CBOR_UINT32(INSTANCE_ADDR)),
-		RPC_RSP(OT_ERROR_INVALID_STATE));
-	error = otInstanceErasePersistentInfo(instance);
+	mock_nrf_rpc_tr_expect_add(RPC_CMD(OT_RPC_CMD_INSTANCE_ERASE_PERSISTENT_INFO),
+				   RPC_RSP(OT_ERROR_INVALID_STATE));
+	error = otInstanceErasePersistentInfo(NULL);
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_equal(error, OT_ERROR_INVALID_STATE);
--- tests/subsys/net/openthread/rpc/server/src/instance_suite.c
+++ tests/subsys/net/openthread/rpc/server/src/instance_suite.c
@@ -54,28 +54,10 @@ static void tc_setup(void *f)
 
 /*
  * Test reception of otInstanceInitSingle() command.
- * Test serialization of the result: NULL.
  */
-ZTEST(ot_rpc_instance, test_otInstanceInitSingle_0)
+ZTEST(ot_rpc_instance, test_otInstanceInitSingle)
 {
-	otInstanceInitSingle_fake.return_val = NULL;
-
-	mock_nrf_rpc_tr_expect_add(RPC_RSP(0), NO_RSP);
-	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_INIT_SINGLE));
-	mock_nrf_rpc_tr_expect_done();
-
-	zassert_equal(otInstanceInitSingle_fake.call_count, 1);
-}
-
-/*
- * Test reception of otInstanceInitSingle() command.
- * Test serialization of the result: 0xffffffff.
- */
-ZTEST(ot_rpc_instance, test_otInstanceInitSingle_max)
-{
-	otInstanceInitSingle_fake.return_val = (otInstance *)UINT32_MAX;
-
-	mock_nrf_rpc_tr_expect_add(RPC_RSP(CBOR_UINT32(UINT32_MAX)), NO_RSP);
+	mock_nrf_rpc_tr_expect_add(RPC_RSP(), NO_RSP);
 	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_INIT_SINGLE));
 	mock_nrf_rpc_tr_expect_done();
 
@@ -88,16 +70,14 @@ ZTEST(ot_rpc_instance, test_otInstanceInitSingle_max)
  */
 ZTEST(ot_rpc_instance, test_otInstanceGetId_0)
 {
-	uintptr_t instance = (uintptr_t)openthread_get_default_instance();
-
 	otInstanceGetId_fake.return_val = 0;
 
 	mock_nrf_rpc_tr_expect_add(RPC_RSP(0), NO_RSP);
-	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_GET_ID, CBOR_UINT32(instance)));
+	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_GET_ID));
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_equal(otInstanceGetId_fake.call_count, 1);
-	zassert_equal(otInstanceGetId_fake.arg0_val, (otInstance *)instance);
+	zassert_equal(otInstanceGetId_fake.arg0_val, openthread_get_default_instance());
 }
 
 /*
@@ -106,16 +86,14 @@ ZTEST(ot_rpc_instance, test_otInstanceGetId_0)
  */
 ZTEST(ot_rpc_instance, test_otInstanceGetId_max)
 {
-	uintptr_t instance = (uintptr_t)openthread_get_default_instance();
-
-	otInstanceGetId_fake.return_val = 0xffffffff;
+	otInstanceGetId_fake.return_val = UINT32_MAX;
 
 	mock_nrf_rpc_tr_expect_add(RPC_RSP(CBOR_UINT32(UINT32_MAX)), NO_RSP);
-	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_GET_ID, CBOR_UINT32(instance)));
+	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_GET_ID));
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_equal(otInstanceGetId_fake.call_count, 1);
-	zassert_equal(otInstanceGetId_fake.arg0_val, (otInstance *)instance);
+	zassert_equal(otInstanceGetId_fake.arg0_val, openthread_get_default_instance());
 }
 
 /*
@@ -124,16 +102,14 @@ ZTEST(ot_rpc_instance, test_otInstanceGetId_max)
  */
 ZTEST(ot_rpc_instance, test_otInstanceIsInitialized_false)
 {
-	uintptr_t instance = (uintptr_t)openthread_get_default_instance();
-
 	otInstanceIsInitialized_fake.return_val = false;
 
 	mock_nrf_rpc_tr_expect_add(RPC_RSP(CBOR_FALSE), NO_RSP);
-	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_IS_INITIALIZED, CBOR_UINT32(instance)));
+	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_IS_INITIALIZED));
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_equal(otInstanceIsInitialized_fake.call_count, 1);
-	zassert_equal(otInstanceIsInitialized_fake.arg0_val, (otInstance *)instance);
+	zassert_equal(otInstanceIsInitialized_fake.arg0_val, openthread_get_default_instance());
 }
 
 /*
@@ -142,16 +118,14 @@ ZTEST(ot_rpc_instance, test_otInstanceIsInitialized_false)
  */
 ZTEST(ot_rpc_instance, test_otInstanceIsInitialized_true)
 {
-	uintptr_t instance = (uintptr_t)openthread_get_default_instance();
-
 	otInstanceIsInitialized_fake.return_val = true;
 
 	mock_nrf_rpc_tr_expect_add(RPC_RSP(CBOR_TRUE), NO_RSP);
-	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_IS_INITIALIZED, CBOR_UINT32(instance)));
+	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_IS_INITIALIZED));
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_equal(otInstanceIsInitialized_fake.call_count, 1);
-	zassert_equal(otInstanceIsInitialized_fake.arg0_val, (otInstance *)instance);
+	zassert_equal(otInstanceIsInitialized_fake.arg0_val, openthread_get_default_instance());
 }
 
 /*
@@ -159,13 +133,12 @@ ZTEST(ot_rpc_instance, test_otInstanceIsInitialized_true)
  */
 ZTEST(ot_rpc_instance, test_otInstanceFinalize)
 {
-	uintptr_t instance = (uintptr_t)openthread_get_default_instance();
-
 	mock_nrf_rpc_tr_expect_add(RPC_RSP(), NO_RSP);
-	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_FINALIZE, CBOR_UINT32(instance)));
+	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_FINALIZE));
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_equal(otInstanceFinalize_fake.call_count, 1);
+	zassert_equal(otInstanceFinalize_fake.arg0_val, openthread_get_default_instance());
 }
 
 /*
@@ -174,17 +147,15 @@ ZTEST(ot_rpc_instance, test_otInstanceFinalize)
  */
 ZTEST(ot_rpc_instance, test_otInstanceErasePersistentInfo_ok)
 {
-	uintptr_t instance = (uintptr_t)openthread_get_default_instance();
-
 	otInstanceErasePersistentInfo_fake.return_val = OT_ERROR_NONE;
 
 	mock_nrf_rpc_tr_expect_add(RPC_RSP(OT_ERROR_NONE), NO_RSP);
-	mock_nrf_rpc_tr_receive(
-		RPC_CMD(OT_RPC_CMD_INSTANCE_ERASE_PERSISTENT_INFO, CBOR_UINT32(instance)));
+	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_ERASE_PERSISTENT_INFO));
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_equal(otInstanceErasePersistentInfo_fake.call_count, 1);
-	zassert_equal(otInstanceErasePersistentInfo_fake.arg0_val, (otInstance *)instance);
+	zassert_equal(otInstanceErasePersistentInfo_fake.arg0_val,
+		      openthread_get_default_instance());
 }
 
 /*
@@ -193,17 +164,15 @@ ZTEST(ot_rpc_instance, test_otInstanceErasePersistentInfo_ok)
  */
 ZTEST(ot_rpc_instance, test_otInstanceErasePersistentInfo_error)
 {
-	uintptr_t instance = (uintptr_t)openthread_get_default_instance();
-
 	otInstanceErasePersistentInfo_fake.return_val = OT_ERROR_INVALID_STATE;
 
 	mock_nrf_rpc_tr_expect_add(RPC_RSP(OT_ERROR_INVALID_STATE), NO_RSP);
-	mock_nrf_rpc_tr_receive(
-		RPC_CMD(OT_RPC_CMD_INSTANCE_ERASE_PERSISTENT_INFO, CBOR_UINT32(instance)));
+	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_ERASE_PERSISTENT_INFO));
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_equal(otInstanceErasePersistentInfo_fake.call_count, 1);
-	zassert_equal(otInstanceErasePersistentInfo_fake.arg0_val, (otInstance *)instance);
+	zassert_equal(otInstanceErasePersistentInfo_fake.arg0_val,
+		      openthread_get_default_instance());
 }
 
 /*
@@ -211,14 +180,12 @@ ZTEST(ot_rpc_instance, test_otInstanceErasePersistentInfo_error)
  */
 ZTEST(ot_rpc_instance, test_otInstanceFactoryReset)
 {
-	uintptr_t instance = (uintptr_t)openthread_get_default_instance();
-
 	mock_nrf_rpc_tr_expect_add(RPC_RSP(), NO_RSP);
 	mock_nrf_rpc_tr_receive(RPC_CMD(OT_RPC_CMD_INSTANCE_FACTORY_RESET));
 	mock_nrf_rpc_tr_expect_done();
 
 	zassert_equal(otInstanceFactoryReset_fake.call_count, 1);
-	zassert_equal(otInstanceFactoryReset_fake.arg0_val, (otInstance *)instance);
+	zassert_equal(otInstanceFactoryReset_fake.arg0_val, openthread_get_default_instance());
 }
 
 /*
