0ebd718d51a7b4f29fe2e9bcc0721b4baee4e7f0
Bartosz Miller <bartosz.miller@nordicsemi.no>
2025-11-20 12:33:49 +0100

    tests: drivers: timer: Run GRTC timer (LFRC src) on nrf54l
    Run the GRTC timer (sourced from LFRC) accuracy
    test on the nrf54l devices
    
    Signed-off-by: Bartosz Miller <bartosz.miller@nordicsemi.no>

--- tests/drivers/timer/grtc_timer_lfrc/src/main.c
+++ tests/drivers/timer/grtc_timer_lfrc/src/main.c
@@ -3,95 +3,30 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/timer/nrf_grtc_timer.h>
-#include <hal/nrf_grtc.h>
-#include <zephyr/drivers/counter.h>
-#include <zephyr/devicetree/clocks.h>
-#include <zephyr/drivers/clock_control/nrf_clock_control.h>
 
-#define REQUEST_SERVING_WAIT_TIME_US 10000
-#if defined(CONFIG_COVERAGE)
-#define DIFF_TOLERANCE 2
-#else
-#define DIFF_TOLERANCE 1
-#endif
-#define NUMBER_OF_REPETITIONS 5
-
-typedef enum {
-	NO_CHANGE = 0,
-	CHANGE_TO_SYNTH = 1,
-	CHANGE_TO_LFRC = 2
-} lfclk_source_change_option;
-
-struct accuracy_test_limit {
-	uint32_t grtc_timer_count_time_ms;
-	uint32_t time_delta_abs_tolerance_us;
-};
+#include "common.h"
 
 static volatile uint8_t compare_isr_call_counter;
 static volatile uint64_t compare_count_value;
 static volatile uint64_t reference_timer_value_us;
 
 const struct device *const tst_timer_dev = DEVICE_DT_GET(DT_ALIAS(tst_timer));
-const struct device *const lfclk_dev = DEVICE_DT_GET(DT_NODELABEL(lfclk));
-const struct device *const fll16m_dev = DEVICE_DT_GET(DT_NODELABEL(fll16m));
 
-const struct nrf_clock_spec lfclk_lfrc_mode = {
-	.frequency = 32768,
-	.accuracy = 0,
-	.precision = NRF_CLOCK_CONTROL_PRECISION_DEFAULT,
-};
-
-const struct nrf_clock_spec lfclk_synth_mode = {
-	.frequency = 32768,
+const struct nrf_clock_spec clock_hf = {
+	.frequency = MHZ(32),
 	.accuracy = 30,
 	.precision = NRF_CLOCK_CONTROL_PRECISION_DEFAULT,
 };
 
-const struct nrf_clock_spec fll16m_bypass_mode = {
-	.frequency = MHZ(16),
-	.accuracy = 30,
-	.precision = NRF_CLOCK_CONTROL_PRECISION_DEFAULT,
-};
-
-static int request_clock_spec(const struct device *clk_dev, const struct nrf_clock_spec *clk_spec)
-{
-	int ret = 0;
-	int res = 0;
-	struct onoff_client cli;
-	uint32_t rate;
-
-	printk("Clock: %s, requesting frequency=%d, accuracy=%d, precision=%d\n", clk_dev->name,
-	       clk_spec->frequency, clk_spec->accuracy, clk_spec->precision);
-	sys_notify_init_spinwait(&cli.notify);
-	ret = nrf_clock_control_request(clk_dev, clk_spec, &cli);
-	if (!(ret >= 0 && ret <= 2)) {
-		return -1;
-	}
-	do {
-		ret = sys_notify_fetch_result(&cli.notify, &res);
-		k_yield();
-	} while (ret == -EAGAIN);
-	if (ret != 0) {
-		return -2;
-	}
-	if (res != 0) {
-		return -3;
-	}
-	ret = clock_control_get_rate(clk_dev, NULL, &rate);
-	if (ret != -ENOSYS) {
-		if (ret != 0) {
-			return -4;
-		}
-		if (rate != clk_spec->frequency) {
-			return -5;
-		};
-		k_busy_wait(REQUEST_SERVING_WAIT_TIME_US);
-	}
-
-	return 0;
-}
+#if defined(CONFIG_SOC_NRF54H20)
+extern const struct device *const lfclk_dev;
+extern const struct device *const fll16m_dev;
+extern const struct nrf_clock_spec lfclk_lfrc_mode;
+extern const struct nrf_clock_spec lfclk_synth_mode;
+extern const struct nrf_clock_spec fll16m_bypass_mode;
+#else
+extern const struct device *const hfclock;
+#endif /* CONFIG_SOC_NRF54H20 */
 
 static void timer_compare_interrupt_handler(int32_t id, uint64_t expire_time, void *user_data)
 {
@@ -140,6 +75,8 @@ static int test_timer_compare(int32_t channel, const struct accuracy_test_limit 
 		printk("Compare register set failed\n");
 		return -2;
 	}
+
+#if defined(CONFIG_SOC_NRF54H20)
 	if (tst_dynamic_clk_change_option == CHANGE_TO_SYNTH) {
 		printk("Changing clock source of the GRTC (LFCLK) to SYNTH while waiting for "
 		       "compare callback\n");
@@ -151,6 +88,9 @@ static int test_timer_compare(int32_t channel, const struct accuracy_test_limit 
 	} else {
 		printk("Clock source change not requested\n");
 	}
+#else /* CONFIG_SOC_NRF54H20 */
+	printk("Clock source change not supported\n");
+#endif
 
 	k_sleep(K_MSEC(2 * test_limit->grtc_timer_count_time_ms));
 	if (compare_isr_call_counter != 1) {
@@ -188,11 +128,13 @@ static int test_timer_count_in_compare_mode_lfclk_source_from_lfrc(int32_t grtc_
 						       .time_delta_abs_tolerance_us = 250};
 
 	printk("test_timer_count_in_compare_mode_lfclk_source_from_lfrc\n");
+#if defined(CONFIG_SOC_NRF54H20)
 	request_clock_spec(lfclk_dev, &lfclk_lfrc_mode);
 	if (ret != 0) {
 		printk("Clock request failed: %d\n", ret);
 		return -1;
 	}
+#endif /* CONFIG_SOC_NRF54H20 */
 
 	for (int i = 0; i < NUMBER_OF_REPETITIONS; i++) {
 		ret += test_timer_compare(grtc_channel, &test_limit, NO_CHANGE);
@@ -200,7 +142,7 @@ static int test_timer_count_in_compare_mode_lfclk_source_from_lfrc(int32_t grtc_
 	return ret;
 }
 
-#if !defined(CONFIG_SOC_NRF54H20_CPURAD)
+#if defined(CONFIG_SOC_NRF54H20_CPUAPP)
 static int test_timer_count_in_compare_mode_lfclk_source_from_synth(int32_t grtc_channel)
 {
 	int ret = 0;
@@ -257,14 +199,19 @@ static int test_timer_count_in_compare_mode_lfclk_runtime_update_synth_to_lfrc(i
 	}
 	return ret;
 }
-#endif /* !CONFIG_SOC_NRF54H20_CPURAD */
+#endif /* CONFIG_SOC_NRF54H20_CPUAPP */
 
 int main(void)
 {
 	int32_t grtc_channel;
 	int ret = 0;
 
+#if defined(CONFIG_SOC_NRF54H20)
 	ret = request_clock_spec(fll16m_dev, &fll16m_bypass_mode);
+#else
+	k_msleep(HFCLOCK_SETUP_TIME_MS);
+	ret = clock_control_on(hfclock, CLOCK_CONTROL_NRF_SUBSYS_HF);
+#endif /* CONFIG_SOC_NRF54H20 */
 	if (ret != 0) {
 		printk("Clock request failed: %d\n", ret);
 		return -1;
@@ -279,12 +226,11 @@ int main(void)
 	}
 
 	ret = test_timer_count_in_compare_mode_lfclk_source_from_lfrc(grtc_channel);
-#if !defined(CONFIG_SOC_NRF54H20_CPURAD)
-	/* Swiching to SYNTH via Clock Control Api is not possible from CPURAD */
+#if defined(CONFIG_SOC_NRF54H20_CPUAPP)
 	ret += test_timer_count_in_compare_mode_lfclk_source_from_synth(grtc_channel);
 	ret += test_timer_count_in_compare_mode_lfclk_runtime_update_lfrc_to_synth(grtc_channel);
 	ret += test_timer_count_in_compare_mode_lfclk_runtime_update_synth_to_lfrc(grtc_channel);
-#endif /* !CONFIG_SOC_NRF54H20_CPURAD */
+#endif /* CONFIG_SOC_NRF54H20_CPUAPP */
 
 	if (ret != 0) {
 		printk("Tests status: FAIL\n");
