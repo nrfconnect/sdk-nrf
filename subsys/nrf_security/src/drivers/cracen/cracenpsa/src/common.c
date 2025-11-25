de1dad4e973a7bb02043d00148e8abb6f6526117
Anton Zyma <anton.zyma@nordicsemi.no>
2025-11-25 19:08:01 +0200

    nrf_security: CRACEN: Added SSWU hash-to-element method
    Added implementation of the SSWU (Simplified Shallue-Woestijne-Ulas)
    deterministic hash-to-element method.
    
    This is required to derive an element of an ECC group
    (point on the curve) from hash.
    
    The algorithm is aimed to be used for PT secret element
    creation needed by WPA3-SAE.
    
    Ref: NCSDK-29191
    
    Signed-off-by: Anton Zyma <anton.zyma@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/src/common.c
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/src/common.c
@@ -334,6 +334,557 @@ psa_status_t cracen_ecc_reduce_p256(const uint8_t *input, size_t input_size, uin
 	return silex_statuscodes_to_psa(sx_status);
 }
 
+psa_status_t cracen_ecc_is_quadratic_residue(const sx_const_op *curve_prime,
+					     const sx_const_op *value,
+					     bool *is_qr)
+{
+#if PSA_VENDOR_ECC_MAX_CURVE_BITS > 0
+	int sx_status;
+	const size_t op_size = curve_prime->sz;
+	/** Multipurpose buffers: these can be reused so their
+	 *  names consist of the values they can contain, ending with "_buf"
+	 */
+	uint8_t one_prsh_buf[PSA_BITS_TO_BYTES(PSA_VENDOR_ECC_MAX_CURVE_BITS)] = {};
+	uint8_t pm1_tmp_buf[PSA_BITS_TO_BYTES(PSA_VENDOR_ECC_MAX_CURVE_BITS)] = {};
+
+	/**
+	 * value is a quadratic residue mod p if
+	 * value^((p-1)/2) mod p equals zero or one
+	 */
+
+	/* p_m_1 = (p - 1) */
+	cracen_be_add(one_prsh_buf, op_size, 1); /* 1 */
+	if (cracen_be_sub(curve_prime->bytes, one_prsh_buf, pm1_tmp_buf, op_size) != 0) {
+		return PSA_ERROR_INVALID_ARGUMENT;
+	}
+	sx_op p_m_1 = {.sz = op_size, .bytes = pm1_tmp_buf};
+
+	/* (p - 1) / 2 */
+	cracen_be_rshift(p_m_1.bytes, 1, one_prsh_buf, op_size);
+	sx_const_op pm1_div_2 = {.sz = op_size, .bytes = one_prsh_buf};
+
+	/* tmp = value^((p-1)/2) mod p */
+	sx_op tmp = {.sz = op_size, .bytes = pm1_tmp_buf};
+
+	sx_status = sx_mod_exp(NULL, value, &pm1_div_2, curve_prime, &tmp);
+	if (sx_status != SX_OK) {
+		return silex_statuscodes_to_psa(sx_status);
+	}
+
+	safe_memzero(one_prsh_buf, op_size);
+	cracen_be_add(one_prsh_buf, op_size, 1);
+
+	*is_qr = constant_memcmp_is_zero(tmp.bytes, tmp.sz);
+	*is_qr = *is_qr || (constant_memcmp(tmp.bytes, one_prsh_buf, tmp.sz) == 0);
+
+	return PSA_SUCCESS;
+#else
+	return PSA_ERROR_NOT_SUPPORTED;
+#endif /* PSA_VENDOR_ECC_MAX_CURVE_BITS > 0 */
+}
+
+#if PSA_VENDOR_ECC_MAX_CURVE_BITS > 0
+static psa_status_t cracen_get_sswu_z_brainpool_curve(size_t curve_bits, int *z)
+{
+	/**
+	 *  Unique curve parameter Z is taken from table 12.2 of
+	 *  the IEEE802.11 12.4.4.2.3
+	 */
+	switch (curve_bits) {
+	case 256:
+		/* IANA value (group): 28 */
+		*z = -2;
+		break;
+	case 384:
+		/* IANA value (group): 29 */
+		*z = -5;
+		break;
+	case 512:
+		/* IANA value (group): 30 */
+		*z = 7;
+		break;
+	default:
+		return PSA_ERROR_INVALID_ARGUMENT;
+	}
+
+	return PSA_SUCCESS;
+}
+
+static psa_status_t cracen_get_sswu_z_r1_curve(size_t curve_bits, int *z)
+{
+	/**
+	 *  Unique curve parameter Z is taken from table 12.2 of
+	 *  the IEEE802.11 12.4.4.2.3
+	 */
+	switch (curve_bits) {
+	case 192:
+		/* IANA value (group): 25 */
+		*z = -5;
+		break;
+	case 224:
+		/* IANA value (group): 26 */
+		*z = 31;
+		break;
+	case 256:
+		/* IANA value (group): 19 */
+		*z = -10;
+		break;
+	case 384:
+		/* IANA value (group): 20 */
+		*z = -12;
+		break;
+	case 521:
+		/* IANA value (group): 21 */
+		*z = -4;
+		break;
+	default:
+		return PSA_ERROR_INVALID_ARGUMENT;
+	}
+
+	return PSA_SUCCESS;
+}
+
+static psa_status_t cracen_get_sswu_z(psa_ecc_family_t curve_family,
+				      size_t curve_bits,
+				      int *z)
+{
+	switch (curve_family) {
+	case PSA_ECC_FAMILY_BRAINPOOL_P_R1:
+		return cracen_get_sswu_z_brainpool_curve(curve_bits, z);
+	case PSA_ECC_FAMILY_SECP_R1:
+		return cracen_get_sswu_z_r1_curve(curve_bits, z);
+	case PSA_ECC_FAMILY_MONTGOMERY:
+	case PSA_ECC_FAMILY_TWISTED_EDWARDS:
+	case PSA_ECC_FAMILY_SECP_K1:
+	case PSA_ECC_FAMILY_SECP_R2:
+	case PSA_ECC_FAMILY_SECT_K1:
+	case PSA_ECC_FAMILY_SECT_R1:
+	case PSA_ECC_FAMILY_SECT_R2:
+		return PSA_ERROR_NOT_SUPPORTED;
+	default:
+		return PSA_ERROR_INVALID_ARGUMENT;
+	}
+}
+
+static int cracen_ecc_h2e_sswu_calc_gx(const sx_const_op *curve_a, const sx_const_op *curve_b,
+				       const sx_const_op *modulo, const sx_const_op *x, sx_op *gx)
+{
+	int sx_status = SX_ERR_CORRUPTION_DETECTED;
+	size_t op_size = curve_a->sz;
+	/** Multipurpose buffers: these can be reused so their
+	 *  names consist of the values they can contain, ending with "_buf"
+	 */
+	uint8_t three_ax_x3ax_buf[PSA_BITS_TO_BYTES(PSA_VENDOR_ECC_MAX_CURVE_BITS)] = {};
+	uint8_t x3_buf[PSA_BITS_TO_BYTES(PSA_VENDOR_ECC_MAX_CURVE_BITS)] = {};
+	uint8_t ax_buf[PSA_BITS_TO_BYTES(PSA_VENDOR_ECC_MAX_CURVE_BITS)] = {};
+
+	const struct sx_pk_cmd_def *cmd_mul = SX_PK_CMD_ODD_MOD_MULT;
+	const struct sx_pk_cmd_def *cmd_add = SX_PK_CMD_MOD_ADD;
+
+	/* gx = x^3 + a*x + b */
+	/* x_3 = x^3 */
+	cracen_be_add(three_ax_x3ax_buf, op_size, 3); /* 3 */
+	sx_const_op exp = {.sz = op_size, .bytes = three_ax_x3ax_buf};
+	sx_op x_3 = {.sz = op_size, .bytes = x3_buf};
+
+	sx_status = sx_mod_exp(NULL, x, &exp, modulo, &x_3);
+	if (sx_status != SX_OK) {
+		return sx_status;
+	}
+
+	/* ax = a*x */
+	sx_op ax = {.sz = op_size, .bytes = ax_buf};
+
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_mul, modulo, curve_a, x, &ax);
+	if (sx_status != SX_OK) {
+		return sx_status;
+	}
+
+	/* x_3_ax = x^3 + a*x */
+	sx_const_op x_3_const;
+	sx_const_op ax_const;
+	sx_op x_3_ax = {.sz = op_size, .bytes = three_ax_x3ax_buf};
+
+	sx_get_const_op(&x_3, &x_3_const);
+	sx_get_const_op(&ax, &ax_const);
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_add, modulo, &x_3_const, &ax_const, &x_3_ax);
+	if (sx_status != SX_OK) {
+		return sx_status;
+	}
+
+	/* gx = x_3_ax + b */
+	sx_const_op x_3_ax_const;
+
+	sx_get_const_op(&x_3_ax, &x_3_ax_const);
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_add, modulo, &x_3_ax_const, curve_b, gx);
+
+	return sx_status;
+}
+
+static int cracen_ecc_h2e_sswu_calc_m(size_t curve_opsize,
+				      const sx_const_op *z_param,
+				      const sx_const_op *u,
+				      const sx_const_op *modulo,
+				      sx_op *zu_sqr, sx_op *m)
+{
+	int sx_status = SX_ERR_CORRUPTION_DETECTED;
+	const size_t max_key_size = PSA_BITS_TO_BYTES(PSA_VENDOR_ECC_MAX_CURVE_BITS);
+	/** Multipurpose buffer: it can be reused so its
+	 *  name consists of the values they can contain, ending with "_buf"
+	 */
+	uint8_t usqr_zufourth_buf[max_key_size];
+
+	const struct sx_pk_cmd_def *cmd_mul = SX_PK_CMD_ODD_MOD_MULT;
+	const struct sx_pk_cmd_def *cmd_add = SX_PK_CMD_MOD_ADD;
+
+	/** m = (z^2 * u^4 + z * u^2) modulo p
+	 *  m = z*u^2(z*u^2 + 1);
+	 *  zu_sqr = z*u^2 => m = zu_sqr(zu_sqr + 1) => m = (zu_sqr^2 + zu_sqr) modulo p
+	 */
+
+	/* u_sqr = u^2 */
+	sx_op u_sqr = {.sz = curve_opsize, .bytes = usqr_zufourth_buf};
+
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_mul, modulo, u, u, &u_sqr);
+	if (sx_status != SX_OK) {
+		return sx_status;
+	}
+
+	/* zu_sqr = z * u_sqr */
+	sx_const_op u_sqr_const;
+
+	sx_get_const_op(&u_sqr, &u_sqr_const);
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_mul, modulo, z_param, &u_sqr_const, zu_sqr);
+	if (sx_status != SX_OK) {
+		return sx_status;
+	}
+
+	/* zu_fourth = zu_sqr^2 */
+	sx_const_op zu_sqr_const;
+	sx_op zu_fourth = {.sz = curve_opsize, .bytes = usqr_zufourth_buf};
+
+	sx_get_const_op(zu_sqr, &zu_sqr_const);
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_mul, modulo, &zu_sqr_const,
+					 &zu_sqr_const, &zu_fourth);
+	if (sx_status != SX_OK) {
+		return sx_status;
+	}
+
+	/* m = zu_fourth + zu_sqr */
+	sx_const_op zu_fourth_const;
+
+	sx_get_const_op(&zu_fourth, &zu_fourth_const);
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_add, modulo, &zu_fourth_const,
+					 &zu_sqr_const, m);
+
+	return sx_status;
+}
+
+static int cracen_ecc_h2e_sswu_calc_x1(const sx_const_op *a,
+				       const sx_const_op *b,
+				       const sx_const_op *modulo,
+				       const sx_const_op *z_param,
+				       const sx_const_op *t_const,
+				       bool is_m_zero,
+				       sx_op *x1)
+{
+	int sx_status = SX_ERR_CORRUPTION_DETECTED;
+	const size_t max_key_size = PSA_BITS_TO_BYTES(PSA_VENDOR_ECC_MAX_CURVE_BITS);
+	size_t curve_op_size = a->sz;
+	/** Multipurpose buffers: these can be reused so their
+	 *  names consist of the values they can contain, ending with "_buf"
+	 */
+	uint8_t x1b_buf[max_key_size];
+	uint8_t za_pmb_one_buf[max_key_size];
+	uint8_t x1a_buf[max_key_size];
+	uint8_t inva_onept_buf[max_key_size];
+	uint8_t mbinva_buf[max_key_size];
+
+	const struct sx_pk_cmd_def *cmd_mul  = SX_PK_CMD_ODD_MOD_MULT;
+	const struct sx_pk_cmd_def *cmd_add  = SX_PK_CMD_MOD_ADD;
+	const struct sx_pk_cmd_def *cmd_div  = SX_PK_CMD_ODD_MOD_DIV;
+	const struct sx_pk_cmd_def *cmd_inv  = SX_PK_CMD_ODD_MOD_INV;
+
+	/**
+	 *  x1 = CSEL(l, (b / (z * a) modulo p), ((– b/a) * (1 + t)) modulo p)
+	 *
+	 *  x1a = b / (z * a) modulo p;
+	 *  x1b = ((– b/a) * (1 + t)) modulo p;
+	 *  x1 = CSEL(l, x1a, x1b);
+	 */
+
+	/* (z * a) */
+	sx_op za = {.sz = curve_op_size, .bytes = za_pmb_one_buf};
+
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_mul, modulo, z_param, a, &za);
+	if (sx_status != SX_OK) {
+		return sx_status;
+	}
+
+	/* x1a = b / (z * a) modulo p */
+	sx_const_op za_const;
+	sx_op x1a = {.sz = curve_op_size, .bytes = x1a_buf};
+
+	sx_get_const_op(&za, &za_const);
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_div, modulo, b, &za_const, &x1a);
+	if (sx_status != SX_OK) {
+		return sx_status;
+	}
+
+	/* x1b calculation */
+	/* p_m_b = p - b */
+	if (cracen_be_sub(modulo->bytes, b->bytes, za_pmb_one_buf, modulo->sz) != 0) {
+		return SX_ERR_INVALID_ARG;
+	}
+	sx_const_op p_m_b = {.sz = modulo->sz, .bytes = za_pmb_one_buf};
+
+	/* inv_a = 1/a mod p */
+	sx_op inv_a = {.sz = curve_op_size, .bytes = inva_onept_buf};
+
+	sx_status = sx_mod_single_op_cmd(cmd_inv, modulo, a, &inv_a);
+	if (sx_status != SX_OK) {
+		return sx_status;
+	}
+
+	/* m_b_inv_a = (p - b) mod p * 1/a mod p = (– b/a) mod p */
+	sx_const_op inv_a_const;
+	sx_op m_b_inv_a = {.sz = curve_op_size, .bytes = mbinva_buf};
+
+	sx_get_const_op(&inv_a, &inv_a_const);
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_mul, modulo, &p_m_b, &inv_a_const, &m_b_inv_a);
+	if (sx_status != SX_OK) {
+		return sx_status;
+	}
+
+	/* one_p_t = (1 + t) mod p */
+	safe_memzero(za_pmb_one_buf, curve_op_size);
+	cracen_be_add(za_pmb_one_buf, curve_op_size, 1);
+
+	sx_const_op one = {.sz = curve_op_size, .bytes = za_pmb_one_buf};
+	sx_op one_p_t = {.sz = curve_op_size, .bytes = inva_onept_buf};
+
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_add, modulo, &one, t_const, &one_p_t);
+	if (sx_status != SX_OK) {
+		return sx_status;
+	}
+
+	/* x1b = (m_b_inv_a * one_p_t) mod p = ((– b/a) * (1 + t)) mod p */
+	sx_const_op m_b_inv_a_const;
+	sx_const_op one_p_t_const;
+	sx_op x1b = {.sz = curve_op_size, .bytes = x1b_buf};
+
+	sx_get_const_op(&m_b_inv_a, &m_b_inv_a_const);
+	sx_get_const_op(&one_p_t, &one_p_t_const);
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_mul, modulo, &m_b_inv_a_const,
+					 &one_p_t_const, &x1b);
+	if (sx_status != SX_OK) {
+		return sx_status;
+	}
+
+	/* x1 = CSEL(l, (b / (z * a) modulo p), ((– b/a) * (1 + t)) modulo p) */
+	constant_select_bin(is_m_zero, x1a.bytes, x1b.bytes, x1->bytes, x1->sz);
+	return sx_status;
+}
+#endif /* PSA_VENDOR_ECC_MAX_CURVE_BITS > 0 */
+
+psa_status_t cracen_ecc_h2e_sswu(psa_ecc_family_t curve_family, size_t curve_bits,
+				 const sx_const_op *u, const sx_op *result)
+{
+#if PSA_VENDOR_ECC_MAX_CURVE_BITS > 0
+	psa_status_t status = PSA_ERROR_CORRUPTION_DETECTED;
+	int sx_status;
+	const size_t max_key_size = PSA_BITS_TO_BYTES(PSA_VENDOR_ECC_MAX_CURVE_BITS);
+
+	int z_int;
+	uint8_t z_buf[max_key_size];
+	uint8_t x2_buf[max_key_size];
+	uint8_t v_buf[max_key_size];
+	/** Multipurpose buffers: these can be reused so their
+	 *  names consist of the values they can contain, ending with "_buf"
+	 */
+	uint8_t two_pdec_buf[max_key_size];
+	uint8_t pmy_buf[max_key_size];
+	uint8_t zero_t_gx1_x_buf[max_key_size];
+	uint8_t zusqr_gx2_y_buf[max_key_size];
+	uint8_t m_x1_buf[max_key_size];
+
+	const struct sx_pk_ecurve *sx_curve = NULL;
+	const struct sx_pk_cmd_def *cmd_mul  = SX_PK_CMD_ODD_MOD_MULT;
+	const struct sx_pk_cmd_def *cmd_sqrt = SX_PK_CMD_MOD_SQRT;
+
+	status = cracen_ecc_get_ecurve_from_psa(curve_family, curve_bits, &sx_curve);
+	if (status != PSA_SUCCESS) {
+		return status;
+	}
+
+	/* Output of this algorithm is ECC curve point (x; y) */
+	if (result->sz < (u->sz * 2u)) {
+		return PSA_ERROR_INVALID_ARGUMENT;
+	}
+
+	sx_const_op z_param = {
+		.sz = sx_pk_curve_opsize(sx_curve),
+		.bytes = z_buf
+	};
+
+	status = cracen_get_sswu_z(curve_family, curve_bits, &z_int);
+	if (status != PSA_SUCCESS) {
+		return status;
+	}
+
+	if (z_int < 0) {
+		safe_memzero(zero_t_gx1_x_buf, sx_pk_curve_opsize(sx_curve));
+		cracen_be_add(zero_t_gx1_x_buf, sx_pk_curve_opsize(sx_curve), abs(z_int));
+		cracen_be_sub(sx_pk_curve_prime(sx_curve), zero_t_gx1_x_buf, z_buf,
+			      sx_pk_curve_opsize(sx_curve));
+	} else {
+		cracen_be_add(z_buf, sx_pk_curve_opsize(sx_curve), abs(z_int));
+	}
+
+	sx_const_op modulo = {.sz = sx_pk_curve_opsize(sx_curve),
+			      .bytes = sx_pk_curve_prime(sx_curve)
+	};
+
+	/**
+	 * CEQ(x, y) - operates in constant time and returns true if x equals y and false otherwise;
+	 * CSEL(x, y, z) - operates in constant time and returns y if x is true and z otherwise
+	 * inv0(x) - is calculated as x^(p-2) modulo p.
+	 *
+	 * Note: SSWU algorithm shall always be executed in constant time.
+	 */
+
+	sx_const_op zu_sqr_const;
+	sx_op zu_sqr = {.sz = sx_pk_curve_opsize(sx_curve), .bytes = zusqr_gx2_y_buf};
+	sx_op m = {.sz = sx_pk_curve_opsize(sx_curve), .bytes = m_x1_buf};
+
+	/* m = (z^2 * u^4 + z * u^2) modulo p */
+	sx_status = cracen_ecc_h2e_sswu_calc_m(sx_pk_curve_opsize(sx_curve),
+					       &z_param, u, &modulo, &zu_sqr, &m);
+	if (sx_status != SX_OK) {
+		goto error;
+	}
+	sx_get_const_op(&zu_sqr, &zu_sqr_const);
+
+	/**
+	 * l = CEQ(m, 0) =>
+	 * is_m_zero = (m == 0);
+	 */
+	bool is_m_zero = constant_memcmp_is_zero(m.bytes, m.sz);
+
+	/**
+	 * t = inv0(m) =>
+	 * t = m^(p-2) modulo p;
+	 */
+	safe_memzero(two_pdec_buf, sx_pk_curve_opsize(sx_curve));
+	cracen_be_add(two_pdec_buf, sx_pk_curve_opsize(sx_curve), 2);
+	cracen_be_sub(sx_pk_curve_prime(sx_curve), two_pdec_buf, two_pdec_buf,
+		      sx_pk_curve_opsize(sx_curve));
+
+	sx_const_op m_const;
+	sx_const_op p_decreased = {.sz = sx_pk_curve_opsize(sx_curve), .bytes = two_pdec_buf};
+	sx_op t = {.sz = sx_pk_curve_opsize(sx_curve), .bytes = zero_t_gx1_x_buf};
+
+	sx_get_const_op(&m, &m_const);
+	sx_status = sx_mod_exp(NULL, &m_const, &p_decreased, &modulo, &t);
+	if (sx_status != SX_OK) {
+		goto error;
+	}
+
+	/* x1 = CSEL(l, (b / (z * a) modulo p), ((– b/a) * (1 + t)) modulo p) */
+	sx_const_op a = {.sz = sx_pk_curve_opsize(sx_curve),
+			 .bytes = sx_pk_curve_param_a(sx_curve)};
+	sx_const_op b = {.sz = sx_pk_curve_opsize(sx_curve),
+			 .bytes = sx_pk_curve_param_b(sx_curve)};
+	sx_op x1 = {.sz = sx_pk_curve_opsize(sx_curve), .bytes = m_x1_buf};
+	sx_const_op t_const;
+
+	sx_get_const_op(&t, &t_const);
+	sx_status = cracen_ecc_h2e_sswu_calc_x1(&a, &b, &modulo, &z_param,
+						&t_const, is_m_zero, &x1);
+	if (sx_status != SX_OK) {
+		goto error;
+	}
+
+	/* gx1 = (x1^3 + a * x1 + b) modulo p */
+	sx_const_op x1_const;
+	sx_op gx1 = {.sz = sx_pk_curve_opsize(sx_curve), .bytes = zero_t_gx1_x_buf};
+
+	sx_get_const_op(&x1, &x1_const);
+	sx_status = cracen_ecc_h2e_sswu_calc_gx(&a, &b, &modulo, &x1_const, &gx1);
+	if (sx_status != SX_OK) {
+		goto error;
+	}
+
+	/* x2 = (z * u^2 * x1) modulo p */
+	sx_op x2 = {.sz = sx_pk_curve_opsize(sx_curve), .bytes = x2_buf};
+
+	sx_status = sx_mod_primitive_cmd(NULL, cmd_mul, &modulo, &zu_sqr_const, &x1_const, &x2);
+	if (sx_status != SX_OK) {
+		goto error;
+	}
+
+	/* gx2 = (x2^3 + a * x2 + b) modulo p */
+	sx_const_op x2_const;
+	sx_op gx2 = {.sz = sx_pk_curve_opsize(sx_curve), .bytes = zusqr_gx2_y_buf};
+
+	sx_get_const_op(&x2, &x2_const);
+	sx_status = cracen_ecc_h2e_sswu_calc_gx(&a, &b, &modulo, &x2_const, &gx2);
+	if (sx_status != SX_OK) {
+		goto error;
+	}
+
+	/* l = gx1 is a quadratic residue modulo p */
+	sx_const_op gx1_const;
+	bool is_gx1_qr = false;
+
+	sx_get_const_op(&gx1, &gx1_const);
+	status = cracen_ecc_is_quadratic_residue(&modulo, &gx1_const, &is_gx1_qr);
+	if (status != PSA_SUCCESS) {
+		return status;
+	}
+
+	/* v = CSEL(l, gx1, gx2) */
+	sx_op v = {.sz = sx_pk_curve_opsize(sx_curve), .bytes = v_buf};
+
+	constant_select_bin(is_gx1_qr, gx1.bytes, gx2.bytes, v.bytes, v.sz);
+
+	/* x = CSEL(l, x1, x2 ) */
+	sx_op x = {.sz = sx_pk_curve_opsize(sx_curve), .bytes = zero_t_gx1_x_buf};
+
+	constant_select_bin(is_gx1_qr, x1.bytes, x2.bytes, x.bytes, x.sz);
+
+	/* y = sqrt(v) */
+	sx_const_op v_const;
+	sx_op y = {.sz = sx_pk_curve_opsize(sx_curve), .bytes = zusqr_gx2_y_buf};
+
+	sx_get_const_op(&v, &v_const);
+	sx_status = sx_mod_single_op_cmd(cmd_sqrt, &modulo, &v_const, &y);
+	if (sx_status != SX_OK) {
+		goto error;
+	}
+
+	/* l = CEQ(LSB(u), LSB(y)) */
+	uint8_t lsb_u = u->bytes[u->sz - 1] & 0x01;
+	uint8_t lsb_y = y.bytes[y.sz - 1] & 0x01;
+	bool lsb_eq = (constant_memcmp(&lsb_u, &lsb_y, 1) == 0);
+
+	/* P = CSEL(l, (x,y), (x, p – y)) */
+	memcpy(result->bytes, x.bytes, sx_pk_curve_opsize(sx_curve));
+	/* pmy_buf = p - y */
+	cracen_be_sub(sx_pk_curve_prime(sx_curve), y.bytes, pmy_buf,
+		      sx_pk_curve_opsize(sx_curve));
+	constant_select_bin(lsb_eq, y.bytes, pmy_buf,
+			    result->bytes + sx_pk_curve_opsize(sx_curve),
+			    sx_pk_curve_opsize(sx_curve));
+
+	return status;
+error:
+	return silex_statuscodes_to_psa(sx_status);
+#else
+	return PSA_ERROR_NOT_SUPPORTED;
+#endif /* PSA_VENDOR_ECC_MAX_CURVE_BITS > 0 */
+}
+
 psa_status_t cracen_ecc_check_public_key(const struct sx_pk_ecurve *curve,
 					 const sx_pk_const_affine_point *in_pnt)
 {
@@ -880,6 +1431,25 @@ int cracen_be_sub(const uint8_t *a, const uint8_t *b, uint8_t *c, size_t sz)
 	}
 
 	return borrow ? -1 : 0;
+}
+
+void cracen_be_rshift(const uint8_t *a, int n, uint8_t *r, size_t sz)
+{
+	unsigned int carry = 0;
+	unsigned int result;
+	unsigned int start_index;
+	size_t byte_shift = (size_t)(n >> 3);	/* n / 8 */
+	size_t bit_shift  = (size_t)(n & 7);	/* n % 8 */
+
+	safe_memzero(r, sz);
+
+	for (size_t i = 0; i < sz - byte_shift; i++) {
+		start_index = i + byte_shift;
+		result = (a[i] >> bit_shift) | (carry << (8 - bit_shift));
+		carry = a[i] & ((1u << bit_shift) - 1u);
+
+		r[start_index] = result;
+	}
 }
 
 int cracen_be_cmp(const uint8_t *a, const uint8_t *b, size_t sz, int carry)
