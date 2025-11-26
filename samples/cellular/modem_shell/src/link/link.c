jtguggedal/fw-nrfconnect-nrf

samples: cellular: modem_shell: Use lte_lc for PDN
Use the PDN APIs provided by LTE link controller.
The PDN library is planned to be deprecated.

Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

samples/cellular/modem_shell/src/link/link.c
@@ -15,7 +15,6 @@
 
 #include <modem/modem_info.h>
 #include <modem/lte_lc.h>
-#include <modem/pdn.h>
 
 #include <dk_buttons_and_leds.h>
 
@@ -116,7 +115,7 @@ static void link_api_activate_mosh_contexts(
 	for (i = 0; i < size; i++) {
 		if (pdn_act_status_arr[i].activated == false &&
 		    link_shell_pdn_info_is_in_list(pdn_act_status_arr[i].cid)) {
-			ret = pdn_activate(pdn_act_status_arr[i].cid, &esm, NULL);
+			ret = lte_lc_pdn_ctx_activate(pdn_act_status_arr[i].cid, &esm, NULL);
 			if (ret) {
 				mosh_warn(
 					"Cannot reactivate ctx with CID #%d, err: %d, removing from the list",
@@ -516,11 +515,11 @@ static int link_default_pdp_context_set(void)
 	int ret;
 
 	if (link_sett_is_defcont_enabled() == true) {
-		ret = pdn_ctx_configure(0, link_sett_defcont_apn_get(),
-					link_sett_defcont_pdn_family_get(),
-					NULL);
+		ret = lte_lc_pdn_ctx_configure(0, link_sett_defcont_apn_get(),
+					       link_sett_defcont_pdn_family_get(),
+					       NULL);
 		if (ret) {
-			mosh_error("pdn_ctx_configure returned err %d", ret);
+			mosh_error("lte_lc_pdn_ctx_configure returned err %d", ret);
 			return ret;
 		}
 	}
@@ -532,12 +531,12 @@ static int link_default_pdp_context_auth_set(void)
 	int ret;
 
 	if (link_sett_is_defcontauth_enabled() == true) {
-		ret = pdn_ctx_auth_set(0, link_sett_defcontauth_prot_get(),
-				       link_sett_defcontauth_username_get(),
-				       link_sett_defcontauth_password_get());
+		ret = lte_lc_pdn_ctx_auth_set(0, link_sett_defcontauth_prot_get(),
+					      link_sett_defcontauth_username_get(),
+					      link_sett_defcontauth_password_get());
 
 		if (ret) {
-			mosh_error("pdn_ctx_auth_set returned err  %d", ret);
+			mosh_error("lte_lc_pdn_ctx_auth_set returned err  %d", ret);
 			return ret;
 		}
 	}

samples/cellular/modem_shell/src/link/link_api.c
@@ -21,7 +21,6 @@
 #include <zephyr/shell/shell.h>
 
 #include <modem/modem_info.h>
-#include <modem/pdn.h>
 
 #include <zephyr/posix/arpa/inet.h>
 #include <zephyr/net/net_ip.h>
@@ -350,8 +349,9 @@ static void link_api_modem_operator_info_read_for_shell(void)
 
 static int link_api_pdp_context_dynamic_params_get(struct pdp_context_info *populated_info)
 {
-	struct pdn_dynamic_info pdn_dynamic_info;
-	const int ret = pdn_dynamic_info_get(populated_info->cid, &pdn_dynamic_info);
+	struct lte_lc_pdn_dynamic_info pdn_dynamic_info;
+	const int ret = lte_lc_pdn_ctx_dynamic_info_get(populated_info->cid,
+							&pdn_dynamic_info);
 
 	populated_info->ipv4_mtu = pdn_dynamic_info.ipv4_mtu;
 	populated_info->ipv6_mtu = pdn_dynamic_info.ipv6_mtu;
@@ -427,7 +427,7 @@ parse:
 		mosh_error("Could not parse CID, err: %d", ret);
 		goto clean_exit;
 	}
-	ret = pdn_id_get(populated_info[iterator].cid);
+	ret = lte_lc_pdn_ctx_connection_id_get(populated_info[iterator].cid);
 	if (ret < 0) {
 		mosh_error(
 			"Could not get PDN for CID %d, err: %d\n",

nrfconnect/sdk-nrf

samples/cellular/modem_shell/src/link/link.c
@@ -15,7 +15,6 @@
 
 #include <modem/modem_info.h>
 #include <modem/lte_lc.h>
-#include <modem/pdn.h>
 
 #include <dk_buttons_and_leds.h>
 
@@ -116,7 +115,7 @@ static void link_api_activate_mosh_contexts(
 	for (i = 0; i < size; i++) {
 		if (pdn_act_status_arr[i].activated == false &&
 		    link_shell_pdn_info_is_in_list(pdn_act_status_arr[i].cid)) {
-			ret = pdn_activate(pdn_act_status_arr[i].cid, &esm, NULL);
+			ret = lte_lc_pdn_activate(pdn_act_status_arr[i].cid, &esm, NULL);
 			if (ret) {
 				mosh_warn(
 					"Cannot reactivate ctx with CID #%d, err: %d, removing from the list",
@@ -516,11 +515,11 @@ static int link_default_pdp_context_set(void)
 	int ret;
 
 	if (link_sett_is_defcont_enabled() == true) {
-		ret = pdn_ctx_configure(0, link_sett_defcont_apn_get(),
-					link_sett_defcont_pdn_family_get(),
-					NULL);
+		ret = lte_lc_pdn_ctx_configure(0, link_sett_defcont_apn_get(),
+					       link_sett_defcont_pdn_family_get(),
+					       NULL);
 		if (ret) {
-			mosh_error("pdn_ctx_configure returned err %d", ret);
+			mosh_error("lte_lc_pdn_ctx_configure returned err %d", ret);
 			return ret;
 		}
 	}
@@ -532,12 +531,12 @@ static int link_default_pdp_context_auth_set(void)
 	int ret;
 
 	if (link_sett_is_defcontauth_enabled() == true) {
-		ret = pdn_ctx_auth_set(0, link_sett_defcontauth_prot_get(),
-				       link_sett_defcontauth_username_get(),
-				       link_sett_defcontauth_password_get());
+		ret = lte_lc_pdn_ctx_auth_set(0, link_sett_defcontauth_prot_get(),
+					      link_sett_defcontauth_username_get(),
+					      link_sett_defcontauth_password_get());
 
 		if (ret) {
-			mosh_error("pdn_ctx_auth_set returned err  %d", ret);
+			mosh_error("lte_lc_pdn_ctx_auth_set returned err  %d", ret);
 			return ret;
 		}
 	}

samples/cellular/modem_shell/src/link/link_api.c
@@ -21,7 +21,6 @@
 #include <zephyr/shell/shell.h>
 
 #include <modem/modem_info.h>
-#include <modem/pdn.h>
 
 #include <zephyr/posix/arpa/inet.h>
 #include <zephyr/net/net_ip.h>
@@ -350,8 +349,9 @@ static void link_api_modem_operator_info_read_for_shell(void)
 
 static int link_api_pdp_context_dynamic_params_get(struct pdp_context_info *populated_info)
 {
-	struct pdn_dynamic_info pdn_dynamic_info;
-	const int ret = pdn_dynamic_info_get(populated_info->cid, &pdn_dynamic_info);
+	struct lte_lc_pdn_dynamic_info pdn_dynamic_info;
+	const int ret = lte_lc_pdn_dynamic_info_get(populated_info->cid,
+						    &pdn_dynamic_info);
 
 	populated_info->ipv4_mtu = pdn_dynamic_info.ipv4_mtu;
 	populated_info->ipv6_mtu = pdn_dynamic_info.ipv6_mtu;
@@ -427,7 +427,7 @@ parse:
 		mosh_error("Could not parse CID, err: %d", ret);
 		goto clean_exit;
 	}
-	ret = pdn_id_get(populated_info[iterator].cid);
+	ret = lte_lc_pdn_id_get(populated_info[iterator].cid);
 	if (ret < 0) {
 		mosh_error(
 			"Could not get PDN for CID %d, err: %d\n",