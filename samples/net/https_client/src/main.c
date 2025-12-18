d9c9a2bd9d1d354ed043227e916fad866cf84869
Jorgen Kvalvaag <jorgen.kvalvaag@nordicsemi.no>
2025-12-18 10:00:15 +0100

    samples: https_client: Certificate update
    Example.com changed to Cloudflare certificates. Update sample
    to use correct CA.
    
    Signed-off-by: Jorgen Kvalvaag <jorgen.kvalvaag@nordicsemi.no>

--- samples/net/https_client/src/main.c
+++ samples/net/https_client/src/main.c
@@ -44,7 +44,7 @@ static char recv_buf[RECV_BUF_SIZE];
 static K_SEM_DEFINE(network_connected_sem, 0, 1);
 /* Certificate for `example.com` */
 static const char cert[] = {
-	#include "DigiCertGlobalG3.pem.inc"
+	#include "CloudflareTLSIssuingECCCA3.pem.inc"
 
 	/* Null terminate certificate if running Mbed TLS on the application core.
 	 * Required by TLS credentials API.
