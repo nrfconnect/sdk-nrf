5509c2ead7261fde18e6464686a19425c36c8c86
Michał Stasiak <michal.stasiak@nordicsemi.no>
2026-01-16 13:37:39 +0100

    treewide: remove Eng A naming from nRF54LS05B
    Aligned to changes in MDK and Zephyr.
    
    Signed-off-by: Michał Stasiak <michal.stasiak@nordicsemi.no>

--- modules/hal_nordic/nrfx_config_ext.h
+++ modules/hal_nordic/nrfx_config_ext.h
@@ -9,8 +9,12 @@
 
 /* Include nrfx_config_nrf*.h for products which are yet to be upstreamed. */
 
-#if (defined(NRF54LS05B_ENGA_XXAA)) && defined(NRF_APPLICATION)
-    #include"templates/nrfx_config_nrf54ls05b_enga_application.h"
+#if (defined(NRF7120_ENGA_XXAA) && defined(NRF_APPLICATION))
+    #include "templates/nrfx_config_nrf7120_enga_application.h"
+#elif (defined(NRF7120_ENGA_XXAA)) && defined(NRF_FLPR)
+    #include"templates/nrfx_config_nrf7120_enga_flpr.h"
+#elif (defined(NRF54LS05B_XXAA)) && defined(NRF_APPLICATION)
+    #include"templates/nrfx_config_nrf54ls05b_application.h"
 #else
     #error "Unknown device."
 #endif
