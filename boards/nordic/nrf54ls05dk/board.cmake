ee5b845b62207a990aa38349a9e477861e575179
Michał Stasiak <michal.stasiak@nordicsemi.no>
2026-01-16 13:37:39 +0100

    treewide: remove Eng A naming from nRF54LS05B
    Aligned to changes in MDK and Zephyr.
    
    Signed-off-by: Michał Stasiak <michal.stasiak@nordicsemi.no>

--- boards/nordic/nrf54ls05dk/board.cmake
+++ boards/nordic/nrf54ls05dk/board.cmake
@@ -1,7 +1,7 @@
 # Copyright (c) 2025 Nordic Semiconductor ASA
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-if(CONFIG_SOC_NRF54LS05B_ENGA_CPUAPP)
+if(CONFIG_SOC_NRF54LS05B_CPUAPP)
   board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
 endif()
 
