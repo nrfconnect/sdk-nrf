62c6bdf7b3b65068c42b79f366b170a4683d6046
Anton Zyma <anton.zyma@nordicsemi.no>
2026-01-22 16:07:17 +0200

    nrf_security: CRACEN: Move ECDH operations from cracen_psa_key_derivation
    Functions implementing ECDH key agreement moved from
    "cracen_psa_key_derivation.c" to a separate modules.
    
    Ref: NCSDK-37020
    
    Signed-off-by: Anton Zyma <anton.zyma@nordicsemi.no>

---
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/src/internal/ecdh/cracen_ecdh_montgomery.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include "cracen_ecdh_montgomery.h"
+
+#include <cracen/common.h>
+#include <cracen/mem_helpers.h>
+#include <cracen/ec_helpers.h>
+#include <silexpk/sxbuf/sxbufop.h>
+#include <silexpk/montgomery.h>
+#include <silexpk/ec_curves.h>
+#include "../ecc/cracen_ecc_helpers.h"
+
+psa_status_t cracen_ecdh_montgmr_calc_secret(const struct sx_pk_ecurve *curve,
+					     const uint8_t *priv_key, size_t priv_key_size,
+					     const uint8_t *publ_key, size_t publ_key_size,
+					     uint8_t *output, size_t output_size,
+					     size_t *output_length)
+{
+	int sx_status;
+	const size_t curve_op_sz = sx_pk_curve_opsize(curve);
+
+	if (publ_key_size != curve_op_sz) {
+		return PSA_ERROR_INVALID_ARGUMENT;
+	} else if (output_size < curve_op_sz) {
+		return PSA_ERROR_BUFFER_TOO_SMALL;
+	}
+
+	sx_status = SX_ERR_INVALID_CURVE_PARAM;
+
+	if (curve_op_sz == CRACEN_X25519_KEY_SIZE_BYTES) {
+		struct sx_x25519_op k;
+
+		memcpy(k.bytes, priv_key, CRACEN_X25519_KEY_SIZE_BYTES);
+		decode_scalar_25519(k.bytes);
+
+		struct sx_x25519_op pt;
+
+		memcpy(pt.bytes, publ_key, CRACEN_X25519_KEY_SIZE_BYTES);
+		decode_u_coordinate_25519(pt.bytes);
+
+		sx_status = sx_x25519_ptmult(&k, &pt, (struct sx_x25519_op *)output);
+
+	} else if (curve_op_sz == CRACEN_X448_KEY_SIZE_BYTES) {
+		struct sx_x448_op k;
+
+		memcpy(k.bytes, priv_key, CRACEN_X448_KEY_SIZE_BYTES);
+		decode_scalar_448(k.bytes);
+
+		/* 448 % 8 = 0, so there is no need to decode pt coordinate. */
+		sx_status = sx_x448_ptmult(&k, (struct sx_x448_op *)publ_key,
+					   (struct sx_x448_op *)output);
+	}
+
+	if (sx_status != SX_OK) {
+		return silex_statuscodes_to_psa(sx_status);
+	}
+
+	*output_length = curve_op_sz;
+
+	return PSA_SUCCESS;
+}
