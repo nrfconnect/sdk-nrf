97ae54063a3f7d6262ef4c741ba570ba48794ebd
Alexander Svensen <alexander.svensen@nordicsemi.no>
2026-01-26 07:43:36 +0100

    tests: nrf_auraconfig: Revert compile.sh changes
    - Revert breaking changes in compile.sh
    -OCT-3473
    
    Signed-off-by: Alexander Svensen <alexander.svensen@nordicsemi.no>

--- tests/bluetooth/bsim/nrf_auraconfig/compile.sh
+++ tests/bluetooth/bsim/nrf_auraconfig/compile.sh
@@ -6,11 +6,10 @@ BOARD=nrf5340bsim/nrf5340/cpuapp
 set -ue
 
 : "${ZEPHYR_BASE:?ZEPHYR_BASE must be set to point to the zephyr root directory}"
-: "${ZEPHYR_NRF_MODULE_DIR:?ZEPHYR_NRF_MODULE_DIR must be set to point to the nrf module directory}"
 
 source ${ZEPHYR_BASE}/tests/bsim/compile.source
 
-app_root="${ZEPHYR_NRF_MODULE_DIR}" app=tests/bluetooth/bsim/nrf_auraconfig sysbuild=1 compile
-app_root="${ZEPHYR_NRF_MODULE_DIR}" app=tests/bluetooth/bsim/nrf_auraconfig/tester sysbuild=1 compile
+app=${ZEPHYR_NRF_MODULE_DIR}tests/bluetooth/bsim/nrf_auraconfig sysbuild=1 compile
+app=${ZEPHYR_NRF_MODULE_DIR}tests/bluetooth/bsim/nrf_auraconfig/tester sysbuild=1 compile
 
 wait_for_background_jobs
