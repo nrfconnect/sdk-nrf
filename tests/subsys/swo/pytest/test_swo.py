940a237c9f29aae43244b8da7b2a6aa2a64898e6
Sebastian Głąb <sebastian.glab@nordicsemi.no>
2026-01-29 10:40:44 +0100

    tests: Stop testing on nrf54lv10dk@0.2.0
    Remove nrf54lv10dk@0.2.0/nrf54lv10a/cpuXXX from
    platform_allow lists in samples and tests.
    
    Board revision 0.2.0 is obsolete.
    Testing continues on board revision 0.7.0 which is the default now.
    
    Signed-off-by: Sebastian Głąb <sebastian.glab@nordicsemi.no>

--- tests/subsys/swo/pytest/test_swo.py
+++ tests/subsys/swo/pytest/test_swo.py
@@ -91,12 +91,6 @@ def test_swo_logging(dut: DeviceAdapter):
             "swofreq": 1000000,
             "args": f"-jlinkscriptfile {TRACEPORT_JLINK_SCRIPT}",
         },
-        "nrf54lv10dk@0.2.0/nrf54lv10a/cpuapp": {
-            "device": "NRF54LV10A_M33",
-            "cpufreq": 128000000,
-            "swofreq": 1000000,
-            "args": f"-jlinkscriptfile {TRACEPORT_JLINK_SCRIPT}",
-        },
         "nrf54lv10dk@0.7.0/nrf54lv10a/cpuapp/ns": {
             "device": "NRF54LV10A_M33",
             "cpufreq": 128000000,
