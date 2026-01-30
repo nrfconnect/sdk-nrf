0148041cc107bc40e99f32e0d4348a0f8d33499a
Robert Robinson <robert.robinson@nordicsemi.no>
2026-01-30 11:15:48 +0000

    modules: hal_nordic: nrfx: Remove nRF7120 references that are upstream
    Removes nRF7120 specific definitions that have been upstreamed to
    zephyr.
    
    Signed-off-by: Robert Robinson <robert.robinson@nordicsemi.no>

--- modules/hal_nordic/nrfx_config_ext.h
+++ modules/hal_nordic/nrfx_config_ext.h
@@ -9,11 +9,7 @@
 
 /* Include nrfx_config_nrf*.h for products which are yet to be upstreamed. */
 
-#if (defined(NRF7120_ENGA_XXAA) && defined(NRF_APPLICATION))
-    #include "templates/nrfx_config_nrf7120_enga_application.h"
-#elif (defined(NRF7120_ENGA_XXAA)) && defined(NRF_FLPR)
-    #include"templates/nrfx_config_nrf7120_enga_flpr.h"
-#elif (defined(NRF54LS05B_ENGA_XXAA)) && defined(NRF_APPLICATION)
+#if (defined(NRF54LS05B_ENGA_XXAA)) && defined(NRF_APPLICATION)
     #include"templates/nrfx_config_nrf54ls05b_enga_application.h"
 #else
     #error "Unknown device."
