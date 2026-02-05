620580cd7dedc9e05b09437c21e12f40b978a8b5
Kristoffer Skøien <kristoffer.skoien@nordicsemi.no>
2026-02-05 12:55:59 +0100

    Applications: nrf5340_audio: Added new functions pres MTL
    OCT-3559
    
    Signed-off-by: Kristoffer Skøien <kristoffer.skoien@nordicsemi.no>

--- applications/nrf5340_audio/src/bluetooth/bt_stream/le_audio.h
+++ applications/nrf5340_audio/src/bluetooth/bt_stream/le_audio.h
@@ -17,6 +17,7 @@
 #define _LE_AUDIO_H_
 
 #include <zephyr/bluetooth/audio/bap.h>
+#include <zephyr/bluetooth/audio/cap.h>
 #include <zephyr/net_buf.h>
 #include <audio_defines.h>
 
@@ -235,6 +236,8 @@ bool le_audio_freq_check(const struct bt_audio_codec_cfg *codec);
 void le_audio_print_codec(const struct bt_audio_codec_cfg *codec, enum bt_audio_dir dir);
 
 void le_audio_print_qos_from_stream(struct bt_bap_stream const *const stream);
+
+int le_audio_print_cig(struct bt_cap_unicast_group const *const unicast_group);
 /**
  * @}
  */
