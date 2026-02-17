922868452aba160f47286fecdb822752e8adb53d
Michal Frankiewicz <michal.frankiewicz@nordicsemi.no>
2026-02-17 13:11:30 +0100

    treewide: Replace clock api with clock api v2
    Replaced old clock_control.h api with nrf_clock_control.h api.
    Old api is still available when CONFIG_CLOCK_CONTROL_NRF is set.
    Replaced nrfx_clock.h api calls with nrfx_clock_... calls.
    
    Signed-off-by: Michal Frankiewicz <michal.frankiewicz@nordicsemi.no>

--- tests/bluetooth/iso/src/main.c
+++ tests/bluetooth/iso/src/main.c
@@ -6,21 +6,15 @@
 #include <zephyr/bluetooth/bluetooth.h>
 #include "iso_broadcast_src.h"
 #include "iso_broadcast_sink.h"
-#include "nrfx_clock.h"
+#include "nrfx_clock_hfclk.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_ISO_TEST_LOG_LEVEL);
 
 static int hfclock_config_and_start(void)
 {
-	int ret;
-
 	/* Use this to turn on 128 MHz clock for cpu_app */
-	ret = nrfx_clock_divider_set(NRF_CLOCK_DOMAIN_HFCLK, NRF_CLOCK_HFCLK_DIV_1);
-
-	if (ret) {
-		return ret;
-	}
+	nrfx_clock_hfclk_divider_set(NRF_CLOCK_HFCLK_DIV_1);
 
 	nrf_clock_hfclk_t clk_src;
 
