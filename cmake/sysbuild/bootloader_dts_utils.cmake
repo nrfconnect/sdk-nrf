064c13067328d83fe0dd57be44eba82188ea81df
Dominik Ermel <dominik.ermel@nordicsemi.no>
2026-02-19 18:26:04 +0000

    ke: Add function to read partition size from DTS defined partition
    Allows to pick parition size by label or node. Size is in bytes.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- cmake/sysbuild/bootloader_dts_utils.cmake
+++ cmake/sysbuild/bootloader_dts_utils.cmake
@@ -108,6 +108,74 @@ function(dt_partition_addr var)
   set(${var} "${dt_partition_offset}" PARENT_SCOPE)
 endfunction()
 
+# Usage:
+#   dt_partition_size(<var> [LABEL <label>|PATH <path>] [TARGET <target>])
+#
+# Get the partition size, based on the path or label.
+# The value will be returned in the <var> parameter.
+#
+# The <path> value may be any of these:
+#
+# - absolute path to a node, like '/foo/bar'
+# - a node alias, like 'my-alias'
+# - a node alias followed by a path to a child node, like 'my-alias/child-node'
+#
+# Results can be:
+# - The size in bytes.
+# - <var>-NOTFOUND if node does not exists or does not have a register block at the requested index
+#   or with the requested name
+#
+# <var>          : Return variable where the size value will be stored
+# LABEL <label>  : Node label
+# PATH <path>    : Node path
+# TARGET <target>: Optional target to retrieve devicetree information from
+# REQUIRED       : Generate a fatal error if the node is not found
+function(dt_partition_size var)
+  cmake_parse_arguments(arg_DT_PARTITION "REQUIRED" "LABEL;PATH;TARGET" "" ${ARGN})
+
+  zephyr_check_arguments_required(${CMAKE_CURRENT_FUNCTION} arg_DT_PARTITION PATH LABEL)
+  zephyr_check_arguments_exclusive(${CMAKE_CURRENT_FUNCTION} arg_DT_PARTITION PATH LABEL)
+
+  if(NOT arg_DT_PARTITION_PATH)
+    # Calculate the partition path, based on the label.
+    dt_nodelabel(arg_DT_PARTITION_PATH NODELABEL "${arg_DT_PARTITION_LABEL}" TARGET
+      "${arg_DT_PARTITION_TARGET}")
+    if(NOT DEFINED arg_DT_PARTITION_PATH)
+      if(arg_DT_PARTITION_REQUIRED)
+        message(FATAL_ERROR "dt_partition_addr: Unable to find a node with label: "
+                            "${arg_DT_PARTITION_LABEL} for target: ${arg_DT_PARTITION_TARGET}")
+      else()
+        set(${var} "${var}-NOTFOUND" PARENT_SCOPE)
+        return()
+      endif()
+    endif()
+  else()
+    # Check if a partition with the given path exists.
+    dt_node_exists(arg_DT_PARTITION_EXISTS PATH "${arg_DT_PARTITION_PATH}" TARGET
+      "${arg_DT_PARTITION_TARGET}")
+    if(NOT arg_DT_PARTITION_EXISTS)
+      if(arg_DT_PARTITION_REQUIRED)
+        message(FATAL_ERROR "dt_partition_addr: Unable to find a node with path: "
+                            "${arg_DT_PARTITION_PATH} for target: ${arg_DT_PARTITION_TARGET}")
+      else()
+        set(${var} "${var}-NOTFOUND" PARENT_SCOPE)
+        return()
+      endif()
+    endif()
+  endif()
+
+  # Get the list of partitions and subpartitions.
+  dt_comp_path(fixed_partitions COMPATIBLE "fixed-partitions" TARGET "${arg_DT_PARTITION_TARGET}")
+  dt_comp_path(fixed_subpartitions COMPATIBLE "fixed-subpartitions" TARGET
+    "${arg_DT_PARTITION_TARGET}")
+
+  # Read the partition size
+  dt_prop(dt_partition_size PATH "${arg_DT_PARTITION_PATH}" PROPERTY "reg" INDEX 1 TARGET
+    "${arg_DT_PARTITION_TARGET}")
+
+  set(${var} "${dt_partition_size}" PARENT_SCOPE)
+endfunction()
+
 function(get_address_from_dt_partition_nodelabel label address)
   dt_partition_addr(${address} LABEL "${label}" TARGET "${DEFAULT_IMAGE}" ABSOLUTE REQUIRED)
   set(${address} "${${address}}" PARENT_SCOPE)
