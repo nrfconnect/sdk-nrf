ae7d7b36ab185fd49cbf4be8f12e055a32b02c9d
Aleksandar Stanoev <aleksandar.stanoev@nordicsemi.no>
2026-03-02 13:13:15 +0000

    bluetooth: controller: Experimental support for LE Flushable ACL Data
    Add experimental support for the LE Flushable ACL Data feature to SDC.
    This adds support for the LE Read and Write Automatic Flush Timeout HCI
    commands and the automatically flushable HCI packet boundary flag.
    
    Signed-off-by: Aleksandar Stanoev <aleksandar.stanoev@nordicsemi.no>

--- subsys/bluetooth/controller/hci_driver.c
+++ subsys/bluetooth/controller/hci_driver.c
@@ -924,6 +924,10 @@ static void configure_supported_features(void)
 		sdc_support_extended_feature_set();
 	}
 
+	if (IS_ENABLED(CONFIG_BT_CTLR_LE_FLUSHABLE_ACL_DATA)) {
+		sdc_support_flushable_acl_data();
+	}
+
 	if (IS_ENABLED(CONFIG_BT_CTLR_CHANNEL_SOUNDING_TEST)) {
 		sdc_support_channel_sounding_test();
 	}
