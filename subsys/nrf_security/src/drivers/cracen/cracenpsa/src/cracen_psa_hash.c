2c42616bc72237db7f19226ed80c138065000ad7
Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>
2026-03-04 10:03:21 +0100

    nrf_security: CRACEN: Update psa_hash
    Update psa_hash to use internal helpers.
    This saves some space.
    
    Signed-off-by: Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/src/cracen_psa_hash.c
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/src/cracen_psa_hash.c
@@ -27,8 +27,6 @@ _Static_assert(SX_HASH_MAX_ENABLED_BLOCK_SIZE != 1,
 psa_status_t cracen_hash_compute(psa_algorithm_t alg, const uint8_t *input, size_t input_length,
 				 uint8_t *hash, size_t hash_size, size_t *hash_length)
 {
-	int sx_status;
-	struct sxhash c = {0};
 	const struct sxhashalg *sx_hash_algo = NULL;
 
 	psa_status_t psa_status = hash_get_algo(alg, &sx_hash_algo);
@@ -43,30 +41,7 @@ psa_status_t cracen_hash_compute(psa_algorithm_t alg, const uint8_t *input, size
 
 	*hash_length = sx_hash_get_alg_digestsz(sx_hash_algo);
 
-	sx_status = sx_hw_reserve(&c.dma, SX_HW_RESERVE_DEFAULT);
-	if (sx_status != SX_OK) {
-		goto exit;
-	}
-
-	sx_status = sx_hash_create(&c, sx_hash_algo, sizeof(c));
-	if (sx_status) {
-		goto exit;
-	}
-
-	sx_status = sx_hash_feed(&c, input, input_length);
-	if (sx_status != SX_OK) {
-		goto exit;
-	}
-
-	sx_status = sx_hash_digest(&c, hash);
-	if (sx_status != SX_OK) {
-		goto exit;
-	}
-
-	sx_status = sx_hash_wait(&c);
-
-exit:
-	return silex_statuscodes_to_psa(sx_status);
+	return silex_statuscodes_to_psa(cracen_hash_input(input, input_length, sx_hash_algo, hash));
 }
 
 psa_status_t cracen_hash_setup(cracen_hash_operation_t *operation, psa_algorithm_t alg)
@@ -89,22 +64,15 @@ static int init_or_resume_context(cracen_hash_operation_t *operation)
 	int sx_status;
 
 	/* The SX driver expects to have gotten some data, before being able to
-	 * save the context,
-	 * therefore the create call is done here, then feed data and then safe
-	 * the context.
+	 * save the context; create or resume here.
 	 */
 	if (!operation->has_saved_state) {
 		sx_status = sx_hash_create(&operation->sx_ctx, operation->sx_hash_algo,
 					   sizeof(operation->sx_ctx));
-		if (sx_status != SX_OK) {
-			return sx_status;
-		}
-		operation->has_saved_state = true;
-
+		operation->has_saved_state = (sx_status == SX_OK);
 	} else {
-		/* Get back the old state if previous operation had been done */
 		sx_status = sx_hash_resume_state(&operation->sx_ctx);
-
+	}
 	return sx_status;
 }
 
@@ -112,6 +80,7 @@ psa_status_t cracen_hash_update(cracen_hash_operation_t *operation, const uint8_
 				const size_t input_length)
 {
 	int sx_status;
+	size_t block_sz;
 	size_t input_chunk_length = 0;
 	size_t remaining_bytes = 0;
 
@@ -125,12 +94,12 @@ psa_status_t cracen_hash_update(cracen_hash_operation_t *operation, const uint8_
 	 */
 	__ASSERT_NO_MSG(input != NULL);
 
+	block_sz = sx_hash_get_alg_blocksz(operation->sx_hash_algo);
 	if (input_length < operation->bytes_left_for_next_block) {
 		/* sx_hash_feed doesn't buffer the input data until sx_hash_wait
 		 * is called so a local buffer is used.
 		 */
-		size_t offset = sx_hash_get_alg_blocksz(operation->sx_hash_algo) -
-				operation->bytes_left_for_next_block;
+		size_t offset = block_sz - operation->bytes_left_for_next_block;
 		memcpy(operation->input_buffer + offset, input, input_length);
 		operation->bytes_left_for_next_block -= input_length;
 
@@ -143,7 +112,6 @@ psa_status_t cracen_hash_update(cracen_hash_operation_t *operation, const uint8_
 		return silex_statuscodes_to_psa(sx_status);
 	}
 
-	/* Initialize or resume an already initialized context. */
 	sx_status = init_or_resume_context(operation);
 	if (sx_status != SX_OK) {
 		goto exit;
@@ -151,8 +119,7 @@ psa_status_t cracen_hash_update(cracen_hash_operation_t *operation, const uint8_
 
 	/* Feed the data that are currently in the input buffer to the driver. */
 	sx_status = sx_hash_feed(&operation->sx_ctx, operation->input_buffer,
-				 sx_hash_get_alg_blocksz(operation->sx_hash_algo) -
-					 operation->bytes_left_for_next_block);
+				 block_sz - operation->bytes_left_for_next_block);
 	if (sx_status != SX_OK) {
 		goto exit;
 	}
@@ -162,7 +129,7 @@ psa_status_t cracen_hash_update(cracen_hash_operation_t *operation, const uint8_
 	 * power of two.
 	 */
 	input_chunk_length = input_length - ((input_length - operation->bytes_left_for_next_block) %
-					     sx_hash_get_alg_blocksz(operation->sx_hash_algo));
+					     block_sz);
 	remaining_bytes = input_length - input_chunk_length;
 
 	/* forward the data to the driver and process the data */
@@ -184,7 +151,7 @@ psa_status_t cracen_hash_update(cracen_hash_operation_t *operation, const uint8_
 	/* As we just passed the last block reset the remaining size and clean
 	 * input buffer
 	 */
-	operation->bytes_left_for_next_block = sx_hash_get_alg_blocksz(operation->sx_hash_algo);
+	operation->bytes_left_for_next_block = block_sz;
 	safe_memzero(operation->input_buffer, sizeof(operation->input_buffer));
 
 	/* Copy the remaining bytes to the local input buffer */
@@ -202,6 +169,7 @@ psa_status_t cracen_hash_finish(cracen_hash_operation_t *operation, uint8_t *has
 				size_t *hash_length)
 {
 	int sx_status;
+	size_t block_sz;
 
 	__ASSERT_NO_MSG(hash_length != NULL);
 
@@ -219,9 +187,9 @@ psa_status_t cracen_hash_finish(cracen_hash_operation_t *operation, uint8_t *has
 		goto exit;
 	}
 
+	block_sz = sx_hash_get_alg_blocksz(operation->sx_hash_algo);
 	sx_status = sx_hash_feed(&operation->sx_ctx, operation->input_buffer,
-				 sx_hash_get_alg_blocksz(operation->sx_hash_algo) -
-					 operation->bytes_left_for_next_block);
+				 block_sz - operation->bytes_left_for_next_block);
 	if (sx_status != SX_OK) {
 		goto exit;
 	}
