d8941de5ae6b5288546ccc784ba8a041a3ddd4e2
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2026-03-06 17:17:17 +0100

    samples/dfu/single_slot: add heartbeet LED to app
    Application is blinking the LED when it is running.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- samples/dfu/single_slot/src/main.c
+++ samples/dfu/single_slot/src/main.c
@@ -5,9 +5,19 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+void io_led_init(void);
+void io_led_set(int value);
+
+LOG_MODULE_REGISTER(single_slot, LOG_LEVEL_DBG);
 
 int main(void)
 {
+	io_led_init();
+
 	printk("Starting single_slot sample\n");
 
 	/* using __TIME__ ensure that a new binary will be built on every
@@ -15,5 +25,42 @@ int main(void)
 	 */
 	printk("build time: " __DATE__ " " __TIME__ "\n");
 
+
+	while (1) {
+		io_led_set(1);
+		k_msleep(100);
+		io_led_set(0);
+		k_msleep(900);
+	}
 	return 0;
+}
+
+/*
+ * The led0 devicetree alias is optional.
+ */
+#if DT_NODE_EXISTS(DT_ALIAS(led0))
+#define LED0_NODE DT_ALIAS(led0)
+#endif
+
+#if DT_NODE_HAS_STATUS(LED0_NODE, okay) && DT_NODE_HAS_PROP(LED0_NODE, gpios)
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+#else
+/* A build error here means your board isn't set up to drive an LED. */
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
+
+void io_led_init(void)
+{
+	if (!device_is_ready(led0.port)) {
+		LOG_ERR("Didn't find LED device referred by the LED0_NODE\n");
+		return;
+	}
+
+	gpio_pin_configure_dt(&led0, GPIO_OUTPUT);
+	gpio_pin_set_dt(&led0, 0);
+}
+
+void io_led_set(int value)
+{
+	gpio_pin_set_dt(&led0, value);
 }
