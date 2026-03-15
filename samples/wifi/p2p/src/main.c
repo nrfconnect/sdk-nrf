b72fccafdae5f6911ced825620e980373bd4deca
Triveni Danda <triveni.danda@nordicsemi.no>
2026-03-16 00:00:06 +0530

    samples: wifi: Add P2P CLI Mode
    Add P2P sample demonstrating CLI mode operation.
    In this mode, the device performs peer discovery and
    initiates a P2P connection with the configured peer.
    
    Fixes SHEL-3862.
    
    Signed-off-by: Triveni Danda <triveni.danda@nordicsemi.no>

---
+++ samples/wifi/p2p/src/main.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ * @brief Wi-Fi P2P sample - CLI (client) or GO (group owner) mode.
+ */
+
+#include "p2p.h"
+
+int main(void)
+{
+
+#ifdef CONFIG_P2P_SAMPLE_CLI_MODE
+	p2p_cli_run();
+#endif
+
+	return 0;
+}
