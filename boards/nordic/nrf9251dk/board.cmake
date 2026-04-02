1edbe7878ed32bc151e40a287235f81c2c9dde1e
Jari Tervonen <jari.tervonen@nordicsemi.no>
2026-04-02 15:05:10 +0300

    soc: Add nrf9251 board and soc.
    Introduce new board called nrf9251dk, associated devicetree files
    and drivers for the SoC.
    
    Signed-off-by: Jari Tervonen <jari.tervonen@nordicsemi.no>

---
+++ boards/nordic/nrf9251dk/board.cmake
@@ -0,0 +1,25 @@
+# Copyright (c) 2026 Nordic Semiconductor ASA
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
+
+if(CONFIG_BOARD_NRF9251DK_NRF9251_CPUAPP)
+  set(jlink_script_file ${CMAKE_CURRENT_LIST_DIR}/support/nrf9251_cpuapp.JLinkScript)
+
+  board_runner_args(jlink "--device=CORTEX-M33" "--speed=4000" "--tool-opt=-jlinkscriptfile ${jlink_script_file}")
+
+  include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+endif()
+
+if(CONFIG_BOARD_NRF9251DK_NRF9251_CPUPPR OR CONFIG_BOARD_NRF9251DK_NRF9251_CPUFLPR)
+  if(CONFIG_SOC_NRF9251_CPUPPR)
+    set(jlink_script_file ${CMAKE_CURRENT_LIST_DIR}/support/nrf9251_cpuppr.JLinkScript)
+  else()
+    set(jlink_script_file ${CMAKE_CURRENT_LIST_DIR}/support/nrf9251_cpuflpr.JLinkScript)
+  endif()
+
+  # Workaround: Use device nRF54L15_RV32 until something else is defined.
+  board_runner_args(jlink "--device=nRF54L15_RV32" "--speed=4000" "--tool-opt=-jlinkscriptfile ${jlink_script_file}")
+
+  include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+endif()
