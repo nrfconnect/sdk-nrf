438e90b43e822471a4d16f984a93cc0f6c59fba7
Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>
2026-04-15 15:47:32 +0200

    samples: protectedmem_periphconf: expand and improve sample
    Show both how to configure PROTECTEDMEM and SECONDARY.PROTECTEDMEM,
    and demonstrate that repairing the corrupted PROTECTEDMEM region
    from secondary will fix the system. Update the sample to use the
    flash API instead of writing addresses directly.
    
    Also show that the secondary firmware is informed by IronSide SE
    through the boot report that it was booted because the PROTECTEDMEM
    region was corrupted.
    
    Signed-off-by: Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>

--- samples/ironside_se/protectedmem_periphconf/secondary/src/main.c
+++ samples/ironside_se/protectedmem_periphconf/secondary/src/main.c
@@ -6,14 +6,52 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/drivers/flash.h>
+#include <zephyr/storage/flash_map.h>
+#include <zephyr/arch/cpu.h>
+#include <ironside/se/boot_report.h>
+
+#define MRAM_WRITE_BLOCK_SIZE 16
+#define ERASE_OFFSET                                                                               \
+	(PARTITION_OFFSET(protectedmem_partition) + PARTITION_SIZE(protectedmem_partition) -       \
+	 MRAM_WRITE_BLOCK_SIZE)
 
 int main(void)
 {
-	printk("PROTECTEDMEM integrity check failed - secondary firmware booted\n");
+	printk("Secondary: Secondary firmware booted\n");
 
-	while (1) {
-		k_msleep(1000);
+	const struct ironside_se_boot_report *const report = IRONSIDE_SE_BOOT_REPORT;
+
+	if (report->init_context.boot_reason == IRONSIDE_SE_BOOT_REASON_BOOTERROR) {
+		if (report->init_context.trigger_init_status.boot_error ==
+		    IRONSIDE_SE_BOOT_ERROR_UICR_PROTECTEDMEM_INTEGRITY_FAILED) {
+			printk("Secondary: Booted because of PROTECTEDMEM failure\n");
+		} else {
+			printk("Secondary: Booted because of unexpected boot error: 0x%x\n",
+			       report->init_context.trigger_init_status.boot_error);
+		}
+	} else {
+		printk("Secondary: Booted because of unexpected reason: 0x%x\n",
+		       report->init_context.boot_reason);
 	}
+
+	printk("Secondary: Reverting test pattern write to protected memory...\n");
+
+	const struct device *flash_dev = PARTITION_DEVICE(protectedmem_partition);
+	int rc = flash_erase(flash_dev, ERASE_OFFSET, MRAM_WRITE_BLOCK_SIZE);
+
+	if (rc) {
+		printk("Secondary: flash_erase failed: %d\n", rc);
+		return rc;
+	}
+
+	printk("Secondary: Rebooting after 1s. Primary firmware should boot again since the "
+	       "corruption was fixed.\n");
+
+	k_msleep(1000);
+	NVIC_SystemReset();
+
+	CODE_UNREACHABLE;
 
 	return 0;
 }
