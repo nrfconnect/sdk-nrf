f68984e76b02b7eb2792697b44d9853305f87c96
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2026-04-16 15:09:52 +0200

    sysbuild: Align Matter to non-PM builds
    Added matter.cmake to use image flasher for generating
    factory data. Aligned CMakeLists.txt with changes in Matter SDK.
    
    Signed-off-by: Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>

---
+++ sysbuild/matter.cmake
@@ -0,0 +1,16 @@
+
+#
+# Copyright (c) 2026 Nordic Semiconductor
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+#
+
+if(SB_CONFIG_ZEPHYR_CONNECTEDHOMEIP_MODULE AND SB_CONFIG_MATTER AND
+  SB_CONFIG_MATTER_FACTORY_DATA_GENERATE AND NOT SB_CONFIG_PARTITION_MANAGER
+)
+  include(image_flasher.cmake)
+  add_image_flasher(
+    NAME matter_factory_data
+    HEX_FILE "${CMAKE_BINARY_DIR}/matter_factory_data/zephyr/factory_data.hex"
+  )
+endif()
