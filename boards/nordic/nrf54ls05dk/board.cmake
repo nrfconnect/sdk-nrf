ce5ef6bfcaec0ab78e15d2b6293d7c9f86c467aa
Michał Stasiak <michal.stasiak@nordicsemi.no>
2026-04-17 15:39:12 +0200

    boards: nrf54ls05dk: use dedicated JLink device
    JLink now supports nRF54LS05. Use dedicated device.
    
    Signed-off-by: Michał Stasiak <michal.stasiak@nordicsemi.no>

--- boards/nordic/nrf54ls05dk/board.cmake
+++ boards/nordic/nrf54ls05dk/board.cmake
@@ -1,8 +1,10 @@
 # Copyright (c) 2025 Nordic Semiconductor ASA
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-if(CONFIG_SOC_NRF54LS05A_CPUAPP OR CONFIG_SOC_NRF54LS05B_CPUAPP)
-  board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
+if(CONFIG_SOC_NRF54LS05A_CPUAPP)
+  board_runner_args(jlink "--device=NRF54LS05A_M33" "--speed=4000")
+elseif(CONFIG_SOC_NRF54LS05B_CPUAPP)
+  board_runner_args(jlink "--device=NRF54LS05B_M33" "--speed=4000")
 endif()
 
 include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
