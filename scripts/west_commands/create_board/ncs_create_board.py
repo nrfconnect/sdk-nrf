107884da03333da454060daa1c52be434ed7a17b
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2026-04-20 09:24:18 +0100

    scripts: west_commands: create_board: Disable PM for non nRF91
    Disables partition manager being enabled by default for
    board templates, with exception to nRF91 which still requires PM
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- scripts/west_commands/create_board/ncs_create_board.py
+++ scripts/west_commands/create_board/ncs_create_board.py
@@ -163,6 +163,12 @@ class NcsCreateBoard(WestCommand):
         tmpl = TEMPLATE_DIR / series / "board-pinctrl.dtsi"
         shutil.copy(tmpl, out_dir / f"{ input['board'] }-pinctrl.dtsi")
 
+        try:
+            tmpl = TEMPLATE_DIR / series / "Kconfig.sysbuild"
+            shutil.copy(tmpl, out_dir)
+        except FileNotFoundError:
+            pass
+
         tmpl = env.get_template("board.cmake.jinja2")
         with open(out_dir / "board.cmake", "w") as f:
             f.write(tmpl.render())
