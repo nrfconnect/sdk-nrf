edad2e2d50fc56d38e5d200437634ce549e03912
Nicolae Dicu <nicolae.dicu@nordicsemi.no>
2026-05-15 11:36:19 +0300

    scripts: sbom: Fix license headers
    Update license headers to allow ncs-sbom tool to detect it.
    
    Signed-off-by: Nicolae Dicu <nicolae.dicu@nordicsemi.no>

--- doc/_scripts/merge_search_indexes.py
+++ doc/_scripts/merge_search_indexes.py
@@ -13,6 +13,8 @@ Usage
 python merge_search_indexes.py -b path/to/doc/build/dir
 
 Copyright (c) 2021 Nordic Semiconductor ASA
+
+SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 """
 
 import argparse
