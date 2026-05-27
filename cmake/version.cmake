9a084fee22073f0353053d2e22fe99056832479a
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2026-05-27 12:12:12 +0200

    cmake: adopt VERSION to common VERSION file format
    This commit adopts the NCS top-level VERSION file to follow the same
    format as VERSION files used by samples, apps, and Zephyr.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,20 +1,9 @@
-math(EXPR NCS_VERSION_CODE "(${NCS_VERSION_MAJOR} << 16) + (${NCS_VERSION_MINOR} << 8)  + (${NCS_VERSION_PATCH})")
-math(EXPR NCS_VERSION_NUMBER ${NCS_VERSION_CODE} OUTPUT_FORMAT HEXADECIMAL)
-
 add_custom_command(
   OUTPUT ${PROJECT_BINARY_DIR}/include/generated/ncs_version.h
   COMMAND ${CMAKE_COMMAND} -DZEPHYR_BASE=${ZEPHYR_BASE}
     -DOUT_FILE=${PROJECT_BINARY_DIR}/include/generated/ncs_version.h
     -DVERSION_TYPE=NCS
-    # We don't want Zephyr to parse the NCS VERSION file as it is not conforming to Zephyr VERSION
-    # format. Pointing to a non-existing file allows us full control from CMake instead.
-    -DVERSION_FILE=${NRF_DIR}/VERSIONFILEDUMMY
-    -DNCS_VERSION_CODE=${NCS_VERSION_CODE}
-    -DNCS_VERSION_NUMBER=${NCS_VERSION_NUMBER}
-    -DNCS_VERSION_MAJOR=${NCS_VERSION_MAJOR}
-    -DNCS_VERSION_MINOR=${NCS_VERSION_MINOR}
-    -DNCS_PATCHLEVEL=${NCS_VERSION_PATCH}
-    -DNCS_VERSION_STRING=${NCS_VERSION}
+    -DVERSION_FILE=${NRF_DIR}/VERSION
     -P ${ZEPHYR_BASE}/cmake/gen_version_h.cmake
     DEPENDS ${NRF_DIR}/VERSION ${git_dependency}
 )
