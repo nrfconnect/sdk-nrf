92ce7685bddb832462a17b3395a58749a9f5c138
Triveni Danda <triveni.danda@nordicsemi.no>
2026-06-05 16:31:39 +0530

    wifi: samples: sta: Make LED support configurable
    Move LED functionality into a dedicated file and add a
    LED_SUPPORT Kconfig option to control its inclusion.
    
    This enables the sample to be built on platforms that do not
    provide GPIO-based LED support.
    
    Signed-off-by: Triveni Danda <triveni.danda@nordicsemi.no>

---
+++ samples/wifi/sta/src/led.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include "led.h"
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(led, CONFIG_LOG_DEFAULT_LEVEL);
+
+/* 1000 msec = 1 sec */
+#define LED_SLEEP_TIME_MS   100
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+static bool connected;
+
+void led_set_connected(bool is_connected)
+{
+	connected = is_connected;
+}
+
+static void toggle_led(void)
+{
+	int ret;
+
+	if (!device_is_ready(led.port)) {
+		LOG_ERR("LED device is not ready");
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		LOG_ERR("Error %d: failed to configure LED pin", ret);
+		return;
+	}
+
+	while (1) {
+		if (connected) {
+			gpio_pin_toggle_dt(&led);
+			k_msleep(LED_SLEEP_TIME_MS);
+		} else {
+			gpio_pin_set_dt(&led, 0);
+			k_msleep(LED_SLEEP_TIME_MS);
+		}
+	}
+}
+
+K_THREAD_DEFINE(led_thread_id, 1024, toggle_led, NULL, NULL, NULL,
+		7, 0, 0);
