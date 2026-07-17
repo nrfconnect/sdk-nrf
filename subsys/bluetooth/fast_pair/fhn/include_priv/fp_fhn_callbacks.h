a9ab299b69b38f2440a05575f26518a1bf7cf9d5
Mateusz Kapala <mateusz.kapala@nordicsemi.no>
2026-07-17 16:30:31 +0200

    bluetooth: fast_pair: fhn: Integrate with DULT multi-user coexistence
    Wire new DULT multi-user feature (DULT API variant V2) into Google
    Find Hub Network stack, to allow coexistence at the pre-association
    state with other accessory-locating networks.
    
    Jira: NCSDK-39238
    
    Signed-off-by: Mateusz Kapala <mateusz.kapala@nordicsemi.no>

--- subsys/bluetooth/fast_pair/fhn/include_priv/fp_fhn_callbacks.h
+++ subsys/bluetooth/fast_pair/fhn/include_priv/fp_fhn_callbacks.h
@@ -43,6 +43,19 @@ void fp_fhn_callbacks_conn_authenticated_notify(struct bt_conn *conn);
  */
 void fp_fhn_callbacks_provisioning_state_changed_notify(bool provisioned);
 
+/** Notify the callback layer about a DULT ownership state change. Relates to the
+ *  DULT v2 API (@kconfig{CONFIG_DULT_API_VARIANT_V2}) only and fires on every DULT
+ *  ownership transition. A registered listener is mandatory only when more than one
+ *  DULT user is configured (@kconfig{CONFIG_DULT_USER_MAX} greater than one) and
+ *  optional with a single user. With the v1 API this is never called, as DULT emits
+ *  no ownership transitions.
+ *
+ *  @param state    true if ownership was claimed, false if released.
+ *  @param is_owner When @p state is true, true if the FHN stack became the owner.
+ *                  When @p state is false, true if the FHN stack was the owner.
+ */
+void fp_fhn_callbacks_dult_ownership_state_changed_notify(bool state, bool is_owner);
+
 /** Register the information callbacks internally. The internally registered
  *  callbacks have higher priority than the callbacks registered with the public
  *  Fast Pair API. You can call this function only in the disabled state of the
