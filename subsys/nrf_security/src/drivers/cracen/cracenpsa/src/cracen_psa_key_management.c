5327c0f8bcec3f366572a0fccac5ab5b7734e0df
Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>
2026-07-20 15:58:23 +0200

    nrf_security: cracen: Wipe stack key in generate_key_for_kmu
    The plaintext key stayed on the stack after generation.
    Zeroize it on every exit path.
    
    Signed-off-by: Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/src/cracen_psa_key_management.c
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/src/cracen_psa_key_management.c
@@ -224,15 +224,20 @@ static psa_status_t generate_key_for_kmu(const psa_key_attributes_t *attributes,
 	} else if (key_type == PSA_KEY_TYPE_AES || key_type == PSA_KEY_TYPE_HMAC ||
 		   key_type == PSA_KEY_TYPE_CHACHA20) {
 		status = cracen_get_random(NULL, key, key_size);
-		if (status != PSA_SUCCESS) {
-			return status;
-		}
 	} else {
 		return PSA_ERROR_NOT_SUPPORTED;
 	}
 
-	return cracen_import_key(attributes, key, key_size, key_buffer, key_buffer_size,
-				 key_buffer_length, &key_bits);
+	if (status != PSA_SUCCESS) {
+		goto cleanup;
+	}
+
+	status = cracen_import_key(attributes, key, key_size, key_buffer, key_buffer_size,
+				   key_buffer_length, &key_bits);
+
+cleanup:
+	safe_memzero(key, sizeof(key));
+	return status;
 }
 
 psa_status_t cracen_generate_key(const psa_key_attributes_t *attributes, uint8_t *key_buffer,
