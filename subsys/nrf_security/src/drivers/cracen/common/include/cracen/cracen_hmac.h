16e81a13361b19ed44bcded94aa25542a6c6d9ed
Anton Zyma <anton.zyma@nordicsemi.no>
2026-08-04 18:24:03 +0300

    nrf_security: CRACEN: Refactor names of HMAC helper functions
    Names of helper functions used to produce HMAC were
    renamed to have the same style and "cracen_" prefix.
    
    Signed-off-by: Anton Zyma <anton.zyma@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/common/include/cracen/cracen_hmac.h
+++ subsys/nrf_security/src/drivers/cracen/common/include/cracen/cracen_hmac.h
@@ -20,11 +20,11 @@
 #include <psa/crypto.h>
 #include <stdint.h>
 
-int mac_create_hmac(const struct sxhashalg *hashalg, struct sxhash *hashopctx, const uint8_t *key,
-		    size_t keysz, uint8_t *workmem, size_t workmemsz);
+int cracen_hmac_create(const struct sxhashalg *hashalg, struct sxhash *hashopctx,
+		       const uint8_t *key, size_t keysz, uint8_t *workmem, size_t workmemsz);
 
-int hmac_produce(struct sxhash *hashctx, const struct sxhashalg *hashalg, uint8_t *out, size_t sz,
-		 uint8_t *workmem);
+int cracen_hmac_produce(struct sxhash *hashctx, const struct sxhashalg *hashalg, uint8_t *out,
+			size_t sz, uint8_t *workmem);
 
 /** @} */
 
