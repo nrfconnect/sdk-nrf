7e1df369388384071afb3e3d48b11e0e8a6e9daf
Antoni Duda <antoni.duda@nordicsemi.no>
2026-08-06 11:44:20 +0200

    doc: zephyr: reuse upstream zephyr.doxyfile.in
    Instead of maintaining our own copy of a file keep a small wrapper
    which overwrites only the necessary values.
    
    Signed-off-by: Antoni Duda <antoni.duda@nordicsemi.no>

--- doc/zephyr/conf.py
+++ doc/zephyr/conf.py
@@ -74,6 +74,12 @@ if kconfig_mapping:
 
 # -- Options for doxyrunner plugin ---------------------------------------------
 
+_UPSTREAM_DOXYFILE = utils.get_builddir() / "upstream.doxyfile"
+
+def get_upstream_doxyfile() -> str:
+    with open(_UPSTREAM_DOXYFILE) as f:
+        return f.read()
+
 _doxyrunner_outdir = utils.get_builddir() / "html" / "zephyr" / "doxygen"
 
 doxyrunner_doxygen = os.environ.get("DOXYGEN_EXECUTABLE", "doxygen")
@@ -87,6 +93,7 @@ doxyrunner_projects = {
             "DOCSET_SOURCE_BASE": str(ZEPHYR_BASE),
             "DOCSET_BUILD_DIR": str(_doxyrunner_outdir),
             "DOCSET_VERSION": version,
+            "ZEPHYR_UPSTREAM_DOXYFILE": get_upstream_doxyfile(),
         }
     }
 }
