30b74e1762c758ab205dff4808f5a69c33e45cb9
Sebastian Głąb <sebastian.glab@nordicsemi.no>
2026-08-19 11:37:52 +0200

    tests: benchmarks: Run idle_spim_loopback on nrf9251dk
    Add overlays required to run the idle_spim_loopback power consumption
    test on nrf9251dk/nrf9251/cpuapp and cpuppr.
    
    Signed-off-by: Sebastian Głąb <sebastian.glab@nordicsemi.no>

--- tests/benchmarks/multicore/idle_spim_loopback/sysbuild.cmake
+++ tests/benchmarks/multicore/idle_spim_loopback/sysbuild.cmake
@@ -4,14 +4,22 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 
-if(SB_CONFIG_SOC_NRF54H20)
-  set(REMOTE_SOURCE_DIR ${ZEPHYR_NRF_MODULE_DIR}/tests/benchmarks/power_consumption/common/remote_sleep_forever)
+set(TEST_REMOTE_SOURCE_DIR ${ZEPHYR_NRF_MODULE_DIR}/tests/benchmarks/power_consumption/common/remote_sleep_forever)
 
-  # Add remote project
+if(SB_CONFIG_SOC_NRF54H20)
+  # On nrf54h20, no matter if APP or PPR, build remote_sleep_forever on Radio
+  set(TEST_REMOTE_BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpurad)
+elseif(SB_CONFIG_SOC_NRF9251)
+  # On nrf9251 use PPR instead of Radio
+  set(TEST_REMOTE_BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpuppr)
+endif()
+
+if(NOT SB_CONFIG_SOC_NRF9251_CPUPPR)
+  # Add remote project unless test runs on nrf9251 PPR
   ExternalZephyrProject_Add(
     APPLICATION remote
-    SOURCE_DIR ${REMOTE_SOURCE_DIR}
-    BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpurad
+    SOURCE_DIR ${TEST_REMOTE_SOURCE_DIR}
+    BOARD ${TEST_REMOTE_BOARD}
     BOARD_REVISION ${BOARD_REVISION}
   )
 endif()
